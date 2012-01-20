5e12f48e7b08d1ca57f97684326db317d8ad61fe
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-01-20 13:38:00 -0600

    Issue #202 - restoring rpm directory to ss_sync.
    Former-commit-id: 0213efde5229bfb0be1e5689b033cb5198be5174 [formerly d537df4ab073ae474e929a76e67a61f87cce8ffc] [formerly 4453ad2d10cb02790c92de600dd3e100a8de2653] [formerly 4453ad2d10cb02790c92de600dd3e100a8de2653 [formerly 65e1e4ee0be1cc2d90c6a02328c88c73301c6764]] [formerly 0213efde5229bfb0be1e5689b033cb5198be5174 [formerly d537df4ab073ae474e929a76e67a61f87cce8ffc] [formerly 4453ad2d10cb02790c92de600dd3e100a8de2653] [formerly 4453ad2d10cb02790c92de600dd3e100a8de2653 [formerly 65e1e4ee0be1cc2d90c6a02328c88c73301c6764]] [formerly ea1c82452b04e05c6153ba5f36b9e6b9dc88b4fd [formerly 4453ad2d10cb02790c92de600dd3e100a8de2653 [formerly 65e1e4ee0be1cc2d90c6a02328c88c73301c6764] [formerly ea1c82452b04e05c6153ba5f36b9e6b9dc88b4fd [formerly 56068aa96fa6daf113861476bf4b7aebe2021ca4]]]]]
    Former-commit-id: ea1c82452b04e05c6153ba5f36b9e6b9dc88b4fd
    Former-commit-id: 632f7f479911b57e6499e66ef7e3d25cf7ccbc5c [formerly 69f4822ad10f733b744dd37d50749598c7e47f9e] [formerly 9f6780c4ddf7f45e7030a094222b0ee971a9e827] [formerly 9a5fa2ff1e6acda1befecf45654ce58d3b528acc [formerly e63dac039e52161602915d3cf34685762acd5d9e] [formerly 9f6780c4ddf7f45e7030a094222b0ee971a9e827 [formerly 7f1b468010dc135a4bc70b698760eaaeab7223f0]]]
    Former-commit-id: d9254a849e8396b0b716f6ba4d5fbd703e20ed6c [formerly ed7bf459df1503ce7bd9500dc984c56a535b768f] [formerly bb662ca1753668e52d649a10dcc9e70f43d33684 [formerly dafb6b18d8afbdb53102b94355ab22c321d8eafe]]
    Former-commit-id: 3e2c9510cbedf1aac8a71cd47fe5a233af920a47 [formerly bb662ca1753668e52d649a10dcc9e70f43d33684]
    Former-commit-id: a89f0a64f0c79c4b3a19b40b52d82bbff7bba87c

---
+++ rpms/awips2.core/Installer.httpd-pypies/configuration/apache/pypies.wsgi
@@ -0,0 +1,5 @@
+import site
+site.addsitedir('/awips2/python/lib/python2.7/site-packages')
+
+import pypies.handlers
+application = pypies.handlers.pypies_response
---
+++ rpms/awips2.edex/deploy.builder/build.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# Arguments:
+#   ${1} == Build Version
+#   ${2} == Build Release
+
+if [ "${RPM_TOP_DIR}" = "" ]; then
+   echo "ERROR: You Must Set The RPM_TOP_DIR Environment Variable."
+   echo "Unable To Continue ... Terminating."
+   exit 1
+fi
+
+# !! NOTE !! - We Assume That We Are In A Workspace With The Installer Projects,
+#              The Edex Projects, The Cave Projects, The Native Projects, And Etc.
+export WORKSPACE_DIR=`cd ../../../; pwd;`
+
+source ${WORKSPACE_DIR}/Installer.rpm/awips2.core/deploy.builder/env.sh
+
+# If there is an actual version, it will be given to us as an argument.
+if [ "${1}" = "" ]; then
+   # Check version.txt
+   VERSION_TXT="${WORKSPACE_DIR}/Installer.rpm/version.txt"
+   if [ ! -f ${VERSION_TXT} ]; then
+      echo "ERROR: Unable to find the version file - ${VERSION_TXT}."
+      exit 1
+   fi
+   export BUILD_VERSION=`cat ${VERSION_TXT}`
+   # Ensure that a version has been specified.
+   if [ "${BUILD_VERSION}" = "" ]; then
+      echo "ERROR: A build version has not been specified."
+      exit 1
+   fi
+else
+   export BUILD_VERSION="${1}"
+fi
+# If there is an actual release, it will be given to us as an argument.
+if [ "${2}" = "" ]; then
+   # Use the date.
+   export BUILD_RELEASE=`date +"%Y%m%d"`
+else
+   export BUILD_RELEASE="${2}"
+fi
+
+function buildRPM()
+{
+   BUILDROOT_DIR=/tmp/awips-component
+
+   COMPONENT_DIR=${1}
+   COMPONENT_SPECS=${COMPONENT_DIR}/component.spec
+
+   # We Need To Delete The 'BuildRoot' Directory After Each RPM Is
+   # Built Whether The Build Is Successful Or Not.
+   rm -rf ${BUILDROOT_DIR}
+
+   # We Build The List Of Files That Need To Be Installed On-Demand Now.
+   # If One Exists From A Previous Build, Delete It.
+   if [ -f ${RPM_TOP_DIR}/BUILD/component-files.txt ]; then
+      rm -f ${RPM_TOP_DIR}/BUILD/component-files.txt
+   fi
+
+   # Build The RPM.
+   rpmbuild -ba --target=i386 \
+      --define '_topdir %(echo ${RPM_TOP_DIR})' \
+      --define '_component_version %(echo ${BUILD_VERSION})' \
+      --define '_component_release %(echo ${BUILD_RELEASE})' \
+      --buildroot ${BUILDROOT_DIR} ${COMPONENT_SPECS}
+   RC="$?"
+   if [ ! "${RC}" = "0" ]; then
+      echo ""
+      echo "ERROR: Unable To Build The RPM Defined In: ${COMPONENT_DIR}."
+      echo "Unable To Continue ... Terminating."
+      exit 1
+   fi
+}
+
+# Get A List Of The RPM Directories (Excluding This One)
+# Note: Presently, We Are In ../../Installer.rpm/deploy.builder
+
+# Adjust Our Execution Position.
+cd ../
+
+buildRPM "Installer.edex-base"
+buildRPM "Installer.edex-configuration"
+buildRPM "Installer.edex-gfe"
+buildRPM "Installer.edex-bufr"
+buildRPM "Installer.edex-common-core"
+buildRPM "Installer.edex-core"
+buildRPM "Installer.edex-cots"
+buildRPM "Installer.edex-dat"
+buildRPM "Installer.edex-dataplugins"
+buildRPM "Installer.edex-grib"
+buildRPM "Installer.edex-hydro"
+buildRPM "Installer.edex-radar"
+buildRPM "Installer.edex-ncep"
+buildRPM "Installer.edex-ncep-nsharp"
+buildRPM "Installer.edex-satellite"
+buildRPM "Installer.edex-text"
+buildRPM "Installer.edex-native"
+buildRPM "Installer.edex-shapefiles"
+buildRPM "Installer.edex-npp"
