33756de9c8caa016696e5c2e334973a337d8e22f
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-01-24 09:59:50 -0600

    Issue #181 integrating ncep edex code
    Change-Id: Iaf11559e4cdb4f846d02283c5d2038474c0118e3
    
    Former-commit-id: 3aa13bd49e23ef963127f7efbc91054c06808f1c

--- ncep/gov.noaa.nws.ncep.common.dataplugin.wcp/src/gov/noaa/nws/ncep/common/dataplugin/wcp/WcpSevrln.java
+++ ncep/gov.noaa.nws.ncep.common.dataplugin.wcp/src/gov/noaa/nws/ncep/common/dataplugin/wcp/WcpSevrln.java
@@ -14,6 +14,8 @@
  * 12Dec2008    37          F. J. Yen   Initial
  * 17Apr2009	37			F. J. Yen	Refactored for TO10
  * 17May2010	37			F. J. Yen	Refactored to dataplugin for migration to to11dr11
+ * 09/2011      		    Chin Chen   changed to improve purge performance and
+ * 										removed xml serialization as well
  *       
  * </pre>
  * 
@@ -27,9 +29,6 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Iterator;
-
-import gov.noaa.nws.ncep.common.dataplugin.wcp.WcpRecord;
 import gov.noaa.nws.ncep.common.dataplugin.wcp.WcpLatlons;
 
 import javax.persistence.CascadeType;
@@ -39,15 +38,10 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.Index;
 
 import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
@@ -55,7 +49,6 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
 @Entity
 @Table(name="wcp_sevrln")
-@XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
 /*
  * 8888888888888888 public class WcpSevrln extends PersistableDataObject
@@ -69,49 +62,39 @@ public class WcpSevrln implements Serializable, ISerializableObject {
 	@GeneratedValue
 	private Integer recordId = null;
 
-	/** An identifier used to link this to its parent WcpRecord */
-	/** The WcpSevrln record this object belongs to */
-	@ManyToOne
-	@JoinColumn(name="parentID", nullable=false)
-	private WcpRecord parentID;
+	
 
 	//@DataURI(position=1)
 	@Column(length = 8)
-	@XmlElement
 	@DynamicSerializeElement
 	private String watchNumber;
 
 
 	//@DataURI(position=2)
 	@Column(length = 8)
-	@XmlElement
 	@DynamicSerializeElement
 	private String eventType;
 
 	@Column
-	@XmlElement
 	@DynamicSerializeElement
 	private Calendar startTime;
 
 	@Column
-	@XmlElement
 	@DynamicSerializeElement
 	private Calendar endTime;
 
 	@Column
-	@XmlElement
 	@DynamicSerializeElement
 	private Integer numPnts;
 
 	@Column(length = 272)
-	@XmlElement
 	@DynamicSerializeElement
 	private String sevrLines;
 
 	@DynamicSerializeElement
-	@XmlElement
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "parentID", fetch = FetchType.EAGER)
-	@OnDelete(action = OnDeleteAction.CASCADE)
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "parentID", nullable = false)
+    @Index(name = "wcpLatLon_parentid_idex")
 	private Set<WcpLatlons> wcpLatLon = new HashSet<WcpLatlons>();
 
 	/**
@@ -129,28 +112,7 @@ public class WcpSevrln implements Serializable, ISerializableObject {
 		return serialVersionUID;
 	}
 
-	/**
-	 * @return the parentID
-	 */
-	public WcpRecord getParentID() {
-		return parentID;
-	}
-
-	/**
-	 * @param parentID
-	 *            the parentID to set
-	 */
-	public void setParentID(WcpRecord parentID) {
-		this.parentID = parentID;
-
-		if (this.getWcpLatLon() != null && this.getWcpLatLon().size() > 0) {
-			for (Iterator<WcpLatlons> iter = this.getWcpLatLon().iterator();
-			        iter.hasNext();) {
-				WcpLatlons cond = iter.next();
-				cond.setParentID(this);
-			}
-		}
-	}
+	
 
 	public String getWatchNumber() {
 		return watchNumber;
