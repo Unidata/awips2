2b2d71ec9704a729e3e3868ec70bace144586246
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-01-24 09:59:50 -0600

    Issue #181 integrating ncep edex code
    Change-Id: Iaf11559e4cdb4f846d02283c5d2038474c0118e3
    
    Former-commit-id: 33756de9c8caa016696e5c2e334973a337d8e22f [formerly 6366bc35cba59741457a8240b1ec338ec747bfe9 [formerly fe52a35f7a16dfb872151304061de90a5a704ef1] [formerly 33756de9c8caa016696e5c2e334973a337d8e22f [formerly 3aa13bd49e23ef963127f7efbc91054c06808f1c]]]
    Former-commit-id: 6366bc35cba59741457a8240b1ec338ec747bfe9 [formerly fe52a35f7a16dfb872151304061de90a5a704ef1]
    Former-commit-id: 6366bc35cba59741457a8240b1ec338ec747bfe9
    Former-commit-id: daa0663af0ad17daca3c23f8227c7582957fe3b4

--- ncep/gov.noaa.nws.ncep.common.dataplugin.convsigmet/src/gov/noaa/nws/ncep/common/dataplugin/convsigmet/dao/ConvSigmetDao.java
+++ ncep/gov.noaa.nws.ncep.common.dataplugin.convsigmet/src/gov/noaa/nws/ncep/common/dataplugin/convsigmet/dao/ConvSigmetDao.java
@@ -1,31 +1,14 @@
-/**
- * This software was developed and / or modified by Raytheon Company,
- * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
- * U.S. EXPORT CONTROLLED TECHNICAL DATA
- * This software product contains export-restricted data whose
- * export/transfer/disclosure is restricted by U.S. law. Dissemination
- * to non-U.S. persons whether in the United States or abroad requires
- * an export license or other authorization.
- * 
- * Contractor Name:        Raytheon Company
- * Contractor Address:     6825 Pine Street, Suite 340
- *                         Mail Stop B8
- *                         Omaha, NE 68106
- *                         402.291.0100
- * 
- * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
- * further licensing information.
- **/
 package gov.noaa.nws.ncep.common.dataplugin.convsigmet.dao;
 
 import gov.noaa.nws.ncep.common.dataplugin.convsigmet.ConvSigmetRecord;
-import gov.noaa.nws.ncep.edex.common.dao.NcepDefaultPluginDao;
 
 import java.util.List;
 
 import com.raytheon.uf.common.dataplugin.PluginException;
+import com.raytheon.uf.common.dataplugin.persist.IPersistable;
+import com.raytheon.uf.common.datastorage.IDataStore;
 import com.raytheon.uf.edex.database.DataAccessLayerException;
+import com.raytheon.uf.edex.database.plugin.PluginDao;
 
 /**
  * TODO Add Description
@@ -36,6 +19,8 @@ import com.raytheon.uf.edex.database.DataAccessLayerException;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Sep 23, 2009            jkorman     Initial creation
+ * 09/2011      		   Chin Chen   changed to improve purge performance and
+ * 									   removed xml serialization as well
  * 
  * </pre>
  * 
@@ -43,7 +28,7 @@ import com.raytheon.uf.edex.database.DataAccessLayerException;
  * @version 1.0
  */
 
-public class ConvSigmetDao extends NcepDefaultPluginDao {
+public class ConvSigmetDao extends PluginDao {
 
     /**
      * Creates a new ReccoDao
@@ -92,4 +77,11 @@ public class ConvSigmetDao extends NcepDefaultPluginDao {
 
         return results;
     }
+
+	@Override
+	protected IDataStore populateDataStore(IDataStore dataStore,
+			IPersistable obj) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
