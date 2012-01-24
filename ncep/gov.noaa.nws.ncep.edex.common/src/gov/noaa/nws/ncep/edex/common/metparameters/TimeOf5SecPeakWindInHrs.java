865b413cea2d643585d6e69583e9036e0fb717d6
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-01-24 09:49:27 -0600

    Issue #181 integrating ncep edex code
    Change-Id: I7f3fd64d1e21d5a82b29b404bb90a62bfa6f79bf
    
    Former-commit-id: e9519957369e8155b4aef111ba9e05097874dbe9 [formerly a015cc136b4e3c60d3f7bb3de0a4f5b09be4bb87] [formerly f96e6a822b5eb98beb973f6f11fa48c312c27fbc [formerly 71d87e07ef1d5ce2509fddce9170870a5950ba7b]]
    Former-commit-id: f96e6a822b5eb98beb973f6f11fa48c312c27fbc
    Former-commit-id: 873469f7714bd5d132ff8aebe67ec26271a64c9d

---
+++ ncep/gov.noaa.nws.ncep.edex.common/src/gov/noaa/nws/ncep/edex/common/metparameters/TimeOf5SecPeakWindInHrs.java
@@ -0,0 +1,32 @@
+package gov.noaa.nws.ncep.edex.common.metparameters;
+
+import javax.measure.quantity.Duration;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.raytheon.uf.common.serialization.ISerializableObject;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+
+/**
+ * Maps to the GEMPAK parameter PWHR
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@DynamicSerialize
+
+
+
+public class TimeOf5SecPeakWindInHrs extends AbstractMetParameter
+implements Duration, ISerializableObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8731345402856925803L;
+
+	public TimeOf5SecPeakWindInHrs(){
+		 super( UNIT );
+	}
+	
+}
