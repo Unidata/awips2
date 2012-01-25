b427209b3c9898224fbdccde7d7f9672d36bb19a
Matt Nash <Matt_Nash@raytheon.com>
2012-01-25 12:50:31 -0600

    Issue #26 add tear off code to git baseline
    Former-commit-id: 4286cb03422f27f991dab253f24b26ace66ba878 [formerly 892cb9a7ae5d7088186f0d94f79f52ed2e4af152 [formerly cf4e1f764ea160764c46730f9ecc65a78472b52f] [formerly 4286cb03422f27f991dab253f24b26ace66ba878 [formerly dedac216cfa51bd413f7c4d1f7db5ee8ce665866]]]
    Former-commit-id: 892cb9a7ae5d7088186f0d94f79f52ed2e4af152 [formerly cf4e1f764ea160764c46730f9ecc65a78472b52f]
    Former-commit-id: 892cb9a7ae5d7088186f0d94f79f52ed2e4af152
    Former-commit-id: ea6ef7380201f3e1ab2d1cda784b6e6a7709d4ec

--- cave/com.raytheon.uf.viz.ui.menus/src/com/raytheon/uf/viz/ui/menus/xml/SubmenuContribution.java
+++ cave/com.raytheon.uf.viz.ui.menus/src/com/raytheon/uf/viz/ui/menus/xml/SubmenuContribution.java
@@ -23,12 +23,14 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuListener;
 
 import com.raytheon.uf.common.menus.xml.CommonAbstractMenuContribution;
 import com.raytheon.uf.common.menus.xml.CommonSubmenuContribution;
 import com.raytheon.uf.common.menus.xml.VariableSubstitution;
 import com.raytheon.uf.viz.core.exception.VizException;
 import com.raytheon.uf.viz.ui.menus.widgets.SubmenuContributionItem;
+import com.raytheon.uf.viz.ui.menus.widgets.tearoff.TearOffMenuListener;
 
 /**
  * Describes a submenu contribution
@@ -46,7 +48,12 @@ import com.raytheon.uf.viz.ui.menus.widgets.SubmenuContributionItem;
  * @version 1.0
  */
 public class SubmenuContribution extends
-        AbstractMenuContributionItem<CommonSubmenuContribution> {
+        AbstractMenuContributionItem<CommonSubmenuContribution> implements
+        IVizMenuManager {
+
+    private SubmenuContributionItem submenuCont = null;
+
+    private IMenuListener mListener = null;
 
     /*
      * (non-Javadoc)
@@ -64,9 +71,25 @@ public class SubmenuContribution extends
         if (removals.contains(item.id))
             return new IContributionItem[0];
 
-        final SubmenuContributionItem smci = new SubmenuContributionItem(subs,
-                item.menuText, item.contributions, new HashSet<String>());
-        return new IContributionItem[] { smci };
+        submenuCont = new SubmenuContributionItem(subs, item.menuText,
+                item.contributions, new HashSet<String>(), mListener);
+        // adding tear off listener, seems out of place, but must be done
+        if (mListener == null
+                && com.raytheon.uf.viz.core.Activator.getDefault()
+                        .getPreferenceStore().getBoolean("tearoffmenus")) {
+            mListener = new TearOffMenuListener(submenuCont);
+            submenuCont.addMenuListener(mListener);
+        }
+        return new IContributionItem[] { submenuCont };
     }
 
+    @Override
+    public void addMenuListener(IMenuListener listener) {
+        mListener = listener;
+    }
+
+    @Override
+    public void removeMenuListener(IMenuListener listener) {
+        submenuCont.removeMenuListener(listener);
+    }
 }
