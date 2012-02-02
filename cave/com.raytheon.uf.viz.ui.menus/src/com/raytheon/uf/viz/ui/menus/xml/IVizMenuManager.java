b19b17ff16dd08acdb1f7a9ae0472cdea05f36ea
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-02-02 16:52:38 -0600

    Issue #29 Fixed issue with radar menu dispose
    Former-commit-id: aa88742504969483ba2aa02ccf7b8588baca8bce [formerly 1cde213e67c04629d959da38bed5caa7408e7ed1] [formerly 56897ee0df05739a33ae3f1207baf38051494668] [formerly 56897ee0df05739a33ae3f1207baf38051494668 [formerly f131667c9ef4bc66075beb705735d372b333271d]] [formerly 8f0fbbf4e8d6db33bba370547f9e13ae58372700 [formerly 56897ee0df05739a33ae3f1207baf38051494668 [formerly f131667c9ef4bc66075beb705735d372b333271d] [formerly 8f0fbbf4e8d6db33bba370547f9e13ae58372700 [formerly 279c4037f9f6fbefe53bc12cbf82017daa3399d2]]]]
    Former-commit-id: 8f0fbbf4e8d6db33bba370547f9e13ae58372700
    Former-commit-id: ba2c154be97441f256509b7a2f530d48f7934e9c [formerly 1b6d9389ac6e57e2c133f2575ba2dbdfc9896c98] [formerly f5b7ebc249d4089fb50b56eee8faf1ab50051848 [formerly f33039a2692ebf6de15aa1761b4ff7becc829278]]
    Former-commit-id: f5b7ebc249d4089fb50b56eee8faf1ab50051848
    Former-commit-id: d26d122c5629cc451941262a11dba3e8e2398af2

--- cave/com.raytheon.uf.viz.ui.menus/src/com/raytheon/uf/viz/ui/menus/xml/IVizMenuManager.java
+++
@@ -1,9 +0,0 @@
-package com.raytheon.uf.viz.ui.menus.xml;
-
-import org.eclipse.jface.action.IMenuListener;
-
-public interface IVizMenuManager {
-    public void addMenuListener(IMenuListener listener);
-
-    public void removeMenuListener(IMenuListener listener);
-}
