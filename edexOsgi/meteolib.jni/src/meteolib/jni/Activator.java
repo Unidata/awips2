38ccaf882b0590ff6f6502936330cf6e1a366453
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-02-17 11:09:13 -0600

    Issue #182 - the meteolib Activator will now return a properly formatted path to the meteolib dll when on a Windows workstation. The meteolib "cape function" will now acurrately report if it is out of memory.
    Former-commit-id: 4276fc2fe8949275c339960fad09e939aeda09ed [formerly 1ec51accade835c1133eac9222131f9186985053]
    Former-commit-id: 9f1b7a613a4dee7d95d7a2100264c87376b7d925

--- edexOsgi/meteolib.jni/src/meteolib/jni/Activator.java
+++ edexOsgi/meteolib.jni/src/meteolib/jni/Activator.java
@@ -8,6 +8,7 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.apache.commons.lang.SystemUtils;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -90,9 +91,45 @@ public class Activator extends AbstractUIPlugin {
         	return null;
         }
         
+        /*
+         * We do not need to reformat the path for Windows here because
+         * Java does not care.
+         */
         System.load(nativeLibraryPath);
         
+        // Re-format for Windows.
+        if (SystemUtils.IS_OS_WINDOWS) 
+        {
+        	/*
+        	 * We reformat here because this is the path that will be
+        	 * returned to python scripts that use the meteoLib library.
+        	 */
+        	return formatWindowsPath(nativeLibraryPath);
+        }
+        
         return nativeLibraryPath;
+    }
+    
+    private static String formatWindowsPath(String nativeLibraryPath) {
+    	/*
+    	 * Remove the leading "/" if there is one.
+    	 */
+    	if (nativeLibraryPath.startsWith("/"))
+    	{
+    		int length = nativeLibraryPath.length();
+    		nativeLibraryPath = nativeLibraryPath.substring(1, length);
+    	}
+    	
+    	/*
+    	 * Replace all file path separators with the Windows version of
+    	 * a file path separator.
+    	 */
+    	if (nativeLibraryPath.contains("/"))
+    	{
+    		nativeLibraryPath = nativeLibraryPath.replace("/", "\\");
+    	}
+    	
+    	return nativeLibraryPath;
     }
 
     /*
@@ -116,5 +153,4 @@ public class Activator extends AbstractUIPlugin {
     public static Activator getDefault() {
         return plugin;
     }
-
 }
