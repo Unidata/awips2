7a91f24e27d3da1234b056cbdcdc556e7c4be2fb
James Korman <james.korman@raytheon.com>
2012-03-07 15:38:20 -0600

    Issue #232 Initial collaboration framework plugin checkin
    Former-commit-id: 373f5b4060ebdddc49f1fb7247424c1a3345a1a9 [formerly 4368616970fb0feeb7c3fe9c99bf34009cb2247e] [formerly 526a2633ce25fdde821a1fc83479a8a4beace577] [formerly 5a91d71c121807a01e633657f41694eb39f7b59b [formerly 526a2633ce25fdde821a1fc83479a8a4beace577 [formerly 2351a53e68bbc8cfe11917d202cbc5424768b5a6]]]
    Former-commit-id: 5a91d71c121807a01e633657f41694eb39f7b59b
    Former-commit-id: 30ad0a0f992968ee55254e2e103edff30614f710 [formerly 2fea86a5919bf678ba9f342f114f14853623a361]
    Former-commit-id: f242c1a430823c064373d1b98c7cde78d72d9b8a

---
+++ cave/com.raytheon.uf.viz.collaboration/src/com/raytheon/uf/viz/collaboration/comm/provider/roster/Roster.java
@@ -0,0 +1,168 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.comm.provider.roster;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster;
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterEntry;
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterGroup;
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterItem;
+import com.raytheon.uf.viz.collaboration.comm.identity.user.IChatID;
+import com.raytheon.uf.viz.collaboration.comm.identity.user.ID;
+import com.raytheon.uf.viz.collaboration.comm.identity.user.IQualifiedID;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 27, 2012            jkorman     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author jkorman
+ * @version 1.0
+ */
+
+public class Roster extends RosterItem implements IRoster {
+
+    private Collection<IRosterEntry> entries;
+
+    private Collection<IRosterGroup> groups;
+
+    private final IChatID user;
+
+    private boolean roomRoster = false;
+
+    // From ecf . . . .
+    // IPresenceContainerAdapter presence = (IPresenceContainerAdapter)
+    // container
+    // .getAdapter(IPresenceContainerAdapter.class);
+    // private IPresenceContainerAdapter presenceAdapter = null;
+    //
+    //
+    // presenceAdapter.getRosterManager.getRoster();
+    // We will then build our internal roster from the ecf roster.
+    //
+
+    /**
+     * 
+     * @param user
+     */
+    public Roster(IChatID user) {
+        this.user = user;
+        entries = new ArrayList<IRosterEntry>();
+    }
+
+    /**
+     * @return
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#getUser()
+     */
+    @Override
+    public IChatID getUser() {
+        return user;
+    }
+
+    /**
+     * Does this roster support nested groups?
+     * 
+     * @return This roster support nested groups.
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#supportsNestedGroups()
+     */
+    public boolean supportsNestedGroups() {
+        return false;
+    }
+
+    /**
+     * Is this a roster for a chat room?
+     * 
+     * @return Is this a chat room roster?
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#isRoomRoster()
+     */
+    @Override
+    public boolean isRoomRoster() {
+        return roomRoster;
+    }
+
+    /**
+     * @param entry
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#addEntry(com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterEntry)
+     */
+    @Override
+    public void addRosterEntry(IRosterEntry entry) {
+        entries.add(entry);
+    }
+
+    /**
+     * Get all entries belonging with this roster.
+     * 
+     * @return A Collection of entries belonging to this Roster.
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#getEntries()
+     */
+    @Override
+    public Collection<IRosterEntry> getEntries() {
+        return entries;
+    }
+
+    /**
+     * Get all groups associated with this roster.
+     * 
+     * @return A Collection of groups belonging to this Roster.
+     */
+    @Override
+    public Collection<IRosterGroup> getGroups() {
+        return groups;
+    }
+
+    /**
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#addRosterEntry(com.raytheon.uf.viz.collaboration.comm.identity.user.IQualifiedID,
+     *      java.lang.String, java.lang.String[])
+     */
+    @Override
+    public void addRosterEntry(IQualifiedID user, String nickName,
+            String[] groups) {
+    }
+
+    /**
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#modifyRosterEntry(com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterEntry)
+     */
+    @Override
+    public void modifyRosterEntry(IRosterEntry entry) {
+    }
+
+    /*
+     * 
+     * 
+     * @see com.raytheon.uf.viz.collaboration.comm.identity.roster.IRoster#
+     * removeFromRoster(com.raytheon.uf.viz.collaboration.comm.identity.user.ID)
+     */
+    @Override
+    public void removeFromRoster(ID user) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
