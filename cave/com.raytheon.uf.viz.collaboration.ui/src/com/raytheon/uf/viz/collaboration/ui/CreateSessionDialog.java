1a2608a2fa4b818cde1214d68d4b046ae1466514
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2012-03-08 08:30:02 -0600

    Issue #244 update
    Change-Id: I6f92dbd64d95cc853f6dcf43d39c209607a70105
    
    Former-commit-id: c9459e2e8cc0604d64c6a727b65fdde48900ac14 [formerly 28969960d80066def0ef02930875e40d7693c234]
    Former-commit-id: 2114b95a0b0440b0d2cdfef9deb1cd33d3f5ead4

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/CreateSessionDialog.java
@@ -0,0 +1,293 @@
+package com.raytheon.uf.viz.collaboration.ui;
+
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import com.raytheon.uf.viz.collaboration.comm.identity.info.IVenueInfo;
+import com.raytheon.uf.viz.collaboration.data.CollaborationDataManager;
+import com.raytheon.uf.viz.collaboration.data.DataUser;
+import com.raytheon.viz.ui.dialogs.CaveSWTDialogBase;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 15, 2012            rferrel     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author rferrel
+ * @version 1.0
+ */
+public class CreateSessionDialog extends CaveSWTDialogBase {
+    private static DataUser.StatusType[] status = null;
+
+    private Text nameTF;
+
+    private Text subjectTF;
+
+    private Button inviteUsers;
+
+    private boolean showInvite;
+
+    private StyledText inviteMessageTF;
+
+    private Label inviteLabel;
+
+    public CreateSessionDialog(Shell parentShell, boolean showInvite) {
+        super(parentShell);
+        this.showInvite = showInvite;
+        setText("Create Collaboration Session");
+    }
+
+    private Control createDialogArea(Composite parent) {
+        if (status == null) {
+            DataUser.StatusType[] types = DataUser.StatusType.values();
+            status = new DataUser.StatusType[types.length - 1];
+            int index = 0;
+            for (DataUser.StatusType type : types) {
+                if (type != DataUser.StatusType.NOT_ON_LINE) {
+                    status[index] = type;
+                    ++index;
+                }
+            }
+        }
+        Composite body = new Composite(parent, SWT.NONE);
+        body.setLayout(new GridLayout(2, false));
+        body.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+                | GridData.HORIZONTAL_ALIGN_FILL));
+        Label label = null;
+        label = new Label(body, SWT.NONE);
+        label.setText("Name: ");
+        nameTF = new Text(body, SWT.BORDER);
+        nameTF.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+                | GridData.HORIZONTAL_ALIGN_FILL));
+
+        label = new Label(body, SWT.NONE);
+        label.setText("Subject: ");
+        subjectTF = new Text(body, SWT.BORDER);
+        subjectTF.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL
+                | GridData.HORIZONTAL_ALIGN_FILL));
+
+        inviteUsers = new Button(body, SWT.CHECK);
+        inviteUsers.setText("Invite Selected Users");
+        inviteUsers.setSelection(showInvite);
+        inviteUsers.setVisible(showInvite);
+        label = new Label(body, SWT.NONE);
+        label.setText("");
+        label.setVisible(showInvite);
+        if (showInvite) {
+            inviteLabel = new Label(body, SWT.NONE);
+            inviteLabel.setText("Invite Message: ");
+            inviteMessageTF = new StyledText(body, SWT.BORDER | SWT.MULTI
+                    | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL);
+            inviteMessageTF.setLayoutData(new GridData(GridData.FILL_BOTH));
+            inviteMessageTF.pack();
+            Point p = inviteMessageTF.getSize();
+            GridData gd = (GridData) inviteMessageTF.getLayoutData();
+            gd.heightHint = p.y * 3;
+            inviteUsers.addSelectionListener(new SelectionListener() {
+
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    boolean selected = ((Button) e.widget).getSelection();
+                    inviteLabel.setVisible(selected);
+                    inviteMessageTF.setVisible(selected);
+                }
+
+                @Override
+                public void widgetDefaultSelected(SelectionEvent e) {
+                    boolean selected = ((Button) e.widget).getSelection();
+                    inviteLabel.setVisible(selected);
+                    inviteMessageTF.setVisible(selected);
+
+                }
+            });
+        }
+        return body;
+    }
+
+    @Override
+    protected void initializeComponents(Shell shell) {
+        shell.setLayout(new GridLayout(1, false));
+        createDialogArea(shell);
+        createButtonBar(shell);
+    }
+
+    private void createButtonBar(Composite parent) {
+        Composite b = new Composite(parent, SWT.NONE);
+
+        // set up to center buttons.
+        b.setLayout(new GridLayout(3, true));
+        new Composite(b, SWT.NONE);
+        Composite bar = new Composite(b, SWT.NONE);
+        new Composite(b, SWT.NONE);
+        bar.setLayout(new GridLayout(0, true));
+        createButton(bar, IDialogConstants.OK_ID, "Create", true);
+
+        createButton(bar, IDialogConstants.CANCEL_ID,
+                IDialogConstants.CANCEL_LABEL, false);
+    }
+
+    @Override
+    protected void preOpened() {
+        super.preOpened();
+    }
+
+    /**
+     * Creates a new button with the given id.
+     * <p>
+     * The <code>Dialog</code> implementation of this framework method creates a
+     * standard push button, registers it for selection events including button
+     * presses, and registers default buttons with its shell. The button id is
+     * stored as the button's client data. If the button id is
+     * <code>IDialogConstants.CANCEL_ID</code>, the new button will be
+     * accessible from <code>getCancelButton()</code>. If the button id is
+     * <code>IDialogConstants.OK_ID</code>, the new button will be accesible
+     * from <code>getOKButton()</code>. Note that the parent's layout is assumed
+     * to be a <code>GridLayout</code> and the number of columns in this layout
+     * is incremented. Subclasses may override.
+     * </p>
+     * 
+     * @param parent
+     *            the parent composite
+     * @param id
+     *            the id of the button (see <code>IDialogConstants.*_ID</code>
+     *            constants for standard dialog button ids)
+     * @param label
+     *            the label from the button
+     * @param defaultButton
+     *            <code>true</code> if the button is to be the default button,
+     *            and <code>false</code> otherwise
+     * 
+     * @return the new button
+     * 
+     * @see #getCancelButton
+     * @see #getOKButton()
+     */
+    protected Button createButton(Composite parent, int id, String label,
+            boolean defaultButton) {
+        // increment the number of columns in the button bar
+        ((GridLayout) parent.getLayout()).numColumns++;
+        Button button = new Button(parent, SWT.PUSH);
+        button.setText(label);
+        // button.setFont(JFaceResources.getDialogFont());
+        button.setData(new Integer(id));
+        button.addSelectionListener(new SelectionAdapter() {
+            public void widgetSelected(SelectionEvent event) {
+                Integer val = (Integer) event.widget.getData();
+                if (val != IDialogConstants.OK_ID) {
+                    setReturnValue(null);
+                    CreateSessionDialog.this.getShell().dispose();
+                } else {
+                    Text focusField = null;
+                    List<String> errorMessages = new ArrayList<String>();
+                    String subject = subjectTF.getText().trim();
+                    String err = validateVenuName();
+                    String name = nameTF.getText();
+                    if (err != null) {
+                        focusField = nameTF;
+                        errorMessages.add(err);
+                    }
+
+                    if (focusField == null) {
+                        CreateSessionData result = new CreateSessionData();
+                        result.setName(name);
+                        result.setSubject(subject);
+                        result.setInviteUsers(inviteUsers.getSelection());
+                        setReturnValue(result);
+                        CreateSessionDialog.this.getShell().dispose();
+                    } else {
+                        StringBuilder sb = new StringBuilder();
+                        String prefix = "";
+                        for (String msg : errorMessages) {
+                            sb.append(prefix).append(msg);
+                            prefix = "\n";
+                        }
+                        MessageBox messageBox = new MessageBox(event.widget
+                                .getDisplay().getActiveShell(), SWT.ERROR);
+                        messageBox.setText("Session Creation Error");
+                        messageBox.setMessage(sb.toString());
+                        messageBox.open();
+                        event.doit = false;
+                        setReturnValue(null);
+                        focusField.setFocus();
+                        focusField.selectAll();
+                    }
+                }
+            }
+        });
+        if (defaultButton) {
+            Shell shell = parent.getShell();
+            if (shell != null) {
+                shell.setDefaultButton(button);
+            }
+        }
+        return button;
+    }
+
+    private String validateVenuName() {
+        String name = nameTF.getText().trim();
+        nameTF.setText(name);
+        String err = null;
+        if (name.length() <= 0) {
+            err = "Must have session name.";
+        } else if (false) {
+            // TODO Above else make it a test for invalid characters.
+            err = "Name contains invalid characters.";
+        } else {
+            Collection<IVenueInfo> info = CollaborationDataManager
+                    .getInstance().getSessionManager().getVenueInfo();
+            for (IVenueInfo i : info) {
+                if (name.equals(i.getVenueName())) {
+                    err = "Session already exists. Pick a different name.";
+                    break;
+                }
+            }
+        }
+        return err;
+    }
+}
