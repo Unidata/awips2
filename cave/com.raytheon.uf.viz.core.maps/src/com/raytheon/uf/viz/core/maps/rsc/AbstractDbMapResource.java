4662ece0600a3d2953a05786356ef44d3fc392fe
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-03-09 12:31:50 -0600

    Issue #358 Merging core drawing API improvements from Collaboration
    Former-commit-id: d43b0456d869873df6e6c4cf69b898da7863a4da [formerly a266a93ec2dca3ffbd7d2fd1f9dc47d7e18e6904]
    Former-commit-id: 89f603f4488d583a2edc18ae06fd187e10a97e4a

--- cave/com.raytheon.uf.viz.core.maps/src/com/raytheon/uf/viz/core/maps/rsc/AbstractDbMapResource.java
+++ cave/com.raytheon.uf.viz.core.maps/src/com/raytheon/uf/viz/core/maps/rsc/AbstractDbMapResource.java
@@ -89,6 +89,7 @@ public abstract class AbstractDbMapResource<T extends AbstractDbMapResourceData,
 
         if (font != null) {
             font.dispose();
+            font = null;
         }
     }
 
