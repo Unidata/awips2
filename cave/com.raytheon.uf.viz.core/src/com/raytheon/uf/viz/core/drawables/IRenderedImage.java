59cbd3efc2e675b8ce545af173952d88292a84fd
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-03-09 12:31:50 -0600

    Issue #358 Merging core drawing API improvements from Collaboration
    Former-commit-id: a36ae8f15dc0200c922320383b47d6da4fa08423 [formerly 3beecc1cdfb5555adf61c8ad3815d2a1994fc389] [formerly 18805c68f779216de3226cf33a02422b7af0ba08] [formerly 18805c68f779216de3226cf33a02422b7af0ba08 [formerly d43b0456d869873df6e6c4cf69b898da7863a4da]] [formerly 89f603f4488d583a2edc18ae06fd187e10a97e4a [formerly 18805c68f779216de3226cf33a02422b7af0ba08 [formerly d43b0456d869873df6e6c4cf69b898da7863a4da] [formerly 89f603f4488d583a2edc18ae06fd187e10a97e4a [formerly a266a93ec2dca3ffbd7d2fd1f9dc47d7e18e6904]]]]
    Former-commit-id: 89f603f4488d583a2edc18ae06fd187e10a97e4a
    Former-commit-id: 6dc1489d12555cc7d28c9dace11e041f855e7f11 [formerly 69a2b029e678fdd8a18305144330e4c95774ec78] [formerly f78b932abbc4d8bc78608810b553fa7a4506184e [formerly 4662ece0600a3d2953a05786356ef44d3fc392fe]]
    Former-commit-id: f78b932abbc4d8bc78608810b553fa7a4506184e
    Former-commit-id: 8b6aa63464dd2ed5e83567fdf8e200d071843059

---
+++ cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/IRenderedImage.java
@@ -0,0 +1,50 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.core.drawables;
+
+import java.awt.image.RenderedImage;
+
+/**
+ * IImage object represents java RenderedImage
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 5, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public interface IRenderedImage extends IImage {
+
+    /**
+     * Get the RenderedImage associated with this object
+     * 
+     * @return
+     */
+    public RenderedImage getImage();
+
+}
