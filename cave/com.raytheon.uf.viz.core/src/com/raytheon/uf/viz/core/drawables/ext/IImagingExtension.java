8f08fab0a7ca07191057006f7e44d51527cec7e5
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-03-14 09:49:32 -0500

    Issue #358 Cleaned up mesh extension and API to use geotools for defining a mesh.  This is better practice than using arbitrary MathTransform objects as it is well defined and allows for serialization of the mesh to occur.
    Former-commit-id: 8fb7eccb73a4635ba6e854014e3dfe41bd65dfa4 [formerly 6e5964df9db558cd39bb0f5cb822f89cc52ac3ba] [formerly 50765e8a346e97389e20aaa2d9dcda707be45bc3 [formerly 7e4abe637edacca71e1e1c98d99521cd33415fb0]]
    Former-commit-id: 50765e8a346e97389e20aaa2d9dcda707be45bc3
    Former-commit-id: cf440b3884db75aa705a68943bcf0ffba3cd62a0

--- cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/ext/IImagingExtension.java
+++ cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/ext/IImagingExtension.java
@@ -19,14 +19,7 @@
  **/
 package com.raytheon.uf.viz.core.drawables.ext;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.raytheon.uf.viz.core.DrawableImage;
-import com.raytheon.uf.viz.core.IGraphicsTarget;
-import com.raytheon.uf.viz.core.IGraphicsTarget.RasterMode;
-import com.raytheon.uf.viz.core.drawables.IImage;
-import com.raytheon.uf.viz.core.drawables.IImage.Status;
 import com.raytheon.uf.viz.core.drawables.PaintProperties;
 import com.raytheon.uf.viz.core.drawables.ext.GraphicsExtension.IGraphicsExtensionInterface;
 import com.raytheon.uf.viz.core.exception.VizException;
@@ -61,100 +54,4 @@ public interface IImagingExtension extends IGraphicsExtensionInterface {
     public boolean drawRasters(PaintProperties paintProps,
             DrawableImage... images) throws VizException;
 
-    public static class ImagingSupport {
-
-        protected static final TextureLoader textureLoader = TextureLoader
-                .getInstance();
-
-        /**
-         * Prepares images for painting by staging and/or targeting the image
-         * 
-         * @param target
-         * @param images
-         * @throws VizException
-         */
-        public static void prepareImages(IGraphicsTarget target,
-                DrawableImage... images) throws VizException {
-            for (DrawableImage di : images) {
-                IImage image = di.getImage();
-                RasterMode mode = di.getMode();
-
-                if (image.getStatus() != Status.LOADED) {
-                    if (image.getStatus() != Status.STAGED) {
-                        if (mode == RasterMode.ASYNCHRONOUS) {
-                            textureLoader.requestLoad(image);
-                            target.setNeedsRefresh(true);
-                        } else if (mode == RasterMode.SYNCHRONOUS) {
-                            image.stage();
-                        }
-                    }
-
-                    if (image.getStatus() == Status.STAGED) {
-                        image.target(target);
-                    }
-                }
-            }
-        }
-
-        /**
-         * Routes the images to be rendered by their proper extensions
-         * 
-         * @param target
-         * @param paintProps
-         * @param images
-         * @return
-         * @throws VizException
-         */
-        public static boolean routeImages(IGraphicsTarget target,
-                PaintProperties paintProps, DrawableImage[] images)
-                throws VizException {
-            boolean rval = true;
-            boolean skipped = false;
-            List<DrawableImage> bulk = new ArrayList<DrawableImage>();
-            Class<? extends IImagingExtension> lastExt = null;
-            for (DrawableImage di : images) {
-                IImage image = di.getImage();
-                if (image.getStatus() == IImage.Status.LOADED) {
-                    Class<? extends IImagingExtension> imageExt = image
-                            .getExtensionClass();
-                    if (imageExt.equals(lastExt) == false && bulk.size() > 0) {
-                        DrawableImage[] extImages = bulk
-                                .toArray(new DrawableImage[bulk.size()]);
-                        // Render what we have
-                        IImagingExtension impl = target.getExtension(lastExt);
-                        rval &= impl.drawRasters(paintProps, extImages);
-                        bulk.clear();
-                    }
-
-                    bulk.add(di);
-                    lastExt = imageExt;
-                } else {
-                    skipped = true;
-                }
-            }
-
-            if (bulk.size() > 0) {
-                // Render what is left
-                IImagingExtension impl = target.getExtension(lastExt);
-                rval &= impl.drawRasters(paintProps,
-                        bulk.toArray(new DrawableImage[bulk.size()]));
-            }
-
-            return rval & skipped;
-        }
-
-        /**
-         * Prepares the images, then routes them for rendering
-         * 
-         * @param paintProps
-         * @param images
-         * @return
-         */
-        public static boolean drawRasters(IGraphicsTarget target,
-                PaintProperties paintProps, DrawableImage[] images)
-                throws VizException {
-            prepareImages(target, images);
-            return routeImages(target, paintProps, images);
-        }
-    }
 }
