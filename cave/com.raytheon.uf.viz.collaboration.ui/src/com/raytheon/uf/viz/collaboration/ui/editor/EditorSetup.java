98fb91164b5f740edc7b76fdecc58984f986e8d1
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-03-22 09:39:59 -0500

    Issue #427 Share maps and editor
    Change-Id: I6bd3148ec199c348ffd85d08322fdb8befa4096c
    
    Former-commit-id: 615962f5dde47a1a617432989e590311cb7d4b9a [formerly 615962f5dde47a1a617432989e590311cb7d4b9a [formerly 7a58d8a17be53fc504cafe32d19de2f52121eeff]]
    Former-commit-id: 0e214bcd7c6a3a18da468024f3c772a812687785
    Former-commit-id: 2801c4fdfbad7c5052568aba4207f60b1c96b430

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/EditorSetup.java
@@ -0,0 +1,171 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.editor;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.raytheon.uf.common.serialization.SerializationException;
+import com.raytheon.uf.common.serialization.SerializationUtil;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.PixelExtent;
+import com.raytheon.uf.viz.core.drawables.AbstractRenderableDisplay;
+import com.raytheon.uf.viz.core.drawables.IDescriptor;
+import com.raytheon.uf.viz.core.drawables.ResourcePair;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.core.map.MapDescriptor;
+import com.raytheon.uf.viz.core.maps.display.MapRenderableDisplay;
+import com.raytheon.uf.viz.core.maps.rsc.MapResourceGroup;
+import com.raytheon.uf.viz.core.rsc.ResourceProperties;
+import com.raytheon.viz.ui.EditorUtil;
+import com.raytheon.viz.ui.UiUtil;
+import com.raytheon.viz.ui.editor.AbstractEditor;
+import com.vividsolutions.jts.geom.Envelope;
+
+/**
+ * Utilities for setting up collaboration editors.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 19, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public class EditorSetup {
+
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(EditorSetup.class);
+
+    public static final String PATH = "/tmp/sharedEditor.xml"; // TODO delete
+
+    /**
+     * Extracts a SharedEditor object from the editor passed in.
+     * 
+     * @param editor
+     *            the editor to extract a shared editor for.
+     * @return
+     */
+    public static SharedEditor extractSharedEditor(AbstractEditor editor) {
+        // AbstractEditor editor = (AbstractEditor) EditorUtil
+        // .findEditor(editorId);
+        SharedEditor se = new SharedEditor();
+
+        // extract grid geometry
+        IDescriptor desc = editor.getActiveDisplayPane().getDescriptor();
+        se.setGeometry(desc.getGridGeometry());
+
+        // extract extent to get the proper zoom/pan
+        IExtent extent = editor.getActiveDisplayPane().getRenderableDisplay()
+                .getExtent();
+        se.setEnvelope(new Envelope(extent.getMinX(), extent.getMaxX(), extent
+                .getMinY(), extent.getMaxY()));
+
+        List<ResourcePair> rscList = new ArrayList<ResourcePair>();
+
+        // extract map resources
+        Iterator<ResourcePair> itr = desc.getResourceList().iterator();
+        while (itr.hasNext()) {
+            ResourcePair rp = itr.next();
+            if (rp.getResource() instanceof MapResourceGroup) {
+                rscList.add(rp);
+            }
+        }
+        se.setLocalResources(rscList);
+
+        return se;
+    }
+
+    public static void createEditor(SharedEditor sharedEditor) {
+        AbstractRenderableDisplay[] displays = new AbstractRenderableDisplay[1];
+        try {
+            // TODO make it work with any IDescriptor and
+            // AbstractRenderableDisplay
+            MapDescriptor descriptor = new MapDescriptor(
+                    sharedEditor.getGeometry());
+            if (sharedEditor.getLocalResources() != null) {
+                for (ResourcePair rp : sharedEditor.getLocalResources()) {
+                    if (rp.getProperties() != null) {
+                        rp.getProperties().setSystemResource(true);
+                    } else {
+                        ResourceProperties props = new ResourceProperties();
+                        props.setSystemResource(true);
+                        rp.setProperties(props);
+                    }
+                    descriptor.getResourceList().add(rp);
+                }
+            }
+
+            // setup renderable display
+            MapRenderableDisplay disp = new MapRenderableDisplay(descriptor);
+            PixelExtent extent = new PixelExtent(sharedEditor.getEnvelope()
+                    .getMinX(), sharedEditor.getEnvelope().getMaxX(),
+                    sharedEditor.getEnvelope().getMinY(), sharedEditor
+                            .getEnvelope().getMaxY());
+            disp.setExtent(extent);
+
+            displays[0] = disp;
+            UiUtil.createEditor(CollaborationEditor.EDITOR_ID, displays);
+        } catch (VizException e) {
+            // TODO Auto-generated catch block. Please revise as appropriate.
+            statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
+        }
+
+    }
+
+    // TODO delete
+    public static void testSaveEditorData() {
+        AbstractEditor editor = (AbstractEditor) EditorUtil.getActiveEditor();
+        SharedEditor se = extractSharedEditor(editor);
+        try {
+            SerializationUtil.jaxbMarshalToXmlFile(se, PATH);
+        } catch (SerializationException e) {
+            // TODO Auto-generated catch block. Please revise as appropriate.
+            statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
+        }
+
+    }
+
+    // TODO delete
+    public static SharedEditor testLoadEditorData() {
+        SharedEditor se = null;
+        try {
+            se = (SharedEditor) SerializationUtil
+                    .jaxbUnmarshalFromXmlFile(PATH);
+        } catch (SerializationException e) {
+            // TODO Auto-generated catch block. Please revise as appropriate.
+            statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
+        }
+        return se;
+
+    }
+
+}
