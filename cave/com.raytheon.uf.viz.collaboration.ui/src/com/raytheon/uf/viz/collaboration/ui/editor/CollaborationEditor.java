2ba67353247c453793f519071e10963160e7a42a
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-03-22 09:39:59 -0500

    Issue #427 Share maps and editor
    Change-Id: I6bd3148ec199c348ffd85d08322fdb8befa4096c
    
    Former-commit-id: e9b2e7089285147df68968ed3f6d30f5ca6fe134 [formerly 37d4401747d6e9f6663fe67a3f0dfbecd024572a] [formerly 615962f5dde47a1a617432989e590311cb7d4b9a] [formerly 0e214bcd7c6a3a18da468024f3c772a812687785 [formerly 615962f5dde47a1a617432989e590311cb7d4b9a [formerly 7a58d8a17be53fc504cafe32d19de2f52121eeff]]]
    Former-commit-id: 0e214bcd7c6a3a18da468024f3c772a812687785
    Former-commit-id: 2f70306617907f26efb4f42d9c736b68c3f4f5a2 [formerly 2801c4fdfbad7c5052568aba4207f60b1c96b430]
    Former-commit-id: 8d94771854d8ae97a828338510475e8d4cbfa5a1

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/CollaborationEditor.java
@@ -0,0 +1,56 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.editor;
+
+import com.raytheon.uf.viz.core.rsc.IInputHandler.InputPriority;
+import com.raytheon.viz.ui.editor.AbstractEditor;
+import com.raytheon.viz.ui.panes.PaneManager;
+
+/**
+ * A collaboration editor that displays the display of an editor shared by the
+ * Data Provider.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 16, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public class CollaborationEditor extends AbstractEditor {
+
+    public static final String EDITOR_ID = "com.raytheon.uf.viz.collaboration.display.editor.CollaborationEditor";
+
+    @Override
+    protected PaneManager getNewPaneManager() {
+        PaneManager pm = new PaneManager();
+        pm.registerMouseHandler(new CollaborationEditorInputHandler(),
+                InputPriority.SYSTEM_RESOURCE);
+        return pm;
+    }
+
+}
