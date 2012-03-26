62b16fef0d5072fd0a1b6f7c37c7c79dee559d81
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-03-26 13:05:01 -0500

    Issue #427 initial role based event handling
    Change-Id: I73d4df3f09c7cfa60025142ec5a4dccdad4423bb
    
    Former-commit-id: 4d7a53c5839dd20bbb760600322b3cef24a01330 [formerly b752ced02d726771a6e11a9982967a892d2e55f3]
    Former-commit-id: 619d19e24c198409148d5bdda5743de0535b7a2b

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/role/AbstractRoleEventController.java
@@ -0,0 +1,60 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.role;
+
+import com.raytheon.uf.viz.collaboration.comm.provider.session.VenueSession;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 26, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public abstract class AbstractRoleEventController implements
+        IRoleEventController {
+
+    protected VenueSession session;
+
+    protected AbstractRoleEventController(VenueSession session) {
+        this.session = session;
+    }
+
+    @Override
+    public void startup() {
+        session.registerEventHandler(this);
+    }
+
+    @Override
+    public void shutdown() {
+        session.unRegisterEventHandler(this);
+    }
+
+}
