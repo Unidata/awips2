a5d38a0968cc407b242dc690271e90691ad4edda
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-03-28 14:34:25 -0500

    Issue #427 initial input event code
    Change-Id: I37dc96ed8dbef37d8e3f7a8ae39fc6fcad59f4aa
    
    Former-commit-id: 5fdc253d77e25cccf894d5e57c63b0ee4c05be01 [formerly 6297c04332bb349d8351b07e2ffd1c24c046ccd7]
    Former-commit-id: bbf96992ee51c336a5111dc31ed0b430211edeba

--- cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/role/DataProviderEventController.java
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/role/DataProviderEventController.java
@@ -19,6 +19,9 @@
  **/
 package com.raytheon.uf.viz.collaboration.ui.role;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Event;
+
 import com.google.common.eventbus.Subscribe;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
@@ -29,6 +32,8 @@ import com.raytheon.uf.viz.collaboration.comm.identity.event.ParticipantEventTyp
 import com.raytheon.uf.viz.collaboration.comm.provider.event.VenueParticipantEvent;
 import com.raytheon.uf.viz.collaboration.ui.editor.EditorSetup;
 import com.raytheon.uf.viz.collaboration.ui.editor.SharedEditor;
+import com.raytheon.uf.viz.collaboration.ui.editor.event.InputEvent;
+import com.raytheon.uf.viz.core.IDisplayPane;
 import com.raytheon.viz.ui.VizWorkbenchManager;
 import com.raytheon.viz.ui.editor.AbstractEditor;
 
@@ -62,6 +67,8 @@ public class DataProviderEventController extends AbstractRoleEventController {
     @Subscribe
     public void participantChanged(VenueParticipantEvent event) {
         if (event.getEventType().equals(ParticipantEventType.ARRIVED)) {
+            // TODO this seems to trigger when you create the room, in which
+            // case you don't need to send it for yourself
             // TODO instead of going to active editor, should get ones
             // specifically shared with this session
             AbstractEditor editor = (AbstractEditor) VizWorkbenchManager
@@ -75,6 +82,85 @@ public class DataProviderEventController extends AbstractRoleEventController {
                                 + event.getParticipant().getName(), e);
             }
         }
+    }
+
+    @Subscribe
+    public void sessionLeaderInput(InputEvent event) {
+        // TODO TBD will this pick up events sent by the data provider (ie this
+        // cave) too? if so, need to rework it cause this code should only
+        // execute on the data provider when someone else's cave is the session
+        // leader. ideally no InputEvents are sent when data provider and
+        // session leader are one and the same
+
+        // TODO get the actively shared editor, not the active editor
+        AbstractEditor editor = (AbstractEditor) VizWorkbenchManager
+                .getInstance().getActiveEditor();
+        IDisplayPane pane = editor.getDisplayPanes()[0];
+        Event swtEvent = new Event();
+
+        // translate event type
+        switch (event.getType()) {
+        case MOUSE_DOWN:
+            swtEvent.type = SWT.MouseDown;
+            break;
+        case MOUSE_UP:
+            swtEvent.type = SWT.MouseUp;
+            break;
+        case MOUSE_DOWN_MOVE:
+        case MOUSE_MOVE:
+            swtEvent.type = SWT.MouseMove;
+            break;
+        case DOUBLE_CLICK:
+            swtEvent.type = SWT.MouseDoubleClick;
+            break;
+        case MOUSE_HOVER:
+            swtEvent.type = SWT.MouseHover;
+            break;
+        case MOUSE_WHEEL:
+            swtEvent.type = SWT.MouseWheel;
+            break;
+        case KEY_DOWN:
+            swtEvent.type = SWT.KeyDown;
+            break;
+        case KEY_UP:
+            swtEvent.type = SWT.KeyUp;
+            break;
+        }
+
+        // translate coordinates of event
+        switch (event.getType()) {
+        case MOUSE_DOWN:
+        case MOUSE_DOWN_MOVE:
+        case MOUSE_UP:
+        case MOUSE_HOVER:
+        case MOUSE_MOVE:
+        case MOUSE_WHEEL:
+        case DOUBLE_CLICK:
+            double[] screen = pane.gridToScreen(new double[] { event.getX(),
+                    event.getY() });
+            swtEvent.x = (int) Math.round(screen[0]);
+            swtEvent.y = (int) Math.round(screen[1]);
+            break;
+        }
+
+        // translate specific metadata
+        switch (event.getType()) {
+        case MOUSE_DOWN:
+        case MOUSE_DOWN_MOVE:
+        case MOUSE_UP:
+        case DOUBLE_CLICK:
+            swtEvent.button = event.getEventData();
+            break;
+        case MOUSE_WHEEL:
+            swtEvent.count = event.getEventData();
+            break;
+        case KEY_DOWN:
+        case KEY_UP:
+            swtEvent.keyCode = event.getEventData();
+            break;
+        }
+
+        editor.getMouseManager().handleEvent(swtEvent);
     }
 
 }
