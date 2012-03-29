0fa5ab5052e552dab3416c48cb9725ab76df08bc
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-03-29 10:44:08 -0500

    Issue #427 Right click on editor tab support
    Change-Id: Ia9240a01c145c81bbf98268d4df5fd2242ca7987
    
    Former-commit-id: a4965d6bf99e6e9d631466797bb349fd7882efbe [formerly 202bd0adf8e876274a50e6f54f9cbdfef289e86f] [formerly d2d3a245b3570cf445f54422c55fb95264a0e031] [formerly a4965d6bf99e6e9d631466797bb349fd7882efbe [formerly 202bd0adf8e876274a50e6f54f9cbdfef289e86f] [formerly d2d3a245b3570cf445f54422c55fb95264a0e031] [formerly aa000b4b06914f7d7ca5b97f4c98438b8106daeb [formerly d2d3a245b3570cf445f54422c55fb95264a0e031 [formerly 7597db5bbc0642072143e4c7ea0291108154eb40]]]]
    Former-commit-id: aa000b4b06914f7d7ca5b97f4c98438b8106daeb
    Former-commit-id: 274e2df58695be0fa219a23ab83165f3ec7e047c [formerly 9792be406a7727cdb661b16e978ef5f700c9c86d] [formerly a6d73562ce75aff32ab9bfe5f1b0ddce7553fd0c [formerly 6aae45687a6127b1ddc7e6ca249587a753366bd3]]
    Former-commit-id: ba593db28636bbaf2f46e5a77d5b70b957e81f82 [formerly ac8087a21a66cffe53188c6b58e6f43f8ae8c5c4]
    Former-commit-id: a0c6c92587d9ff3222883500a3bfa0d46be4cfa6

---
+++ cave/com.raytheon.viz.ui/src/com/raytheon/viz/ui/actions/ContributedEditorMenuAction.java
@@ -0,0 +1,81 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.viz.ui.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.ui.internal.presentations.ISelfUpdatingAction;
+import org.eclipse.ui.presentations.IPresentablePart;
+
+/**
+ * Every type of user contributed action to the editor should extend this class
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 21, 2012            mnash     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mnash
+ * @version 1.0
+ */
+
+@SuppressWarnings("restriction")
+public class ContributedEditorMenuAction extends Action implements
+        ISelfUpdatingAction {
+
+    private IPresentablePart part;
+
+    /**
+ * 
+ */
+    public ContributedEditorMenuAction() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void update() {
+        // do nothing to for this class
+    }
+
+    @Override
+    public boolean shouldBeVisible() {
+        return true;
+    }
+
+    /**
+     * @param part
+     *            the part to set
+     */
+    public void setPart(IPresentablePart part) {
+        this.part = part;
+    }
+
+    /**
+     * @return the part
+     */
+    public IPresentablePart getPart() {
+        return part;
+    }
+
+}
