4d7572a632dc9d79fe533537421c60f7d3c77c89
Steve Harris <Steven_L_Harris@raytheon.com>
2012-03-30 09:50:48 -0500

    Merge branch 'core_exp' into ss_sync (Graphics Improvements for NPP/Collaboration)
    Initial reconciliation of Graphics Improvements for NPP/Collaboration with 12.3.1 baseline
    
    Conflicts:
    	cave/com.raytheon.uf.viz.derivparam/src/com/raytheon/uf/viz/derivparam/tree/DerivedLevelNode.java*
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/AbstractGLMesh.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/GLCanvasCache.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/GLFactoryAdapter.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/GLGeometryPainter.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/IGLTarget.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/images/AbstractGLImage.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/images/GLCMTextureData.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/images/GLColormappedImage.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/images/GLImage.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/internal/GLTarget.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/internal/GLWireframeShape2D.java
    	cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/objects/GLVertexBufferObject.java
    	cave/com.raytheon.viz.grid/src/com/raytheon/viz/grid/util/GribDataCubeAdapter.java
    	cave/com.raytheon.viz.pointdata/src/com/raytheon/viz/pointdata/drawables/GeneralPointImageExtension.java
    	cave/com.raytheon.viz.pointdata/src/com/raytheon/viz/pointdata/drawables/IPointImageExtension.java
    	cave/com.raytheon.viz.pointdata/src/com/raytheon/viz/pointdata/rsc/PlotResource2.java*
    	cave/com.raytheon.viz.radar/src/com/raytheon/viz/radar/rsc/mosaic/ext/IRadarMosaicImageExtension.java
    	edexOsgi/com.raytheon.uf.common.time/src/com/raytheon/uf/common/time/msgs/GetServerTimeRequest.java
    
    Former-commit-id: 5e1b9319941eed516a08a135a2049c14bad77863

--- cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/map/IMapMeshExtension.java
+++ cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/map/IMapMeshExtension.java
@@ -19,7 +19,12 @@
  **/
 package com.raytheon.uf.viz.core.map;
 
+import org.geotools.coverage.grid.GeneralGridGeometry;
+import org.geotools.coverage.grid.GridGeometry2D;
+
 import com.raytheon.uf.viz.core.IMesh;
+import com.raytheon.uf.viz.core.drawables.IDescriptor;
+import com.raytheon.uf.viz.core.drawables.ext.GraphicsExtension.IGraphicsExtensionInterface;
 import com.raytheon.uf.viz.core.exception.VizException;
 
 /**
@@ -39,17 +44,31 @@ import com.raytheon.uf.viz.core.exception.VizException;
  * @version 1.0
  */
 
-public interface IMapMeshExtension {
+public interface IMapMeshExtension extends IGraphicsExtensionInterface {
 
     /**
-     * Create a mesh
+     * Constructs a mesh for mapping the imageGeometry onto the targetGeometry
      * 
-     * @param descriptor
-     * 
+     * @param imageGeometry
+     * @param targetGeometry
      * @return
      * @throws VizException
      */
-    public abstract IMesh constructMesh(IMapDescriptor descriptor)
-            throws VizException;
+    public abstract IMesh constructMesh(GridGeometry2D imageGeometry,
+            GeneralGridGeometry targetGeometry) throws VizException;
+
+    /**
+     * Convenient method for constructing a mesh for mapping the imageGeometry
+     * onto the targetDescriptor. Same as calling
+     * {@link #constructMesh(GridGeometry2D, GeneralGridGeometry)} passing in
+     * target.getGridGeometry()
+     * 
+     * @param imageGeometry
+     * @param targetDescriptor
+     * @return
+     * @throws VizException
+     */
+    public abstract IMesh constructMesh(GridGeometry2D imageGeometry,
+            IDescriptor targetDescriptor) throws VizException;
 
 }
