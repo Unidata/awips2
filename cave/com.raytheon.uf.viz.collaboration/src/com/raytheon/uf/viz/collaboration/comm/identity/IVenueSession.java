ea6d408a9e1ccb1b6e3708da8916d1bf8e8f1387
James Korman <james.korman@raytheon.com>
2012-03-30 11:27:09 -0500

    Issue #232 - Modified invitation methods
    Former-commit-id: 80ea4c01044d2eac349e88653de57a1a5b90e75b [formerly a907eeb4edb366c81d617ca0741ca8ba3eb8002f] [formerly 436b967ff1c36274ed92236ea84a944cc8212a9c] [formerly 0b6a076129b0f947658b978391c30edc091c8299 [formerly 436b967ff1c36274ed92236ea84a944cc8212a9c [formerly 1c9381e1ba1a8b01084717db7ff3e94b26eb0b89]]]
    Former-commit-id: 0b6a076129b0f947658b978391c30edc091c8299
    Former-commit-id: 110dbacc4d32bab470b9fd9825b11ea1516e3ff2 [formerly 48fb9fe7e40e6731ad1968634625090166b42240]
    Former-commit-id: ae115263ce6d8e182771e4ea77829da5d740da10

--- cave/com.raytheon.uf.viz.collaboration/src/com/raytheon/uf/viz/collaboration/comm/identity/IVenueSession.java
+++ cave/com.raytheon.uf.viz.collaboration/src/com/raytheon/uf/viz/collaboration/comm/identity/IVenueSession.java
@@ -31,30 +31,34 @@ import com.raytheon.uf.viz.collaboration.comm.identity.listener.IVenueParticipan
 import com.raytheon.uf.viz.collaboration.comm.identity.user.ParticipantRole;
 
 /**
- *
+ * 
  * 
  * <ul>
- * <li>EventBus subscription events. Implementors are required to post the following events.</li>
+ * <li>EventBus subscription events. Implementors are required to post the
+ * following events.</li>
  * <ul>
- * <li><strong>IVenueParticipantEvent</strong> : This event is posted when a venue participant enters, leaves a venue, or
- * updates their status in the venue.</li>
- * <li><strong>TextMessage</strong> : Text messages send between users. Meant to be displayed as conversation.</li>
- * <li><strong>CollaborationMessage</strong> : These messages are CAVE to CAVE command messages.</li>
+ * <li><strong>IVenueParticipantEvent</strong> : This event is posted when a
+ * venue participant enters, leaves a venue, or updates their status in the
+ * venue.</li>
+ * <li><strong>TextMessage</strong> : Text messages send between users. Meant to
+ * be displayed as conversation.</li>
+ * <li><strong>CollaborationMessage</strong> : These messages are CAVE to CAVE
+ * command messages.</li>
  * </ul>
  * </ul>
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Mar 5, 2012            jkorman     Initial creation
- *
+ * 
  * </pre>
- *
+ * 
  * @author jkorman
- * @version 1.0	
+ * @version 1.0
  */
 
 public interface IVenueSession extends ISession, IEventPublisher {
@@ -62,16 +66,18 @@ public interface IVenueSession extends ISession, IEventPublisher {
     /**
      * Return this session as an ISharedDisplaySession if it is supported. If
      * the interface is not supported the method must return a null reference.
-     * @return 
+     * 
+     * @return
      */
     ISharedDisplaySession spawnSharedDisplaySession();
-    
+
     /**
-     * Returns information about a venue. 
+     * Returns information about a venue.
+     * 
      * @return Information about a venue
      */
     IVenue getVenue();
-    
+
     /**
      * 
      * @param role
@@ -80,36 +86,48 @@ public interface IVenueSession extends ISession, IEventPublisher {
     boolean hasRole(ParticipantRole role);
 
     /**
+     * Set the subject of this collaboration venue conversation.
+     * 
+     * @param subject
+     *            The subject.
+     */
+    void setSubject(String subject);
+
+    /**
+     * Get the subject of this collaboration venue conversation.
+     * 
+     * @return The subject.
+     */
+    String getSubject();
+
+    /**
      * Send a Collaboration message.
-     * @param message The message to send.
+     * 
+     * @param message
+     *            The message to send.
      */
     void sendTextMessage(String message) throws CollaborationException;
 
     /**
      * Send an invitation from this venue to another user.
-     * @param invitation An invitation
+     * 
+     * @param id
+     *            The target user for this invitation.
+     * @param subject
+     *            The intended subject of the venue conversation.
      * @return
      */
-    int sendInvitation(IInvitation invitation);
-    
+    int sendInvitation(String id, String body);
+
     /**
-     * Send an invitation from this venue to another user.
-     * @param room The target venue for this invitation.
-     * @param id The target user for this invitation.
-     * @param subject The intended subject of the venue conversation.
-     * @param body Any text that the user may wish to include.
+     * Send an invitation from this venue to one or more users.
+     * 
+     * @param ids
+     *            A list of target users for this invitation.
+     * @param body
+     *            Any text that the user may wish to include.
      * @return
      */
-    int sendInvitation(String room, String id, String subject, String body);
-    
-    /**
-     * Send an invitation from this venue to another user.
-     * @param room The target venue for this invitation.
-     * @param ids A list of target users for this invitation.
-     * @param subject The intended subject of the venue conversation.
-     * @param body Any text that the user may wish to include.
-     * @return
-     */
-    int sendInvitation(String room, List<String> ids, String subject, String body);
-    
+    int sendInvitation(List<String> ids, String body);
+
 }
