6623dd703db0de962791298d53bd83a81fd43338
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-03 15:11:20 -0500

    Issue #239 Start of remote target capabilities
    Former-commit-id: d2d03e2c64d58a15a131d58484c226dfd4d94625 [formerly ac1fc79f5b4f3d2eebfb8f21591c3726b1405f62 [formerly 6c411ffb07d5480ec98b7ee8bb518e763f9f6918] [formerly d2d03e2c64d58a15a131d58484c226dfd4d94625 [formerly 30b76609cff9a737b8e4190bbfebb5a37a259fad]]]
    Former-commit-id: ac1fc79f5b4f3d2eebfb8f21591c3726b1405f62 [formerly 6c411ffb07d5480ec98b7ee8bb518e763f9f6918]
    Former-commit-id: ac1fc79f5b4f3d2eebfb8f21591c3726b1405f62
    Former-commit-id: 6e94ff6098ca6156d322e9ef8a8c046ee9114557

---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/DispatchingGraphicsFactory.java
@@ -0,0 +1,182 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics;
+
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.opengis.coverage.grid.GridEnvelope;
+
+import com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter;
+import com.raytheon.uf.viz.core.IDisplayPaneContainer;
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.IView;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.vividsolutions.jts.geom.Coordinate;
+
+/**
+ * Graphics factory for creating graphics objects that forward (dispatch)
+ * important events for remote processing
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 5, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class DispatchingGraphicsFactory extends AbstractGraphicsFactoryAdapter {
+
+    private AbstractGraphicsFactoryAdapter delegate;
+
+    private Dispatcher dispatcher;
+
+    private DispatchingGraphicsFactory(AbstractGraphicsFactoryAdapter delegate,
+            Dispatcher dispatcher) {
+        this.delegate = delegate;
+        this.dispatcher = dispatcher;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructView()
+     */
+    @Override
+    public IView constructView() throws VizException {
+        // TODO: Construct DispatchingView?
+        return delegate.constructView();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructTarget
+     * (org.eclipse.swt.widgets.Canvas, float, float)
+     */
+    @Override
+    public IGraphicsTarget constructTarget(Canvas canvas, float width,
+            float height) throws VizException {
+        return new DispatchGraphicsTarget(delegate.constructTarget(canvas,
+                width, height), dispatcher);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructExtent
+     * (com.vividsolutions.jts.geom.Coordinate[])
+     */
+    @Override
+    public IExtent constructExtent(Coordinate[] coords) throws VizException {
+        return delegate.constructExtent(coords);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructExtent
+     * (double, double, double, double)
+     */
+    @Override
+    public IExtent constructExtent(double aMinX, double aMaxX, double aMinY,
+            double aMaxY) throws VizException {
+        return delegate.constructExtent(aMinX, aMaxX, aMinY, aMaxY);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructExtent
+     * (org.eclipse.swt.graphics.Rectangle)
+     */
+    @Override
+    public IExtent constructExtent(Rectangle rect) throws VizException {
+        return delegate.constructExtent(rect);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constructExtent
+     * (org.opengis.coverage.grid.GridEnvelope)
+     */
+    @Override
+    public IExtent constructExtent(GridEnvelope range) throws VizException {
+        return delegate.constructExtent(range);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.AbstractGraphicsFactoryAdapter#constrcutCanvas
+     * (org.eclipse.swt.widgets.Composite)
+     */
+    @Override
+    public Canvas constrcutCanvas(Composite canvasComp) throws VizException {
+        return delegate.constrcutCanvas(canvasComp);
+    }
+
+    public static void injectRemoteFunctionality(
+            IDisplayPaneContainer container, DispatcherFactory factory) {
+        // TODO: Do this more cleanly and effectively
+        // for (IDisplayPane pane : container.getDisplayPanes()) {
+        // Dispatcher dispatcher = factory.createNewDispatcher();
+        //
+        // VizDisplayPane vizPane = (VizDisplayPane) pane;
+        //
+        // // Wrap view in dispatching view
+        // IRenderableDisplay display = pane.getRenderableDisplay();
+        // ((AbstractRenderableDisplay) display).setView(new ViewWrapper(
+        // display.getView()));
+        //
+        // // Wrap the graphics adapter in dispatching one
+        // vizPane.setGraphicsAdapter(new DispatchingGraphicsFactory(vizPane
+        // .getGraphicsAdapter(), dispatcher));
+        //
+        // // Wrap target in dispatching target
+        // vizPane.setTarget(new DispatchGraphicsTarget(vizPane.getTarget(),
+        // dispatcher));
+        // display.setup(vizPane.getTarget());
+        //
+        // for (ResourcePair rp : pane.getDescriptor().getResourceList()) {
+        // if (rp.getResource() != null) {
+        // rp.getResource().recycle();
+        // }
+        // }
+        // }
+    }
+
+}
