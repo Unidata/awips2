522382bb0bae40229c65e52392f3c66ead9b9182
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-03 15:11:20 -0500

    Issue #239 Start of remote target capabilities
    Former-commit-id: 6c411ffb07d5480ec98b7ee8bb518e763f9f6918 [formerly 6c411ffb07d5480ec98b7ee8bb518e763f9f6918 [formerly 30b76609cff9a737b8e4190bbfebb5a37a259fad]]
    Former-commit-id: d2d03e2c64d58a15a131d58484c226dfd4d94625
    Former-commit-id: 5d7e504e89364e7eb35f3847bb701b427747953a

---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/objects/DispatchingMesh.java
@@ -0,0 +1,104 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics.objects;
+
+import org.geotools.coverage.grid.GeneralGridGeometry;
+
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.IMesh;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.remote.graphics.Dispatcher;
+import com.raytheon.uf.viz.remote.graphics.DispatchingObject;
+import com.raytheon.uf.viz.remote.graphics.events.RemoteGraphicsEventFactory;
+import com.raytheon.uf.viz.remote.graphics.events.mesh.DisposeMeshEvent;
+import com.raytheon.uf.viz.remote.graphics.events.mesh.ReprojectMeshEvent;
+
+/**
+ * Dispatching mesh object created from graphics mesh and forwards key events to
+ * remote clients
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 5, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class DispatchingMesh extends DispatchingObject<IMesh> implements IMesh {
+
+    /**
+     * @param targetObject
+     * @param dispatcher
+     */
+    public DispatchingMesh(IMesh targetObject, Dispatcher dispatcher) {
+        super(targetObject, dispatcher);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.core.IMesh#dispose()
+     */
+    @Override
+    public void dispose() {
+        wrappedObject.dispose();
+        // Send event to dispose mesh
+        dispatch(RemoteGraphicsEventFactory.createEvent(DisposeMeshEvent.class,
+                this));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.IMesh#intersects(com.raytheon.uf.viz.core.IExtent
+     * )
+     */
+    @Override
+    public boolean intersects(IExtent extent) {
+        return wrappedObject.intersects(extent);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.core.IMesh#reproject(org.geotools.coverage.grid.
+     * GeneralGridGeometry)
+     */
+    @Override
+    public void reproject(GeneralGridGeometry targetGeometry)
+            throws VizException {
+        wrappedObject.reproject(targetGeometry);
+
+        // Send event for reprojection
+        ReprojectMeshEvent event = RemoteGraphicsEventFactory.createEvent(
+                ReprojectMeshEvent.class, this);
+        event.setTargetGeometry(targetGeometry);
+        dispatch(event);
+    }
+
+}
