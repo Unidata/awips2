a20e542438eea0c4e3289768f489d85c95613729
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-04-04 17:06:00 -0500

    Issue #427 fixed subject
    Change-Id: Ieb5499e16b2c30169fe49eb06d73b8e3ede6ce70
    
    Former-commit-id: d68efe319e061f1d262360d823b42d711b46a187 [formerly b084156a30cfbbaaa6cf4a32e28a724154179c96] [formerly 4999e547a1a0c70698934f163c2ab470c84db998 [formerly 8908b81e0e47cfe112ad132a82f2b2cb1a0dd55e]]
    Former-commit-id: 4999e547a1a0c70698934f163c2ab470c84db998
    Former-commit-id: 477d1bb0137432f6309ab34c372f883577fabbd9

--- cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/event/EventForwardingInputHandler.java
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/event/EventForwardingInputHandler.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.viz.collaboration.ui.editor;
+package com.raytheon.uf.viz.collaboration.ui.editor.event;
 
 import org.eclipse.swt.widgets.Event;
 
@@ -27,15 +27,13 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
 import com.raytheon.uf.viz.collaboration.comm.identity.CollaborationException;
 import com.raytheon.uf.viz.collaboration.comm.identity.ISharedDisplaySession;
 import com.raytheon.uf.viz.collaboration.comm.identity.event.IDisplayEvent;
-import com.raytheon.uf.viz.collaboration.ui.editor.event.InputEvent;
 import com.raytheon.uf.viz.collaboration.ui.editor.event.InputEvent.EventType;
 import com.raytheon.uf.viz.core.IDisplayPane;
 import com.raytheon.uf.viz.core.rsc.IInputHandler;
 
 /**
- * An input handler that disables mouse and key input if it's not the Session
- * Leader. If it is the Session Leader, it sends out events related to the
- * input.
+ * An input handler that forwards events to the Data Provider. Intended for use
+ * by the Session Leader.
  * 
  * <pre>
  * 
@@ -51,16 +49,16 @@ import com.raytheon.uf.viz.core.rsc.IInputHandler;
  * @version 1.0
  */
 
-public class CollaborationEditorInputHandler implements IInputHandler {
+public class EventForwardingInputHandler implements IInputHandler {
 
     private static final transient IUFStatusHandler statusHandler = UFStatus
-            .getHandler(CollaborationEditorInputHandler.class);
+            .getHandler(EventForwardingInputHandler.class);
 
     private ISharedDisplaySession session;
 
     private IDisplayPane displayPane;
 
-    public CollaborationEditorInputHandler(ISharedDisplaySession session,
+    public EventForwardingInputHandler(ISharedDisplaySession session,
             IDisplayPane displayPane) {
         this.session = session;
         this.displayPane = displayPane;
@@ -75,12 +73,6 @@ public class CollaborationEditorInputHandler implements IInputHandler {
         }
     }
 
-    protected boolean isSessionLeader() {
-        // TODO does this work?
-        // return session.getUserID().equals(session.getCurrentSessionLeader());
-        return false;
-    }
-
     /*
      * (non-Javadoc)
      * 
@@ -89,14 +81,11 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleMouseDown(int x, int y, int mouseButton) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            double[] coords = displayPane.screenToGrid(x, y, 0);
-            InputEvent event = new InputEvent(EventType.MOUSE_DOWN, coords[0],
-                    coords[1], mouseButton);
-            sendEvent(event);
-        }
-        return !leader;
+        double[] coords = displayPane.screenToGrid(x, y, 0);
+        InputEvent event = new InputEvent(EventType.MOUSE_DOWN, coords[0],
+                coords[1], mouseButton);
+        sendEvent(event);
+        return false;
     }
 
     /*
@@ -107,14 +96,11 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleMouseDownMove(int x, int y, int mouseButton) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            double[] coords = displayPane.screenToGrid(x, y, 0);
-            InputEvent event = new InputEvent(EventType.MOUSE_DOWN_MOVE,
-                    coords[0], coords[1], mouseButton);
-            sendEvent(event);
-        }
-        return !leader;
+        double[] coords = displayPane.screenToGrid(x, y, 0);
+        InputEvent event = new InputEvent(EventType.MOUSE_DOWN_MOVE, coords[0],
+                coords[1], mouseButton);
+        sendEvent(event);
+        return false;
     }
 
     /*
@@ -125,14 +111,11 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleMouseUp(int x, int y, int mouseButton) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            double[] coords = displayPane.screenToGrid(x, y, 0);
-            InputEvent event = new InputEvent(EventType.MOUSE_UP, coords[0],
-                    coords[1], mouseButton);
-            sendEvent(event);
-        }
-        return !leader;
+        double[] coords = displayPane.screenToGrid(x, y, 0);
+        InputEvent event = new InputEvent(EventType.MOUSE_UP, coords[0],
+                coords[1], mouseButton);
+        sendEvent(event);
+        return false;
     }
 
     /*
@@ -144,7 +127,7 @@ public class CollaborationEditorInputHandler implements IInputHandler {
     @Override
     public boolean handleMouseHover(int x, int y) {
         // TODO doesn't do anything right now to reduce bandwidth
-        return !isSessionLeader();
+        return false;
     }
 
     /*
@@ -155,7 +138,7 @@ public class CollaborationEditorInputHandler implements IInputHandler {
     @Override
     public boolean handleMouseMove(int x, int y) {
         // TODO doesn't do anything right now to reduce bandwidth
-        return !isSessionLeader();
+        return false;
     }
 
     /*
@@ -167,7 +150,7 @@ public class CollaborationEditorInputHandler implements IInputHandler {
     @Override
     public boolean handleDoubleClick(int x, int y, int button) {
         // TODO doesn't do anything right now to reduce bandwidth
-        return !isSessionLeader();
+        return false;
     }
 
     /*
@@ -179,14 +162,11 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleMouseWheel(Event event, int x, int y) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            double[] coords = displayPane.screenToGrid(x, y, 0);
-            InputEvent mevent = new InputEvent(EventType.MOUSE_WHEEL,
-                    coords[0], coords[1], event.count);
-            sendEvent(mevent);
-        }
-        return !leader;
+        double[] coords = displayPane.screenToGrid(x, y, 0);
+        InputEvent mevent = new InputEvent(EventType.MOUSE_WHEEL, coords[0],
+                coords[1], event.count);
+        sendEvent(mevent);
+        return false;
     }
 
     /*
@@ -199,7 +179,7 @@ public class CollaborationEditorInputHandler implements IInputHandler {
     @Override
     public boolean handleMouseExit(Event event) {
         // TODO doesn't do anything right now to reduce bandwidth
-        return !isSessionLeader();
+        return false;
     }
 
     /*
@@ -212,7 +192,7 @@ public class CollaborationEditorInputHandler implements IInputHandler {
     @Override
     public boolean handleMouseEnter(Event event) {
         // TODO doesn't do anything right now to reduce bandwidth
-        return !isSessionLeader();
+        return false;
     }
 
     /*
@@ -222,13 +202,9 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleKeyDown(int keyCode) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            InputEvent event = new InputEvent(EventType.KEY_DOWN, -1, -1,
-                    keyCode);
-            sendEvent(event);
-        }
-        return !leader;
+        InputEvent event = new InputEvent(EventType.KEY_DOWN, -1, -1, keyCode);
+        sendEvent(event);
+        return false;
     }
 
     /*
@@ -238,13 +214,9 @@ public class CollaborationEditorInputHandler implements IInputHandler {
      */
     @Override
     public boolean handleKeyUp(int keyCode) {
-        boolean leader = isSessionLeader();
-        if (leader) {
-            InputEvent event = new InputEvent(EventType.KEY_DOWN, -1, -1,
-                    keyCode);
-            sendEvent(event);
-        }
-        return !leader;
+        InputEvent event = new InputEvent(EventType.KEY_UP, -1, -1, keyCode);
+        sendEvent(event);
+        return false;
     }
 
 }
