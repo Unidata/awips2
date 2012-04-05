a91defee85ab822513c875095b06911af6da8b2a
Matt Nash <Matt_Nash@raytheon.com>
2012-04-05 10:27:10 -0500

    Issue #429 telestrator allows all users to draw
    Former-commit-id: 0c77bdc25d171d97f392c0c94bf608273ce0ab9f [formerly 0c77bdc25d171d97f392c0c94bf608273ce0ab9f [formerly 916b8428e4cc279320a4cc90c8113ef24661f950]] [formerly f200e37a362b590d334a56817419d126ad3befbf] [formerly fd1b12391290a833d8c94feb699818de1a245534 [formerly fd1b12391290a833d8c94feb699818de1a245534 [formerly 09c022bda821e4635de95d7219b28eab794e2ae0]] [formerly f200e37a362b590d334a56817419d126ad3befbf [formerly f200e37a362b590d334a56817419d126ad3befbf [formerly ed59b0803da528f391db3c71aac1184581cc353d]]]]
    Former-commit-id: fd1b12391290a833d8c94feb699818de1a245534 [formerly fd1b12391290a833d8c94feb699818de1a245534 [formerly 09c022bda821e4635de95d7219b28eab794e2ae0]]
    Former-commit-id: fd1b12391290a833d8c94feb699818de1a245534
    Former-commit-id: bdfef01f4a2014b64db1f26418490ea3a515a963 [formerly f985ddaa7ebb11a03d1cc6420d389fad04fddbef]
    Former-commit-id: 70e997fe56be09976108849cc2dd2938f78bd1c0

--- cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/telestrator/CollaborationDrawingLayer.java
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/telestrator/CollaborationDrawingLayer.java
@@ -25,7 +25,7 @@ import java.util.Map;
 
 import org.eclipse.swt.graphics.RGB;
 
-import com.google.common.collect.HashMultimap;
+import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.google.common.eventbus.Subscribe;
@@ -36,12 +36,14 @@ import com.raytheon.uf.viz.collaboration.comm.identity.event.IDisplayEvent;
 import com.raytheon.uf.viz.collaboration.data.CollaborationDataManager;
 import com.raytheon.uf.viz.collaboration.ui.telestrator.event.ClearDrawingEvent;
 import com.raytheon.uf.viz.collaboration.ui.telestrator.event.CollaborationDrawingEvent;
+import com.raytheon.uf.viz.collaboration.ui.telestrator.event.RedoDrawingEvent;
+import com.raytheon.uf.viz.collaboration.ui.telestrator.event.UndoDrawingEvent;
 import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.RGBColors;
 import com.raytheon.uf.viz.core.drawables.IWireframeShape;
 import com.raytheon.uf.viz.core.drawables.PaintProperties;
 import com.raytheon.uf.viz.core.exception.VizException;
 import com.raytheon.uf.viz.core.rsc.LoadProperties;
-import com.raytheon.uf.viz.core.rsc.capabilities.ColorableCapability;
 import com.raytheon.uf.viz.core.rsc.capabilities.OutlineCapability;
 import com.raytheon.uf.viz.drawing.DrawingLayer;
 import com.raytheon.uf.viz.drawing.PathDrawingResourceData;
@@ -72,7 +74,9 @@ public class CollaborationDrawingLayer extends DrawingLayer {
 
     private Multimap<RGB, IWireframeShape> collaboratorShapes;
 
-    private List<IWireframeShape> deletedCollaboratorShapes;
+    private List<ShapeContainer> deletedCollaboratorShapes;
+
+    private IWireframeShape tempRemoteShape = null;
 
     private RGB officialColor = null;
 
@@ -87,43 +91,10 @@ public class CollaborationDrawingLayer extends DrawingLayer {
                 .getActiveEditorAs(AbstractEditor.class);
         CollaborationDataManager mgr = CollaborationDataManager.getInstance();
         for (String str : mgr.getSessions().keySet()) {
-            if (editor.equals(mgr.getEditor(str))) {
-                mgr.getSession(str).registerEventHandler(this);
-                break;
-            }
-        }
-    }
-
-    @Subscribe
-    public void handle(IDisplayEvent event) {
-        if (event instanceof ClearDrawingEvent) {
-            resetTemp();
-            disposeInternal();
-            issueRefresh();
-        } else if (event instanceof CollaborationDrawingEvent) {
-            CollaborationDrawingEvent collEvent = (CollaborationDrawingEvent) event;
-            addCollaborationShape(collEvent.getGeom(),
-                    getCapability(ColorableCapability.class).getColor());
-        }
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.raytheon.uf.viz.drawing.DrawingLayer#reset()
-     */
-    @Override
-    public void reset() {
-        super.reset();
-        ClearDrawingEvent event = new ClearDrawingEvent();
-        Map<String, IVenueSession> sessions = CollaborationDataManager
-                .getInstance().getSessions();
-        for (String str : sessions.keySet()) {
-            try {
-                ((ISharedDisplaySession) sessions.get(str)).sendEvent(event);
-            } catch (CollaborationException e) {
-                e.printStackTrace();
-            }
+            // if (editor.equals(mgr.getEditor(str))) {
+            mgr.getSession(str).registerEventHandler(this);
+            break;
+            // }
         }
     }
 
@@ -137,10 +108,10 @@ public class CollaborationDrawingLayer extends DrawingLayer {
     @Override
     protected void initInternal(IGraphicsTarget target) throws VizException {
         super.initInternal(target);
-        this.collaboratorShapes = HashMultimap.create();
+        this.collaboratorShapes = LinkedHashMultimap.create();
         this.collaboratorShapes = Multimaps
                 .synchronizedMultimap(this.collaboratorShapes);
-        this.deletedCollaboratorShapes = new ArrayList<IWireframeShape>();
+        this.deletedCollaboratorShapes = new ArrayList<ShapeContainer>();
         TelestratorColorManager colorManager = TelestratorColorManager
                 .getColorManager();
         officialColor = colorManager.getColorFromUser(CollaborationDataManager
@@ -172,14 +143,129 @@ public class CollaborationDrawingLayer extends DrawingLayer {
         }
     }
 
-    public void addCollaborationShape(Geometry geom, RGB color) {
-        IWireframeShape shape = target.createWireframeShape(false,
-                getDescriptor());
-        drawTempLinePrimitive(geom, shape);
-        synchronized (collaboratorShapes) {
-            collaboratorShapes.put(color, shape);
+    @Subscribe
+    public void handle(IDisplayEvent event) {
+        if (event instanceof ClearDrawingEvent) {
+            resetTemp();
+            // TODO check if session leader, otherwise only remove my wireframe
+            // shapes
+            disposeInternal();
+            issueRefresh();
+        } else if (event instanceof UndoDrawingEvent) {
+            for (RGB rgb : collaboratorShapes.keySet()) {
+                IWireframeShape lastShape = null;
+                for (IWireframeShape shape : collaboratorShapes.get(rgb)) {
+                    lastShape = shape;
+                }
+                collaboratorShapes.values().remove(lastShape);
+            }
+            issueRefresh();
+        } else if (event instanceof RedoDrawingEvent) {
+
+        } else if (event instanceof CollaborationDrawingEvent) {
+            CollaborationDrawingEvent collEvent = (CollaborationDrawingEvent) event;
+            addCollaborationShape(collEvent.getContainer());
+            issueRefresh();
         }
-        issueRefresh();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.drawing.DrawingLayer#addTempDrawLine(com.vividsolutions
+     * .jts.geom.LineString)
+     */
+    @Override
+    public void addTempDrawLine(LineString line) {
+        super.addTempDrawLine(line);
+        // for showing the line on the fly...
+        // sendDrawEvent(line);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.drawing.DrawingLayer#finalizeLine(com.vividsolutions
+     * .jts.geom.LineString, java.lang.String)
+     */
+    @Override
+    public void finalizeLine(LineString line, String uuid) {
+        super.finalizeLine(line, uuid);
+        // for showing the line only after the artist lets up on the mouse
+        sendDrawEvent(line);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.drawing.DrawingLayer#undoAdd()
+     */
+    @Override
+    public void undoAdd() {
+        super.undoAdd();
+        UndoDrawingEvent event = new UndoDrawingEvent();
+        sendGenericEvent(event);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.drawing.DrawingLayer#redoAdd()
+     */
+    @Override
+    public void redoAdd() {
+        super.redoAdd();
+        RedoDrawingEvent event = new RedoDrawingEvent();
+        sendGenericEvent(event);
+    }
+
+    private void sendDrawEvent(LineString line) {
+        ShapeContainer container = new ShapeContainer();
+        String color = RGBColors.getColorName(officialColor);
+        container.setRgb(color);
+        container.setGeom(line);
+        CollaborationDrawingEvent tObject = new CollaborationDrawingEvent();
+        tObject.setContainer(container);
+        sendGenericEvent(tObject);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.drawing.DrawingLayer#reset()
+     */
+    @Override
+    public void reset() {
+        super.reset();
+        if (/* is session leader */false) {
+            ClearDrawingEvent event = new ClearDrawingEvent();
+            sendGenericEvent(event);
+        }
+    }
+
+    private void sendGenericEvent(IDisplayEvent event) {
+        Map<String, IVenueSession> sessions = CollaborationDataManager
+                .getInstance().getSessions();
+        for (String str : sessions.keySet()) {
+            try {
+                ((ISharedDisplaySession) sessions.get(str)).sendEvent(event);
+            } catch (CollaborationException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void addCollaborationShape(ShapeContainer container) {
+        // if (tempRemoteShape == null){
+        tempRemoteShape = target.createWireframeShape(false, getDescriptor());
+        // }
+        drawTempLinePrimitive(container.getGeom(), tempRemoteShape);
+        synchronized (collaboratorShapes) {
+            RGB color = RGBColors.getRGBColor(container.getRgb());
+            collaboratorShapes.put(color, tempRemoteShape);
+        }
     }
 
     public void removeCollaborationShape(Geometry geom, RGB color) {
@@ -197,60 +283,19 @@ public class CollaborationDrawingLayer extends DrawingLayer {
     @Override
     protected void disposeInternal() {
         super.disposeInternal();
-        synchronized (collaboratorShapes) {
-            for (IWireframeShape shape : collaboratorShapes.values()) {
-                shape.dispose();
+        if (/* is session leader */false) {
+            synchronized (collaboratorShapes) {
+                for (IWireframeShape shape : collaboratorShapes.values()) {
+                    shape.dispose();
+                }
             }
-        }
 
-        for (IWireframeShape shape : deletedCollaboratorShapes) {
-            shape.dispose();
-        }
-
-        collaboratorShapes.clear();
-        deletedCollaboratorShapes.clear();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.viz.drawing.DrawingLayer#finalizeLine(com.vividsolutions
-     * .jts.geom.LineString, java.lang.String)
-     */
-    @Override
-    public void finalizeLine(LineString line, String uuid) {
-        super.finalizeLine(line, uuid);
-        sendDrawEvent(line);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.viz.drawing.DrawingLayer#addTempDrawLine(com.vividsolutions
-     * .jts.geom.LineString)
-     */
-    @Override
-    public void addTempDrawLine(LineString line) {
-        super.addTempDrawLine(line);
-        // sendDrawEvent(line);
-    }
-
-    private void sendDrawEvent(LineString line) {
-        Map<String, IVenueSession> sessions = CollaborationDataManager
-                .getInstance().getSessions();
-        CollaborationDrawingEvent tObject = new CollaborationDrawingEvent(line,
-                officialColor);
-        // get the color of the user here, before sending it off
-        AbstractEditor editor = EditorUtil
-                .getActiveEditorAs(AbstractEditor.class);
-        for (String str : sessions.keySet()) {
-            try {
-                ((ISharedDisplaySession) sessions.get(str)).sendEvent(tObject);
-            } catch (CollaborationException e) {
-                e.printStackTrace();
+            for (ShapeContainer cont : deletedCollaboratorShapes) {
+                cont.shape.dispose();
             }
+
+            collaboratorShapes.clear();
+            deletedCollaboratorShapes.clear();
         }
     }
 }
