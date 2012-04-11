1c0c8374b3df492fc2310210ed82ebb84038d306
James Korman <james.korman@raytheon.com>
2012-04-11 15:37:16 -0500

    Issue #437 - Added event notification for roster/roster status updates
    Former-commit-id: 36a0b2e4d8afdea8a5629328e88064667b542898 [formerly 36a0b2e4d8afdea8a5629328e88064667b542898 [formerly b23bd5351991d81a138ef3e551095fc14f6d5d88]] [formerly d664e77844e77ea2cc29c92cd5f2aa35629bbc6c] [formerly 36a0b2e4d8afdea8a5629328e88064667b542898 [formerly 36a0b2e4d8afdea8a5629328e88064667b542898 [formerly b23bd5351991d81a138ef3e551095fc14f6d5d88]] [formerly d664e77844e77ea2cc29c92cd5f2aa35629bbc6c] [formerly 14c1af5d72bd692133a97b3424cacde6b65e9ac7 [formerly 14c1af5d72bd692133a97b3424cacde6b65e9ac7 [formerly f25744ed0fbb1da8a867a6707e1ebb90932fc930]] [formerly d664e77844e77ea2cc29c92cd5f2aa35629bbc6c [formerly d664e77844e77ea2cc29c92cd5f2aa35629bbc6c [formerly 90f4901c8b7f86a8a0da3be7801d3a5b48b284a8]]]]]
    Former-commit-id: 14c1af5d72bd692133a97b3424cacde6b65e9ac7 [formerly 14c1af5d72bd692133a97b3424cacde6b65e9ac7 [formerly f25744ed0fbb1da8a867a6707e1ebb90932fc930]]
    Former-commit-id: e8e34883f0c506221ca47ee0726a1bc1dc2b6895 [formerly 3813a5b71641a8af2e83ef898a7576a3a7df65f2] [formerly acc4ce6c2e244a33d1687d250dc3d4918fd60f67 [formerly 555679afc0a21b43ec1ab5445f19756ba1ce7777]] [formerly 14c1af5d72bd692133a97b3424cacde6b65e9ac7]
    Former-commit-id: aed3c17c1dcc4ea7fbfc2eff0094ac0e3a4e1e02 [formerly ed34c4c88b867844ff3778c0a9e04aa365061601] [formerly 7a4f07b3847719b4ce21f68a660730da83687ae7 [formerly b42465f64e6d51e2049aa5706624fb83b7ce2faa]]
    Former-commit-id: 0011c5aebbdd3ba417ea6b914270050153e8d7a2 [formerly 0a218f0f68707982ab8bda69646345e4a9b39f0c]
    Former-commit-id: df07cc677726afdee9363ce8192f7520abb6e1f5

---
+++ cave/com.raytheon.uf.viz.collaboration/src/com/raytheon/uf/viz/collaboration/comm/provider/session/RosterEventHandler.java
@@ -0,0 +1,91 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.comm.provider.session;
+
+import java.util.Collection;
+
+import com.google.common.eventbus.Subscribe;
+import com.raytheon.uf.viz.collaboration.comm.identity.IPresence;
+import com.raytheon.uf.viz.collaboration.comm.identity.event.IRosterChangeEvent;
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterEntry;
+import com.raytheon.uf.viz.collaboration.comm.identity.roster.IRosterGroup;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ *
+ * SOFTWARE HISTORY
+ *
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 11, 2012            jkorman     Initial creation
+ *
+ * </pre>
+ *
+ * @author jkorman
+ * @version 1.0	
+ */
+
+public class RosterEventHandler {
+
+    
+    @Subscribe
+    public void eventHandler(IRosterChangeEvent event) {
+        
+        StringBuilder sb = new StringBuilder("-------------------------------------------------------\n");
+        sb.append("RosterEventHandler.eventHandler(");
+        switch(event.getType()) {
+        
+        case ADD : {
+            sb.append("ADD)");
+            break;
+        }
+        case MODIFY : {
+            sb.append("MODIFY)");
+            break;
+        }
+        case DELETE : {
+            sb.append("DELETE)");
+            break;
+        }
+        case PRESENCE : {
+            sb.append("PRESENCE)");
+            break;
+        }
+        }
+        System.out.println(sb.toString());
+        printRosterEntry(event.getEntry());
+    }
+    
+    private void printRosterEntry(IRosterEntry entry) {
+        // System.out.println("handleRosterEntryAdd " + System.currentTimeMillis());
+        System.out.println("    user   : " + entry.getUser().getFQName());
+        Collection<IRosterGroup> groups = entry.getGroups();
+        for(IRosterGroup group : groups) {
+            System.out.println("     " + group.getName());
+        }
+        IPresence presence = entry.getPresence();
+        if(presence != null) {
+            System.out.println("    pres   : " + presence.getType() + " : " + presence.getMode());
+        }
+    }
+    
+}
