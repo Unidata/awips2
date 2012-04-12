13d609af5e1b86733128aaaf1a2c66bca4945502
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-12 16:22:12 -0500

    Issue #239 Added creating of fonts and use in wireframe shapes
    Former-commit-id: d0bdc5d6f07260fecff0faab4f6dfb2eca767fbd [formerly f2fe88790db1fc10cca5a1049511d025cf6b855e] [formerly d0bdc5d6f07260fecff0faab4f6dfb2eca767fbd [formerly f2fe88790db1fc10cca5a1049511d025cf6b855e] [formerly 99209a054ff4169dee024e27c02012815a6d638b [formerly ab047d007f8835554d2ec08e2e11594f7d88a6cb]]]
    Former-commit-id: 99209a054ff4169dee024e27c02012815a6d638b
    Former-commit-id: 43a4688404bd5d0b279319d4f8a6933843ff6dfe [formerly 41d3652701df01dd091e501982dd54515335ef40]
    Former-commit-id: aa07614542de26faa70477ff67eeacf22a7874cc

---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/events/fonts/UpdateFontDataEvent.java
@@ -0,0 +1,99 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics.events.fonts;
+
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+import com.raytheon.uf.viz.remote.graphics.events.AbstractDispatchingObjectEvent;
+
+/**
+ * Event for updating a font object's magnification, smoothing, or scaling
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 12, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+@DynamicSerialize
+public class UpdateFontDataEvent extends AbstractDispatchingObjectEvent {
+
+    @DynamicSerializeElement
+    private Float magnification;
+
+    @DynamicSerializeElement
+    private Boolean smoothing;
+
+    @DynamicSerializeElement
+    private Boolean scaleFont;
+
+    /**
+     * @return the magnification
+     */
+    public Float getMagnification() {
+        return magnification;
+    }
+
+    /**
+     * @param magnification
+     *            the magnification to set
+     */
+    public void setMagnification(Float magnification) {
+        this.magnification = magnification;
+    }
+
+    /**
+     * @return the smoothing
+     */
+    public Boolean getSmoothing() {
+        return smoothing;
+    }
+
+    /**
+     * @param smoothing
+     *            the smoothing to set
+     */
+    public void setSmoothing(Boolean smoothing) {
+        this.smoothing = smoothing;
+    }
+
+    /**
+     * @return the scaleFont
+     */
+    public Boolean getScaleFont() {
+        return scaleFont;
+    }
+
+    /**
+     * @param scaleFont
+     *            the scaleFont to set
+     */
+    public void setScaleFont(Boolean scaleFont) {
+        this.scaleFont = scaleFont;
+    }
+
+}
