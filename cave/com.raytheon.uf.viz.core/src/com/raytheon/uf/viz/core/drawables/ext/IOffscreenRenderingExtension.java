36938dc2228adfd6a8ca099db34515b9cc0465e4
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-12 10:45:50 -0500

    Issue #239 got offscreen rendering working for clients
    Former-commit-id: da48c4f6ac97faabfa276b4fa20601005b566102 [formerly b1687af0acff815781012fdd73dd87c580202421] [formerly 060330edeae7466dfddea42f793be62e75c7a224 [formerly 5ae0ed10d5793160bd859da3f724aa0144da1a62]] [formerly 2de39bb48753bd07905844d4f0c3f13cde7f9c8a [formerly 5ae0ed10d5793160bd859da3f724aa0144da1a62 [formerly f41733465eb0ee048fb76a0d5467ce456d1cf7c9]]]
    Former-commit-id: 2de39bb48753bd07905844d4f0c3f13cde7f9c8a
    Former-commit-id: 0c28e5b5dcdc3b47b56f77d8ab45a6f1e98aba12 [formerly 1a340bb6e5a1e546b2fb004eb2c7ce4c64119f07]
    Former-commit-id: 0f62e40ddafa6eb57b5eac56217511a804d0a76f

--- cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/ext/IOffscreenRenderingExtension.java
+++ cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/ext/IOffscreenRenderingExtension.java
@@ -2,6 +2,7 @@ package com.raytheon.uf.viz.core.drawables.ext;
 
 import java.nio.Buffer;
 
+import com.raytheon.uf.viz.core.IExtent;
 import com.raytheon.uf.viz.core.drawables.ColorMapParameters;
 import com.raytheon.uf.viz.core.drawables.IImage;
 import com.raytheon.uf.viz.core.drawables.ext.GraphicsExtension.IGraphicsExtensionInterface;
@@ -12,12 +13,27 @@ public interface IOffscreenRenderingExtension extends
     /**
      * All drawing between a call to renderOffscreen and the next call to
      * renderOnscreen will be drawn to offscreenImage rather than to the screen.
+     * Will use the current screen's world as the coverage area for the
+     * offscreen image
      * 
      * @param offscreenImage
      *            image to render to
      * @throws VizException
      */
     public void renderOffscreen(IImage offscreenImage) throws VizException;
+
+    /**
+     * All drawing between a call to renderOffscreen and the next call to
+     * renderOnscreen will be drawn to offscreenImage rather than to the screen.
+     * Will use the extent passed in as the world coverage area for the
+     * offscreen image
+     * 
+     * @param offscreenImage
+     * @param offscreenExtent
+     * @throws VizException
+     */
+    public void renderOffscreen(IImage offscreenImage, IExtent offscreenExtent)
+            throws VizException;
 
     /**
      * Reset rendering to the screen. This only needs to be called if
