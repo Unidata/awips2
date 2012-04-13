4a897a76d3fe5b848a4fb43635fd74ce88286958
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-13 12:56:43 -0500

    Issue #239 Got plots working
    Former-commit-id: 559bae6041670363c3798f9f7570acd5455c904c [formerly 682576fe843970da97d8f2d7805e9d53e38ba2ee] [formerly 859262ea988e29b012673c1d8d15fdb6713a6224] [formerly 559bae6041670363c3798f9f7570acd5455c904c [formerly 682576fe843970da97d8f2d7805e9d53e38ba2ee] [formerly 859262ea988e29b012673c1d8d15fdb6713a6224] [formerly 04ab27895d617b77427fc6ff62b7ae99fe8b80a4 [formerly 859262ea988e29b012673c1d8d15fdb6713a6224 [formerly 9c93f3670bd8c3a7a5e5330b160d5f2206b979b3]]]]
    Former-commit-id: 04ab27895d617b77427fc6ff62b7ae99fe8b80a4
    Former-commit-id: c962a314046bf3abdbdc89ecceb2e2c288d940f1 [formerly aedf159c8add68f2b699877d37f1bbe75f933f2c] [formerly 195e125fc2adc09e79e5bdc22b02fc35aa7211e0 [formerly adba2b8aebd71b8c9ba16efec602caf53021cf62]]
    Former-commit-id: 450a95118a8bafa7d4577fd74a442272c642942e [formerly 9b5481968f0b0e6987df0bde4ce487b6d2f04091]
    Former-commit-id: 96a33e2a7a4cb1259f9d7c786b6354665afc4c76

---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/events/imagery/RenderedImageWrapper.java
@@ -0,0 +1,136 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics.events.imagery;
+
+import java.awt.image.RenderedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.media.jai.remote.SerializableRenderedImage;
+
+import com.raytheon.uf.common.serialization.IDeserializationContext;
+import com.raytheon.uf.common.serialization.ISerializationContext;
+import com.raytheon.uf.common.serialization.ISerializationTypeAdapter;
+import com.raytheon.uf.common.serialization.SerializationException;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeTypeAdapter;
+import com.raytheon.uf.viz.remote.graphics.events.imagery.RenderedImageWrapper.RenderedImageSerializer;
+
+/**
+ * Wraps a RenderedImage for dynamic serialization
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 13, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+@DynamicSerialize
+@DynamicSerializeTypeAdapter(factory = RenderedImageSerializer.class)
+public class RenderedImageWrapper {
+
+    public static class RenderedImageSerializer implements
+            ISerializationTypeAdapter<RenderedImageWrapper> {
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * com.raytheon.uf.common.serialization.ISerializationTypeAdapter#serialize
+         * (com.raytheon.uf.common.serialization.ISerializationContext,
+         * java.lang.Object)
+         */
+        @Override
+        public void serialize(ISerializationContext serializer,
+                RenderedImageWrapper object) throws SerializationException {
+            RenderedImage image = object.getWrappedImage();
+            if (image instanceof SerializableRenderedImage == false) {
+                image = new SerializableRenderedImage(image);
+            }
+            // serialize rendered image into bytes
+            try {
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(bytes);
+                oos.writeObject(image);
+                oos.close();
+                serializer.writeBinary(bytes.toByteArray());
+            } catch (IOException e) {
+                throw new SerializationException(
+                        "Error serializing rendered image", e);
+            }
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.raytheon.uf.common.serialization.ISerializationTypeAdapter#
+         * deserialize
+         * (com.raytheon.uf.common.serialization.IDeserializationContext)
+         */
+        @Override
+        public RenderedImageWrapper deserialize(
+                IDeserializationContext deserializer)
+                throws SerializationException {
+            RenderedImageWrapper wrapper = new RenderedImageWrapper();
+            byte[] data = deserializer.readBinary();
+            // deserialize bytes into rendered image
+            try {
+                ObjectInputStream oin = new ObjectInputStream(
+                        new ByteArrayInputStream(data));
+                wrapper.setWrappedImage((RenderedImage) oin.readObject());
+                oin.close();
+            } catch (Exception e) {
+                throw new SerializationException(
+                        "Error deserializing rendered image: "
+                                + e.getLocalizedMessage(), e);
+            }
+            return wrapper;
+        }
+    }
+
+    @DynamicSerializeElement
+    private RenderedImage wrappedImage;
+
+    /**
+     * @return the wrappedImage
+     */
+    public RenderedImage getWrappedImage() {
+        return wrappedImage;
+    }
+
+    /**
+     * @param wrappedImage
+     *            the wrappedImage to set
+     */
+    public void setWrappedImage(RenderedImage wrappedImage) {
+        this.wrappedImage = wrappedImage;
+    }
+
+}
---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/extensions/DispatchingSingleColorImageExtension.java
@@ -0,0 +1,72 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics.extensions;
+
+import java.awt.image.RenderedImage;
+
+import org.eclipse.swt.graphics.RGB;
+
+import com.raytheon.uf.viz.core.drawables.ext.ISingleColorImageExtension;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.remote.graphics.objects.DispatchingSingleColorImage;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 12, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class DispatchingSingleColorImageExtension extends
+        AbstractDispatchingImageExtension implements ISingleColorImageExtension {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.core.drawables.ext.ISingleColorImageExtension#
+     * constructImage(java.awt.image.RenderedImage,
+     * org.eclipse.swt.graphics.RGB)
+     */
+    @Override
+    public ISingleColorImage constructImage(RenderedImage image, RGB color) {
+        try {
+            return new DispatchingSingleColorImage(target.getWrappedObject()
+                    .getExtension(ISingleColorImageExtension.class)
+                    .constructImage(image, color),
+                    DispatchColormappedImageExtension.class,
+                    target.getDispatcher(), color,
+                    new RenderedImage[] { image });
+        } catch (VizException e) {
+            throw new RuntimeException(
+                    "Error constring dispatching single color image", e);
+        }
+    }
+
+}
