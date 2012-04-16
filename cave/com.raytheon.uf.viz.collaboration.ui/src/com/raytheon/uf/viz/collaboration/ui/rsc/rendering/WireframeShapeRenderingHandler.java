0e288625529ceef4b1aa363e5958820e53d4ac50
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-16 17:29:59 -0500

    Issue #239 Refactored Collaboration rendering so it looks up renderes by extension point so radar renderer can be injected by a radar collaboration project
    Former-commit-id: a9864fa590b4a9ed249fc87e88a1c500689dcf88 [formerly 490c75f6846d5724bb28d374ce7126e305bc58d0] [formerly 9b018ea151f0fb1520e970caa82c8fa1f13f112a] [formerly a9864fa590b4a9ed249fc87e88a1c500689dcf88 [formerly 490c75f6846d5724bb28d374ce7126e305bc58d0] [formerly 9b018ea151f0fb1520e970caa82c8fa1f13f112a] [formerly 7d2a7d987d690d6a31b56a531a126495efb9d91e [formerly 9b018ea151f0fb1520e970caa82c8fa1f13f112a [formerly 79f4d46bf18fc03c79df0c80c8dde025fe954f0c]]]]
    Former-commit-id: 7d2a7d987d690d6a31b56a531a126495efb9d91e
    Former-commit-id: 652dc33e7f7e4398f0877f836e18119378a4de9d [formerly fcb459e91db2871d95fd1f62850069ba935c4f89] [formerly ea904c0632a530246f3994e307cb058b799313b4 [formerly fa3d39c5e2ba3c494205ef6fc43922ae5ef80026]]
    Former-commit-id: fc9e8981c63fc38e320856e656d775ffd1034960 [formerly ab9434622421003386d21d6373cb57eb84f86031]
    Former-commit-id: 6f1c372790e5e65751f7d261f9ffed3ebd978b50

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/rsc/rendering/WireframeShapeRenderingHandler.java
@@ -0,0 +1,150 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.rsc.rendering;
+
+import com.google.common.eventbus.Subscribe;
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.drawables.IFont;
+import com.raytheon.uf.viz.core.drawables.IWireframeShape;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.AllocatePointsEvent;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.CreateWireframeShapeEvent;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.RenderWireframeShapeEvent;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.SimpleWireframeShapeEvent;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.UpdateWireframeShapeEvent;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.WireframeShapeData;
+import com.raytheon.uf.viz.remote.graphics.events.wireframe.WireframeShapeData.Label;
+
+/**
+ * Handles render events for wireframe shapes
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class WireframeShapeRenderingHandler extends
+        CollaborationRenderingHandler {
+
+    @Subscribe
+    public void createWireframeShape(CreateWireframeShapeEvent event) {
+        IGraphicsTarget target = getTarget();
+        int shapeId = event.getObjectId();
+        IWireframeShape shape = null;
+        if (event.getSimplificationLevel() != null) {
+            if (event.isSpatialChopFlag() != null) {
+                shape = target.createWireframeShape(event.isMutable(),
+                        event.getGridGeometry(),
+                        event.getSimplificationLevel(),
+                        event.isSpatialChopFlag(), event.getIExtent());
+            } else {
+                shape = target
+                        .createWireframeShape(event.isMutable(),
+                                event.getGridGeometry(),
+                                event.getSimplificationLevel());
+            }
+        } else {
+            shape = target.createWireframeShape(event.isMutable(),
+                    event.getGridGeometry());
+        }
+        dataManager.putRenderableObject(shapeId, shape);
+    }
+
+    @Subscribe
+    public void allocatePointsForShape(AllocatePointsEvent event) {
+        IWireframeShape shape = dataManager.getRenderableObject(
+                event.getObjectId(), IWireframeShape.class);
+        if (shape != null) {
+            shape.allocate(event.getNumberOfPoints());
+        }
+    }
+
+    @Subscribe
+    public void updateWireframeShapeData(UpdateWireframeShapeEvent event) {
+        IWireframeShape shape = dataManager.getRenderableObject(
+                event.getObjectId(), IWireframeShape.class);
+        if (shape != null) {
+            WireframeShapeData data = event.getWireframeData();
+            for (Label label : data.getLabels()) {
+                shape.addLabel(label.getText(), label.getPoint());
+            }
+            for (double[][] coords : data.getCoordinates()) {
+                shape.addLineSegment(coords);
+            }
+        }
+    }
+
+    @Subscribe
+    public void handleSimpleWireframeShapeEvent(SimpleWireframeShapeEvent event) {
+        IWireframeShape shape = dataManager.getRenderableObject(
+                event.getObjectId(), IWireframeShape.class);
+        if (shape != null) {
+            switch (event.getAction()) {
+            case CLEAR_LABELS:
+                shape.clearLabels();
+                break;
+            case COMPILE:
+                shape.compile();
+                break;
+            case RESET:
+                shape.reset();
+                break;
+            }
+        }
+    }
+
+    @Subscribe
+    public void renderWireframeShape(RenderWireframeShapeEvent event)
+            throws VizException {
+        IGraphicsTarget target = getTarget();
+        IWireframeShape shape = dataManager.getRenderableObject(
+                event.getObjectId(), IWireframeShape.class);
+        if (shape != null) {
+            IFont font = null;
+            if (event.getFontId() != null) {
+                font = dataManager.getRenderableObject(event.getFontId(),
+                        IFont.class);
+            }
+            if (event.getAlpha() == null) {
+                target.drawWireframeShape(shape, event.getColor(),
+                        event.getLineWidth(), event.getLineStyle(), font);
+            } else {
+                target.drawWireframeShape(shape, event.getColor(),
+                        event.getLineWidth(), event.getLineStyle(), font,
+                        event.getAlpha());
+            }
+        }
+    }
+
+    @Subscribe
+    public void disposeWireframeShape(IWireframeShape shape) {
+        shape.dispose();
+    }
+
+}
