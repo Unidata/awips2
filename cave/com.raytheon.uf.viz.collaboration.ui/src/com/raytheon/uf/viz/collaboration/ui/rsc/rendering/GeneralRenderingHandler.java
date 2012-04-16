fa3d39c5e2ba3c494205ef6fc43922ae5ef80026
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-16 17:29:59 -0500

    Issue #239 Refactored Collaboration rendering so it looks up renderes by extension point so radar renderer can be injected by a radar collaboration project
    Former-commit-id: 9b018ea151f0fb1520e970caa82c8fa1f13f112a [formerly 79f4d46bf18fc03c79df0c80c8dde025fe954f0c]
    Former-commit-id: 7d2a7d987d690d6a31b56a531a126495efb9d91e

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/rsc/rendering/GeneralRenderingHandler.java
@@ -0,0 +1,84 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.rsc.rendering;
+
+import com.google.common.eventbus.Subscribe;
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.drawables.PaintProperties;
+import com.raytheon.uf.viz.remote.graphics.events.BeginFrameEvent;
+import com.raytheon.uf.viz.remote.graphics.events.DisposeObjectEvent;
+
+/**
+ * Handles general rendering events, begin frame and dispose
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class GeneralRenderingHandler extends CollaborationRenderingHandler {
+
+    /**
+     * General dispose of a renderable object event
+     * 
+     * @param event
+     */
+    @Subscribe
+    public void disposeRenderable(DisposeObjectEvent event) {
+        Object[] toDispose = dataManager.getRenderableObject(
+                event.getObjectId(), Object[].class);
+        if (toDispose != null) {
+            dataManager.dispose(toDispose);
+        }
+    }
+
+    /**
+     * Begin frame event, modifies the target extent
+     * 
+     * @param event
+     */
+    @Subscribe
+    public void handleBeginFrame(BeginFrameEvent event) {
+        PaintProperties paintProps = getPaintProperties();
+        IGraphicsTarget target = getTarget();
+        double[] center = event.getExtentCenter();
+        IExtent copy = paintProps.getView().getExtent().clone();
+        if (center != null) {
+            double[] currCenter = copy.getCenter();
+            copy.shift(center[0] - currCenter[0], center[1] - currCenter[1]);
+            copy.scaleAndBias(event.getExtentFactor() / copy.getScale(),
+                    center[0], center[1]);
+            target.updateExtent(copy);
+            event.setExtentCenter(null);
+            target.setNeedsRefresh(true);
+        }
+    }
+
+}
