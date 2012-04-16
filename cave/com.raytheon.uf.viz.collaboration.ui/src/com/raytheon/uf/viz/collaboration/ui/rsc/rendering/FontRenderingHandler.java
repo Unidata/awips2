cefab90f5a728dea3aecb6943c3f84a921045d2b
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-16 17:29:59 -0500

    Issue #239 Refactored Collaboration rendering so it looks up renderes by extension point so radar renderer can be injected by a radar collaboration project
    Former-commit-id: fa3d39c5e2ba3c494205ef6fc43922ae5ef80026 [formerly 7d2a7d987d690d6a31b56a531a126495efb9d91e] [formerly 9b018ea151f0fb1520e970caa82c8fa1f13f112a [formerly 79f4d46bf18fc03c79df0c80c8dde025fe954f0c]]
    Former-commit-id: 9b018ea151f0fb1520e970caa82c8fa1f13f112a
    Former-commit-id: 490c75f6846d5724bb28d374ce7126e305bc58d0

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/rsc/rendering/FontRenderingHandler.java
@@ -0,0 +1,106 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.rsc.rendering;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.google.common.eventbus.Subscribe;
+import com.raytheon.uf.common.util.FileUtil;
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.drawables.IFont;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.remote.graphics.events.fonts.CreateFontEvent;
+import com.raytheon.uf.viz.remote.graphics.events.fonts.UpdateFontDataEvent;
+
+/**
+ * Handles render events for Font objects
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class FontRenderingHandler extends CollaborationRenderingHandler {
+
+    @Subscribe
+    public void createFont(CreateFontEvent event) throws VizException {
+        IGraphicsTarget target = getTarget();
+        int fontId = event.getObjectId();
+        IFont font = null;
+        if (event.getFontData() != null) {
+            try {
+                File fontFile = File.createTempFile(event.getFontName(), null);
+                FileUtil.bytes2File(event.getFontData(), fontFile);
+                font = target.initializeFont(fontFile, event.getFontSize(),
+                        event.getFontStyle());
+            } catch (IOException e) {
+                throw new VizException("Unable to write font file: "
+                        + e.getLocalizedMessage());
+            }
+        } else {
+            font = target.initializeFont(event.getFontName(),
+                    event.getFontSize(), event.getFontStyle());
+        }
+        font.setMagnification(event.getMagnification());
+        font.setSmoothing(event.isSmoothing());
+        font.setScaleFont(event.isScaleFont());
+        dataManager.putRenderableObject(fontId, font);
+    }
+
+    @Subscribe
+    public void updateFont(UpdateFontDataEvent event) {
+        IFont font = dataManager.getRenderableObject(event.getObjectId(),
+                IFont.class);
+        if (font != null) {
+            if (event.getMagnification() != null) {
+                if (event.getScaleFont() != null) {
+                    font.setMagnification(event.getMagnification(),
+                            event.getScaleFont());
+                } else {
+                    font.setMagnification(event.getMagnification());
+                }
+            } else {
+                if (event.getScaleFont() != null) {
+                    font.setScaleFont(event.getScaleFont());
+                } else if (event.getSmoothing() != null) {
+                    font.setSmoothing(event.getSmoothing());
+                }
+            }
+            font.setMagnification(event.getMagnification());
+            font.setSmoothing(event.getSmoothing());
+            font.setScaleFont(event.getScaleFont());
+        }
+    }
+
+    @Subscribe
+    public void disposeFont(IFont font) {
+        font.dispose();
+    }
+}
