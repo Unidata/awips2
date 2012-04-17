79ecfbf77d65d5c34cbeb66de4bc28852066c387
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-17 17:47:21 -0500

    Issue #239 Got radar and radar mosaic working for collaboration
    Former-commit-id: c2f58145ec85e20390c4dd294271b90bee37d381 [formerly 71e54d0aa5925d8b4fc96367ac4130773dd11c70] [formerly fae382a91ad1e66d94747eba8d784d641d042305] [formerly fae382a91ad1e66d94747eba8d784d641d042305 [formerly d37d8f9bea2334288d2001f6924e0c5427845c3d]] [formerly c2f58145ec85e20390c4dd294271b90bee37d381 [formerly 71e54d0aa5925d8b4fc96367ac4130773dd11c70] [formerly fae382a91ad1e66d94747eba8d784d641d042305] [formerly fae382a91ad1e66d94747eba8d784d641d042305 [formerly d37d8f9bea2334288d2001f6924e0c5427845c3d]] [formerly 96c81a03df927fc2d965535505aabce4b4d4c137 [formerly fae382a91ad1e66d94747eba8d784d641d042305 [formerly d37d8f9bea2334288d2001f6924e0c5427845c3d] [formerly 96c81a03df927fc2d965535505aabce4b4d4c137 [formerly a8c0dd15e221a6415c44f73897a80bc4b69e5ca0]]]]]
    Former-commit-id: 96c81a03df927fc2d965535505aabce4b4d4c137
    Former-commit-id: f9b69ea4096978f6e560b538e3859404c2a48952 [formerly 142730720e384c14b397da1ef8730465d979e002] [formerly c6761afce5833193839ee7a26381ab830ac69d23] [formerly cee68cda469258a90574b76e6b24acb53d010318 [formerly e5f7d10250129ff8077a4d8393b247168d687b5d] [formerly c6761afce5833193839ee7a26381ab830ac69d23 [formerly dbe18c16d20d9b0ab251c0291fce8d4afdb8269f]]]
    Former-commit-id: 0b8b3e4f0a75db6609080946d9d3543d1132d19b [formerly 272097beab1ae876448aaa583ddec2dc9cd4ed08] [formerly 554e519e8c92445b55da77d8a42822d90a852e92 [formerly 4dd82a9211b789c2615155e0233ea4d7be7425eb]]
    Former-commit-id: 554e519e8c92445b55da77d8a42822d90a852e92
    Former-commit-id: 794a8c4effb9c37584bba041a023694e59d0a042

---
+++ cave/com.raytheon.uf.viz.collaboration.radar/src/com/raytheon/uf/viz/collaboration/radar/mosaic/DispatchingMosaicImage.java
@@ -0,0 +1,97 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.radar.mosaic;
+
+import com.raytheon.uf.viz.core.DrawableImage;
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.drawables.ColorMapParameters;
+import com.raytheon.uf.viz.core.drawables.ext.IImagingExtension;
+import com.raytheon.uf.viz.remote.graphics.Dispatcher;
+import com.raytheon.uf.viz.remote.graphics.events.RemoteGraphicsEventFactory;
+import com.raytheon.uf.viz.remote.graphics.events.imagery.PaintImagesEvent;
+import com.raytheon.uf.viz.remote.graphics.objects.DispatchingColormappedImage;
+import com.raytheon.viz.radar.rsc.mosaic.ext.IRadarMosaicImageExtension.IMosaicImage;
+
+/**
+ * Dispatching mosaic image object created from graphics image and forwards key
+ * events to remote clients
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+
+public class DispatchingMosaicImage extends
+        DispatchingColormappedImage<IMosaicImage> implements IMosaicImage {
+
+    /**
+     * @param targetObject
+     * @param extensionClass
+     * @param dispatcher
+     */
+    public DispatchingMosaicImage(IMosaicImage targetObject,
+            Class<? extends IImagingExtension> extensionClass,
+            Dispatcher dispatcher, ColorMapParameters parameters) {
+        super(targetObject, extensionClass, dispatcher, parameters);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.radar.rsc.mosaic.ext.IRadarMosaicImageExtension.IMosaicImage
+     * #setImagesToMosaic(com.raytheon.uf.viz.core.DrawableImage[])
+     */
+    @Override
+    public void setImagesToMosaic(DrawableImage... images) {
+        UpdateImagesToMosaic event = RemoteGraphicsEventFactory.createEvent(
+                UpdateImagesToMosaic.class, this);
+        event.setImagesToMosaic(PaintImagesEvent.toPaintEvents(images));
+        wrappedObject.setImagesToMosaic(PaintImagesEvent
+                .extractTargetImages(images));
+        dispatch(event);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.radar.rsc.mosaic.ext.IRadarMosaicImageExtension.IMosaicImage
+     * #setImageExtent(com.raytheon.uf.viz.core.IExtent)
+     */
+    @Override
+    public void setImageExtent(IExtent imageExtent) {
+        wrappedObject.setImageExtent(imageExtent);
+        UpdateMosaicExtent extentUpdate = RemoteGraphicsEventFactory
+                .createEvent(UpdateMosaicExtent.class, this);
+        extentUpdate.setIExtent(imageExtent);
+        dispatch(extentUpdate);
+    }
+
+}
