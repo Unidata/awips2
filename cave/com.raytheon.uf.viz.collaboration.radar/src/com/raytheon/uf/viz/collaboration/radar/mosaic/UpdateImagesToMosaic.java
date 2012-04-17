c2f58145ec85e20390c4dd294271b90bee37d381
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-04-17 17:47:21 -0500

    Issue #239 Got radar and radar mosaic working for collaboration
    Former-commit-id: 96c81a03df927fc2d965535505aabce4b4d4c137 [formerly fae382a91ad1e66d94747eba8d784d641d042305 [formerly d37d8f9bea2334288d2001f6924e0c5427845c3d] [formerly 96c81a03df927fc2d965535505aabce4b4d4c137 [formerly a8c0dd15e221a6415c44f73897a80bc4b69e5ca0]]]
    Former-commit-id: fae382a91ad1e66d94747eba8d784d641d042305 [formerly d37d8f9bea2334288d2001f6924e0c5427845c3d]
    Former-commit-id: fae382a91ad1e66d94747eba8d784d641d042305
    Former-commit-id: 71e54d0aa5925d8b4fc96367ac4130773dd11c70

---
+++ cave/com.raytheon.uf.viz.collaboration.radar/src/com/raytheon/uf/viz/collaboration/radar/mosaic/UpdateImagesToMosaic.java
@@ -0,0 +1,64 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.radar.mosaic;
+
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+import com.raytheon.uf.viz.remote.graphics.events.AbstractDispatchingObjectEvent;
+import com.raytheon.uf.viz.remote.graphics.events.imagery.PaintImageEvent;
+
+/**
+ * Event for updating what images should be in the mosaic
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+@DynamicSerialize
+public class UpdateImagesToMosaic extends AbstractDispatchingObjectEvent {
+
+    @DynamicSerializeElement
+    private PaintImageEvent[] imagesToMosaic;
+
+    /**
+     * @return the imagesToMosaic
+     */
+    public PaintImageEvent[] getImagesToMosaic() {
+        return imagesToMosaic;
+    }
+
+    /**
+     * @param imagesToMosaic
+     *            the imagesToMosaic to set
+     */
+    public void setImagesToMosaic(PaintImageEvent[] imagesToMosaic) {
+        this.imagesToMosaic = imagesToMosaic;
+    }
+
+}
