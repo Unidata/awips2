03174d8f66b3086f56defde73c518e52f34195b3
Matt Nash <Matt_Nash@raytheon.com>
2012-04-25 14:22:05 -0500

    Issue #429 color final fixes
    Former-commit-id: 506d3e8989aa75107ed038b130cbc0514fb1d306 [formerly 51b937b9be5c9f1a1e17317e241cd93337e9fd0c]
    Former-commit-id: 9bc3f0c75d39ef041275c4249343d83a3c5e8ce8

---
+++ cave/com.raytheon.uf.viz.collaboration/src/com/raytheon/uf/viz/collaboration/comm/provider/user/UserIdAlias.java
@@ -0,0 +1,83 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.comm.provider.user;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 25, 2012            mnash     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mnash
+ * @version 1.0
+ */
+
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.NONE)
+public class UserIdAlias {
+    @XmlAttribute
+    private UserId id;
+
+    @XmlElement
+    private String alias;
+
+    /**
+     * @return the id
+     */
+    public UserId getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(UserId id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the alias
+     */
+    public String getAlias() {
+        return alias;
+    }
+
+    /**
+     * @param alias
+     *            the alias to set
+     */
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+}
