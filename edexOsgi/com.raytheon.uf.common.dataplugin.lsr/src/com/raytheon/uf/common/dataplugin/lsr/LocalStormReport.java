f9e4a8c4bd8f5ceba58b3ef3c42017107ae3875a
Richard Peter <richard.peter@raytheon.com>
2012-04-27 12:00:12 -0500

    Issue #538: Fix Serialization Annotations and Serialization Issues
    Change-Id: I436997106438ab283e987cb7e3055fcee009b608
    
    Former-commit-id: 6b4ed78782b4d179eb8580f20f6a70314a3358fb [formerly 231d98b79e7fe8d8153401238e061474c9800d63] [formerly c33a392a8c7aea1811f4366a110ea88c88933d15] [formerly c33a392a8c7aea1811f4366a110ea88c88933d15 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632]] [formerly 6b4ed78782b4d179eb8580f20f6a70314a3358fb [formerly 231d98b79e7fe8d8153401238e061474c9800d63] [formerly c33a392a8c7aea1811f4366a110ea88c88933d15] [formerly c33a392a8c7aea1811f4366a110ea88c88933d15 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632]] [formerly b11df888d371b9877bd75a74fbd6871588670c19 [formerly c33a392a8c7aea1811f4366a110ea88c88933d15 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632] [formerly b11df888d371b9877bd75a74fbd6871588670c19 [formerly 3aadbc778d9bb22dc9c0b4b93bbbaa70c3fa07d0]]]]]
    Former-commit-id: b11df888d371b9877bd75a74fbd6871588670c19
    Former-commit-id: 13a6f4a6d8e17cf7373c6318968ae1e36a4f75e6 [formerly 51e9a723719bb20a5f5fef8278e56ffdf995a9de] [formerly ae35c2f27c11954175af3a4a25827f476e232ca7] [formerly b9522c372ef83c9df1d199fe66213b21ff00399f [formerly 8e9c6244c73799f26e260982a3f71ee5ca3d5c9a] [formerly ae35c2f27c11954175af3a4a25827f476e232ca7 [formerly e3323febc5dd19b098fb15c40f8873e0e1fbc54e]]]
    Former-commit-id: cf0f9b95b6d540dca5361722823c4dbf999d3e42 [formerly 0fa5241fd917758f0896a74d045990ac166bd25b] [formerly 83564a4dde4ed57b9d1bf07c2a5eb14551169c43 [formerly 0212d3108feb66a863f881f375e98d08fd82d15d]]
    Former-commit-id: 83564a4dde4ed57b9d1bf07c2a5eb14551169c43
    Former-commit-id: b7c5a834b5fdf9b4013ccc2fa60c176f983a17d1

--- edexOsgi/com.raytheon.uf.common.dataplugin.lsr/src/com/raytheon/uf/common/dataplugin/lsr/LocalStormReport.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.lsr/src/com/raytheon/uf/common/dataplugin/lsr/LocalStormReport.java
@@ -75,458 +75,459 @@ import com.vividsolutions.jts.geom.Geometry;
 @XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
 public class LocalStormReport extends PersistablePluginDataObject implements
-        ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
-
-    private static final long serialVersionUID = 1L;
-
-    private static final int MISSING = -9999;
-
-    public static final Unit<Temperature> TEMPERATURE_UNIT = SI.KELVIN;
-
-    public static final Unit<Velocity> WIND_SPEED_UNIT = SI.METERS_PER_SECOND;
-
-    public static final Unit<Angle> WIND_DIR_UNIT = NonSI.DEGREE_ANGLE;
-
-    public static final Unit<Pressure> PRESSURE_UNIT = SI.PASCAL;
-
-    public static final Unit<Angle> LOCATION_UNIT = NonSI.DEGREE_ANGLE;
-
-    public static final Unit<Length> WAVE_UNIT = SI.METER;
-
-    public static final Unit<Length> VISIBILITY_UNIT = NonSI.MILE;
-
-    public static final Unit<DataAmount> CLOUD_COVER = NonSI.OCTET;
-
-    //
-    @DataURI(position = 1)
-    @Column
-    @XmlElement
-    @DynamicSerializeElement
-    private LSREventType eventType;
-
-    // Correction indicator from wmo header
-    @DataURI(position = 2)
-    @Column
-    @XmlElement
-    @DynamicSerializeElement
-    private String corIndicator;
-
-    @Embedded
-    @DataURI(position = 3, embedded = true)
-    @XmlElement
-    @DynamicSerializeElement
-    private SurfaceObsLocation location;
-
-    @Embedded
-    private PointDataView pdv;
-
-    // Text of the WMO header
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String wmoHeader = "";
-
-    // Text of the office
-    @XmlElement
-    @DynamicSerializeElement
-    private String officeid = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String cityLoc = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String source = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String countyLoc = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String stateLoc = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private String remarks = "";
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private Float magnitude = -9999.0f;
-
-    // 0 = unknown
-    // 1 = estimated
-    // 2 = measured
-    // 3 =
-    // 4 =
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private Integer magQual = MISSING;
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private Integer injuries = MISSING;
-
-    //
-    @Transient
-    @XmlElement
-    @DynamicSerializeElement
-    private Integer fatalities = MISSING;
-
-    /**
-     * Empty default constructor
-     */
-    public LocalStormReport() {
-    }
-
-    /**
-     * Construct an instance of this class using the supplied datauri.
-     * 
-     * @param dataUri
-     */
-    public LocalStormReport(String dataUri) {
-        super(dataUri);
-    }
-
-    /**
-     * @return the eventType
-     */
-    public LSREventType getEventType() {
-        return eventType;
-    }
-
-    /**
-     * @param eventType
-     *            the eventType to set
-     */
-    public void setEventType(LSREventType eventType) {
-        this.eventType = eventType;
-    }
-
-    /**
-     * @return the corIndicator
-     */
-    public String getCorIndicator() {
-        return corIndicator;
-    }
-
-    /**
-     * @param corIndicator
-     *            the corIndicator to set
-     */
-    public void setCorIndicator(String corIndicator) {
-        this.corIndicator = corIndicator;
-    }
-
-    /**
-     * @return the wmoHeader
-     */
-    public String getWmoHeader() {
-        return wmoHeader;
-    }
-
-    /**
-     * @param wmoHeader
-     *            the wmoHeader to set
-     */
-    public void setWmoHeader(String wmoHeader) {
-        this.wmoHeader = wmoHeader;
-    }
-
-    /**
-     * @return the officeid
-     */
-    public String getOfficeid() {
-        return officeid;
-    }
-
-    /**
-     * @param officeid
-     *            the officeid to set
-     */
-    public void setOfficeid(String officeid) {
-        this.officeid = officeid;
-    }
-
-    /**
-     * @return the cityLoc
-     */
-    public String getCityLoc() {
-        return cityLoc;
-    }
-
-    /**
-     * @param cityLoc
-     *            the cityLoc to set
-     */
-    public void setCityLoc(String cityLoc) {
-        this.cityLoc = cityLoc;
-    }
-
-    /**
-     * @return the source
-     */
-    public String getSource() {
-        return source;
-    }
-
-    /**
-     * @param source
-     *            the source to set
-     */
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    /**
-     * @return the countyLoc
-     */
-    public String getCountyLoc() {
-        return countyLoc;
-    }
-
-    /**
-     * @param countyLoc
-     *            the countyLoc to set
-     */
-    public void setCountyLoc(String countyLoc) {
-        this.countyLoc = countyLoc;
-    }
-
-    /**
-     * @return the stateLoc
-     */
-    public String getStateLoc() {
-        return stateLoc;
-    }
-
-    /**
-     * @param stateLoc
-     *            the stateLoc to set
-     */
-    public void setStateLoc(String stateLoc) {
-        this.stateLoc = stateLoc;
-    }
-
-    /**
-     * @return the remarks
-     */
-    public String getRemarks() {
-        return remarks;
-    }
-
-    /**
-     * @param remarks
-     *            the remarks to set
-     */
-    public void setRemarks(String remarks) {
-        this.remarks = remarks;
-    }
-
-    /**
-     * @return the magnitude
-     */
-    public Float getMagnitude() {
-        return magnitude;
-    }
-
-    /**
-     * @param magnitude
-     *            the magnitude to set
-     */
-    public void setMagnitude(Float magnitude) {
-        this.magnitude = magnitude;
-    }
-
-    /**
-     * @return the magQual
-     */
-    public Integer getMagQual() {
-        return magQual;
-    }
-
-    /**
-     * @param magQual
-     *            the magQual to set
-     */
-    public void setMagQual(Integer magQual) {
-        this.magQual = magQual;
-    }
-
-    /**
-     * @return the injuries
-     */
-    public Integer getInjuries() {
-        return injuries;
-    }
-
-    /**
-     * @param injuries
-     *            the injuries to set
-     */
-    public void setInjuries(Integer injuries) {
-        this.injuries = injuries;
-    }
-
-    /**
-     * @return the fatalities
-     */
-    public Integer getFatalities() {
-        return fatalities;
-    }
-
-    /**
-     * @param fatalities
-     *            the fatalities to set
-     */
-    public void setFatalities(Integer fatalities) {
-        this.fatalities = fatalities;
-    }
-
-    /**
-     * Set the data uri for this observation.
-     * 
-     * @param dataURI
-     */
-    @Override
-    public void setDataURI(String dataURI) {
-        super.setDataURI(dataURI);
-        identifier = dataURI;
-    }
-
-    @Override
-    public IDecoderGettable getDecoderGettable() {
-        return null;
-    }
-
-    @Override
-    public SurfaceObsLocation getSpatialObject() {
-        return location;
-    }
-
-    public SurfaceObsLocation getLocation() {
-        return location;
-    }
-
-    public void setLocation(SurfaceObsLocation location) {
-        this.location = location;
-    }
-
-    /**
-     * Get this observation's geometry.
-     * 
-     * @return The geometry for this observation.
-     */
-    public Geometry getGeometry() {
-        return location.getGeometry();
-    }
-
-    /**
-     * Get the geometry latitude.
-     * 
-     * @return The geometry latitude.
-     */
-    public double getLatitude() {
-        return location.getLatitude();
-    }
-
-    /**
-     * Get the geometry longitude.
-     * 
-     * @return The geometry longitude.
-     */
-    public double getLongitude() {
-        return location.getLongitude();
-    }
-
-    /**
-     * Get the station identifier for this observation.
-     * 
-     * @return the stationId
-     */
-    public String getStationId() {
-        return location.getStationId();
-    }
-
-    /**
-     * Get the elevation, in meters, of the observing platform or location.
-     * 
-     * @return The observation elevation, in meters.
-     */
-    public Integer getElevation() {
-        return location.getElevation();
-    }
-
-    /**
-     * Get whether the location for this observation is defined.
-     * 
-     * @return Is this location defined.
-     */
-    public Boolean getLocationDefined() {
-        return location.getLocationDefined();
-    }
-
-    @Override
-    public Amount getValue(String paramName) {
-        return null;
-    }
-
-    @Override
-    public String getString(String paramName) {
-        return null;
-    }
-
-    @Override
-    public String[] getStrings(String paramName) {
-        return null;
-    }
-
-    @Override
-    public Collection<Amount> getValues(String paramName) {
-        return null;
-    }
-
-    @Override
-    public PointDataView getPointDataView() {
-        return pdv;
-    }
-
-    @Override
-    public void setPointDataView(PointDataView pdv) {
-        this.pdv = pdv;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        Calendar c = getDataTime().getRefTimeAsCalendar();
-        if (c != null) {
-            sb.append(String.format("LSR:%1$tY%1$tm%1$td%1$tH%1$tM",
-                    getDataTime().getRefTimeAsCalendar()));
-        } else {
-            sb.append("LSR:YYYYMMDDHHmm");
-        }
-        sb.append(String.format("%6.2f %7.2f:", getLatitude(), getLongitude()));
-        sb.append(String.format("%s:", cityLoc));
-        sb.append(String.format("%s:", eventType.getEventName()));
-        sb.append(String.format("%5.2f:%s", getMagnitude(), getEventType()
-                .getEventUnits()));
-        return sb.toString();
-    }
+		ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final int MISSING = -9999;
+
+	public static final Unit<Temperature> TEMPERATURE_UNIT = SI.KELVIN;
+
+	public static final Unit<Velocity> WIND_SPEED_UNIT = SI.METERS_PER_SECOND;
+
+	public static final Unit<Angle> WIND_DIR_UNIT = NonSI.DEGREE_ANGLE;
+
+	public static final Unit<Pressure> PRESSURE_UNIT = SI.PASCAL;
+
+	public static final Unit<Angle> LOCATION_UNIT = NonSI.DEGREE_ANGLE;
+
+	public static final Unit<Length> WAVE_UNIT = SI.METER;
+
+	public static final Unit<Length> VISIBILITY_UNIT = NonSI.MILE;
+
+	public static final Unit<DataAmount> CLOUD_COVER = NonSI.OCTET;
+
+	//
+	@DataURI(position = 1)
+	@Column
+	@XmlElement
+	@DynamicSerializeElement
+	private LSREventType eventType;
+
+	// Correction indicator from wmo header
+	@DataURI(position = 2)
+	@Column
+	@XmlElement
+	@DynamicSerializeElement
+	private String corIndicator;
+
+	@Embedded
+	@DataURI(position = 3, embedded = true)
+	@XmlElement
+	@DynamicSerializeElement
+	private SurfaceObsLocation location;
+
+	@Embedded
+	@DynamicSerializeElement
+	private PointDataView pointDataView;
+
+	// Text of the WMO header
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String wmoHeader = "";
+
+	// Text of the office
+	@XmlElement
+	@DynamicSerializeElement
+	private String officeid = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String cityLoc = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String source = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String countyLoc = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String stateLoc = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private String remarks = "";
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private Float magnitude = -9999.0f;
+
+	// 0 = unknown
+	// 1 = estimated
+	// 2 = measured
+	// 3 =
+	// 4 =
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private Integer magQual = MISSING;
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private Integer injuries = MISSING;
+
+	//
+	@Transient
+	@XmlElement
+	@DynamicSerializeElement
+	private Integer fatalities = MISSING;
+
+	/**
+	 * Empty default constructor
+	 */
+	public LocalStormReport() {
+	}
+
+	/**
+	 * Construct an instance of this class using the supplied datauri.
+	 * 
+	 * @param dataUri
+	 */
+	public LocalStormReport(String dataUri) {
+		super(dataUri);
+	}
+
+	/**
+	 * @return the eventType
+	 */
+	public LSREventType getEventType() {
+		return eventType;
+	}
+
+	/**
+	 * @param eventType
+	 *            the eventType to set
+	 */
+	public void setEventType(LSREventType eventType) {
+		this.eventType = eventType;
+	}
+
+	/**
+	 * @return the corIndicator
+	 */
+	public String getCorIndicator() {
+		return corIndicator;
+	}
+
+	/**
+	 * @param corIndicator
+	 *            the corIndicator to set
+	 */
+	public void setCorIndicator(String corIndicator) {
+		this.corIndicator = corIndicator;
+	}
+
+	/**
+	 * @return the wmoHeader
+	 */
+	public String getWmoHeader() {
+		return wmoHeader;
+	}
+
+	/**
+	 * @param wmoHeader
+	 *            the wmoHeader to set
+	 */
+	public void setWmoHeader(String wmoHeader) {
+		this.wmoHeader = wmoHeader;
+	}
+
+	/**
+	 * @return the officeid
+	 */
+	public String getOfficeid() {
+		return officeid;
+	}
+
+	/**
+	 * @param officeid
+	 *            the officeid to set
+	 */
+	public void setOfficeid(String officeid) {
+		this.officeid = officeid;
+	}
+
+	/**
+	 * @return the cityLoc
+	 */
+	public String getCityLoc() {
+		return cityLoc;
+	}
+
+	/**
+	 * @param cityLoc
+	 *            the cityLoc to set
+	 */
+	public void setCityLoc(String cityLoc) {
+		this.cityLoc = cityLoc;
+	}
+
+	/**
+	 * @return the source
+	 */
+	public String getSource() {
+		return source;
+	}
+
+	/**
+	 * @param source
+	 *            the source to set
+	 */
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	/**
+	 * @return the countyLoc
+	 */
+	public String getCountyLoc() {
+		return countyLoc;
+	}
+
+	/**
+	 * @param countyLoc
+	 *            the countyLoc to set
+	 */
+	public void setCountyLoc(String countyLoc) {
+		this.countyLoc = countyLoc;
+	}
+
+	/**
+	 * @return the stateLoc
+	 */
+	public String getStateLoc() {
+		return stateLoc;
+	}
+
+	/**
+	 * @param stateLoc
+	 *            the stateLoc to set
+	 */
+	public void setStateLoc(String stateLoc) {
+		this.stateLoc = stateLoc;
+	}
+
+	/**
+	 * @return the remarks
+	 */
+	public String getRemarks() {
+		return remarks;
+	}
+
+	/**
+	 * @param remarks
+	 *            the remarks to set
+	 */
+	public void setRemarks(String remarks) {
+		this.remarks = remarks;
+	}
+
+	/**
+	 * @return the magnitude
+	 */
+	public Float getMagnitude() {
+		return magnitude;
+	}
+
+	/**
+	 * @param magnitude
+	 *            the magnitude to set
+	 */
+	public void setMagnitude(Float magnitude) {
+		this.magnitude = magnitude;
+	}
+
+	/**
+	 * @return the magQual
+	 */
+	public Integer getMagQual() {
+		return magQual;
+	}
+
+	/**
+	 * @param magQual
+	 *            the magQual to set
+	 */
+	public void setMagQual(Integer magQual) {
+		this.magQual = magQual;
+	}
+
+	/**
+	 * @return the injuries
+	 */
+	public Integer getInjuries() {
+		return injuries;
+	}
+
+	/**
+	 * @param injuries
+	 *            the injuries to set
+	 */
+	public void setInjuries(Integer injuries) {
+		this.injuries = injuries;
+	}
+
+	/**
+	 * @return the fatalities
+	 */
+	public Integer getFatalities() {
+		return fatalities;
+	}
+
+	/**
+	 * @param fatalities
+	 *            the fatalities to set
+	 */
+	public void setFatalities(Integer fatalities) {
+		this.fatalities = fatalities;
+	}
+
+	/**
+	 * Set the data uri for this observation.
+	 * 
+	 * @param dataURI
+	 */
+	@Override
+	public void setDataURI(String dataURI) {
+		super.setDataURI(dataURI);
+		identifier = dataURI;
+	}
+
+	@Override
+	public IDecoderGettable getDecoderGettable() {
+		return null;
+	}
+
+	@Override
+	public SurfaceObsLocation getSpatialObject() {
+		return location;
+	}
+
+	public SurfaceObsLocation getLocation() {
+		return location;
+	}
+
+	public void setLocation(SurfaceObsLocation location) {
+		this.location = location;
+	}
+
+	/**
+	 * Get this observation's geometry.
+	 * 
+	 * @return The geometry for this observation.
+	 */
+	public Geometry getGeometry() {
+		return location.getGeometry();
+	}
+
+	/**
+	 * Get the geometry latitude.
+	 * 
+	 * @return The geometry latitude.
+	 */
+	public double getLatitude() {
+		return location.getLatitude();
+	}
+
+	/**
+	 * Get the geometry longitude.
+	 * 
+	 * @return The geometry longitude.
+	 */
+	public double getLongitude() {
+		return location.getLongitude();
+	}
+
+	/**
+	 * Get the station identifier for this observation.
+	 * 
+	 * @return the stationId
+	 */
+	public String getStationId() {
+		return location.getStationId();
+	}
+
+	/**
+	 * Get the elevation, in meters, of the observing platform or location.
+	 * 
+	 * @return The observation elevation, in meters.
+	 */
+	public Integer getElevation() {
+		return location.getElevation();
+	}
+
+	/**
+	 * Get whether the location for this observation is defined.
+	 * 
+	 * @return Is this location defined.
+	 */
+	public Boolean getLocationDefined() {
+		return location.getLocationDefined();
+	}
+
+	@Override
+	public Amount getValue(String paramName) {
+		return null;
+	}
+
+	@Override
+	public String getString(String paramName) {
+		return null;
+	}
+
+	@Override
+	public String[] getStrings(String paramName) {
+		return null;
+	}
+
+	@Override
+	public Collection<Amount> getValues(String paramName) {
+		return null;
+	}
+
+	@Override
+	public PointDataView getPointDataView() {
+		return pointDataView;
+	}
+
+	@Override
+	public void setPointDataView(PointDataView pointDataView) {
+		this.pointDataView = pointDataView;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		Calendar c = getDataTime().getRefTimeAsCalendar();
+		if (c != null) {
+			sb.append(String.format("LSR:%1$tY%1$tm%1$td%1$tH%1$tM",
+					getDataTime().getRefTimeAsCalendar()));
+		} else {
+			sb.append("LSR:YYYYMMDDHHmm");
+		}
+		sb.append(String.format("%6.2f %7.2f:", getLatitude(), getLongitude()));
+		sb.append(String.format("%s:", cityLoc));
+		sb.append(String.format("%s:", eventType.getEventName()));
+		sb.append(String.format("%5.2f:%s", getMagnitude(), getEventType()
+				.getEventUnits()));
+		return sb.toString();
+	}
 }
