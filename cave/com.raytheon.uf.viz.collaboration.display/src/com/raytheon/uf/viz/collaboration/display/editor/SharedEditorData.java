99e561a26e19eb0b81a2bafcd2d7b69b5df53e51
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-04-27 16:12:48 -0500

    Issue #427 separated collaboration.ui into two plugins
    Change-Id: I3085f2aae4c1c44b65749e587a9eb753d1d7a34b
    
    Former-commit-id: d952fe084a3e13d3c3e4c9038a7cb0b357901408 [formerly e8f1c0159c149e8f5e061b15974d9fd7f9d0436f] [formerly 9454cbd1b9b07635a4bf890d126a63e50a9b2c44 [formerly f96302b439b39030e54a2b87a6e2387ebb63502f]]
    Former-commit-id: 9454cbd1b9b07635a4bf890d126a63e50a9b2c44
    Former-commit-id: 4a85e47ee909258e58a054368c6efa154ad0c554

--- cave/com.raytheon.uf.viz.collaboration.display/src/com/raytheon/uf/viz/collaboration/display/editor/SharedEditorData.java
+++ cave/com.raytheon.uf.viz.collaboration.display/src/com/raytheon/uf/viz/collaboration/display/editor/SharedEditorData.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.viz.collaboration.ui.editor;
+package com.raytheon.uf.viz.collaboration.display.editor;
 
 import java.util.List;
 
