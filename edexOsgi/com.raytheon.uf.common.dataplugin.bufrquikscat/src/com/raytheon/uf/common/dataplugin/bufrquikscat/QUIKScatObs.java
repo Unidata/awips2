b7c5a834b5fdf9b4013ccc2fa60c176f983a17d1
Richard Peter <richard.peter@raytheon.com>
2012-04-27 12:00:12 -0500

    Issue #538: Fix Serialization Annotations and Serialization Issues
    Change-Id: I436997106438ab283e987cb7e3055fcee009b608
    
    Former-commit-id: 231d98b79e7fe8d8153401238e061474c9800d63 [formerly c33a392a8c7aea1811f4366a110ea88c88933d15] [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632] [formerly 231d98b79e7fe8d8153401238e061474c9800d63 [formerly c33a392a8c7aea1811f4366a110ea88c88933d15] [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632] [formerly b11df888d371b9877bd75a74fbd6871588670c19 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632 [formerly 3aadbc778d9bb22dc9c0b4b93bbbaa70c3fa07d0]]]]
    Former-commit-id: b11df888d371b9877bd75a74fbd6871588670c19
    Former-commit-id: 51e9a723719bb20a5f5fef8278e56ffdf995a9de [formerly ae35c2f27c11954175af3a4a25827f476e232ca7] [formerly 8e9c6244c73799f26e260982a3f71ee5ca3d5c9a [formerly e3323febc5dd19b098fb15c40f8873e0e1fbc54e]]
    Former-commit-id: 0fa5241fd917758f0896a74d045990ac166bd25b [formerly 0212d3108feb66a863f881f375e98d08fd82d15d]
    Former-commit-id: 83564a4dde4ed57b9d1bf07c2a5eb14551169c43

--- edexOsgi/com.raytheon.uf.common.dataplugin.bufrquikscat/src/com/raytheon/uf/common/dataplugin/bufrquikscat/QUIKScatObs.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.bufrquikscat/src/com/raytheon/uf/common/dataplugin/bufrquikscat/QUIKScatObs.java
@@ -68,307 +68,308 @@ import com.vividsolutions.jts.geom.Geometry;
 @XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
 public class QUIKScatObs extends PersistablePluginDataObject implements
-        ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
+		ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Embedded
-    private PointDataView pdv;
+	@Embedded
+	@DynamicSerializeElement
+	private PointDataView pointDataView;
 
-    @DataURI(position = 1)
-    @XmlAttribute
-    @DynamicSerializeElement
-    private Integer satId;
+	@DataURI(position = 1)
+	@XmlAttribute
+	@DynamicSerializeElement
+	private Integer satId;
 
-    @Embedded
-    @DataURI(position = 2, embedded = true)
-    @XmlElement
-    @DynamicSerializeElement
-    private SurfaceObsLocation location;
+	@Embedded
+	@DataURI(position = 2, embedded = true)
+	@XmlElement
+	@DynamicSerializeElement
+	private SurfaceObsLocation location;
 
-    // Text of the WMO header
-    @Column(length = 32)
-    @DynamicSerializeElement
-    @XmlElement
-    private String wmoHeader;
+	// Text of the WMO header
+	@Column(length = 32)
+	@DynamicSerializeElement
+	@XmlElement
+	private String wmoHeader;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Integer orbitNumber;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Integer orbitNumber;
 
-    // The observation time.
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Calendar timeObs;
+	// The observation time.
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Calendar timeObs;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Double windDir;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Double windDir;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Double windSpd;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Double windSpd;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Double probRain;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Double probRain;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Integer rainIndex;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Integer rainIndex;
 
-    /**
-     * Empty constructor.
-     */
-    public QUIKScatObs() {
-    }
+	/**
+	 * Empty constructor.
+	 */
+	public QUIKScatObs() {
+	}
 
-    /**
-     * Constructor for DataURI construction through base class. This is used by
-     * the notification service.
-     * 
-     * @param uri
-     *            A data uri applicable to this class.
-     * @param tableDef
-     *            The table definitions for this class.
-     */
-    public QUIKScatObs(String uri) {
-        super(uri);
-    }
+	/**
+	 * Constructor for DataURI construction through base class. This is used by
+	 * the notification service.
+	 * 
+	 * @param uri
+	 *            A data uri applicable to this class.
+	 * @param tableDef
+	 *            The table definitions for this class.
+	 */
+	public QUIKScatObs(String uri) {
+		super(uri);
+	}
 
-    /**
-     * Get this observation's geometry.
-     * 
-     * @return The geometry for this observation.
-     */
-    public Geometry getGeometry() {
-        return location.getGeometry();
-    }
+	/**
+	 * Get this observation's geometry.
+	 * 
+	 * @return The geometry for this observation.
+	 */
+	public Geometry getGeometry() {
+		return location.getGeometry();
+	}
 
-    /**
-     * Get the geometry latitude.
-     * 
-     * @return The geometry latitude.
-     */
-    public double getLatitude() {
-        return location.getLatitude();
-    }
+	/**
+	 * Get the geometry latitude.
+	 * 
+	 * @return The geometry latitude.
+	 */
+	public double getLatitude() {
+		return location.getLatitude();
+	}
 
-    /**
-     * Get the geometry longitude.
-     * 
-     * @return The geometry longitude.
-     */
-    public double getLongitude() {
-        return location.getLongitude();
-    }
+	/**
+	 * Get the geometry longitude.
+	 * 
+	 * @return The geometry longitude.
+	 */
+	public double getLongitude() {
+		return location.getLongitude();
+	}
 
-    /**
-     * Get the elevation, in meters, of the observing platform or location.
-     * 
-     * @return The observation elevation, in meters.
-     */
-    public Integer getElevation() {
-        return location.getElevation();
-    }
+	/**
+	 * Get the elevation, in meters, of the observing platform or location.
+	 * 
+	 * @return The observation elevation, in meters.
+	 */
+	public Integer getElevation() {
+		return location.getElevation();
+	}
 
-    /**
-     * Was this location defined from the station catalog? False if not.
-     * 
-     * @return Was this location defined from the station catalog?
-     */
-    public Boolean getLocationDefined() {
-        return location.getLocationDefined();
-    }
+	/**
+	 * Was this location defined from the station catalog? False if not.
+	 * 
+	 * @return Was this location defined from the station catalog?
+	 */
+	public Boolean getLocationDefined() {
+		return location.getLocationDefined();
+	}
 
-    /**
-     * @return the location
-     */
-    public SurfaceObsLocation getLocation() {
-        return location;
-    }
+	/**
+	 * @return the location
+	 */
+	public SurfaceObsLocation getLocation() {
+		return location;
+	}
 
-    /**
-     * @param location
-     *            the location to set
-     */
-    public void setLocation(SurfaceObsLocation location) {
-        this.location = location;
-    }
+	/**
+	 * @param location
+	 *            the location to set
+	 */
+	public void setLocation(SurfaceObsLocation location) {
+		this.location = location;
+	}
 
-    /**
-     * @return the satId
-     */
-    public Integer getSatId() {
-        return satId;
-    }
+	/**
+	 * @return the satId
+	 */
+	public Integer getSatId() {
+		return satId;
+	}
 
-    /**
-     * @param satId
-     *            the satId to set
-     */
-    public void setSatId(Integer satId) {
-        this.satId = satId;
-    }
+	/**
+	 * @param satId
+	 *            the satId to set
+	 */
+	public void setSatId(Integer satId) {
+		this.satId = satId;
+	}
 
-    /**
-     * @return the orbitNumber
-     */
-    public Integer getOrbitNumber() {
-        return orbitNumber;
-    }
+	/**
+	 * @return the orbitNumber
+	 */
+	public Integer getOrbitNumber() {
+		return orbitNumber;
+	}
 
-    /**
-     * @param orbitNumber
-     *            the orbitNumber to set
-     */
-    public void setOrbitNumber(Integer orbitNumber) {
-        this.orbitNumber = orbitNumber;
-    }
+	/**
+	 * @param orbitNumber
+	 *            the orbitNumber to set
+	 */
+	public void setOrbitNumber(Integer orbitNumber) {
+		this.orbitNumber = orbitNumber;
+	}
 
-    /**
-     * @return the wmoHeader
-     */
-    public String getWmoHeader() {
-        return wmoHeader;
-    }
+	/**
+	 * @return the wmoHeader
+	 */
+	public String getWmoHeader() {
+		return wmoHeader;
+	}
 
-    /**
-     * @param wmoHeader
-     *            the wmoHeader to set
-     */
-    public void setWmoHeader(String wmoHeader) {
-        this.wmoHeader = wmoHeader;
-    }
+	/**
+	 * @param wmoHeader
+	 *            the wmoHeader to set
+	 */
+	public void setWmoHeader(String wmoHeader) {
+		this.wmoHeader = wmoHeader;
+	}
 
-    /**
-     * @return the windDir
-     */
-    public Double getWindDir() {
-        return windDir;
-    }
+	/**
+	 * @return the windDir
+	 */
+	public Double getWindDir() {
+		return windDir;
+	}
 
-    /**
-     * @param windDir
-     *            the windDir to set
-     */
-    public void setWindDir(Double windDir) {
-        this.windDir = windDir;
-    }
+	/**
+	 * @param windDir
+	 *            the windDir to set
+	 */
+	public void setWindDir(Double windDir) {
+		this.windDir = windDir;
+	}
 
-    /**
-     * @return the windSpd
-     */
-    public Double getWindSpd() {
-        return windSpd;
-    }
+	/**
+	 * @return the windSpd
+	 */
+	public Double getWindSpd() {
+		return windSpd;
+	}
 
-    /**
-     * @param windSpd
-     *            the windSpd to set
-     */
-    public void setWindSpd(Double windSpd) {
-        this.windSpd = windSpd;
-    }
+	/**
+	 * @param windSpd
+	 *            the windSpd to set
+	 */
+	public void setWindSpd(Double windSpd) {
+		this.windSpd = windSpd;
+	}
 
-    /**
-     * @return the probRain
-     */
-    public Double getProbRain() {
-        return probRain;
-    }
+	/**
+	 * @return the probRain
+	 */
+	public Double getProbRain() {
+		return probRain;
+	}
 
-    /**
-     * @param probRain
-     *            the probRain to set
-     */
-    public void setProbRain(Double probRain) {
-        this.probRain = probRain;
-    }
+	/**
+	 * @param probRain
+	 *            the probRain to set
+	 */
+	public void setProbRain(Double probRain) {
+		this.probRain = probRain;
+	}
 
-    /**
-     * @return the rainIndex
-     */
-    public Integer getRainIndex() {
-        return rainIndex;
-    }
+	/**
+	 * @return the rainIndex
+	 */
+	public Integer getRainIndex() {
+		return rainIndex;
+	}
 
-    /**
-     * @param rainIndex
-     *            the rainIndex to set
-     */
-    public void setRainIndex(Integer rainIndex) {
-        this.rainIndex = rainIndex;
-    }
+	/**
+	 * @param rainIndex
+	 *            the rainIndex to set
+	 */
+	public void setRainIndex(Integer rainIndex) {
+		this.rainIndex = rainIndex;
+	}
 
-    /**
-     * Get the observation time for this data.
-     * 
-     * @return The data observation time.
-     */
-    public Calendar getTimeObs() {
-        return timeObs;
-    }
+	/**
+	 * Get the observation time for this data.
+	 * 
+	 * @return The data observation time.
+	 */
+	public Calendar getTimeObs() {
+		return timeObs;
+	}
 
-    /**
-     * Set the observation time for this data.
-     * 
-     * @param timeObs
-     *            The data observation time.
-     */
-    public void setTimeObs(Calendar timeObs) {
-        this.timeObs = timeObs;
-    }
+	/**
+	 * Set the observation time for this data.
+	 * 
+	 * @param timeObs
+	 *            The data observation time.
+	 */
+	public void setTimeObs(Calendar timeObs) {
+		this.timeObs = timeObs;
+	}
 
-    @Override
-    public IDecoderGettable getDecoderGettable() {
-        return null;
-    }
+	@Override
+	public IDecoderGettable getDecoderGettable() {
+		return null;
+	}
 
-    @Override
-    public ISpatialObject getSpatialObject() {
-        return location;
-    }
+	@Override
+	public ISpatialObject getSpatialObject() {
+		return location;
+	}
 
-    @Override
-    public String getString(String paramName) {
-        return null;
-    }
+	@Override
+	public String getString(String paramName) {
+		return null;
+	}
 
-    @Override
-    public String[] getStrings(String paramName) {
-        return null;
-    }
+	@Override
+	public String[] getStrings(String paramName) {
+		return null;
+	}
 
-    @Override
-    public Amount getValue(String paramName) {
-        return null;
-    }
+	@Override
+	public Amount getValue(String paramName) {
+		return null;
+	}
 
-    @Override
-    public Collection<Amount> getValues(String paramName) {
-        return null;
-    }
+	@Override
+	public Collection<Amount> getValues(String paramName) {
+		return null;
+	}
 
-    /**
-     * 
-     */
-    @Override
-    public PointDataView getPointDataView() {
-        return pdv;
-    }
-
-    /**
+	/**
      * 
      */
-    @Override
-    public void setPointDataView(PointDataView pdv) {
-        this.pdv = pdv;
-    }
+	@Override
+	public PointDataView getPointDataView() {
+		return pointDataView;
+	}
+
+	/**
+     * 
+     */
+	@Override
+	public void setPointDataView(PointDataView pointDataView) {
+		this.pointDataView = pointDataView;
+	}
 }
