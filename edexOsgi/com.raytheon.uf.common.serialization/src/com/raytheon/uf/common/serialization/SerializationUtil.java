ae35c2f27c11954175af3a4a25827f476e232ca7
Richard Peter <richard.peter@raytheon.com>
2012-04-27 12:00:12 -0500

    Issue #538: Fix Serialization Annotations and Serialization Issues
    Change-Id: I436997106438ab283e987cb7e3055fcee009b608
    
    Former-commit-id: c33a392a8c7aea1811f4366a110ea88c88933d15 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632] [formerly b11df888d371b9877bd75a74fbd6871588670c19 [formerly 3aadbc778d9bb22dc9c0b4b93bbbaa70c3fa07d0]]
    Former-commit-id: b11df888d371b9877bd75a74fbd6871588670c19
    Former-commit-id: e3323febc5dd19b098fb15c40f8873e0e1fbc54e

--- edexOsgi/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/SerializationUtil.java
+++ edexOsgi/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/SerializationUtil.java
@@ -23,6 +23,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
@@ -48,167 +49,190 @@ import com.raytheon.uf.common.serialization.DynamicSerializationManager.Serializ
 
 public class SerializationUtil {
 
-    private static JAXBManager jaxbManager = null;
+	private static JAXBManager jaxbManager = null;
 
-    private SerializationUtil() {
+	private SerializationUtil() {
 
-    }
+	}
 
-    public static synchronized JAXBManager getJaxbManager()
-            throws JAXBException {
-        if (jaxbManager == null) {
-            List<Class<ISerializableObject>> jaxbClasses = SerializableManager
-                    .getInstance().getJaxbables();
-            jaxbManager = new JAXBManager(
-                    jaxbClasses.toArray(new Class[jaxbClasses.size()]));
-        }
-        return jaxbManager;
-    }
+	public static synchronized JAXBManager getJaxbManager()
+			throws JAXBException {
+		if (jaxbManager == null) {
+			List<Class<ISerializableObject>> jaxbClasses = SerializableManager
+					.getInstance().getJaxbables();
+			jaxbManager = new JAXBManager(
+					jaxbClasses.toArray(new Class[jaxbClasses.size()]));
+		}
+		return jaxbManager;
+	}
 
-    public static JAXBContext getJaxbContext() throws JAXBException {
-        return getJaxbManager().getJaxbContext();
-    }
+	public static JAXBContext getJaxbContext() throws JAXBException {
+		return getJaxbManager().getJaxbContext();
+	}
 
-    /**
-     * Instantiates an object from the XML representation in a string. Uses
-     * JAXB.
-     * 
-     * @param xml
-     *            The XML representation
-     * @return A new instance from the XML representation
-     * @throws JAXBException
-     */
-    public static Object unmarshalFromXml(String xml) throws JAXBException {
-        return getJaxbManager().unmarshalFromXml(xml);
+	/**
+	 * Instantiates an object from the XML representation in a string. Uses
+	 * JAXB.
+	 * 
+	 * @param xml
+	 *            The XML representation
+	 * @return A new instance from the XML representation
+	 * @throws JAXBException
+	 */
+	public static Object unmarshalFromXml(String xml) throws JAXBException {
+		return getJaxbManager().unmarshalFromXml(xml);
 
-    }
+	}
 
-    /**
-     * Convert an instance of a class to an XML representation in a string. Uses
-     * JAXB.
-     * 
-     * @param obj
-     *            Object being marshalled
-     * @return XML string representation of the object
-     * @throws JAXBException
-     */
-    public static String marshalToXml(Object obj) throws JAXBException {
-        return getJaxbManager().marshalToXml(obj);
-    }
+	/**
+	 * Convert an instance of a class to an XML representation in a string. Uses
+	 * JAXB.
+	 * 
+	 * @param obj
+	 *            Object being marshalled
+	 * @return XML string representation of the object
+	 * @throws JAXBException
+	 */
+	public static String marshalToXml(Object obj) throws JAXBException {
+		return getJaxbManager().marshalToXml(obj);
+	}
 
-    /**
-     * Convert an instance of a class to an XML representation and write XML to
-     * file. Uses JAXB.
-     * 
-     * @param obj
-     *            Object to be marshaled
-     * @param filePath
-     *            Path to the output file
-     * @throws SerializationException
-     */
-    public static void jaxbMarshalToXmlFile(Object obj, String filePath)
-            throws SerializationException {
-        try {
-            getJaxbManager().jaxbMarshalToXmlFile(obj, filePath);
-        } catch (JAXBException e) {
-            throw new SerializationException(e);
-        }
+	/**
+	 * Convert an instance of a class to an XML representation and write XML to
+	 * file. Uses JAXB.
+	 * 
+	 * @param obj
+	 *            Object to be marshaled
+	 * @param filePath
+	 *            Path to the output file
+	 * @throws SerializationException
+	 */
+	public static void jaxbMarshalToXmlFile(Object obj, String filePath)
+			throws SerializationException {
+		try {
+			getJaxbManager().jaxbMarshalToXmlFile(obj, filePath);
+		} catch (JAXBException e) {
+			throw new SerializationException(e);
+		}
 
-    }
+	}
 
-    /**
-     * Instantiates an object from the XML representation in a File. Uses JAXB.
-     * 
-     * @param filePath
-     *            The path to the XML file
-     * @return A new instance from the XML representation
-     * @throws SerializationException
-     */
-    public static Object jaxbUnmarshalFromXmlFile(String filePath)
-            throws SerializationException {
-        try {
-            return getJaxbManager().jaxbUnmarshalFromXmlFile(filePath);
-        } catch (JAXBException e) {
-            throw new SerializationException(e);
-        }
-    }
+	/**
+	 * Instantiates an object from the XML representation in a File. Uses JAXB.
+	 * 
+	 * @param filePath
+	 *            The path to the XML file
+	 * @return A new instance from the XML representation
+	 * @throws SerializationException
+	 */
+	public static Object jaxbUnmarshalFromXmlFile(String filePath)
+			throws SerializationException {
+		try {
+			return getJaxbManager().jaxbUnmarshalFromXmlFile(filePath);
+		} catch (JAXBException e) {
+			throw new SerializationException(e);
+		}
+	}
 
-    /**
-     * Instantiates an object from the XML representation in a File. Uses JAXB.
-     * 
-     * @param filePath
-     *            The XML file
-     * @return A new instance from the XML representation
-     * @throws SerializationException
-     */
-    public static Object jaxbUnmarshalFromXmlFile(File file)
-            throws SerializationException {
-        try {
-            return getJaxbManager().jaxbUnmarshalFromXmlFile(file);
-        } catch (Exception e) {
-            throw new SerializationException(e.getLocalizedMessage(), e);
-        }
-    }
+	/**
+	 * Instantiates an object from the XML representation in a File. Uses JAXB.
+	 * 
+	 * @param filePath
+	 *            The XML file
+	 * @return A new instance from the XML representation
+	 * @throws SerializationException
+	 */
+	public static Object jaxbUnmarshalFromXmlFile(File file)
+			throws SerializationException {
+		try {
+			return getJaxbManager().jaxbUnmarshalFromXmlFile(file);
+		} catch (Exception e) {
+			throw new SerializationException(e.getLocalizedMessage(), e);
+		}
+	}
 
-    /**
-     * Instantiates an object from the XML representation in a stream. Uses
-     * JAXB.
-     * 
-     * @param is
-     *            The input stream. The stream will be closed by this operation.
-     * @return A new instance from the XML representation
-     * @throws SerializationException
-     */
-    public static Object jaxbUnmarshalFromInputStream(InputStream is)
-            throws SerializationException {
-        try {
-            return getJaxbManager().jaxbUnmarshalFromInputStream(is);
-        } catch (Exception e) {
-            throw new SerializationException(e.getLocalizedMessage(), e);
-        }
-    }
+	/**
+	 * Instantiates an object from the XML representation in a stream. Uses
+	 * JAXB.
+	 * 
+	 * @param is
+	 *            The input stream. The stream will be closed by this operation.
+	 * @return A new instance from the XML representation
+	 * @throws SerializationException
+	 */
+	public static Object jaxbUnmarshalFromInputStream(InputStream is)
+			throws SerializationException {
+		try {
+			return getJaxbManager().jaxbUnmarshalFromInputStream(is);
+		} catch (Exception e) {
+			throw new SerializationException(e.getLocalizedMessage(), e);
+		}
+	}
 
-    /**
-     * Transforms an object to the thrift protocol using DynamicSerialize
-     * 
-     * @param obj
-     *            the object to convert to bytes
-     * @return the object as bytes
-     * @throws SerializationException
-     */
-    public static byte[] transformToThrift(Object obj)
-            throws SerializationException {
-        DynamicSerializationManager dsm = DynamicSerializationManager
-                .getManager(SerializationType.Thrift);
-        return dsm.serialize(obj);
-    }
+	/**
+	 * Transforms an object to the thrift protocol using DynamicSerialize. The
+	 * object will exist in memory 3 times during this process: once in its
+	 * native object form, once in its serialized form in a byte stream, and
+	 * then as an array of bytes returned from byte stream.
+	 * 
+	 * @param obj
+	 *            the object to convert to bytes
+	 * @return the object as bytes
+	 * @throws SerializationException
+	 */
+	public static byte[] transformToThrift(Object obj)
+			throws SerializationException {
+		DynamicSerializationManager dsm = DynamicSerializationManager
+				.getManager(SerializationType.Thrift);
+		return dsm.serialize(obj);
+	}
 
-    /**
-     * Transforms a byte array from the thrift protocol to an object using
-     * DynamicSerialize
-     * 
-     * @param bytes
-     *            the object as bytes
-     * @return the Java object
-     * @throws SerializationException
-     */
-    public static Object transformFromThrift(byte[] bytes)
-            throws SerializationException {
-        DynamicSerializationManager dsm = DynamicSerializationManager
-                .getManager(SerializationType.Thrift);
-        ByteArrayInputStream bais = null;
-        try {
-            bais = new ByteArrayInputStream(bytes);
-            return dsm.deserialize(bais);
-        } finally {
-            if (bais != null) {
-                try {
-                    bais.close();
-                } catch (IOException e) {
-                    // ignore
-                }
-            }
-        }
-    }
+	/**
+	 * Transforms an object to the thrift protocol using DynamicSerialize.
+	 * Object will be written directly to stream reducing memory usage compared
+	 * to returning a byte array. Named differently from standard
+	 * transformToThrift to avoid ambiguity in camel routes.
+	 * 
+	 * @param obj
+	 *            the object to convert to bytes
+	 * @param os
+	 *            the output stream to write the bytes to
+	 * @return the object as bytes
+	 * @throws SerializationException
+	 */
+	public static void transformToThriftUsingStream(Object obj, OutputStream os)
+			throws SerializationException {
+		DynamicSerializationManager dsm = DynamicSerializationManager
+				.getManager(SerializationType.Thrift);
+		dsm.serialize(obj, os);
+	}
+
+	/**
+	 * Transforms a byte array from the thrift protocol to an object using
+	 * DynamicSerialize
+	 * 
+	 * @param bytes
+	 *            the object as bytes
+	 * @return the Java object
+	 * @throws SerializationException
+	 */
+	public static Object transformFromThrift(byte[] bytes)
+			throws SerializationException {
+		DynamicSerializationManager dsm = DynamicSerializationManager
+				.getManager(SerializationType.Thrift);
+		ByteArrayInputStream bais = null;
+		try {
+			bais = new ByteArrayInputStream(bytes);
+			return dsm.deserialize(bais);
+		} finally {
+			if (bais != null) {
+				try {
+					bais.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+	}
 
 }
