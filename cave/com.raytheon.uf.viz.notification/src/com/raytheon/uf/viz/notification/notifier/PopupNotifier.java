21ac4d37bee1d80307099c52d8020465bdadc015
Matt Nash <Matt_Nash@raytheon.com>
2012-04-27 10:10:05 -0500

    Issue #429 add better notification to chat
    Former-commit-id: 18e07972ca48d126ed2335a85b6ef489861a2e6c [formerly 590862b77de283dac22f06b6bd4a961b2c5307e3] [formerly e0d11ae8b199281831527bd072cba42a1f2cb11c] [formerly 3b2fdaa579bd27aad4f67cad0222317884e3e2f6 [formerly e0d11ae8b199281831527bd072cba42a1f2cb11c [formerly 40db7caa4b6bd45537f4cb37a3f034a907cd8f79]]]
    Former-commit-id: 3b2fdaa579bd27aad4f67cad0222317884e3e2f6
    Former-commit-id: a873337238e14b1e9af1b91cd3bd0b55656415ad [formerly 91035ee2533eb0cfe14b3e4e4f11874a45081a6e]
    Former-commit-id: e94eaf4752a8c301675f6b6411b048644128bbc1

---
+++ cave/com.raytheon.uf.viz.notification/src/com/raytheon/uf/viz/notification/notifier/PopupNotifier.java
@@ -0,0 +1,191 @@
+package com.raytheon.uf.viz.notification.notifier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+import com.raytheon.uf.viz.core.VizApp;
+
+/**
+ * Notifications to show up in the bottom right of the monitor, will stack if
+ * another is currently displayed
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 26, 2012            mnash     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mnash
+ * @version 1.0
+ */
+public class PopupNotifier {
+
+    // how long the the popup is displayed
+    private static final int TIME_TO_DISPLAY = 4500;
+
+    // what is currently popped up
+    private static List<Shell> activeShells = new ArrayList<Shell>();
+
+    private static Shell shell;
+
+    private static Font titleFont = null;
+
+    /**
+     * Creates and shows a notification dialog with a specific title and message
+     * 
+     * For future could allow for different types of notifications to be
+     * different colors and different popup types?
+     * 
+     * @param title
+     * @param message
+     */
+    public static void notify(String title, String message) {
+        if (Display.getCurrent() == null
+                || Display.getCurrent().getActiveShell() == null) {
+            shell = new Shell(Display.getDefault(), SWT.NO_FOCUS | SWT.NO_TRIM
+                    | SWT.ON_TOP);
+            GridLayout layout = new GridLayout(1, false);
+            layout.marginHeight = 0;
+            layout.marginWidth = 0;
+            shell.setLayout(layout);
+            shell.setForeground(Display.getDefault().getSystemColor(
+                    SWT.COLOR_BLACK));
+            shell.addListener(SWT.Dispose, new Listener() {
+                @Override
+                public void handleEvent(Event event) {
+                    activeShells.remove(shell);
+                    if (activeShells.isEmpty()) {
+                        titleFont.dispose();
+                    }
+                }
+            });
+
+            final Composite comp = new Composite(shell, SWT.BORDER);
+
+            layout = new GridLayout(1, false);
+            layout.marginHeight = 0;
+            layout.marginWidth = 0;
+            GridData data = new GridData(SWT.FILL, SWT.FILL, true, true);
+            comp.setLayout(layout);
+            comp.setLayoutData(data);
+
+            CLabel titleLabel = new CLabel(comp, SWT.NONE);
+            data = new GridData(SWT.FILL, SWT.FILL, true, true);
+            data.horizontalSpan = 2;
+            titleLabel.setLayoutData(data);
+
+            // storing off the font, make the title font pop out a little more
+            // to the user
+            if (titleFont == null || titleFont.isDisposed()) {
+                titleFont = titleLabel.getFont();
+                FontData fd = titleFont.getFontData()[0];
+                fd.setStyle(SWT.BOLD);
+                fd.height = fd.height + 2;
+                titleFont = new Font(Display.getCurrent(), fd);
+            }
+            titleLabel.setFont(titleFont);
+            titleLabel.setText(title);
+
+            // separate the title from the body text
+            Label separator = new Label(comp, SWT.HORIZONTAL | SWT.SEPARATOR);
+            data = new GridData(SWT.FILL, SWT.FILL, true, true);
+            separator.setLayoutData(data);
+            separator.setVisible(true);
+
+            // here is the text for the body of the message
+            Label text = new Label(comp, SWT.WRAP);
+            data = new GridData(SWT.FILL, SWT.FILL, true, true);
+            text.setLayoutData(data);
+            text.setText(message);
+
+            // maybe this should be minimum size?
+            shell.setSize(350, 100);
+
+            // get the bottom right corner of the monitor
+            Rectangle clientArea = Display.getDefault().getMonitors()[0]
+                    .getClientArea();
+
+            // and offset that may not be needed, only here to keep the box from
+            // touching the corner
+            int offset = 2;
+            int startX = clientArea.x + clientArea.width - shell.getSize().x
+                    - offset;
+            int startY = clientArea.y + clientArea.height - shell.getSize().y
+                    - offset;
+
+            // move other shells up
+            synchronized (activeShells) {
+                if (!activeShells.isEmpty()) {
+                    List<Shell> modifiable = new ArrayList<Shell>(activeShells);
+                    Collections.reverse(modifiable);
+                    for (Shell shell : modifiable) {
+                        if (shell.isDisposed()) {
+                            activeShells.remove(shell);
+                            continue;
+                        }
+                        Point curLoc = shell.getLocation();
+                        shell.setLocation(curLoc.x, curLoc.y - 100);
+                        if (curLoc.y - 100 < 0) {
+                            activeShells.remove(shell);
+                            shell.dispose();
+                        }
+                    }
+                }
+            }
+
+            shell.setLocation(startX, startY);
+            shell.setVisible(true);
+
+            activeShells.add(shell);
+            startTimer();
+        }
+    }
+
+    /**
+     * @wbp.parser.entryPoint
+     */
+    private static void startTimer() {
+        Timer timer = new Timer("Remove notification");
+        TimerTask task = new TimerTask() {
+
+            @Override
+            public void run() {
+                try {
+                    if (shell == null || shell.isDisposed()) {
+                        return;
+                    }
+                    VizApp.runAsync(new Runnable() {
+                        public void run() {
+                            shell.dispose();
+                        };
+                    });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.schedule(task, TIME_TO_DISPLAY);
+    }
+}
