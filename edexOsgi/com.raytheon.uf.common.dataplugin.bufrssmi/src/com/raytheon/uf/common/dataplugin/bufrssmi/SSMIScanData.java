ab5f21f30854fce6c9479a9a3d39a8dad9a2da31
Richard Peter <richard.peter@raytheon.com>
2012-04-27 12:00:12 -0500

    Issue #538: Fix Serialization Annotations and Serialization Issues
    Change-Id: I436997106438ab283e987cb7e3055fcee009b608
    
    Former-commit-id: 0a3d46e642585ef4462646f9c29b4c2ec73a23d7 [formerly 0a3d46e642585ef4462646f9c29b4c2ec73a23d7 [formerly c33a392a8c7aea1811f4366a110ea88c88933d15]] [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632] [formerly e3323febc5dd19b098fb15c40f8873e0e1fbc54e [formerly e3323febc5dd19b098fb15c40f8873e0e1fbc54e [formerly b11df888d371b9877bd75a74fbd6871588670c19]] [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632 [formerly 349adaeedd99fdcbde5999c2ea7195e2b06b6632 [formerly 3aadbc778d9bb22dc9c0b4b93bbbaa70c3fa07d0]]]]
    Former-commit-id: e3323febc5dd19b098fb15c40f8873e0e1fbc54e [formerly e3323febc5dd19b098fb15c40f8873e0e1fbc54e [formerly b11df888d371b9877bd75a74fbd6871588670c19]]
    Former-commit-id: e3323febc5dd19b098fb15c40f8873e0e1fbc54e
    Former-commit-id: 13e66e6aa5519e5bb35afdab2c5b0e1152447a7e [formerly ae35c2f27c11954175af3a4a25827f476e232ca7]
    Former-commit-id: bc060ebd640e01e56b3c51a19af87a5cbb4c22c9

--- edexOsgi/com.raytheon.uf.common.dataplugin.bufrssmi/src/com/raytheon/uf/common/dataplugin/bufrssmi/SSMIScanData.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.bufrssmi/src/com/raytheon/uf/common/dataplugin/bufrssmi/SSMIScanData.java
@@ -68,322 +68,323 @@ import com.vividsolutions.jts.geom.Geometry;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 public class SSMIScanData extends PersistablePluginDataObject implements
-        ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
+		ISpatialEnabled, IDecoderGettable, IPointData, IPersistable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @DataURI(position = 1)
-    @XmlAttribute
-    @DynamicSerializeElement
-    private Integer satId;
+	@DataURI(position = 1)
+	@XmlAttribute
+	@DynamicSerializeElement
+	private Integer satId;
 
-    @Embedded
-    @DataURI(position = 2, embedded = true)
-    @XmlElement
-    @DynamicSerializeElement
-    private SurfaceObsLocation location;
+	@Embedded
+	@DataURI(position = 2, embedded = true)
+	@XmlElement
+	@DynamicSerializeElement
+	private SurfaceObsLocation location;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Integer orbitNumber;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Integer orbitNumber;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Integer scanNumber;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Integer scanNumber;
 
-    @XmlAttribute
-    @DynamicSerializeElement
-    @Transient
-    private Integer posNumber;
+	@XmlAttribute
+	@DynamicSerializeElement
+	@Transient
+	private Integer posNumber;
 
-    // The profiler observation time.
-    @Column
-    @XmlAttribute
-    @DynamicSerializeElement
-    private Calendar timeObs;
+	// The profiler observation time.
+	@Column
+	@XmlAttribute
+	@DynamicSerializeElement
+	private Calendar timeObs;
 
-    @Embedded
-    private PointDataView pdv;
+	@Embedded
+	@DynamicSerializeElement
+	private PointDataView pointDataView;
 
-    // Text of the WMO header
-    @Column(length = 32)
-    @DynamicSerializeElement
-    @XmlElement
-    private String wmoHeader;
+	// Text of the WMO header
+	@Column(length = 32)
+	@DynamicSerializeElement
+	@XmlElement
+	private String wmoHeader;
 
-    /**
-     * Empty constructor.
-     */
-    public SSMIScanData() {
-    }
+	/**
+	 * Empty constructor.
+	 */
+	public SSMIScanData() {
+	}
 
-    /**
-     * Constructor for DataURI construction through base class. This is used by
-     * the notification service.
-     * 
-     * @param uri
-     *            A data uri applicable to this class.
-     * @param tableDef
-     *            The table definitions for this class.
-     */
-    public SSMIScanData(String uri) {
-        super(uri);
-    }
+	/**
+	 * Constructor for DataURI construction through base class. This is used by
+	 * the notification service.
+	 * 
+	 * @param uri
+	 *            A data uri applicable to this class.
+	 * @param tableDef
+	 *            The table definitions for this class.
+	 */
+	public SSMIScanData(String uri) {
+		super(uri);
+	}
 
-    /**
-     * Get this observation's geometry.
-     * 
-     * @return The geometry for this observation.
-     */
-    public Geometry getGeometry() {
-        return location.getGeometry();
-    }
+	/**
+	 * Get this observation's geometry.
+	 * 
+	 * @return The geometry for this observation.
+	 */
+	public Geometry getGeometry() {
+		return location.getGeometry();
+	}
 
-    /**
-     * Get the geometry latitude.
-     * 
-     * @return The geometry latitude.
-     */
-    public double getLatitude() {
-        return location.getLatitude();
-    }
+	/**
+	 * Get the geometry latitude.
+	 * 
+	 * @return The geometry latitude.
+	 */
+	public double getLatitude() {
+		return location.getLatitude();
+	}
 
-    /**
-     * Get the geometry longitude.
-     * 
-     * @return The geometry longitude.
-     */
-    public double getLongitude() {
-        return location.getLongitude();
-    }
+	/**
+	 * Get the geometry longitude.
+	 * 
+	 * @return The geometry longitude.
+	 */
+	public double getLongitude() {
+		return location.getLongitude();
+	}
 
-    /**
-     * Get the elevation, in meters, of the observing platform or location.
-     * 
-     * @return The observation elevation, in meters.
-     */
-    public Integer getElevation() {
-        return location.getElevation();
-    }
+	/**
+	 * Get the elevation, in meters, of the observing platform or location.
+	 * 
+	 * @return The observation elevation, in meters.
+	 */
+	public Integer getElevation() {
+		return location.getElevation();
+	}
 
-    /**
-     * Was this location defined from the station catalog? False if not.
-     * 
-     * @return Was this location defined from the station catalog?
-     */
-    public Boolean getLocationDefined() {
-        return location.getLocationDefined();
-    }
+	/**
+	 * Was this location defined from the station catalog? False if not.
+	 * 
+	 * @return Was this location defined from the station catalog?
+	 */
+	public Boolean getLocationDefined() {
+		return location.getLocationDefined();
+	}
 
-    /**
-     * @return the satId
-     */
-    public Integer getSatId() {
-        return satId;
-    }
+	/**
+	 * @return the satId
+	 */
+	public Integer getSatId() {
+		return satId;
+	}
 
-    /**
-     * @param satId
-     *            the satId to set
-     */
-    public void setSatId(Integer satId) {
-        this.satId = satId;
-    }
+	/**
+	 * @param satId
+	 *            the satId to set
+	 */
+	public void setSatId(Integer satId) {
+		this.satId = satId;
+	}
 
-    /**
-     * @return the orbitNumber
-     */
-    public Integer getOrbitNumber() {
-        return orbitNumber;
-    }
+	/**
+	 * @return the orbitNumber
+	 */
+	public Integer getOrbitNumber() {
+		return orbitNumber;
+	}
 
-    /**
-     * @param orbitNumber
-     *            the orbitNumber to set
-     */
-    public void setOrbitNumber(Integer orbitNumber) {
-        this.orbitNumber = orbitNumber;
-    }
+	/**
+	 * @param orbitNumber
+	 *            the orbitNumber to set
+	 */
+	public void setOrbitNumber(Integer orbitNumber) {
+		this.orbitNumber = orbitNumber;
+	}
 
-    /**
-     * @return the scanNumber
-     */
-    public Integer getScanNumber() {
-        return scanNumber;
-    }
+	/**
+	 * @return the scanNumber
+	 */
+	public Integer getScanNumber() {
+		return scanNumber;
+	}
 
-    /**
-     * @param scanNumber
-     *            the scanNumber to set
-     */
-    public void setScanNumber(Integer scanNumber) {
-        this.scanNumber = scanNumber;
-    }
+	/**
+	 * @param scanNumber
+	 *            the scanNumber to set
+	 */
+	public void setScanNumber(Integer scanNumber) {
+		this.scanNumber = scanNumber;
+	}
 
-    /**
-     * @return the posNumber
-     */
-    public Integer getPosNumber() {
-        return posNumber;
-    }
+	/**
+	 * @return the posNumber
+	 */
+	public Integer getPosNumber() {
+		return posNumber;
+	}
 
-    /**
-     * @param posNumber
-     *            the posNumber to set
-     */
-    public void setPosNumber(Integer posNumber) {
-        this.posNumber = posNumber;
-    }
+	/**
+	 * @param posNumber
+	 *            the posNumber to set
+	 */
+	public void setPosNumber(Integer posNumber) {
+		this.posNumber = posNumber;
+	}
 
-    /**
-     * Get the observation time for this data.
-     * 
-     * @return The data observation time.
-     */
-    public Calendar getTimeObs() {
-        return timeObs;
-    }
+	/**
+	 * Get the observation time for this data.
+	 * 
+	 * @return The data observation time.
+	 */
+	public Calendar getTimeObs() {
+		return timeObs;
+	}
 
-    /**
-     * Set the observation time for this data.
-     * 
-     * @param timeObs
-     *            The data observation time.
-     */
-    public void setTimeObs(Calendar timeObs) {
-        this.timeObs = timeObs;
-    }
+	/**
+	 * Set the observation time for this data.
+	 * 
+	 * @param timeObs
+	 *            The data observation time.
+	 */
+	public void setTimeObs(Calendar timeObs) {
+		this.timeObs = timeObs;
+	}
 
-    /**
-     * @return the wmoHeader
-     */
-    public String getWmoHeader() {
-        return wmoHeader;
-    }
+	/**
+	 * @return the wmoHeader
+	 */
+	public String getWmoHeader() {
+		return wmoHeader;
+	}
 
-    /**
-     * @param wmoHeader
-     *            the wmoHeader to set
-     */
-    public void setWmoHeader(String wmoHeader) {
-        this.wmoHeader = wmoHeader;
-    }
+	/**
+	 * @param wmoHeader
+	 *            the wmoHeader to set
+	 */
+	public void setWmoHeader(String wmoHeader) {
+		this.wmoHeader = wmoHeader;
+	}
 
-    @Override
-    public IDecoderGettable getDecoderGettable() {
-        return null;
-    }
+	@Override
+	public IDecoderGettable getDecoderGettable() {
+		return null;
+	}
 
-    @Override
-    public SurfaceObsLocation getSpatialObject() {
-        return location;
-    }
+	@Override
+	public SurfaceObsLocation getSpatialObject() {
+		return location;
+	}
 
-    public SurfaceObsLocation getLocation() {
-        return location;
-    }
+	public SurfaceObsLocation getLocation() {
+		return location;
+	}
 
-    public void setLocation(SurfaceObsLocation location) {
-        this.location = location;
-    }
+	public void setLocation(SurfaceObsLocation location) {
+		this.location = location;
+	}
 
-    @Override
-    public String getString(String paramName) {
-        return null;
-    }
+	@Override
+	public String getString(String paramName) {
+		return null;
+	}
 
-    @Override
-    public String[] getStrings(String paramName) {
-        return null;
-    }
+	@Override
+	public String[] getStrings(String paramName) {
+		return null;
+	}
 
-    @Override
-    public Amount getValue(String paramName) {
-        return null;
-    }
+	@Override
+	public Amount getValue(String paramName) {
+		return null;
+	}
 
-    @Override
-    public Collection<Amount> getValues(String paramName) {
-        return null;
-    }
+	@Override
+	public Collection<Amount> getValues(String paramName) {
+		return null;
+	}
 
-    /**
-     * 
-     */
-    @Override
-    public PointDataView getPointDataView() {
-        return pdv;
-    }
-
-    /**
+	/**
      * 
      */
-    @Override
-    public void setPointDataView(PointDataView pdv) {
-        this.pdv = pdv;
-    }
+	@Override
+	public PointDataView getPointDataView() {
+		return pointDataView;
+	}
 
-    /**
+	/**
      * 
-     * @return
      */
-    public final SSMIScanData copyObs() {
-        SSMIScanData obs = new SSMIScanData();
+	@Override
+	public void setPointDataView(PointDataView pointDataView) {
+		this.pointDataView = pointDataView;
+	}
 
-        obs.dataTime = dataTime.clone();
-        obs.timeObs = TimeTools.copy(timeObs);
-        obs.orbitNumber = orbitNumber;
-        obs.satId = satId;
-        obs.scanNumber = scanNumber;
-        obs.wmoHeader = wmoHeader;
+	/**
+	 * 
+	 * @return
+	 */
+	public final SSMIScanData copyObs() {
+		SSMIScanData obs = new SSMIScanData();
 
-        return obs;
-    }
+		obs.dataTime = dataTime.clone();
+		obs.timeObs = TimeTools.copy(timeObs);
+		obs.orbitNumber = orbitNumber;
+		obs.satId = satId;
+		obs.scanNumber = scanNumber;
+		obs.wmoHeader = wmoHeader;
 
-    /**
-     * Returns the hashCode for this object. This implementation returns the
-     * hashCode of the generated dataURI.
-     * 
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((getDataURI() == null) ? 0 : getDataURI().hashCode());
-        return result;
-    }
+		return obs;
+	}
 
-    /**
-     * Checks if this record is equal to another by checking the generated
-     * dataURI.
-     * 
-     * @param obj
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        SSMIScanData other = (SSMIScanData) obj;
-        if (getDataURI() == null) {
-            if (other.getDataURI() != null) {
-                return false;
-            }
-        } else if (!getDataURI().equals(other.getDataURI())) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Returns the hashCode for this object. This implementation returns the
+	 * hashCode of the generated dataURI.
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((getDataURI() == null) ? 0 : getDataURI().hashCode());
+		return result;
+	}
+
+	/**
+	 * Checks if this record is equal to another by checking the generated
+	 * dataURI.
+	 * 
+	 * @param obj
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		SSMIScanData other = (SSMIScanData) obj;
+		if (getDataURI() == null) {
+			if (other.getDataURI() != null) {
+				return false;
+			}
+		} else if (!getDataURI().equals(other.getDataURI())) {
+			return false;
+		}
+		return true;
+	}
 
 }
