1c9389977aae51e417afba1a4dd415769cb2977b
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-05-03 13:07:43 -0500

    Issue #587 Got colorbars rendering properly
    Change-Id: I9fe1b846e68dbdc947202cfcfe88aa7c8e1c1103
    
    Former-commit-id: 14314df63bfb9f32703e945775362d32e8bb187c [formerly 14314df63bfb9f32703e945775362d32e8bb187c [formerly 0f6240eaea340a4042c43f926c52a693f9a96370]] [formerly d4c10e5548f109cf8bf1fb6b5bcf02ac58f47f89] [formerly c6edfad2638ee0bd27e189c57df0f6bd96083386 [formerly c6edfad2638ee0bd27e189c57df0f6bd96083386 [formerly c81c225a6ec68e95ee5c3ef1951bcc3a96ae5592]] [formerly d4c10e5548f109cf8bf1fb6b5bcf02ac58f47f89 [formerly d4c10e5548f109cf8bf1fb6b5bcf02ac58f47f89 [formerly 218d9138f8c096d3f99305e2f3ac80b3b30c9cfe]]]]
    Former-commit-id: c6edfad2638ee0bd27e189c57df0f6bd96083386 [formerly c6edfad2638ee0bd27e189c57df0f6bd96083386 [formerly c81c225a6ec68e95ee5c3ef1951bcc3a96ae5592]]
    Former-commit-id: c6edfad2638ee0bd27e189c57df0f6bd96083386
    Former-commit-id: b58c16d49b3c614159b18b2ccedc0dae7f64adf5 [formerly 726c5432664868120fcb010b002521d55df8fe3b]
    Former-commit-id: 53683fbfab0a47e84464a012eef834da6df88b64

---
+++ cave/com.raytheon.uf.viz.remote.graphics/src/com/raytheon/uf/viz/remote/graphics/events/colormap/DrawColorRampEvent.java
@@ -0,0 +1,246 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.remote.graphics.events.colormap;
+
+import java.util.Arrays;
+
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+import com.raytheon.uf.viz.core.IExtent;
+import com.raytheon.uf.viz.core.PixelExtent;
+import com.raytheon.uf.viz.remote.graphics.events.rendering.AbstractRemoteGraphicsRenderEvent;
+import com.raytheon.uf.viz.remote.graphics.events.rendering.IRenderEvent;
+
+/**
+ * Event for rending a color map
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * May 3, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+@DynamicSerialize
+public class DrawColorRampEvent extends AbstractRemoteGraphicsRenderEvent {
+
+    @DynamicSerializeElement
+    private int colorMapId;
+
+    @DynamicSerializeElement
+    private float alpha = 1.0f;
+
+    @DynamicSerializeElement
+    private float brightness = 1.0f;
+
+    @DynamicSerializeElement
+    private float contrast = 1.0f;
+
+    @DynamicSerializeElement
+    private boolean interpolate = true;
+
+    @DynamicSerializeElement
+    private double[] extent;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.remote.graphics.events.rendering.IRenderEvent#
+     * applyDiffObject
+     * (com.raytheon.uf.viz.remote.graphics.events.rendering.IRenderEvent)
+     */
+    @Override
+    public void applyDiffObject(IRenderEvent diffEvent) {
+        DrawColorRampEvent diffObject = (DrawColorRampEvent) diffEvent;
+        colorMapId = diffObject.colorMapId;
+        alpha = diffObject.alpha;
+        brightness = diffObject.brightness;
+        contrast = diffObject.contrast;
+        interpolate = diffObject.interpolate;
+        if (diffObject.extent != null) {
+            extent = diffObject.extent;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.remote.graphics.events.rendering.
+     * AbstractRemoteGraphicsRenderEvent
+     * #createDiffObject(com.raytheon.uf.viz.remote
+     * .graphics.events.rendering.IRenderEvent)
+     */
+    @Override
+    public IRenderEvent createDiffObject(IRenderEvent event) {
+        DrawColorRampEvent diffEvent = (DrawColorRampEvent) event;
+        DrawColorRampEvent diffObject = new DrawColorRampEvent();
+        diffObject.colorMapId = diffEvent.colorMapId;
+        diffObject.alpha = diffEvent.alpha;
+        diffObject.brightness = diffEvent.brightness;
+        diffObject.contrast = diffEvent.contrast;
+        diffObject.interpolate = diffEvent.interpolate;
+        if (Arrays.equals(extent, diffEvent.extent) == false) {
+            diffObject.extent = diffEvent.extent;
+        }
+        return diffObject;
+    }
+
+    /**
+     * @return the colorMapId
+     */
+    public int getColorMapId() {
+        return colorMapId;
+    }
+
+    /**
+     * @param colorMapId
+     *            the colorMapId to set
+     */
+    public void setColorMapId(int colorMapId) {
+        this.colorMapId = colorMapId;
+    }
+
+    /**
+     * @return the alpha
+     */
+    public float getAlpha() {
+        return alpha;
+    }
+
+    /**
+     * @param alpha
+     *            the alpha to set
+     */
+    public void setAlpha(float alpha) {
+        this.alpha = alpha;
+    }
+
+    /**
+     * @return the brightness
+     */
+    public float getBrightness() {
+        return brightness;
+    }
+
+    /**
+     * @param brightness
+     *            the brightness to set
+     */
+    public void setBrightness(float brightness) {
+        this.brightness = brightness;
+    }
+
+    /**
+     * @return the contrast
+     */
+    public float getContrast() {
+        return contrast;
+    }
+
+    /**
+     * @param contrast
+     *            the contrast to set
+     */
+    public void setContrast(float contrast) {
+        this.contrast = contrast;
+    }
+
+    /**
+     * @return the interpolate
+     */
+    public boolean isInterpolate() {
+        return interpolate;
+    }
+
+    /**
+     * @param interpolate
+     *            the interpolate to set
+     */
+    public void setInterpolate(boolean interpolate) {
+        this.interpolate = interpolate;
+    }
+
+    /**
+     * @return the extent
+     */
+    public double[] getExtent() {
+        return extent;
+    }
+
+    /**
+     * @param extent
+     *            the extent to set
+     */
+    public void setExtent(double[] extent) {
+        this.extent = extent;
+    }
+
+    public void setIExtent(IExtent extent) {
+        if (extent != null) {
+            setExtent(new double[] { extent.getMinX(), extent.getMaxX(),
+                    extent.getMinY(), extent.getMaxY() });
+        }
+    }
+
+    public IExtent getIExtent() {
+        if (extent != null) {
+            return new PixelExtent(extent[0], extent[1], extent[2], extent[3]);
+        }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DrawColorRampEvent other = (DrawColorRampEvent) obj;
+        if (Float.floatToIntBits(alpha) != Float.floatToIntBits(other.alpha))
+            return false;
+        if (Float.floatToIntBits(brightness) != Float
+                .floatToIntBits(other.brightness))
+            return false;
+        if (colorMapId != other.colorMapId)
+            return false;
+        if (Float.floatToIntBits(contrast) != Float
+                .floatToIntBits(other.contrast))
+            return false;
+        if (!Arrays.equals(extent, other.extent))
+            return false;
+        if (interpolate != other.interpolate)
+            return false;
+        return true;
+    }
+
+}
