cb1ff65e777e9ee9a0694963126909c0133a9b7a
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2012-05-10 17:02:18 -0500

    Issue #628 committing more PGEN code received from Plummer on 4-19-2012.
    Former-commit-id: 22a29b98b23bc0d07bb662963d9cf7e5267e8a93 [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463] [formerly 22a29b98b23bc0d07bb662963d9cf7e5267e8a93 [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463] [formerly 0df0aa866343c37f4aa6513ac0eeaa7456509182 [formerly bd658184b246452b5d3e2154d79f2179bdc12594]]]
    Former-commit-id: 0df0aa866343c37f4aa6513ac0eeaa7456509182
    Former-commit-id: af92e8d329e6608d1c68e5bd67e2ab7781193304 [formerly e1f03d93697ac318731b1e80f0e9d41d5784394c]
    Former-commit-id: 998c7b3174c6d54b8883a7919cf6e840e8809d9d

---
+++ ncep/gov.noaa.nws.ncep.staticdataprovider/src/gov/noaa/nws/ncep/staticdataprovider/USStateProvider.java
@@ -0,0 +1,128 @@
+/*
+ * gov.noaa.nws.ncep.common.staticDataProvider.USStateProvider
+ * 
+ * 12 March 2012
+ *
+ * This code has been developed by the NCEP/SIB for use in the AWIPS2 system.
+ */
+
+package gov.noaa.nws.ncep.staticdataprovider;
+
+import gov.noaa.nws.ncep.common.staticdata.USState;
+import gov.noaa.nws.ncep.viz.common.dbQuery.NcDirectDbQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.raytheon.uf.viz.core.catalog.DirectDbQuery.QueryLanguage;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+
+/**
+ * Class to load US states maps.
+ * 
+ * <pre>
+ * SOFTWARE HISTORY
+ * Date       	Ticket#		Engineer	Description
+ * ------------	----------	-----------	--------------------------
+ * 02/12		?			B. Yin   	Moved from PGEN 
+ *
+ * </pre>
+ * 
+ * @author	B. Yin
+ */
+
+public class USStateProvider {
+	
+	private static List<USState> allStates;
+	private static volatile boolean stateLoaded = false;
+	
+	/**
+	 * Get all counties and marine zones from the database
+	 * @return
+	 */
+	public static List<USState> getAllStates(){
+		if ( !stateLoaded ){
+			loadStateTable();
+		}
+		
+		return allStates;
+	}
+	
+	public static synchronized List<USState> loadStateTable(){
+		
+		if ( !stateLoaded ) {
+			
+			allStates = new ArrayList<USState>();
+
+			List<Object[]> states;
+
+			String queryStates = "Select AsBinary(the_geom),state,name,fips,lat, lon FROM mapdata.states;";
+
+			try {
+				states = NcDirectDbQuery.executeQuery(
+						queryStates, "maps", QueryLanguage.SQL);
+
+				WKBReader wkbReader = new WKBReader();
+
+				int tt = 0;
+				for ( Object[] st : states ){
+
+						//read shape
+						byte[] wkb = (byte[]) st[0];
+						Geometry stateGeo = null;
+						try {
+							stateGeo =  wkbReader.read(wkb);
+						} catch (ParseException e) {
+							e.printStackTrace();
+						}
+
+						String stateAbb = (String)st[1];
+						String fullName = (String)st[2];
+						String fips = (String)st[3];
+
+						Coordinate loc = new Coordinate(0,0); 
+						try {
+							loc.x = ((Number)st[5]).doubleValue(); 
+							loc.y =	((Number)st[4]).doubleValue();
+						}
+						catch ( Exception e ){
+							// center location missing in database 
+						}
+
+								USState state = new USState(stateAbb, fullName, fips, loc, stateGeo);
+								allStates.add(state);
+								tt++;
+							
+						}	
+				
+
+				
+			}
+			catch (Exception e ){
+				System.out.println("db exception reading state tables!");	
+				e.printStackTrace();
+			}
+			stateLoaded = true;
+			
+		}
+		return allStates;
+	}
+	
+	public static ArrayList<USState> statesInGeometry(Geometry geo){
+		loadStateTable();
+		ArrayList<USState> ret= new ArrayList<USState>();
+		for ( USState st : allStates){
+			if ( st.intersectGeometry(geo)){
+				ret.add(st);
+			}
+		}
+		return ret;
+	}
+	
+	public static boolean isLoaded(){
+		return stateLoaded;
+	}
+}
