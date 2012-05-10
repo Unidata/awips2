c8c20ccd9d4fa9bb6f18c466aa3c5ade1e69cbd1
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2012-05-10 17:02:18 -0500

    Issue #628 committing more PGEN code received from Plummer on 4-19-2012.
    Former-commit-id: 85e4b6376b361355232cb8947925eae7cfe23412 [formerly 2d87d2e1136037fa70445b54137a838ff374189b] [formerly 2d87d2e1136037fa70445b54137a838ff374189b [formerly 22a29b98b23bc0d07bb662963d9cf7e5267e8a93]] [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463] [formerly e1f03d93697ac318731b1e80f0e9d41d5784394c [formerly e1f03d93697ac318731b1e80f0e9d41d5784394c [formerly 0df0aa866343c37f4aa6513ac0eeaa7456509182]] [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463 [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463 [formerly bd658184b246452b5d3e2154d79f2179bdc12594]]]]
    Former-commit-id: e1f03d93697ac318731b1e80f0e9d41d5784394c [formerly e1f03d93697ac318731b1e80f0e9d41d5784394c [formerly 0df0aa866343c37f4aa6513ac0eeaa7456509182]]
    Former-commit-id: e1f03d93697ac318731b1e80f0e9d41d5784394c
    Former-commit-id: 9542a63b903ee9ff1033eda075f4809cb2964d70 [formerly af92e8d329e6608d1c68e5bd67e2ab7781193304]
    Former-commit-id: 34653c3beb49b3fd1c0d2e2c5afe1d16b71b274f

---
+++ ncep/gov.noaa.nws.ncep.standalone/src/gov/noaa/nws/ncep/standalone/Perspective.java
@@ -0,0 +1,10 @@
+package gov.noaa.nws.ncep.standalone;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
