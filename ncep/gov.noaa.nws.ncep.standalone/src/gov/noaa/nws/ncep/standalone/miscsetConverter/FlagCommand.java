634def51626482618e4ca232d21037823133a391
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2012-05-10 17:02:18 -0500

    Issue #628 committing more PGEN code received from Plummer on 4-19-2012.
    Former-commit-id: dcfb950cfbb5986ad466d551cde1468b2511c063 [formerly 22a29b98b23bc0d07bb662963d9cf7e5267e8a93] [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463] [formerly 0df0aa866343c37f4aa6513ac0eeaa7456509182 [formerly a90d64e1fc26caafd6fc5ad6ef7e8f5e0c3e2463 [formerly bd658184b246452b5d3e2154d79f2179bdc12594]]]
    Former-commit-id: 0df0aa866343c37f4aa6513ac0eeaa7456509182
    Former-commit-id: a5aab1ab3583a86ff017397d59291cb56b659f40 [formerly e1f03d93697ac318731b1e80f0e9d41d5784394c]
    Former-commit-id: af92e8d329e6608d1c68e5bd67e2ab7781193304

---
+++ ncep/gov.noaa.nws.ncep.standalone/src/gov/noaa/nws/ncep/standalone/miscsetConverter/FlagCommand.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package gov.noaa.nws.ncep.standalone.miscsetConverter;
+
+/**
+ * @author bhebbard
+ *
+ */
+
+public interface FlagCommand {
+	public abstract String retrieve(FlagRecord tr);
+}
