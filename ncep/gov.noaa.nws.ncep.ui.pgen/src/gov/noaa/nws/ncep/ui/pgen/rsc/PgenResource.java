64774ccf3261db30215e6a3f1813526d287b9ff1
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2012-05-16 13:09:52 -0500

    Issue #628 changed NCEP PGEN PgenStaticDataProder.java to cause the staticdataprovider Activator to be loaded.  in PgenResource.drawGhost() check for dispatch target before drawing.
    Former-commit-id: b732df5f1cda3dacd515476791351950c54a6555 [formerly b4229b04a76044a10848460f01b2b500add537e1]
    Former-commit-id: 11b93412bf5eee08ec63d97505d0dd26bcccb49d

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/rsc/PgenResource.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/rsc/PgenResource.java
@@ -75,6 +75,7 @@ import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
 import com.raytheon.uf.viz.core.rsc.IResourceDataChanged;
 import com.raytheon.uf.viz.core.rsc.LoadProperties;
 import com.raytheon.uf.viz.core.rsc.ResourceList.RemoveListener;
+import com.raytheon.uf.viz.remote.graphics.DispatchGraphicsTarget;
 import com.raytheon.viz.core.gl.IGLTarget;
 import com.raytheon.viz.ui.editor.AbstractEditor;
 import com.vividsolutions.jts.geom.Coordinate;
@@ -716,7 +717,10 @@ public class PgenResource extends AbstractVizResource<PgenResourceData,MapDescri
 	 */
 	private void drawGhost( IGraphicsTarget target, PaintProperties paintProps,
 			DisplayElementFactory df ){
-		
+        if (target instanceof DispatchGraphicsTarget) {
+            // Ensure ghosting is painted to our own target only
+            target = ((DispatchGraphicsTarget) target).getWrappedObject();
+        }
 		df.setLayerDisplayAttr( false, null, false );
 		
 		Iterator<DrawableElement> iterator = ghost.createDEIterator();
