69ff9675c422b52316e2cf318b2ca6e80815d753
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-05-31 16:05:25 -0500

    Issue #664 Got Hydro MultiPointResource drawing efficiently and working in shared display
    Change-Id: I940cfaab17edfdd84d6b09b5c93e30b8f075a822
    
    Former-commit-id: eab1b5d8918ae3efc484fa48d9e6af34f0501f97 [formerly eab1b5d8918ae3efc484fa48d9e6af34f0501f97 [formerly 1d9ccb6bd307ffc5acc04646f50abe30a191eb3d]] [formerly fde1e68df6f7d90e7b461adde3a1cfac3b885de7] [formerly d4dcc0e12eb0de077b698d6c5de36f5ea59f2072 [formerly d4dcc0e12eb0de077b698d6c5de36f5ea59f2072 [formerly 052cc812238f880a05be3c2d0c5300373b6263b8]] [formerly fde1e68df6f7d90e7b461adde3a1cfac3b885de7 [formerly fde1e68df6f7d90e7b461adde3a1cfac3b885de7 [formerly a375cddf31565d6584256d2ccbe4dbea5a5ec187]]]]
    Former-commit-id: d4dcc0e12eb0de077b698d6c5de36f5ea59f2072 [formerly d4dcc0e12eb0de077b698d6c5de36f5ea59f2072 [formerly 052cc812238f880a05be3c2d0c5300373b6263b8]]
    Former-commit-id: d4dcc0e12eb0de077b698d6c5de36f5ea59f2072
    Former-commit-id: f1db605b55c5864023ce0b0c0fa84445af35c9db [formerly 86f03dfdc32eb164bb43763ae1a84bf948a0e513]
    Former-commit-id: a42afb321e3e2aa2e3d07838e68449540707c6fc

--- cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/EditorSetup.java
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/editor/EditorSetup.java
@@ -20,26 +20,21 @@
 package com.raytheon.uf.viz.collaboration.ui.editor;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.swt.graphics.Rectangle;
 
-import com.raytheon.uf.common.status.IUFStatusHandler;
-import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.common.status.UFStatus.Priority;
 import com.raytheon.uf.viz.collaboration.display.editor.CollaborationEditor;
 import com.raytheon.uf.viz.collaboration.display.editor.SharedEditorData;
 import com.raytheon.uf.viz.collaboration.ui.rsc.CollaborationWrapperResource;
+import com.raytheon.uf.viz.collaboration.ui.rsc.CollaborationWrapperResourceData;
 import com.raytheon.uf.viz.core.IExtent;
 import com.raytheon.uf.viz.core.PixelExtent;
 import com.raytheon.uf.viz.core.drawables.AbstractRenderableDisplay;
 import com.raytheon.uf.viz.core.drawables.IDescriptor;
 import com.raytheon.uf.viz.core.drawables.IRenderableDisplay;
 import com.raytheon.uf.viz.core.drawables.ResourcePair;
-import com.raytheon.uf.viz.core.exception.VizException;
-import com.raytheon.uf.viz.core.map.MapDescriptor;
-import com.raytheon.uf.viz.core.maps.display.PlainMapRenderableDisplay;
+import com.raytheon.uf.viz.core.rsc.ResourceList;
 import com.raytheon.viz.ui.UiUtil;
 import com.raytheon.viz.ui.editor.AbstractEditor;
 import com.vividsolutions.jts.geom.Envelope;
@@ -63,9 +58,6 @@ import com.vividsolutions.jts.geom.Envelope;
 
 public class EditorSetup {
 
-    private static final transient IUFStatusHandler statusHandler = UFStatus
-            .getHandler(EditorSetup.class);
-
     /**
      * Extracts a SharedEditorData object from the editor passed in.
      * 
@@ -78,6 +70,26 @@ public class EditorSetup {
 
         IRenderableDisplay display = editor.getActiveDisplayPane()
                 .getRenderableDisplay();
+        if (display instanceof AbstractRenderableDisplay) {
+            AbstractRenderableDisplay clonedDisplay = ((AbstractRenderableDisplay) display)
+                    .cloneDisplay();
+            List<ResourcePair> toKeep = new ArrayList<ResourcePair>();
+            for (ResourcePair rp : display.getDescriptor().getResourceList()) {
+                if (rp.getResource() instanceof CollaborationWrapperResource) {
+                    CollaborationWrapperResource resource = (CollaborationWrapperResource) rp
+                            .getResource();
+                    ResourcePair copy = new ResourcePair();
+                    copy.setLoadProperties(rp.getLoadProperties());
+                    copy.setProperties(rp.getProperties());
+                    copy.setResourceData(resource.getWrapperResourceData());
+                    toKeep.add(copy);
+                }
+            }
+            ResourceList list = clonedDisplay.getDescriptor().getResourceList();
+            list.clear();
+            list.addAll(toKeep);
+            se.setDisplay(clonedDisplay);
+        }
         // extract grid geometry
         IDescriptor desc = display.getDescriptor();
         se.setGeometry(desc.getGridGeometry());
@@ -87,22 +99,6 @@ public class EditorSetup {
                 .getExtent();
         se.setEnvelope(new Envelope(extent.getMinX(), extent.getMaxX(), extent
                 .getMinY(), extent.getMaxY()));
-
-        List<ResourcePair> rscList = new ArrayList<ResourcePair>();
-
-        // extract map resources
-        Iterator<ResourcePair> itr = desc.getResourceList().iterator();
-        while (itr.hasNext()) {
-            ResourcePair rp = itr.next();
-            if (rp.getResource() instanceof CollaborationWrapperResource) {
-                ResourcePair copy = new ResourcePair();
-                copy.setLoadProperties(rp.getLoadProperties());
-                copy.setProperties(rp.getProperties());
-                copy.setResourceData(rp.getResource().getResourceData());
-                rscList.add(copy);
-            }
-        }
-        se.setLocalResources(rscList);
 
         // Set current size
         Rectangle bounds = display.getBounds();
@@ -122,29 +118,29 @@ public class EditorSetup {
     public static CollaborationEditor createEditor(SharedEditorData sharedEditor) {
         CollaborationEditor editor = null;
         AbstractRenderableDisplay[] displays = new AbstractRenderableDisplay[1];
-        try {
-            // TODO make it work with any IDescriptor and
-            // AbstractRenderableDisplay
-            MapDescriptor descriptor = new MapDescriptor(
-                    sharedEditor.getGeometry());
+        AbstractRenderableDisplay disp = sharedEditor.getDisplay();
+        PixelExtent extent = new PixelExtent(sharedEditor.getEnvelope()
+                .getMinX(), sharedEditor.getEnvelope().getMaxX(), sharedEditor
+                .getEnvelope().getMinY(), sharedEditor.getEnvelope().getMaxY());
+        disp.setExtent(extent);
 
-            // setup renderable display
-            PlainMapRenderableDisplay disp = new PlainMapRenderableDisplay(
-                    descriptor);
-            PixelExtent extent = new PixelExtent(sharedEditor.getEnvelope()
-                    .getMinX(), sharedEditor.getEnvelope().getMaxX(),
-                    sharedEditor.getEnvelope().getMinY(), sharedEditor
-                            .getEnvelope().getMaxY());
-            disp.setExtent(extent);
-
-            displays[0] = disp;
-            editor = (CollaborationEditor) UiUtil.createEditor(
-                    CollaborationEditor.EDITOR_ID, displays);
-            editor.setCanvasSize(new Rectangle(0, 0, sharedEditor.getWidth(),
-                    sharedEditor.getHeight()));
-        } catch (VizException e) {
-            statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
+        List<ResourcePair> toRemove = new ArrayList<ResourcePair>();
+        ResourceList list = disp.getDescriptor().getResourceList();
+        for (ResourcePair rp : list) {
+            if (rp.getResourceData() instanceof CollaborationWrapperResourceData) {
+                rp.setResourceData(((CollaborationWrapperResourceData) rp
+                        .getResourceData()).getWrappedResourceData());
+            } else {
+                toRemove.add(rp);
+            }
         }
+        list.removeAll(toRemove);
+
+        displays[0] = disp;
+        editor = (CollaborationEditor) UiUtil.createEditor(
+                CollaborationEditor.EDITOR_ID, displays);
+        editor.setCanvasSize(new Rectangle(0, 0, sharedEditor.getWidth(),
+                sharedEditor.getHeight()));
         return editor;
     }
 
