a2edb47d13266dc3f74708dd1c462e54b26ca293
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-06-05 16:55:52 -0500

    Issue #691 - All scripts, directories that used to include wes2bridge in the name have been renamed to edex-environment. The edex wes2bridge rpm has been renamed to the awips2-edex-environment rpm.
    Former-commit-id: 8366f5bffa7a0566dcb4c4ea1449b8d653dc5363 [formerly 3478f3089184c56b7b24db9cb424703027c84e4e] [formerly b8622629e395530d0446f412697cd5cb2c8be032] [formerly 8366f5bffa7a0566dcb4c4ea1449b8d653dc5363 [formerly 3478f3089184c56b7b24db9cb424703027c84e4e] [formerly b8622629e395530d0446f412697cd5cb2c8be032] [formerly c0db02e3775926c0fc6c2ed7e4a98f396f0a858d [formerly b8622629e395530d0446f412697cd5cb2c8be032 [formerly 478e494673d498121cab732717f3fc399a0b0c03]]]]
    Former-commit-id: c0db02e3775926c0fc6c2ed7e4a98f396f0a858d
    Former-commit-id: 1397ac4ae6b53366b84dded755683abb18849343 [formerly 733b34202f3ce1391087d3b4114364bee3900f61] [formerly b02340a5b6e6f0e27447ad5a1f532ed0a3b37d18 [formerly d92eb0c2278e2afd7b735120e7704330947b6d53]]
    Former-commit-id: a5cf683e790b615a5bad87dc4f842fa91d4710a1 [formerly 2b48d12f7ce7e9277c5f00b6fd45519fbb0dff9c]
    Former-commit-id: 89a427c1427794c87af9fdb77e5a1ed2949c3b72

--- rpms/awips2.edex-environment/wes2bridge.files/deliverables/scripts/functions/create.sh
+++ rpms/awips2.edex-environment/wes2bridge.files/deliverables/scripts/functions/create.sh
@@ -13,14 +13,14 @@ function createEnvironment()
    fi
    
    # Ensure that an environment does not already exist.
-   if [ -d ${WES2BRIDGE_DIR}/${env_name} ]; then
+   if [ -d ${EDEX_ENV_DIR}/${env_name} ]; then
       echo "ERROR: An edex environment with name = ${env_name} already exists."
       return 1
    fi
 
    # An environment does not exist. Create it.
    # 1) Create the root directory.
-   mkdir -p ${WES2BRIDGE_DIR}/${env_name}
+   mkdir -p ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Failed to create the ${env_name} environment root directory."
       return 1
@@ -28,37 +28,37 @@ function createEnvironment()
    
    # 2) Create the environment by copying components in /awips2.
    # /awips2/edex
-   cp -r /awips2/edex ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/edex ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place edex in the ${env_name} environment."
       return 1
    fi
    # /awips2/GFESuite
-   cp -r /awips2/GFESuite ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/GFESuite ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place GFESuite in the ${env_name} environment."
       return 1
    fi
    # /awips2/database
-   cp -r /awips2/database ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/database ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place the database in the ${env_name} environment."
       return 1
    fi
    # /awips2/data
-   cp -r /awips2/data ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/data ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place the database in the ${env_name} environment."
       return 1
    fi
    # /awips2/postgresql
-   cp -r /awips2/postgresql ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/postgresql ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place postgresql in the ${env_name} environment."
       return 1
    fi
    # /awips2/qpid
-   cp -r /awips2/qpid ${WES2BRIDGE_DIR}/${env_name}
+   cp -r /awips2/qpid ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       echo "ERROR: Unable to place qpid in the ${env_name} environment."
       return 1
@@ -66,7 +66,7 @@ function createEnvironment()
 
    # 3) Update the links in: data/pg_tblspc
    pushd . > /dev/null 2>&1
-   cd ${WES2BRIDGE_DIR}/${env_name}/data/pg_tblspc
+   cd ${EDEX_ENV_DIR}/${env_name}/data/pg_tblspc
    for link in `ls -1`; do
       linked_dir=`readlink ${link}`
       if [ $? -ne 0 ]; then
@@ -83,7 +83,7 @@ function createEnvironment()
          return 1
       fi
       # Create a new link to the correct location.
-      ln -sf ${WES2BRIDGE_DIR}/${env_name}/data/${data_dir} ${link}
+      ln -sf ${EDEX_ENV_DIR}/${env_name}/data/${data_dir} ${link}
       if [ $? -ne 0 ]; then
          return 1
       fi
@@ -91,7 +91,7 @@ function createEnvironment()
    popd > /dev/null 2>&1
 
    # 4) Create a directory for the environment-specific start (init.d) scripts.
-   mkdir -p ${WES2BRIDGE_DIR}/${env_name}/wes2bridge
+   mkdir -p ${EDEX_ENV_DIR}/${env_name}/edex-environment
    if [ $? -ne 0 ]; then
       exit 1
    fi
@@ -104,21 +104,21 @@ function createEnvironment()
    fi
 
    # 6) Adjust permissions.
-   chown -R awips:fxalpha ${WES2BRIDGE_DIR}/${env_name}
+   chown -R awips:fxalpha ${EDEX_ENV_DIR}/${env_name}
    if [ $? -ne 0 ]; then
       return 1
    fi
-   chmod -R 775 ${WES2BRIDGE_DIR}/${env_name}/edex/data
+   chmod -R 775 ${EDEX_ENV_DIR}/${env_name}/edex/data
    if [ $? -ne 0 ]; then
       return 1
    fi
 
    # 7) Lockdown the startup scripts.
-   chown root:root ${WES2BRIDGE_DIR}/${env_name}/wes2bridge/*
+   chown root:root ${EDEX_ENV_DIR}/${env_name}/edex-environment/*
    if [ $? -ne 0 ]; then
       return 1
    fi
-   chmod 644 ${WES2BRIDGE_DIR}/${env_name}/wes2bridge/*
+   chmod 644 ${EDEX_ENV_DIR}/${env_name}/edex-environment/*
    if [ $? -ne 0 ]; then
       return 1
    fi
