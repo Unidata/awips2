c0db02e3775926c0fc6c2ed7e4a98f396f0a858d
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-06-05 16:55:52 -0500

    Issue #691 - All scripts, directories that used to include wes2bridge in the name have been renamed to edex-environment. The edex wes2bridge rpm has been renamed to the awips2-edex-environment rpm.
    Former-commit-id: 478e494673d498121cab732717f3fc399a0b0c03

--- rpms/awips2.edex-environment/wes2bridge.files/deliverables/scripts/functions/list.sh
+++ rpms/awips2.edex-environment/wes2bridge.files/deliverables/scripts/functions/list.sh
@@ -3,7 +3,7 @@
 function listEnvironments()
 {
    pushd . > /dev/null 2>&1
-   cd ${WES2BRIDGE_DIR}
+   cd ${EDEX_ENV_DIR}
    echo "Existing EDEX Environment(s):"
    for env_dir in `ls -1`; do
       echo "   -${env_dir}"
