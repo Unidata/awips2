3f528cbd8545087e0fd5edfce86803f16776b542
Matt Nash <Matt_Nash@raytheon.com>
2012-06-11 10:24:35 -0500

    Issue #693 add nws_collaborate view, and ability for default on startup or not
    Former-commit-id: c3b17038f970923816bd41bcb3ab2c4e7a8bbcd1 [formerly 73da2b1fe8fe65b996508f2402732bc8f3b8e9d2] [formerly c3b17038f970923816bd41bcb3ab2c4e7a8bbcd1 [formerly 73da2b1fe8fe65b996508f2402732bc8f3b8e9d2] [formerly 022e982aae0d3de61ba5f8abdce48e1f035a736f [formerly b50cfeb8633e2b9a0d2fc7bb848bbcf7d0404321]]]
    Former-commit-id: 022e982aae0d3de61ba5f8abdce48e1f035a736f
    Former-commit-id: 945b47e29ec345f862d48db2f61ea96bfe57551c [formerly d5e3c97d983a176b3c66f8fd0d6386ff2a3c870a]
    Former-commit-id: 3a82f9690ba846b01286dd4ae4f5b1c29ec249db

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/prefs/CollaborationSubscribeListPreferences.java
@@ -0,0 +1,69 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.prefs;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 7, 2012            mnash     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mnash
+ * @version 1.0
+ */
+
+public class CollaborationSubscribeListPreferences extends
+        FieldEditorPreferencePage {
+
+    /**
+     * 
+     */
+    public CollaborationSubscribeListPreferences() {
+    }
+
+    /**
+     * @param style
+     */
+    public CollaborationSubscribeListPreferences(int style) {
+        super(GRID);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors
+     * ()
+     */
+    @Override
+    protected void createFieldEditors() {
+        // // TODO Auto-generated method stub
+        // CollaborationConnection.getConnection().getRosterManager().get
+    }
+}
