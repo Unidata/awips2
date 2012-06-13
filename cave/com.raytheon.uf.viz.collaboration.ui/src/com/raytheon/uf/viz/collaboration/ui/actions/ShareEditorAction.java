f0d2893e97ba5ed421966ff2093caa55e402d5c8
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-06-13 17:52:11 -0500

    Issue #697 added support for multiple editors
    Change-Id: Ic3ac12bb9368f0ec54dd8151ba1876002ff44e82
    
    Former-commit-id: 51c2a41e68be2201d4ea3a8ab0fc6991b0478010 [formerly dd99479eeb4a3bbd898fce4c0b258329e1a9eaa9] [formerly fceef3c5a149914c12e9eaea986631409fd33472 [formerly 3afc7b49cb42c005a0e9cf5221e35fc3ab78007a]]
    Former-commit-id: fceef3c5a149914c12e9eaea986631409fd33472
    Former-commit-id: e5e1a4de6a503da824621729d7047c42ee1fd977

--- cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/actions/ShareEditorAction.java
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/actions/ShareEditorAction.java
@@ -19,14 +19,31 @@
  **/
 package com.raytheon.uf.viz.collaboration.ui.actions;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.ActionContributionItem;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuCreator;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+
+import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.viz.collaboration.comm.identity.CollaborationException;
+import com.raytheon.uf.viz.collaboration.comm.identity.ISharedDisplaySession;
+import com.raytheon.uf.viz.collaboration.comm.identity.info.IVenueInfo;
+import com.raytheon.uf.viz.collaboration.display.data.SessionContainer;
+import com.raytheon.uf.viz.collaboration.display.data.SharedDisplaySessionMgr;
+import com.raytheon.uf.viz.collaboration.display.roles.dataprovider.SharedEditorsManager;
+import com.raytheon.uf.viz.collaboration.ui.Activator;
+import com.raytheon.viz.ui.EditorUtil;
 import com.raytheon.viz.ui.actions.ContributedEditorMenuAction;
+import com.raytheon.viz.ui.editor.AbstractEditor;
 
 /**
- * TODO Add Description
+ * Action to share an editor with a chosen session
  * 
  * <pre>
  * 
@@ -42,28 +59,135 @@ import com.raytheon.viz.ui.actions.ContributedEditorMenuAction;
  * @version 1.0
  */
 
-public class ShareEditorAction extends ContributedEditorMenuAction {
+public class ShareEditorAction extends ContributedEditorMenuAction implements
+        IMenuCreator {
 
-    @Override
-    public boolean shouldBeVisible() {
-        // TODO only return true if you are the DataProvider on a session AND
-        // the editor is not already shared
-        if (true) {
-            return true;
-        }
+    private Menu menu;
 
-        return true;
+    public ShareEditorAction() {
+        super("Share with", IAction.AS_DROP_DOWN_MENU);
     }
 
     @Override
-    public void run() {
-        // TODO need to add this editor to the list of editors associated with
-        // the collaboration session
-        // remove the message box and the not yet implemented message
-        MessageBox messageBox = new MessageBox(Display.getCurrent()
-                .getActiveShell(), SWT.ICON_INFORMATION);
-        messageBox.setText("Not Yet Implemented");
-        messageBox.setMessage("Share Editor is not yet implemented");
-        messageBox.open();
+    public boolean shouldBeVisible() {
+        return getActiveSharableEditor() != null && getSessions().size() > 0;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.action.IMenuCreator#dispose()
+     */
+    @Override
+    public void dispose() {
+        if (menu != null) {
+            menu.dispose();
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.jface.action.IMenuCreator#getMenu(org.eclipse.swt.widgets
+     * .Control)
+     */
+    @Override
+    public Menu getMenu(Control parent) {
+        if (menu != null) {
+            menu.dispose();
+        }
+
+        menu = new Menu(parent);
+
+        fillMenu(menu);
+        return menu;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.jface.action.IMenuCreator#getMenu(org.eclipse.swt.widgets
+     * .Menu)
+     */
+    @Override
+    public Menu getMenu(Menu parent) {
+        if (menu != null) {
+            menu.dispose();
+        }
+
+        menu = new Menu(parent);
+
+        fillMenu(menu);
+
+        return menu;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.action.Action#getMenuCreator()
+     */
+    @Override
+    public IMenuCreator getMenuCreator() {
+        return this;
+    }
+
+    /**
+     * @param menu2
+     */
+    private void fillMenu(Menu menu) {
+        final AbstractEditor editor = getActiveSharableEditor();
+        if (editor != null) {
+            List<ISharedDisplaySession> sessions = getSessions();
+            for (final ISharedDisplaySession session : sessions) {
+                final IVenueInfo sessionInfo = session.getVenue().getInfo();
+                ActionContributionItem aci = new ActionContributionItem(
+                        new Action(sessionInfo.getVenueDescription()) {
+                            @Override
+                            public void run() {
+                                try {
+                                    SharedEditorsManager.getManager(session)
+                                            .shareEditor(editor);
+                                } catch (CollaborationException e) {
+                                    Activator.statusHandler.handle(
+                                            Priority.PROBLEM,
+                                            e.getLocalizedMessage(), e);
+                                }
+                            }
+                        });
+                aci.fill(menu, -1);
+            }
+        }
+    }
+
+    private AbstractEditor getActiveSharableEditor() {
+        AbstractEditor editor = EditorUtil
+                .getActiveEditorAs(AbstractEditor.class);
+        if (editor != null
+                && SharedEditorsManager.isBeingShared(editor) == false) {
+            return editor;
+        }
+        return null;
+    }
+
+    private List<ISharedDisplaySession> getSessions() {
+        Collection<String> sessionIds = SharedDisplaySessionMgr
+                .getActiveSessionIds();
+        List<ISharedDisplaySession> sessions = new ArrayList<ISharedDisplaySession>();
+        for (String sessionId : sessionIds) {
+            SessionContainer container = SharedDisplaySessionMgr
+                    .getSessionContainer(sessionId);
+            if (container != null) {
+                ISharedDisplaySession session = container.getSession();
+                if (session != null
+                        && session.getUserID() == session
+                                .getCurrentDataProvider()) {
+                    sessions.add(container.getSession());
+                }
+            }
+        }
+        return sessions;
     }
 }
