fceef3c5a149914c12e9eaea986631409fd33472
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-06-13 17:52:11 -0500

    Issue #697 added support for multiple editors
    Change-Id: Ic3ac12bb9368f0ec54dd8151ba1876002ff44e82
    
    Former-commit-id: 3afc7b49cb42c005a0e9cf5221e35fc3ab78007a

---
+++ cave/com.raytheon.uf.viz.collaboration.display/src/com/raytheon/uf/viz/collaboration/display/data/SessionContainer.java
@@ -0,0 +1,112 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.display.data;
+
+import com.raytheon.uf.viz.collaboration.comm.identity.ISharedDisplaySession;
+import com.raytheon.uf.viz.collaboration.display.editor.ICollaborationEditor;
+import com.raytheon.uf.viz.collaboration.display.roles.IRoleEventController;
+
+/**
+ * A container holding an underlying session and associated data for a shared
+ * display session.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 16, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public class SessionContainer {
+
+    /** the session id **/
+    private String sessionId;
+
+    /** the underlying session object **/
+    private ISharedDisplaySession session;
+
+    /** subscribes to events related to the session based on role **/
+    private IRoleEventController roleEventController;
+
+    private SessionColorManager colorManager;
+
+    /**
+     * the editor associated with a session, only valid if not fulfilling the
+     * Data Provider role
+     **/
+    private ICollaborationEditor collaborationEditor;
+
+    public ISharedDisplaySession getSession() {
+        return session;
+    }
+
+    public void setSession(ISharedDisplaySession session) {
+        this.session = session;
+    }
+
+    public IRoleEventController getRoleEventController() {
+        return roleEventController;
+    }
+
+    public void setRoleEventController(IRoleEventController roleEventController) {
+        this.roleEventController = roleEventController;
+    }
+
+    public ICollaborationEditor getCollaborationEditor() {
+        return collaborationEditor;
+    }
+
+    public void setCollaborationEditor(ICollaborationEditor collaborationEditor) {
+        this.collaborationEditor = collaborationEditor;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    /**
+     * @return the colorManager
+     */
+    public SessionColorManager getColorManager() {
+        if (colorManager == null) {
+            colorManager = new SessionColorManager();
+        }
+        return colorManager;
+    }
+
+    /**
+     * @param colorManager
+     *            the colorManager to set
+     */
+    public void setColorManager(SessionColorManager colorManager) {
+        this.colorManager = colorManager;
+    }
+}
