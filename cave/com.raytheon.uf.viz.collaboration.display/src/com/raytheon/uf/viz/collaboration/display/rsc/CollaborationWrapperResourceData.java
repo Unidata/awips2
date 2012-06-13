30b132819a104dc26841ab36c71c1c80d492063f
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-06-13 17:52:11 -0500

    Issue #697 added support for multiple editors
    Change-Id: Ic3ac12bb9368f0ec54dd8151ba1876002ff44e82
    
    Former-commit-id: 869d7f1b6984341993f2e25510e7853db379620f [formerly 869d7f1b6984341993f2e25510e7853db379620f [formerly 51c2a41e68be2201d4ea3a8ab0fc6991b0478010]] [formerly dd99479eeb4a3bbd898fce4c0b258329e1a9eaa9] [formerly 869d7f1b6984341993f2e25510e7853db379620f [formerly 869d7f1b6984341993f2e25510e7853db379620f [formerly 51c2a41e68be2201d4ea3a8ab0fc6991b0478010]] [formerly dd99479eeb4a3bbd898fce4c0b258329e1a9eaa9] [formerly e5e1a4de6a503da824621729d7047c42ee1fd977 [formerly e5e1a4de6a503da824621729d7047c42ee1fd977 [formerly fceef3c5a149914c12e9eaea986631409fd33472]] [formerly dd99479eeb4a3bbd898fce4c0b258329e1a9eaa9 [formerly dd99479eeb4a3bbd898fce4c0b258329e1a9eaa9 [formerly 3afc7b49cb42c005a0e9cf5221e35fc3ab78007a]]]]]
    Former-commit-id: e5e1a4de6a503da824621729d7047c42ee1fd977 [formerly e5e1a4de6a503da824621729d7047c42ee1fd977 [formerly fceef3c5a149914c12e9eaea986631409fd33472]]
    Former-commit-id: 40ed7f97e5be396b437f46b8c2aeb9bdaf321487 [formerly a3b103162371c453e836d7ca11eea9376f55414d] [formerly 028c76d38acd1eec3713d4207ece1dfc151691b0 [formerly f0d2893e97ba5ed421966ff2093caa55e402d5c8]] [formerly e5e1a4de6a503da824621729d7047c42ee1fd977]
    Former-commit-id: 110fa601ce4b122a9417a7a69eb180b9031cec2c [formerly d763be2b4c27f37bcee166d3a98b86f556ad383e] [formerly 0591ec86198a0ccea2d54acba362889ff50cce78 [formerly f2e34fe03c85894ac1955f721f3fc370a507f0f0]]
    Former-commit-id: c93a993753a463de3155733429c72b0a517320ea [formerly 7bac48f6430ce54e6f1c40e705422929e77fbd7e]
    Former-commit-id: 09f9187c53861cfee31c05094936e3a67cb2c4a3

---
+++ cave/com.raytheon.uf.viz.collaboration.display/src/com/raytheon/uf/viz/collaboration/display/rsc/CollaborationWrapperResourceData.java
@@ -0,0 +1,128 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.display.rsc;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+import com.raytheon.uf.viz.core.drawables.IDescriptor;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.core.rsc.AbstractResourceData;
+import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
+import com.raytheon.uf.viz.core.rsc.LoadProperties;
+
+/**
+ * Collaboration resource data that wraps an existing resource data and
+ * constructs CollaborationWrapperResource which wraps the resource created by
+ * the wrapped resource data
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 10, 2012            mschenke     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mschenke
+ * @version 1.0
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+public class CollaborationWrapperResourceData extends AbstractResourceData {
+
+    @XmlElement
+    private AbstractResourceData wrappedResourceData;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.rsc.AbstractResourceData#construct(com.raytheon
+     * .uf.viz.core.rsc.LoadProperties,
+     * com.raytheon.uf.viz.core.drawables.IDescriptor)
+     */
+    @Override
+    public AbstractVizResource<?, ?> construct(LoadProperties loadProperties,
+            IDescriptor descriptor) throws VizException {
+        return new CollaborationWrapperResource(this, loadProperties,
+                wrappedResourceData.construct(loadProperties, descriptor));
+    }
+
+    public AbstractResourceData getWrappedResourceData() {
+        return wrappedResourceData;
+    }
+
+    /**
+     * @param wrappedResourceData
+     *            the wrappedResourceData to set
+     */
+    public void setWrappedResourceData(AbstractResourceData wrappedResourceData) {
+        this.wrappedResourceData = wrappedResourceData;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.rsc.AbstractResourceData#update(java.lang.Object
+     * )
+     */
+    @Override
+    public void update(Object updateData) {
+        wrappedResourceData.update(updateData);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        return wrappedResourceData.hashCode();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass()) {
+            return wrappedResourceData.equals(obj);
+        }
+        CollaborationWrapperResourceData other = (CollaborationWrapperResourceData) obj;
+        if (wrappedResourceData == null) {
+            if (other.wrappedResourceData != null)
+                return false;
+        } else if (!wrappedResourceData.equals(other.wrappedResourceData))
+            return false;
+        return true;
+    }
+
+}
