870972e3a9bf9b2d104bbcbc9f31249fd847cf19
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-06-27 16:22:53 -0500

    Issue #817 - fix ncep/gov.noaa.nws.ncep.metParameters and a few merge issues
    Change-Id: I54dc0ee5e4f9a6592bb0a3281a2bc6d806abff55
    
    Former-commit-id: 98fdfa6f3716b54429b10bed16244d9831332aff [formerly 1e06e23eca13e52cc159777e4fe73d214baf5222 [formerly f7d19019a92776c7370d1adbc64b65eb27545cd5] [formerly 98fdfa6f3716b54429b10bed16244d9831332aff [formerly b6990b5dc58e07fe7d5c85f8fa322723391193a4]]]
    Former-commit-id: 1e06e23eca13e52cc159777e4fe73d214baf5222 [formerly f7d19019a92776c7370d1adbc64b65eb27545cd5]
    Former-commit-id: 1e06e23eca13e52cc159777e4fe73d214baf5222
    Former-commit-id: 466046f60536bb4a72aa1d9d0a85021984e19aa1

---
+++ ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/InterWindSpeed.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package gov.noaa.nws.ncep.metparameters;
+
+
+import javax.measure.quantity.Velocity;
+
+/**
+ * @author archana
+ *
+ */
+public class InterWindSpeed extends AbstractMetParameter implements Velocity {
+              public InterWindSpeed ( ){
+            	  super ( UNIT );
+              }
+}
