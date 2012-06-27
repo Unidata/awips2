98fdfa6f3716b54429b10bed16244d9831332aff
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-06-27 16:22:53 -0500

    Issue #817 - fix ncep/gov.noaa.nws.ncep.metParameters and a few merge issues
    Change-Id: I54dc0ee5e4f9a6592bb0a3281a2bc6d806abff55
    
    Former-commit-id: b6990b5dc58e07fe7d5c85f8fa322723391193a4

---
+++ ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/Precip03Hr.java
@@ -0,0 +1,13 @@
+package gov.noaa.nws.ncep.metparameters;
+
+
+import javax.measure.unit.Unit;
+
+import com.raytheon.uf.common.dataplugin.IDecoderGettable.Amount;
+
+ public class Precip03Hr extends AbstractMetParameter implements javax.measure.quantity.Length{
+	 public  Precip03Hr ( ){
+     		 super( UNIT );
+          }
+
+  }
---
+++ ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/Precip18Hr.java
@@ -0,0 +1,9 @@
+package gov.noaa.nws.ncep.metparameters;
+
+
+public class Precip18Hr extends AbstractMetParameter implements javax.measure.quantity.Length{
+    public Precip18Hr() {
+		 super( UNIT );
+	}
+
+}
