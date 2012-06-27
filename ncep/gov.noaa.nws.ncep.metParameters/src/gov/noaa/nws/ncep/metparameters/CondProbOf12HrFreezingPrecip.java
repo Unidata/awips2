2ea8bed91e3d8a16e300c9eeed72051a3eacc950
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-06-27 16:22:53 -0500

    Issue #817 - fix ncep/gov.noaa.nws.ncep.metParameters and a few merge issues
    Change-Id: I54dc0ee5e4f9a6592bb0a3281a2bc6d806abff55
    
    Former-commit-id: f7d19019a92776c7370d1adbc64b65eb27545cd5 [formerly f7d19019a92776c7370d1adbc64b65eb27545cd5 [formerly b6990b5dc58e07fe7d5c85f8fa322723391193a4]]
    Former-commit-id: 98fdfa6f3716b54429b10bed16244d9831332aff
    Former-commit-id: 5874255a05473b9a83bf91c7c1d4ea02f65865a1

---
+++ ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/CondProbOf12HrFreezingPrecip.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gov.noaa.nws.ncep.metparameters;
+
+
+import javax.measure.quantity.Dimensionless;
+import javax.measure.unit.Unit;
+
+/**
+ * @author archana
+ *
+ */
+ public class CondProbOf12HrFreezingPrecip extends AbstractMetParameter implements
+		Dimensionless {
+
+	 public CondProbOf12HrFreezingPrecip() {
+	      super( UNIT );
+	}	 
+ }
---
+++ ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/Highest1MinMeanWindSpeedInPastHour.java
@@ -0,0 +1,16 @@
+package gov.noaa.nws.ncep.metparameters;
+
+
+import javax.measure.quantity.Velocity;
+import javax.measure.unit.Unit;
+
+import com.raytheon.uf.common.dataplugin.IDecoderGettable.Amount;
+
+  public class Highest1MinMeanWindSpeedInPastHour extends AbstractMetParameter
+		implements Velocity {
+
+	public Highest1MinMeanWindSpeedInPastHour() {
+		super( UNIT );
+	}
+	
+  }
