df316256ad59a7dd776d7dfb571ecfc1abb3adf9
Ron Anderson <ron.anderson@raytheon.com>
2012-07-05 16:32:44 -0500

    Issue #851 Cleaned up unnecessary Java imports in baselined tools/procedures.
    Change-Id: I49ad2815c2b524a63c115c1b2424b41831247d97
    
    Former-commit-id: e71059c2b9adc5fe6cf131ab7101f9144b4c3a29 [formerly d68ece4d55f744683918cce957b6129a206dff08 [formerly d3e0cd5af85731fc3f358472dd4a0d94153f5b02] [formerly e71059c2b9adc5fe6cf131ab7101f9144b4c3a29 [formerly ff032c53a059f25558aabdc5b483c4cc59ad3f6e]]]
    Former-commit-id: d68ece4d55f744683918cce957b6129a206dff08 [formerly d3e0cd5af85731fc3f358472dd4a0d94153f5b02]
    Former-commit-id: d68ece4d55f744683918cce957b6129a206dff08
    Former-commit-id: 9667367c3c3a466e9efb92976840cdabdc05144b

--- cave/build/static/common/cave/etc/gfe/userPython/gfeConfig/imageTest1.py
+++ cave/build/static/common/cave/etc/gfe/userPython/gfeConfig/imageTest1.py
@@ -22,6 +22,8 @@
 from gfeConfig import *
 import gfeConfig
 
+HideConfigFile = 1
+
 # Defines the T as the displayable weather element
 Png_parms = ['T_SFC:_Fcst -1', 'Td_SFC:_Fcst -1', 'Wind_SFC:_Fcst -1']
 Png_image = 'T'
