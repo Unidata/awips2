14018f6213b0780f9ffe79eb9a63f5acbeeab9b7
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2012-07-10 15:43:54 -0500

    Issue #772 switch satellite from offscreen rendering to mosaic for a more well defined graphics API. Change-Id: If293a65f577f13f6ba2ccae41b100dd9542d6998
    Former-commit-id: 38fe2039c1f9a9ca2c35806d0e179f761a3e6c17 [formerly 38fe2039c1f9a9ca2c35806d0e179f761a3e6c17 [formerly 5bf019e1bb8cfbbd8c6154fad2fd13ef08bd93e9]] [formerly 74a69d1fb9dbaac9da2061ef4c85fc32f6a36fac] [formerly 2d632e1c4a159661e365df24d9cd47af3d49d88f [formerly 4182352f8c49ef7c4654123ae3341861778d2e76] [formerly 4182352f8c49ef7c4654123ae3341861778d2e76 [formerly 79ab850efa797828e90d59e479bca9c742859b07]] [formerly 74a69d1fb9dbaac9da2061ef4c85fc32f6a36fac [formerly 74a69d1fb9dbaac9da2061ef4c85fc32f6a36fac [formerly 009e769a180898cb62e9df4a604b40e139cf2886]]]]
    Former-commit-id: 2d632e1c4a159661e365df24d9cd47af3d49d88f [formerly 4182352f8c49ef7c4654123ae3341861778d2e76] [formerly 4182352f8c49ef7c4654123ae3341861778d2e76 [formerly 79ab850efa797828e90d59e479bca9c742859b07]]
    Former-commit-id: 2d632e1c4a159661e365df24d9cd47af3d49d88f [formerly 2d632e1c4a159661e365df24d9cd47af3d49d88f [formerly 4182352f8c49ef7c4654123ae3341861778d2e76]]
    Former-commit-id: 3d7e8a222b5ab55bb0c93f70dc9d51dff3d02593 [formerly 612e586a6c5beb17f09e8fd0af320f035f5718bb]
    Former-commit-id: 035db46327f296665e534f659069c8054e755517

---
+++ cave/com.raytheon.uf.viz.core/src/com/raytheon/uf/viz/core/drawables/ext/IMosaicMaxValImageExtension.java
@@ -0,0 +1,42 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.core.drawables.ext;
+
+/**
+ * A mosaicing extension that combines images by using the largest value for
+ * overlapped areas of multiple iamges.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jul 10, 2012            bsteffen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bsteffen
+ * @version 1.0
+ */
+
+public interface IMosaicMaxValImageExtension extends IMosaicImageExtension {
+
+}
