79b8579d43dd69914a67ec8108fd6c17cc5e78ef
Ron Anderson <ron.anderson@raytheon.com>
2012-07-11 17:10:27 -0500

    Issue #851 Fixed issues with DatabaseID.dateFormat
    Change-Id: Ia5d99932059cec78074096e9ef08fa7b90b13e72
    
    Former-commit-id: 1d80177029b13d20233858680dda8a53e16c8ba5 [formerly 1d80177029b13d20233858680dda8a53e16c8ba5 [formerly 21431b26938b0a4af94ffc787b5582abfcb15ab4]] [formerly 1f2e581534a3c8eb368c34693e1afa7eb3d71712] [formerly bc034e1863583bbbaed6283d29f778243ff0b54b [formerly bc034e1863583bbbaed6283d29f778243ff0b54b [formerly b52f1400ef8d46c9df484392f338648c6c911176]] [formerly 1f2e581534a3c8eb368c34693e1afa7eb3d71712 [formerly 1f2e581534a3c8eb368c34693e1afa7eb3d71712 [formerly 1816e3f6c96b1064d52a2cbdf75ae02fb27e3693]]]]
    Former-commit-id: bc034e1863583bbbaed6283d29f778243ff0b54b [formerly bc034e1863583bbbaed6283d29f778243ff0b54b [formerly b52f1400ef8d46c9df484392f338648c6c911176]]
    Former-commit-id: bc034e1863583bbbaed6283d29f778243ff0b54b
    Former-commit-id: 1fecc4bf9a7f46a322507a1e85dfc77ffd5a54bf [formerly e184dceb911655222fef3cfcc51e159450863326]
    Former-commit-id: eb3ef1d27bd8f39e93e9e30441109d1410322b3d

--- edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/db/objects/DatabaseID.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/db/objects/DatabaseID.java
@@ -74,7 +74,7 @@ public class DatabaseID implements Serializable, Comparable<DatabaseID>,
 
     public static final String MODEL_TIME_FORMAT = "yyyyMMdd_HHmm";
 
-    public static final SimpleDateFormat dateFormat = new SimpleDateFormat(
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat(
             MODEL_TIME_FORMAT);
 
     static {
@@ -150,7 +150,14 @@ public class DatabaseID implements Serializable, Comparable<DatabaseID>,
      */
     public DatabaseID(String siteId, DataType format, String dbType,
             String modelName, Date modelTime) {
-        this(siteId, format, dbType, modelName, dateFormat.format(modelTime));
+        this.siteId = siteId;
+        this.format = format;
+        this.dbType = dbType;
+        this.modelName = modelName;
+        synchronized (dateFormat) {
+            this.modelTime = dateFormat.format(modelTime);
+        }
+        encodeIdentifier();
     }
 
     /**
@@ -345,10 +352,15 @@ public class DatabaseID implements Serializable, Comparable<DatabaseID>,
     }
 
     private boolean decodeDtg(String dtgString) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmm");
+        if (dtgString == null
+                || dtgString.length() != MODEL_TIME_FORMAT.length()) {
+            return false;
+        }
         try {
-            Date date = sdf.parse(dtgString);
-            modelTime = sdf.format(date);
+            synchronized (dateFormat) {
+                dateFormat.parse(dtgString);
+            }
+            modelTime = dtgString;
         } catch (ParseException e) {
             return false;
         }
