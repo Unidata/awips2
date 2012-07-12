4fbdefff689524c5c58871393d5ffbbf6aa1cba6
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2012-07-12 13:35:52 -0500

    Issue #808 create API for managing floating views and made CaveDetachedWindow more robust.
    Former-commit-id: ea0c7046af014d8771776310b88e97edf4296595 [formerly ea0c7046af014d8771776310b88e97edf4296595 [formerly 1f9b6d2a0b7d45f68bda57c6fcaeec493cbb835a]]
    Former-commit-id: 8984b35ff82d156050e93f1ca95b718bfe3493e1
    Former-commit-id: 1c68f4e01e65c588c50adb454b54480d3da75d4d

--- cave/com.raytheon.viz.ui/src/com/raytheon/viz/ui/views/CaveDetachedWindow.java
+++ cave/com.raytheon.viz.ui/src/com/raytheon/viz/ui/views/CaveDetachedWindow.java
@@ -37,13 +37,13 @@ import org.eclipse.ui.IPropertyListener;
 import org.eclipse.ui.ISaveablePart;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IViewReference;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPartConstants;
 import org.eclipse.ui.IWorkbenchPartReference;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.contexts.IContextService;
 import org.eclipse.ui.internal.DetachedWindow;
 import org.eclipse.ui.internal.EditorManager;
+import org.eclipse.ui.internal.ILayoutContainer;
 import org.eclipse.ui.internal.IWorkbenchConstants;
 import org.eclipse.ui.internal.IWorkbenchHelpContextIds;
 import org.eclipse.ui.internal.LayoutPart;
@@ -56,6 +56,7 @@ import org.eclipse.ui.internal.dnd.DragUtil;
 import org.eclipse.ui.internal.dnd.IDragOverListener;
 import org.eclipse.ui.internal.dnd.IDropTarget;
 import org.eclipse.ui.internal.presentations.PresentationFactoryUtil;
+import org.eclipse.ui.presentations.IPresentablePart;
 import org.eclipse.ui.presentations.StackDropResult;
 
 /**
@@ -84,6 +85,13 @@ public class CaveDetachedWindow extends DetachedWindow implements
             // wasn't canceled
             e.doit = handleClose();
         }
+
+        @Override
+        public void shellActivated(ShellEvent e) {
+            page.getPartService().setActivePart(
+                    folder.getSelection().getPartReference());
+        }
+
     };
 
     private Listener resizeListener = new Listener() {
@@ -119,7 +127,111 @@ public class CaveDetachedWindow extends DetachedWindow implements
         this.page = workbenchPage;
 
         folder = new ViewStack(page, false, PresentationFactoryUtil.ROLE_VIEW,
-                null);
+                null) {
+
+            /**
+             * Override close because super.close expects to find the view in
+             * the page.
+             */
+            @Override
+            protected void close(IPresentablePart part) {
+                if (!presentationSite.isCloseable(part)) {
+                    return;
+                }
+
+                LayoutPart layoutPart = getPaneFor(part);
+                if (layoutPart != null && layoutPart instanceof PartPane) {
+                    PartPane viewPane = (PartPane) layoutPart;
+                    IViewReference ref = (IViewReference) viewPane
+                            .getPartReference();
+                    CaveWorkbenchPageManager.getInstance(page).hideView(ref);
+                }
+            }
+
+            /**
+             * Override paneDragStart so we can check if the stack is empty and
+             * close the window
+             */
+            @Override
+            public void paneDragStart(LayoutPart pane, Point initialLocation,
+                    boolean keyboard) {
+                super.paneDragStart(pane, initialLocation, keyboard);
+                if (pane == null) {
+                    // This means the whole stack is getting dragged:
+                    for (LayoutPart lpart : getChildren()) {
+                        checkChild(lpart);
+                    }
+                } else {
+                    checkChild(pane);
+                }
+            }
+
+            /**
+             * Use this after dragging to check if I still only my children,
+             * normally the page removes them but that doesn't work for
+             * CaveDetachedWindows so this removes it from the stack when
+             * someone else gets it.
+             * 
+             * @param pane
+             */
+            private void checkChild(LayoutPart pane) {
+                if (pane.getContainer() != this) {
+                    // the part does nto get properly dereferenced since it is
+                    // not part of the page
+                    ILayoutContainer newContainer = pane.getContainer();
+                    newContainer.remove(pane);
+                    remove(pane);
+                    newContainer.add(pane);
+                    if (this.getItemCount() == 0 && windowShell != null) {
+                        windowShell.dispose();
+                    }
+                }
+            }
+
+            /**
+             * Override add so we can change floating status
+             */
+            @Override
+            public void add(LayoutPart child, Object cookie) {
+                super.add(child, cookie);
+                setFloating(child, true);
+
+            }
+
+            /**
+             * Override remove so we can change floating status
+             */
+            @Override
+            public void remove(LayoutPart child) {
+                super.remove(child);
+                setFloating(child, false);
+            }
+
+            private void setFloating(LayoutPart child, boolean floating) {
+                if (child instanceof ViewPane) {
+                    ViewPane pane = (ViewPane) child;
+                    IViewPart view = pane.getViewReference().getView(false);
+                    if (view instanceof CaveFloatingView) {
+                        ((CaveFloatingView) view).setFloating(floating);
+
+                    }
+                }
+            }
+
+            /**
+             * Override set selection so we can fire appropriate activation
+             * listeners.
+             */
+            @Override
+            public void setSelection(LayoutPart part) {
+                super.setSelection(part);
+                if (part instanceof PartPane) {
+                    page.getPartService().setActivePart(
+                            ((PartPane) part).getPartReference());
+                }
+            }
+
+        };
         folder.addListener(propertyListener);
     }
 
@@ -180,6 +292,7 @@ public class CaveDetachedWindow extends DetachedWindow implements
 
     public void create() {
         windowShell = new Shell(Display.getCurrent(), SWT.SHELL_TRIM);
+        windowShell.addShellListener(shellListener);
         windowShell.setData(this);
         windowShell.setText("Detached Window");
         DragUtil.addDragTarget(windowShell, this);
@@ -215,33 +328,6 @@ public class CaveDetachedWindow extends DetachedWindow implements
     }
 
     /**
-     * Adds a visual part to this window. Supports reparenting.
-     */
-    public void add(ViewPane part) {
-
-        Shell shell = getShell();
-        if (shell != null) {
-            part.reparent(shell);
-        }
-        folder.add(part);
-        updateMinimumSize();
-    }
-
-    public boolean belongsToWorkbenchPage(IWorkbenchPage workbenchPage) {
-        return (this.page == workbenchPage);
-    }
-
-    public boolean close() {
-        hideViewsOnClose = false;
-        Shell shell = getShell();
-        if (shell != null && !shell.isDisposed()) {
-            shell.removeShellListener(shellListener);
-            shell.close();
-        }
-        return true;
-    }
-
-    /**
      * Closes this window and disposes its shell.
      */
     private boolean handleClose() {
@@ -262,7 +348,8 @@ public class CaveDetachedWindow extends DetachedWindow implements
 
                 // Only close if closable...
                 if (child.isCloseable()) {
-                    page.hideView(child.getViewReference());
+                    CaveWorkbenchPageManager.getInstance(page).hideView(
+                            child.getViewReference());
 
                     // Was the close cancelled?
                     if (child.getContainer() != null)
@@ -352,34 +439,36 @@ public class CaveDetachedWindow extends DetachedWindow implements
     public IDropTarget drag(Control currentControl, Object draggedObject,
             Point position, Rectangle dragRectangle) {
 
-        if (!(draggedObject instanceof PartPane)) {
+        if (!(draggedObject instanceof ViewPane)) {
             return null;
         }
 
-        final PartPane sourcePart = (PartPane) draggedObject;
+        ViewPane sourcePart = (ViewPane) draggedObject;
 
         if (sourcePart.getWorkbenchWindow() != page.getWorkbenchWindow()) {
             return null;
         }
 
-        // Only handle the event if the source part is acceptable to the
-        // particular PartStack
-        IDropTarget target = null;
-        if (sourcePart instanceof ViewPane) {
-            target = folder.getDropTarget(draggedObject, position);
-
-            if (target == null) {
-                Rectangle displayBounds = DragUtil.getDisplayBounds(folder
-                        .getControl());
-                if (displayBounds.contains(position)) {
-                    target = folder.createDropTarget(sourcePart,
-                            new StackDropResult(displayBounds, null));
-                } else {
-                    return null;
-                }
-            }
+        // If you put views in a CaveDetachedWindow then you can't find them
+        // using the workbenchPage, this is expected for FLoatingViews but
+        // puttiing other views in the detached window would cause API problems
+        // by unexpectedly losing views.
+        if (!(sourcePart.getViewReference().getPart(false) instanceof CaveFloatingView)) {
+            return null;
         }
 
+        IDropTarget target = folder.getDropTarget(draggedObject, position);
+
+        if (target == null) {
+            Rectangle displayBounds = DragUtil.getDisplayBounds(folder
+                    .getControl());
+            if (displayBounds.contains(position)) {
+                target = folder.createDropTarget(sourcePart,
+                        new StackDropResult(displayBounds, null));
+            } else {
+                return null;
+            }
+        }
         return target;
     }
 
@@ -444,35 +533,6 @@ public class CaveDetachedWindow extends DetachedWindow implements
     /**
      * @see IPersistablePart
      */
-    public void restoreState(IMemento memento) {
-        // Read the bounds.
-        Integer bigInt;
-        bigInt = memento.getInteger(IWorkbenchConstants.TAG_X);
-        int x = bigInt.intValue();
-        bigInt = memento.getInteger(IWorkbenchConstants.TAG_Y);
-        int y = bigInt.intValue();
-        bigInt = memento.getInteger(IWorkbenchConstants.TAG_WIDTH);
-        int width = bigInt.intValue();
-        bigInt = memento.getInteger(IWorkbenchConstants.TAG_HEIGHT);
-        int height = bigInt.intValue();
-        bigInt = memento.getInteger(IWorkbenchConstants.TAG_FLOAT);
-
-        // Set the bounds.
-        bounds = new Rectangle(x, y, width, height);
-        if (getShell() != null) {
-            getShell().setBounds(bounds);
-        }
-
-        // Create the folder.
-        IMemento childMem = memento.getChild(IWorkbenchConstants.TAG_FOLDER);
-        if (childMem != null) {
-            folder.restoreState(childMem);
-        }
-    }
-
-    /**
-     * @see IPersistablePart
-     */
     public void saveState(IMemento memento) {
         if (getShell() != null) {
             bounds = getShell().getBounds();
@@ -515,6 +575,10 @@ public class CaveDetachedWindow extends DetachedWindow implements
         }
 
         return Window.OK;
+    }
+
+    public void setBounds(Rectangle bounds) {
+        this.bounds = bounds;
     }
 
 }
