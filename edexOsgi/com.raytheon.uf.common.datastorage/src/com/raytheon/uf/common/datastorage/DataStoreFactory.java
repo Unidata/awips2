264413c407725b28909a37594f227ede2617dbb3
James Korman <james.korman@raytheon.com>
2012-07-13 10:04:26 -0500

    Issue #798 - Code review changes
    Issue #798 - Code review updates
    Change-Id: I5447574392a42c79fa4295ce4f9bb5ac295d06bb
    
    Former-commit-id: baa78f956c4af30e30af24bb0fe191d28879b597 [formerly 0df0cf8e1965c8ec088a363ec70514ed9ced93b1] [formerly 0df0cf8e1965c8ec088a363ec70514ed9ced93b1 [formerly 1166a7cbb15c013b6af16b4b16ead706c7b45a14]] [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae] [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 4cbbc59278b99dbd5e47e67b9d6fabdc9bfc819c]] [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae [formerly cb05a9f95384806537106dc804833331d6c7db48]]]]
    Former-commit-id: 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 4cbbc59278b99dbd5e47e67b9d6fabdc9bfc819c]]
    Former-commit-id: 5df52e38f98b3c5d7a3b8e8612c961d9d391d515
    Former-commit-id: 92a00c3aba5f6c8e902ff84c6f358c80a184b3d6 [formerly 16643cb1bc59f6f062a543fc11c6931d6a786813]
    Former-commit-id: 8308f3084f830e06f329b5346b29b47b522311f9

--- edexOsgi/com.raytheon.uf.common.datastorage/src/com/raytheon/uf/common/datastorage/DataStoreFactory.java
+++ edexOsgi/com.raytheon.uf.common.datastorage/src/com/raytheon/uf/common/datastorage/DataStoreFactory.java
@@ -43,12 +43,37 @@ import com.raytheon.uf.common.datastorage.records.StringDataRecord;
  * Feb 12, 2007             chammack    Initial Creation.
  * 20070914            379  jkorman     Added createStorageRecord factory methods.
  *                                      Refactored from HDFDataStore.
+ * - AWIPS2 Baseline Repository --------
+ * Jul 18, 2012        798 jkorman      Extracted methods {@link #createDataSetName}, {@link #createGroupName}, and
+ * {@link #isInterpolated} from various classes.
  * </pre>
  * 
  * @author chammack
  * @version 1
  */
 public class DataStoreFactory {
+
+    /**
+     * Default data set name; {@value #DEF_DATASET_NAME}.
+     */
+    public static final String DEF_DATASET_NAME = "Data";
+
+    /**
+     * Default interpolation suffix ({@value #DEF_INTERPOLATED_GROUP}) for
+     * interpolated groups.
+     */
+    public static final String DEF_INTERPOLATED_GROUP = "-interpolated";
+
+    /**
+     * Default group element separator; {@value #DEF_SEPARATOR}.
+     */
+    public static final String DEF_SEPARATOR = "/";
+
+    /**
+     * Base interpolation level. Any interpolation level greater than this value
+     * ({@value #BASE_LEVEL}) is considered to be a valid interpolation level.
+     */
+    public static final int BASE_LEVEL = 0;
 
     private static final DataStoreFactory instance = new DataStoreFactory();
 
@@ -160,4 +185,190 @@ public class DataStoreFactory {
         return record;
     }
 
+    /**
+     * Create a storage dataset name using the group and data set name and an
+     * interpolation level. Any interpolation level less than or equal to zero
+     * generates the base dataset name with no interpolation. Any interpolation
+     * levels greater than zero are considered to be decimated levels of the
+     * original data.
+     * <table>
+     * <tr>
+     * <th>base group</th>
+     * <th>dataset</th>
+     * <th>interpolation</th>
+     * <th>result</th>
+     * </tr>
+     * <tr>
+     * <td>null</td>
+     * <td>null</td>
+     * <td>-1</td>
+     * <td>/Data</td>
+     * </tr>
+     * </tr>
+     * <tr>
+     * <td>null</td>
+     * <td>null</td>
+     * <td>0</td>
+     * <td>/Data</td>
+     * </tr>
+     * <tr>
+     * <td>null</td>
+     * <td>null</td>
+     * <td>4</td>
+     * <td>/Data-interpolated/4</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>null</td>
+     * <td>0</td>
+     * <td>/data/group/Data</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>null</td>
+     * <td>3</td>
+     * <td>/data/group/Data-interpolated/3</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>dsname</td>
+     * <td>-1</td>
+     * <td>/data/group/dsname</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>dsname</td>
+     * <td>2</td>
+     * <td>/data/group/dsname-interpolated/2</td>
+     * </tr>
+     * </table>
+     * 
+     * @param groupName
+     *            The group name this data set belongs to. If null, an empty
+     *            group name is generated.
+     * @param baseDataSet
+     *            Data set name The dataset name. This name and the
+     *            {@link DEF_INTERPOLATED_GROUP} are used to create the
+     *            interpolated suffix for the group name. If null and
+     *            interpolation is requested, a default value
+     *            {@link DEF_DATASET_NAME} will be used.
+     * @param interpolatedLevel
+     *            The interpolation level data set numeric identifier.
+     * @return The generated fully qualified dataset name.
+     */
+    public static String createDataSetName(String groupName,
+            String baseDataSet, int interpolatedLevel) {
+        boolean interpolated = isInterpolated(interpolatedLevel);
+        StringBuilder interpolatedGroup = new StringBuilder(createGroupName(
+                groupName, baseDataSet, interpolated));
+
+        interpolatedGroup.append(DEF_SEPARATOR);
+        if (interpolated) {
+            interpolatedGroup.append(String.valueOf(interpolatedLevel));
+        } else {
+            if (baseDataSet != null) {
+                interpolatedGroup.append(baseDataSet);
+            } else {
+                interpolatedGroup.append(DEF_DATASET_NAME);
+            }
+        }
+        return interpolatedGroup.toString();
+    }
+
+    /**
+     * Create a hierarchical group name, given a base group name, a dataset name
+     * and if interpolated levels are being created. If interpolation is not
+     * requested then the base group name is returned unchanged or if null an
+     * empty string is returned. When interpolation is requested, the dataset
+     * name is appended with an interpolation identifer to create an
+     * interpolation level group name.
+     * <table>
+     * <tr>
+     * <th>base group</th>
+     * <th>dataset</th>
+     * <th>interpolation</th>
+     * <th>result</th>
+     * </tr>
+     * <tr>
+     * <td>null</td>
+     * <td>null</td>
+     * <td>false</td>
+     * <td>zero length string</td>
+     * </tr>
+     * <tr>
+     * <td>null</td>
+     * <td>null</td>
+     * <td>true</td>
+     * <td>/Data-interpolated</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>null</td>
+     * <td>false</td>
+     * <td>/data/group</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>null</td>
+     * <td>true</td>
+     * <td>/data/group/Data-interpolated</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>dsname</td>
+     * <td>false</td>
+     * <td>/data/group</td>
+     * </tr>
+     * <tr>
+     * <td>/data/group</td>
+     * <td>dsname</td>
+     * <td>true</td>
+     * <td>/data/group/dsname-interpolated</td>
+     * </tr>
+     * </table>
+     * 
+     * @param groupName
+     *            The base group name.
+     * @param baseDataSet
+     *            Data set name The dataset name. This name and the
+     *            {@link #DEF_INTERPOLATED_GROUP} are used to create the
+     *            interpolated suffix for the group name. If null and
+     *            interpolation is requested, a default value
+     *            {@link #DEF_DATASET_NAME} will be used.
+     * @param interpolatedLevel
+     *            Create an interpolated group name.
+     * @return The generated group name.
+     */
+    public static String createGroupName(String groupName, String baseDataSet,
+            boolean interpolated) {
+        StringBuilder interpolatedGroup = new StringBuilder();
+        if (groupName != null) {
+            interpolatedGroup.append(groupName);
+        }
+        if (interpolated) {
+            interpolatedGroup.append(DEF_SEPARATOR);
+            if (baseDataSet != null) {
+                if (baseDataSet.length() > BASE_LEVEL) {
+                    interpolatedGroup.append(baseDataSet);
+                } else {
+                    interpolatedGroup.append(DEF_DATASET_NAME);
+                }
+            } else {
+                interpolatedGroup.append(DEF_DATASET_NAME);
+            }
+            interpolatedGroup.append(DEF_INTERPOLATED_GROUP);
+        }
+        return interpolatedGroup.toString();
+    }
+
+    /**
+     * Is the specified interpolation greater than the {@link BASE_LEVEL}?
+     * 
+     * @param interpolatedLevel
+     *            An interpolation level.
+     * @return
+     */
+    public static boolean isInterpolated(int interpolatedLevel) {
+        return (interpolatedLevel > BASE_LEVEL);
+    }
 }
