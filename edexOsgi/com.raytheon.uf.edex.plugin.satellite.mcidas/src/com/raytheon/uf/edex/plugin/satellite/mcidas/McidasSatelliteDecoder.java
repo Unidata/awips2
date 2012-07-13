8848af54438b41e9443a0dea346e91858226367c
James Korman <james.korman@raytheon.com>
2012-07-13 10:04:26 -0500

    Issue #798 - Code review changes
    Issue #798 - Code review updates
    Change-Id: I5447574392a42c79fa4295ce4f9bb5ac295d06bb
    
    Former-commit-id: 0df0cf8e1965c8ec088a363ec70514ed9ced93b1 [formerly 0df0cf8e1965c8ec088a363ec70514ed9ced93b1 [formerly 1166a7cbb15c013b6af16b4b16ead706c7b45a14]] [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae] [formerly 0df0cf8e1965c8ec088a363ec70514ed9ced93b1 [formerly 0df0cf8e1965c8ec088a363ec70514ed9ced93b1 [formerly 1166a7cbb15c013b6af16b4b16ead706c7b45a14]] [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae] [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 4cbbc59278b99dbd5e47e67b9d6fabdc9bfc819c]] [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae [formerly 38a2e426d9324602707a0e8b6a6d7f380265c2ae [formerly cb05a9f95384806537106dc804833331d6c7db48]]]]]
    Former-commit-id: 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515 [formerly 4cbbc59278b99dbd5e47e67b9d6fabdc9bfc819c]]
    Former-commit-id: 264413c407725b28909a37594f227ede2617dbb3 [formerly 8308f3084f830e06f329b5346b29b47b522311f9] [formerly 92a00c3aba5f6c8e902ff84c6f358c80a184b3d6 [formerly 16643cb1bc59f6f062a543fc11c6931d6a786813]] [formerly 5df52e38f98b3c5d7a3b8e8612c961d9d391d515]
    Former-commit-id: c48182c18a48e50a9458e2c7cb5c69b55be2e50b [formerly 7a90d6858e65dff6ded932709bbea507c562e15a] [formerly 9292d07cd620f4aff64b758930a666bf25b92719 [formerly 41a80adfb1e297100f025fd1a6cfb72ddee58d90]]
    Former-commit-id: 8085fc734955ba58c89d489df613f00bc0c9cd0a [formerly c901590446b57df5ee0a274623fc4d961ab2d66e]
    Former-commit-id: f48e3c1643a2e10d701598dd65db4cb41497a679

--- edexOsgi/com.raytheon.uf.edex.plugin.satellite.mcidas/src/com/raytheon/uf/edex/plugin/satellite/mcidas/McidasSatelliteDecoder.java
+++ edexOsgi/com.raytheon.uf.edex.plugin.satellite.mcidas/src/com/raytheon/uf/edex/plugin/satellite/mcidas/McidasSatelliteDecoder.java
@@ -40,9 +40,11 @@ import com.raytheon.uf.edex.decodertools.time.TimeTools;
 import com.raytheon.uf.edex.plugin.satellite.mcidas.util.McidasSatelliteLookups;
 import com.raytheon.uf.edex.plugin.satellite.mcidas.util.McidasSatelliteLookups.PhysicalElementValue;
 
-/** McIDAS AREA Decoder
+/**
+ * McIDAS AREA Decoder
  * 
- * <p>Implemented:
+ * <p>
+ * Implemented:
  * <ul>
  * <li>Mercator projection</li>
  * <li>Multiple bands</li>
@@ -55,32 +57,53 @@ import com.raytheon.uf.edex.plugin.satellite.mcidas.util.McidasSatelliteLookups.
  * <li>Calibration block</li>
  * <li>Non-byte data types</li>
  * </ul>
+ * <pre>
+ * 
+ * OFTWARE HISTORY
+ *                   
+ * Date         Ticket#     Engineer    Description
+ * -----------  ----------  ----------- --------------------------
+ * No previous history
+ * - AWIPS2 Baseline Repository --------
+ * 07/12/2012    798        jkorman     Changed projection "magic" numbers 
+ * </pre>
+ * 
+ * @author
+ * @version
  */
 public class McidasSatelliteDecoder {
-    
+
     private static final IUFStatusHandler theHandler = UFStatus
-        .getHandler(McidasSatelliteDecoder.class);
+            .getHandler(McidasSatelliteDecoder.class);
+
     private static final String UNEXPECTED_HEADER_VALUE = "Unexpected value in format";
+
     private static final int EXPECTED_IMAGE_TYPE_LE = 4;
+
     private static final int EXPECTED_IMAGE_TYPE_BE = 0x04000000;
-    
-    private static final double HALFPI = Math.PI/2.;
-    private static final double RTD = 180./Math.PI;
-    private static final double DTR = Math.PI/180.;
-    
+
+    private static final double HALFPI = Math.PI / 2.;
+
+    private static final double RTD = 180. / Math.PI;
+
+    private static final double DTR = Math.PI / 180.;
+
     private SatelliteDao dao;
+
     private String traceId;
-    
+
     public McidasSatelliteDecoder() {
-        
+
     }
-    
-    public PluginDataObject[] decode(byte[] data, Headers headers) throws Exception {
+
+    public PluginDataObject[] decode(byte[] data, Headers headers)
+            throws Exception {
         traceId = (String) headers.get("traceId");
         try {
             return decodeMcidasArea(data);
         } catch (DecoderException e) {
-            // Any DecoderExceptions throw by this decoder do not need a stack trace
+            // Any DecoderExceptions throw by this decoder do not need a stack
+            // trace
             theHandler.error(e.getMessage(), e);
             return new PluginDataObject[0];
         }
@@ -89,8 +112,8 @@ public class McidasSatelliteDecoder {
     /**
      * References:<br/>
      * <ul>
-     *   <li> http://www.ssec.wisc.edu/mcidas/doc/prog_man/2006/formats-1.html </li>
-     *   <li> http://www.ssec.wisc.edu/mcidas/doc/misc_doc/area2.html </li>
+     * <li>http://www.ssec.wisc.edu/mcidas/doc/prog_man/2006/formats-1.html</li>
+     * <li>http://www.ssec.wisc.edu/mcidas/doc/misc_doc/area2.html</li>
      * </ul>
      * 
      * @param data
@@ -100,7 +123,7 @@ public class McidasSatelliteDecoder {
     private PluginDataObject[] decodeMcidasArea(byte[] data) throws Exception {
         ByteBuffer buf = ByteBuffer.wrap(data);
         buf.order(ByteOrder.LITTLE_ENDIAN);
-        
+
         // Decode the directory block
         if (buf.getInt() != 0)
             formatError(UNEXPECTED_HEADER_VALUE);
@@ -152,37 +175,36 @@ public class McidasSatelliteDecoder {
         buf.getInt(); // reserved
         /* int calibrationOffset = */buf.getInt();
         buf.getInt(); // comment cards
-        
+
         long bandBits = ((long) bandMap33to64 << 32) | bandMap1to32;
         if (nBands != Long.bitCount(bandBits))
             formatError("Specified number of bands does not match number of bits in band map");
 
         // Decode the navigation block
         buf.position(navBlockOffset);
-        SatMapCoverage coverage = decodeNavigation(elementResolution, lineResolution,
-                ulImageElement, ulImageLine,
-                nElementsPerLine, nLines, buf); 
-        
-        // Decode the data block, creating a SatelliteRecord for each band. 
+        SatMapCoverage coverage = decodeNavigation(elementResolution,
+                lineResolution, ulImageElement, ulImageLine, nElementsPerLine,
+                nLines, buf);
+
+        // Decode the data block, creating a SatelliteRecord for each band.
         PluginDataObject[] result = new PluginDataObject[nBands];
         int bitIndex = 0;
-        RECORD:
-        for (int ri = 0; ri < nBands; ++ri) {
+        RECORD: for (int ri = 0; ri < nBands; ++ri) {
             while ((bandBits & (1L << bitIndex)) == 0)
                 if (++bitIndex >= 64)
                     break RECORD; // shouldn't happen
-            
+
             SatelliteRecord rec = new SatelliteRecord();
             rec.setDataTime(new DataTime(unpackTime(yyyddd, hhmmss)));
             rec.setSource("McIDAS");
             rec.setCreatingEntity(getCreatingEntity(sensorSourceNumber));
-            PhysicalElementValue pev = getPhysicalElement(sensorSourceNumber, 
-                    bitIndex + 1); 
+            PhysicalElementValue pev = getPhysicalElement(sensorSourceNumber,
+                    bitIndex + 1);
             rec.setPhysicalElement(pev.name);
             rec.setUnits(pev.units);
             rec.setSectorID(getAreaName(areaNumber));
             rec.setCoverage(coverage);
-            
+
             // TODO: Line pad if not a multiple of four bytes
             if (linePrefixLength == 0 && nBytesPerElement == 1 && nBands == 1) {
                 byte[] imageBytes = new byte[nLines * nElementsPerLine];
@@ -202,8 +224,8 @@ public class McidasSatelliteDecoder {
                     }
                 }
             } else
-                unimplemented("non-byte elements");                
-            
+                unimplemented("non-byte elements");
+
             rec.setTraceId(traceId);
             rec.setPersistenceTime(TimeTools.getSystemCalendar().getTime());
             rec.setPluginName("satellite");
@@ -214,14 +236,14 @@ public class McidasSatelliteDecoder {
 
         return result;
     }
-    
+
     /**
-     * Reference: http://www.ssec.wisc.edu/mcidas/doc/prog_man/2006/formats-13a.html
+     * Reference:
+     * http://www.ssec.wisc.edu/mcidas/doc/prog_man/2006/formats-13a.html
      * 
      */
-    private SatMapCoverage decodeNavigation(int xImgRes, int yImgRes,
-            int ulX, int ulY,
-            int nx, int ny, ByteBuffer buf) throws Exception {
+    private SatMapCoverage decodeNavigation(int xImgRes, int yImgRes, int ulX,
+            int ulY, int nx, int ny, ByteBuffer buf) throws Exception {
         SatMapCoverage result = new SatMapCoverage();
         String navType = get4cc(buf);
         if (navType.equals("MERC")) {
@@ -231,121 +253,127 @@ public class McidasSatelliteDecoder {
             int spacingAtStdLatInMeters = buf.getInt();
             int nrmlLonDDMMSS = buf.getInt();
 
-            // NOTE: We do not check the following for compatibility with WGS84. 
+            // NOTE: We do not check the following for compatibility with WGS84.
             int radiusInMeters = buf.getInt();
-            /*int eccentricity = */buf.getInt();
-            /*boolean geodetic = */buf.getInt()/* >= 0*/;
-            
+            /* int eccentricity = */buf.getInt();
+            /* boolean geodetic = */buf.getInt()/* >= 0 */;
+
             boolean westPositive = buf.getInt() >= 0;
             float la1, lo1, la2, lo2;
-            
-            /* The following is based on
-             * gov.noaa.nws.ncep.edex.plugin.mcidas/src/gov/noaa/nws/ncep/edex/plugin/mcidas/decoder/McidasDecoder.java
+
+            /*
+             * The following is based on
+             * gov.noaa.nws.ncep.edex.plugin.mcidas/src
+             * /gov/noaa/nws/ncep/edex/plugin/mcidas/decoder/McidasDecoder.java
              */
-            
+
             double clon = flipLon(unpackDdmmss(nrmlLonDDMMSS), westPositive);
             double clat = unpackDdmmss(stdLatDDMMSS);
             double dx = spacingAtStdLatInMeters * xImgRes;
             double dy = spacingAtStdLatInMeters * yImgRes;
-            
+
             {
                 double phi0r = clat * DTR;
                 double rxp = ((double) (elementOfEquator - ulX) / xImgRes + 1.);
                 double ryp = (ny - (double) (lineOfEquator - ulY) / yImgRes);
-                
+
                 double dxp = 1. - rxp;
                 double dyp = 1. - ryp;
                 double rm = dx * dyp;
                 double rcos = radiusInMeters * Math.cos(phi0r);
-                double arg = Math.exp(rm/rcos);
-                la1 = (float) ((2. * Math.atan(arg) - HALFPI ) * RTD);
-                lo1 = (float) prnlon ((clon + ((dx * dxp) / rcos) * RTD));
-                dxp = nx -rxp;
-                dyp = ny -ryp;
+                double arg = Math.exp(rm / rcos);
+                la1 = (float) ((2. * Math.atan(arg) - HALFPI) * RTD);
+                lo1 = (float) prnlon((clon + ((dx * dxp) / rcos) * RTD));
+                dxp = nx - rxp;
+                dyp = ny - ryp;
                 rm = dx * dyp;
                 arg = Math.exp(rm / rcos);
-                la2 = (float) ((2. * Math.atan(arg) - HALFPI ) * RTD);
-                lo2 = (float) prnlon ((clon + ((dx * dxp) / rcos) * RTD));
-                lo2 = (float) prnlon (lo2);
+                la2 = (float) ((2. * Math.atan(arg) - HALFPI) * RTD);
+                lo2 = (float) prnlon((clon + ((dx * dxp) / rcos) * RTD));
+                lo2 = (float) prnlon(lo2);
             }
-            
-            result = SatSpatialFactory.getInstance().getMapCoverage(1, nx, ny, 
-                    (float) dx, (float) dy, (float) clon, (float) clat, 
-                    la1, lo1, la2, lo2);
+
+            result = SatSpatialFactory.getInstance().getMapCoverage(
+                    SatMapCoverage.PROJ_MERCATOR, nx, ny, (float) dx,
+                    (float) dy, (float) clon, (float) clat, la1, lo1, la2, lo2);
         } else
             unimplemented(String.format("navigation type \"%s\"", navType));
-        
+
         return result;
     }
-    
-    private static double prnlon (double lon) {       
-        double dlon = lon - (int)(lon / 360.f) * 360.f;
+
+    private static double prnlon(double lon) {
+        double dlon = lon - (int) (lon / 360.f) * 360.f;
         if (lon < -180.) {
             dlon = lon + 360.f;
-        }
-        else if (lon > 180.) {
+        } else if (lon > 180.) {
             dlon = (double) (lon - 360.);
         }
         return dlon;
     }
-    
+
     private static Calendar unpackTime(int yyyddd, int hhmmss) {
         Calendar cal = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
         cal.setTimeInMillis(0);
 
-        cal.set(Calendar.YEAR, + 1900 +  yyyddd / 1000);
-        cal.set(Calendar.DAY_OF_YEAR, yyyddd % 1000);        
-        
+        cal.set(Calendar.YEAR, +1900 + yyyddd / 1000);
+        cal.set(Calendar.DAY_OF_YEAR, yyyddd % 1000);
+
         int hh = hhmmss / 10000;
         cal.set(Calendar.HOUR_OF_DAY, hh);
         cal.set(Calendar.MINUTE, (hhmmss - hh * 10000) / 100);
-        cal.set(Calendar.SECOND, hhmmss % 100);       
-        
+        cal.set(Calendar.SECOND, hhmmss % 100);
+
         return cal;
     }
-    
+
     private static double unpackDdmmss(int ddmmss) {
         int dd = ddmmss / 10000;
         int mm = (ddmmss - dd * 10000) / 100;
         int ss = ddmmss % 100;
-        return dd + mm/60.0 + ss/3600.0; 
+        return dd + mm / 60.0 + ss / 3600.0;
     }
-    
+
     private static double flipLon(double lon, boolean flip) {
-        return flip ? - lon : lon;
+        return flip ? -lon : lon;
     }
-    
+
     private static String get4cc(ByteBuffer buf) {
         byte[] bytes = new byte[4];
         buf.get(bytes);
         return new String(bytes, Charset.forName("ISO-8859-1"));
     }
-    
+
     private String getCreatingEntity(int sensorSourceNumber) {
-        String value = McidasSatelliteLookups.getInstance().getCreatingEntity(sensorSourceNumber);
-        return value != null ? value : String.format("Unknown-%d", sensorSourceNumber);
+        String value = McidasSatelliteLookups.getInstance().getCreatingEntity(
+                sensorSourceNumber);
+        return value != null ? value : String.format("Unknown-%d",
+                sensorSourceNumber);
     }
 
     private PhysicalElementValue getPhysicalElement(int ssn, int bandIndex) {
-        PhysicalElementValue value = McidasSatelliteLookups.getInstance().getPhysicalElement(ssn, bandIndex);
-        return value != null ? value : 
-            new PhysicalElementValue(String.format("Unknown-%d", bandIndex), null);
+        PhysicalElementValue value = McidasSatelliteLookups.getInstance()
+                .getPhysicalElement(ssn, bandIndex);
+        return value != null ? value : new PhysicalElementValue(String.format(
+                "Unknown-%d", bandIndex), null);
     }
-    
+
     private String getAreaName(int areaNumber) {
-        String value = McidasSatelliteLookups.getInstance().getAreaName(areaNumber);
+        String value = McidasSatelliteLookups.getInstance().getAreaName(
+                areaNumber);
         return value != null ? value : String.format("AREA%04d", areaNumber);
     }
-    
+
     private void formatError(String message) throws DecoderException {
         throw new DecoderException(String.format("%s: %s", traceId, message));
     }
 
     protected void unimplemented(String feature) throws DecoderException {
-        throw new DecoderException(String.format("%s: unimplemented: %s", traceId, feature));
+        throw new DecoderException(String.format("%s: unimplemented: %s",
+                traceId, feature));
     }
 
-   /**
+    /**
      * @return the dao
      */
     public SatelliteDao getDao() {
@@ -353,7 +381,8 @@ public class McidasSatelliteDecoder {
     }
 
     /**
-     * @param dao the dao to set
+     * @param dao
+     *            the dao to set
      */
     public void setDao(SatelliteDao dao) {
         this.dao = dao;
