e5952c1c99cb156cd03f9f87b9c459213d8f2b76
Ron Anderson <ron.anderson@raytheon.com>
2012-08-07 10:01:11 -0500

    Issue #436 merging elevation base changes into branch off of development baseline.
    Former-commit-id: 93c632c1ff24334234a86c652a46f9d5045b6182 [formerly 1321db6a99b43e24b2117f4d7c12e019e7650fc8] [formerly 819b9c8e567a4bd6cd1e2e463a419f65912ff9a7 [formerly 9287cf98369abb8a674fd5bab99439d055218a05]]
    Former-commit-id: 819b9c8e567a4bd6cd1e2e463a419f65912ff9a7
    Former-commit-id: 6eafdda705bd7dc07560909b6f7d88ff2048488d

---
+++ cave/build/static/common/cave/etc/gfe/userPython/procedures/MakeHazard.py
@@ -0,0 +1,298 @@
+# ----------------------------------------------------------------------------
+# This software is in the public domain, furnished "as is", without technical
+# support, and with no warranty, express or implied, as to its usefulness for
+# any purpose.
+#
+# MakeHazard.py
+#
+# SOFTWARE HISTORY
+# Date         Ticket#    Engineer     Description
+# ------------ ---------- -----------  --------------------------
+#  Apr 03,2012 436        randerso     Converted to Python procedure to allow some
+#                                      level of site customization
+#  Apr 09,2012 436        randerso     Merged RNK's MakeHazards_Elevation procedure
+#
+# Author: randerso
+# ----------------------------------------------------------------------------
+
+# The MenuItems list defines the GFE menu item(s) under which the
+# Procedure is to appear.
+# Possible items are: Populate, Edit, Consistency, Verify, Hazards
+MenuItems = ["Hazards"]
+
+
+import SmartScript
+import time, string, sys
+import HazardUtils
+import re
+import numpy
+import LogStream
+import JUtil
+
+if sys.modules.has_key("MakeHazardConfig"):
+    sys.modules.__delitem__("MakeHazardConfig")
+import MakeHazardConfig
+
+class Procedure (SmartScript.SmartScript):
+    def __init__(self, dbss):
+        SmartScript.SmartScript.__init__(self, dbss)
+
+        self._dataManager = dbss
+        self._afterInit = 0  #flag indicating init is done.
+
+        self._tropicalHaz = ['HU.W','HU.A','HU.S','TR.W','TR.A']
+        self._natlBaseETN = 1001
+
+
+    def setUpUI(self):
+
+        args = {}
+        args['dataManager'] = self._dataManager
+        args['selectedTimeRange'] = self.selectedTimeRange
+        args['mapColor'] = MakeHazardConfig.mapColor
+        args['defaultMapWidth'] = MakeHazardConfig.defaultMapWidth
+        args['timeScaleEndTime'] = MakeHazardConfig.timeScaleEndTime
+        args['areaThreshold'] = MakeHazardConfig.areaThreshold
+        args['defaultHazardType'] = MakeHazardConfig.defaultHazardType
+        args['mapNames'] = MakeHazardConfig.mapNames
+        args['hazardDict'] = MakeHazardConfig.hazardDict
+        args['tcmList'] = MakeHazardConfig.tcmList
+        args['tropicalHaz'] = self._tropicalHaz
+        args['natlBaseETN'] = self._natlBaseETN
+        args['localEffectAreas'] = MakeHazardConfig.localEffectAreas
+        args['localAreaData'] = MakeHazardConfig.localAreaData
+
+        # create the Java/SWT dialog and open it
+        from com.raytheon.viz.gfe.makehazard import MakeHazardDialog
+        self.__dlg = MakeHazardDialog.createFromPython(
+            JUtil.pyValToJavaObj(args)
+        )
+        self.__dlg.openFromPython()
+        
+        # run the Java/SWT event loop
+        try:
+            dismiss = False
+            while not dismiss:
+                args = JUtil.javaObjToPyVal(self.__dlg.runFromPython(), converter)
+                dismiss = True;
+                # if args is None, then Cancel was pressed
+                if args is not None:
+                    # dismiss is True if the Run/Dismiss button is pressed,
+                    # false if Run is pressed
+                    dismiss = args["dismiss"]
+                    del args["dismiss"]
+                    
+                    if self.makeHazardGrid(**args) != 1:
+                        dismiss = False
+        finally:
+            # close the Java/SWT dialog when Cancelled, Dismissed or exception occurs
+            self.__dlg.closeFromPython()
+           
+    # RJM modified this routine from the HazardUtility file
+    # returns a Numeric mask where each zone in zoneList is set to 1
+    def _makeMask(self, zoneList, hazLocalEffect):
+
+        #  RJM had to modify this next line to point to the hazUtils
+        #  for the getGridSize routine.
+        gridSize = self._hazUtils._getGridSize()
+        mask = numpy.zeros(gridSize)
+        eaList = self.editAreaList()
+
+        #  Get the elevation from the GUI input.  We'll do this by clipping
+        #  of any numerical digits from the local effect.
+#        elevation_string = re.findall("\d+", hazLocalEffect)
+#        print "re elevation=", elevation_string, "xxx"
+#        try:
+#            elevation = elevation_string[0]
+#        except:
+#            elevation = "None"
+#        print "re elevation=", elevation, "xxx"
+        for z in zoneList:
+            print "in _makeMask processing zone ", z
+
+            if z in eaList:
+                zoneArea = self.getEditArea(z)
+                zoneMask = self.encodeEditArea(zoneArea)
+
+                #  Code added by RJM.  This checks to see if the local effect
+                #  area was specified and is a valid edit area.  If so,
+                #  make a mask from it, and then do an intersection with
+                #  the zone mask.
+                if hazLocalEffect in eaList:
+                    print "Masking",z,"with",hazLocalEffect
+                    localEffectArea = self.getEditArea(hazLocalEffect)
+                    localEffectMask = self.encodeEditArea(localEffectArea)
+                    zoneMask = numpy.logical_and(zoneMask, localEffectMask)
+
+                mask = numpy.logical_or(mask, zoneMask)
+#            else:
+#                if z in eaList:
+#                    zoneArea = self.getEditArea(z)
+#                    zoneMask = self.encodeEditArea(zoneArea)
+#                    mask = numpy.logical_or(mask, zoneMask)
+
+        return mask
+
+    # Creates the hazard grid based on the dialog input
+    def makeHazardGrid(self, selectedHazard, timeRange, areaList, segmentNumber, 
+                       selectedTimeRange, defaultAreaList, defaultHazard, defaultSegment, 
+                       hazLocalEffect):
+        siteID = self.getSiteID()
+        usingHazLocalEffect = (hazLocalEffect != 'None')
+
+        if len(areaList) == 0:
+            editArea = self.getActiveEditArea()
+            mask = self.encodeEditArea(editArea)
+        else:
+            # make the mask based on the list selections
+            if not usingHazLocalEffect:
+                mask = self._hazUtils._makeMask(areaList)
+            else:
+                mask = self._makeMask(areaList, hazLocalEffect)
+                
+        if usingHazLocalEffect:
+            # get the segment number and filter for valid characters
+            segNum = segmentNumber
+            
+            # get the hazards currently defined as temporary grids
+            hazParms = self.getHazardParmNames()
+            
+            # look through the list of grids and create a list of
+            # segment numbers (if any) that are already in use
+            # for the current hazard
+#            if len(hazParms) == 0:
+#                self.statusBarMsg("No temporary grids to merge.", "S")
+#                return 0
+            segList = []
+            print "selectedHazard=", selectedHazard
+            selectedPhen = selectedHazard[0:2]
+            selectedSig  = selectedHazard[3]
+            print "selectedPhen,selectedSig=", selectedPhen, ".", selectedSig
+            for hazParm in hazParms:
+                print "hazParm=", hazParm
+                trList = self._hazUtils._getWEInventory(hazParm)
+                for tr in trList:
+                    print "  tr=", tr, timeRange
+                    intersect_hours = tr.intersection(timeRange).duration()
+                    print "  intersect=", intersect_hours
+                    intersect_percent = intersect_hours / timeRange.duration() * 100.0
+                    print "  intersect %=", intersect_percent
+                phen = hazParm[3:5]
+                sig = hazParm[5:6]
+                print "phen,sig=", phen, ".", sig
+                if len(hazParm) > 6:
+                    if hazParm[6:].isdigit():
+                        seg = int(hazParm[6:])
+                        print "  seg=", seg
+                        if phen == selectedPhen and sig == selectedSig:
+                            segList.append(seg)
+                            print "appending ", seg
+                    else:
+                        seg = 0
+            segList.sort()
+
+#            print "looping through segList"
+#            for seg in segList:
+#                print "   seg=", seg," elev=", elevation
+#                if str(elevation) == str(seg):
+#                    print "adding 1 to elevation"
+#                    elevation += 1 
+#
+#            if elevation > 400:
+#                print "using elevation for segNum"
+#                segNum = elevation
+#                #  replace the segmentNumber field with the elevation +/- the Above/Below indicator.
+#                self.__dlg.setSegmentNumber(elevation)
+#                segmentNumber = str(elevation)
+#                print "*** segmentNumber=", segmentNumber
+            
+        index = string.find(selectedHazard, " ")
+        if index != -1:
+            selectedHazard = selectedHazard[0:index]
+        if len(segmentNumber) > 0:
+            hazardKey = selectedHazard + ":" + segmentNumber
+        else:
+            hazardKey = selectedHazard
+            
+        defaultHazKey = ""
+        if len(defaultSegment) > 0 and defaultHazard is not None:
+            defaultHazKey = defaultHazard + ":" + defaultSegment
+
+        weName = self._hazUtils._makeTempWEName(hazardKey)        
+
+        # if we're modifying, remove the old grid first
+        if defaultAreaList != [] and hazardKey == defaultHazKey:
+            self.deleteCmd([weName], self.selectedTimeRange)
+
+        # if we have no selection prevent user from making an empty hazard
+        if 1 not in mask:
+            self.statusBarMsg("NO EDIT AREA SELECTED:  \n Select area from map or load edit area in GFE!", "S")
+            return 0
+
+        self._hazUtils._addHazard(weName, timeRange, hazardKey, mask)
+        LogStream.logUse("Set: ", weName, 
+          self._hazUtils._printTime(timeRange.startTime().unixTime()), 
+          self._hazUtils._printTime(timeRange.endTime().unixTime()), hazardKey, 
+          self._hazUtils._printAreas(areaList))
+
+        return 1
+    
+    def getHazardParmNames(self):
+        # get the list of loaded temporary hazard parms
+        parms = self.loadedParms()
+        hazParms = []
+        for weName, level, dbID in parms:
+            if "haz" in weName:
+                key = self._hazUtils._tempWENameToKey(weName)
+                index = string.find(key, ":")
+                if index != -1:
+                    mkey = key[0:index]
+                    segNum = key[index+1:]
+                else:
+                    mkey = key
+                    segNum = ""
+
+                # append the hazard and a description
+                parmName = "haz" + key
+                parmName = string.replace(parmName, ".", "")
+                parmName = string.replace(parmName, ":", "")
+                hazParms.append(parmName)
+
+        return hazParms
+
+    def execute(self, timeRange):
+        #self._hazUtils = HazardUtils.HazardUtils(self._dataManager, self.eaMgr())
+        self._hazUtils = HazardUtils.HazardUtils(self._dataManager, None)
+        # save the selected timeRange
+        self.selectedTimeRange = timeRange
+        
+        self.setToolType("numeric")
+
+        # see if the Hazards WE is loaded in the GFE, if not abort the tool
+        if not self._hazUtils._hazardsLoaded():
+            self.statusBarMsg("Hazards Weather Element must be loaded in " + \
+              "the GFE before running MakeHazard", "S")
+            self.cancel()
+
+
+        # always separate the Hazards grid first
+        self._hazUtils._separateHazardGrids()
+
+        self.setUpUI()
+
+        self._afterInit = 1   #initialization done
+
+        return
+
+def converter(obj):
+    import AbsTime
+    import TimeRange
+    retVal = None
+
+    objtype = obj.jclassname
+    if objtype == "java.util.Date":
+        retVal = AbsTime.AbsTime(obj)
+    elif objtype == "com.raytheon.uf.common.time.TimeRange":
+        retVal = TimeRange.TimeRange(obj)
+    return retVal
+
---
+++ cave/build/static/common/cave/etc/gfe/userPython/utilities/MakeHazardConfig.py
@@ -0,0 +1,175 @@
+# ----------------------------------------------------------------------------
+# This software is in the public domain, furnished "as is", without technical
+# support, and with no warranty, express or implied, as to its usefulness for
+# any purpose.
+#
+# MakeHazard.py
+#
+# SOFTWARE HISTORY
+# Date         Ticket#    Engineer     Description
+# ------------ ---------- -----------  --------------------------
+#  Jul 10,2012 436        randerso     Separated configuration data from the 
+#                                      MakeHazard procedure
+#
+# Author: randerso
+# ----------------------------------------------------------------------------
+
+def sortHazardList(dict):
+    #sorts the entries in the menus in alphabetical order, returns sorted
+    #dictionary
+    import VTECTable
+    for ent in dict.keys():
+        values = dict[ent]
+        # get the descriptive word for this phen/sig
+        items = []
+        for v in values:
+            desc = VTECTable.VTECTable.get(v,'')
+            items.append((desc, v))
+        items.sort()   #sorts by description
+        #extract out the sorted phen/sig
+        phensig = []
+        for desc, v in items:
+            phensig.append(v)
+        dict[ent] = phensig
+
+    return dict
+
+# Lists of hazards organized by type in a dictionary
+# Set these to value you use for your site.  To minimize scrolling,
+# change the order so that the most common values your site uses are
+# near the front of each list.  The key is the menu entry on the 
+# Make Hazard dialog, the values are the key values for Hazards.
+hazardDict = {
+    'Winter Weather' : ["BZ.W", "BZ.A", "ZR.Y", 
+          "IS.W", "LE.Y", "LE.W", "LE.A",
+          "WC.Y", "WC.W", "WC.A", "WS.W", "WS.A", "WW.Y"],
+    'Hydrology' : ["FF.A", "FA.A"],
+    'Fire Weather' : ["FW.A", "FW.W"],
+    'Convective Watches' : ["SV.A", "TO.A"],
+    'Coastal Flood' : ["CF.S", "LS.S", "CF.Y", "CF.W", "CF.A", 
+          "SU.Y", "SU.W", "LS.Y", "LS.W", "LS.A", "RP.S"],
+    'Non-Precipitation' : ["AF.W", "AF.Y", "AQ.Y", "AS.O", "AS.Y", "DU.Y", 
+          "DS.W", "EH.W", "EH.A", "EC.W", "EC.A", "FG.Y", "FZ.W", "FZ.A", 
+          "HZ.W", "HZ.A", "ZF.Y", "FR.Y", "HT.Y", "HW.W", "HW.A", 
+          "LW.Y", "SM.Y", "WI.Y"],
+    'Marine' : ["MA.S", "MH.W", "MH.Y", "BW.Y", "UP.Y", "MF.Y",
+          "GL.A", "GL.W", "SE.A", "SE.W", "UP.A", "UP.W", "HF.A", "HF.W", "LO.Y", "SC.Y", "SW.Y", 
+          "RB.Y", "SI.Y", "MS.Y", "SR.A", "SR.W"],
+    'Tropical Cyclone' : ["HU.W", "HU.A", "HU.S", "TR.W", "TR.A"],
+    'Tsunami' : ["TS.A", "TS.W"],
+      
+    #'Local' : ["TEST"],  #example of adding local hazards
+    # you can define your own groups of hazards by adding new categories
+    }
+
+# for GUM use comment out the above definition and uncomment the one below
+
+#hazardDict = {
+#    'Hydrology' : ["FF.A", "FA.A"],
+#    'Fire Weather' : ["FW.A", "FW.W"],
+#    'Coastal Flood' : ["CF.S", "LS.S", "CF.Y", "CF.W", "CF.A", 
+#          "SU.Y", "SU.W", "LS.Y", "LS.W", "LS.A", "RP.S"],
+#    'Non-Precipitation' : ["AF.W", "AF.Y", "AQ.Y", "AS.O", "AS.Y", "DU.Y", 
+#          "DS.W", "EH.W", "EH.A", "EC.W", "EC.A", "FG.Y", "FZ.W", "FZ.A", 
+#          "HZ.W", "HZ.A", "ZF.Y", "FR.Y", "HT.Y", "HW.W", "HW.A", 
+#          "LW.Y", "SM.Y", "WI.Y"],
+#    'Marine' : ["MA.S", "MH.W", "MH.Y", "BW.Y", "UP.Y", "MF.Y",
+#          "GL.A", "GL.W", "SE.A", "SE.W", "UP.A", "UP.W", "HF.A", "HF.W", "LO.Y", "SC.Y", "SW.Y", 
+#          "RB.Y", "SI.Y", "MS.Y", "SR.A", "SR.W"],
+#    'Typhoon' : ["TY.A", "TY.W", "TR.A", "TR.W", "HU.S"],
+#    'Tsunami' : ["TS.A", "TS.W"],
+#      
+#    #'Local' : ["TEST"],  #example of adding local hazards
+#    # you can define your own groups of hazards by adding new categories
+#    }
+
+
+# This function sorts the hazards in the hazardDict by description.
+# Comment it out if this is not desired.
+hazardDict = sortHazardList(hazardDict)
+
+        # Dictionary of map categories and the map names.  The "<site>" is
+        # substituted with your site name.  The names of the map must match
+        # those defined in the ifpServer.   The keys in mapNames must match
+        # the keys in hazardDict.
+
+mapNames = {
+  'Fire Weather' :            ["FireWxZones_<site>"],
+  'Hydrology' :               ["Zones_<site>"],
+  'Coastal Flood':            ["Zones_<site>"],
+  'Convective Watches' :      ["Marine_Zones_<site>","FIPS_<site>"],
+  'Non-Precipitation' :       ["Zones_<site>"],
+  'Tropical Cyclone' :        ["Offshore_Marine_Zones_<site>",
+                               "Marine_Zones_<site>","Zones_<site>"],
+  'Typhoon' :                 ["Offshore_Marine_Zones_<site>",
+                               "Marine_Zones_<site>","Zones_<site>"],
+  'Tsunami' :                 ["Offshore_Marine_Zones_<site>",
+                               "Marine_Zones_<site>","Zones_<site>"],
+  'Winter Weather' :          ["Zones_<site>"],
+  'Marine' :                  ["Offshore_Marine_Zones_<site>",
+                               "Marine_Zones_<site>"],
+  #'Local' :     ["Zones_<site>"],   #example of adding local class
+  }
+
+# The defaultHazardType - selected when the tool is first run.  This
+# must be one of the categories (keys) in the mapNames and hazardDict.
+defaultHazardType = "Non-Precipitation"
+
+# this is the color for the selected areas in the map
+mapColor = "red"  # color of selected areas
+
+# initial map width
+defaultMapWidth = 400;
+
+# the percentage that an area must be covered to default to selected
+areaThreshold = 0.10
+
+# End time in hours of the time scales
+timeScaleEndTime = 96
+
+# Define the tropical product used to identify the particular storm
+tcmList = []  # Comment out for HLS sites
+        
+# Uncomment line below for Atlantic basin sites 
+#tcmList = ["TCMAT1", "TCMAT2", "TCMAT3", "TCMAT4", "TCMAT5"]
+
+# Uncomment line below for EPac basin sites 
+#tcmList = ["TCMEP1", "TCMEP2", "TCMEP3", "TCMEP4", "TCMEP5"]
+
+# Uncomment line below for CPac basin sites 
+#tcmList = ["TCMCP1", "TCMCP2", "TCMCP3", "TCMCP4", "TCMCP5"]
+
+# Dictionary mapping Hazard Types to applicable local effect areas 
+#    that can be intersected with the zone edit areas 
+localEffectAreas = {}
+
+#localEffectAreas = {
+#  'Winter Weather' : ["Below_1000","Below_1500","Below_2000","Below_2500","Below_3000","Below_3500","Below_4000",
+#                      "Above_1000","Above_1500","Above_2000","Above_2500","Above_3000","Above_3500"],
+#                   }
+
+# Dictionary associating local Effect Area names with a corresponding 
+#    segment number, display name, and list of zones to be auto-selected
+# If you do not wish to auto-select zones you should supply an empty list
+#
+# The display name allows you to display a "pretty" string in the UI rather 
+# than the edit area name. If the display name is empty ("") the edit area 
+# name will be used.
+localAreaData = {} 
+
+#localAreaData = { 
+#     "Below_1000" : ( 999, "", []),
+#     "Below_1500" : (1499, "", []),
+#     "Below_2000" : (1999, "", []),
+#     "Below_2500" : (2499, "", []),
+#     "Below_3000" : (2999, "", []),
+#     "Below_3500" : (3499, "", []),
+#     "Below_4000" : (3999, "", []),
+#     "Above_1000" : (1000, "", []),
+#     "Above_1500" : (1500, "", []),
+#     "Above_2000" : (2000, "", []),
+#     "Above_2500" : (2500, "", []),
+#     "Above_3000" : (3000, "", []),
+#     "Above_3500" : (3500, "", []),            
+#     }
+
