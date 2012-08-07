6507a97665adf8b6ba63577b65926d7e95b5492d
Jonathan Sanchez <jpsanchez1@raytheon.com>
2012-08-07 14:50:15 -0500

    Issue #883 Sorted warning records and prevented altered entries to be in the same frame as new entries.
    Former-commit-id: 5c8c0783c8e054792ac38b4487188495dd0b0319

--- cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WarningRecordComparator.java
+++ cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WarningRecordComparator.java
@@ -3,11 +3,12 @@ package com.raytheon.viz.warnings.rsc;
 import java.util.Comparator;
 
 import com.raytheon.uf.common.dataplugin.warning.AbstractWarningRecord;
+import com.raytheon.uf.common.dataplugin.warning.WarningRecord.WarningAction;
 
 /**
  * 
- * Sorts the WarningRecords by phenSig, ETN, starttime (descending order), then
- * action
+ * Compares the WarningRecords by phenSig, ETN, action, then starttime
+ * (descending order)
  * 
  * <pre>
  * 
@@ -24,6 +25,11 @@ import com.raytheon.uf.common.dataplugin.warning.AbstractWarningRecord;
  */
 public class WarningRecordComparator implements
         Comparator<AbstractWarningRecord> {
+
+    /**
+     * Compares the WarningRecords by phenSig, ETN, action, then starttime
+     * (descending order)
+     */
     public int compare(AbstractWarningRecord wr1, AbstractWarningRecord wr2) {
         int rval = 0;
 
@@ -33,9 +39,19 @@ public class WarningRecordComparator implements
             rval = Double.compare(Double.parseDouble(wr1.getEtn()),
                     Double.parseDouble(wr2.getEtn()));
             if (rval == 0) {
-                rval = -1 * wr1.getStartTime().compareTo(wr2.getStartTime());
-                if (rval == 0) {
+                if (wr1.getAct().equals(wr2.getAct())) {
+                    rval = 0;
+                } else if (wr1.getAct().equals(WarningAction.NEW.toString())) {
+                    rval = -1;
+                } else if (wr2.getAct().equals(WarningAction.NEW.toString())) {
+                    rval = 1;
+                } else {
                     rval = wr1.getAct().compareTo(wr2.getAct());
+                }
+
+                if (rval == 0) {
+                    rval = -1
+                            * wr1.getStartTime().compareTo(wr2.getStartTime());
                 }
             }
         }
