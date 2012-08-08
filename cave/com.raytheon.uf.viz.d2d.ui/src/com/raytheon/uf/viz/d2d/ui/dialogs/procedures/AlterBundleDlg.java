f8d05127035adacd80a60836db4c29df6de40365
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2012-08-08 13:35:47 -0500

    Issue #875 Markers and group added to Cave.
    Amend: Removed python file
    
    Amemd: Changes based on reviewer comments and additon of popup menus in the PontsMgrDlg.
    
    Amend: deleted unwanted .project file
    
    Amend: Removed comment out code and fix class descriptions
    
    Amend: Changes based on reviewer comments.
    
    Change-Id: I32c9dd754d1bfa06b470872b052b61e42adf11e8
    
    Former-commit-id: f4ae8eb1501aac6e7d267d411d361caf1ce02313 [formerly 2e3fc4b4000e25030ff3be4e74272471e81dd595 [formerly c8e5bf1a82979ebc2445acb88682e32ca8813f43]]
    Former-commit-id: 2e3fc4b4000e25030ff3be4e74272471e81dd595
    Former-commit-id: b3c357c96455396845753f63e7aacfa12e11f814

--- cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/dialogs/procedures/AlterBundleDlg.java
+++ cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/dialogs/procedures/AlterBundleDlg.java
@@ -20,6 +20,7 @@
 package com.raytheon.uf.viz.d2d.ui.dialogs.procedures;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -27,22 +28,28 @@ import java.util.Map.Entry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Layout;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.status.UFStatus.Priority;
 import com.raytheon.uf.viz.core.procedures.AlterBundleFactory;
 import com.raytheon.uf.viz.core.procedures.Bundle;
 import com.raytheon.uf.viz.core.procedures.IAlterBundleContributor;
 import com.raytheon.viz.ui.dialogs.CaveSWTDialog;
+import com.raytheon.viz.ui.widgets.MenuButton;
 
 /**
- * TODO Add Description
+ * Dialog for selecting an alternate bundle.
  * 
  * <pre>
  * 
@@ -54,6 +61,8 @@ import com.raytheon.viz.ui.dialogs.CaveSWTDialog;
  *                                      to bundle on Load. Prevents
  *                                      the window's 'x' close from
  *                                      trying to perform a load.
+ * Jul 31, 2012 #875       rferrel     Use MenuButton to organize entries
+ *                                      in  menus.
  * 
  * </pre>
  * 
@@ -61,6 +70,16 @@ import com.raytheon.viz.ui.dialogs.CaveSWTDialog;
  * @version 1.0
  */
 public class AlterBundleDlg extends CaveSWTDialog {
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(AlterBundleDlg.class);
+
+    private static final String Top_MENU_KEY = "<top>";
+
+    private static final String MENU_SEP = IAlterBundleContributor.MENU_SEPARATOR;
+
+    private static final String MI_SEP = IAlterBundleContributor.MI_SEPARATOR;
+
+    private static final int MENU_SEP_LEN = MENU_SEP.length();
 
     private static class AlterBundleEntry {
 
@@ -101,14 +120,17 @@ public class AlterBundleDlg extends CaveSWTDialog {
     @Override
     protected void initializeComponents(Shell shell) {
         Composite mainComposite = new Composite(shell, SWT.NONE);
-        GridLayout mainLayout = new GridLayout(3, false);
+        GridLayout mainLayout = new GridLayout(1, true);
         mainComposite.setLayout(mainLayout);
 
         initializeCombos(mainComposite);
         initializeBottomButtons(mainComposite);
     }
 
-    private void initializeCombos(Composite comp) {
+    private void initializeCombos(Composite parent) {
+        Composite comp = new Composite(parent, SWT.NONE);
+        GridLayout layout = new GridLayout(3, false);
+        comp.setLayout(layout);
         for (IAlterBundleContributor contrib : AlterBundleFactory
                 .getContributors()) {
             Map<String, String[]> alterables = contrib.getAlterables();
@@ -143,27 +165,13 @@ public class AlterBundleDlg extends CaveSWTDialog {
                 Label label = new Label(comp, SWT.CENTER);
                 label.setText(entry.getKey() + " = ");
 
-                final Combo valueCbo = new Combo(comp, SWT.DROP_DOWN);
-                gd = new GridData(SWT.CENTER, SWT.CENTER, false, true);
-                valueCbo.setLayoutData(gd);
-
-                String[] values = entry.getValue();
-                valueCbo.setItems(values);
-                if (values.length > 0) {
-                    valueCbo.select(0);
-                }
-
                 final AlterBundleEntry abe = new AlterBundleEntry();
                 abe.contributor = contrib;
                 abe.alterKey = entry.getKey();
-                if (values.length > 0) {
-                    abe.alterValue = valueCbo.getItem(valueCbo
-                            .getSelectionIndex());
-                }
                 entries.add(abe);
                 contribEntries.add(abe);
 
-                valueCbo.addSelectionListener(new SelectionAdapter() {
+                SelectionListener listener = new SelectionAdapter() {
                     /*
                      * (non-Javadoc)
                      * 
@@ -173,10 +181,19 @@ public class AlterBundleDlg extends CaveSWTDialog {
                      */
                     @Override
                     public void widgetSelected(SelectionEvent e) {
-                        abe.alterValue = valueCbo.getItem(valueCbo
-                                .getSelectionIndex());
+                        MenuButton menuButton = (MenuButton) e.getSource();
+                        String value = menuButton.getSelectedItem().getText();
+                        abe.alterValue = value;
                     }
-                });
+                };
+                MenuButton menuButton = new MenuButton(comp);
+                gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+                menuButton.setLayoutData(gd);
+                menuButton.addSelectionListener(listener);
+                Menu menu = new Menu(menuButton);
+                MenuItem topMi = createMenu(menu, entry);
+                menuButton.setMenu(menu);
+                menuButton.setSelectedItem(topMi);
 
                 ++i;
             }
@@ -184,14 +201,95 @@ public class AlterBundleDlg extends CaveSWTDialog {
         }
     }
 
-    private void initializeBottomButtons(Composite comp) {
-        Composite buttonComp = new Composite(comp, SWT.NONE);
-        GridData gd = new GridData(SWT.DEFAULT, SWT.DEFAULT, true, true);
-        gd.horizontalSpan = 3;
-        buttonComp.setLayoutData(gd);
+    private MenuItem createMenu(Menu topMenu, Entry<String, String[]> entry) {
+        Map<String, Menu> menuMap = new HashMap<String, Menu>();
 
+        menuMap.put(Top_MENU_KEY, topMenu);
+
+        Menu menu = null;
+        MenuItem mi = null;
+        MenuItem topMi = null;
+
+        for (String value : entry.getValue()) {
+            if (value.startsWith(MENU_SEP)) {
+                value = value.substring(MENU_SEP_LEN);
+            }
+
+            if (!value.contains(MENU_SEP)) {
+                menu = menuMap.get(Top_MENU_KEY);
+                if (value.equals(MI_SEP)) {
+                    new MenuItem(menu, SWT.SEPARATOR);
+                } else {
+                    mi = new MenuItem(menu, SWT.PUSH);
+                    mi.setText(value);
+                    if (topMi == null) {
+                        topMi = mi;
+                    }
+                }
+            } else if (value.endsWith(MENU_SEP)) {
+                // create new sub menu here.
+                menu = menuMap.get(value);
+                if (menu == null) {
+                    String[] vals = parseNameParentKey(value);
+                    String name = vals[0];
+                    String parentKey = vals[1];
+                    Menu parentMenu = menuMap.get(parentKey);
+                    if (parentMenu != null) {
+                        menu = new Menu(parentMenu);
+                        mi = new MenuItem(parentMenu, SWT.CASCADE);
+                        mi.setMenu(menu);
+                        mi.setText(name);
+                        menuMap.put(value, menu);
+                    } else {
+                        statusHandler.handle(Priority.DEBUG,
+                                "Attempting to recreate a menu: " + value);
+                    }
+                }
+            } else {
+                // Add item to the desired menu
+                String[] vals = parseNameParentKey(value);
+                String name = vals[0];
+                String parentKey = vals[1];
+                menu = menuMap.get(parentKey);
+                if (menu != null) {
+                    if (name.equals(MI_SEP)) {
+                        mi = new MenuItem(menu, SWT.SEPARATOR);
+                    } else {
+                        mi = new MenuItem(menu, SWT.NONE);
+                        mi.setText(name);
+                    }
+                } else {
+                    statusHandler.handle(Priority.DEBUG,
+                            "Attempting to add an item to non-existent menu: "
+                                    + value);
+                }
+            }
+        }
+        return topMi;
+    }
+
+    private String[] parseNameParentKey(String value) {
+        String[] vals = value.split(MENU_SEP);
+        String name = vals[vals.length - 1];
+        String parentKey = null;
+        if (vals.length == 1) {
+            parentKey = Top_MENU_KEY;
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < vals.length - 1; ++j) {
+                sb.append(vals[j]).append(MENU_SEP);
+            }
+            parentKey = sb.toString();
+        }
+        return new String[] { name, parentKey };
+    }
+
+    private void initializeBottomButtons(Composite parent) {
+        Composite buttonComp = new Composite(parent, SWT.NONE);
         GridLayout layout = new GridLayout(2, true);
         buttonComp.setLayout(layout);
+        GridData gd = new GridData(SWT.CENTER, SWT.DEFAULT, true, true);
+        buttonComp.setLayoutData(gd);
 
         loadBtn = new Button(buttonComp, SWT.PUSH);
         loadBtn.setText("Load");
