e67422ce9b91e565208ae99de672c5d912e8fec2
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2012-08-08 13:35:47 -0500

    Issue #875 Markers and group added to Cave.
    Amend: Removed python file
    
    Amemd: Changes based on reviewer comments and additon of popup menus in the PontsMgrDlg.
    
    Amend: deleted unwanted .project file
    
    Amend: Removed comment out code and fix class descriptions
    
    Amend: Changes based on reviewer comments.
    
    Change-Id: I32c9dd754d1bfa06b470872b052b61e42adf11e8
    
    Former-commit-id: 8da653ea5d374320d06795e043f34304cc22e899 [formerly 8da653ea5d374320d06795e043f34304cc22e899 [formerly b3c357c96455396845753f63e7aacfa12e11f814]] [formerly 2e3fc4b4000e25030ff3be4e74272471e81dd595] [formerly 8da653ea5d374320d06795e043f34304cc22e899 [formerly 8da653ea5d374320d06795e043f34304cc22e899 [formerly b3c357c96455396845753f63e7aacfa12e11f814]] [formerly 2e3fc4b4000e25030ff3be4e74272471e81dd595] [formerly ae41499ba24a5f896435f4b218a09dbb94bea3e1 [formerly ae41499ba24a5f896435f4b218a09dbb94bea3e1 [formerly f4ae8eb1501aac6e7d267d411d361caf1ce02313]] [formerly 2e3fc4b4000e25030ff3be4e74272471e81dd595 [formerly 2e3fc4b4000e25030ff3be4e74272471e81dd595 [formerly c8e5bf1a82979ebc2445acb88682e32ca8813f43]]]]]
    Former-commit-id: ae41499ba24a5f896435f4b218a09dbb94bea3e1 [formerly ae41499ba24a5f896435f4b218a09dbb94bea3e1 [formerly f4ae8eb1501aac6e7d267d411d361caf1ce02313]]
    Former-commit-id: c7f7ea61bab7c666b5da5e700ad7a0fb9c6b7764 [formerly 4c730e7b2e7640203a394650edd57b82458d15f2] [formerly 629ecaa78434e430a76a0b5ec5197ec2a3488674 [formerly 69f22fe6bb9ac04c91c920f94ea433323358c459]] [formerly ae41499ba24a5f896435f4b218a09dbb94bea3e1]
    Former-commit-id: c0c097894d922ecd919c8ea5d062ab74e9715056 [formerly 738fa33453adfa6eefd75686857468745cfeddf5] [formerly 8e96c8fe4ebd9d814ee07591831ae87928094d76 [formerly 4e356b8a13c3952c6304069c2819aea36df993c8]]
    Former-commit-id: e2e4f3f4850ac19b819283e018ba5c7905ebb06f [formerly 4e15803926d637cbeac6cc3d9242368dacd208e8]
    Former-commit-id: 3b51b6465a9fea77a9e8fab4002c73cdde60802e

---
+++ cave/com.raytheon.viz.awipstools/src/com/raytheon/viz/awipstools/ui/dialog/PointTreeViewerSorter.java
@@ -0,0 +1,74 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.viz.awipstools.ui.dialog;
+
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerSorter;
+
+import com.raytheon.uf.viz.points.data.IPointNode;
+
+/**
+ * Sort point nodes placing group nodes at the top of the list.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jul 31, 2012 #875       rferrel     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author rferrel
+ * @version 1.0
+ */
+
+public class PointTreeViewerSorter extends ViewerSorter {
+    @Override
+    public int compare(Viewer viewer, Object e1, Object e2) {
+        if (e1 == e2) {
+            return 0;
+        }
+        IPointNode node1 = (IPointNode) e1;
+        IPointNode node2 = (IPointNode) e2;
+        if (node1.isGroup()) {
+            if (!node2.isGroup()) {
+                return -1;
+            }
+        } else if (node2.isGroup()) {
+            return 1;
+        }
+
+        String s1 = null;
+        String s2 = null;
+
+        if (node1.isGroup()) {
+            s1 = node1.getGroup();
+            s2 = node2.getGroup();
+        } else {
+            s1 = node1.getName();
+            s2 = node2.getName();
+        }
+
+        // Both are either points or groups
+        return s1.compareToIgnoreCase(s2);
+    }
+}
