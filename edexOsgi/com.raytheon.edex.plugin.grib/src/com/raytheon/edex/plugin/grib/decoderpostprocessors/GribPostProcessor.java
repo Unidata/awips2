73466bae4486a97151fd99449086b151e5515781
root <root@awipscm.omaha.us.ray.com>
2012-08-15 22:15:20 +0000

    Merge 12.9.1-4 into development_on_ss_builds
    Former-commit-id: bb7b5341dd03a52186b64a46535866639bbc92d1 [formerly 0987fc6b3dbf2d46b4154002ab363c64b6d44901] [formerly cb46f578791f5b64d6706ae92cfd9c6b5ed375b1] [formerly bb7b5341dd03a52186b64a46535866639bbc92d1 [formerly 0987fc6b3dbf2d46b4154002ab363c64b6d44901] [formerly cb46f578791f5b64d6706ae92cfd9c6b5ed375b1] [formerly 5a453ee98401bfaa4480243b3165e8682ef4ff61 [formerly cb46f578791f5b64d6706ae92cfd9c6b5ed375b1 [formerly a178af491f4b4cd75ed1517f023354bab0b9c8c8]]]]
    Former-commit-id: 5a453ee98401bfaa4480243b3165e8682ef4ff61
    Former-commit-id: 791f3f5b8577ce31c1ee35a7ebb720102b549645 [formerly 537dbadf6497c8fc1ee4161d002d0a40eca77b13] [formerly 9fa79e8d9e9e82e29b8bae3cd42dbb3e0f9f0f26 [formerly 8a66b2f2092d4e9d2cbd07e7186f00374383b125]]
    Former-commit-id: 167e4e289205b778c1de6873be7610daf3a39f5f [formerly 1792158ef8bb9a7595252ab79fce6040fc1c4c82]
    Former-commit-id: 0eb9520e151055563e4beb636d87a0b013730e7c

--- edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/GribPostProcessor.java
+++ edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/GribPostProcessor.java
@@ -20,23 +20,20 @@
 
 package com.raytheon.edex.plugin.grib.decoderpostprocessors;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.raytheon.uf.common.dataplugin.grib.GribRecord;
 import com.raytheon.uf.common.dataplugin.grib.exception.GribException;
-import com.raytheon.uf.common.localization.IPathManager;
-import com.raytheon.uf.common.localization.LocalizationContext.LocalizationLevel;
-import com.raytheon.uf.common.localization.LocalizationContext.LocalizationType;
+import com.raytheon.uf.common.dataplugin.grib.util.GribModelLookup;
 import com.raytheon.uf.common.localization.PathManagerFactory;
+import com.raytheon.uf.common.serialization.SerializationException;
+import com.raytheon.uf.common.serialization.SerializationUtil;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.common.status.UFStatus.Priority;
 
 /**
  * An implementation to modify a grib record after the initial grid decoding if
@@ -56,147 +53,162 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
  * @version 1
  */
 public class GribPostProcessor {
-    private static final transient IUFStatusHandler statusHandler = UFStatus
-            .getHandler(GribPostProcessor.class);
+	private static final transient IUFStatusHandler statusHandler = UFStatus
+			.getHandler(GribPostProcessor.class);
 
-    /** The singleton instance */
-    private static GribPostProcessor instance;
+	/** The singleton instance */
+	private static GribPostProcessor instance;
 
-    /** The map containing the currently registered grib post processors */
-    private static Map<String, List<IDecoderPostProcessor>> processorMap = new HashMap<String, List<IDecoderPostProcessor>>();
+	/** The map containing the currently registered grib post processors */
+	private static Map<String, List<IDecoderPostProcessor>> processorMap = new HashMap<String, List<IDecoderPostProcessor>>();
 
-    /**
-     * Gets the singleton instance of GribPostProcessor
-     * 
-     * @return The singleton instance of GribPostProcessor
-     */
-    public static synchronized GribPostProcessor getInstance() {
-        if (instance == null) {
-            instance = new GribPostProcessor();
-        }
-        return instance;
-    }
+	private static final String CLASS_PREFIX = "com.raytheon.edex.plugin.grib.decoderpostprocessors.";
 
-    /**
-     * Creates a new GribPostProcessor instance
-     */
-    private GribPostProcessor() {
-        IPathManager pm = PathManagerFactory.getPathManager();
-        String processorFile = pm.getFile(
-                pm.getContext(LocalizationType.EDEX_STATIC,
-                        LocalizationLevel.BASE),
-                "/grib/postProcessModels/postProcessedModels.txt").getPath();
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new FileReader(processorFile));
-            String line = null;
-            String[] tokens = null;
-            while ((line = in.readLine()) != null) {
-                if (line.startsWith("#") || line.trim().isEmpty()) {
-                    continue;
-                }
-                tokens = line.split(":");
-                String model = tokens[0].trim();
-                IDecoderPostProcessor postProcessor = getPostProcessor(
-                        tokens[0].trim(), tokens[1].trim());
-                if (processorMap.containsKey(model)) {
-                    processorMap.get(model).add(postProcessor);
-                } else {
-                    ArrayList<IDecoderPostProcessor> processors = new ArrayList<IDecoderPostProcessor>();
-                    processors.add(postProcessor);
-                    processorMap.put(model, processors);
-                }
-            }
-            in.close();
-        } catch (IOException e) {
-            statusHandler.handle(Priority.PROBLEM,
-                    "Error reading post processed model file", e);
-        } catch (GribException e) {
-            statusHandler.handle(Priority.PROBLEM,
-                    "Error instantiating decoder post processor", e);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    statusHandler.handle(Priority.PROBLEM,
-                            "Error closing post processed model file", e);
-                }
-            }
-        }
-    }
+	/**
+	 * Gets the singleton instance of GribPostProcessor
+	 * 
+	 * @return The singleton instance of GribPostProcessor
+	 */
+	public static synchronized GribPostProcessor getInstance() {
+		if (instance == null) {
+			instance = new GribPostProcessor();
+		}
+		return instance;
+	}
 
-    /**
-     * Processes the GribRecords to determine if they need post processing
-     * 
-     * @param records
-     *            The records to examine
-     * @return The GribRecords including any new records created during the post
-     *         processing
-     * @throws GribException
-     */
-    public GribRecord[] process(GribRecord[] records) throws GribException {
-        List<IDecoderPostProcessor> processors;
-        GribRecord[] results = null;
-        List<GribRecord> additionalGrids = null;
-        for (int i = 0; i < records.length; i++) {
-            // Check the map to see if this grib record is part of a model for
-            // which post processing is necessary
-            processors = processorMap.get(records[i].getModelInfo()
-                    .getModelName());
-            if (processors != null) {
-                for (IDecoderPostProcessor processor : processors) {
-                    // Post processing is not necessary, so we continue
-                    if (processor == null) {
-                        continue;
-                    }
-                    // Post processing is necessary
-                    else {
-                        results = processor.process(records[i]);
-                        if (results.length == 0) {
-                            return results;
-                        }
-                        records[i] = results[0];
-                        if (results.length > 1) {
-                            if (additionalGrids == null) {
-                                additionalGrids = new ArrayList<GribRecord>();
-                            }
-                            for (int j = 1; j < results.length; j++) {
-                                additionalGrids.add(results[j]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        if (additionalGrids == null) {
-            return records;
-        } else {
-            for (int i = 0; i < records.length; i++) {
-                additionalGrids.add(records[i]);
-            }
-            return additionalGrids.toArray(new GribRecord[] {});
-        }
-    }
+	/**
+	 * Creates a new GribPostProcessor instance
+	 */
+	private GribPostProcessor() {
+		String processorFile = PathManagerFactory
+				.getPathManager()
+				.getStaticFile(
+						"/grib/postProcessModels/postProcessedModels.xml")
+				.getPath();
 
-    private IDecoderPostProcessor getPostProcessor(String modelName,
-            String processorClassName) throws GribException {
+		try {
+			// Get the list of available model names
+			Set<String> modelNames = GribModelLookup.getInstance()
+					.getModelNames();
 
-        for (List<IDecoderPostProcessor> processors : processorMap.values()) {
-            for (IDecoderPostProcessor processor : processors) {
-                if (processor.getClass().getCanonicalName()
-                        .equals(processorClassName)) {
-                    return processor;
-                }
-            }
-        }
-        try {
-            return (IDecoderPostProcessor) Class.forName(processorClassName)
-                    .newInstance();
-        } catch (Exception e) {
-            throw new GribException(
-                    "Error instantiating decoder post processor for "
-                            + modelName + " model.", e);
-        }
-    }
+			// Unmarshal the post processed model file
+			PostProcessedModelSet ppModelSet = (PostProcessedModelSet) SerializationUtil
+					.jaxbUnmarshalFromXmlFile(processorFile);
+
+			/*
+			 * Iterate over post processed models. Determine which models apply
+			 * to each post processor if a regex is present
+			 */
+			for (PostProcessedModel ppModel : ppModelSet.getModels()) {
+				for (String modelName : modelNames) {
+					if (modelName.matches(ppModel.getModelName())) {
+						List<IDecoderPostProcessor> processorInstances = processorMap
+								.get(modelName);
+						if (processorInstances == null) {
+							processorMap.put(modelName,
+									new ArrayList<IDecoderPostProcessor>());
+							processorInstances = processorMap.get(modelName);
+						}
+
+						for (String processor : ppModel.getProcessors()) {
+							try {
+								processorInstances
+										.add((IDecoderPostProcessor) Class
+												.forName(
+														CLASS_PREFIX
+																+ processor)
+												.newInstance());
+							} catch (InstantiationException e) {
+								statusHandler
+										.fatal("Error instantiating grib post processor!",
+												e);
+							} catch (IllegalAccessException e) {
+								statusHandler
+										.fatal("Error instantiating grib post processor!",
+												e);
+							} catch (ClassNotFoundException e) {
+								statusHandler
+										.info("Class ["
+												+ CLASS_PREFIX
+												+ processor
+												+ "] not found.  Trying to load class: ["
+												+ processor + "]");
+								try {
+									processorInstances
+											.add((IDecoderPostProcessor) Class
+													.forName(processor)
+													.newInstance());
+								} catch (Exception e1) {
+									statusHandler
+											.fatal("Error instantiating grib post processor!",
+													e1);
+								}
+							}
+
+						}
+
+					}
+				}
+			}
+		} catch (SerializationException e) {
+			statusHandler.fatal(
+					"Error unmarshalling post processed model list: "+processorFile, e);
+		}
+
+	}
+
+	/**
+	 * Processes the GribRecords to determine if they need post processing
+	 * 
+	 * @param records
+	 *            The records to examine
+	 * @return The GribRecords including any new records created during the post
+	 *         processing
+	 * @throws GribException
+	 */
+	public GribRecord[] process(GribRecord[] records) throws GribException {
+		List<IDecoderPostProcessor> processors;
+		GribRecord[] results = null;
+		List<GribRecord> additionalGrids = null;
+		for (int i = 0; i < records.length; i++) {
+			// Check the map to see if this grib record is part of a model for
+			// which post processing is necessary
+			processors = processorMap.get(records[i].getModelInfo()
+					.getModelName());
+			if (processors != null) {
+				for (IDecoderPostProcessor processor : processors) {
+					// Post processing is not necessary, so we continue
+					if (processor == null) {
+						continue;
+					}
+					// Post processing is necessary
+					else {
+						results = processor.process(records[i]);
+						if (results.length == 0) {
+							continue;
+						}
+						records[i] = results[0];
+						if (results.length > 1) {
+							if (additionalGrids == null) {
+								additionalGrids = new ArrayList<GribRecord>();
+							}
+							for (int j = 1; j < results.length; j++) {
+								additionalGrids.add(results[j]);
+							}
+						}
+					}
+				}
+			}
+		}
+		if (additionalGrids == null) {
+			return records;
+		} else {
+			GribRecord[] returnArray = new GribRecord[records.length+additionalGrids.size()];
+			System.arraycopy(records, 0, returnArray, 0, records.length);
+			for (int i = records.length; i < returnArray.length; i++) {
+				returnArray[i] = additionalGrids.get(i-records.length);
+			}
+			return returnArray;
+		}
+	}
 }
