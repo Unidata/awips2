b6b02aa7f9159cbd86244607c81533dde3e8768c
Jonathan Sanchez <jpsanchez1@raytheon.com>
2012-08-23 09:27:46 -0500

    Issue #1006 Implemented urban areas. Cherry picked from development on SS builds
    Conflicts:
    
    	edexOsgi/com.raytheon.uf.common.dataplugin.warning/src/com/raytheon/uf/common/dataplugin/warning/config/WarngenConfiguration.java
    
    Change-Id: Ife20e07ccffeb6e6f5cc4aabdc6c57c9ed6e9ff8
    
    Former-commit-id: a09fa0b6491be8237bfa025c3b81c31f28d61aff [formerly f646b185da0d81a21f88fc7b81293c9ae0c059f8] [formerly 1b45f1b6d9526c20ccd20b0ed2f6b54663f9cd53 [formerly a7f030f7ee05c60820c1ef82a5093b404e5e97a0]]
    Former-commit-id: 1b45f1b6d9526c20ccd20b0ed2f6b54663f9cd53
    Former-commit-id: 20c65a4677d37e6696ce5af4f08e2abff2b7226c

--- edexOsgi/com.raytheon.uf.common.dataplugin.warning/src/com/raytheon/uf/common/dataplugin/warning/config/AreaSourceConfiguration.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.warning/src/com/raytheon/uf/common/dataplugin/warning/config/AreaSourceConfiguration.java
@@ -21,11 +21,11 @@ import com.raytheon.uf.common.dataquery.requests.RequestableMetadataMarshaller;
  *   
  *    Date         Ticket#     Engineer    Description
  *    ------------ ----------  ----------- --------------------------
- *    Mar 29, 2012 #14691      Qinglu Lin  Added feAreaField and its getter and setter, etc. 
+ *    Mar 29, 2012 #14691      Qinglu Lin  Added feAreaField and its getter and setter, etc.
  * 
  * </pre>
  * 
- * @author 
+ * @author
  * @version 1
  */
 
@@ -37,7 +37,7 @@ public class AreaSourceConfiguration {
     }
 
     @XmlElement
-    private AreaType areaType = AreaType.INTERSECT;
+    private AreaType type = AreaType.HATCHING;
 
     /*
      * TODO This is for 12.9 to be backwards compatible. This will be removed in
@@ -100,7 +100,6 @@ public class AreaSourceConfiguration {
     }
 
     public AreaSourceConfiguration(AreaConfiguration areaConfig) {
-        setAreaType(AreaType.HATCHING);
         setVariable("areas");
         setAreaField(areaConfig.getAreaField());
         setAreaNotationField(areaConfig.getAreaNotationField());
@@ -254,14 +253,6 @@ public class AreaSourceConfiguration {
         this.includedWatchAreaBuffer = includedWatchAreaBuffer;
     }
 
-    public AreaType getAreaType() {
-        return areaType;
-    }
-
-    public void setAreaType(AreaType areaType) {
-        this.areaType = areaType;
-    }
-
     public String getVariable() {
         return variable;
     }
@@ -270,13 +261,21 @@ public class AreaSourceConfiguration {
         this.variable = variable;
     }
 
-	public String getTimeZoneField() {
-		return timeZoneField;
-	}
+    public String getTimeZoneField() {
+        return timeZoneField;
+    }
 
-	public void setTimeZoneField(String timeZoneField) {
-		this.timeZoneField = timeZoneField;
-	}
+    public void setTimeZoneField(String timeZoneField) {
+        this.timeZoneField = timeZoneField;
+    }
+
+    public AreaType getType() {
+        return type;
+    }
+
+    public void setType(AreaType type) {
+        this.type = type;
+    }
 
     public AreaType getAreaType() {
         return areaType;
