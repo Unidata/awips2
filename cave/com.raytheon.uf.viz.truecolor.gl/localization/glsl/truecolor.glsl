f7a72491314fead215125c2c18e4b611f29b0889
Max Schenkelberg <max.schenkelberg@raytheon.com>
2012-08-23 17:37:12 -0500

    Issue #28 Changed ResourceList so it can be extended to allow duplicates.  Removed deleted files from ISerializableObject files.  Made TileSetRenderable properly issue refresh when images were still being requested.  Made true color code work by only showing intersecting areas of all bands on screen. Made so shader files would be reread in if edited.
    Change-Id: Ib2ce27522c59f25ce1cef65e6d7665e28963f081
    
    Former-commit-id: 7f506ce81d36ec2a0a3aa9fd2309705482f9a5ff [formerly c4812b77455cc2fc6eaee483c158bedca2310363] [formerly 75b2cdff4784c9a70219bad5b6cf52f49d9a5fdf [formerly 291edbc6bd69815e003179e9ce708b6fdad254ad]]
    Former-commit-id: 75b2cdff4784c9a70219bad5b6cf52f49d9a5fdf
    Former-commit-id: 1f84ddbddf5610dbaecd8322c37b8c536ebb2d5b

--- cave/com.raytheon.uf.viz.truecolor.gl/localization/glsl/truecolor.glsl
+++ cave/com.raytheon.uf.viz.truecolor.gl/localization/glsl/truecolor.glsl
@@ -1,6 +1,9 @@
 #include <colorUtil>
 #include <indexing>
 
+// Multiplier used to store bit mask safely between 0-1
+const float maskMultiplier = 8.0;
+
 uniform sampler2D rawTex;
 uniform float naturalMin;
 uniform float naturalMax;
@@ -16,7 +19,11 @@ uniform int width;
 
 uniform float noDataValue;
 uniform float alphaStep;
-uniform int checkAlpha;
+uniform int expectedMask;
+
+int toBitMask(float alpha) {
+	return int((alpha * maskMultiplier) + 0.5);
+}
 
 float getIndex(sampler2D rawTex, float cmapMin, float cmapMax, float naturalMin, float naturalMax, int isFloat) {
 	vec4 textureValue = texture2D(rawTex, gl_TexCoord[0].st);
@@ -25,7 +32,7 @@ float getIndex(sampler2D rawTex, float cmapMin, float cmapMax, float naturalMin,
 		naturalVal = ((naturalVal * (naturalMax - naturalMin)) + naturalMin);
 	}
 	
-	float index = 0.0;
+	float index = -1.0;
 	if (naturalVal != noDataValue && naturalVal == naturalVal) {
 		index = findIndex(naturalVal, cmapMin, cmapMax);
 	}
@@ -34,28 +41,50 @@ float getIndex(sampler2D rawTex, float cmapMin, float cmapMax, float naturalMin,
 
 void main(void)
 {
-	vec2 xy = gl_FragCoord.xy;
-	vec4 imageVal = texture2D(rawTex,gl_TexCoord[0].st);
-	vec4 curVal = texture2D(trueColorTexture, vec2((xy.x / float(width)), (xy.y / float(height))));
-	
-	float r = curVal.r;
-	float g = curVal.g;
-	float b = curVal.b;
-	float a = curVal.a;
-	
-	float index = getIndex(rawTex, cmapMin, cmapMax, naturalMin, naturalMax, isFloat);
-	if ( band == 0 && index > r ) {
-		r = index;
-	} else if ( band == 1 && index > g ) {
-		g = index;
-	} else if ( band == 2 && index > b ) {
-		b = index;
+	if ( band == -1 ) {
+		vec4 imageVal = texture2D(rawTex,gl_TexCoord[0].st);
+		float r = imageVal.r;
+		float g = imageVal.g;
+		float b = imageVal.b;
+		float a = imageVal.a;
+		
+		// Round because of 8-bit floating point precision
+		int bitMask = toBitMask(a);
+		if (expectedMask > 0 && bitMask == expectedMask ) {
+			a = 1.0;
+		} else {
+			a = 0.0;
+		}
+		
+		gl_FragColor = vec4(r,g,b,a);
+	} else {
+		vec2 xy = gl_FragCoord.xy;
+		vec4 imageVal = texture2D(rawTex,gl_TexCoord[0].st);
+		vec4 curVal = texture2D(trueColorTexture, vec2((xy.x / float(width)), (xy.y / float(height))));
+		
+		float r = curVal.r;
+		float g = curVal.g;
+		float b = curVal.b;
+		float a = curVal.a;
+		
+		float index = getIndex(rawTex, cmapMin, cmapMax, naturalMin, naturalMax, isFloat);
+		if ( index != -1.0 ) {
+			int currentMask = toBitMask(a);
+			int bitValue = (1 << band);
+			if ( band == 0 && index > r ) {
+				r = index;
+			} else if ( band == 1 && index > g ) {
+				g = index;
+			} else if ( band == 2 && index > b ) {
+				b = index;
+			}
+			
+			if ( (currentMask & bitValue) == 0 ) {
+				// alpha does not contain this bit yet!
+				a = (currentMask | bitValue) / maskMultiplier;
+			}
+		}
+		
+		gl_FragColor = vec4(r,g,b,a);
 	}
-	a = a + alphaStep;
-	
-	if (checkAlpha == 1 && a < 1.0) {
-		a = 0.0;
-	}
-	
-	gl_FragColor = vec4(r,g,b,a);
 }
