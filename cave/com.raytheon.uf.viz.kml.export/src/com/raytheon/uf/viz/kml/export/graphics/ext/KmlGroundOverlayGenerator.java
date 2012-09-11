6f60eabacf7c133f114614d6f7c85807cf82311c
Steve Harris <Steven_L_Harris@raytheon.com>
2012-09-11 16:00:38 -0500

    12.10.1-3 baseline
    Former-commit-id: 438d139fdd9d17486f3af442e1e5285a0d9bf825 [formerly 969e7e6dfd6c74f2db042b869e609a61a71fc61f] [formerly 21f430062b5961e35660d0ee63a83d1b63e46810 [formerly 1a94c1ca3674c8e4dbb8847de5190efe33da371c]]
    Former-commit-id: 21f430062b5961e35660d0ee63a83d1b63e46810
    Former-commit-id: de1639aea2600f50bef36407ce33e2e5cdb438c9

---
+++ cave/com.raytheon.uf.viz.kml.export/src/com/raytheon/uf/viz/kml/export/graphics/ext/KmlGroundOverlayGenerator.java
@@ -0,0 +1,143 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.kml.export.graphics.ext;
+
+import java.awt.image.RenderedImage;
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.nio.FloatBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.keyvalue.MultiKey;
+import org.eclipse.swt.graphics.RGB;
+import org.geotools.coverage.grid.GridEnvelope2D;
+import org.geotools.coverage.grid.GridGeometry2D;
+import org.geotools.geometry.Envelope2D;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
+
+import com.raytheon.uf.common.geospatial.interpolation.BilinearInterpolation;
+import com.raytheon.uf.common.geospatial.interpolation.GridReprojection;
+import com.raytheon.uf.common.geospatial.interpolation.Interpolation;
+import com.raytheon.uf.common.geospatial.interpolation.NearestNeighborInterpolation;
+import com.raytheon.uf.common.geospatial.interpolation.data.DataSource;
+import com.raytheon.uf.common.geospatial.interpolation.data.FloatArrayWrapper;
+import com.raytheon.uf.viz.core.data.IColorMapDataRetrievalCallback.ColorMapData;
+import com.raytheon.uf.viz.core.data.prep.Colormapper;
+import com.raytheon.uf.viz.core.drawables.ColorMapParameters;
+import com.raytheon.uf.viz.kml.export.KmlFeatureGenerator;
+import com.raytheon.uf.viz.kml.export.io.KmlOutputManager;
+
+import de.micromata.opengis.kml.v_2_2_0.GroundOverlay;
+import de.micromata.opengis.kml.v_2_2_0.LatLonBox;
+
+/**
+ * Base class for any feature generator that is creating a ground overlay,
+ * provides the reproject logic as well as logic for actually making the
+ * overlay.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 14, 2012            bsteffen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bsteffen
+ * @version 1.0
+ */
+
+public abstract class KmlGroundOverlayGenerator extends KmlFeatureGenerator {
+
+    private static Map<MultiKey, Reference<GridReprojection>> reprojCache = new HashMap<MultiKey, Reference<GridReprojection>>();
+
+    protected final double alpha;
+
+    public KmlGroundOverlayGenerator(double alpha) {
+        this.alpha = alpha;
+    }
+
+    protected void makeOverlay(KmlOutputManager out, GridGeometry2D geometry,
+            float[] fdata, ColorMapParameters parameters) {
+        GridEnvelope2D range = geometry.getGridRange2D();
+        int[] dimensions = { range.width, range.height };
+        ColorMapData data = new ColorMapData(FloatBuffer.wrap(fdata),
+                dimensions);
+        RenderedImage image = Colormapper.colorMap(data, parameters);
+        makeOverlay(out, image, geometry.getEnvelope2D());
+    }
+
+    protected void makeOverlay(KmlOutputManager out, RenderedImage image,
+            Envelope2D envelope) {
+        GroundOverlay overlay = new GroundOverlay();
+        overlay.setColor(toColorStr(alpha, new RGB(255, 255, 255)));
+        overlay.setName("Image");
+        overlay.createAndSetIcon().setHref(out.addImage(image));
+        LatLonBox box = overlay.createAndSetLatLonBox();
+        box.setNorth(envelope.getMaxY());
+        box.setEast(envelope.getMaxX());
+        box.setSouth(envelope.getMinY());
+        box.setWest(envelope.getMinX());
+        out.addFeature(overlay);
+    }
+
+    protected float[] reproject(GridGeometry2D src, GridGeometry2D dest,
+            DataSource data, Boolean interpolated) throws FactoryException,
+            TransformException {
+        GridReprojection reproj = getReprojection(src, dest);
+        Interpolation interp = null;
+        if (interpolated) {
+            interp = new BilinearInterpolation();
+        } else {
+            interp = new NearestNeighborInterpolation();
+        }
+        FloatArrayWrapper dst = new FloatArrayWrapper(dest);
+        return reproj.reprojectedGrid(interp, data, dst).getArray();
+    }
+
+    protected GridReprojection getReprojection(GridGeometry2D src,
+            GridGeometry2D dest) throws FactoryException, TransformException {
+        MultiKey key = new MultiKey(src, dest);
+        GridReprojection reproj = null;
+        boolean needsCompute = false;
+        synchronized (reprojCache) {
+            Reference<GridReprojection> reprojRef = reprojCache.get(key);
+            if (reprojRef != null) {
+                reproj = reprojRef.get();
+            }
+            if (reproj == null) {
+                reproj = new GridReprojection(src, dest);
+                needsCompute = true;
+                reprojCache.put(key,
+                        new SoftReference<GridReprojection>(reproj));
+            }
+        }
+        synchronized (reproj) {
+            if (needsCompute) {
+                reproj.computeTransformTable();
+            }
+        }
+        return reproj;
+    }
+}
---
+++ cave/com.raytheon.uf.viz.kml.export/src/com/raytheon/uf/viz/kml/export/graphics/ext/KmlRasterImageExtension.java
@@ -0,0 +1,122 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.kml.export.graphics.ext;
+
+import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridCoverageFactory;
+import org.geotools.coverage.processing.Operations;
+import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.Envelope2D;
+import org.geotools.geometry.jts.ReferencedEnvelope;
+import org.opengis.referencing.operation.MathTransform;
+import org.opengis.referencing.operation.TransformException;
+
+import com.raytheon.uf.common.geospatial.MapUtil;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.viz.core.DrawableImage;
+import com.raytheon.uf.viz.core.PixelCoverage;
+import com.raytheon.uf.viz.core.drawables.PaintProperties;
+import com.raytheon.uf.viz.core.drawables.ext.GraphicsExtension;
+import com.raytheon.uf.viz.core.drawables.ext.IImagingExtension;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.kml.export.graphics.KmlGraphicsTarget;
+import com.raytheon.uf.viz.kml.export.io.KmlOutputManager;
+
+/**
+ * Extension for creating KML Ground Overlay from raster images.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 11, 2012            bsteffen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bsteffen
+ * @version 1.0
+ */
+
+public class KmlRasterImageExtension extends
+        GraphicsExtension<KmlGraphicsTarget> implements IImagingExtension {
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(KmlRasterImageExtension.class);
+
+    @Override
+    public boolean drawRasters(PaintProperties paintProps,
+            DrawableImage... images) throws VizException {
+        target.addGenerator(new Generator(paintProps.getAlpha(), images));
+        return true;
+    }
+
+    @Override
+    public int getCompatibilityValue(KmlGraphicsTarget target) {
+        return Compatibilty.TARGET_COMPATIBLE;
+    }
+
+    private static class Generator extends KmlGroundOverlayGenerator {
+
+        private final DrawableImage[] images;
+
+        public Generator(float alpha, DrawableImage[] images) {
+            super(alpha);
+            this.images = images;
+        }
+
+        @Override
+        public void addFeature(KmlOutputManager outputManager) {
+            GridCoverageFactory coverageFactory = new GridCoverageFactory();
+            for (DrawableImage image : images) {
+                KmlRasterImage kmlImage = (KmlRasterImage) image.getImage();
+                PixelCoverage coverage = image.getCoverage();
+                MathTransform transform = gridGeometry.getGridToCRS();
+                DirectPosition2D min = new DirectPosition2D(coverage.getMinX(),
+                        coverage.getMinY());
+                DirectPosition2D max = new DirectPosition2D(coverage.getMaxX(),
+                        coverage.getMaxY());
+                try {
+                    transform.transform(min, min);
+                    transform.transform(max, max);
+                    ReferencedEnvelope env = new ReferencedEnvelope(
+                            new Envelope2D(min, max),
+                            gridGeometry.getCoordinateReferenceSystem());
+                    GridCoverage2D source = coverageFactory.create("Test",
+                            kmlImage.getImage(), env);
+                    GridCoverage2D result = (GridCoverage2D) new Operations(
+                            null).resample(source, MapUtil.LATLON_PROJECTION);
+                    makeOverlay(outputManager, result.getRenderedImage(),
+                            result.getEnvelope2D());
+                } catch (VizException e) {
+                    statusHandler.handle(Priority.PROBLEM,
+                            e.getLocalizedMessage(), e);
+
+                } catch (TransformException e) {
+                    statusHandler.handle(Priority.PROBLEM,
+                            e.getLocalizedMessage(), e);
+                }
+            }
+        }
+    }
+
+}
