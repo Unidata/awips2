de1639aea2600f50bef36407ce33e2e5cdb438c9
Steve Harris <Steven_L_Harris@raytheon.com>
2012-09-11 16:00:38 -0500

    12.10.1-3 baseline
    Former-commit-id: 969e7e6dfd6c74f2db042b869e609a61a71fc61f [formerly 1a94c1ca3674c8e4dbb8847de5190efe33da371c]
    Former-commit-id: 21f430062b5961e35660d0ee63a83d1b63e46810

---
+++ cave/com.raytheon.uf.viz.kml.export/src/com/raytheon/uf/viz/kml/export/KmlExportHandler.java
@@ -0,0 +1,103 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.kml.export;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.raytheon.uf.viz.core.IDisplayPane;
+import com.raytheon.uf.viz.core.IDisplayPaneContainer;
+import com.raytheon.uf.viz.core.drawables.AbstractRenderableDisplay;
+import com.raytheon.uf.viz.core.maps.display.MapRenderableDisplay;
+import com.raytheon.uf.viz.kml.export.KmlExportOptions.KmlExportTimeMode;
+import com.raytheon.viz.ui.EditorUtil;
+
+/**
+ * 
+ * Handler for events from the KML Export menu item.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 1, 2012            bsteffen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bsteffen
+ * @version 1.0
+ */
+public class KmlExportHandler extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        new KmlExportDialog(HandlerUtil.getActiveShell(event),
+                getDefaultOptions()).open();
+        return null;
+    }
+
+    protected static KmlExportOptions getDefaultOptions() {
+        KmlExportOptions options = new KmlExportOptions();
+        options.setFirstFrameIndex(Integer.MIN_VALUE);
+        options.setLastFrameIndex(Integer.MAX_VALUE);
+        options.setPreserveVisibility(true);
+        options.setShadeEarth(false);
+        options.setTimeMode(KmlExportTimeMode.TIME_SPAN);
+        options.setKmzFileLocation(new File(System.getProperty("user.home"),
+                "caveExport.kmz"));
+        options.setPlotIconScale(3.5);
+        options.setFillPlotBackground(false);
+        options.setPaintSleepMillis(10);
+        options.setMaxRefreshSeconds(60);
+        IDisplayPaneContainer container = EditorUtil.getActiveVizContainer();
+        List<KmlPane> panes = new ArrayList<KmlPane>();
+        for (IDisplayPane pane : container.getDisplayPanes()) {
+            AbstractRenderableDisplay display = (AbstractRenderableDisplay) pane
+                    .getRenderableDisplay();
+            panes.add(new KmlPane(display, pane.getBounds()));
+        }
+        options.setPanes(panes);
+        return options;
+    }
+
+    @Override
+    public void setEnabled(Object evaluationContext) {
+        IDisplayPaneContainer container = EditorUtil.getActiveVizContainer();
+        if (container == null) {
+            super.setBaseEnabled(false);
+            return;
+        }
+        IDisplayPane pane = container.getActiveDisplayPane();
+        if (pane == null) {
+            super.setBaseEnabled(false);
+            return;
+        }
+        super.setBaseEnabled(pane.getRenderableDisplay() instanceof MapRenderableDisplay);
+    }
+
+}
