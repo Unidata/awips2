987397b2db1ff270a764e4ae84e0d243d08254a0
Steve Harris <Steven_L_Harris@raytheon.com>
2012-09-13 12:33:36 -0500

    12.10.1-4 baseline
    Former-commit-id: ddc923e3e0716ed32ee978c5da6904cbe2182d6d [formerly 9cd1f3ec0f6cc40531671acdc219be6210e48eb7] [formerly 791ef6cedfaa60ab9f904882a0343d026d14c9fa] [formerly ddc923e3e0716ed32ee978c5da6904cbe2182d6d [formerly 9cd1f3ec0f6cc40531671acdc219be6210e48eb7] [formerly 791ef6cedfaa60ab9f904882a0343d026d14c9fa] [formerly 92cc2d860530d02afc07c9681db3d0309406b7cb [formerly 791ef6cedfaa60ab9f904882a0343d026d14c9fa [formerly ce574eeff1d197862c8aac2d3fc838a452862576]]]]
    Former-commit-id: 92cc2d860530d02afc07c9681db3d0309406b7cb
    Former-commit-id: 27d8601fe0205bc68877cf3598765dbd5cbdb9d3 [formerly f641cdd4e011686a7a35cbe69966208ce0bee0fb] [formerly 53da655c90a1b4d0e3b7bbcaf373c23ba32edc6a [formerly b4d2a0e8ddac59716fb10dd1abdc753792f64146]]
    Former-commit-id: 5159825bf9b81d8d0188fb4bcfd469ccb27fd393 [formerly 451459d8cb25736d15720a38ea7bfc5355514039]
    Former-commit-id: f61c5114e87643f57aeac0781578ff986dd11ded

--- edexOsgi/com.raytheon.edex.plugin.shef/src/com/raytheon/edex/plugin/shef/alarms/AlertalarmStdTextProductUtil.java
+++ edexOsgi/com.raytheon.edex.plugin.shef/src/com/raytheon/edex/plugin/shef/alarms/AlertalarmStdTextProductUtil.java
@@ -293,7 +293,7 @@ public class AlertalarmStdTextProductUtil {
         String suffix = reportOptions.getFileSuffix();
 
         if (suffix.length() > 0)
-            currReport = new File(reportDir + File.separator + pid + suffix);
+            currReport = new File(reportDir + File.separator + pid + "." + suffix);
         else
             currReport = new File(reportDir + File.separator + pid);
 
