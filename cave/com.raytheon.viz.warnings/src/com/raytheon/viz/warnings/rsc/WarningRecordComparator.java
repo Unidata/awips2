06593d4e6253d768cdcf43d536f7e2783e231fde
Jonathan Sanchez <jpsanchez1@raytheon.com>
2012-10-01 12:19:32 -0500

    Issue #1149 Refactored watches and warnings.
    Former-commit-id: eb0ca6d08ebbfa3f9bb5a6b7d5f04649eab5a96e [formerly d0ce171ca5a4568fb1e90b3ca5436e20c5fc4e79] [formerly b19f4810e200ac2b7470f5be3bc7659e651a2cd8] [formerly eb0ca6d08ebbfa3f9bb5a6b7d5f04649eab5a96e [formerly d0ce171ca5a4568fb1e90b3ca5436e20c5fc4e79] [formerly b19f4810e200ac2b7470f5be3bc7659e651a2cd8] [formerly e859dd05e94f5c45923c0ab5821017085f7a0922 [formerly b19f4810e200ac2b7470f5be3bc7659e651a2cd8 [formerly 3e4148ce433d9f8810f383f56c8ca9b24802de93]]]]
    Former-commit-id: e859dd05e94f5c45923c0ab5821017085f7a0922
    Former-commit-id: a153f11d5bf7009e9c9916b0c47f66d79567962d [formerly 0037b2b7f6c41621aab39eb881ec6d8e5825e73f] [formerly c4082d7451917277f5148dd2ae1918611c6736ef [formerly 81853ab4ab118cca13642b83e09dae1d9dcf8ef2]]
    Former-commit-id: a5662fcc4ce898e00dcc6728daa1a690c83cd014 [formerly 103ba6e17c34d09c5ec829f370747fddfac12140]
    Former-commit-id: fba0461571ab3385cc6a8e4098b53b99ec086217

--- cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WarningRecordComparator.java
+++ cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WarningRecordComparator.java
@@ -17,7 +17,7 @@ import com.raytheon.uf.common.dataplugin.warning.WarningRecord.WarningAction;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * May 12, 2011            jsanchez     Initial creation
- * 
+ * Sep 27, 2012     1149   jsanchez     Updated order of actions. Put CON before CAN,EXP
  * </pre>
  * 
  * @author jsanchez
@@ -30,6 +30,7 @@ public class WarningRecordComparator implements
      * Compares the WarningRecords by phenSig, ETN, action, then starttime
      * (descending order)
      */
+    @Override
     public int compare(AbstractWarningRecord wr1, AbstractWarningRecord wr2) {
         int rval = 0;
 
@@ -39,19 +40,30 @@ public class WarningRecordComparator implements
             rval = Double.compare(Double.parseDouble(wr1.getEtn()),
                     Double.parseDouble(wr2.getEtn()));
             if (rval == 0) {
-                if (wr1.getAct().equals(wr2.getAct())) {
+                WarningAction act1 = WarningAction.valueOf(wr1.getAct());
+                WarningAction act2 = WarningAction.valueOf(wr2.getAct());
+                if (act1 == act2) {
                     rval = 0;
-                } else if (wr1.getAct().equals(WarningAction.NEW.toString())) {
+                } else if (act1 == WarningAction.NEW) {
                     rval = -1;
-                } else if (wr2.getAct().equals(WarningAction.NEW.toString())) {
+                } else if (act2 == WarningAction.NEW) {
                     rval = 1;
+                } else if (act1 == WarningAction.CON
+                        && (act2 == WarningAction.CAN || act2 == WarningAction.EXP)) {
+                    return -1;
+                } else if (act2 == WarningAction.CON
+                        && (act1 == WarningAction.CAN || act1 == WarningAction.EXP)) {
+                    return 1;
                 } else {
                     rval = wr1.getAct().compareTo(wr2.getAct());
                 }
 
                 if (rval == 0) {
-                    rval = -1
-                            * wr1.getStartTime().compareTo(wr2.getStartTime());
+                    rval = wr1.getStartTime().compareTo(wr2.getStartTime());
+                    // sort warnings in descending order
+                    if (!wr1.getSig().equals("A")) {
+                        rval *= -1;
+                    }
                 }
             }
         }
