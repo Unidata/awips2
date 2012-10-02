7c1ec8bec57092b6b24e0e31c586f69d3ed06f73
Matt Nash <Matt_Nash@raytheon.com>
2012-10-02 15:49:52 -0500

    Issue #1228 make PersistSrv better handle duplicates, make radar only allow overwrites on GSM products, make PyPies send back duplicate messages
    Change-Id: I9ead2da2d38732c1070e4b2ea8259f44c19ffd63
    
    Former-commit-id: af0aa2299b4da6f72594827ea5620b112e7c66c2

--- edexOsgi/com.raytheon.edex.plugin.radar/src/com/raytheon/edex/plugin/radar/dao/RadarDao.java
+++ edexOsgi/com.raytheon.edex.plugin.radar/src/com/raytheon/edex/plugin/radar/dao/RadarDao.java
@@ -64,238 +64,249 @@ import com.raytheon.uf.edex.database.query.DatabaseQuery;
 
 public class RadarDao extends PluginDao {
 
-	/**
-	 * Creates a new radar dao
-	 * 
-	 * @param pluginName
-	 *            "radar"
-	 * @throws PluginException
-	 *             If the dao cannot be initialized
-	 */
-	public RadarDao(String pluginName) throws PluginException {
-		super(pluginName);
-	}
+    /**
+     * Creates a new radar dao
+     * 
+     * @param pluginName
+     *            "radar"
+     * @throws PluginException
+     *             If the dao cannot be initialized
+     */
+    public RadarDao(String pluginName) throws PluginException {
+        super(pluginName);
+    }
 
-	@Override
-	protected IDataStore populateDataStore(IDataStore dataStore,
-			IPersistable obj) throws Exception {
-		RadarRecord radarRec = (RadarRecord) obj;
-		StorageProperties sp = null;
-		String compression = PluginRegistry.getInstance()
-				.getRegisteredObject(pluginName).getCompression();
-		if (compression != null) {
-			sp = new StorageProperties();
-			sp.setCompression(StorageProperties.Compression
-					.valueOf(compression));
-		}
-		if (radarRec.getRawData() != null) {
-			IDataRecord rec = new ByteDataRecord(RadarStoredData.RAW_DATA_ID,
-					radarRec.getDataURI(), radarRec.getRawData(), 2,
-					new long[] { radarRec.getNumRadials(),
-							radarRec.getNumBins() });
-			rec.setCorrelationObject(radarRec);
-			dataStore.addDataRecord(rec, sp);
-		}
+    @Override
+    protected IDataStore populateDataStore(IDataStore dataStore,
+            IPersistable obj) throws Exception {
+        RadarRecord radarRec = (RadarRecord) obj;
+        StorageProperties sp = null;
+        String compression = PluginRegistry.getInstance()
+                .getRegisteredObject(pluginName).getCompression();
+        if (compression != null) {
+            sp = new StorageProperties();
+            sp.setCompression(StorageProperties.Compression
+                    .valueOf(compression));
+        }
+        if (radarRec.getRawData() != null) {
+            IDataRecord rec = new ByteDataRecord(RadarStoredData.RAW_DATA_ID,
+                    radarRec.getDataURI(), radarRec.getRawData(), 2,
+                    new long[] { radarRec.getNumRadials(),
+                            radarRec.getNumBins() });
+            rec.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(rec, sp);
+        }
 
-		if (radarRec.getRawShortData() != null) {
-			IDataRecord rec = new ShortDataRecord(
-					RadarStoredData.SHORT_DATA_ID, radarRec.getDataURI(),
-					radarRec.getRawShortData(), 2, new long[] {
-							radarRec.getNumRadials(), radarRec.getNumBins() });
-			rec.setCorrelationObject(radarRec);
-			dataStore.addDataRecord(rec, sp);
-		}
+        if (radarRec.getRawShortData() != null) {
+            IDataRecord rec = new ShortDataRecord(
+                    RadarStoredData.SHORT_DATA_ID, radarRec.getDataURI(),
+                    radarRec.getRawShortData(), 2, new long[] {
+                            radarRec.getNumRadials(), radarRec.getNumBins() });
+            rec.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(rec, sp);
+        }
 
-		if (radarRec.getAngleData() != null) {
-			IDataRecord rec = new FloatDataRecord(
-					RadarStoredData.ANGLE_DATA_ID, radarRec.getDataURI(),
-					radarRec.getAngleData(), 1,
-					new long[] { radarRec.getNumRadials() });
-			rec.setCorrelationObject(radarRec);
-			dataStore.addDataRecord(rec, sp);
-		}
+        if (radarRec.getAngleData() != null) {
+            IDataRecord rec = new FloatDataRecord(
+                    RadarStoredData.ANGLE_DATA_ID, radarRec.getDataURI(),
+                    radarRec.getAngleData(), 1,
+                    new long[] { radarRec.getNumRadials() });
+            rec.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(rec, sp);
+        }
 
-		if (radarRec.getThresholds() != null && radarRec.getProductCode() != 2) {
-			IDataRecord rec = new ShortDataRecord(
-					RadarStoredData.THRESHOLDS_ID, radarRec.getDataURI(),
-					radarRec.getThresholds(), 1, new long[] { 16 });
-			rec.setCorrelationObject(radarRec);
-			dataStore.addDataRecord(rec, sp);
-		}
+        if (radarRec.getThresholds() != null && radarRec.getProductCode() != 2) {
+            IDataRecord rec = new ShortDataRecord(
+                    RadarStoredData.THRESHOLDS_ID, radarRec.getDataURI(),
+                    radarRec.getThresholds(), 1, new long[] { 16 });
+            rec.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(rec, sp);
+        }
 
-		if (radarRec.getSymbologyBlock() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getSymbologyBlock());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.SYM_BLOCK_ID, radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getSymbologyBlock() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getSymbologyBlock());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.SYM_BLOCK_ID, radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getSymbologyData() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getSymbologyData());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.SYM_DATA_ID, radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getSymbologyData() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getSymbologyData());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.SYM_DATA_ID, radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getGraphicBlock() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getGraphicBlock());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.GRAPHIC_BLOCK_ID, radarRec.getDataURI(),
-					data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getGraphicBlock() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getGraphicBlock());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.GRAPHIC_BLOCK_ID, radarRec.getDataURI(),
+                    data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getMapProductVals() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getMapProductVals());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.PRODUCT_VALS_ID, radarRec.getDataURI(),
-					data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getMapProductVals() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getMapProductVals());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.PRODUCT_VALS_ID, radarRec.getDataURI(),
+                    data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getAlphanumericValues() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getAlphanumericValues());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.ALPHANUMERIC_ID, radarRec.getDataURI(),
-					data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getAlphanumericValues() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getAlphanumericValues());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.ALPHANUMERIC_ID, radarRec.getDataURI(),
+                    data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getTabularBlock() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getTabularBlock());
-			ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.TABULAR_ID,
-					radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getTabularBlock() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getTabularBlock());
+            ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.TABULAR_ID,
+                    radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getProductDependentValues() != null) {
-			IDataRecord rec = new ShortDataRecord(
-					RadarStoredData.DEPENDENT_VALS_ID, radarRec.getDataURI(),
-					radarRec.getProductDependentValues(), 1,
-					new long[] { radarRec.getProductDependentValues().length });
-			rec.setCorrelationObject(radarRec);
-			dataStore.addDataRecord(rec, sp);
-		}
+        if (radarRec.getProductDependentValues() != null) {
+            IDataRecord rec = new ShortDataRecord(
+                    RadarStoredData.DEPENDENT_VALS_ID, radarRec.getDataURI(),
+                    radarRec.getProductDependentValues(), 1,
+                    new long[] { radarRec.getProductDependentValues().length });
+            rec.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(rec, sp);
+        }
 
-		if (radarRec.getMapRecordVals() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getMapRecordVals());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.RECORD_VALS_ID, radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getMapRecordVals() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getMapRecordVals());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.RECORD_VALS_ID, radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getStormIDs() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(radarRec.getStormIDs());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.STORM_IDS_ID, radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getStormIDs() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(radarRec.getStormIDs());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.STORM_IDS_ID, radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getGsmMessage() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getGsmMessage());
-			ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.GSM_ID,
-					radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
-		if (radarRec.getAlertMessage() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getAlertMessage());
-			ByteDataRecord bdr = new ByteDataRecord(
-					RadarStoredData.ALERT_MESSAGE_ID, radarRec.getDataURI(),
-					data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getGsmMessage() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getGsmMessage());
+            ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.GSM_ID,
+                    radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
+        if (radarRec.getAlertMessage() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getAlertMessage());
+            ByteDataRecord bdr = new ByteDataRecord(
+                    RadarStoredData.ALERT_MESSAGE_ID, radarRec.getDataURI(),
+                    data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		if (radarRec.getAapMessage() != null) {
-			byte[] data = DynamicSerializationManager.getManager(
-					SerializationType.Thrift).serialize(
-					radarRec.getAapMessage());
-			ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.AAP_ID,
-					radarRec.getDataURI(), data);
-			dataStore.addDataRecord(bdr, sp);
-		}
+        if (radarRec.getAapMessage() != null) {
+            byte[] data = DynamicSerializationManager.getManager(
+                    SerializationType.Thrift).serialize(
+                    radarRec.getAapMessage());
+            ByteDataRecord bdr = new ByteDataRecord(RadarStoredData.AAP_ID,
+                    radarRec.getDataURI(), data);
+            bdr.setCorrelationObject(radarRec);
+            dataStore.addDataRecord(bdr, sp);
+        }
 
-		return dataStore;
-	}
+        return dataStore;
+    }
 
-	@Override
-	public List<IDataRecord[]> getHDF5Data(List<PluginDataObject> objects,
-			int tileSet) throws PluginException {
-		List<IDataRecord[]> retVal = new ArrayList<IDataRecord[]>();
+    @Override
+    public List<IDataRecord[]> getHDF5Data(List<PluginDataObject> objects,
+            int tileSet) throws PluginException {
+        List<IDataRecord[]> retVal = new ArrayList<IDataRecord[]>();
 
-		for (PluginDataObject obj : objects) {
-			IDataRecord[] record = null;
+        for (PluginDataObject obj : objects) {
+            IDataRecord[] record = null;
 
-			if (obj instanceof IPersistable) {
-				/* connect to the data store and retrieve the data */
-				try {
-					record = getDataStore((IPersistable) obj).retrieve(
-							obj.getDataURI());
-				} catch (Exception e) {
-					throw new PluginException(
-							"Error retrieving radar HDF5 data", e);
-				}
-				retVal.add(record);
-			}
-		}
+            if (obj instanceof IPersistable) {
+                /* connect to the data store and retrieve the data */
+                try {
+                    record = getDataStore((IPersistable) obj).retrieve(
+                            obj.getDataURI());
+                } catch (Exception e) {
+                    throw new PluginException(
+                            "Error retrieving radar HDF5 data", e);
+                }
+                retVal.add(record);
+            }
+        }
 
-		return retVal;
-	}
+        return retVal;
+    }
 
-	@Override
-	public PluginDataObject[] getFullRecord(DatabaseQuery query, int tile)
-			throws PluginException {
-		PluginDataObject[] queryResults = getMetadata(query);
-		for (PluginDataObject obj : queryResults) {
-			RadarRecord record = (RadarRecord) obj;
-			record.setPluginName(pluginName);
-			IDataRecord[] hdf5Data = getHDF5Data(record, tile);
-			record.setMessageData(hdf5Data[0].getDataObject());
-			record.setAngleData((float[]) hdf5Data[1].getDataObject());
-			record.setThresholds((short[]) hdf5Data[2].getDataObject());
-			record.setProductDependentValues((short[]) hdf5Data[8]
-					.getDataObject());
+    @Override
+    public PluginDataObject[] getFullRecord(DatabaseQuery query, int tile)
+            throws PluginException {
+        PluginDataObject[] queryResults = getMetadata(query);
+        for (PluginDataObject obj : queryResults) {
+            RadarRecord record = (RadarRecord) obj;
+            record.setPluginName(pluginName);
+            IDataRecord[] hdf5Data = getHDF5Data(record, tile);
+            record.setMessageData(hdf5Data[0].getDataObject());
+            record.setAngleData((float[]) hdf5Data[1].getDataObject());
+            record.setThresholds((short[]) hdf5Data[2].getDataObject());
+            record.setProductDependentValues((short[]) hdf5Data[8]
+                    .getDataObject());
 
-			record.setProductVals((HashMap<RadarConstants.MapValues, Map<String, Map<RadarConstants.MapValues, String>>>) hdf5Data[5]
-					.getDataObject());
-			record.setMapRecordVals((HashMap<RadarConstants.MapValues, Map<RadarConstants.MapValues, String>>) hdf5Data[6]);
-			record.setGsmMessage((GSMMessage) hdf5Data[7].getDataObject());
-			try {
-				record.setSymbologyBlock((SymbologyBlock) SerializationUtil
-						.transformFromThrift((byte[]) hdf5Data[3]
-								.getDataObject()));
-				record.setGraphicBlock((GraphicBlock) SerializationUtil
-						.transformFromThrift((byte[]) hdf5Data[4]
-								.getDataObject()));
+            record.setProductVals((HashMap<RadarConstants.MapValues, Map<String, Map<RadarConstants.MapValues, String>>>) hdf5Data[5]
+                    .getDataObject());
+            record.setMapRecordVals((HashMap<RadarConstants.MapValues, Map<RadarConstants.MapValues, String>>) hdf5Data[6]);
+            record.setGsmMessage((GSMMessage) hdf5Data[7].getDataObject());
+            try {
+                record.setSymbologyBlock((SymbologyBlock) SerializationUtil
+                        .transformFromThrift((byte[]) hdf5Data[3]
+                                .getDataObject()));
+                record.setGraphicBlock((GraphicBlock) SerializationUtil
+                        .transformFromThrift((byte[]) hdf5Data[4]
+                                .getDataObject()));
 
-			} catch (SerializationException e) {
-				throw new PluginException(
-						"Error deserializing symbology block", e);
-			}
-		}
-		return queryResults;
-	}
+            } catch (SerializationException e) {
+                throw new PluginException(
+                        "Error deserializing symbology block", e);
+            }
+        }
+        return queryResults;
+    }
 
-	public void populateData(RadarRecord record) throws Exception {
-		RadarDataRetriever.populateRadarRecord(getDataStore(record), record);
-	}
+    public void populateData(RadarRecord record) throws Exception {
+        RadarDataRetriever.populateRadarRecord(getDataStore(record), record);
+    }
 
 }
