185acb8d878df61b71ee77ff5aee24409f8195b5
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2012-10-16 12:52:30 -0500

    Issue #1229 Changes for non-blocking ProcedureDlg.
    Change-Id: Iefdf2f8e8ceeb7af5173dd4c630b6abc2c1ce41a
    
    Former-commit-id: d9fee38d21c351794faf918898734426708d13dc [formerly 207c1dfec7f51a429c9db61258d09631ef2049c9]
    Former-commit-id: 683c49fc4915a5c82ebad8ca0fa7881740e89a88

--- cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/dialogs/procedures/ProcedureDlg.java
+++ cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/dialogs/procedures/ProcedureDlg.java
@@ -23,7 +23,7 @@ package com.raytheon.uf.viz.d2d.ui.dialogs.procedures;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -97,6 +97,7 @@ import com.raytheon.viz.ui.editor.AbstractEditor;
  * ------------ ---------- ----------- --------------------------
  *                                     Initial Creation
  * Oct 16, 2012 1229       rferrel     Changes for non-blocking AlterBundleDlg.
+ * Oct 16, 2012 1229       rferrel     Changes to have displayDialog method.
  * 
  * </pre>
  * 
@@ -114,7 +115,7 @@ public class ProcedureDlg extends CaveSWTDialog {
 
     public static final String PROCEDURES_DIR = "/procedures";
 
-    private static Collection<ProcedureDlg> openDialogs = new ArrayList<ProcedureDlg>();
+    private static final Map<String, ProcedureDlg> openDialogs = new HashMap<String, ProcedureDlg>();
 
     private Font font;
 
@@ -228,7 +229,7 @@ public class ProcedureDlg extends CaveSWTDialog {
     protected void disposed() {
         font.dispose();
         synchronized (openDialogs) {
-            openDialogs.remove(this);
+            openDialogs.remove(fileName);
         }
     }
 
@@ -927,6 +928,18 @@ public class ProcedureDlg extends CaveSWTDialog {
             return;
         }
 
+        ProcedureDlg oldDlg = getDialog(fn);
+
+        if (oldDlg != null) {
+            oldDlg.close();
+        }
+
+        // Update mapping to new file name.
+        synchronized (openDialogs) {
+            openDialogs.remove(fileName);
+            openDialogs.put(fn, this);
+        }
+
         frozen = dlg.isFrozen();
         fileName = fn;
         saveProcedure();
@@ -1040,36 +1053,30 @@ public class ProcedureDlg extends CaveSWTDialog {
      * @return
      */
     public static ProcedureDlg getDialog(String fileName) {
-        synchronized (openDialogs) {
-            if (fileName != null) {
-                for (ProcedureDlg dialog : openDialogs) {
-                    if (fileName.equals(dialog.fileName)) {
-                        return dialog;
-                    }
-                }
-            }
-            return null;
+        synchronized (ProcedureDlg.openDialogs) {
+            ProcedureDlg dialog = openDialogs.get(fileName);
+            return dialog;
         }
     }
 
     /**
-     * Get the ProcedureDlg for the given fileName. If the fileName is null or
-     * if there is no open dialog, create a new ProcedureDlg.
+     * Get the ProcedureDlg for the given fileName and display it.
      * 
      * @param fileName
      * @param p
      * @param parent
-     * @return
      */
-    public static ProcedureDlg getOrCreateDialog(String fileName, Procedure p,
-            Shell parent) {
+    public static void displayDialog(String fileName, Procedure p, Shell parent) {
         synchronized (openDialogs) {
             ProcedureDlg dialog = getDialog(fileName);
-            if (dialog == null) {
+            if (dialog == null || dialog.getShell() == null
+                    || dialog.isDisposed()) {
                 dialog = new ProcedureDlg(fileName, p, parent);
-                openDialogs.add(dialog);
+                openDialogs.put(fileName, dialog);
+                dialog.open();
+            } else {
+                dialog.bringToTop();
             }
-            return dialog;
         }
     }
 }
