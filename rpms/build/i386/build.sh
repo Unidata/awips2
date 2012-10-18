b0607fb5d0faf5a9005ce20d8b7ed20997de92ae
Steve Harris <Steven_L_Harris@raytheon.com>
2012-10-18 15:53:01 -0500

    12.11.1-6 baseline
    Former-commit-id: 1c1cebef02bdd2710067168802b2b90d89b6d007 [formerly 58cd1e90f82b1949f3bf7222b2b2630bb3bcb9ca] [formerly 30c786a885e146add973370596e6f8affd56da85] [formerly 30c786a885e146add973370596e6f8affd56da85 [formerly fba4a443a02377687138b1ac7c4d5909e6e96dea]] [formerly 5c1f3cdc466a7061fb85d6650894fa4296bac35f [formerly 30c786a885e146add973370596e6f8affd56da85 [formerly fba4a443a02377687138b1ac7c4d5909e6e96dea] [formerly 5c1f3cdc466a7061fb85d6650894fa4296bac35f [formerly 18939af3b7cf54a36970d65d528003f4fe489eb7]]]]
    Former-commit-id: 5c1f3cdc466a7061fb85d6650894fa4296bac35f
    Former-commit-id: 4f00b6daa5853c32074c9c1a0f708770dd6c65a6 [formerly 4bf85ed3f3986d98a009ad57add9c40fe46fa2f2] [formerly 8beb6fc0b822dd6d8dbcb1e957746c9288e8863a [formerly 08fc9f6b149405cde44e2102c539283774c24671]]
    Former-commit-id: 8beb6fc0b822dd6d8dbcb1e957746c9288e8863a
    Former-commit-id: ae73939b0278ed635db0ea0ed747c941f397ad01

--- rpms/build/i386/build.sh
+++ rpms/build/i386/build.sh
@@ -174,11 +174,11 @@ if [ "${1}" = "-delta" ]; then
 fi
 
 if [ "${1}" = "-full" ]; then
-#   buildCAVE
-#   if [ $? -ne 0 ]; then
-#      exit 1
-#   fi
-#   buildRPM "awips2-alertviz"
+   buildCAVE
+   if [ $? -ne 0 ]; then
+      exit 1
+   fi
+   buildRPM "awips2-alertviz"
    buildEDEX
    if [ $? -ne 0 ]; then
       exit 1
