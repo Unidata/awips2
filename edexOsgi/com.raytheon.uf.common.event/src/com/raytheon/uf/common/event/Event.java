fcfc68b5aa549875b9f9ccad5e23229b0136f3e2
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2012-11-01 09:49:22 -0500

    Issue #1305 updated ProcessEvent with quantitative statistics. removed duplicate processStats.properties.
    Amend:
       removed groupBy attribute "fileName"
       added LogLevel enum and attribute to Event.java.  Default level is DEBUG.
    
    Change-Id: I13c155b1fae19cf5a3c82fbb633bf6c7fb217c07
    
    Former-commit-id: 7acf0f1874926ec440a13ae501cbc6d8e0bb01a2 [formerly cbce70fd0bd62d3ba380e466cceb5890fb418b98] [formerly 6ddd1cbdb3f03d5fa9f8da91e6179a53c6db9438] [formerly a4bc175653f631381138612dd20696306e581da8 [formerly 6ddd1cbdb3f03d5fa9f8da91e6179a53c6db9438 [formerly 51802e671ac15d451fec19262dfa7a352f0e4c09]]]
    Former-commit-id: a4bc175653f631381138612dd20696306e581da8
    Former-commit-id: d18adde3f073af91cd602064130a1c394a72bc1b [formerly ce47edb9055b00cf704ee48c2342024a2eb7d356]
    Former-commit-id: 5257f9a81ae553d76d8d4daaf258143130320ba4

--- edexOsgi/com.raytheon.uf.common.event/src/com/raytheon/uf/common/event/Event.java
+++ edexOsgi/com.raytheon.uf.common.event/src/com/raytheon/uf/common/event/Event.java
@@ -9,9 +9,28 @@ import javax.persistence.Column;
 import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
+/**
+ * Provides logging and deletion services for camel
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Nov 5, 2012  #1305      bgonzale    Added LogLevel enum and transient attribute.
+ * 
+ * </pre>
+ * 
+ * @author jsanchez
+ * @version 1.0
+ */
 public abstract class Event implements Serializable, ISerializableObject {
 
     private static final long serialVersionUID = 1L;
+
+    public enum LogLevel {
+        DEBUG, INFO, WARN, ERROR, FATAL, ALL, OFF, TRACE
+    }
 
     @Column
     @DynamicSerializeElement
@@ -21,8 +40,16 @@ public abstract class Event implements Serializable, ISerializableObject {
     @DynamicSerializeElement
     protected String id;
 
+    @DynamicSerializeElement
+    protected LogLevel logLevel;
+
     public Event() {
+        this(LogLevel.DEBUG);
+    }
+
+    public Event(LogLevel logLevel) {
         date = Calendar.getInstance();
+        this.logLevel = logLevel;
     }
 
     public Calendar getDate() {
@@ -44,6 +71,21 @@ public abstract class Event implements Serializable, ISerializableObject {
     public String toString() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-d hh:mm:ss");
         return formatter.format(date.getTime()) + " Id: " + id;
+    }
+
+    /**
+     * @return the logLevel
+     */
+    public LogLevel getLogLevel() {
+        return logLevel;
+    }
+
+    /**
+     * @param logLevel
+     *            the logLevel to set
+     */
+    public void setLogLevel(LogLevel logLevel) {
+        this.logLevel = logLevel;
     }
 
 }
