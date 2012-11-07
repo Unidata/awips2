26210e06ef7d9886e39cb6ae82b809d8e0e80af3
Mike Duff <michael.duff@raytheon.com>
2012-11-07 12:48:25 -0600

    Issue #1317 - Update the stats config files Issue #1317 - Peer review comments.
    Change-Id: Ic4fa6574a0d71fbdf246483f0ea2553d4d058c9f
    
    Former-commit-id: 7398e4fd683de8cb314d65d6c05cfee6ff018bed [formerly e72c2822ad9ed1cd3bf83284c54dabe26e2bd11b] [formerly 1372c08cdc9bc36a0698050cc7c2e46aaf967d22] [formerly 50f0c4c226b806bc429e5230f5ba0d5e39d88d87 [formerly 1372c08cdc9bc36a0698050cc7c2e46aaf967d22 [formerly 8cc9210d25324ab4b5cbd5af3687b371101d282b]]]
    Former-commit-id: 50f0c4c226b806bc429e5230f5ba0d5e39d88d87
    Former-commit-id: f2b77e9dc771b60a8323ff83614ff068514ec645 [formerly 688b7cce301160ae29d2c636d773f232637e82cc]
    Former-commit-id: 7bed38a373f4ececa11b30bf847b427bc2ff0f3d

--- edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/util/ConfigLoader.java
+++ edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/util/ConfigLoader.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -23,70 +23,60 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationContext;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationType;
 import com.raytheon.uf.common.localization.LocalizationFile;
 import com.raytheon.uf.common.localization.PathManagerFactory;
+import com.raytheon.uf.common.serialization.JAXBManager;
+import com.raytheon.uf.common.stats.xml.StatisticsAggregate;
+import com.raytheon.uf.common.stats.xml.StatisticsConfig;
+import com.raytheon.uf.common.stats.xml.StatisticsEvent;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.edex.stats.xml.Aggregate;
 import com.raytheon.uf.edex.stats.xml.Statistics;
-import com.raytheon.uf.edex.stats.xml.StatsConfig;
 
 /**
- * Loads statsConfig files from localization.
- * 
+ * Loads StatisticsConfig files from localization.
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Aug 21, 2012            jsanchez     Updated error handling and validated config files.
- * 
+ * Aug 21, 2012            jsanchez    Updated error handling and validated config files.
+ * Nov 07, 2012   1317     mpduff      Update config files.
+ *
  * </pre>
- * 
+ *
  * @author jsanchez
- * 
+ *
  */
 public class ConfigLoader {
 
-    private static final transient IUFStatusHandler statusHandler = UFStatus
+    private static final IUFStatusHandler statusHandler = UFStatus
             .getHandler(ConfigLoader.class);
 
-    private JAXBContext jax;
+    private static final JAXBManager jaxbManager;
+    static {
+        try {
+            jaxbManager = new JAXBManager(StatisticsConfig.class,
+                    Statistics.class);
+        } catch (JAXBException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
 
-    /** Unmarshaller object */
-    private Unmarshaller unmarshaller;
+    private final IPathManager pm = PathManagerFactory.getPathManager();
 
-    /** Marshaller object */
-    private Marshaller marshaller;
-
-    private IPathManager pm = PathManagerFactory.getPathManager();
-
-    private List<StatsConfig> configurations;
+    private final List<StatisticsConfig> configurations = new ArrayList<StatisticsConfig>();
 
     private final String STATS_DIR = "stats";
 
     /**
-     * Constructor. Performs an initial statsCon
-     */
-    public ConfigLoader() throws JAXBException {
-        jax = JAXBContext.newInstance(new Class[] { StatsConfig.class,
-                Statistics.class });
-        unmarshaller = jax.createUnmarshaller();
-        this.marshaller = jax.createMarshaller();
-        this.marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        this.configurations = new ArrayList<StatsConfig>();
-    }
-
-    /**
-     * Loads the statsConfig files in the STATS_DIR directory.
+     * Loads the StatisticsConfig files in the STATS_DIR directory.
      */
     public void load() throws Exception {
         LocalizationContext[] searchContext = pm
@@ -97,7 +87,6 @@ public class ConfigLoader {
             if (localizationFiles != null && localizationFiles.length > 0) {
                 break;
             }
-
         }
 
         if (localizationFiles != null && localizationFiles.length > 0) {
@@ -105,8 +94,9 @@ public class ConfigLoader {
             for (LocalizationFile localizationFile : localizationFiles) {
                 if (localizationFile.getFile() != null
                         && localizationFile.getFile().exists()) {
-                    StatsConfig config = (StatsConfig) unmarshaller
-                            .unmarshal(localizationFile.getFile());
+                    StatisticsConfig config = (StatisticsConfig) jaxbManager
+                            .jaxbUnmarshalFromXmlFile(localizationFile
+                                    .getFile());
                     config = validateAggregates(config);
                     configurations.add(config);
                 }
@@ -116,43 +106,43 @@ public class ConfigLoader {
 
     /**
      * Removes the aggregate if its not a numerical parameter.
-     * 
+     *
      * @param config
      */
-    private StatsConfig validateAggregates(StatsConfig config)
+    private StatisticsConfig validateAggregates(StatisticsConfig config)
             throws ClassNotFoundException {
-        Class<?> clazz = Class.forName(config.getEventType());
-        List<Aggregate> aggregates = new ArrayList<Aggregate>();
+        List<StatisticsAggregate> aggregates = new ArrayList<StatisticsAggregate>();
+        for (StatisticsEvent event : config.getEvents()) {
+            Class<?> clazz = Class.forName(event.getType());
 
-        for (Aggregate aggregate : config.getAggregates()) {
-            String aggregateField = aggregate.getField();
-            try {
-                Field field = clazz.getDeclaredField(aggregateField);
-                if (!field.getType().isPrimitive()) {
-                    statusHandler
-                            .info("'"
-                                    + aggregateField
-                                    + "' not a primitive type. Aggregate being removed. ");
+            for (StatisticsAggregate aggregate : event.getAggregateList()) {
+                String aggregateField = aggregate.getField();
+                try {
+                    Field field = clazz.getDeclaredField(aggregateField);
+                    if (!field.getType().isPrimitive()) {
+                        statusHandler
+                                .info("'"
+                                        + aggregateField
+                                        + "' not a primitive type. Aggregate being removed. ");
+                    }
+                    aggregates.add(aggregate);
+                } catch (NoSuchFieldException e) {
+                    statusHandler.info("'" + aggregateField
+                            + "' not a valid field. Aggregate being removed. ");
                 }
-                aggregates.add(aggregate);
-            } catch (NoSuchFieldException e) {
-                statusHandler.info("'" + aggregateField
-                        + "' not a valid field. Aggregate being removed. ");
             }
+            event.setAggregateList(aggregates);
         }
-
-        config.setAggregates(aggregates.toArray(new Aggregate[aggregates.size()]));
 
         return config;
     }
 
     /**
-     * Returns a list of all statsConfig files.
-     * 
+     * Returns a list of all StatisticsConfig files.
+     *
      * @return
      */
-    public List<StatsConfig> getConfigurations() {
+    public List<StatisticsConfig> getConfigurations() {
         return configurations;
     }
-
 }
