7bed38a373f4ececa11b30bf847b427bc2ff0f3d
Mike Duff <michael.duff@raytheon.com>
2012-11-07 12:48:25 -0600

    Issue #1317 - Update the stats config files Issue #1317 - Peer review comments.
    Change-Id: Ic4fa6574a0d71fbdf246483f0ea2553d4d058c9f
    
    Former-commit-id: e72c2822ad9ed1cd3bf83284c54dabe26e2bd11b [formerly 1372c08cdc9bc36a0698050cc7c2e46aaf967d22] [formerly 50f0c4c226b806bc429e5230f5ba0d5e39d88d87 [formerly 8cc9210d25324ab4b5cbd5af3687b371101d282b]]
    Former-commit-id: 50f0c4c226b806bc429e5230f5ba0d5e39d88d87
    Former-commit-id: 688b7cce301160ae29d2c636d773f232637e82cc

--- edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/handler/StatsHandler.java
+++ edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/handler/StatsHandler.java
@@ -1,19 +1,19 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- * 
+ *
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- * 
+ *
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- * 
+ *
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
@@ -29,33 +29,38 @@ import com.raytheon.uf.common.event.Event;
 import com.raytheon.uf.common.serialization.SerializationException;
 import com.raytheon.uf.common.serialization.SerializationUtil;
 import com.raytheon.uf.common.stats.StatsRecord;
+import com.raytheon.uf.common.stats.xml.StatisticsConfig;
+import com.raytheon.uf.common.stats.xml.StatisticsEvent;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.edex.database.dao.CoreDao;
 import com.raytheon.uf.edex.database.dao.DaoConfig;
 import com.raytheon.uf.edex.event.EventBus;
-import com.raytheon.uf.edex.stats.xml.StatsConfig;
 
 /**
  * Subscribes to the event bus and stores them in the appropriate stats table
- * 
- * 
+ *
+ *
  * <pre>
- * 
+ *
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Aug 21, 2012            jsanchez     Removed instance variable of event bus.
- * 
+ * Aug 21, 2012            jsanchez    Removed instance variable of event bus.
+ * Nov 07, 2012   1317     mpduff      Updated config files.
+ *
  * </pre>
- * 
+ *
  * @author jsanchez
- * 
+ *
  */
 public class StatsHandler {
+    private static final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(StatsHandler.class);
 
-    private CoreDao dao = new CoreDao(DaoConfig.forClass("metadata",
+    private final CoreDao dao = new CoreDao(DaoConfig.forClass("metadata",
             StatsRecord.class));
 
-    // TODO Make unmodifiable
     private static Set<String> validEventTypes = new HashSet<String>();
 
     /**
@@ -80,16 +85,23 @@ public class StatsHandler {
                 record.setEvent(bytes);
                 dao.persist(record);
             } catch (SerializationException e) {
-                e.printStackTrace();
+                statusHandler.error("Error transforming to Thrift.", e);
             }
         }
     }
 
-    public static void setValidEventTypes(List<StatsConfig> configurations) {
+    /**
+     * Set the valid event types.
+     *
+     * @param configurations
+     *            List of StatisticsConfig objects
+     */
+    public static void setValidEventTypes(List<StatisticsConfig> configurations) {
         validEventTypes = new HashSet<String>();
-        for (StatsConfig config : configurations) {
-            validEventTypes.add(config.getEventType());
+        for (StatisticsConfig config : configurations) {
+            for (StatisticsEvent event : config.getEvents()) {
+                validEventTypes.add(event.getType());
+            }
         }
     }
-
 }
