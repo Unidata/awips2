5877fc87e292a96e6d6b6ec0341b8ff6c2d7eb5a
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-11-12 09:37:32 -0600

    Issue #1323 initial commit of interfaces of data access framework
    Change-Id: I301d94a85a4e86550cd409bc17ef94ea3fdd6e02
    
    Former-commit-id: 3a108da7dd0bdc5ec76ec4ca330ccea3c027a54c [formerly d7f7152e93ebe764e462f8649ebb17341c5f61a3] [formerly b24c9eb456ca50594605b53e70268801893237fb] [formerly b24c9eb456ca50594605b53e70268801893237fb [formerly f255f65b3cb461214449f4a0fbf862fd46525fe6]] [formerly 3a108da7dd0bdc5ec76ec4ca330ccea3c027a54c [formerly d7f7152e93ebe764e462f8649ebb17341c5f61a3] [formerly b24c9eb456ca50594605b53e70268801893237fb] [formerly b24c9eb456ca50594605b53e70268801893237fb [formerly f255f65b3cb461214449f4a0fbf862fd46525fe6]] [formerly bf72e29752870673592dfc6d9874cf724b5ef7bc [formerly b24c9eb456ca50594605b53e70268801893237fb [formerly f255f65b3cb461214449f4a0fbf862fd46525fe6] [formerly bf72e29752870673592dfc6d9874cf724b5ef7bc [formerly d6b63788ef2af3da474a0d3affcafdddec3690a5]]]]]
    Former-commit-id: bf72e29752870673592dfc6d9874cf724b5ef7bc
    Former-commit-id: c8948b09176963dd834173a19a5529d329704829 [formerly 6d3beb139e6cf8f85baeb177724f583b6e33ace5] [formerly cfed9d9664416b453b87cf29d1e2b6294ba4303d] [formerly aaa1ce5e9b7edfd327399492eddafe0aabba90d3 [formerly 321cb3de1f60b624ae9aad5c85b473334d3d249e] [formerly cfed9d9664416b453b87cf29d1e2b6294ba4303d [formerly 78a1185b19d5f80118a6994823d9c57ac2b1d219]]]
    Former-commit-id: abaad8073b7f6183416173da6f288a5a2f013334 [formerly 614aba708193784746c4469f65a0cc453381e39b] [formerly 7fce83f574f7a2670689e6f254e5c7046f4a98ce [formerly 5b837a1b13581c3ec1c506da0d74d03eaa5a6869]]
    Former-commit-id: 7fce83f574f7a2670689e6f254e5c7046f4a98ce
    Former-commit-id: 7a6797dd62e0edb492b5d967dc230a1ef974757a

---
+++ edexOsgi/com.raytheon.uf.common.dataaccess/src/com/raytheon/uf/common/dataaccess/grid/IGridData.java
@@ -0,0 +1,93 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.common.dataaccess.grid;
+
+import javax.measure.unit.Unit;
+
+import org.geotools.coverage.grid.GridGeometry2D;
+
+import com.raytheon.uf.common.dataaccess.IData;
+import com.raytheon.uf.common.dataplugin.level.Level;
+import com.raytheon.uf.common.geospatial.interpolation.data.DataDestination;
+import com.raytheon.uf.common.geospatial.interpolation.data.UnitConvertingDataDestination;
+
+/**
+ * An IGridData represents data that is gridded, ie rectangular (when not
+ * projected) with a set x and y size.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Oct 9, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public interface IGridData extends IData {
+
+    /**
+     * Gets the parameter of the data
+     * 
+     * @return the parameter of the data
+     */
+    public String getParameter();
+
+    /**
+     * Gets the level of the data
+     * 
+     * @return the level of the data
+     */
+    public Level getLevel();
+
+    /**
+     * Gets the GridGeometry of the data
+     * 
+     * @return the grid geometry of the data
+     */
+    public GridGeometry2D getGridGeometry();
+
+    /**
+     * Gets the unit of the raw data. This may differ from the unit of a data
+     * destination.
+     * 
+     * @return the unit of the data
+     */
+    public Unit<?> getUnit();
+
+    /**
+     * Populates the DataDestination argument with the raw data converted to the
+     * type to match the DataDestination. The destination must not be null. If
+     * unit conversions are desired, use the
+     * {@link UnitConvertingDataDestination} to specify what unit conversion
+     * should be applied to the data.
+     * 
+     * @param destination
+     *            the destination to fill with data
+     * @return the data destination that was passed in, with the data populated
+     */
+    public <DD extends DataDestination> DD populateData(DD destination);
+
+}
