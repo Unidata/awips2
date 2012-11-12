3a108da7dd0bdc5ec76ec4ca330ccea3c027a54c
Nate Jensen <Nathan.Jensen@raytheon.com>
2012-11-12 09:37:32 -0600

    Issue #1323 initial commit of interfaces of data access framework
    Change-Id: I301d94a85a4e86550cd409bc17ef94ea3fdd6e02
    
    Former-commit-id: bf72e29752870673592dfc6d9874cf724b5ef7bc [formerly b24c9eb456ca50594605b53e70268801893237fb [formerly f255f65b3cb461214449f4a0fbf862fd46525fe6] [formerly bf72e29752870673592dfc6d9874cf724b5ef7bc [formerly d6b63788ef2af3da474a0d3affcafdddec3690a5]]]
    Former-commit-id: b24c9eb456ca50594605b53e70268801893237fb [formerly f255f65b3cb461214449f4a0fbf862fd46525fe6]
    Former-commit-id: b24c9eb456ca50594605b53e70268801893237fb
    Former-commit-id: d7f7152e93ebe764e462f8649ebb17341c5f61a3

---
+++ edexOsgi/com.raytheon.uf.common.dataaccess/src/com/raytheon/uf/common/dataaccess/impl/AbstractDataRequest.java
@@ -0,0 +1,90 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.common.dataaccess.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.raytheon.uf.common.dataplugin.level.Level;
+
+/**
+ * 
+ * An abstract request for requesting data through the Data Access Framework.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Nov 6, 2012            njensen     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author njensen
+ * @version 1.0
+ */
+
+public abstract class AbstractDataRequest {
+
+    protected String datatype;
+
+    protected Map<String, Object> identifiers;
+
+    protected String[] parameters;
+
+    protected Level[] levels;
+
+    public void setDatatype(String datatype) {
+        this.datatype = datatype;
+    }
+
+    public void addIdentifier(String key, Object value) {
+        if (identifiers == null) {
+            identifiers = new HashMap<String, Object>();
+        }
+        identifiers.put(key, value);
+    }
+
+    public void setParameters(String... params) {
+        this.parameters = params;
+    }
+
+    public void setLevels(Level... levels) {
+        this.levels = levels;
+    }
+
+    public String getDatatype() {
+        return datatype;
+    }
+
+    public Map<String, Object> getIdentifiers() {
+        return identifiers;
+    }
+
+    public String[] getParameters() {
+        return parameters;
+    }
+
+    public Level[] getLevels() {
+        return levels;
+    }
+
+}
