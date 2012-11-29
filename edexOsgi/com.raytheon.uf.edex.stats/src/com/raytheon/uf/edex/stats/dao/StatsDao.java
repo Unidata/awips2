00e14510a7a46ca6cd64b81480e7ea52dc7c95d1
Richard Peter <richard.peter@raytheon.com>
2012-11-29 14:30:03 -0600

    Issue #1350: Fix stats aggregation
    Change-Id: I652db6f6f5bc0cab8fc9e85969bf57f1c2382da3
    
    Former-commit-id: 251c050333a1de8b4cafd92823aab039203c067b [formerly 251c050333a1de8b4cafd92823aab039203c067b [formerly 71574f210ded5439846ce0780d2622c4a23cb141]] [formerly 94df67d3542a240cabd69e1f47894936716db711] [formerly 251c050333a1de8b4cafd92823aab039203c067b [formerly 251c050333a1de8b4cafd92823aab039203c067b [formerly 71574f210ded5439846ce0780d2622c4a23cb141]] [formerly 94df67d3542a240cabd69e1f47894936716db711] [formerly 5a45254db0b3e188c494f48fa8824f65814399b2 [formerly 5a45254db0b3e188c494f48fa8824f65814399b2 [formerly 2b124db62a2eb980cf9f4ef57dc1c1584f69f3a8]] [formerly 94df67d3542a240cabd69e1f47894936716db711 [formerly 94df67d3542a240cabd69e1f47894936716db711 [formerly b06d51266d600e0b04f7fe10bc36e312a2275428]]]]]
    Former-commit-id: 5a45254db0b3e188c494f48fa8824f65814399b2 [formerly 5a45254db0b3e188c494f48fa8824f65814399b2 [formerly 2b124db62a2eb980cf9f4ef57dc1c1584f69f3a8]]
    Former-commit-id: 1cee9904f02e47ff4098426812c5378f3d3f73ed [formerly fd8188866133b1f88989911e08fa710ac3d98c7e] [formerly f5f04b35366669aee897148adeaa067a5dfb8b59 [formerly 99f19186ef04dc8c4b7cf4eccb5464a18ee795f2]] [formerly 5a45254db0b3e188c494f48fa8824f65814399b2]
    Former-commit-id: cde4b6bf5722dbe543ed3b8839c53c8e930281cd [formerly a90a1e13a3d9fb811eba7c8ae3162b65286ee60b] [formerly e410ddcd0c27f200a7fc768b6f91a6f1ea94a876 [formerly 9a51a3d3b2cecf1d8c62c41833bcd327f3edb3d2]]
    Former-commit-id: 5b2857311a24975424702a3e182802fed199248b [formerly 55ae5a07cf8a8cd71cf5eae282de655d275e1561]
    Former-commit-id: 1d103579b7519bcbf03dd5fc7531fd8691e610d0

--- edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/dao/StatsDao.java
+++ edexOsgi/com.raytheon.uf.edex.stats/src/com/raytheon/uf/edex/stats/dao/StatsDao.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 import com.raytheon.uf.common.dataquery.db.QueryParam.QueryOperand;
 import com.raytheon.uf.common.stats.StatsRecord;
-import com.raytheon.uf.common.status.IUFStatusHandler;
-import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.edex.database.DataAccessLayerException;
 import com.raytheon.uf.edex.database.dao.CoreDao;
 import com.raytheon.uf.edex.database.dao.DaoConfig;
 import com.raytheon.uf.edex.database.query.DatabaseQuery;
 
 public class StatsDao extends CoreDao {
-    private static final transient IUFStatusHandler statusHandler = UFStatus
-            .getHandler(StatsDao.class);
-
     /**
      * Creates a new data access object
      */
@@ -28,30 +23,24 @@ public class StatsDao extends CoreDao {
      * 
      * @param limit
      * @param eventType
+     * @param maxResults
+     *            if greater than 0 will limit database results to maxResults
      * @return an array of stat records. If an error occurs, then an array of
      *         size 0 will be returned.
      * @throws DataAccessLayerException
      */
-    public StatsRecord[] retrieveRecords(Calendar limit, String eventType) {
+    @SuppressWarnings("unchecked")
+    public List<StatsRecord> retrieveRecords(Calendar limit, String eventType,
+            int maxResults) throws DataAccessLayerException {
         DatabaseQuery query = new DatabaseQuery(StatsRecord.class);
         query.addQueryParam("eventType", eventType, QueryOperand.EQUALS);
         query.addQueryParam("date", limit, QueryOperand.LESSTHAN);
-        query.setMaxResults(1000);
+        query.addOrder("date", true);
 
-        // TODO Need to make StatsDao to keep track to determine next 1000
-        // results.
-        StatsRecord[] records = null;
-        try {
-            List<?> objects = queryByCriteria(query);
-            records = new StatsRecord[objects.size()];
-            for (int i = 0; i < records.length; i++) {
-                records[i] = (StatsRecord) objects.get(i);
-            }
-        } catch (DataAccessLayerException e) {
-            records = new StatsRecord[0];
-            statusHandler.error("Error querying the stats table", e);
+        if (maxResults > 0) {
+            query.setMaxResults(maxResults);
         }
 
-        return records;
+        return (List<StatsRecord>) queryByCriteria(query);
     }
 }
