52fa1e03634bb5532bf71c445f6e256dec04818a
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2012-12-03 11:53:40 -0600

    Issue #1377 Break registry dependency on data delivery
    Change-Id: I232f4d23497f37d5f4f9883048e9c3bda84551ea
    
    Former-commit-id: 1b165c8e73f233366f71704c175a9ec44dec459f [formerly 529238d01034bbb5449506e60cc003928fd74bbf] [formerly bba092f51efff9d4583fc975dd74906d0c0b1b9a [formerly f560beb5c66541859abd36cab855a7b6a8c8ded9]]
    Former-commit-id: bba092f51efff9d4583fc975dd74906d0c0b1b9a
    Former-commit-id: 6d1831b0023e75ad518d0951b1d00853329de781

---
+++ edexOsgi/com.raytheon.uf.common.auth/src/com/raytheon/uf/common/auth/RequestConstants.java
@@ -0,0 +1,43 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.common.auth;
+
+/**
+ * Holds constants for the request server.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Dec 3, 2012  1377      djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+
+public final class RequestConstants {
+
+    public static final String REQUEST_SERVER = "request.server";
+
+}
