d346b7fb3edd06b98225a53b3300820ea64acbf3
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2012-12-11 08:37:50 -0600

    Issue #1407 EventBus changes to support sending notifications for subscription verification
    Change-Id: I8f979e44520e32fa908aa700bb0c93596a2dd820
    
    Former-commit-id: 79fa61bb8a56e2fdc58836b9aa359ad0f130a1bd [formerly 8b942c31dd5c4858892223ccebc48f1e8bba7162] [formerly 79fa61bb8a56e2fdc58836b9aa359ad0f130a1bd [formerly 8b942c31dd5c4858892223ccebc48f1e8bba7162] [formerly 3d900240b74d997db224e3fc8d2466ee771fd50a [formerly 12b51905d9b710ebc6fa54346d82cad9178b0bba]]]
    Former-commit-id: 3d900240b74d997db224e3fc8d2466ee771fd50a
    Former-commit-id: 4de56a014108aca8c1238e2f6fc3c6bbaef52274 [formerly e72d5dc09d5e73f2cd6061300b3c6c3152049c5f]
    Former-commit-id: 88625668bb72289439f8ce8e77442c11fd377e31

--- edexOsgi/com.raytheon.uf.edex.event/src/com/raytheon/uf/edex/event/EventBus.java
+++ edexOsgi/com.raytheon.uf.edex.event/src/com/raytheon/uf/edex/event/EventBus.java
@@ -1,11 +1,7 @@
 package com.raytheon.uf.edex.event;
 
-import java.util.concurrent.Executors;
-
-import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.annotations.VisibleForTesting;
 import com.raytheon.uf.common.event.Event;
-import com.raytheon.uf.common.status.IUFStatusHandler;
-import com.raytheon.uf.common.status.UFStatus;
 
 /**
  * 
@@ -19,6 +15,7 @@ import com.raytheon.uf.common.status.UFStatus;
  * ------------ ---------- ----------- --------------------------
  * Oct 11, 2012 1261       djohnson     Add SW history, create constants for fields, 
  *                                      add unregister.
+ * Dec 11, 2012 1407       djohnson     Separate the creation of the Google EventBus from the wrapper class.
  * 
  * </pre>
  * 
@@ -26,59 +23,72 @@ import com.raytheon.uf.common.status.UFStatus;
  * @version 1.0
  */
 public class EventBus {
-    private static final IUFStatusHandler statusHandler = UFStatus
-            .getHandler(EventBus.class);
 
-    private static final EventBus instance = new EventBus();
+    @VisibleForTesting
+    static GoogleEventBusFactory eventBusFactory = new AsynchronousEventBusFactory();
 
-    private static final AsyncEventBus asyncEventBus;
-    static {
-        int threadCount = 15;
-        try {
-            threadCount = Integer.parseInt(System.getProperty(
-                    "eventBusThreadCount", "15"));
-        } catch (Exception e) {
-            statusHandler
-                    .error("Unable to set thread pool size from property eventBusThreadCount; defaulting size to "
-                            + threadCount + ".", e);
-        }
-        asyncEventBus = new AsyncEventBus("EventBus",
-                Executors.newFixedThreadPool(threadCount));
+    /**
+     * Holder class which allows safe, concurrent, and on-demand creation of the
+     * EventBus. It also enforces the singleton contract.
+     */
+    private static class EventBusHolder {
+        private static com.google.common.eventbus.EventBus eventBus = eventBusFactory
+                .getEventBus();
+
+        private static final EventBus instance = new EventBus(eventBus);
     }
 
     /**
-     * Returns the same instance of the data delivery event bus
+     * Returns the singleton instance of the data delivery event bus
      * 
-     * @return
+     * @return the singleton instance
      */
     public static EventBus getInstance() {
-        return instance;
+        return EventBusHolder.instance;
+    }
+
+    /**
+     * The actual Google EventBus being wrapped.
+     */
+    private final com.google.common.eventbus.EventBus googleEventBus;
+
+    /**
+     * Constructor that accepts a Google EventBus.
+     * 
+     * @param eventBus
+     *            the Google EventBus
+     */
+    private EventBus(com.google.common.eventbus.EventBus eventBus) {
+        this.googleEventBus = eventBus;
     }
 
     /**
      * Register an object with the event bus.
      * 
      * @param subscriber
+     *            the subscriber to register
      */
     public void register(Object subscriber) {
-        asyncEventBus.register(subscriber);
+        this.googleEventBus.register(subscriber);
     }
 
     /**
      * Publishes events for all subscribers to receive
      * 
      * @param event
+     *            the event
      */
     public void publish(Event event) {
-        asyncEventBus.post(event);
+        this.googleEventBus.post(event);
     }
 
     /**
      * Unregister an object with the event bus.
      * 
-     * @param instance2
+     * @param subscriber
+     *            the object subscribed to the event buss
      */
     public void unregister(Object subscriber) {
-        asyncEventBus.unregister(subscriber);
+        this.googleEventBus.unregister(subscriber);
     }
 }
