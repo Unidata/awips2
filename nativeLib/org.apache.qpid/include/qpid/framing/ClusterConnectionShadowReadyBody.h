574680b06626b418674051bfce6df3070513c6f3
Bryan Kowal <Bryan.Kowal@raytheon.com>
2012-12-12 10:46:18 -0600

    Issue #1262:
    - We will now be building a 64-bit version of the awips2-notification rpm
    - Updated qpid-devel in nativeLib to match the version of qpid we are using
    - Added 64-bit libraries to thrift and qpid in nativeLib
    - The notification executables are now built from source out of nativeLib
    
    
    Former-commit-id: 94906521032dc606b8f8ca01df969e7aa07706ad [formerly 09702591148f2dc245c0b563788648ccd3301287] [formerly a210f08acc354449cc48e4a9976a434dfbe6222a [formerly 3205e61aaeba7ec7bccb0a4bd2ed8a6e83a77951]]
    Former-commit-id: a210f08acc354449cc48e4a9976a434dfbe6222a
    Former-commit-id: c3da463f39647af3c14ea20229707500dc255f47

--- nativeLib/org.apache.qpid/include/qpid/framing/ClusterConnectionShadowReadyBody.h
+++ nativeLib/org.apache.qpid/include/qpid/framing/ClusterConnectionShadowReadyBody.h
@@ -42,6 +42,7 @@ namespace framing {
 class ClusterConnectionShadowReadyBody : public ModelMethod {
     uint64_t memberId;
     uint64_t connectionId;
+    string managementId;
     string userName;
     string fragment;
     uint32_t sendMax;
@@ -52,11 +53,13 @@ public:
     ClusterConnectionShadowReadyBody(
         ProtocolVersion, uint64_t _memberId,
         uint64_t _connectionId,
+        const string& _managementId,
         const string& _userName,
         const string& _fragment,
         uint32_t _sendMax) : 
         memberId(_memberId),
         connectionId(_connectionId),
+        managementId(_managementId),
         userName(_userName),
         fragment(_fragment),
         sendMax(_sendMax),
@@ -66,6 +69,7 @@ public:
         flags |= (1 << 10);
         flags |= (1 << 11);
         flags |= (1 << 12);
+        flags |= (1 << 13);
     }
     ClusterConnectionShadowReadyBody(ProtocolVersion=ProtocolVersion())  : memberId(0), connectionId(0), sendMax(0), flags(0) {}
     
@@ -77,6 +81,10 @@ public:
     QPID_COMMON_EXTERN uint64_t getConnectionId() const;
     QPID_COMMON_EXTERN bool hasConnectionId() const;
     QPID_COMMON_EXTERN void clearConnectionIdFlag();
+    QPID_COMMON_EXTERN void setManagementId(const string& _managementId);
+    QPID_COMMON_EXTERN const string& getManagementId() const;
+    QPID_COMMON_EXTERN bool hasManagementId() const;
+    QPID_COMMON_EXTERN void clearManagementIdFlag();
     QPID_COMMON_EXTERN void setUserName(const string& _userName);
     QPID_COMMON_EXTERN const string& getUserName() const;
     QPID_COMMON_EXTERN bool hasUserName() const;
@@ -92,7 +100,7 @@ public:
     typedef void ResultType;
 
     template <class T> ResultType invoke(T& invocable) const {
-        return invocable.shadowReady(getMemberId(), getConnectionId(), getUserName(), getFragment(), getSendMax());
+        return invocable.shadowReady(getMemberId(), getConnectionId(), getManagementId(), getUserName(), getFragment(), getSendMax());
     }
 
     using  AMQMethodBody::accept;
