2100b136a9ea18d67bdfa8b46e59ce25a3d31e5e
Richard Peter <richard.peter@raytheon.com>
2012-12-16 19:52:11 -0600

    Revert "Issue #1262:"
    This reverts commit bad53d1b7fa8e5661220b4a6ce996280f83aac6c [formerly 94906521032dc606b8f8ca01df969e7aa07706ad] [formerly 09702591148f2dc245c0b563788648ccd3301287] [formerly c3da463f39647af3c14ea20229707500dc255f47 [formerly a210f08acc354449cc48e4a9976a434dfbe6222a] [formerly 09702591148f2dc245c0b563788648ccd3301287 [formerly 3205e61aaeba7ec7bccb0a4bd2ed8a6e83a77951]]]
    
    
    Former-commit-id: 9d03a53fb520ea0570ed6483cda71052f234b0b1 [formerly 9a7cfa2383eadbabb0536c16aef98a4eee96dfa4] [formerly f37f948456f1e167b03c5bf45e215b230b3f643c [formerly a4e51a9f814ff0e004be46460b6d27a848cb2fe9]]
    Former-commit-id: f37f948456f1e167b03c5bf45e215b230b3f643c
    Former-commit-id: 48641ca26fbbc3b97c8f6db5e77f5f41bc0c24ed

--- nativeLib/org.apache.qpid/include/qpid/messaging/Sender.h
+++ nativeLib/org.apache.qpid/include/qpid/messaging/Sender.h
@@ -21,74 +21,65 @@
  * under the License.
  *
  */
-#include "qpid/messaging/ImportExport.h"
-
-#include "qpid/messaging/Handle.h"
+#include "qpid/client/ClientImportExport.h"
+#include "qpid/client/Handle.h"
 #include "qpid/sys/IntegerTypes.h"
-
 #include <string>
 
 namespace qpid {
-namespace messaging {
+namespace client {
 
 template <class> class PrivateImplRef;
+
+}
+
+namespace messaging {
+
 class Message;
 class SenderImpl;
 class Session;
-/**   \ingroup messaging 
+/**
  * Interface through which messages are sent.
  */
-class Sender : public qpid::messaging::Handle<SenderImpl>
+class Sender : public qpid::client::Handle<SenderImpl>
 {
   public:
-    QPID_MESSAGING_EXTERN Sender(SenderImpl* impl = 0);
-    QPID_MESSAGING_EXTERN Sender(const Sender&);
-    QPID_MESSAGING_EXTERN ~Sender();
-    QPID_MESSAGING_EXTERN Sender& operator=(const Sender&);
+    QPID_CLIENT_EXTERN Sender(SenderImpl* impl = 0);
+    QPID_CLIENT_EXTERN Sender(const Sender&);
+    QPID_CLIENT_EXTERN ~Sender();
+    QPID_CLIENT_EXTERN Sender& operator=(const Sender&);
 
-    /**
-     * Sends a message
-     * 
-     * @param message the message to send
-     * @param sync if true the call will block until the server
-     * confirms receipt of the messages; if false will only block for
-     * available capacity (i.e. pending == capacity)
-     */
-    QPID_MESSAGING_EXTERN void send(const Message& message, bool sync=false);
-    QPID_MESSAGING_EXTERN void close();
+    QPID_CLIENT_EXTERN void send(const Message& message);
+    QPID_CLIENT_EXTERN void cancel();
 
     /**
      * Sets the capacity for the sender. The capacity determines how
      * many outgoing messages can be held pending confirmation of
      * receipt by the broker.
      */
-    QPID_MESSAGING_EXTERN void setCapacity(uint32_t);
+    QPID_CLIENT_EXTERN void setCapacity(uint32_t);
     /**
      * Returns the capacity of the sender. 
      * @see setCapacity
      */
-    QPID_MESSAGING_EXTERN uint32_t getCapacity();
+    QPID_CLIENT_EXTERN uint32_t getCapacity();
     /**
      * Returns the number of sent messages pending confirmation of
      * receipt by the broker. (These are the 'in-doubt' messages).
      */
-    QPID_MESSAGING_EXTERN uint32_t getUnsettled();
-    /**
-     * Returns the number of messages for which there is available
-     * capacity.
-     */
-    QPID_MESSAGING_EXTERN uint32_t getAvailable();
+    QPID_CLIENT_EXTERN uint32_t pending();
+
     /**
      * Returns the name of this sender.
      */
-    QPID_MESSAGING_EXTERN const std::string& getName() const;
+    QPID_CLIENT_EXTERN const std::string& getName() const;
 
     /**
      * Returns a handle to the session associated with this sender.
      */
-    QPID_MESSAGING_EXTERN Session getSession() const;
+    QPID_CLIENT_EXTERN Session getSession() const;
   private:
-  friend class qpid::messaging::PrivateImplRef<Sender>;
+  friend class qpid::client::PrivateImplRef<Sender>;
 };
 }} // namespace qpid::messaging
 
