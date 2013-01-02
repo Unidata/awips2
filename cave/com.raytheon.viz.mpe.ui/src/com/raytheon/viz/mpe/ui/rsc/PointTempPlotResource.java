cec66a1784d543cc1767694fb31d7f91c49f86a6
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-01-02 16:32:48 -0600

    Issue #1457 Initial changes for MPE time refactor.  Wrote custom resource that handles multiple times and tailored code to use resource instead of old XmrgResource which was not written to support multiple frames
    Amend: Cleaned up comments and some bugs in code after review
    
    Amend: Rebased changes
    
    Change-Id: I512b9bb30a08b9a1cfc5e8d9b5008ebc4f352c2d
    
    Former-commit-id: 320336f4ac908092f76380086fc9022c2ff9ff94 [formerly 320336f4ac908092f76380086fc9022c2ff9ff94 [formerly e5cd1901a50255c9f00b2cc717b5acee7c7db494]] [formerly ff72ce0b0cf5774c06fab51098d25d65b2ec95d8] [formerly c246f5656818d9e45f8075194451377859c58a52 [formerly c246f5656818d9e45f8075194451377859c58a52 [formerly db3e7bab996c007869d9342f122fe0153652afc4]] [formerly ff72ce0b0cf5774c06fab51098d25d65b2ec95d8 [formerly ff72ce0b0cf5774c06fab51098d25d65b2ec95d8 [formerly ab74da7808aa343983f532ee076820c7e63fdded]]]]
    Former-commit-id: c246f5656818d9e45f8075194451377859c58a52 [formerly c246f5656818d9e45f8075194451377859c58a52 [formerly db3e7bab996c007869d9342f122fe0153652afc4]]
    Former-commit-id: c246f5656818d9e45f8075194451377859c58a52
    Former-commit-id: 8aa2150505254828e8493417e73ef004108faa23 [formerly fac87693360157ce38a4df1998ccabb65d04b8d4]
    Former-commit-id: dd990ee5e59e834d14793fab5cdee95bce93c751

--- cave/com.raytheon.viz.mpe.ui/src/com/raytheon/viz/mpe/ui/rsc/PointTempPlotResource.java
+++ cave/com.raytheon.viz.mpe.ui/src/com/raytheon/viz/mpe/ui/rsc/PointTempPlotResource.java
@@ -555,7 +555,7 @@ public class PointTempPlotResource extends
         this.paintProps = paintProps;
         MPEDisplayManager displayMgr = getResourceData().getMPEDisplayManager();
         // Fonts are shared and cached, no need to init or dispose
-        font = fontFactory.getMPEFont(displayMgr.getFontState());
+        font = fontFactory.getMPEFont(MPEDisplayManager.getFontId());
 
         if (DailyQcUtils.points_flag == 1 && displayMgr.isMaxmin() == true) {
             Iterator<String> iter = dataMap.keySet().iterator();
