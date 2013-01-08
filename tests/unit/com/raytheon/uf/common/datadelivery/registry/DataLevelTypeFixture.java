4441a14560fbc8419555ce27c2acc7f82f57933e
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-01-08 11:55:11 -0600

    Issue #1453 Fix the handling of subscription active period dates.
    Amend:
      Move tests project over that was missed while moving datadelivery code
    
    Change-Id: I10611f8a4b6c2efe6af1813e5181fadc059e4f9a
    
    Former-commit-id: 5846ceb3578b9fad7d1c20a83282cb157700bd3c [formerly 9ea65bb8933f83979cc987449b89552bd6eed083] [formerly 9ea65bb8933f83979cc987449b89552bd6eed083 [formerly 1975cd04426af124b943ed713ea7bdc0eb6e4e8d]] [formerly 6a9077921483d19fece673b57d66cb41311e14d1] [formerly 492cee49926d88b772251617b6f2db7455596ebf [formerly 492cee49926d88b772251617b6f2db7455596ebf [formerly 2635e057231bd16debb18bc268d47c86f90eb3fd]] [formerly 6a9077921483d19fece673b57d66cb41311e14d1 [formerly 6a9077921483d19fece673b57d66cb41311e14d1 [formerly 0e1397db6ca1a7a577d20507bc2c4daf823188fe]]]]
    Former-commit-id: 492cee49926d88b772251617b6f2db7455596ebf [formerly 492cee49926d88b772251617b6f2db7455596ebf [formerly 2635e057231bd16debb18bc268d47c86f90eb3fd]]
    Former-commit-id: 492cee49926d88b772251617b6f2db7455596ebf
    Former-commit-id: aba03272005ab51722f3ae499979c4d47446dce0 [formerly 885f315d3035c285c6a7449f6c4aa92487b268b4]
    Former-commit-id: 9fa5e6e5967950acbc67c83f35a9e4884a6cbf5b

---
+++ tests/unit/com/raytheon/uf/common/datadelivery/registry/DataLevelTypeFixture.java
@@ -0,0 +1,63 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.common.datadelivery.registry;
+
+import com.raytheon.uf.common.util.AbstractFixture;
+
+/**
+ * {@link AbstractFixture} implementation for {@link DataLevelType} objects.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Dec 07, 2012 1104      djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+
+public class DataLevelTypeFixture extends AbstractFixture<DataLevelType> {
+
+    public static final DataLevelTypeFixture INSTANCE = new DataLevelTypeFixture();
+
+    /**
+     * Disabled constructor.
+     */
+    private DataLevelTypeFixture() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DataLevelType get(long seedValue) {
+        DataLevelType obj = new DataLevelType();
+
+        // TODO: Populate attributes
+
+        return obj;
+    }
+
+}
