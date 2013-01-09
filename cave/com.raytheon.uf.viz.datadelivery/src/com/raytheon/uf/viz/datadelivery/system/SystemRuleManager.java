660064ea6affce535f776f4e05cff894f64d1248
Mike Duff <michael.duff@raytheon.com>
2013-01-09 08:38:47 -0600

    Issue #1420 - Rules Peer review comments
    Former-commit-id: f41d525fa31c9725654bc474fd4c7d119b3df3c5 [formerly 4c63333a965dc033fe51299d3df50dd6f7a9112a] [formerly f41d525fa31c9725654bc474fd4c7d119b3df3c5 [formerly 4c63333a965dc033fe51299d3df50dd6f7a9112a] [formerly 6e0367cdc7dd7704d1de782f4882b216212514b0 [formerly 9f6756325c5788dd1b98d8f24ab3437993da7dcd]]]
    Former-commit-id: 6e0367cdc7dd7704d1de782f4882b216212514b0
    Former-commit-id: ed95eae2e322e9647865fd3f725bcfb077430308 [formerly 67486aa43b82dcd5230f231cbff46eec30993ddd]
    Former-commit-id: 8313877a3af4f0087a2d6884d410c8517280b1ba

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/system/SystemRuleManager.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/system/SystemRuleManager.java
@@ -21,15 +21,13 @@ package com.raytheon.uf.viz.datadelivery.system;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Shell;
 
 import com.raytheon.uf.common.datadelivery.bandwidth.IBandwidthService;
 import com.raytheon.uf.common.datadelivery.registry.Network;
@@ -44,8 +42,15 @@ import com.raytheon.uf.common.localization.exception.LocalizationOpFailedExcepti
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
-import com.raytheon.uf.viz.datadelivery.subscription.xml.SubscriptionRuleXML;
-import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.LatencyRuleXML;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.LatencyRulesXML;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.PriorityRuleXML;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.PriorityRulesXML;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.RuleXML;
+import com.raytheon.uf.viz.datadelivery.subscription.xml.RulesXML;
+import com.raytheon.uf.viz.datadelivery.utils.DataSetFrequency;
+import com.raytheon.uf.viz.datadelivery.utils.NameOperationItems;
+import com.raytheon.uf.viz.datadelivery.utils.TypeOperationItems;
 
 /**
  * System Rule Manager.
@@ -57,7 +62,8 @@ import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Sep 17, 2012    730      jpiatt     Initial creation.
- * Oct 23, 2012 1286        djohnson   Hook into bandwidth management.
+ * Oct 23, 2012   1286      djohnson   Hook into bandwidth management.
+ * Jan 04, 2013   1420      mpduff     Move rules into a single file.
  * 
  * </pre>
  * 
@@ -67,13 +73,19 @@ import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
 public class SystemRuleManager {
 
     /** SystemRuleManager instance */
-    private static SystemRuleManager instance = new SystemRuleManager();
+    private static final SystemRuleManager instance = new SystemRuleManager();
 
     /** Directory Path to rules */
-    private final String RULE_PATH = "dataDelivery" + File.separator + "rules" + File.separator;
+    private final String RULE_PATH = "datadelivery" + File.separator
+            + "systemManagement" + File.separator + "rules" + File.separator;
+
+    private final String LATENCY_RULE_FILE = RULE_PATH + "latencyRules.xml";
+
+    private final String PRIORITY_RULE_FILE = RULE_PATH + "priorityRules.xml";
 
     /** Status Handler */
-    private final IUFStatusHandler statusHandler = UFStatus.getHandler(SystemRuleManager.class);
+    private final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(SystemRuleManager.class);
 
     /** JAXB context */
     private JAXBContext jax;
@@ -84,9 +96,6 @@ public class SystemRuleManager {
     /** Unmarshaller object */
     private Unmarshaller unmarshaller;
 
-    /** Array of rule localization files */
-    private LocalizationFile[] ruleFiles;
-
     private IBandwidthService bandwidthService;
 
     /**
@@ -95,7 +104,7 @@ public class SystemRuleManager {
     private SystemRuleManager() {
         createContext();
     }
-    
+
     /**
      * Get an instance of the SystemRuleManager.
      * 
@@ -110,178 +119,74 @@ public class SystemRuleManager {
      */
     @SuppressWarnings("rawtypes")
     private void createContext() {
-        Class[] classes = new Class[] { SubscriptionRuleXML.class };
+        Class[] classes = new Class[] { RuleXML.class, PriorityRuleXML.class,
+                LatencyRuleXML.class, LatencyRulesXML.class,
+                PriorityRulesXML.class, RulesXML.class, OperatorTypes.class,
+                TypeOperationItems.class, NameOperationItems.class };
 
         try {
             jax = JAXBContext.newInstance(classes);
             this.unmarshaller = jax.createUnmarshaller();
             this.marshaller = jax.createMarshaller();
             this.marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        } catch (JAXBException e) {
+        } catch (Exception e) {
             statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
         }
     }
 
     /**
-     * Get priority file names
+     * Get the names of the priority rules
      * 
-     * @return String[]
-     *           Array of priority rule names
-     * @throws JAXBException 
+     * @return String[] of names
+     * @throws JAXBException
      */
-    public String[] getPriorityRules() throws JAXBException {
+    public List<String> getPriorityRuleNames() {
+        return getPriorityRules().getRuleNames();
+    }
 
-        ArrayList<String> priorityList = new ArrayList<String>();
-        ruleFiles = getRules();
-
-        //determine which are priority files
-        for (LocalizationFile lf : ruleFiles) {
-            try {
-                SubscriptionRuleXML xml = (SubscriptionRuleXML) unmarshaller.unmarshal(lf.getFile());
-                Integer priority = xml.getRulePriority();
-
-                if (priority != null) {
-                    String lfName = lf.getFile().getName();
-                    priorityList.add(lfName);
-                } 
-
-            } catch (JAXBException e) {
-                statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
+    public PriorityRuleXML loadPriorityRule(String name) {
+        PriorityRulesXML priorityRules = getPriorityRules();
+        for (PriorityRuleXML rule : priorityRules.getRules()) {
+            if (rule.getRuleName().equals(name)) {
+                return rule;
             }
         }
 
-        String[] priorityRules = null;
+        return new PriorityRuleXML();
+    }
 
-        if (priorityList.size() > 0) {
-            priorityRules = priorityList.toArray(new String[priorityList.size()]);
+    public LatencyRuleXML loadLatencyRule(String name) {
+        LatencyRulesXML latencyRules = getLatencyRules();
+        for (LatencyRuleXML rule : latencyRules.getRules()) {
+            if (rule.getRuleName().equals(name)) {
+                return rule;
+            }
         }
 
-        return priorityRules;
-
+        return new LatencyRuleXML();
     }
 
     /**
      * Get latency file names
      * 
-     * @return String[]
-     *           Array of latency rule names
-     * @throws JAXBException 
+     * @return String[] Array of latency rule names
+     * @throws JAXBException
      */
-    public String[] getLatencyRules() throws JAXBException {
-
-        ArrayList<String> latencyList = new ArrayList<String>();
-        ruleFiles = getRules();
-
-        //need to determine which are latency files
-        for (LocalizationFile lf : ruleFiles) {
-            try {
-                SubscriptionRuleXML xml = (SubscriptionRuleXML) unmarshaller.unmarshal(lf.getFile());
-                Integer latency = xml.getRuleLatency();
-
-                if (latency != null) {
-                    String lfName = lf.getFile().getName();
-                    latencyList.add(lfName);
-                }
-
-
-            } catch (JAXBException e) {
-                statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
-            }
-        }
-
-        String[] latencyRules = null;
-
-        if (!latencyList.isEmpty()) {
-            latencyRules = latencyList.toArray(new String[latencyList.size()]);
-        }
-
-        return latencyRules;
-
+    public List<String> getLatencyRuleNames() {
+        return getLatencyRules().getRuleNames();
     }
 
-    /**
-     * Get rule files
-     * 
-     * @return String[]
-     *           Array of latency rule names
-     * @throws JAXBException 
-     */
-    public LocalizationFile[] getRules() throws JAXBException {
+    public boolean savePriorityRules(PriorityRulesXML xmlObj) {
         IPathManager pm = PathManagerFactory.getPathManager();
 
-        LocalizationContext context = pm.getContext(LocalizationType.CAVE_STATIC, LocalizationLevel.USER);
-        String[] extensions = new String[] { "xml" };
-        ruleFiles = pm.listFiles(context, RULE_PATH, extensions, false, true);
-
-        return ruleFiles;
-
-    }
-
-    /**
-     * Load a saved rule into memory
-     * 
-     * @param ruleName
-     *            The subset name
-     * 
-     * @return The SubscriptionRuleXML object
-     */
-    public SubscriptionRuleXML loadRule(String ruleName) {
-        // Load the rule file
-        if (ruleFiles == null || ruleFiles.length == 0) {
-            try {
-                getRules();
-            } catch (JAXBException e) {
-                statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
-            }
-        }
-
-        for (LocalizationFile lf : ruleFiles) {
-            if (lf.getFile().getName().equals(ruleName + ".xml")) {
-                try {
-                    return (SubscriptionRuleXML) unmarshaller.unmarshal(lf.getFile());
-                } catch (JAXBException e) {
-                    statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(), e);
-                }
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * Save a rule xml object.
-     * 
-     * @param rule
-     *            the object to save
-     * @param shell 
-     * @param create false for edit
-     * @return true if successfully saved
-     */
-    public boolean saveRule(SubscriptionRuleXML rule, Shell shell) {
-
-        String ruleName = rule.getRuleName();
-
-        if (!ruleName.endsWith("xml")) {
-            rule.setRuleName((ruleName) + ".xml");
-        }
-
-        IPathManager pm = PathManagerFactory.getPathManager();
-
-        LocalizationContext context = pm.getContext(LocalizationType.CAVE_STATIC, LocalizationLevel.USER);
-        LocalizationFile subscriptionRuleLocFile = pm.getLocalizationFile(context, RULE_PATH + rule.getRuleName());
-
-        if (subscriptionRuleLocFile.getFile().exists()) {
-                String msg = "The file " + subscriptionRuleLocFile.getFile().getName()
-                + " already exists.\n\nWould you like to overwrite the file?";
-                int response = DataDeliveryUtils.showMessage(shell, SWT.YES | SWT.NO, "File Exists", msg);
-                if (response == SWT.NO) {
-                    return false;
-                }
-        } 
+        LocalizationContext context = pm.getContext(
+                LocalizationType.COMMON_STATIC, LocalizationLevel.SITE);
+        LocalizationFile priorityRulesLocFile = pm.getLocalizationFile(context,
+                this.PRIORITY_RULE_FILE);
 
         try {
-            marshaller.marshal(rule, subscriptionRuleLocFile.getFile());
-            subscriptionRuleLocFile.save();
+            marshaller.marshal(xmlObj, priorityRulesLocFile.getFile());
+            priorityRulesLocFile.save();
             return true;
         } catch (JAXBException e) {
             statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
@@ -290,6 +195,234 @@ public class SystemRuleManager {
         }
 
         return false;
+    }
+
+    public boolean saveLatencyRules(LatencyRulesXML xmlObj) {
+        IPathManager pm = PathManagerFactory.getPathManager();
+
+        LocalizationContext context = pm.getContext(
+                LocalizationType.COMMON_STATIC, LocalizationLevel.SITE);
+        LocalizationFile latencyRulesLocFile = pm.getLocalizationFile(context,
+                this.LATENCY_RULE_FILE);
+
+        try {
+            marshaller.marshal(xmlObj, latencyRulesLocFile.getFile());
+            latencyRulesLocFile.save();
+            return true;
+        } catch (JAXBException e) {
+            statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
+        } catch (LocalizationOpFailedException e) {
+            statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
+        }
+
+        return false;
+    }
+
+    public void deleteLatencyRule(String ruleName) {
+        LatencyRulesXML latencyRules = getLatencyRules();
+
+        for (LatencyRuleXML rule : latencyRules.getRules()) {
+            if (rule.getRuleName().equals(ruleName)) {
+                latencyRules.removeRuleByName(ruleName);
+                saveLatencyRules(latencyRules);
+                return;
+            }
+        }
+    }
+
+    public void deletePriorityRule(String ruleName) {
+        PriorityRulesXML priorityRules = getPriorityRules();
+
+        for (PriorityRuleXML rule : priorityRules.getRules()) {
+            if (rule.getRuleName().equals(ruleName)) {
+                priorityRules.removeRuleByName(ruleName);
+                savePriorityRules(priorityRules);
+                return;
+            }
+        }
+    }
+
+    public boolean updateRule(LatencyRuleXML rule) {
+        LatencyRulesXML rulesXml = getLatencyRules();
+        boolean saved = rulesXml.updateRule(rule);
+        if (saved) {
+            return saveLatencyRules(rulesXml);
+        }
+
+        return false;
+    }
+
+    public boolean updateRule(PriorityRuleXML rule) {
+        PriorityRulesXML rulesXml = getPriorityRules();
+        boolean saved = rulesXml.updateRule(rule);
+        if (saved) {
+            saved = savePriorityRules(rulesXml);
+        }
+
+        if (!saved) {
+            this.statusHandler.warn("Error saving rules.");
+        }
+
+        return saved;
+    }
+
+    public boolean saveRule(PriorityRuleXML rule) {
+        PriorityRulesXML rulesXml = getPriorityRules();
+        boolean saved = rulesXml.addRule(rule);
+        if (saved) {
+            saved = savePriorityRules(rulesXml);
+        }
+
+        if (!saved) {
+            this.statusHandler.warn("Error saving Priority rules.");
+        }
+
+        return saved;
+    }
+
+    public boolean saveRule(LatencyRuleXML rule) {
+        LatencyRulesXML rulesXml = getLatencyRules();
+        boolean saved = rulesXml.addRule(rule);
+        if (saved) {
+            saved = saveLatencyRules(rulesXml);
+        }
+
+        if (!saved) {
+            this.statusHandler.warn("Error saving Latency rules.");
+        }
+
+        return saved;
+    }
+
+    private LatencyRulesXML getLatencyRules() {
+        LocalizationFile lfile = getRules(this.LATENCY_RULE_FILE);
+
+        LatencyRulesXML latencyRules = new LatencyRulesXML();
+        if (lfile != null && lfile.exists()) {
+            try {
+                latencyRules = (LatencyRulesXML) unmarshaller.unmarshal(lfile
+                        .getFile());
+            } catch (JAXBException e) {
+                statusHandler
+                        .handle(Priority.ERROR, e.getLocalizedMessage(), e);
+            }
+        }
+
+        return latencyRules;
+    }
+
+    private PriorityRulesXML getPriorityRules() {
+        LocalizationFile lfile = getRules(this.PRIORITY_RULE_FILE);
+
+        PriorityRulesXML priorityRules = new PriorityRulesXML();
+        if (lfile != null && lfile.exists()) {
+            try {
+                priorityRules = (PriorityRulesXML) unmarshaller.unmarshal(lfile
+                        .getFile());
+            } catch (JAXBException e) {
+                statusHandler
+                        .handle(Priority.ERROR, e.getLocalizedMessage(), e);
+            }
+        }
+        return priorityRules;
+    }
+
+    private LocalizationFile getRules(String name) {
+        IPathManager pm = PathManagerFactory.getPathManager();
+        return pm.getStaticLocalizationFile(name);
+    }
+
+    /**
+     * Get the default latency.
+     * 
+     * @param DataSetFrequency
+     *            Frequency of the data
+     * 
+     * @return The default latency
+     */
+    public int getDefaultLatency(DataSetFrequency freq) {
+        int frequency = 40;
+        switch (freq) {
+        case HOURLY:
+            frequency = 40;
+            break;
+        case SIX_HOURLY:
+            frequency = 115;
+            break;
+        }
+
+        return frequency;
+    }
+
+    /**
+     * Get the default latency value given the cycleTimes.
+     * 
+     * @param cycleTimes
+     * @return
+     */
+    public int getDefaultLatency(List<Integer> cycleTimes) {
+        DataSetFrequency freq = DataSetFrequency.fromCycleTimes(cycleTimes);
+
+        return getDefaultLatency(freq);
+    }
+
+    /**
+     * Return the lowest latency value defined by the rules.
+     * 
+     * @param sub
+     *            The subscription
+     * @param cycleTimes
+     *            The available cycle times
+     * @return
+     */
+    public int getLatency(Subscription sub, Set<Integer> cycleTimes) {
+        LatencyRulesXML rulesXml = this.getLatencyRules();
+        int latency = 999;
+        boolean found = false;
+        for (LatencyRuleXML rule : rulesXml.getRules()) {
+            if (rule.matches(sub, cycleTimes)) {
+                if (rule.getLatency() < latency) {
+                    latency = rule.getLatency();
+                    found = true;
+                }
+            }
+        }
+
+        // Set default if none found
+        if (!found) {
+            latency = this
+                    .getDefaultLatency(new ArrayList<Integer>(cycleTimes));
+        }
+
+        return latency;
+    }
+
+    /**
+     * Return the lowest priority value defined by the rules.
+     * 
+     * @param sub
+     * @param cycleTimes
+     * @return
+     */
+    public int getPriority(Subscription sub, Set<Integer> cycleTimes) {
+        PriorityRulesXML rulesXml = this.getPriorityRules();
+        int priority = 3;
+        boolean found = false;
+        for (PriorityRuleXML rule : rulesXml.getRules()) {
+            if (rule.matches(sub, cycleTimes)) {
+                if (rule.getPriority() < priority) {
+                    priority = rule.getPriority();
+                    found = true;
+                }
+            }
+        }
+
+        // Default to normal priority
+        if (!found) {
+            priority = 2;
+        }
+
+        return priority;
     }
 
     /**
