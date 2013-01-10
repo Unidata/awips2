9896a4877d82d61bf37df5218835d9978a8a1b38
Mike Duff <michael.duff@raytheon.com>
2013-01-10 14:03:14 -0600

    Issue #1420 - Additional changes and cleanup Issue #1420 - remove commented out code, move method Issue #1420 - fix for max latency Change-Id: I8a983812e4fab7477659d7f175eaa005bb92f064
    Former-commit-id: 6f04b5d915b718f381a34fb0038bda2f85f365f2 [formerly 6f04b5d915b718f381a34fb0038bda2f85f365f2 [formerly 329a010b07caf0930ec54cf472c953dbb5baaa1d]] [formerly 279729e88e42bc15a63e29dfe2500d06bc6810e3] [formerly 6f04b5d915b718f381a34fb0038bda2f85f365f2 [formerly 6f04b5d915b718f381a34fb0038bda2f85f365f2 [formerly 329a010b07caf0930ec54cf472c953dbb5baaa1d]] [formerly 279729e88e42bc15a63e29dfe2500d06bc6810e3] [formerly 950c71c164c58a442c5b9b7304f84709be4a3b64 [formerly 950c71c164c58a442c5b9b7304f84709be4a3b64 [formerly 8411a46e3e6f0db8a4d96b78b8db09cf45083b08]] [formerly 279729e88e42bc15a63e29dfe2500d06bc6810e3 [formerly 279729e88e42bc15a63e29dfe2500d06bc6810e3 [formerly d69e77e9b902d164a5716e432f62bd99e1b784dd]]]]]
    Former-commit-id: 950c71c164c58a442c5b9b7304f84709be4a3b64 [formerly 950c71c164c58a442c5b9b7304f84709be4a3b64 [formerly 8411a46e3e6f0db8a4d96b78b8db09cf45083b08]]
    Former-commit-id: 14fe18f31e3f58a3bce7173da9eb9dc8105f06ad [formerly abcf507f9cc7cb1de67b9ed581afe053afd0b344] [formerly 5f9ba27b7fbf053687ee4d2a0db9f479ae15257c [formerly d3ed6f61488d3a8c62903fcff4bc6a80ef6e0656]] [formerly 950c71c164c58a442c5b9b7304f84709be4a3b64]
    Former-commit-id: 775491599ea378cf03a106d88fee2cc2799a4212 [formerly 0a79c64278d2232a27fefc79b0809008651153dd] [formerly c128136495f47afcf24e92e7ff61b96f5b3bd92f [formerly 772154de38333a6fdfc38a665ca5f44c6cfe7371]]
    Former-commit-id: a9ec92e98506fc22a626fbc0299b47342cd58af5 [formerly 66dc038d024589cd59f3ddf27df4842257239f2a]
    Former-commit-id: 6980325f07ffe587de2279002b28a260e3fab1c3

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/utils/TypeOperationItems.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/utils/TypeOperationItems.java
@@ -1,3 +1,22 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.uf.viz.datadelivery.utils;
 
 import javax.xml.bind.annotation.XmlEnum;
@@ -6,7 +25,22 @@ import javax.xml.bind.annotation.XmlType;
 
 import com.raytheon.uf.viz.datadelivery.system.Operator;
 
-/** Enumeration to use for Datatype operations */
+/**
+ * Data Type Operation Enum.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jan 8, 2013    1420     mpduff      Initial creation.
+ * 
+ * </pre>
+ * 
+ * @author mpduff
+ * @version 1.0
+ */
 @XmlType(name = "typeOperationItems")
 @XmlEnum
 public enum TypeOperationItems implements Operator<String> {
