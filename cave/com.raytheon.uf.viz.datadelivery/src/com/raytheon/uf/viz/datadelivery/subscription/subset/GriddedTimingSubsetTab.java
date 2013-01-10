16a86f47e6ff55ab8dff022680b329405cfdb08e
Mike Duff <michael.duff@raytheon.com>
2013-01-10 10:19:09 -0600

    Issue #1444 - Fix updating subsets by loading a saved subset from the saved subset tab. Change-Id: Ibc6b93fd8a9d7a9fdf07b28d77f4f32e706ae05a
    Former-commit-id: 4cbe807a52386afaab3134d917580267bcb87d8b [formerly 843a771d3c6247326d1e832eb598072f7c313499]
    Former-commit-id: 35d73f035498f4df533c8288d8d7b3bc46011459

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/GriddedTimingSubsetTab.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/GriddedTimingSubsetTab.java
@@ -20,6 +20,8 @@
 package com.raytheon.uf.viz.datadelivery.subscription.subset;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
@@ -37,26 +39,27 @@ import com.raytheon.viz.ui.widgets.duallist.DualListConfig;
 
 /**
  * {@link IDataTimingSubsetView} implementation for Gridded data.
- *
+ * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Aug 22, 2012 0743       djohnson     Initial creation
  * Aug 29, 2012 0223       mpduff       Removed cycle times.
  * Sep 07, 2012 0684       mpduff       Clear fcstHour selection before setting new selection.
  * Sep 24, 2012 1209       djohnson     Display text when there are no available cycles, move validation to presenter.
+ * Jan 10, 2013 1444       mpduff       Add updateSelectedForecastHours method.
  * 
  * </pre>
- *
+ * 
  * @author djohnson
  * @version 1.0
  */
 
-public class GriddedTimingSubsetTab extends DataTimingSubsetTab
-        implements IGriddedDataTimingSubsetView {
+public class GriddedTimingSubsetTab extends DataTimingSubsetTab implements
+        IGriddedDataTimingSubsetView {
 
     /** Forecast dual list */
     private DualList fcstDualList;
@@ -68,14 +71,14 @@ public class GriddedTimingSubsetTab extends DataTimingSubsetTab
 
     /**
      * Constructor.
-     *
+     * 
      * @param parentComp
      * @param dataSet
      * @param callback
      * @param shell
      */
-    public GriddedTimingSubsetTab(Composite parentComp,
-            IDataSize callback, Shell shell) {
+    public GriddedTimingSubsetTab(Composite parentComp, IDataSize callback,
+            Shell shell) {
         super(parentComp, callback, shell);
     }
 
@@ -111,7 +114,7 @@ public class GriddedTimingSubsetTab extends DataTimingSubsetTab
     }
 
     /**
-     *
+     * 
      * {@inheritDoc}
      */
     @Override
@@ -132,8 +135,45 @@ public class GriddedTimingSubsetTab extends DataTimingSubsetTab
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setAvailableForecastHours(List<String> forecastHours) {
         fcstDualList.setFullList(new ArrayList<String>(forecastHours));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateSelectedForecastHours(List<String> fcstHours) {
+        List<String> selectedHrs = new ArrayList<String>();
+        String[] selectedItems = fcstDualList.getSelectedSelection();
+
+        // Add the saved hours
+        for (String fcstHr : fcstDualList.getAvailableListItems()) {
+            if (fcstHours.contains(fcstHr)) {
+                selectedHrs.add(fcstHr);
+            }
+        }
+
+        // Add in the previously selected hours
+        selectedHrs.addAll(Arrays.asList(selectedItems));
+
+        // Sort the hours
+        List<Integer> intList = new ArrayList<Integer>();
+        for (String hr : selectedHrs) {
+            intList.add(Integer.parseInt(hr));
+        }
+
+        Collections.sort(intList);
+
+        selectedHrs.clear();
+        for (int i : intList) {
+            selectedHrs.add(String.valueOf(i));
+        }
+        fcstDualList.selectItems(selectedHrs.toArray(new String[selectedHrs
+                .size()]));
     }
 }
