bf911de7016bdf1b59bd6785d9a4dfa4c51deabd
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-01-10 11:33:21 -0600

    Issue #1414 - adding Satellite to the Data Access Framework
    - addressed peer review comments
    - includes the CAVE generic grid resource and resource data
    - the generic grid resource now utilizes the populateData method of IGridData
    - the data time methods of the satellite factory will now throw data retrieval exceptions
    
    Change-Id: I763cddc8ba8cac715eb0605594904bd9f9c21db9
    
    Former-commit-id: fda4bb74482d2602c3ea4f8fe3326f7acd09970c [formerly fda4bb74482d2602c3ea4f8fe3326f7acd09970c [formerly a315e4461b666242d6301617614be4299e1af4c6]] [formerly 4939e96872eda8ef7c536b8205ab501f73b5e4ea] [formerly fda4bb74482d2602c3ea4f8fe3326f7acd09970c [formerly fda4bb74482d2602c3ea4f8fe3326f7acd09970c [formerly a315e4461b666242d6301617614be4299e1af4c6]] [formerly 4939e96872eda8ef7c536b8205ab501f73b5e4ea] [formerly 53a280116bb02bd95b49fc53a2738b530a0fdfac [formerly 53a280116bb02bd95b49fc53a2738b530a0fdfac [formerly 789359a7795d8e5d61f275ec6ce1baaae0f2fdaa]] [formerly 4939e96872eda8ef7c536b8205ab501f73b5e4ea [formerly 4939e96872eda8ef7c536b8205ab501f73b5e4ea [formerly b084e4a721edc9400fc7a8f25c7da539d6dd8480]]]]]
    Former-commit-id: 53a280116bb02bd95b49fc53a2738b530a0fdfac [formerly 53a280116bb02bd95b49fc53a2738b530a0fdfac [formerly 789359a7795d8e5d61f275ec6ce1baaae0f2fdaa]]
    Former-commit-id: c4598b71f8fa71f3ac2a8ac94de9fee9f889abfc [formerly 535d83dfebc765bf4b65fbfc491babfdd6f21993] [formerly 0f76b089e708baeec3e2f85f9662f9e8e656c536 [formerly 03ccca567a1677b741f19873a322e9364d3f1a52]] [formerly 53a280116bb02bd95b49fc53a2738b530a0fdfac]
    Former-commit-id: 0a4788699429d96b1ae095a87ee12ff729e5a635 [formerly 9e6a07e0cd3a19ce34eb5273a9ca2f44722e993e] [formerly 31410ff5cb1e9275a4c83f7f14e1aa751de3c244 [formerly e443218bfb2ab505d637a1a36d422ab9c7ab8368]]
    Former-commit-id: f339f07883f3e41df10594060034222c400a4818 [formerly f99ad00934f98c5b5cddb9748d1ea3fd29e63e79]
    Former-commit-id: f2ae31b9b806e0b1a401bff8d5f6fa51cfb0188c

---
+++ cave/com.raytheon.viz.dataaccess/src/com/raytheon/viz/dataaccess/rsc/GenericGridResource.java
@@ -0,0 +1,170 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.viz.dataaccess.rsc;
+
+import java.nio.Buffer;
+
+import org.geotools.coverage.grid.GridGeometry2D;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.drawables.ColorMapLoader;
+import com.raytheon.uf.viz.core.drawables.ColorMapParameters;
+import com.raytheon.uf.viz.core.drawables.PaintProperties;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.core.map.MapDescriptor;
+import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
+import com.raytheon.uf.viz.core.rsc.LoadProperties;
+import com.raytheon.uf.viz.core.rsc.capabilities.ColorMapCapability;
+import com.raytheon.uf.viz.core.style.level.SingleLevel;
+import com.raytheon.uf.common.dataaccess.grid.IGridData;
+import com.raytheon.uf.common.geospatial.interpolation.data.ByteBufferWrapper;
+import com.raytheon.uf.common.time.DataTime;
+import com.raytheon.viz.core.drawables.ColorMapParameterFactory;
+import com.raytheon.viz.core.rsc.displays.GriddedImageDisplay2;
+
+/**
+ * Renders a generic grid image based on grid data that is retrieved using the
+ * Data Access Framework.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jan 8, 2013            bkowal     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bkowal
+ * @version 1.0
+ */
+
+public class GenericGridResource extends
+        AbstractVizResource<GenericGridResourceData, MapDescriptor> {
+
+    private static final String GRID_COLORMAP = "Grid/gridded data";
+
+    private static final String GENERIC_GRID_LEGEND_TEXT = "Generic Grid ";
+
+    private static final String NODATA_LEGEND_TEXT = GENERIC_GRID_LEGEND_TEXT
+            + " No Data Available";
+
+    private GriddedImageDisplay2 griddedImageDisplay;
+
+    private boolean noData = false;
+
+    private Buffer buffer = null;
+
+    private GridGeometry2D gridGeometry;
+
+    private DataTime dataTime;
+
+    protected GenericGridResource(GenericGridResourceData resourceData,
+            LoadProperties loadProperties) {
+        super(resourceData, loadProperties);
+        this.griddedImageDisplay = null;
+    }
+
+    /**
+     * Initializes the data buffer, the geometry, and prepares the color map.
+     * Since this is generic grid data, the color map will always be set to
+     * "Grid/gridded data"
+     * 
+     * @throws VizException
+     */
+    private void prepareData() throws VizException {
+        IGridData gridData = this.resourceData.getGridData();
+        if (gridData == null) {
+            this.noData = true;
+            return;
+        }
+
+        this.gridGeometry = gridData.getGridGeometry();
+
+        // Extract the raw data
+        ByteBufferWrapper byteBufferWrapper = new ByteBufferWrapper(
+                this.gridGeometry);
+        byteBufferWrapper = gridData.populateData(byteBufferWrapper);
+        this.buffer = byteBufferWrapper.getBuffer();
+
+        this.dataTime = gridData.getDataTime();
+
+        // Prepare the Color Map
+        SingleLevel singleLevel = null;
+        if ((gridData.getLevel() == null) == false) {
+            // TODO: convert level to single level
+        }
+        ColorMapParameters colorMapParameters = ColorMapParameterFactory.build(
+                this.buffer.array(), gridData.getParameter(),
+                gridData.getUnit(), singleLevel);
+
+        colorMapParameters.setColorMapName(GRID_COLORMAP);
+        colorMapParameters.setColorMap(ColorMapLoader
+                .loadColorMap(colorMapParameters.getColorMapName()));
+        getCapability(ColorMapCapability.class).setColorMapParameters(
+                colorMapParameters);
+    }
+
+    @Override
+    public String getName() {
+        if (this.noData) {
+            return NODATA_LEGEND_TEXT;
+        }
+
+        return GENERIC_GRID_LEGEND_TEXT + this.dataTime.getLegendString();
+    }
+
+    @Override
+    protected void disposeInternal() {
+        if ((this.griddedImageDisplay == null) == false) {
+            this.griddedImageDisplay.dispose();
+        }
+        this.griddedImageDisplay = null;
+    }
+
+    @Override
+    protected void paintInternal(IGraphicsTarget target,
+            PaintProperties paintProps) throws VizException {
+        if (this.noData) {
+            return;
+        }
+
+        if (this.griddedImageDisplay == null) {
+            this.griddedImageDisplay = new GriddedImageDisplay2(this.buffer,
+                    this.gridGeometry, this);
+        }
+        this.griddedImageDisplay.paint(target, paintProps);
+    }
+
+    @Override
+    public void project(CoordinateReferenceSystem mapData) throws VizException {
+        if ((this.griddedImageDisplay == null) == false) {
+            this.griddedImageDisplay.dispose();
+            this.griddedImageDisplay = null;
+        }
+    }
+
+    @Override
+    protected void initInternal(IGraphicsTarget target) throws VizException {
+        this.prepareData();
+    }
+}
