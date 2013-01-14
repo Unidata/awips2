5cd31fc0b719f1b2e40c04694de2ca29259b2110
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-01-14 16:07:15 -0600

    Issue #1286 Fix bugs found during 1286 testing
    Amend:
      Add time domain code.
    
    Change-Id: Ib62ea23571a7e80877f8090064a8638e111fd2d2
    
    Former-commit-id: 3d74a68e947b045e9ae4f90b8813b775393e1f1d [formerly 3d74a68e947b045e9ae4f90b8813b775393e1f1d [formerly 9b79cfc11541c737087fb613c686d50cf79e0a7b]]
    Former-commit-id: 2cf52d976bd132a7b405ac06a6fbb6e6f51f43db
    Former-commit-id: aa7acedf0a3919b928b441f536254b3aa00c421f

---
+++ tests/unit/com/raytheon/uf/viz/datadelivery/subscription/xml/OperatorAdapterTest.java
@@ -0,0 +1,75 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.datadelivery.subscription.xml;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.raytheon.uf.viz.datadelivery.system.Operator;
+import com.raytheon.uf.viz.datadelivery.system.OperatorTypes;
+import com.raytheon.uf.viz.datadelivery.utils.NameOperationItems;
+import com.raytheon.uf.viz.datadelivery.utils.TypeOperationItems;
+
+/**
+ * Test {@link OperatorAdapter}.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jan 14, 2013 1286       djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+@SuppressWarnings({ "unchecked", "rawtypes" })
+public class OperatorAdapterTest {
+
+    @Test
+    public void testMarshalUnmarshalOperatorNameOperationItems() {
+        verifyOperatorsUnmarshalAsSameOperator(NameOperationItems.values());
+    }
+
+    @Test
+    public void testMarshalUnmarshalOperatorOperatorTypes() {
+        verifyOperatorsUnmarshalAsSameOperator(OperatorTypes.values());
+    }
+
+    @Test
+    public void testMarshalUnmarshalOperatorTypeOperationItems() {
+        verifyOperatorsUnmarshalAsSameOperator(TypeOperationItems.values());
+    }
+
+    private void verifyOperatorsUnmarshalAsSameOperator(Operator... operators) {
+        for (Operator operator : operators) {
+            assertThat(operator,
+                    is(sameInstance(OperatorAdapter.fromString(OperatorAdapter
+                            .toString(operator)))));
+        }
+    }
+
+}
