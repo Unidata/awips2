fba28ea69b8003593ddb7b70644952178a7e58fa
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-01-14 11:50:00 -0600

    Issue #1469 - pypies will now read the hdf5 root directory from configuration. Relative paths can now be passed to pypies. Rebased to fix merge conflict.
    Change-Id: I38873e478729a3644db7ce2813e9bd6c15d9bdf3
    
    Former-commit-id: 4db9c6c321f26d9a198c6ef07e55b18f77a273a1 [formerly ed2e78738b391bcb9a40ddde7b7a6acf7ccd1b30]
    Former-commit-id: e2a12845d3e2dc4ff1bec07188010b982398cf78

---
+++ pythonPackages/pypies/pypies/config/pypiesConfigurationManager.py
@@ -0,0 +1,68 @@
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+
+
+#
+# Configuration for pypies logging
+#  
+#    
+#     SOFTWARE HISTORY
+#    
+#    Date            Ticket#       Engineer       Description
+#    ------------    ----------    -----------    --------------------------
+#    01/10/13                      bkowal         Initial Creation.
+#    
+# 
+#
+
+import os, ConfigParser
+
+class PypiesConfigurationManager:
+    
+    def __init__(self):
+        self.__configLoaded = False
+        
+        self.__initConfigLocation()
+        if (not self.__configLoc):
+            raise RuntimeError("No pypies.cfg found")
+        
+        self.__loadConfig()
+
+    def __initConfigLocation(self):        
+        self.__configLoc = '/awips2/pypies/conf/pypies.cfg'
+        if not os.path.exists(self.__configLoc):
+            print "Unable to find pypies.cfg at ", self.__configLoc
+            self.__configLoc = None
+        else:
+            print "Found pypies.cfg at ", self.__configLoc        
+        
+    def __loadConfig(self):
+        self.__scp = ConfigParser.SafeConfigParser()
+        self.__scp.read(self.__configLoc)
+        self.__configLoaded = True
+
+    def getConfigurationLocation(self):
+        return self.__configLoc
+        
+    def hasConfigurationBeenLoaded(self):
+        return self.__configLoaded
+        
+    def getConfiguration(self):
+        return self.__scp
