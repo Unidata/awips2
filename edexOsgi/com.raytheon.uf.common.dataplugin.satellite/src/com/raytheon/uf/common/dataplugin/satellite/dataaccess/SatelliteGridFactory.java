bd32a947c745d44d5a5e7457194805d8e50877d1
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-01-23 11:22:30 -0600

    Issue #1521 create grid data access factory and refactor satellite to share common functionality. Change-Id: I54de0b41b60ccd058124eb79e6f032cb2ea9a3cf
    Former-commit-id: 3032dc21892b44ba46aba0791d458fde6d9b3f51 [formerly 2133adac87eaa53977c26700ef656c304e81ee1b]
    Former-commit-id: a19b51cf2e3b953d9fd9155d49551d7c1521d04d

--- edexOsgi/com.raytheon.uf.common.dataplugin.satellite/src/com/raytheon/uf/common/dataplugin/satellite/dataaccess/SatelliteGridFactory.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.satellite/src/com/raytheon/uf/common/dataplugin/satellite/dataaccess/SatelliteGridFactory.java
@@ -21,45 +21,35 @@ package com.raytheon.uf.common.dataplugin.satellite.dataaccess;
 
 import java.text.ParseException;
 import java.text.ParsePosition;
-import java.util.Collections;
-import java.util.Map;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
 
-import org.geotools.coverage.grid.GridGeometry2D;
 import javax.measure.unit.Unit;
 import javax.measure.unit.UnitFormat;
 
+import org.geotools.coverage.grid.GridGeometry2D;
+
 import com.raytheon.uf.common.dataaccess.exception.DataRetrievalException;
-import com.raytheon.uf.common.dataaccess.exception.TimeAgnosticDataException;
 import com.raytheon.uf.common.dataaccess.grid.IGridData;
 import com.raytheon.uf.common.dataaccess.grid.IGridDataFactory;
 import com.raytheon.uf.common.dataaccess.grid.IGridRequest;
-import com.raytheon.uf.common.dataaccess.impl.AbstractDataFactory;
+import com.raytheon.uf.common.dataaccess.impl.AbstractGridDataPluginFactory;
 import com.raytheon.uf.common.dataaccess.impl.DefaultGridData;
 import com.raytheon.uf.common.dataaccess.util.DataWrapperUtil;
-import com.raytheon.uf.common.dataaccess.util.PDOUtil;
-import com.raytheon.uf.common.dataplugin.satellite.units.SatelliteUnits;
-import com.raytheon.uf.common.time.BinOffset;
-import com.raytheon.uf.common.time.DataTime;
-import com.raytheon.uf.common.time.TimeRange;
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
 import com.raytheon.uf.common.dataplugin.satellite.SatelliteRecord;
+import com.raytheon.uf.common.dataplugin.satellite.units.SatelliteUnits;
 import com.raytheon.uf.common.dataquery.requests.DbQueryRequest;
-import com.raytheon.uf.common.dataquery.requests.TimeQueryRequest;
 import com.raytheon.uf.common.dataquery.requests.RequestConstraint;
 import com.raytheon.uf.common.dataquery.requests.RequestConstraint.ConstraintType;
 import com.raytheon.uf.common.dataquery.responses.DbQueryResponse;
 import com.raytheon.uf.common.datastorage.records.IDataRecord;
-import com.raytheon.uf.common.serialization.comm.RequestRouter;
+import com.raytheon.uf.common.time.DataTime;
 
 /**
  * A data factory for getting satellite data from the metadata database. There
  * are currently not any required identifiers.
- * 
- * Note: This factory currently does not support Storage Requests even though
- * they can be included in an IGridRequest.
  * 
  * <pre>
  * 
@@ -68,106 +58,25 @@ import com.raytheon.uf.common.serialization.comm.RequestRouter;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Jan 02, 2012            bkowal      Initial creation
+ * Jan 22, 2012            bsteffen    Extract common functionality to AbstractGridDataPluginFactory
  * 
  * </pre>
  * 
  * @author bkowal
  * @version 1.0
  */
-public class SatelliteGridFactory extends AbstractDataFactory implements
-        IGridDataFactory {
-    private static final String FIELD_DATATIME = "dataTime.refTime";
+public class SatelliteGridFactory extends AbstractGridDataPluginFactory
+        implements IGridDataFactory {
 
     private static final String FIELD_PYHSICAL_ELEMENT = "physicalElement";
 
     private static final String FIELD_SECTOR_ID = "sectorID";
-
-    public static final String DBQUERY_PLUGIN_NAME_KEY = "pluginName";
 
     private Map<String, GridGeometry2D> sectorGeometryMapCache;
 
     public SatelliteGridFactory() {
         this.sectorGeometryMapCache = new HashMap<String, GridGeometry2D>();
         SatelliteUnits.register();
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.common.dataaccess.IDataFactory#getAvailableTimes(com.
-     * raytheon.uf.common.dataaccess.IDataRequest)
-     */
-    @Override
-    public DataTime[] getAvailableTimes(IGridRequest request)
-            throws TimeAgnosticDataException {
-        return this.getAvailableTimes(request, null);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.common.dataaccess.IDataFactory#getAvailableTimes(com.
-     * raytheon.uf.common.dataaccess.IDataRequest,
-     * com.raytheon.uf.common.time.BinOffset)
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public DataTime[] getAvailableTimes(IGridRequest request,
-            BinOffset binOffset) throws TimeAgnosticDataException {
-        TimeQueryRequest timeQueryRequest = this.buildTimeQueryRequest(request);
-        if ((binOffset == null) == false) {
-            timeQueryRequest.setBinOffset(binOffset);
-        }
-
-        List<Object> results = null;
-        try {
-            results = (List<Object>) RequestRouter.route(timeQueryRequest);
-        } catch (Exception e) {
-            throw new DataRetrievalException(
-                    "Failed to retrieve available data times for plugin "
-                            + request.getDatatype() + " for request "
-                            + request.toString(), e);
-        }
-
-        List<DataTime> dataTimes = new ArrayList<DataTime>();
-        for (Object result : results) {
-            dataTimes.add((DataTime) result);
-        }
-        Collections.sort(dataTimes);
-
-        return dataTimes.toArray(new DataTime[dataTimes.size()]);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.common.dataaccess.IDataFactory#getData(com.raytheon.uf
-     * .common.dataaccess.IDataRequest, com.raytheon.uf.common.time.DataTime[])
-     */
-    @Override
-    public IGridData[] getData(IGridRequest request, DataTime... times) {
-
-        DbQueryRequest dbQueryRequest = this
-                .buildDbQueryRequest(request, times);
-        return this.getData(request, dbQueryRequest);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.common.dataaccess.IDataFactory#getData(com.raytheon.uf
-     * .common.dataaccess.IDataRequest, com.raytheon.uf.common.time.TimeRange)
-     */
-    @Override
-    public IGridData[] getData(IGridRequest request, TimeRange timeRange) {
-
-        DbQueryRequest dbQueryRequest = this.buildDbQueryRequest(request,
-                timeRange);
-        return this.getData(request, dbQueryRequest);
     }
 
     /*
@@ -185,6 +94,8 @@ public class SatelliteGridFactory extends AbstractDataFactory implements
             return null;
         }
 
+        GridGeometry2D geometry = null;
+
         /*
          * Has the Geometry for the sector id already been cached?
          */
@@ -193,31 +104,33 @@ public class SatelliteGridFactory extends AbstractDataFactory implements
                 /*
                  * Return the Geometry from cache.
                  */
-                return this.sectorGeometryMapCache.get(satelliteSectorID);
+                geometry = this.sectorGeometryMapCache.get(satelliteSectorID);
             }
         }
 
-        /*
-         * Retrieve the Geometry.
-         */
-        IGridData[] records = this.getData(request, new DataTime[] {});
-        if (records.length <= 0) {
-            // No records were found
-            return null;
-        }
-        GridGeometry2D geometry = records[0].getGridGeometry();
+        if (geometry == null) {
+            /*
+             * Retrieve the Geometry.
+             */
+            IGridData[] records = this.getData(request, new DataTime[] {});
+            if (records.length <= 0) {
+                // No records were found
+                return null;
+            }
+            geometry = records[0].getGridGeometry();
 
-        /*
-         * Cache the Geometry.
-         */
-        synchronized (this.sectorGeometryMapCache) {
-            this.sectorGeometryMapCache.put(satelliteSectorID, geometry);
+            /*
+             * Cache the Geometry.
+             */
+            synchronized (this.sectorGeometryMapCache) {
+                this.sectorGeometryMapCache.put(satelliteSectorID, geometry);
+            }
         }
 
         /*
          * Return the Geometry.
          */
-        return geometry;
+        return trimGridGeometryToRequest(geometry, request.getStorageRequest());
     }
 
     /**
@@ -276,214 +189,30 @@ public class SatelliteGridFactory extends AbstractDataFactory implements
         return null;
     }
 
-    /**
-     * Executes the provided DbQueryRequest and returns an array of IGridData
-     * 
-     * @param request
-     *            the original grid request
-     * @param dbQueryRequest
-     *            the db query request to execute
-     * @return an array of IGridData
-     */
-    /*
-     * TODO: add support for StorageRequest (included in the IGridRequest)
-     */
-    private IGridData[] getData(IGridRequest request,
-            DbQueryRequest dbQueryRequest) {
-        DbQueryResponse dbQueryResponse = this.executeDbQueryRequest(
-                dbQueryRequest, request.toString());
-
-        List<IGridData> gridData = new ArrayList<IGridData>();
-        for (Map<String, Object> resultMap : dbQueryResponse.getResults()) {
-            if (resultMap.containsKey(null) == false) {
-                throw new DataRetrievalException(
-                        "The results of the DbQueryRequest do not consist of PluginDataObject objects as expected.");
-            }
-            if ((resultMap.get(null) instanceof SatelliteRecord) == false) {
-                throw new DataRetrievalException(
-                        "The PluginDataObject objects returned by the DbQueryRequest are not of type SatelliteRecord as expected.");
-            }
-
-            SatelliteRecord satelliteRecord = (SatelliteRecord) resultMap
-                    .get(null);
-
-            IDataRecord[] dataRecords = null;
-
-            try {
-                dataRecords = PDOUtil.getDataRecords(satelliteRecord);
-            } catch (Exception e1) {
-                throw new DataRetrievalException(
-                        "Failed to retrieve the IDataRecords for PluginDataObject: "
-                                + satelliteRecord.toString());
-            }
-
-            /*
-             * Extract the grid geometry.
-             */
-            final GridGeometry2D gridGeometry = PDOUtil
-                    .retrieveGeometry(satelliteRecord);
-
-            for (IDataRecord dataRecord : dataRecords) {
-                DefaultGridData defaultGridData = null;
-                try {
-                    defaultGridData = this.constructGridDataResponse(request,
-                            satelliteRecord, gridGeometry, dataRecord);
-                } catch (ParseException e) {
-                    throw new DataRetrievalException(
-                            "Failed to parse the Unit: "
-                                    + satelliteRecord.getUnits(), e);
-                }
-                gridData.add(defaultGridData);
-            }
+    protected DefaultGridData constructGridDataResponse(IGridRequest request,
+            PluginDataObject pdo, GridGeometry2D gridGeometry,
+            IDataRecord dataRecord)  {
+        if(pdo instanceof SatelliteRecord == false){
+            throw new DataRetrievalException(this.getClass().getSimpleName()
+                    + " cannot handle " + pdo.getClass().getSimpleName());
         }
-
-        return gridData.toArray(new IGridData[gridData.size()]);
-    }
-
-    /**
-     * Executes the provided DbQueryRequest and returns a DbQueryResponse
-     * 
-     * @param dbQueryRequest
-     *            the DbQueryRequest to execute
-     * @param gridRequestString
-     *            the original grid request for reporting purposes
-     * @return a DbQueryResponse
-     */
-    private DbQueryResponse executeDbQueryRequest(
-            DbQueryRequest dbQueryRequest, String gridRequestString) {
-        DbQueryResponse dbQueryResponse = null;
-
-        try {
-            dbQueryResponse = (DbQueryResponse) RequestRouter
-                    .route(dbQueryRequest);
-        } catch (Exception e1) {
-            throw new DataRetrievalException(
-                    "Unable to complete the DbQueryRequest for request: "
-                            + gridRequestString, e1);
-        }
-
-        return dbQueryResponse;
-    }
-
-    /**
-     * Builds a TimeQueryRequest that will be used to retrieve Data Times.
-     * 
-     * @param request
-     *            the original grid request
-     * @return a TimeQueryRequest to execute
-     */
-    /*
-     * TODO: maybe this method needs to be a util or a protected method in a
-     * superclass so that other data types that utilize a TimeQueryRequest will
-     * be able to use this logic?
-     */
-    private TimeQueryRequest buildTimeQueryRequest(IGridRequest request) {
-        TimeQueryRequest timeQueryRequest = new TimeQueryRequest();
-        timeQueryRequest.setPluginName(request.getDatatype());
-        timeQueryRequest.setQueryTerms(this
-                .buildConstraintsFromRequest(request));
-
-        return timeQueryRequest;
-    }
-
-    /**
-     * Constructs a db query request using the provided data times
-     * 
-     * @param request
-     *            the original grid request
-     * @param dataTimes
-     *            the data times to include in the request (if any)
-     * @return a DbQueryRequest to execute
-     */
-    private DbQueryRequest buildDbQueryRequest(IGridRequest request,
-            DataTime[] dataTimes) {
-        DbQueryRequest dbQueryRequest = this.buildDbQueryRequest(request);
-        if (dataTimes.length <= 0) {
-            return dbQueryRequest;
-        }
-        /* Add the DataTime Constraint */
-        RequestConstraint requestConstraint = new RequestConstraint();
-        requestConstraint.setConstraintType(ConstraintType.IN);
-        String[] dataTimeStrings = new String[dataTimes.length];
-        int index = 0;
-        for (DataTime dataTime : dataTimes) {
-            dataTimeStrings[index] = dataTime.toString();
-            ++index;
-        }
-        requestConstraint.setConstraintValueList(dataTimeStrings);
-        dbQueryRequest.addConstraint(FIELD_DATATIME, requestConstraint);
-
-        return dbQueryRequest;
-    }
-
-    /**
-     * Constructs a db request using the provided time range
-     * 
-     * @param request
-     *            the original grid request
-     * @param timeRange
-     *            the time range to include in the request
-     * @return a DbQueryRequest to execute
-     */
-    private DbQueryRequest buildDbQueryRequest(IGridRequest request,
-            TimeRange timeRange) {
-        DbQueryRequest dbQueryRequest = this.buildDbQueryRequest(request);
-        /* Add the TimeRange Constraint */
-        RequestConstraint requestConstraint = new RequestConstraint();
-        requestConstraint.setConstraintType(ConstraintType.BETWEEN);
-        String[] dateTimeStrings = new String[] {
-                timeRange.getStart().toString(), timeRange.getEnd().toString() };
-        requestConstraint.setBetweenValueList(dateTimeStrings);
-        dbQueryRequest.addConstraint(FIELD_DATATIME, requestConstraint);
-
-        return dbQueryRequest;
-    }
-
-    /**
-     * Constructs the base of a db query request using the supplied grid request
-     * 
-     * @param request
-     *            the original grid request
-     * @return the base DbQueryRequest
-     */
-    private DbQueryRequest buildDbQueryRequest(IGridRequest request) {
-        DbQueryRequest dbQueryRequest = new DbQueryRequest();
-        Map<String, RequestConstraint> constraints = this
-                .buildConstraintsFromRequest(request);
-        constraints.put(DBQUERY_PLUGIN_NAME_KEY,
-                new RequestConstraint(request.getDatatype()));
-        dbQueryRequest.setConstraints(constraints);
-
-        return dbQueryRequest;
-    }
-
-    /**
-     * Builds an IGridData with the information that is supplied
-     * 
-     * @param request
-     *            the original grid request
-     * @param satelliteRecord
-     *            a record that was retrieved from the database
-     * @param gridGeometry
-     *            the geometry extracted from the pdo
-     * @param dataRecord
-     *            the raw data
-     * @return the IGridData that was constructed
-     * @throws ParseException
-     */
-    private DefaultGridData constructGridDataResponse(IGridRequest request,
-            SatelliteRecord satelliteRecord, GridGeometry2D gridGeometry,
-            IDataRecord dataRecord) throws ParseException {
+        
+        SatelliteRecord satelliteRecord = (SatelliteRecord) pdo;
         DefaultGridData defaultGridData = new DefaultGridData(
                 DataWrapperUtil.constructArrayWrapper(dataRecord), gridGeometry);
-        defaultGridData.setDataTime(satelliteRecord.getDataTime());
+        defaultGridData.setDataTime(pdo.getDataTime());
         defaultGridData.setParameter(satelliteRecord.getPhysicalElement());
         defaultGridData.setLevel(null);
         // unit
         Unit<?> unit = null;
         if ((satelliteRecord.getUnits() == null) == false) {
-            unit = UnitFormat.getUCUMInstance().parseSingleUnit(
-                    satelliteRecord.getUnits(), new ParsePosition(0));
+            try {
+                unit = UnitFormat.getUCUMInstance().parseSingleUnit(
+                        satelliteRecord.getUnits(), new ParsePosition(0));
+            } catch (ParseException e) {
+                throw new DataRetrievalException("Failed to parse the Unit: "
+                        + satelliteRecord.getUnits(), e);
+            }
         }
         defaultGridData.setUnit(unit);
         defaultGridData.setAttributes(request.getIdentifiers());
@@ -503,7 +232,7 @@ public class SatelliteGridFactory extends AbstractDataFactory implements
      * multiple factories will be building a base constraint map using the same
      * technique
      */
-    private Map<String, RequestConstraint> buildConstraintsFromRequest(
+    protected Map<String, RequestConstraint> buildConstraintsFromRequest(
             IGridRequest request) {
         Map<String, RequestConstraint> constraints = new HashMap<String, RequestConstraint>();
         if ((request.getIdentifiers() == null) == false) {
