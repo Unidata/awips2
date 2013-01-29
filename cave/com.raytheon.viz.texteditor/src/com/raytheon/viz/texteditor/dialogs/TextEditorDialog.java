e11366a15357b18bd2529777b033610985254696
Steve Harris <Steven_L_Harris@raytheon.com>
2013-01-29 15:52:43 -0600

    13.2.1-3 baseline
    Former-commit-id: 0c726758913e48748e5cfa046cb7cdcff3113e3e [formerly 0c726758913e48748e5cfa046cb7cdcff3113e3e [formerly 283147571fe6a46f5362e4143dc61db64b43a40a]] [formerly f45120a1f490aa79428829a2e2b5adb9f53c9076] [formerly 3edfa1e0be679f8f385c602f85d57e99bd6dfbd2 [formerly 3edfa1e0be679f8f385c602f85d57e99bd6dfbd2 [formerly 7a739c6a5be624d41ed7ac3b152727a8b1d8777d]] [formerly f45120a1f490aa79428829a2e2b5adb9f53c9076 [formerly f45120a1f490aa79428829a2e2b5adb9f53c9076 [formerly fc7291332a08966a746605aef725e1947ec9643c]]]]
    Former-commit-id: 3edfa1e0be679f8f385c602f85d57e99bd6dfbd2 [formerly 3edfa1e0be679f8f385c602f85d57e99bd6dfbd2 [formerly 7a739c6a5be624d41ed7ac3b152727a8b1d8777d]]
    Former-commit-id: 3edfa1e0be679f8f385c602f85d57e99bd6dfbd2
    Former-commit-id: 5a1c5b2593cd86e0ac19b35756ab44dedcae1332 [formerly daca1c3e73a267255a2445dff5ba2aee059e48bd]
    Former-commit-id: f02f23b811d4733a87364361477cbaed674a5754

--- cave/com.raytheon.viz.texteditor/src/com/raytheon/viz/texteditor/dialogs/TextEditorDialog.java
+++ cave/com.raytheon.viz.texteditor/src/com/raytheon/viz/texteditor/dialogs/TextEditorDialog.java
@@ -307,8 +307,7 @@ import com.raytheon.viz.ui.dialogs.SWTMessageBox;
  * 28Nov2012   14842	    M.Gamazaychikov	Re-wrote processPopup method
  * 13Dec2012   1353         rferrel     Change to make edit cancel message not
  *                                       dispaly the red had kill job message.
- * 31Dec2012   15651	    M.Gamazaychikov	Added an argument to re-factored PrintDisplay.print
- * 10JAN2012   15704		M.Gamazaychikov Added setting userKeyPressed to false in verifyText method.
+ * 10JAN2012   15704		M.Gamazaychikov Added setting userKeyPressed to false in verifyText method
  * </pre>
  * 
  * @author lvenable
@@ -4192,7 +4191,7 @@ public class TextEditorDialog extends CaveSWTDialog implements VerifyListener,
      */
     private void printAllText() {
         FontData fontData = textEditor.getFont().getFontData()[0];
-        PrintDisplay.print(textEditor.getText(), fontData, charWrapCol, statusHandler);
+        PrintDisplay.print(textEditor.getText(), fontData, statusHandler);
     }
 
     /**
@@ -4215,7 +4214,7 @@ public class TextEditorDialog extends CaveSWTDialog implements VerifyListener,
             String tmpText = textEditor.getText();
             Point point = textEditor.getSelection();
             FontData fontData = textEditor.getFont().getFontData()[0];
-            PrintDisplay.print(textEditor.getSelectionText(), fontData, charWrapCol,
+            PrintDisplay.print(textEditor.getSelectionText(), fontData, 
                     statusHandler);
             textEditor.setText(tmpText);
             textEditor.setSelection(point);
