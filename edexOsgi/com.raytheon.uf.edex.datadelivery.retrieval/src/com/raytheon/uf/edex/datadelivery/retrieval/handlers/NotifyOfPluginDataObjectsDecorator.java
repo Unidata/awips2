7b2374b5ffef29bc3b6dbe43ed7255f5eb914fcc
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-01-30 13:05:47 -0600

    Issue #1543 Break apart the retrieval process to support SBN capabilities. Made interfaces for various aspects of retrieval that are Spring injectable. Created an initial SBN retrieval task that will watch the /awips2/crawl directory, for proof of concept.
    Amend:
      Check whether debug is enabled prior to debug message.
      Move log of request xml inside null check.
    
    Change-Id: I5e3fe835f028cb93c98abce9735d3d59cfc663dc
    
    Former-commit-id: be89d9e6b014e365ef1166985814927f46a7b835 [formerly 3112fce5405e58632e658c9d321bfd49e8d4517c] [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143] [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143 [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233]] [formerly be89d9e6b014e365ef1166985814927f46a7b835 [formerly 3112fce5405e58632e658c9d321bfd49e8d4517c] [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143] [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143 [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233]] [formerly 1a17e4a0f5c0b18d32405e2f23852ef70bb6ec1e [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143 [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233] [formerly 1a17e4a0f5c0b18d32405e2f23852ef70bb6ec1e [formerly 120296239cfa021507b4ce9bc3f7333509b1dfb9]]]]]
    Former-commit-id: 1a17e4a0f5c0b18d32405e2f23852ef70bb6ec1e
    Former-commit-id: 9755ebc41ac0b9ef907fc233bda808aa6cc1f40a [formerly 82c86df837a246ca4cf56bfa47868307f3aa0549] [formerly 2940a059d1ea8ea2fb0a7e2fa532a03e5f99e9a7] [formerly c75e7199401a0fe0470b6cbbd108b42dcfc47508 [formerly 3cb29f382d95ee1e760fcb5f018ec70b8bd3cb00] [formerly 2940a059d1ea8ea2fb0a7e2fa532a03e5f99e9a7 [formerly b5f69e21d83295e809921a68c5a2e37f17578953]]]
    Former-commit-id: be5162500d2abacb74f95d93a51a46e377df3b8a [formerly 80d55cc82b3caa3784bcb0f5d2966f02f9acfa60] [formerly 55f7212a0e4cf3b7ccd489dadc8b178204b1fe47 [formerly bef0f2eca1b8354dc50c02276ac810afafc42b4c]]
    Former-commit-id: 55f7212a0e4cf3b7ccd489dadc8b178204b1fe47
    Former-commit-id: 2f19170098d8d9f3b8cf06edf64f40fdbe4c3283

---
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/handlers/NotifyOfPluginDataObjectsDecorator.java
@@ -0,0 +1,109 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.datadelivery.retrieval.handlers;
+
+import java.util.List;
+
+import com.raytheon.uf.common.datadelivery.event.retrieval.DataRetrievalEvent;
+import com.raytheon.uf.common.datadelivery.registry.Provider.ServiceType;
+import com.raytheon.uf.common.datadelivery.retrieval.util.DataSizeUtils;
+import com.raytheon.uf.common.datadelivery.retrieval.xml.Retrieval;
+import com.raytheon.uf.common.datadelivery.retrieval.xml.RetrievalAttribute;
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.edex.datadelivery.retrieval.db.RetrievalRequestRecord;
+import com.raytheon.uf.edex.event.EventBus;
+
+/**
+ * Performs processing on the retrieved plugin data objects, and then sends an
+ * event about the retrieval.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 01, 2013 1543       djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+public class NotifyOfPluginDataObjectsDecorator implements
+        IRetrievalPluginDataObjectsProcessor {
+
+    private static final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(NotifyOfPluginDataObjectsDecorator.class);
+
+    private static final EventBus EVENT_BUS = EventBus.getInstance();
+
+    private final IRetrievalPluginDataObjectsProcessor retrievedDataProcessor;
+
+    public NotifyOfPluginDataObjectsDecorator(
+            IRetrievalPluginDataObjectsProcessor retrievedDataProcessor) {
+        this.retrievedDataProcessor = retrievedDataProcessor;
+    }
+
+    @Override
+    public void processRetrievedPluginDataObjects(
+            RetrievalPluginDataObjects retrievalPluginDataObjects)
+            throws Exception {
+
+        // TODO: What if one of the records fails to store or serialize, is that
+        // already handled somewhere?
+        retrievedDataProcessor
+                .processRetrievedPluginDataObjects(retrievalPluginDataObjects);
+
+        final RetrievalRequestRecord requestRecord = retrievalPluginDataObjects
+                .getRequestRecord();
+        final List<RetrievalAttributePluginDataObjects> retrievalAttributePluginDataObjects = retrievalPluginDataObjects
+                .getRetrievalAttributePluginDataObjects();
+
+        for (RetrievalAttributePluginDataObjects pluginDataObjectEntry : retrievalAttributePluginDataObjects) {
+            RetrievalAttribute attXML = pluginDataObjectEntry.getAttributeXml();
+            PluginDataObject[] value = pluginDataObjectEntry
+                    .getPluginDataObjects();
+
+            if (value.length == 0) {
+                continue;
+            }
+
+            final String pluginName = value[0].getPluginName();
+            Retrieval retrieval = requestRecord.getRetrievalObj();
+            ServiceType serviceType = retrieval.getServiceType();
+
+            statusHandler.info("Successfully processed: " + value.length
+                    + " : " + serviceType + " Plugin : " + pluginName);
+            DataRetrievalEvent event = new DataRetrievalEvent();
+            event.setId(retrieval.getSubscriptionName());
+            event.setOwner(retrieval.getOwner());
+            event.setNetwork(retrieval.getNetwork().name());
+            event.setPlugin(pluginName);
+            event.setProvider(attXML.getProvider());
+            event.setNumRecords(value.length);
+            event.setBytes(DataSizeUtils.calculateSize(attXML, serviceType));
+
+            EVENT_BUS.publish(event);
+        }
+    }
+}
