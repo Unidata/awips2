a0477085cda1690f4df5c137df293fb5ba7f260d
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-01-30 13:05:47 -0600

    Issue #1543 Break apart the retrieval process to support SBN capabilities. Made interfaces for various aspects of retrieval that are Spring injectable. Created an initial SBN retrieval task that will watch the /awips2/crawl directory, for proof of concept.
    Amend:
      Check whether debug is enabled prior to debug message.
      Move log of request xml inside null check.
    
    Change-Id: I5e3fe835f028cb93c98abce9735d3d59cfc663dc
    
    Former-commit-id: 4d62cf375d1ff8b157a2b92049318bb4edc912ff [formerly 4d62cf375d1ff8b157a2b92049318bb4edc912ff [formerly cf063000ca0c6d25d3f94227c4fec2296d1e0143]] [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233] [formerly b5f69e21d83295e809921a68c5a2e37f17578953 [formerly b5f69e21d83295e809921a68c5a2e37f17578953 [formerly 1a17e4a0f5c0b18d32405e2f23852ef70bb6ec1e]] [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233 [formerly 3b20efeb7dd2018734c05a8f15b5b1d778f06233 [formerly 120296239cfa021507b4ce9bc3f7333509b1dfb9]]]]
    Former-commit-id: b5f69e21d83295e809921a68c5a2e37f17578953 [formerly b5f69e21d83295e809921a68c5a2e37f17578953 [formerly 1a17e4a0f5c0b18d32405e2f23852ef70bb6ec1e]]
    Former-commit-id: b5f69e21d83295e809921a68c5a2e37f17578953
    Former-commit-id: 1fbc845e60e41e80a094fe2f5599db831931504f [formerly 2940a059d1ea8ea2fb0a7e2fa532a03e5f99e9a7]
    Former-commit-id: d2bb929913b53d82dc8252fbbb618c7bf396461c

--- edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/handlers/IRetrievalPluginDataObjectsProcessor.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/handlers/IRetrievalPluginDataObjectsProcessor.java
@@ -1,5 +1,3 @@
-package com.raytheon.uf.edex.datadelivery.retrieval.interfaces;
-
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
@@ -19,44 +17,40 @@ package com.raytheon.uf.edex.datadelivery.retrieval.interfaces;
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
+package com.raytheon.uf.edex.datadelivery.retrieval.handlers;
 
-import java.util.Map;
-
-import com.raytheon.uf.common.datadelivery.retrieval.xml.Retrieval;
-import com.raytheon.uf.common.datadelivery.retrieval.xml.RetrievalAttribute;
-import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.serialization.SerializationException;
 
 /**
- * Interface for Provider Retrieval Adapter
+ * Processes {@link RetrievalPluginDataObjects} that were generated from a
+ * retrieval.
  * 
  * <pre>
  * 
  * SOFTWARE HISTORY
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Jan 07, 2011            dhladky     Initial creation
+ * Feb 01, 2013 1543       djohnson     Initial creation
  * 
  * </pre>
  * 
- * /
- * 
- * @author dhladky
+ * @author djohnson
  * @version 1.0
  */
 
-public interface IRetrievalAdapter {
+public interface IRetrievalPluginDataObjectsProcessor {
 
-    public IRetrievalRequestBuilder createRequestMessage(
-            RetrievalAttribute prxml);
-
-    public com.raytheon.uf.edex.datadelivery.retrieval.response.RetrievalResponse performRequest(
-            IRetrievalRequestBuilder requestBuilder);
-
-    public Map<String, PluginDataObject[]> processResponse(
-            IRetrievalResponse response) throws Exception;
-
-    public void setProviderRetrievalXML(Retrieval prxml);
-
-    public Retrieval getProviderRetrievalXMl();
-
+    /**
+     * Process plugin data objects that were created as a result of a data
+     * delivery retrieval.
+     * 
+     * @param retrievalPluginDataObjects
+     *            the retrieval plugin data objects
+     * @throws SerializationException
+     *             on error with serialization
+     */
+    void processRetrievedPluginDataObjects(
+            RetrievalPluginDataObjects retrievalPluginDataObjects)
+            throws Exception;
 }
