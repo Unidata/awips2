9e84a07e99f19eeb11ee94b7606da56e1cee41c1
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-01-31 17:32:00 -0600

    Issue #1555 - initial generic geometry resource
    - refactored viz data access resources
    - updated hydro factory based on existing hydro code
    - geometric lines and polygons are now both rendered using wireframe shapes and the same logic
    - relocated the data used to render the geometries to the resource
    - points are created instead of circles for geometric points
    - do not store legend text as a static variable in the abstract data access resource
    - addressed peer review comments
    
    Change-Id: Ic7792aede9e24445af541f01631886645a8cf1f0
    
    Former-commit-id: e9391b94491a2ee2181c2b784a3370e886143e52 [formerly e9391b94491a2ee2181c2b784a3370e886143e52 [formerly 442a2f8fa4c07be806d779804d86cccdf8480e8a]] [formerly a86d2b57690be5d5768c236c174d7400929b7167] [formerly e9391b94491a2ee2181c2b784a3370e886143e52 [formerly e9391b94491a2ee2181c2b784a3370e886143e52 [formerly 442a2f8fa4c07be806d779804d86cccdf8480e8a]] [formerly a86d2b57690be5d5768c236c174d7400929b7167] [formerly c8a6b08394c6e60a79931f3c96be3c9bacd7e1b3 [formerly c8a6b08394c6e60a79931f3c96be3c9bacd7e1b3 [formerly 1732e6b4eafd932cd25f70da1671047f820a1263]] [formerly a86d2b57690be5d5768c236c174d7400929b7167 [formerly a86d2b57690be5d5768c236c174d7400929b7167 [formerly 04179bd343c3ec976c601e2856a3949e4515b472]]]]]
    Former-commit-id: c8a6b08394c6e60a79931f3c96be3c9bacd7e1b3 [formerly c8a6b08394c6e60a79931f3c96be3c9bacd7e1b3 [formerly 1732e6b4eafd932cd25f70da1671047f820a1263]]
    Former-commit-id: 83e1ab394feac74ceb5b5536735b88d79f63c376 [formerly 62b706d552d0fdbb473a5df8449f752e55f83e78] [formerly b08cefb5375f21f77e2ce0cf2f1eb392ed769377 [formerly 9c7d8ca3d3cdf75f3261d2d7ba94af1b9de80dd9]] [formerly c8a6b08394c6e60a79931f3c96be3c9bacd7e1b3]
    Former-commit-id: 406b843260434888909822dd3f748a8c3a290d0e [formerly 228d81d5e3193c7724c183d0f349ec037f5867fb] [formerly 8a302c6da0c0cbfdf8a19b309897b66338e3a184 [formerly 3b2342193f3acc78059ba117e8da54335888d6bf]]
    Former-commit-id: aaf4b1b9db663cd7e0e47eb7efc0490193e6f9a6 [formerly eaccf2d09add9a45f211a8377c5f79940a9ea338]
    Former-commit-id: 3f77ca3b7bf70e425b9bb69c83b1046d74b4908e

---
+++ cave/com.raytheon.viz.dataaccess/src/com/raytheon/viz/dataaccess/rsc/geometry/GenericGeometryResource.java
@@ -0,0 +1,236 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.viz.dataaccess.rsc.geometry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Point;
+
+import com.raytheon.uf.common.dataaccess.geom.IGeometryData;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.drawables.IWireframeShape;
+import com.raytheon.uf.viz.core.drawables.PaintProperties;
+import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.core.rsc.LoadProperties;
+import com.raytheon.uf.viz.core.rsc.capabilities.ColorableCapability;
+import com.raytheon.uf.viz.core.rsc.capabilities.MagnificationCapability;
+import com.raytheon.uf.viz.core.rsc.capabilities.OutlineCapability;
+import com.raytheon.viz.core.rsc.jts.JTSCompiler;
+import com.raytheon.viz.core.rsc.jts.JTSCompiler.PointStyle;
+import com.raytheon.viz.dataaccess.rsc.AbstractDataAccessResource;
+
+/**
+ * Renders various geometric entities based on geometry data that is retrieved
+ * using the Data Access Framework.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jan 30, 2013            bkowal     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bkowal
+ * @version 1.0
+ */
+
+public class GenericGeometryResource extends
+        AbstractDataAccessResource<GenericGeometryResourceData> {
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(GenericGeometryResource.class);
+
+    private static final String GENERIC_LEGEND_TEXT = "Generic Geometry ";
+
+    private boolean geometriesReady;
+
+    private List<double[]> pointsToRender;
+
+    private IWireframeShape shape;
+
+    /**
+     * Constructor
+     * 
+     * @param resourceData
+     * @param loadProperties
+     */
+    protected GenericGeometryResource(GenericGeometryResourceData resourceData,
+            LoadProperties loadProperties) {
+        super(resourceData, loadProperties, GENERIC_LEGEND_TEXT);
+        this.geometriesReady = false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.dataaccess.rsc.AbstractDataAccessResource#disposeResource
+     * ()
+     */
+    @Override
+    protected void disposeInternal() {
+        this.purgeDrawableStorage();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.core.rsc.AbstractVizResource#paintInternal(com.raytheon
+     * .uf.viz.core.IGraphicsTarget,
+     * com.raytheon.uf.viz.core.drawables.PaintProperties)
+     */
+    @Override
+    protected void paintInternal(IGraphicsTarget target,
+            PaintProperties paintProps) throws VizException {
+        if (this.geometriesReady == false) {
+            this.prepareData(target);
+        }
+
+        // First, draw the points
+        if (this.pointsToRender.size() > 0) {
+            target.drawPoints(this.pointsToRender,
+                    getCapability(ColorableCapability.class).getColor(),
+                    IGraphicsTarget.PointStyle.CIRCLE,
+                    getCapability(MagnificationCapability.class)
+                            .getMagnification().floatValue());
+        }
+
+        OutlineCapability outlineCapability = getCapability(OutlineCapability.class);
+        // Finally, draw the shape
+        if (this.shape != null && outlineCapability.isOutlineOn()) {
+            target.drawWireframeShape(this.shape,
+                    getCapability(ColorableCapability.class).getColor(),
+                    outlineCapability.getOutlineWidth(),
+                    outlineCapability.getLineStyle());
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.dataaccess.rsc.AbstractDataAccessResource#reprojectResource
+     * (org.opengis.referencing.crs.CoordinateReferenceSystem)
+     */
+    @Override
+    public void project(CoordinateReferenceSystem mapData) throws VizException {
+        this.purgeDrawableStorage();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.dataaccess.rsc.AbstractDataAccessResource#prepareData
+     * (com.raytheon.uf.viz.core.IGraphicsTarget)
+     */
+    @Override
+    protected void prepareData(IGraphicsTarget target) throws VizException {
+        this.initDrawableStorage();
+
+        int numberOfPoints = 0;
+        List<Geometry> shapeGeometries = new ArrayList<Geometry>();
+
+        for (IGeometryData geometryData : this.resourceData.getData()) {
+            Geometry geometry = geometryData.getGeometry();
+            if (geometry instanceof Point) {
+                double[] pixels = this.descriptor
+                        .worldToPixel(new double[] {
+                                geometry.getCoordinate().x,
+                                geometry.getCoordinate().y });
+                this.pointsToRender.add(pixels);
+            } else {
+                // Calculate the number of points; build the list of geometries
+                // we will be adding to the shape.
+                for (int i = 0; i < geometry.getNumGeometries(); i++) {
+                    Geometry _geometry = geometry.getGeometryN(i);
+                    numberOfPoints += _geometry.getNumPoints();
+                    shapeGeometries.add((Geometry) _geometry.clone());
+                }
+            }
+        }
+
+        if (numberOfPoints > 0) {
+            // create the wireframe shape
+            this.shape = target.createWireframeShape(false, this.descriptor,
+                    0.0f);
+
+            JTSCompiler jtsCompiler = new JTSCompiler(null, this.shape,
+                    this.descriptor, PointStyle.CROSS);
+            this.shape.allocate(numberOfPoints);
+            // add the geometries
+            for (Geometry geometry : shapeGeometries) {
+                try {
+                    jtsCompiler.handle(geometry);
+                } catch (VizException e1) {
+                    statusHandler.handle(UFStatus.Priority.ERROR,
+                            "Failed to handle Geometry "
+                                    + geometry.getClass().getName(), e1);
+                }
+            }
+
+            // compile
+            this.shape.compile();
+        }
+        this.geometriesReady = true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.viz.dataaccess.rsc.AbstractDataAccessResource#buildLegendText
+     * ()
+     */
+    @Override
+    protected String buildLegendTextInternal() {
+        return StringUtils.EMPTY;
+    }
+
+    /**
+     * Initializes the lists that will track the geometric entities that we will
+     * be drawing.
+     */
+    private void initDrawableStorage() {
+        this.pointsToRender = new ArrayList<double[]>();
+    }
+
+    /**
+     * Normally invoked when the resource is disposed; purges all saved
+     * geometric entities
+     */
+    public void purgeDrawableStorage() {
+        this.pointsToRender = null;
+        if (this.shape != null) {
+            this.shape.dispose();
+        }
+        this.shape = null;
+        this.geometriesReady = false;
+    }
+}
