c59d2a990a8b5059d2c40d498fa2aea7a2a15b7d
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-01-31 17:32:00 -0600

    Issue #1555 - initial generic geometry resource
    - refactored viz data access resources
    - updated hydro factory based on existing hydro code
    - geometric lines and polygons are now both rendered using wireframe shapes and the same logic
    - relocated the data used to render the geometries to the resource
    - points are created instead of circles for geometric points
    - do not store legend text as a static variable in the abstract data access resource
    - addressed peer review comments
    
    Change-Id: Ic7792aede9e24445af541f01631886645a8cf1f0
    
    Former-commit-id: 1732e6b4eafd932cd25f70da1671047f820a1263 [formerly a86d2b57690be5d5768c236c174d7400929b7167 [formerly 04179bd343c3ec976c601e2856a3949e4515b472]]
    Former-commit-id: a86d2b57690be5d5768c236c174d7400929b7167
    Former-commit-id: 442a2f8fa4c07be806d779804d86cccdf8480e8a

--- edexOsgi/com.raytheon.uf.common.dataaccess/src/com/raytheon/uf/common/dataaccess/impl/DefaultGeometryRequest.java
+++ edexOsgi/com.raytheon.uf.common.dataaccess/src/com/raytheon/uf/common/dataaccess/impl/DefaultGeometryRequest.java
@@ -19,7 +19,13 @@
  **/
 package com.raytheon.uf.common.dataaccess.impl;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
 import com.raytheon.uf.common.dataaccess.geom.IGeometryRequest;
+import com.raytheon.uf.common.serialization.adapters.JTSEnvelopeAdapter;
 import com.vividsolutions.jts.geom.Envelope;
 
 /**
@@ -32,18 +38,21 @@ import com.vividsolutions.jts.geom.Envelope;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Nov 5, 2012            njensen     Initial creation
+ * Jan 30, 2013 #1555     bkowal      Added XML Annotations
  * 
  * </pre>
  * 
  * @author njensen
  * @version 1.0
  */
-
+@XmlAccessorType(XmlAccessType.NONE)
 public class DefaultGeometryRequest extends AbstractDataRequest implements
         IGeometryRequest {
 
+    @XmlJavaTypeAdapter(value = JTSEnvelopeAdapter.class)
     protected Envelope envelope;
 
+    @XmlElement(name="locationName")
     protected String[] locationNames;
 
     @Override
