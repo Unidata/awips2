d82a175df658217404cede7156fbbeda376a177f
Dave Hladky <david_j_hladky@raytheon.com>
2013-02-05 11:52:59 -0600

    Issue #1478 FFMP cache file read/writes overloading NAS, more efficiency
    Change-Id: Ib5f49d9c1551b72a3620629437386d52c12b0ecf
    
    Former-commit-id: 893d7a96b724229adfe94318eddaa6d3622bd1ac [formerly 893d7a96b724229adfe94318eddaa6d3622bd1ac [formerly 7b49430e34a7d03758306da1f9cd61e87e2e1f65]] [formerly c0712160d71dd34444d5c953e003e34e62f12431] [formerly f6bd4ea0fa4b67f2b9394dbf2587d0e2850abd1b [formerly f6bd4ea0fa4b67f2b9394dbf2587d0e2850abd1b [formerly 7af5aab08aa859621e3fa4975ad042664dd767a0]] [formerly c0712160d71dd34444d5c953e003e34e62f12431 [formerly c0712160d71dd34444d5c953e003e34e62f12431 [formerly 9c7d813ee094864e0a7da60d54dee5424c50efba]]]]
    Former-commit-id: f6bd4ea0fa4b67f2b9394dbf2587d0e2850abd1b [formerly f6bd4ea0fa4b67f2b9394dbf2587d0e2850abd1b [formerly 7af5aab08aa859621e3fa4975ad042664dd767a0]]
    Former-commit-id: f6bd4ea0fa4b67f2b9394dbf2587d0e2850abd1b
    Former-commit-id: 220391ee39615b001744c08e568444522f4d5559 [formerly b134741e4c199d6f336c644f0899a18218bdbe86]
    Former-commit-id: 992cdc52b8ed63cfe49357620c5b19b24525ec88

--- edexOsgi/com.raytheon.uf.common.dataplugin.ffmp/src/com/raytheon/uf/common/dataplugin/ffmp/FFMPVirtualGageBasin.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.ffmp/src/com/raytheon/uf/common/dataplugin/ffmp/FFMPVirtualGageBasin.java
@@ -1,12 +1,27 @@
 package com.raytheon.uf.common.dataplugin.ffmp;
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 
 import java.util.Comparator;
 import java.util.Date;
 import java.util.TreeMap;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlRootElement;
 
 import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
@@ -21,14 +36,13 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * 02may10      3937       dhladky     Setup
+ * 01/27/13     1478       dhladky     Removed un-needed XML annotations
  * 
  * </pre>
  * 
  * @author dhladky
  * @version 1.0
  */
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
 public class FFMPVirtualGageBasin extends FFMPBasin implements
         ISerializableObject {
