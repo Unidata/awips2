0d548bd66f0beb4af6f9320323d3c4006b046f93
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-02-05 15:32:26 -0600

    Issue #1543 Add testing of current OpenDAP type retrievals. - Add test for current behavior of SBN-like retrievals. - Change session management to use a factory implementation. - Add service locator strategy to find jaxbables. - Tests cleanup. - Add mock versions of OpenDAP service classes to test more OpenDAP retrieval functionality.
    Amend:
      Peer review comments.
      Change to use Spring transactions.
    
    Change-Id: Ifce73361a5d864c54fdd493240c40d5ae66ef366
    
    Former-commit-id: ef32853dc18c1da06f187ee9db59f09ff921f58f [formerly bb9ab0ceb479a8c6a727b9764433edb70eec31f1]
    Former-commit-id: 8307af022cff62460522ed040630e692fbd8fd0e

---
+++ tests/unit/com/raytheon/uf/edex/datadelivery/retrieval/metadata/adapters/MockGridMetaDataAdapter.java
@@ -0,0 +1,75 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.datadelivery.retrieval.metadata.adapters;
+
+import org.junit.Ignore;
+
+import com.raytheon.uf.common.datadelivery.registry.GriddedCoverageFixture;
+import com.raytheon.uf.common.datadelivery.retrieval.xml.RetrievalAttribute;
+import com.raytheon.uf.common.dataplugin.level.Level;
+import com.raytheon.uf.common.gridcoverage.GridCoverage;
+
+/**
+ * Overrides specific methods in {@link GridMetadataAdapter} that require
+ * database interaction.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 06, 2013 1543       djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+@Ignore
+public class MockGridMetaDataAdapter extends GridMetadataAdapter {
+
+    /**
+     * Constructor.
+     * 
+     * @param attXML
+     * @throws InstantiationException
+     */
+    public MockGridMetaDataAdapter(RetrievalAttribute attXML)
+            throws InstantiationException {
+        super(attXML);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    GridCoverage getCoverageFromCache(GridCoverage coverage) {
+        return GriddedCoverageFixture.INSTANCE.get().getGridCoverage();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    Level[] getLevels(RetrievalAttribute attXML) {
+        return new Level[] { new Level("0.0") };
+    }
+}
