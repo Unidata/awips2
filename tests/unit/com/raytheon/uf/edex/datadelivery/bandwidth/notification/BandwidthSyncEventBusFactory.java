ef32853dc18c1da06f187ee9db59f09ff921f58f
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-02-05 15:32:26 -0600

    Issue #1543 Add testing of current OpenDAP type retrievals. - Add test for current behavior of SBN-like retrievals. - Change session management to use a factory implementation. - Add service locator strategy to find jaxbables. - Tests cleanup. - Add mock versions of OpenDAP service classes to test more OpenDAP retrieval functionality.
    Amend:
      Peer review comments.
      Change to use Spring transactions.
    
    Change-Id: Ifce73361a5d864c54fdd493240c40d5ae66ef366
    
    Former-commit-id: bb9ab0ceb479a8c6a727b9764433edb70eec31f1

---
+++ tests/unit/com/raytheon/uf/edex/datadelivery/bandwidth/notification/BandwidthSyncEventBusFactory.java
@@ -0,0 +1,67 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.datadelivery.bandwidth.notification;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * A synchronous event bus factory for the bandwidth manager.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 06, 2013 1543       djohnson     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author djohnson
+ * @version 1.0
+ */
+
+public class BandwidthSyncEventBusFactory implements BandwidthEventBusFactory {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EventBus getSubscriptionBus() {
+        return new EventBus();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EventBus getRetrievalBus() {
+        return new EventBus();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EventBus getDataSetBus() {
+        return new EventBus();
+    }
+
+}
