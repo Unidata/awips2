1c602ec3a7b80a3d2c6b6153b4240dce04cc6477
Steve Harris <Steven_L_Harris@raytheon.com>
2013-02-06 11:02:32 -0600

    13.2.1-5 baseline
    Former-commit-id: 232734091dfbe316d43640d3baab0096a76fcb26 [formerly c56f1fdc3e2419c5dd6bebbbb1e0e688bd5e0092]
    Former-commit-id: 146fcc6da87f8b182412d9aa085af31236c4128d

--- edexOsgi/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonNumpyFloatArray.java
+++ edexOsgi/com.raytheon.uf.common.python/src/com/raytheon/uf/common/python/PythonNumpyFloatArray.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.viz.derivparam.python;
+package com.raytheon.uf.common.python;
 
 import jep.INumpyable;
 
