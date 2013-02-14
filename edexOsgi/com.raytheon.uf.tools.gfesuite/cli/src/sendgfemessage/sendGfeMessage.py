634e7dc90bbc84bb11cb51c3c5201c153da17d58
Steve Harris <Steven_L_Harris@raytheon.com>
2013-02-14 13:24:43 -0600

    13.1.1-10 baseline
    Former-commit-id: 9b73b6a2a949e29bf11b8054d41545cb1a7ca67c [formerly baa5de21478f513fa1bc599255b0179b4550077b [formerly 06d0e6dd16343fcff0012224a728fa01cece970d] [formerly 9b73b6a2a949e29bf11b8054d41545cb1a7ca67c [formerly 1e337899d45617a8ee1998f2d2c86f2912668b67]]]
    Former-commit-id: baa5de21478f513fa1bc599255b0179b4550077b [formerly 06d0e6dd16343fcff0012224a728fa01cece970d]
    Former-commit-id: baa5de21478f513fa1bc599255b0179b4550077b
    Former-commit-id: 99a9726284e73ada1eea610c70962c1b6c8c53ba

--- edexOsgi/com.raytheon.uf.tools.gfesuite/cli/src/sendgfemessage/sendGfeMessage.py
+++ edexOsgi/com.raytheon.uf.tools.gfesuite/cli/src/sendgfemessage/sendGfeMessage.py
@@ -35,7 +35,8 @@ import os, sys
 #    09/14/10                      dgilling       Initial Creation.
 #    11/04/10        5849          cjeanbap		  Updated Notification package location
 #    06/09/11        9841          rferrel        fixed help and argument order
-#                                                 for sendNotificationMsg.
+#                                                 for sendNotificationMsg.   
+#    11/29/12        DR14016       jzeng          change priority definition from 0-3 to 1-4
 # 
 #
 class PrintHelpOnErrorParser(ArgumentParser):
@@ -55,13 +56,13 @@ def main():
 	                  help="Port upon which the alertViz server is running.",
 	                  metavar="port")
 	group = parser.add_mutually_exclusive_group()
-	group.add_argument("-r", action="store_const", const=3, dest="priority",
+	group.add_argument("-r", action="store_const", const=4, dest="priority",
 	                  help="Send as a routine message. (default)")
-	group.add_argument("-s", action="store_const", const=1, dest="priority", 
+	group.add_argument("-s", action="store_const", const=2, dest="priority", 
 	                  help="Send as a significant message.")
-	group.add_argument("-u", action="store_const", const=0, dest="priority", 
+	group.add_argument("-u", action="store_const", const=1, dest="priority", 
 	                  help="Send as an urgent message.")
-	group.add_argument("-a", action="store_const", const=2, dest="priority", 
+	group.add_argument("-a", action="store_const", const=3, dest="priority", 
 	                  help="Send as an alert message.")
 	parser.add_argument("-c", action="store", dest="msgClass", 
 	                  help="Message class.", metavar="class", default="GFE")
