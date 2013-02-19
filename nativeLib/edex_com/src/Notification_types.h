a0e0bce4171c909681196e3555ae049295126ca1
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-02-19 13:50:55 -0600

    Issue #1638 Refactored viz/edex/common core projects to removed common and viz dependencies on edex projects
    Amend: Accepted ticket for work, again!  Ensured uframe product project was not deleted
    
    Change-Id: Iaa7754f25d74d893f84a7f82df605fb906e1e719
    
    Former-commit-id: 5922261f76e64f9debb0a0f44fa0d24dd791e884 [formerly d0b7850775df6a0b77d21a2bfac212a8854ea0d2] [formerly 2611980ca4d1f97af893652c36d9fc8cadc33c83] [formerly 2611980ca4d1f97af893652c36d9fc8cadc33c83 [formerly 4ed4ef8d71aa8714bb01d2064eca1fc98a10547d]] [formerly 5922261f76e64f9debb0a0f44fa0d24dd791e884 [formerly d0b7850775df6a0b77d21a2bfac212a8854ea0d2] [formerly 2611980ca4d1f97af893652c36d9fc8cadc33c83] [formerly 2611980ca4d1f97af893652c36d9fc8cadc33c83 [formerly 4ed4ef8d71aa8714bb01d2064eca1fc98a10547d]] [formerly e0ba97d547450a195307c288bc8e52539cb98ce6 [formerly 2611980ca4d1f97af893652c36d9fc8cadc33c83 [formerly 4ed4ef8d71aa8714bb01d2064eca1fc98a10547d] [formerly e0ba97d547450a195307c288bc8e52539cb98ce6 [formerly 615fa06253cc5226ddbedbb7835f3f03cf4cbb5b]]]]]
    Former-commit-id: e0ba97d547450a195307c288bc8e52539cb98ce6
    Former-commit-id: d5bc6b91c6258d7f731c6cb1ab8da72641f96827 [formerly 2aef608c3f3966a45267562c7010a53cbb0b44aa] [formerly 5617dfaa67677e476f3a51915385f435693169c8] [formerly 439eebcd94725f46de33a660eafea5629b681d39 [formerly c22d83de78a7efcb6e70f98b882e95d6a6e64ae6] [formerly 5617dfaa67677e476f3a51915385f435693169c8 [formerly 6b1426dd3445e60683f5b0eb4e38db28a689da60]]]
    Former-commit-id: 259f080c0e3c9f47dc5cb8a66db2602c3ef4e7cd [formerly 3e1df0aa7db6eca9855386f240baade979e24e3a] [formerly 1073393fbb20d52a8217fb7c53e9fb8abf28c9cf [formerly 453ac44050a337c66df9c1e3c4a0261e43f29284]]
    Former-commit-id: 1073393fbb20d52a8217fb7c53e9fb8abf28c9cf
    Former-commit-id: 9b89da117c64b1c168174490a2e85243bc1e46f9

--- nativeLib/edex_com/src/Notification_types.h
+++ nativeLib/edex_com/src/Notification_types.h
@@ -16,41 +16,41 @@
 
 
 
-typedef struct _com_raytheon_edex_msg_DataURINotificationMessage__isset {
-  _com_raytheon_edex_msg_DataURINotificationMessage__isset() : dataURIs(false) {}
+typedef struct _com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage__isset {
+  _com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage__isset() : dataURIs(false) {}
   bool dataURIs;
-} _com_raytheon_edex_msg_DataURINotificationMessage__isset;
+} _com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage__isset;
 
-class com_raytheon_edex_msg_DataURINotificationMessage {
+class com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage {
  public:
 
   static const char* ascii_fingerprint; // = "ACE4F644F0FDD289DDC4EE5B83BC13C0";
   static const uint8_t binary_fingerprint[16]; // = {0xAC,0xE4,0xF6,0x44,0xF0,0xFD,0xD2,0x89,0xDD,0xC4,0xEE,0x5B,0x83,0xBC,0x13,0xC0};
 
-  com_raytheon_edex_msg_DataURINotificationMessage() {
+  com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage() {
   }
 
-  virtual ~com_raytheon_edex_msg_DataURINotificationMessage() throw() {}
+  virtual ~com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage() throw() {}
 
   std::vector<std::string>  dataURIs;
 
-  _com_raytheon_edex_msg_DataURINotificationMessage__isset __isset;
+  _com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage__isset __isset;
 
   void __set_dataURIs(const std::vector<std::string> & val) {
     dataURIs = val;
   }
 
-  bool operator == (const com_raytheon_edex_msg_DataURINotificationMessage & rhs) const
+  bool operator == (const com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage & rhs) const
   {
     if (!(dataURIs == rhs.dataURIs))
       return false;
     return true;
   }
-  bool operator != (const com_raytheon_edex_msg_DataURINotificationMessage &rhs) const {
+  bool operator != (const com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage &rhs) const {
     return !(*this == rhs);
   }
 
-  bool operator < (const com_raytheon_edex_msg_DataURINotificationMessage & ) const;
+  bool operator < (const com_raytheon_uf_common_dataplugin_message_DataURINotificationMessage & ) const;
 
   uint32_t read(::apache::thrift::protocol::TProtocol* iprot);
   uint32_t write(::apache::thrift::protocol::TProtocol* oprot) const;
