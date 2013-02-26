03c9d67a6fd2f161d57fa70f8821f11c8dab3674
David Gillingham <David.Gillingham@raytheon.com>
2013-02-26 17:12:13 -0600

    Issue #1447: Initial commit of VTEC table sharing functionality: port A1 changes to VTECTableSqueeze, port requestAT/sendAT, re-port ingestAT/MergeVTEC, some minor bug fixes to serverConfig.py, project configs.
    Change-Id: Ibecb0aad913845224be1b7dd713e9fefef19d510
    
    Former-commit-id: 77a3f33fd5bb54390b5ff4e6bdc5551568f05c52 [formerly 9351f33c1c0c61de31e66a012ea0e8d067cfff2a] [formerly 50744c1f92559b4b985ea6b3920ce09ea5a92f9b [formerly 1e72fe30d97504f70352a181de35805a197e50f4]]
    Former-commit-id: 50744c1f92559b4b985ea6b3920ce09ea5a92f9b
    Former-commit-id: ca81ae4a710cdef0b03bbeb925d925d88f7e7800

--- edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/python/GfePyIncludeUtil.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/python/GfePyIncludeUtil.java
@@ -19,16 +19,13 @@
  **/
 package com.raytheon.uf.common.dataplugin.gfe.python;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationContext;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationLevel;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationType;
 import com.raytheon.uf.common.localization.LocalizationFile;
-import com.raytheon.uf.common.localization.PathManagerFactory;
 import com.raytheon.uf.common.python.PyUtil;
+import com.raytheon.uf.common.python.PythonIncludePathUtil;
 import com.raytheon.uf.common.util.FileUtil;
 
 /**
@@ -40,15 +37,14 @@ import com.raytheon.uf.common.util.FileUtil;
  * ------------ ---------- ----------- --------------------------
  * Oct  9, 2008            njensen     Initial creation
  * Sep 18, 2012      #1091 randerso    added base directory to getGfeConfigIncludePath
+ * Feb 27, 2013      #1447 dgilling    Re-factor based on PythonPathIncludeUtil.
  * </pre>
  * 
  * @author njensen
  * @version 1.0
  */
 
-public class GfePyIncludeUtil {
-
-    public static final String PYTHON = "python";
+public class GfePyIncludeUtil extends PythonIncludePathUtil {
 
     public static final String GFE = "gfe";
 
@@ -98,33 +94,6 @@ public class GfePyIncludeUtil {
 
     public static final String VCMOD_UTILS = FileUtil
             .join(VCMODULES, "utility");
-
-    private static final IPathManager PATH_MANAGER = PathManagerFactory
-            .getPathManager();
-
-    private static Map<LocalizationContext, Map<String, String>> pathMap = new HashMap<LocalizationContext, Map<String, String>>();
-
-    private static String getPath(LocalizationContext ctx, String locPath) {
-        Map<String, String> ctxMap = pathMap.get(ctx);
-        if (ctxMap == null) {
-            ctxMap = new HashMap<String, String>();
-            pathMap.put(ctx, ctxMap);
-        }
-        String fsPath = ctxMap.get(locPath);
-        if (fsPath == null) {
-            LocalizationFile file = PATH_MANAGER.getLocalizationFile(ctx,
-                    locPath);
-            fsPath = file.getFile().getAbsolutePath();
-            ctxMap.put(locPath, fsPath);
-        }
-        return fsPath;
-    }
-
-    // LocalizationFile getters
-
-    public static LocalizationFile getCommonPythonLF(LocalizationContext ctx) {
-        return PATH_MANAGER.getLocalizationFile(ctx, PYTHON);
-    }
 
     public static LocalizationFile getCommonGfeLF(LocalizationContext ctx) {
         return PATH_MANAGER.getLocalizationFile(ctx, COMMON_GFE);
@@ -188,11 +157,6 @@ public class GfePyIncludeUtil {
     }
 
     // Include Path getters
-
-    public static String getCommonPythonIncludePath() {
-        return getPath(PATH_MANAGER.getContext(LocalizationType.COMMON_STATIC,
-                LocalizationLevel.BASE), PYTHON);
-    }
 
     public static String getCommonGfeIncludePath() {
         String pythonDir = getCommonPythonIncludePath();
