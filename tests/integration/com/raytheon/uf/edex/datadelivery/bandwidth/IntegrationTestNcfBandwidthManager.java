73f884753abe55423e6099bcc9a9a837daefa806
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-02-27 13:06:53 -0600

    Issue #1644 WFO bandwidth manager delegates to NCF bandwidth manager for SBN subscriptions
    Amend:
      Use isEmpty() where possible in BandwidthManager.
    
    Change-Id: I85fe54982245e088f60dd33148aed72e6abfc9ec
    
    Former-commit-id: 54c3980cbaba3821402903ca478f6a9de9a7caea [formerly 28c97849800c624bdaa161f05777703ee2a510ac]
    Former-commit-id: 1161125c843c43de9d1c83b7626426fd7e6f6862

--- tests/integration/com/raytheon/uf/edex/datadelivery/bandwidth/IntegrationTestNcfBandwidthManager.java
+++ tests/integration/com/raytheon/uf/edex/datadelivery/bandwidth/IntegrationTestNcfBandwidthManager.java
@@ -21,6 +21,7 @@ package com.raytheon.uf.edex.datadelivery.bandwidth;
 
 import com.raytheon.uf.common.util.SpringFiles;
 import com.raytheon.uf.common.util.TestUtil;
+import com.raytheon.uf.edex.datadelivery.bandwidth.NcfBandwidthManagerCreator.NcfBandwidthManager;
 import com.raytheon.uf.edex.datadelivery.bandwidth.dao.IBandwidthDao;
 import com.raytheon.uf.edex.datadelivery.bandwidth.dao.IBandwidthDbInit;
 import com.raytheon.uf.edex.datadelivery.bandwidth.retrieval.RetrievalManager;
@@ -37,6 +38,7 @@ import com.raytheon.uf.edex.datadelivery.bandwidth.util.BandwidthDaoUtil;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Feb 18, 2013 1543       djohnson     Initial creation
+ * Feb 27, 2013 1644       djohnson     Extend NCF bandwidth manager.
  * 
  * </pre>
  * 
@@ -44,7 +46,7 @@ import com.raytheon.uf.edex.datadelivery.bandwidth.util.BandwidthDaoUtil;
  * @version 1.0
  */
 
-public class IntegrationTestNcfBandwidthManager extends BandwidthManager {
+public class IntegrationTestNcfBandwidthManager extends NcfBandwidthManager {
 
     static final String[] INTEGRATION_TEST_SPRING_FILES = new String[] {
             "/bandwidth/bandwidth-datadelivery-integrationtest-impl.xml",
