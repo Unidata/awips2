0ede7445bb46a52b994370b8ab178e34e0898bce
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-02-28 14:59:39 -0600

    Issue #1738: Removed unused project com.raytheon.uf.edex.ebxml
    Change-Id: I87783b45692c84cf851fb5695dae7efa79308655
    
    Former-commit-id: a186e5bf980ed25df53b11089a32120a2cd6cad2 [formerly c528a3572a10091495253c8ffcc456ffa3abe9cb] [formerly fd5302a619f7a508602e25e9a1714d5c69df984d] [formerly fd5302a619f7a508602e25e9a1714d5c69df984d [formerly e0033900cfcd893e7bb7f7dc73c6c2bec2e5e41e]] [formerly a186e5bf980ed25df53b11089a32120a2cd6cad2 [formerly c528a3572a10091495253c8ffcc456ffa3abe9cb] [formerly fd5302a619f7a508602e25e9a1714d5c69df984d] [formerly fd5302a619f7a508602e25e9a1714d5c69df984d [formerly e0033900cfcd893e7bb7f7dc73c6c2bec2e5e41e]] [formerly e01ae283f757ab3171d5cb6308c90499d198f6c8 [formerly fd5302a619f7a508602e25e9a1714d5c69df984d [formerly e0033900cfcd893e7bb7f7dc73c6c2bec2e5e41e] [formerly e01ae283f757ab3171d5cb6308c90499d198f6c8 [formerly 323bdc5919de9f04849e325dfdeb868ccdb39603]]]]]
    Former-commit-id: e01ae283f757ab3171d5cb6308c90499d198f6c8
    Former-commit-id: b5edc878213cc2ba785da538fdef5848e58081d9 [formerly 36f3eb6c2696a9f78a445463892114f0c249f052] [formerly 1314c47acf58b21f7f802dcdc8530bc4dd7490c2] [formerly d66e940665bc4c0fbdbcf4c4a0f8b86e732e857d [formerly d0b44ceeaadb2df294bc2baaa3e56918394af32f] [formerly 1314c47acf58b21f7f802dcdc8530bc4dd7490c2 [formerly 3940bf0a6dc9106d6097c50437afb3fb42fe8ede]]]
    Former-commit-id: e1c87028b9c4985e845b029ee6d66e7d34e588b4 [formerly a1bce088536c522eba3b9ee52d2ed913d0803cec] [formerly b4fb275c2966ba72ae2cd59eb2b565a5b0c8a7d6 [formerly 32a205e8807f08aa923a9be407269650b0eb9bf3]]
    Former-commit-id: b4fb275c2966ba72ae2cd59eb2b565a5b0c8a7d6
    Former-commit-id: fb01a02f165b7c664c99d991d586cbb8c3da39af

--- edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/notification/NotificationListenerImpl.java
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/notification/NotificationListenerImpl.java
@@ -1,18 +1,10 @@
-/**
- * 
- */
-package com.raytheon.uf.edex.ebxml.services;
+package com.raytheon.uf.edex.registry.ebxml.services.notification;
 
 import oasis.names.tc.ebxml.regrep.wsdl.registry.services.v4.NotificationListener;
 import oasis.names.tc.ebxml.regrep.xsd.rim.v4.NotificationType;
 
-/**
- * @author jsherida
- *
- */
 public class NotificationListenerImpl implements NotificationListener {
 
-    /** {@inheritDoc} */
     @Override
     public void onNotification(NotificationType notification) {
         // TODO Auto-generated method stub
