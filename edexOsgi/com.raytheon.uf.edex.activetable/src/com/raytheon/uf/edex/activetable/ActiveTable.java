e12342512792f4291e83bec74367ae879df78b44
David Gillingham <David.Gillingham@raytheon.com>
2013-03-01 11:23:41 -0600

    Issue #1447: Implement active table fetching, make bug fixes to active table sharing from testing.
    Change-Id: I84a3ed28f2cc8bdce0248faef984a4453d734c0d
    
    Former-commit-id: 74967080ccaab5f81a0484b04fd7a68c2e87378e [formerly c30aefd88996b12b4761edae209ef9575e7f4041] [formerly 75e82d9fca6bff921b250c4e73c7a9c2644e6de4] [formerly 75e82d9fca6bff921b250c4e73c7a9c2644e6de4 [formerly 95f8074c970a385cf479f22208070a59ce43e17d]] [formerly 0a79d08dd891f25278207144b9c240339656d8d1 [formerly 75e82d9fca6bff921b250c4e73c7a9c2644e6de4 [formerly 95f8074c970a385cf479f22208070a59ce43e17d] [formerly 0a79d08dd891f25278207144b9c240339656d8d1 [formerly cede300be734c8a05f90b9c84372866d9bd4f695]]]]
    Former-commit-id: 0a79d08dd891f25278207144b9c240339656d8d1
    Former-commit-id: fddd0fcbb0978e917b9c92005b6a96a956174231 [formerly 34ef93fbfd1261df01dfa61865a606e7d9a7141f] [formerly 94bccc87a37b46cd2baf50e40d17bbd6e2345451 [formerly 5ad72e2a7f14b368b427f59d2eec5a7e989d35d0]]
    Former-commit-id: 94bccc87a37b46cd2baf50e40d17bbd6e2345451
    Former-commit-id: 266c316c9c9b220da729382d41d7127c5d96a8a2

--- edexOsgi/com.raytheon.uf.edex.activetable/src/com/raytheon/uf/edex/activetable/ActiveTable.java
+++ edexOsgi/com.raytheon.uf.edex.activetable/src/com/raytheon/uf/edex/activetable/ActiveTable.java
@@ -20,13 +20,11 @@
 package com.raytheon.uf.edex.activetable;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -39,6 +37,7 @@ import com.raytheon.uf.common.activetable.MergeResult;
 import com.raytheon.uf.common.activetable.OperationalActiveTableRecord;
 import com.raytheon.uf.common.activetable.PracticeActiveTableRecord;
 import com.raytheon.uf.common.activetable.VTECChange;
+import com.raytheon.uf.common.activetable.VTECPartners;
 import com.raytheon.uf.common.activetable.VTECTableChangeNotification;
 import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationContext;
@@ -61,8 +60,6 @@ import com.raytheon.uf.edex.database.cluster.handler.CurrentTimeClusterLockHandl
 import com.raytheon.uf.edex.database.dao.CoreDao;
 import com.raytheon.uf.edex.database.dao.DaoConfig;
 import com.raytheon.uf.edex.database.query.DatabaseQuery;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
 
 /**
  * ActiveTable container and logic. The ActiveTable is a legacy GFE concept of
@@ -574,169 +571,6 @@ public class ActiveTable {
         CoreDao dao = practiceDao;
         String sql = "delete from practice_activetable;";
         dao.executeNativeSql(sql);
-    }
-
-    /**
-     * Convert the active table to a list of Map<String, ?>s. Doing it directly
-     * in Java eliminates the need for Python paths, handling JepExceptions, and
-     * at least one Python/Java conversion of the active table.
-     * 
-     * @param records
-     *            A list of ActiveTableRecords to convert to
-     *            Map<String,Object>s.
-     * @return records, converted to a list of Maps.
-     * 
-     *         TODO: move this method to a static utility class
-     */
-    public static List<Map<String, Object>> convertToDict(
-            List<ActiveTableRecord> records, String site) {
-
-        List<Map<String, Object>> dicts = new ArrayList<Map<String, Object>>(
-                records.size());
-        for (ActiveTableRecord atr : records) {
-            Map<String, Object> template = new HashMap<String, Object>();
-            template.put("vtecstr", atr.getVtecstr());
-            template.put("etn", Integer.valueOf(atr.getEtn()));
-            template.put("sig", atr.getSig());
-            template.put("phen", atr.getPhen());
-            if (atr.getSegText() != null) {
-                template.put("segText", atr.getSegText());
-            }
-            if (atr.getOverviewText() != null) {
-                template.put("overviewText", atr.getOverviewText());
-                template.put("hdln", atr.getOverviewText());
-            }
-            template.put("phensig", atr.getPhensig());
-            template.put("act", atr.getAct());
-            template.put("seg", atr.getSeg());
-            template.put("startTime",
-                    atr.getStartTime().getTimeInMillis() / 1000);
-            template.put("endTime", atr.getEndTime().getTimeInMillis() / 1000);
-            template.put("ufn", atr.isUfn());
-            template.put("officeid", atr.getOfficeid());
-            template.put("purgeTime",
-                    atr.getPurgeTime().getTimeInMillis() / 1000);
-            template.put("issueTime",
-                    atr.getIssueTime().getTimeInMillis() / 1000);
-            template.put("state", "Decoded");
-            template.put("xxxid", atr.getXxxid());
-
-            template.put("pil",
-                    remapPil(site, atr.getPhen(), atr.getSig(), atr.getPil()));
-            template.put("productClass", atr.getProductClass());
-
-            template.put("id", atr.getUgcZone());
-
-            template.put("rawMessage", atr.getRawmessage());
-            template.put("countyheader", atr.getCountyheader());
-            Calendar floodBegin = atr.getFloodBegin();
-            if (floodBegin != null) {
-                long floodBeginMillis = floodBegin.getTimeInMillis();
-                if (floodBeginMillis != 0) {
-                    template.put("floodBegin", floodBeginMillis / 1000);
-                }
-            }
-            template.put("wmoid", atr.getWmoid());
-
-            // Warngen fields
-            Calendar floodCrest = atr.getFloodCrest();
-            if (floodCrest != null) {
-                long floodCrestMillis = floodCrest.getTimeInMillis();
-                if (floodCrestMillis != 0) {
-                    template.put("floodCrest", floodCrestMillis / 1000);
-                }
-            }
-            Calendar floodEnd = atr.getFloodEnd();
-            if (floodEnd != null) {
-                long floodEndMillis = floodEnd.getTimeInMillis();
-                if (floodEndMillis != 0) {
-                    template.put("floodBegin", floodEndMillis / 1000);
-                }
-            }
-            String floodStatus = atr.getFloodRecordStatus();
-            if (floodStatus != null && !"".equals(floodStatus.trim())) {
-                template.put("floodrecordstatus", floodStatus);
-            }
-            String floodSeverity = atr.getFloodSeverity();
-            if (floodSeverity != null && !"".equals(floodSeverity.trim())) {
-                template.put("floodseverity", floodSeverity);
-            }
-
-            Geometry geometry = atr.getGeometry();
-            if (geometry != null && !geometry.isEmpty()) {
-                StringBuilder sb = new StringBuilder();
-                String sep = "";
-                long lat;
-                long lon;
-                for (Coordinate coordinate : geometry.getCoordinates()) {
-                    sb.append(sep);
-                    sep = " ";
-                    lat = Math.round(Math.abs(coordinate.y) * 100.0);
-                    lon = Math.round(Math.abs(coordinate.x) * 100.0);
-                    sb.append(String.format("%d %d", lat, lon));
-                }
-                template.put("geometry", sb.toString());
-            }
-
-            String immediateCause = atr.getImmediateCause();
-            if (immediateCause != null && !"".equals(immediateCause.trim())) {
-                template.put("immediateCause", immediateCause);
-            }
-
-            String loc = atr.getLoc();
-            if (loc != null && !"".equals(loc.trim())) {
-                template.put("loc", loc);
-            }
-
-            String locationId = atr.getLocationID();
-            if (locationId != null && !"".equals(locationId.trim())) {
-                template.put("locationId", locationId);
-            }
-
-            Integer motdir = atr.getMotdir();
-            if (motdir != null) {
-                template.put("motdir", motdir);
-            }
-
-            Integer motspd = atr.getMotspd();
-            if (motspd != null) {
-                template.put("motspd", motspd);
-            }
-
-            dicts.add(template);
-        }
-        return dicts;
-    }
-
-    /**
-     * Some events are issued in one PIL and cancelled or extended in another.
-     * This finds the PIL needed.
-     * 
-     * @param siteID
-     *            The site from which
-     * @param phen
-     *            The phenomenon code to look for
-     * @param sig
-     *            The significance code to look for
-     * @param dft
-     *            The PIL to use if the phensig is not remapped
-     * @return The PIL after remapping.
-     */
-    @SuppressWarnings("unchecked")
-    protected static String remapPil(String siteId, String phen, String sig,
-            String dft) {
-        String result = dft;
-        Map<Object, String> MappedPils = (Map<Object, String>) VTECPartners
-                .getInstance(siteId).getattr("VTEC_MAPPED_PILS");
-        List<String> key = new ArrayList<String>(3);
-        key.add(phen);
-        key.add(sig);
-        key.add(dft);
-        String mPil = MappedPils.get(key);
-        if (mPil != null) {
-            result = mPil;
-        }
-        return result;
     }
 
     public static File dumpProductToTempFile(String productText) {
