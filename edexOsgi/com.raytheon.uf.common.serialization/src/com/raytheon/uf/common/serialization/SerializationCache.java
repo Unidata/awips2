494514cc7339f414d9d61326927597e67595a81b
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-03-01 10:48:03 -0600

    Issue #1744 actually fix deadlock this time
    Change-Id: I9c5468d4d7d8fe19e709bc9cce42f06ca60d3eeb
    
    Former-commit-id: 8fb1ba45ced61467f967e92964e0898ddb08ce81 [formerly 8fb1ba45ced61467f967e92964e0898ddb08ce81 [formerly 3dc69632c155d42c85e9d28fd4a5b25b57a9dd00]] [formerly 8c84bc5fbb4d1a636d66acb815afe38a3d934f5a] [formerly 8fb1ba45ced61467f967e92964e0898ddb08ce81 [formerly 8fb1ba45ced61467f967e92964e0898ddb08ce81 [formerly 3dc69632c155d42c85e9d28fd4a5b25b57a9dd00]] [formerly 8c84bc5fbb4d1a636d66acb815afe38a3d934f5a] [formerly 33da766f63f78a04878892cb53a8bad963523a59 [formerly 33da766f63f78a04878892cb53a8bad963523a59 [formerly 35007e04602fe42d06dd946e5f9a70ce3ef0569a]] [formerly 8c84bc5fbb4d1a636d66acb815afe38a3d934f5a [formerly 8c84bc5fbb4d1a636d66acb815afe38a3d934f5a [formerly 56d6dd7fb0d16ef5fdb545e816fedddb28273024]]]]]
    Former-commit-id: 33da766f63f78a04878892cb53a8bad963523a59 [formerly 33da766f63f78a04878892cb53a8bad963523a59 [formerly 35007e04602fe42d06dd946e5f9a70ce3ef0569a]]
    Former-commit-id: 38218319591024263b48912c8b665e7ebbd01bb9 [formerly fe65b62d4361d035a8f827797bfa44c74a683e1d] [formerly 1ba882ea5a5793b8bbacdc07e0e35fa65aeb8a59 [formerly 348f5f0f4182248d1bbde685c08cb787813dcc14]] [formerly 33da766f63f78a04878892cb53a8bad963523a59]
    Former-commit-id: 8b83650169cdbd70e9b5d9884223fbe9887b33d5 [formerly 76f826cec1ae665ced78dda0243a23e32627f9e7] [formerly af397adac2e01505aa6dcb36cd332f075addc443 [formerly eec5f87ab24eb4a49083ac0580fc81ada09603eb]]
    Former-commit-id: 84fd6658a91a6a562bec6a5e7a1105a1ff7591d7 [formerly 312e2e07aed7e04dba9a28549f8f90caec5c6cef]
    Former-commit-id: 53df71e73d23b26a4b3afafb4ab1152b9384602e

--- edexOsgi/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/SerializationCache.java
+++ edexOsgi/com.raytheon.uf.common.serialization/src/com/raytheon/uf/common/serialization/SerializationCache.java
@@ -106,7 +106,7 @@ public class SerializationCache {
         // and will block other threads, meanwhile deeper inside create()
         // it needs to obtain a lock on the classloader which another thread
         // might already have
-        synchronized (SerializationCache.class) {
+        synchronized (SerializationCache.class.getClassLoader()) {
             bm = generator.create();
         }
         generator.setBean(null);
