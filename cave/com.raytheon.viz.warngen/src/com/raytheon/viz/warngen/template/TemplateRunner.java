5a005ceb80b0aa18356115468f3b0096d07bdf63
Steve Harris <Steven_L_Harris@raytheon.com>
2013-03-04 17:34:39 -0500

    13.2.1-11 baseline
    Former-commit-id: 5acc4265aefe5ec950f986ce363fd8a43781a692 [formerly 0fa57eeaf254f59ffc4ea822f0825a391876157d] [formerly 41dc7c0396fee753a45413d73574782641342dc9] [formerly 0684d77845c84937fc1ceabcdcc6c288ada54b1d [formerly 41dc7c0396fee753a45413d73574782641342dc9 [formerly 9e9758e833caa408d168a5de2fe35be8e5451051]]]
    Former-commit-id: 0684d77845c84937fc1ceabcdcc6c288ada54b1d
    Former-commit-id: 350b197211835ca4f69364b7aed4480599794989 [formerly 9cc1613b5024445a5ef6c5e2ff2db5fb430405ab]
    Former-commit-id: 6710698a1004d2a637e4e108339961cdbb7faf97

--- cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/template/TemplateRunner.java
+++ cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/template/TemplateRunner.java
@@ -76,6 +76,7 @@ import com.raytheon.uf.viz.core.localization.LocalizationManager;
 import com.raytheon.uf.viz.core.requests.ThriftClient;
 import com.raytheon.viz.awipstools.ToolsDataManager;
 import com.raytheon.viz.awipstools.common.StormTrackData;
+import com.raytheon.viz.awipstools.common.stormtrack.AbstractStormTrackResource;
 import com.raytheon.viz.awipstools.common.stormtrack.StormTrackState;
 import com.raytheon.viz.awipstools.common.stormtrack.StormTrackState.DisplayType;
 import com.raytheon.viz.core.mode.CAVEMode;
@@ -133,6 +134,8 @@ import com.vividsolutions.jts.io.WKTReader;
  * Dec 17, 2012   15571    Qinglu Lin  For hydro products, resolved issue caused by calling wkt.read(loc) 
  *                                     while loc is null.
  * Jan  8, 2013   15664    Qinglu Lin  Appended selectedAction to handler.handle()'s argument list.                                  
+ * Feb 12, 2013   1600     jsanchez    Correctly set the StormTrackData's motion direction for a CAN and EXP.
+ * Feb 15, 2013   1607     jsanchez    Added two variables corEventTime and corCreateTime.
  *                                   
  * </pre>
  * 
@@ -506,17 +509,18 @@ public class TemplateRunner {
                     	coords[i] = new Point2D.Double(locs[i].x, locs[i].y);
                     }
                     context.put("eventLocation", coords);
-                    double motionDirection = oldWarn.getMotdir();
-                    while (motionDirection >= 360) {
-                        motionDirection -= 360;
-                    }
-                    context.put("movementDirection", motionDirection);
+                    context.put("movementDirection", oldWarn.getMotdir());
                     context.put("movementInKnots", oldWarn.getMotspd());
 
+                    // StormTrackData motion direction is between -180/180,
+                    // whereas a WarningRecord motion direction is between
+                    // -360/360
+                    double motionDirection = AbstractStormTrackResource
+                            .adjustAngle(oldWarn.getMotdir() - 180);
                     StormTrackData std = ToolsDataManager.getInstance()
                             .getStormTrackData();
                     std.setDate(simulatedTime);
-                    std.setMotionDirection(oldWarn.getMotdir());
+                    std.setMotionDirection(motionDirection);
                     std.setMotionSpeed(oldWarn.getMotspd());
                     t0 = System.currentTimeMillis();
                     ToolsDataManager.getInstance().setStormTrackData(std);
@@ -580,9 +584,14 @@ public class TemplateRunner {
                 context.put("start", oldWarn.getIssueTime().getTime());
                 if (oldWarn.getAct().equals("NEW")) {
                     context.put("now", new Date(wwaMNDTime));
+                    // original warning's 'now' time used in MND header
+                    context.put("corCreateTime", new Date(wwaMNDTime));
                 } else
                     context.put("now", simulatedTime);
                 context.put("event", oldWarn.getIssueTime().getTime());
+                // original warning's 'event' time, which should match the storm
+                // track
+                context.put("corEventTime", eventTime);
 
                 String message = oldWarn.getRawmessage();
                 if (!stormTrackState.originalTrack) {
