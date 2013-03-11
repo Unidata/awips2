3adbc72e91d8897ef28cc16a0498a6605b0aa778
David Gillingham <David.Gillingham@raytheon.com>
2013-03-11 12:53:14 -0500

    Issue #1759: Convert ifpnetCDF and iscMosaic to use more robust request implementations, convert request handlers to use new python concurrent framework, excise GfeScript and GfeScriptExecutor from the system.
    Change-Id: I801b7b9812a72fdfc72975a311d8dc3806cda152
    
    Former-commit-id: fd2a8f663f889ea44d11a90e6df0188c851c08aa [formerly fd2a8f663f889ea44d11a90e6df0188c851c08aa [formerly 87013aad892f438abf16c28c84b608af02e6b974]] [formerly 7c745107233b89bf5e1ef5ee7b8200c03667ebd9] [formerly 90b8c80837eaf868e2dd0825341f395a377c4a37 [formerly 90b8c80837eaf868e2dd0825341f395a377c4a37 [formerly 566927980afbaa281bc33dbe007dc67fd58141c4]] [formerly 7c745107233b89bf5e1ef5ee7b8200c03667ebd9 [formerly 7c745107233b89bf5e1ef5ee7b8200c03667ebd9 [formerly edaef1edf93ed7d7f626591ecfd86fb4c76f92e8]]]]
    Former-commit-id: 90b8c80837eaf868e2dd0825341f395a377c4a37 [formerly 90b8c80837eaf868e2dd0825341f395a377c4a37 [formerly 566927980afbaa281bc33dbe007dc67fd58141c4]]
    Former-commit-id: 90b8c80837eaf868e2dd0825341f395a377c4a37
    Former-commit-id: b793ba534231d50ea0d1b59910ff459c5045c9e1 [formerly d8dd50f2ba74ca33dfab09030bf41629c94d106b]
    Former-commit-id: 1804a0efca739683cfc6bfdb27b3f44b09e2af9c

--- edexOsgi/com.raytheon.edex.plugin.gfe/src/com/raytheon/edex/plugin/gfe/server/handler/ExecuteIscMosaicRequestHandler.java
+++ edexOsgi/com.raytheon.edex.plugin.gfe/src/com/raytheon/edex/plugin/gfe/server/handler/ExecuteIscMosaicRequestHandler.java
@@ -1,12 +1,22 @@
 package com.raytheon.edex.plugin.gfe.server.handler;
 
-import com.raytheon.edex.plugin.gfe.isc.GfeScriptExecutor;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.raytheon.edex.plugin.gfe.isc.IscScript;
+import com.raytheon.edex.plugin.gfe.isc.IscScriptExecutor;
+import com.raytheon.edex.site.SiteUtil;
 import com.raytheon.uf.common.dataplugin.gfe.request.ExecuteIscMosaicRequest;
 import com.raytheon.uf.common.dataplugin.gfe.server.message.ServerResponse;
+import com.raytheon.uf.common.python.concurrent.PythonJobCoordinator;
 import com.raytheon.uf.common.serialization.comm.IRequestHandler;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
 
 /**
- * TODO Add Description
+ * {@link IRequestHandler} class for {@link ExecuteIscMosaicRequest} that
+ * executes the python script iscMosaic, which imports GFE grids from a NetCDF
+ * file.
  * 
  * <pre>
  * 
@@ -14,6 +24,7 @@ import com.raytheon.uf.common.serialization.comm.IRequestHandler;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Sep 15, 2010            dgilling     Initial creation
+ * Mar 12, 2013  #1759     dgilling     Re-write using new IscScript classes.
  * 
  * </pre>
  * 
@@ -22,20 +33,65 @@ import com.raytheon.uf.common.serialization.comm.IRequestHandler;
  */
 public class ExecuteIscMosaicRequestHandler implements
         IRequestHandler<ExecuteIscMosaicRequest> {
+    private static final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(ExecuteIscMosaicRequest.class);
+
+    private static final String METHOD_NAME = "main";
+
+    private final PythonJobCoordinator<IscScript> threadPool;
+
+    public ExecuteIscMosaicRequestHandler(
+            PythonJobCoordinator<IscScript> threadPool) {
+        this.threadPool = threadPool;
+    }
 
     @Override
-    public ServerResponse<String> handleRequest(ExecuteIscMosaicRequest request)
+    public ServerResponse<Boolean> handleRequest(ExecuteIscMosaicRequest request)
             throws Exception {
-        ServerResponse<String> sr = new ServerResponse<String>();
-        GfeScriptExecutor scriptRunner = new GfeScriptExecutor();
+        ServerResponse<Boolean> sr = new ServerResponse<Boolean>();
+        sr.setPayload(Boolean.FALSE);
 
-        String retVal = scriptRunner.execute("iscMosaic "
-                + request.getArgString());
+        statusHandler.debug("Received iscMosaic request: " + request);
+        Map<String, Object> args = new HashMap<String, Object>();
+        args.put("siteID", determineSiteId(request.getSiteID()));
+        args.put("userID", request.getUserID());
+        args.put("databaseID", request.getDatabaseID().toString());
+        args.put("parmsToProcess", request.getParmsToProcess());
+        args.put("blankOtherPeriods", request.isBlankOtherPeriods());
+        args.put("startTime", request.getStartTime());
+        args.put("endTime", request.getEndTime());
+        args.put("altMask", request.getAltMask());
+        args.put("replaceOnly", request.isReplaceOnly());
+        args.put("eraseFirst", request.isEraseFirst());
+        args.put("announce", request.getAnnounce());
+        args.put("renameWE", request.isRenameWE());
+        args.put("iscSends", request.isIscSends());
+        args.put("inFiles", request.getInFiles());
+        args.put("ignoreMask", request.isIgnoreMask());
+        args.put("adjustTranslate", request.isAdjustTranslate());
+        args.put("deleteInput", request.isDeleteInput());
+        args.put("parmsToIgnore", request.getParmsToIgnore());
+        args.put("gridDelay", request.getGridDelay());
+        args.put("logFileName", request.getLogFileName());
 
-        if (!retVal.equals(GfeScriptExecutor.SUCCESS)) {
+        IscScriptExecutor executor = new IscScriptExecutor(METHOD_NAME,
+                request.getSiteID(), args);
+        String retVal = threadPool.submitSyncJob(executor);
+
+        if (retVal != null) {
             sr.addMessage(retVal);
+            return sr;
         }
 
+        sr.setPayload(Boolean.TRUE);
         return sr;
+    }
+
+    private String determineSiteId(String inputSite) {
+        if ((inputSite == null) || inputSite.isEmpty()) {
+            return SiteUtil.getSite();
+        } else {
+            return inputSite;
+        }
     }
 }
