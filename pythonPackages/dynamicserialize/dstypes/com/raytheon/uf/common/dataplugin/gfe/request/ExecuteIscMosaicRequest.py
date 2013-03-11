fd639744bfd93327b46352010b815adc0e27637d
David Gillingham <David.Gillingham@raytheon.com>
2013-03-11 12:53:14 -0500

    Issue #1759: Convert ifpnetCDF and iscMosaic to use more robust request implementations, convert request handlers to use new python concurrent framework, excise GfeScript and GfeScriptExecutor from the system.
    Change-Id: I801b7b9812a72fdfc72975a311d8dc3806cda152
    
    Conflicts:
    
    	edexOsgi/com.raytheon.edex.plugin.gfe/META-INF/MANIFEST.MF
    	edexOsgi/com.raytheon.edex.plugin.gfe/utility/edex_static/base/gfe/isc/iscUtil.py
    
    
    Former-commit-id: aaddb9cd58400c4b3239cbd115d31fda37c4c83b [formerly 5ae3fbd325c6d4cb53e683f84b010fe0365c0fc5] [formerly d24fcc85e22dc9d100e6e18c5090fb5e702912d3] [formerly 56235e370f2b8719a7e63db0c859bd168a1d3428 [formerly d24fcc85e22dc9d100e6e18c5090fb5e702912d3 [formerly 37020656f1502afee59e41ed7daf16775e200cbf]]]
    Former-commit-id: 56235e370f2b8719a7e63db0c859bd168a1d3428
    Former-commit-id: 0808d1706a10907ff236663e85686c504930a1cb [formerly 60e8146a1832f360ca45a483cc3ea33c58f563c9]
    Former-commit-id: b97b1766ee79a8f78b0483d4515b58b088e5a95b

--- pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataplugin/gfe/request/ExecuteIscMosaicRequest.py
+++ pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataplugin/gfe/request/ExecuteIscMosaicRequest.py
@@ -19,29 +19,216 @@
 ##
 
 # File auto-generated against equivalent DynamicSerialize Java class
+# and then modified post-generation to use AbstractGfeRequest and
+# implement str(), repr()
+#    
+#     SOFTWARE HISTORY
+#    
+#    Date            Ticket#       Engineer       Description
+#    ------------    ----------    -----------    --------------------------
+#    xx/xx/??                      dgilling       Initial Creation.
+#    03/13/13         1759         dgilling       Add software history header.
+#    
+# 
+#
 
-class ExecuteIscMosaicRequest(object):
 
-    def __init__(self):
-        self.argString = None
-        self.workstationID = None
-        self.siteID = None
+from dynamicserialize.dstypes.com.raytheon.uf.common.dataplugin.gfe.request import AbstractGfeRequest
+from dynamicserialize.dstypes.com.raytheon.uf.common.message import WsId
 
-    def getArgString(self):
-        return self.argString
+class ExecuteIscMosaicRequest(AbstractGfeRequest):
 
-    def setArgString(self, argString):
-        self.argString = argString
+    def __init__(self, userID=None, databaseID=None, parmsToProcess=[], 
+                 blankOtherPeriods=False, startTime=None, endTime=None,
+                 altMask=None, replaceOnly=False, eraseFirst=False, announce="",
+                 renameWE=False, iscSends=False, inFiles=[], ignoreMask=False,
+                 adjustTranslate=False, deleteInput=False, parmsToIgnore=[],
+                 gridDelay=0.0, logFileName=None):
+        super(ExecuteIscMosaicRequest, self).__init__()
+        self.userID = userID
+        self.databaseID = databaseID
+        self.parmsToProcess = parmsToProcess
+        self.blankOtherPeriods = blankOtherPeriods
+        self.startTime = startTime
+        self.endTime = endTime
+        self.altMask = altMask
+        self.replaceOnly = replaceOnly
+        self.eraseFirst = eraseFirst
+        self.announce = announce
+        self.renameWE = renameWE
+        self.iscSends = iscSends
+        self.inFiles = inFiles
+        self.ignoreMask = ignoreMask
+        self.adjustTranslate = adjustTranslate
+        self.deleteInput = deleteInput
+        self.parmsToIgnore = parmsToIgnore
+        self.gridDelay = gridDelay
+        self.logFileName = logFileName
+        if self.userID is not None:
+            self.workstationID = WsId(progName='iscMosaic', userName=self.userID)
+        if self.databaseID is not None:
+            self.siteID = self.databaseID.getSiteId()
+        
+    def __str__(self):
+        retVal = "ExecuteIscMosaicRequest[" 
+        retVal += "wokstationID: " + str(self.workstationID) + ", "
+        retVal += "siteID: " + str(self.siteID) + ", "
+        retVal += "userID: " + str(self.userID) + ", "
+        retVal += "databaseID: " + str(self.databaseID) + ", "
+        retVal += "parmsToProcess: " + str(self.parmsToProcess) + ", "
+        retVal += "blankOtherPeriods: " + str(self.blankOtherPeriods) + ", "
+        retVal += "startTime: " + str(self.startTime) + ", "
+        retVal += "endTime: " + str(self.endTime) + ", "
+        retVal += "altMask: " + str(self.altMask) + ", "
+        retVal += "replaceOnly: " + str(self.replaceOnly) + ", "
+        retVal += "eraseFirst: " + str(self.eraseFirst) + ", "
+        retVal += "announce: " + str(self.announce) + ", "
+        retVal += "renameWE: " + str(self.renameWE) + ", "
+        retVal += "iscSends: " + str(self.iscSends) + ", "
+        retVal += "inFiles: " + str(self.inFiles) + ", "
+        retVal += "ignoreMask: " + str(self.ignoreMask) + ", "
+        retVal += "adjustTranslate: " + str(self.adjustTranslate) + ", "
+        retVal += "deleteInput: " + str(self.deleteInput) + ", "
+        retVal += "parmsToIgnore: " + str(self.parmsToIgnore) + ", "
+        retVal += "gridDelay: " + str(self.gridDelay) + ", "
+        retVal += "logFileName: " + str(self.logFileName) + "]"
+        return retVal
+    
+    def __repr__(self):
+        retVal = "ExecuteIscMosaicRequest(" 
+        retVal += "wokstationID= " + str(self.workstationID) + ", "
+        retVal += "siteID= " + str(self.siteID) + ", "
+        retVal += "userID= " + str(self.userID) + ", "
+        retVal += "databaseID= " + str(self.databaseID) + ", "
+        retVal += "parmsToProcess= " + str(self.parmsToProcess) + ", "
+        retVal += "blankOtherPeriods= " + str(self.blankOtherPeriods) + ", "
+        retVal += "startTime= " + str(self.startTime) + ", "
+        retVal += "endTime= " + str(self.endTime) + ", "
+        retVal += "altMask= " + str(self.altMask) + ", "
+        retVal += "replaceOnly= " + str(self.replaceOnly) + ", "
+        retVal += "eraseFirst= " + str(self.eraseFirst) + ", "
+        retVal += "announce= " + str(self.announce) + ", "
+        retVal += "renameWE= " + str(self.renameWE) + ", "
+        retVal += "iscSends= " + str(self.iscSends) + ", "
+        retVal += "inFiles= " + str(self.inFiles) + ", "
+        retVal += "ignoreMask= " + str(self.ignoreMask) + ", "
+        retVal += "adjustTranslate= " + str(self.adjustTranslate) + ", "
+        retVal += "deleteInput= " + str(self.deleteInput) + ", "
+        retVal += "parmsToIgnore= " + str(self.parmsToIgnore) + ", "
+        retVal += "gridDelay= " + str(self.gridDelay) + ", "
+        retVal += "logFileName= " + str(self.logFileName) + ")"
+        return retVal
 
-    def getWorkstationID(self):
-        return self.workstationID
+    def getUserID(self):
+        return self.userID
 
-    def setWorkstationID(self, workstationID):
-        self.workstationID = workstationID
+    def setUserID(self, userID):
+        self.userID = userID
 
-    def getSiteID(self):
-        return self.siteID
+    def getDatabaseID(self):
+        return self.databaseID
 
-    def setSiteID(self, siteID):
-        self.siteID = siteID
+    def setDatabaseID(self, databaseID):
+        self.databaseID = databaseID
 
+    def getParmsToProcess(self):
+        return self.parmsToProcess
+
+    def setParmsToProcess(self, parmsToProcess):
+        self.parmsToProcess = parmsToProcess
+
+    def getBlankOtherPeriods(self):
+        return self.blankOtherPeriods
+
+    def setBlankOtherPeriods(self, blankOtherPeriods):
+        self.blankOtherPeriods = blankOtherPeriods
+
+    def getStartTime(self):
+        return self.startTime
+
+    def setStartTime(self, startTime):
+        self.startTime = startTime
+
+    def getEndTime(self):
+        return self.endTime
+
+    def setEndTime(self, endTime):
+        self.endTime = endTime
+
+    def getAltMask(self):
+        return self.altMask
+
+    def setAltMask(self, altMask):
+        self.altMask = altMask
+
+    def getReplaceOnly(self):
+        return self.replaceOnly
+
+    def setReplaceOnly(self, replaceOnly):
+        self.replaceOnly = replaceOnly
+
+    def getEraseFirst(self):
+        return self.eraseFirst
+
+    def setEraseFirst(self, eraseFirst):
+        self.eraseFirst = eraseFirst
+
+    def getAnnounce(self):
+        return self.announce
+
+    def setAnnounce(self, announce):
+        self.announce = announce
+
+    def getRenameWE(self):
+        return self.renameWE
+
+    def setRenameWE(self, renameWE):
+        self.renameWE = renameWE
+
+    def getIscSends(self):
+        return self.iscSends
+
+    def setIscSends(self, iscSends):
+        self.iscSends = iscSends
+
+    def getInFiles(self):
+        return self.inFiles
+
+    def setInFiles(self, inFiles):
+        self.inFiles = inFiles
+
+    def getIgnoreMask(self):
+        return self.ignoreMask
+
+    def setIgnoreMask(self, ignoreMask):
+        self.ignoreMask = ignoreMask
+
+    def getAdjustTranslate(self):
+        return self.adjustTranslate
+
+    def setAdjustTranslate(self, adjustTranslate):
+        self.adjustTranslate = adjustTranslate
+
+    def getDeleteInput(self):
+        return self.deleteInput
+
+    def setDeleteInput(self, deleteInput):
+        self.deleteInput = deleteInput
+
+    def getParmsToIgnore(self):
+        return self.parmsToIgnore
+
+    def setParmsToIgnore(self, parmsToIgnore):
+        self.parmsToIgnore = parmsToIgnore
+
+    def getGridDelay(self):
+        return self.gridDelay
+
+    def setGridDelay(self, gridDelay):
+        self.gridDelay = gridDelay
+
+    def getLogFileName(self):
+        return self.logFileName
+
+    def setLogFileName(self, logFileName):
+        self.logFileName = logFileName
