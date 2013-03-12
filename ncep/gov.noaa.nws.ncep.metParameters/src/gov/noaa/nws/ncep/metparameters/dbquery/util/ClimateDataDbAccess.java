e98ea6446e9fce1af54d1a4eaacc9ac213b2b702
Steve Harris <Steven_L_Harris@raytheon.com>
2013-03-12 08:35:45 -0500

    13.3.1-6 baseline
    Former-commit-id: 773ad46112ade4dae698d8a801baacb8abc852d0 [formerly 4eef6862d6c4b97a758afcb4db027a3b8971d65d] [formerly 2e110bd151b12f234acdaaba54829aea99c47c42] [formerly 2e110bd151b12f234acdaaba54829aea99c47c42 [formerly 3a4c7931e1caa6dc7353437f5e02402d50a05fec]] [formerly 566b677d09cd0db26cfa0eaac987372fc47f8ae3 [formerly 2e110bd151b12f234acdaaba54829aea99c47c42 [formerly 3a4c7931e1caa6dc7353437f5e02402d50a05fec] [formerly 566b677d09cd0db26cfa0eaac987372fc47f8ae3 [formerly fecc19685d09be66f3eeeee392e565a1ce8b7e65]]]]
    Former-commit-id: 566b677d09cd0db26cfa0eaac987372fc47f8ae3
    Former-commit-id: f5fbbd7106390fe6058dba0c40b94bb2cb99c2d1 [formerly d7e03fcb3e7009a23e58cf48707fadfffebfe3d0] [formerly 6fe4b169248a87e1a4d3fbc61a2f3ffe572a1568 [formerly 4ed21b702bc22e6ebdf4ce2659b118928864203c]]
    Former-commit-id: 6fe4b169248a87e1a4d3fbc61a2f3ffe572a1568
    Former-commit-id: 0cec06c17150ae68dc7c0da555052cc5b58320cb

--- ncep/gov.noaa.nws.ncep.metParameters/src/gov/noaa/nws/ncep/metparameters/dbquery/util/ClimateDataDbAccess.java
+++
@@ -1,8 +0,0 @@
-package gov.noaa.nws.ncep.metparameters.dbquery.util;
-
-public interface ClimateDataDbAccess {
-	double getTDYF(String stationId, String monthString, String monthDayString); 
-	double getTNTF(String stationId, String monthString, String monthDayString); 
-	double getPPNT(String stationId, String monthString, String monthDayString); 
-	double getPP24(String stationId, String monthString, String monthDayString); 
-}
