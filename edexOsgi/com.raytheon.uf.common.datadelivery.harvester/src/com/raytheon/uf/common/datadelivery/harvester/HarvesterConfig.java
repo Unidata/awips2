ddb23ae59f1a60b6d2e0ed10a910a1b91bb78f9a
Dave Hladky <david_j_hladky@raytheon.com>
2013-03-18 09:03:34 -0500

    Issue #1796 Resurrected DPA code from old 5-DataDelviery repo, merged with current development!
    Change-Id: I71b56de45f61bef15c03ee4fc3f45790c59b527d
    
    Former-commit-id: a52596494feb2255269513a3cd19490628d4ae10

--- edexOsgi/com.raytheon.uf.common.datadelivery.harvester/src/com/raytheon/uf/common/datadelivery/harvester/HarvesterConfig.java
+++ edexOsgi/com.raytheon.uf.common.datadelivery.harvester/src/com/raytheon/uf/common/datadelivery/harvester/HarvesterConfig.java
@@ -1,4 +1,4 @@
-package com.raytheon.uf.edex.datadelivery.harvester.config;
+package com.raytheon.uf.common.datadelivery.harvester;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
