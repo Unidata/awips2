03219ce56a8a2bb408d00c2ce9bedab51df079d1
Dave Hladky <david_j_hladky@raytheon.com>
2013-03-19 17:36:19 -0500

    Issue #1796 updated some headers, general cleanup
    Change-Id: I272a5eb5ce706481a24af2b4cbd21fe1c44f0cd6
    
    Former-commit-id: 958b897c2133da02d37322fd90d26fdeca81ee75 [formerly 5362771743deb9953c4516ba3239802d44870a44] [formerly 958b897c2133da02d37322fd90d26fdeca81ee75 [formerly 5362771743deb9953c4516ba3239802d44870a44] [formerly 38494cec1686eaee9533e3a05e1cbe48b14cd612 [formerly edb363d0515ade9a2fdd430468ca45cd70961ac2]]]
    Former-commit-id: 38494cec1686eaee9533e3a05e1cbe48b14cd612
    Former-commit-id: 69c6fdd88795bbd252679deaaad6ea033035cd32 [formerly 8a46d432160169af610f3ed5d91bc696998f0f58]
    Former-commit-id: 7046f31351d5221d3a0effdf1ff962a88fcb5d68

--- edexOsgi/com.raytheon.uf.edex.ogc.common/src/com/raytheon/uf/edex/ogc/common/db/SimpleLayer.java
+++ edexOsgi/com.raytheon.uf.edex.ogc.common/src/com/raytheon/uf/edex/ogc/common/db/SimpleLayer.java
@@ -1,33 +1,22 @@
-/*
- * The following software products were developed by Raytheon:
- *
- * ADE (AWIPS Development Environment) software
- * CAVE (Common AWIPS Visualization Environment) software
- * EDEX (Environmental Data Exchange) software
- * uFrame™ (Universal Framework) software
- *
- * Copyright (c) 2010 Raytheon Co.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/org/documents/epl-v10.php
- *
- *
- * Contractor Name: Raytheon Company
- * Contractor Address:
- * 6825 Pine Street, Suite 340
- * Mail Stop B8
- * Omaha, NE 68106
- * 402.291.0100
- *
- *
- * SOFTWARE HISTORY
- *
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Mar 29, 2011            bclement     Initial creation
- *
- */
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.uf.edex.ogc.common.db;
 
 import java.util.Date;
@@ -56,18 +45,21 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 import com.vividsolutions.jts.geom.Polygon;
 
-/**
- * Simplified layer representation
- * 
- * @author bclement
- * @version 1.0
- */
+ /**
+ * SOFTWARE HISTORY
+ *
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 29, 2011            bclement     Initial creation
+ *
+ **/
+
 @Entity
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class SimpleLayer implements IPersistableDataObject {
+public abstract class SimpleLayer implements IPersistableDataObject<Object> {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
@@ -203,8 +195,8 @@ public abstract class SimpleLayer implements IPersistableDataObject {
 	}
 
 	@Override
-	public Object getIdentifier() {
-		return name;
+	public Integer getIdentifier() {
+		return id;
 	}
 
 	public int getId() {
