d6cc12387e210a37f9cbaa6355faf92fafe45c45
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-03-21 13:58:36 -0500

    Issue #1661 - yajsw will now log to both the console and log file indefinitely
    - yajsw can now be built from source on-demand
    - the console debug parameter set by start.sh will now be passed to the jvm
    
    Change-Id: I6146cbcb29887b462d9bda53bd3803b5775031d0
    
    Former-commit-id: 42ab5f42443667aa8ee2c3cd4e64c016cbbf3b32 [formerly ad523b9cee3a499cf9724ebfd8e2432fd42b8650 [formerly 939ff752e8e0aed3f8ac95ae9af7fcba0f052b1f] [formerly 42ab5f42443667aa8ee2c3cd4e64c016cbbf3b32 [formerly d19b2efc155e5834ee079bcfc88b0263f8652279]]]
    Former-commit-id: ad523b9cee3a499cf9724ebfd8e2432fd42b8650 [formerly 939ff752e8e0aed3f8ac95ae9af7fcba0f052b1f]
    Former-commit-id: ad523b9cee3a499cf9724ebfd8e2432fd42b8650
    Former-commit-id: 2dbabffbe5a4b8456743a681e2c7c7528d3c7438

---
+++ javaUtilities/yajsw/src/test/java/org/rzo/test/RunGroovyTestNg.java
@@ -0,0 +1,19 @@
+package org.rzo.test;
+
+import java.util.List;
+
+// boot yajsw, load groovy test files and run the tests
+public class RunGroovyTestNg
+{
+	
+	static List loadGroovyScripts(String path)
+	{
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
