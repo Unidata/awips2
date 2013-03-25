ca9572a977fbc67d99d5f7dad33667b7da0ee009
Jonathan Sanchez <jpsanchez1@raytheon.com>
2013-03-25 12:23:51 -0500

    Issue #1810 Accepted other values/types for useDirs.
    Former-commit-id: 539c4dfa8e0b9eb156ec252af1c580f895e2d947 [formerly b77ed18eb4b641f43db346060c365c4b1c0f40c6] [formerly d00c88a38cf773d12ff72756224a9e863d37ffa1 [formerly 7492d4e2ceea99c71f8040237610d95e45c1e78a]]
    Former-commit-id: d00c88a38cf773d12ff72756224a9e863d37ffa1
    Former-commit-id: 2d6be3d3dadca33f576da67f04edc9c2901b1d7a

--- cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/config/DbAreaSourceDataAdaptor.java
+++ cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/config/DbAreaSourceDataAdaptor.java
@@ -39,6 +39,7 @@ import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
  * ------------ ---------- ----------- --------------------------
  * Sep 25, 2012 #15425     Qinglu Lin   Updated createClosestPoint().
  * Feb 13, 2012  1605      jsanchez     Calculated the point based on lat,lon values.
+ * Mar 25, 2013  1810      jsanchez     Allowed other values to be accepted as a true value for useDirs.
  * 
  * </pre>
  * 
@@ -156,8 +157,10 @@ public class DbAreaSourceDataAdaptor extends AbstractDbSourceDataAdaptor {
             Map<String, Object> attributes, Geometry geom) {
         List<String> partOfArea = null;
 
-        boolean userDirections = Boolean.valueOf(String.valueOf(attributes
-                .get(useDirectionField)));
+        String userDir = String.valueOf(attributes.get(useDirectionField))
+                .toLowerCase();
+        boolean userDirections = Boolean.valueOf(userDir)
+                || userDir.equals("t") || userDir.equals("1");
         if (userDirections) {
             PreparedGeometry prepGeom = PreparedGeometryFactory.prepare(geom);
             if (prepGeom.intersects(searchArea) && !prepGeom.within(searchArea)) {
