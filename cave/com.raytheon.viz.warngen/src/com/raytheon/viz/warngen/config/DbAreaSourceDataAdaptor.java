de39c398f114b6ce4aa2418847fa42db06e788b8
Jonathan Sanchez <jpsanchez1@raytheon.com>
2013-03-25 14:41:55 -0500

    Issue #1605 Check if storm location is over an urban bound area.
    Former-commit-id: f4bb8f1f84064d4f07bdb23fc427b8da90eebcd2 [formerly 3ca4cc9dcf21a1941239871dacc12a1571746d4b] [formerly 06cd161d0e8b92a195d512f3428843988890a21d] [formerly ddcb4f3451355ab1f69ebbfc8feacbd57cab48e4 [formerly 06cd161d0e8b92a195d512f3428843988890a21d [formerly 2d58fc8149587bdc089db2b8a69f2a176c24ba23]]]
    Former-commit-id: ddcb4f3451355ab1f69ebbfc8feacbd57cab48e4
    Former-commit-id: 754ef39c309a748adb8063b8b937a6a19b5bd89c [formerly c9295e676265b8691192f4b053a089a52521b43c]
    Former-commit-id: cd1095fb29f3ac753e44fe44a092169ba0097538

--- cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/config/DbAreaSourceDataAdaptor.java
+++ cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/config/DbAreaSourceDataAdaptor.java
@@ -40,6 +40,7 @@ import com.vividsolutions.jts.geom.prep.PreparedGeometryFactory;
  * Sep 25, 2012 #15425     Qinglu Lin   Updated createClosestPoint().
  * Feb 13, 2012  1605      jsanchez     Calculated the point based on lat,lon values.
  * Mar 25, 2013  1810      jsanchez     Allowed other values to be accepted as a true value for useDirs.
+ * Mar 25, 2013  1605      jsanchez     Set ClosestPoint's prepGeom.
  * 
  * </pre>
  * 
@@ -116,9 +117,10 @@ public class DbAreaSourceDataAdaptor extends AbstractDbSourceDataAdaptor {
         List<String> partOfArea = getPartOfArea(ptFields, attributes,
                 ptRslt.geometry);
         int gid = getGid(ptFields, attributes);
-
-        return new ClosestPoint(name, point, population, warngenlev,
+        ClosestPoint cp = new ClosestPoint(name, point, population, warngenlev,
                 partOfArea, gid);
+        cp.setPrepGeom(PreparedGeometryFactory.prepare(ptRslt.geometry));
+        return cp;
     }
 
     /**
