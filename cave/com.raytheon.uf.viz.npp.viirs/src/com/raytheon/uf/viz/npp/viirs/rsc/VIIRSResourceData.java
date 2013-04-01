fff2823bd72e0ea3e42bae8c3817246c75a5eacd
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-04-01 12:39:45 -0500

    Issue #1847 Merged NPP NUCAPS changes from omaha_13.2.1-NPP into development.
    Change-Id: I37c3135794c991e2405d556823a22768dd491f00
    
    Conflicts:
    
    	cave/com.raytheon.uf.viz.npp.viirs/src/com/raytheon/uf/viz/npp/viirs/rsc/VIIRSDataCallback.java
    
    
    Former-commit-id: 675d4d54ec81e9d7a39fef93b6a17e0daca8ca34 [formerly a5994b89835612686cce0fa9fd5e9211d89ae80c] [formerly be574276f9ec54443f15185c618381b1f8428fc3] [formerly be574276f9ec54443f15185c618381b1f8428fc3 [formerly 6d0960c4efdd362e6cefb3538883c48f60d220af]] [formerly 675d4d54ec81e9d7a39fef93b6a17e0daca8ca34 [formerly a5994b89835612686cce0fa9fd5e9211d89ae80c] [formerly be574276f9ec54443f15185c618381b1f8428fc3] [formerly be574276f9ec54443f15185c618381b1f8428fc3 [formerly 6d0960c4efdd362e6cefb3538883c48f60d220af]] [formerly b042037a67c07d8aa274174226da59323699f056 [formerly be574276f9ec54443f15185c618381b1f8428fc3 [formerly 6d0960c4efdd362e6cefb3538883c48f60d220af] [formerly b042037a67c07d8aa274174226da59323699f056 [formerly ed981dc2487d169a502f7396520899fc55ff3299]]]]]
    Former-commit-id: b042037a67c07d8aa274174226da59323699f056
    Former-commit-id: 6ec5c714d2c7f95e1f28392e6e8c7349c13db583 [formerly c8e0d2ca98237dec7c1164db629be2af5d855281] [formerly fb1a6e203ba89c32941a6be05139e175d83c719e] [formerly 76517ce2463cc94613fc879ae261141c8d736223 [formerly 79c9f6bf695445a8d8715c158cdad1b211214ced] [formerly fb1a6e203ba89c32941a6be05139e175d83c719e [formerly 1b81c79b13b0c7c8bccf8a1fc3708046b1106f64]]]
    Former-commit-id: f2910355e3e0a8c0fa39c3197aa1012951315a79 [formerly b54658d12950068a772a67b068d99d660f27a3a9] [formerly 61f17d8d340513c05740dfa7ab2f8806406b7cfc [formerly 95e73c34f95c6873acee1ee306c0e7cc1864a5da]]
    Former-commit-id: 61f17d8d340513c05740dfa7ab2f8806406b7cfc
    Former-commit-id: 509942bffc6e8105f583eb685c04a0525c0e18de

--- cave/com.raytheon.uf.viz.npp.viirs/src/com/raytheon/uf/viz/npp/viirs/rsc/VIIRSResourceData.java
+++ cave/com.raytheon.uf.viz.npp.viirs/src/com/raytheon/uf/viz/npp/viirs/rsc/VIIRSResourceData.java
@@ -19,33 +19,18 @@
  **/
 package com.raytheon.uf.viz.npp.viirs.rsc;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.Queue;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 
 import com.raytheon.uf.common.dataplugin.PluginDataObject;
 import com.raytheon.uf.common.dataplugin.npp.viirs.VIIRSDataRecord;
-import com.raytheon.uf.common.dataquery.requests.RequestConstraint;
-import com.raytheon.uf.common.dataquery.requests.RequestConstraint.ConstraintType;
-import com.raytheon.uf.common.time.DataTime;
-import com.raytheon.uf.common.time.TimeRange;
-import com.raytheon.uf.viz.core.catalog.LayerProperty;
-import com.raytheon.uf.viz.core.datastructure.DataCubeContainer;
 import com.raytheon.uf.viz.core.exception.VizException;
-import com.raytheon.uf.viz.core.rsc.AbstractRequestableResourceData;
 import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
 import com.raytheon.uf.viz.core.rsc.LoadProperties;
+import com.raytheon.uf.viz.npp.AbstractNppResourceData;
 
 /**
  * VIIRS Resource data
@@ -66,38 +51,7 @@ import com.raytheon.uf.viz.core.rsc.LoadProperties;
  * @version 1.0
  */
 @XmlAccessorType(XmlAccessType.NONE)
-public class VIIRSResourceData extends AbstractRequestableResourceData {
-
-    private static class DataTimeIterator<T extends PluginDataObject>
-            implements Iterator<DataTime> {
-
-        private Iterator<T> pdoIter;
-
-        private T lastAccesed;
-
-        private DataTimeIterator(Iterator<T> pdoIter) {
-            this.pdoIter = pdoIter;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return pdoIter.hasNext();
-        }
-
-        @Override
-        public DataTime next() {
-            lastAccesed = pdoIter.next();
-            return lastAccesed.getDataTime();
-        }
-
-        @Override
-        public void remove() {
-            pdoIter.remove();
-        }
-    }
-
-    @XmlElement
-    private int groupTimeRangeMinutes = 3;
+public class VIIRSResourceData extends AbstractNppResourceData {
 
     /*
      * (non-Javadoc)
@@ -117,178 +71,6 @@ public class VIIRSResourceData extends AbstractRequestableResourceData {
             }
         }
         return new VIIRSResource(this, loadProperties, viirsRecords);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.raytheon.uf.viz.core.rsc.AbstractRequestableResourceData#
-     * requestPluginDataObjects(java.util.Collection)
-     */
-    @Override
-    protected PluginDataObject[] requestPluginDataObjects(
-            Collection<DataTime> loadSet) throws VizException {
-        List<DataTime> timesToLoad = new ArrayList<DataTime>(loadSet);
-        Collections.sort(timesToLoad);
-        DataTime first = timesToLoad.get(0);
-        DataTime last = timesToLoad.get(timesToLoad.size() - 1);
-        Map<String, RequestConstraint> requestMap = new HashMap<String, RequestConstraint>(
-                getMetadataMap());
-        RequestConstraint timeConst = new RequestConstraint();
-        timeConst.setConstraintType(ConstraintType.BETWEEN);
-        timeConst.setBetweenValueList(new String[] {
-                new DataTime(first.getValidPeriod().getStart()).toString(),
-                new DataTime(last.getValidPeriod().getEnd()).toString() });
-        requestMap.put("dataTime.refTime", timeConst);
-
-        LayerProperty property = new LayerProperty();
-        property.setEntryQueryParameters(requestMap, false);
-        property.setNumberOfImages(9999);
-
-        List<Object> pdos = DataCubeContainer.getData(property, 60000);
-        List<PluginDataObject> finalList = new ArrayList<PluginDataObject>(
-                pdos != null ? pdos.size() : 0);
-
-        if (pdos != null) {
-            for (Object obj : pdos) {
-                PluginDataObject pdo = (PluginDataObject) obj;
-                for (DataTime dt : loadSet) {
-                    if (withinRange(dt.getValidPeriod(), pdo.getDataTime())) {
-                        finalList.add(pdo);
-                        break;
-                    }
-                }
-            }
-            Collections.sort(finalList, layerComparator);
-        }
-        return finalList.toArray(new PluginDataObject[finalList.size()]);
-    }
-
-    public static boolean withinRange(TimeRange range, DataTime time) {
-        long refTime = time.getMatchRef();
-        return range.getStart().getTime() <= refTime
-                && range.getEnd().getTime() >= refTime;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.raytheon.uf.viz.core.rsc.AbstractRequestableResourceData#
-     * getAvailableTimes()
-     */
-    @Override
-    public DataTime[] getAvailableTimes() throws VizException {
-        Collection<DataTime> dts = groupTimes(Arrays.asList(super
-                .getAvailableTimes()));
-        return dts.toArray(new DataTime[dts.size()]);
-    }
-
-    /**
-     * Group times for a {@link PluginDataObject} collection based on
-     * {@link #groupTimeRangeMinutes}
-     * 
-     * @param records
-     * @return
-     */
-    public <T extends PluginDataObject> Map<DataTime, Collection<T>> groupRecordTimes(
-            Collection<T> records) {
-        long groupTimeInMillis = groupTimeRangeMinutes * 60 * 1000;
-        Map<DataTime, Collection<T>> grouped = new HashMap<DataTime, Collection<T>>();
-        Queue<T> objects = new ArrayDeque<T>(records);
-        while (objects.size() > 0) {
-            T record = objects.remove();
-            DataTime current = record.getDataTime();
-            TimeRange prev, curr;
-            prev = curr = current.getValidPeriod();
-
-            List<T> group = new ArrayList<T>();
-            while (curr != null) {
-                prev = curr;
-                DataTimeIterator<T> iter = new DataTimeIterator<T>(
-                        objects.iterator());
-                curr = match(iter, current.getValidPeriod(), groupTimeInMillis);
-                if (curr != null) {
-                    group.add(iter.lastAccesed);
-                }
-            }
-
-            grouped.put(new DataTime(prev.getStart().getTime(), prev), group);
-        }
-        return grouped;
-    }
-
-    /**
-     * Group the {@link DataTime} collection based on
-     * {@link #groupTimeRangeMinutes}
-     * 
-     * @param dataTimes
-     * @return
-     */
-    public Collection<DataTime> groupTimes(Collection<DataTime> dataTimes) {
-        long groupTimeInMillis = groupTimeRangeMinutes * 60 * 1000;
-        List<DataTime> grouped = new ArrayList<DataTime>(dataTimes.size());
-        Queue<DataTime> objects = new ArrayDeque<DataTime>(dataTimes);
-        while (objects.size() > 0) {
-            DataTime current = objects.remove();
-            TimeRange prev, curr;
-            prev = curr = current.getValidPeriod();
-            while (curr != null) {
-                prev = curr;
-                curr = match(objects.iterator(), current.getValidPeriod(),
-                        groupTimeInMillis);
-            }
-
-            grouped.add(new DataTime(prev.getStart().getTime(), prev));
-        }
-        return grouped;
-    }
-
-    private TimeRange match(Iterator<DataTime> iter, TimeRange time,
-            long groupTimeInMillis) {
-        long startT = time.getStart().getTime();
-        long endT = time.getEnd().getTime();
-        while (iter.hasNext()) {
-            DataTime dt = iter.next();
-            TimeRange dtRange = dt.getValidPeriod();
-            long s = dtRange.getStart().getTime();
-            long e = dtRange.getEnd().getTime();
-            long startCheck = s - groupTimeInMillis;
-            long endCheck = e + groupTimeInMillis;
-            if ((startT <= startCheck && endT >= startCheck)
-                    || (startCheck <= startT && endCheck >= startT)) {
-                iter.remove();
-                return new TimeRange(Math.min(s, startT), Math.max(e, endT));
-            }
-        }
-        return null;
-    }
-
-    /**
-     * @return the groupTimeRangeMinutes
-     */
-    public int getGroupTimeRangeMinutes() {
-        return groupTimeRangeMinutes;
-    }
-
-    /**
-     * @param groupTimeRangeMinutes
-     *            the groupTimeRangeMinutes to set
-     */
-    public void setGroupTimeRangeMinutes(int groupTimeRangeMinutes) {
-        this.groupTimeRangeMinutes = groupTimeRangeMinutes;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * com.raytheon.uf.viz.core.rsc.AbstractRequestableResourceData#update(java
-     * .lang.Object)
-     */
-    @Override
-    public void update(Object updateData) {
-        super.update(updateData);
-        invalidateAvailableTimesCache();
     }
 
 }
