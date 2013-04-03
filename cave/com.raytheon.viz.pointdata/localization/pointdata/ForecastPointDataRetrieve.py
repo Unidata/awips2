ef8461965eb13d681f1e418d9f39c5776b8453f7
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2013-04-03 07:41:39 -0500

    Issue #1735 ForecastPointDataRetrieve now uses DbQueryRequest to obtain forecast times in asscending order and option to limit times returned. Change reviewer comments.
    Change-Id: Ida69b63198ffa5c229eedd3abb42c4f5f9e1d00d
    
    Former-commit-id: 50ffd4082b0c6c1a5e28a7ee91075f93ca604ad8 [formerly 90dae591a610a9bc65296c32b9b0c501faa85523] [formerly 998714524d3e0425d1984ffdc9c3252fc3742870] [formerly b00d64ec0afd1b2faf84ed05a8bde02b8c2ef563 [formerly 998714524d3e0425d1984ffdc9c3252fc3742870 [formerly 882f96509c19f0621a4f6e6806e3177f6c566562]]]
    Former-commit-id: b00d64ec0afd1b2faf84ed05a8bde02b8c2ef563
    Former-commit-id: 3ba9851638529124c64d3eb3b31a74ca7dd0db71 [formerly 8c5f3fb8f76c148fb40544e24dc9315100b4e694]
    Former-commit-id: 5c4a938c7209a97e45996d5b2586796d6af42b47

--- cave/com.raytheon.viz.pointdata/localization/pointdata/ForecastPointDataRetrieve.py
+++ cave/com.raytheon.viz.pointdata/localization/pointdata/ForecastPointDataRetrieve.py
@@ -31,34 +31,47 @@ import PointDataView, PointDataContainer, NoDataException, PointDataRetrieve
 #    Date            Ticket#       Engineer       Description
 #    ------------    ----------    -----------    --------------------------
 #    25Apr2012       14688         rferrel        Initial Creation.
-#    
+#    03Apr2013       1735          rferrel        Use DbQueryRequest to get forecast times
+#                                                  and added option to limit number returned.
 # 
 #
     
 class ForecastPointDataRetrieve(PointDataRetrieve.PointDataRetrieve):
 
-    def __init__(self, pluginName, site, parameters, keyId='forecastHr', refTime=None, constraint={}, maxSize=99):
-        super(ForecastPointDataRetrieve, self).__init__(pluginName, site, parameters, keyId, refTime, constraint, maxSize)     
+    def __init__(self, pluginName, site, parameters, keyId='forecastHr', refTime=None, constraint={}, maxSize=99, forecastTimesLimit=-999):
+        self.forecastTimesLimit = forecastTimesLimit
+        super(ForecastPointDataRetrieve, self).__init__(pluginName, site, parameters, keyId, refTime, constraint, maxSize)
     
     def _query(self, parameters, maxSize):
-        times = self.__queryForecastTimes()
-        self.__javaPdc = self.__requestForecastData(times, parameters)
+        fctTimes = self.__queryForecastTimes()
+        self.__javaPdc = self.__requestForecastData(fctTimes, parameters)
         pdvDict = self._organizeData(self.__javaPdc)        
-        self.pdc = PointDataContainer.PointDataContainer(pdvDict, self.__javaPdc, self.refTime)            
+        self.pdc = PointDataContainer.PointDataContainer(pdvDict, self.__javaPdc, self.refTime)
     
-    def __queryForecastTimes(self):        
-        from com.raytheon.uf.viz.core.catalog import CatalogQuery
-        return CatalogQuery.performQuery('dataTime.fcstTime', self._buildConstraints(self.refTime))
+    def __queryForecastTimes(self):
+        from com.raytheon.uf.common.dataquery.requests import DbQueryRequest
+        from com.raytheon.uf.viz.core.requests import ThriftClient
+        from java.lang import Integer
+        request = DbQueryRequest()
+        request.setConstraints(self._buildConstraints(self.refTime))
+        request.addRequestField('dataTime.fcstTime')
+        request.setOrderByField('dataTime.fcstTime')
+        request.setDistinct(True)
+        iForecastTimesLimit = Integer(self.forecastTimesLimit)
+        if (self.forecastTimesLimit > 0) :
+            request.setLimit(iForecastTimesLimit)
+        return ThriftClient.sendRequest(request).getFieldObjects('dataTime.fcstTime', iForecastTimesLimit.getClass())
     
     def __requestForecastData(self, availableHours, parameters):
         from com.raytheon.viz.pointdata import PointDataRequest
         from com.raytheon.uf.common.time import DataTime
         from java.lang import String
         import jep
-        dts = jep.jarray(len(availableHours), DataTime)
-        for i in range(len(availableHours)):
-            dts[i] = DataTime(self.refTime, int(availableHours[i]))
-        constraints = self._buildConstraints(None) #times are explicitly set so we don't need to constrain those
+        dtsLen = len(availableHours)
+        dts = jep.jarray(dtsLen, DataTime)
+        for i in range(dtsLen):
+            dts[i] = DataTime(self.refTime, availableHours[i].intValue())
+        constraints = self._buildConstraints(None) #fctTimes are explicitly set so we don't need to constrain those
         params = jep.jarray(len(parameters), String)
         for i in range(len(parameters)):
             params[i] = String(parameters[i])
@@ -69,11 +82,8 @@ class ForecastPointDataRetrieve(PointDataRetrieve.PointDataRetrieve):
             stations = None
         return PointDataRequest.requestPointData(dts,
             self.pluginName, params, stations,
-            constraints)        
+            constraints)
     
-def retrieve(pluginName, site, parameters, keyId='forecastHr', refTime=None, constraint={}, maxSize=99):
-    ret = ForecastPointDataRetrieve(pluginName, site, parameters, keyId, refTime, constraint, maxSize)
+def retrieve(pluginName, site, parameters, keyId='forecastHr', refTime=None, constraint={}, maxSize=99, forecastTimesLimit=-999):
+    ret = ForecastPointDataRetrieve(pluginName, site, parameters, keyId, refTime, constraint, maxSize, forecastTimesLimit)
     return ret.pdc
-
-    
-    
