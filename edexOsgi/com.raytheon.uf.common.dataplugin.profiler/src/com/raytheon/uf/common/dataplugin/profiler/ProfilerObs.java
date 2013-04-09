7b7efd0bd1bf2035960d599beab66dd61ac7471c
Steve Harris <Steven_L_Harris@raytheon.com>
2013-04-09 08:37:12 -0500

    13.3.1-19 baseline
    Former-commit-id: dc9fef33a9f7b821c6ece5a8881a3c7697abddb6 [formerly 9be238bcc48cc1ce52ec0003f14e2b18010a93cb] [formerly dc9fef33a9f7b821c6ece5a8881a3c7697abddb6 [formerly 9be238bcc48cc1ce52ec0003f14e2b18010a93cb] [formerly abaf9f12f92bc2255ad62f87eb30904f8592faed [formerly ce855032b2b7235b2aea8dc66aff89ca7c54854a]]]
    Former-commit-id: abaf9f12f92bc2255ad62f87eb30904f8592faed
    Former-commit-id: ed4150db5f37aceb0e06b43e92ec52965c284f57 [formerly 340a2cfb2304ebca07ae1808f87dc1c0523ca9cf]
    Former-commit-id: 7d76371536e7ab21673bf0aedb9cdaab173f623b

--- edexOsgi/com.raytheon.uf.common.dataplugin.profiler/src/com/raytheon/uf/common/dataplugin/profiler/ProfilerObs.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.profiler/src/com/raytheon/uf/common/dataplugin/profiler/ProfilerObs.java
@@ -43,6 +43,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import org.hibernate.annotations.Index;
 
 import com.raytheon.uf.common.dataplugin.IDecoderGettable;
 import com.raytheon.uf.common.dataplugin.annotations.DataURI;
@@ -69,6 +70,7 @@ import com.vividsolutions.jts.geom.Geometry;
  * 20090413           2251 jsanchez    Implemented IDecoderGettable methods
  *                                      and plotted Profiler plots.
  * 20090610           2489 jsanchez    Updated the windSpeeed & windDirection.
+ * Apr 4, 2013        1846 bkowal      Added an index on refTime and forecastTime
  * 
  * </pre>
  * 
@@ -77,6 +79,16 @@ import com.vividsolutions.jts.geom.Geometry;
  */
 @Entity
 @Table(name = "profiler", uniqueConstraints = { @UniqueConstraint(columnNames = { "dataURI" }) })
+/*
+ * Both refTime and forecastTime are included in the refTimeIndex since
+ * forecastTime is unlikely to be used.
+ */
+@org.hibernate.annotations.Table(
+		appliesTo = "profiler",
+		indexes = {
+				@Index(name = "profiler_refTimeIndex", columnNames = { "refTime", "forecastTime" } )
+		}
+)
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 @DynamicSerialize
