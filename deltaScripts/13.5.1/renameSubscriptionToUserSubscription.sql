fc09a0686892a3cc3ea95bc65bd220dedefc702a
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-04-09 08:18:25 -0500

    Issue #1841 Update delta script for renaming PendingSubscription to PendingUserSubscription.
    Change-Id: Ie1e485a5b8f5bce3032806fe3b0f4e78d46f93e6
    
    Former-commit-id: 8822e46c1998214dbe7b659623304b4f2793db00 [formerly 82f815752c6bb15ed0dcf11c68fdb8316a584968] [formerly 7616bb80aa2750a6cf995b0e7039ad04840004bc] [formerly 4441bdebcbf5c27472519552e6a893236970f049 [formerly 7616bb80aa2750a6cf995b0e7039ad04840004bc [formerly bba3563265cb49a5376de7ee09459ed1b4a69e1f]]] [formerly ce9609076338aa91a45faaadc9ad4ba089cf90dd [formerly 7616bb80aa2750a6cf995b0e7039ad04840004bc [formerly bba3563265cb49a5376de7ee09459ed1b4a69e1f] [formerly ce9609076338aa91a45faaadc9ad4ba089cf90dd [formerly d2a1704aa98f03086f7a07e416aa6aaea1d36121]]]]
    Former-commit-id: ce9609076338aa91a45faaadc9ad4ba089cf90dd
    Former-commit-id: af02b6dc935273012960d2ce9a023b7ca9c7178e [formerly 833ee9cb3ec6ce81c7dd96ce6a139bd461b36928] [formerly 8c5d379b724ba57d782c2317e1feca076a8d9910 [formerly 08a08803fee3beca5fbd6bc2121a4374f2939f8d]]
    Former-commit-id: 8c5d379b724ba57d782c2317e1feca076a8d9910
    Former-commit-id: 07176c47523932700f5ef6d8b58238cf457e6e93

--- deltaScripts/13.5.1/renameSubscriptionToUserSubscription.sql
+++ deltaScripts/13.5.1/renameSubscriptionToUserSubscription.sql
@@ -44,5 +44,28 @@ update ebxml.value set stringvalue = regexp_replace(stringvalue, '</subscription
 -- Update the aggregated events
 update events.aggregate set grouping = regexp_replace(grouping, 'com\.raytheon\.uf\.common\.datadelivery\.registry\.Subscription', 'com.raytheon.uf.common.datadelivery.registry.UserSubscription', 'g');
 
+-- New classification nodes for InitialPendingUserSubscription
+insert into ebxml.classificationnode(id, lid, objecttype, owner, status, userversionname, versionname, description_key, name_key, code, parent, path) SELECT 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription', objecttype, owner, status, userversionname, versionname, description_key, name_key, 'com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription', parent, '/com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription' FROM ebxml.classificationnode where id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingSubscription';
+insert into ebxml.classificationnode(id, lid, objecttype, owner, status, userversionname, versionname, description_key, name_key, code, parent, path) SELECT 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingUserSubscription', 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingUserSubscription', objecttype, owner, status, userversionname, versionname, description_key, name_key, 'UserSubscription', parent, '/urn:oasis:names:tc:ebxml-regrep:classificationScheme:ObjectType/RegistryObject/UserSubscription' FROM ebxml.classificationnode where id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingSubscription';
+
+-- Update foreign keys for the taxonomy to InitialPendingUserSubscription
+update ebxml.taxonomyelementtype_classificationnode set classificationnode_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription' where classificationnode_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingSubscription';
+update ebxml.taxonomyelementtype_classificationnode set classificationnode_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingUserSubscription' where classificationnode_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingSubscription';
+
+-- Delete InitialPendingSubscription classification nodes
+delete from ebxml.classificationnode where id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:InitialPendingSubscription';
+delete from ebxml.classificationnode where id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingSubscription';
+
+-- Update registry object references from InitialPendingSubscription to InitialPendingUserSubscription
+update ebxml.registryobjectlist_registryobject set registryobject_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription' where registryobject_id = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingSubscription';
+update ebxml.registryobject set objecttype = 'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription' where objecttype ='urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.InitialPendingSubscription';
+update ebxml.value set stringvalue = regexp_replace(stringvalue, '<pendingSubscription ', '<pendingUserSubscription ', 'g');
+update ebxml.value set stringvalue = regexp_replace(stringvalue, '</pendingSubscription>', '</pendingUserSubscription>', 'g');
+update ebxml.value set stringvalue = regexp_replace(stringvalue, '<initialPendingSubscription ', '<initialPendingUserSubscription ', 'g');
+update ebxml.value set stringvalue = regexp_replace(stringvalue, '</initialPendingSubscription>', '</initialPendingUserSubscription>', 'g');
+
+-- Update the aggregated events
+update events.aggregate set grouping = regexp_replace(grouping, 'com\.raytheon\.uf\.common\.datadelivery\.registry\.InitialPendingSubscription', 'com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription', 'g');
+
 -- Commit the transaction
 END;
