e5fbf006dd83ebf45d632adc56e93a27fd33bbde
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-04-10 16:24:14 -0500

    Issue #1802 Implemented ebxml registry replication
    Change-Id: I9ddcb2f3a8410e5bae8f03b4c881625ecef8a531
    
    Former-commit-id: e3f3483b517a8db7880918ed84fe2ad17abba3ec [formerly 120906de786091655f48c2f0aedcdde0c619b0be] [formerly ec69cdeb445d042bfac7a5cd25167935dc0f90dc [formerly 078545ff0fdbd591ebf69f22eb2e8400386c2bdf]]
    Former-commit-id: ec69cdeb445d042bfac7a5cd25167935dc0f90dc
    Former-commit-id: c0099a8a39574eca3e85c64def0cf20990a42056

--- edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/annotations/RegistryObjectAssociation.java
+++ edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/annotations/RegistryObjectAssociation.java
@@ -7,27 +7,25 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import com.raytheon.uf.common.registry.RegistryManager;
+import com.raytheon.uf.common.registry.constants.AssociationTypes;
 import com.raytheon.uf.common.registry.ebxml.RegistryObjectResolver;
-import com.raytheon.uf.common.registry.ebxml.RegistryUtil;
-
-
 
 /**
- * An annotation that indicates that an association relationship exists
- * between two registry objects.  The nature of that relationship is
- * described by two arguments: An AssociationResolver and an association
- * type.
+ * An annotation that indicates that an association relationship exists between
+ * two registry objects. The nature of that relationship is described by two
+ * arguments: An AssociationResolver and an association type.
  * 
  * The AssociationResolver is used to extract the registry object(s) that
- * are/will be associated with the object marked with the RegistryObjectAssociation
- * annotation.  The association type is used to define the relationship 
- * between the objects.
+ * are/will be associated with the object marked with the
+ * RegistryObjectAssociation annotation. The association type is used to define
+ * the relationship between the objects.
  * 
  * <pre>
  * SOFTWARE HISTORY
  * Date         Ticket#     Engineer    Description
  * ------------ ----------  ----------- --------------------------
  * Apr 17, 2012 455         jspinks     Initial creation
+ * 4/9/2013     1802      bphillip   Using constant values from constants package instead of RegistryUtil
  * 
  * </pre>
  * 
@@ -41,5 +39,6 @@ import com.raytheon.uf.common.registry.ebxml.RegistryUtil;
 @Documented
 public @interface RegistryObjectAssociation {
     public Class<? extends RegistryObjectResolver> value();
-    public String associationType() default RegistryUtil.ASSOCIATION_CONTAINS;
+
+    public String associationType() default AssociationTypes.CONTAINS;
 }
