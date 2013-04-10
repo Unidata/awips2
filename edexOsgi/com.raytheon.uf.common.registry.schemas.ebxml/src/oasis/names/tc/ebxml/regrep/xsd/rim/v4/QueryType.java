0a930ce83c549f2c268bca2a5aa037d1c127e178
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-04-10 16:24:14 -0500

    Issue #1802 Implemented ebxml registry replication
    Change-Id: I9ddcb2f3a8410e5bae8f03b4c881625ecef8a531
    
    Former-commit-id: fa6cdf46dbe0a0b024af9990df60e92ead8e9fc2 [formerly e3f3483b517a8db7880918ed84fe2ad17abba3ec] [formerly 120906de786091655f48c2f0aedcdde0c619b0be] [formerly ec69cdeb445d042bfac7a5cd25167935dc0f90dc [formerly 120906de786091655f48c2f0aedcdde0c619b0be [formerly 078545ff0fdbd591ebf69f22eb2e8400386c2bdf]]]
    Former-commit-id: ec69cdeb445d042bfac7a5cd25167935dc0f90dc
    Former-commit-id: 8dba2ff501e80c05042800d7e6c73e3267e77ee2 [formerly c0099a8a39574eca3e85c64def0cf20990a42056]
    Former-commit-id: e5fbf006dd83ebf45d632adc56e93a27fd33bbde

--- edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/QueryType.java
+++ edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/QueryType.java
@@ -21,12 +21,15 @@
 package oasis.names.tc.ebxml.regrep.xsd.rim.v4;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 import org.hibernate.annotations.Cache;
@@ -64,10 +67,16 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 @XmlType(name = "QueryType")
 @DynamicSerialize
 @Entity
-@Cache(region="registryObjects",usage = CacheConcurrencyStrategy.TRANSACTIONAL)
+@Cache(region = "registryObjects", usage = CacheConcurrencyStrategy.TRANSACTIONAL)
 @Table(schema = "ebxml", name = "Query")
 public class QueryType extends ExtensibleObjectType {
+
     @Id
+    @SequenceGenerator(name = "QueryTypeGenerator", schema = "ebxml", sequenceName = "ebxml.Query_sequence")
+    @GeneratedValue(generator = "QueryTypeGenerator")
+    @XmlTransient
+    private Integer key;
+
     @XmlAttribute(required = true)
     @DynamicSerializeElement
     protected String queryDefinition;
@@ -91,6 +100,14 @@ public class QueryType extends ExtensibleObjectType {
      */
     public void setQueryDefinition(String value) {
         this.queryDefinition = value;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
     }
 
 }
--- edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/types/canonical/GarbageCollector.java
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/types/canonical/GarbageCollector.java
@@ -24,13 +24,13 @@ import java.util.List;
 
 import oasis.names.tc.ebxml.regrep.xsd.query.v4.QueryResponse;
 import oasis.names.tc.ebxml.regrep.xsd.rim.v4.QueryType;
-import oasis.names.tc.ebxml.regrep.xsd.rim.v4.RegistryObjectType;
 
+import com.raytheon.uf.common.registry.constants.CanonicalQueryTypes;
 import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
 import com.raytheon.uf.edex.registry.ebxml.services.query.types.CanonicalEbxmlQuery;
 
 /**
- * TODO Add Description
+ * Garbage collector implementation to purge items from the registry
  * 
  * <pre>
  * 
@@ -39,6 +39,7 @@ import com.raytheon.uf.edex.registry.ebxml.services.query.types.CanonicalEbxmlQu
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Jan 18, 2012            bphillip     Initial creation
+ * 4/9/2013     1802       bphillip     Changed abstract method signature, modified return processing, and changed static variables
  * 
  * </pre>
  * 
@@ -48,18 +49,15 @@ import com.raytheon.uf.edex.registry.ebxml.services.query.types.CanonicalEbxmlQu
 
 public class GarbageCollector extends CanonicalEbxmlQuery {
 
-    public static final String QUERY_DEFINITION = QUERY_CANONICAL_PREFIX
-            + "GarbageCollector";
-
     @Override
     public String getQueryDefinition() {
-        return QUERY_DEFINITION;
+        return CanonicalQueryTypes.GARBAGE_COLLECTOR;
     }
 
     @Override
-    protected <T extends RegistryObjectType> List<T> query(QueryType queryType,
-            QueryResponse queryResponse) throws EbxmlRegistryException {
-        return Collections.emptyList();
+    protected void query(QueryType queryType, QueryResponse queryResponse)
+            throws EbxmlRegistryException {
+        // TODO:Implement
     }
 
     @Override
