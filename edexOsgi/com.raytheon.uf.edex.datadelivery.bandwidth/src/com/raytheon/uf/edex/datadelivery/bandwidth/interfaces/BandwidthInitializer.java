bcafc2a2b524a9b2e9ce6b5b5aaff9fdba52d9c0
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-04-16 09:34:02 -0500

    Issue #1906 Remove new registry plugin from datadelivery feature
    Change-Id: I3ca4e0335af4b7d0de195f84fe4b3ae9c6fb8598
    
    Former-commit-id: cbc5e600f4a1e83d98d54011ea613224e2cc3b3d [formerly cbc5e600f4a1e83d98d54011ea613224e2cc3b3d [formerly e44d3a8715c11000b74fb2dbbbc46b6079ecfdca]] [formerly cc20736f289cac7c42d15d44bebc2d47e7578077 [formerly c8241840c572b69525a5486763d9d052e3710fc7]] [formerly cbc5e600f4a1e83d98d54011ea613224e2cc3b3d [formerly cbc5e600f4a1e83d98d54011ea613224e2cc3b3d [formerly e44d3a8715c11000b74fb2dbbbc46b6079ecfdca]] [formerly cc20736f289cac7c42d15d44bebc2d47e7578077 [formerly c8241840c572b69525a5486763d9d052e3710fc7]] [formerly 1a8b770da15261622b4b2d6af1ed7d8334ea7f80 [formerly 1a8b770da15261622b4b2d6af1ed7d8334ea7f80 [formerly 977702e8760541ac3d7d3850028d83e78ffbbe14]] [formerly c8241840c572b69525a5486763d9d052e3710fc7 [formerly c8241840c572b69525a5486763d9d052e3710fc7 [formerly 71b4bb07870f5490d89a1fe657429444c54a2689]]]]]
    Former-commit-id: 1a8b770da15261622b4b2d6af1ed7d8334ea7f80 [formerly 1a8b770da15261622b4b2d6af1ed7d8334ea7f80 [formerly 977702e8760541ac3d7d3850028d83e78ffbbe14]]
    Former-commit-id: 0dfb976e636c0e5fe9376155c1f39e1271a789c6 [formerly 26603eae0a187c89c7cd87a69eb1bbb4f1cf8130] [formerly eac760e58f4b9de7fd76eed3348755bf493af32f [formerly ee0572026a449d4e9bdba13898f3fc2b11bbeed7]] [formerly e0e75ca9a7e1726e890b1ca6b0a0bb5470792e13 [formerly b44a14776a22c7beba3eb9bcf0db8b3655808e1a] [formerly 6218114ae20ab3a80449f68d90cf1987c8d64cf7 [formerly 8170693941cc7c773f0d42756cd31741103a85e4]]]
    Former-commit-id: 3f6b7004491aa3dfc67a6c0294e25e2cb0940aa7 [formerly b226fecd8c9529d94c9f75d312709c4ed7601550] [formerly ccd74a419c19172ca440933c8373d182775a4e88 [formerly 8095deee57302407c79e0e246f4d8e3c78a23930]]
    Former-commit-id: eba3b896400c6218c9ada5806e083d1c398b36f5 [formerly ff9f489feac88d799a98423f811b117f3eae0459]
    Former-commit-id: 388cdcb89bef4d44e537499d27207a82914f2a6e

--- edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/interfaces/BandwidthInitializer.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/interfaces/BandwidthInitializer.java
@@ -2,6 +2,7 @@ package com.raytheon.uf.edex.datadelivery.bandwidth.interfaces;
 
 import com.raytheon.uf.edex.datadelivery.bandwidth.IBandwidthManager;
 import com.raytheon.uf.edex.datadelivery.bandwidth.dao.IBandwidthDbInit;
+import com.raytheon.uf.edex.registry.ebxml.init.RegistryInitializedListener;
 
 /**
  * * An interface for initialization of the BandwidthManager instance. The
@@ -16,13 +17,14 @@ import com.raytheon.uf.edex.datadelivery.bandwidth.dao.IBandwidthDbInit;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Oct 26, 2012 1286       djohnson     Initial creation
+ * Apr 16, 2013 1906       djohnson     Extends RegistryInitializedListener.
  * 
  * </pre>
  * 
  * @author djohnson
  * @version 1.0
  */
-public interface BandwidthInitializer {
+public interface BandwidthInitializer extends RegistryInitializedListener {
 
     /**
      * Initialize the instance of BandwidthManager.
