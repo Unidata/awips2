469f41d3579c941797e66731d0c62316ec54a1b4
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-04-16 11:28:05 -0500

    Issue #1638 Added viz base feature as well as personalities cave project extracting out non-awips specific things in personalities awips project
    Amend: Removed eclipse feature all together, added os and arch to fragments.  Ensured unpack flag stayed same when changing features.
    
    Change-Id: I78a2c5262ebdbe3ace3c180254b5f036011c0a65
    
    Former-commit-id: 665192e766e6e79b902370a1efbd4874e76f63a5 [formerly 16cce70f9d9d0433c4f05a705e545b1c8fbc58fe] [formerly 6ccef4d3c7b08519733591636afa54b71fe29632] [formerly 6ccef4d3c7b08519733591636afa54b71fe29632 [formerly bd52681be3aefbea68df775068bd0dbc38d482c7]] [formerly 665192e766e6e79b902370a1efbd4874e76f63a5 [formerly 16cce70f9d9d0433c4f05a705e545b1c8fbc58fe] [formerly 6ccef4d3c7b08519733591636afa54b71fe29632] [formerly 6ccef4d3c7b08519733591636afa54b71fe29632 [formerly bd52681be3aefbea68df775068bd0dbc38d482c7]] [formerly 7513e5982ba6d54c469a5b40550c88a515560add [formerly 6ccef4d3c7b08519733591636afa54b71fe29632 [formerly bd52681be3aefbea68df775068bd0dbc38d482c7] [formerly 7513e5982ba6d54c469a5b40550c88a515560add [formerly dd4ddb8aacf82f7c69860b470c3f754a59f0c124]]]]]
    Former-commit-id: 7513e5982ba6d54c469a5b40550c88a515560add
    Former-commit-id: 2aa881db67378151d17a1b2f675b7b995fe9643d [formerly d0edd6a882dcdbb1c7516e5bbc9c58127a5eafac] [formerly 1950aff01a9fa585af7810022016324ce1e9ed51] [formerly 22b9b60c01df6ad9c0115590423de8d624e5f904 [formerly 0dd1d5c1eb848344c287d03fc5cb6b4556d376ab] [formerly 1950aff01a9fa585af7810022016324ce1e9ed51 [formerly 57f34e908037d26ccd1f9de09198903f4455ac4b]]]
    Former-commit-id: bc5eea40deabf407d67e325fb6b5b69d235f86d2 [formerly d0f63d287352e6fa8f52b87780f423ef41d87efa] [formerly eb6a0d25d0f3313bf55d8c1da4e071ba386e8501 [formerly 2e5c1a7c32032749c43477f50c3f62142093ca72]]
    Former-commit-id: fbc8d336c541d68e1875cf299262c5e59332d5ed [formerly eb6a0d25d0f3313bf55d8c1da4e071ba386e8501]
    Former-commit-id: 10ee43ad9798389025be0c67247671de75e23eb9

--- cave/com.raytheon.uf.viz.personalities.cave/src/com/raytheon/uf/viz/personalities/cave/workbench/OpenPerspectiveList.java
+++ cave/com.raytheon.uf.viz.personalities.cave/src/com/raytheon/uf/viz/personalities/cave/workbench/OpenPerspectiveList.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.viz.ui.personalities.awips;
+package com.raytheon.uf.viz.personalities.cave.workbench;
 
 import java.util.ArrayList;
 import java.util.HashMap;
