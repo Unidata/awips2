0082ba65b4d7bb4892fa97ba49981efe2255ace6
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-04-22 09:48:58 -0500

    Issue #1931 Implemented REST binding for registry
    Change-Id: I604c02dc849e380f70e2d27fa1f2b30dadcf0776
    
    Former-commit-id: e614a5155d0b62d629f0eb2da48e76e8e5c64129 [formerly c6ee9830c20558fa4ca090489d661b810307f740] [formerly 9a7971c872c852ab4c59cdf0f5bbd70fdf8ae661] [formerly 9a7971c872c852ab4c59cdf0f5bbd70fdf8ae661 [formerly 9065ebcb65cf9045ed84344fabcab171fad91454]] [formerly 3d2e073dd7986159bcf6d27bb1a2d63e703dcd5b [formerly 9a7971c872c852ab4c59cdf0f5bbd70fdf8ae661 [formerly 9065ebcb65cf9045ed84344fabcab171fad91454] [formerly 3d2e073dd7986159bcf6d27bb1a2d63e703dcd5b [formerly daef43d8947f690d15f9314f85b8cf31d30c06ab]]]]
    Former-commit-id: 3d2e073dd7986159bcf6d27bb1a2d63e703dcd5b
    Former-commit-id: 07042dd53b92d5e3416c65ae656b67d64174b756 [formerly 81b93e88fcb675dc12f014d71b56dacd0de0e82f] [formerly 6edfda46bfee26edf828418c8df0041bec02a968 [formerly cf7da4b0010ba65653cfdfd02761814d2ceb35a1]]
    Former-commit-id: 6edfda46bfee26edf828418c8df0041bec02a968
    Former-commit-id: 1dfad752b297b53e82a8957b1fba241ecd16a644

--- edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/web/ModifyRegistryParty.java
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/web/ModifyRegistryParty.java
@@ -21,19 +21,22 @@ package com.raytheon.uf.edex.registry.ebxml.web;
 
 import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
 
 import oasis.names.tc.ebxml.regrep.xsd.lcm.v4.SubmitObjectsRequest;
 import oasis.names.tc.ebxml.regrep.xsd.rim.v4.PartyType;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.raytheon.uf.common.registry.ebxml.RegistryUtil;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.edex.core.EDEXUtil;
 import com.raytheon.uf.edex.registry.ebxml.dao.PartyDao;
 import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
 import com.raytheon.uf.edex.registry.ebxml.services.lifecycle.LifecycleManagerImpl;
@@ -41,6 +44,7 @@ import com.raytheon.uf.edex.registry.ebxml.services.lifecycle.LifecycleManagerIm
 /**
  * 
  * Servlet implementation used to modify a user or organization in the registry
+ * FIXME: This class will be refactored in a later ticket
  * 
  * <pre>
  * 
@@ -50,15 +54,17 @@ import com.raytheon.uf.edex.registry.ebxml.services.lifecycle.LifecycleManagerIm
  * ------------ ---------- ----------- --------------------------
  * 7/31/2012    #724       bphillip     Initial creation
  * 3/13/2013    1082       bphillip     Made transactional
+ * 4/19/2013    1931       bphillip     Refactored to use web application spring container and cxf services
  * 
  * </pre>
  * 
  * @author bphillip
  * @version 1.0
  */
+@Path("/DeleteRegistryParty")
+@Service
 @Transactional
-public class ModifyRegistryParty extends javax.servlet.http.HttpServlet
-        implements javax.servlet.Servlet {
+public class ModifyRegistryParty {
 
     /** The serial ID */
     private static final long serialVersionUID = -2361555059266130462L;
@@ -69,93 +75,88 @@ public class ModifyRegistryParty extends javax.servlet.http.HttpServlet
     /** Page to display upon failure */
     private static final String ERROR_RESPONSE_PAGE = "modifyPartyFailure";
 
-    // FIXME: Add spring support to servlets
+    private PartyDao partyDao;
 
-    private PartyDao partyDao = (PartyDao) EDEXUtil.getESBComponent("partyDao");
+    private LifecycleManagerImpl lcm;
 
-    private LifecycleManagerImpl lcm = (LifecycleManagerImpl) EDEXUtil
-            .getESBComponent("lcmServiceImpl");
-
-    private RegistryWebUtil webUtil = (RegistryWebUtil) EDEXUtil
-            .getESBComponent("webUtil");
+    private RegistryWebUtil webUtil;
 
     /** The logger */
     private static final transient IUFStatusHandler statusHandler = UFStatus
             .getHandler(ModifyRegistryParty.class);
 
-    @Override
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws IOException {
+    @POST
+    @Produces("text/html")
+    public Response doPost(@Context HttpServletRequest request)
+            throws IOException {
 
-        try {
-            String partyId = request.getParameter(WebFields.ID.fieldName());
-            String objectType = request.getParameter(WebFields.OBJ_TYPE
-                    .fieldName());
-            PartyType existingParty = null;
+        String partyId = request.getParameter(WebFields.ID.fieldName());
+        String objectType = request
+                .getParameter(WebFields.OBJ_TYPE.fieldName());
+        PartyType existingParty = null;
 
-            // The EDEX internal user cannot be modified
-            if (partyId.equals(RegistryUtil.DEFAULT_OWNER)) {
-                webUtil.sendErrorResponse(request, response,
-                        ERROR_RESPONSE_PAGE, partyId, objectType,
-                        "Cannot modify EDEX Internal User");
-            }
-
-            /*
-             * Check if the party already exists. If the party does not exist,
-             * the party cannot be modified. An error response is sent to the
-             * requester
-             */
-            existingParty = partyDao.getById(partyId);
-
-            if (existingParty == null) {
-                webUtil.sendErrorResponse(request, response,
-                        ERROR_RESPONSE_PAGE, partyId, objectType,
-                        "Unable to modify " + objectType + " " + partyId + ". "
-                                + objectType + " does not exist");
-            }
-
-            /*
-             * Create the submit request
-             */
-            SubmitObjectsRequest submitRequest = null;
-            try {
-                submitRequest = webUtil.createParty(request);
-            } catch (EbxmlRegistryException e) {
-                statusHandler.error("Error modifying user", e);
-                webUtil.sendErrorResponse(
-                        request,
-                        response,
-                        ERROR_RESPONSE_PAGE,
-                        partyId,
-                        objectType,
-                        "Error modifying " + objectType + "\n"
-                                + e.getLocalizedMessage());
-            }
-
-            /*
-             * Remove any associations originating from the modified party
-             */
-            try {
-                webUtil.removeAssociationsFrom(existingParty);
-                lcm.submitObjects(submitRequest);
-                webUtil.updatePC(request);
-            } catch (Exception e) {
-                statusHandler.error("Error modifying user", e);
-                webUtil.sendErrorResponse(request, response,
-                        ERROR_RESPONSE_PAGE, partyId, objectType,
-                        "Error removing associations to " + objectType + "\n"
-                                + e.getLocalizedMessage());
-            }
-            /*
-             * Send back success message to the requester
-             */
-            webUtil.sendSuccessResponse(request, response,
-                    SUCCESS_RESPONSE_PAGE, partyId, objectType);
-        } catch (ServletException e) {
-            statusHandler.error("Error generating response", e);
-            response.sendError(1,
-                    "Error generating response: " + e.getLocalizedMessage());
+        // The EDEX internal user cannot be modified
+        if (partyId.equals(RegistryUtil.DEFAULT_OWNER)) {
+            return Response.serverError().build();
+            // webUtil.sendErrorResponse(request, response,
+            // ERROR_RESPONSE_PAGE, partyId, objectType,
+            // "Cannot modify EDEX Internal User");
         }
+
+        /*
+         * Check if the party already exists. If the party does not exist, the
+         * party cannot be modified. An error response is sent to the requester
+         */
+        existingParty = partyDao.getById(partyId);
+
+        if (existingParty == null) {
+            return Response.serverError().build();
+            // webUtil.sendErrorResponse(request, response,
+            // ERROR_RESPONSE_PAGE, partyId, objectType,
+            // "Unable to modify " + objectType + " " + partyId + ". "
+            // + objectType + " does not exist");
+        }
+
+        /*
+         * Create the submit request
+         */
+        SubmitObjectsRequest submitRequest = null;
+        try {
+            submitRequest = webUtil.createParty(request);
+        } catch (EbxmlRegistryException e) {
+            statusHandler.error("Error modifying user", e);
+            return Response.serverError().build();
+            // webUtil.sendErrorResponse(
+            // request,
+            // response,
+            // ERROR_RESPONSE_PAGE,
+            // partyId,
+            // objectType,
+            // "Error modifying " + objectType + "\n"
+            // + e.getLocalizedMessage());
+        }
+
+        /*
+         * Remove any associations originating from the modified party
+         */
+        try {
+            webUtil.removeAssociationsFrom(existingParty);
+            lcm.submitObjects(submitRequest);
+            webUtil.updatePC(request);
+        } catch (Exception e) {
+            statusHandler.error("Error modifying user", e);
+            return Response.serverError().build();
+            // webUtil.sendErrorResponse(request, response,
+            // ERROR_RESPONSE_PAGE, partyId, objectType,
+            // "Error removing associations to " + objectType + "\n"
+            // + e.getLocalizedMessage());
+        }
+        /*
+         * Send back success message to the requester
+         */
+        // webUtil.sendSuccessResponse(request, response, SUCCESS_RESPONSE_PAGE,
+        // partyId, objectType);
+        return Response.ok().build();
     }
 
     public void setPartyDao(PartyDao partyDao) {
