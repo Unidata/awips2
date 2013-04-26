a276e4ee076baba00e24899252f1ad8f74ec241c
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-04-26 08:02:49 -0500

    Issue #1910 More registry object validators, shutdown BandwidthManagers in a finally
    Amend: Add comment to validator xml
    
    Change-Id: I8c0007b94bf01a15eb2030ede7bc02dff68bca38
    
    Former-commit-id: a73a0ac4c4c6de052d91f6f2ee77455e7c094287 [formerly 3429859d84274d7986a4526819bea70a773dc85c]
    Former-commit-id: 7598629ef5e8d971a6abb260662a315232b8c90f

--- tests/unit/com/raytheon/uf/common/util/SpringFiles.java
+++ tests/unit/com/raytheon/uf/common/util/SpringFiles.java
@@ -32,6 +32,7 @@ import org.junit.Ignore;
  * ------------ ---------- ----------- --------------------------
  * Feb 12, 2013 1543       djohnson     Initial creation
  * Apr 23, 2013 1910       djohnson     Add constants for ebxml spring files.
+ * May 02, 2013 1910       djohnson     Add validator plugins spring file.
  * 
  * </pre>
  * 
@@ -71,13 +72,15 @@ public class SpringFiles {
 
     public static final String EBXML_REGISTRY_DAO_XML = "/spring/ebxml-registry-dao.xml";
 
-    public static final String EBXML_REPLICATION_XML = "/spring/ebxml-replication.xml";
-
     public static final String EBXML_REPLICATION_DATADELIVERY_WFO_XML = "/spring/registry-replication-datadelivery-wfo.xml";
+
+    public static final String EBXML_SUBSCRIPTION_XML = "/spring/ebxml-subscription.xml";
 
     public static final String EBXML_XACML_XML = "/spring/ebxml-xacml.xml";
 
     public static final String EBXML_WEBSERVICES_XML = "/spring/ebxml-webservices.xml";
+
+    public static final String EBXML_VALIDATOR_PLUGINS_XML = "/spring/ebxml-validator-plugins.xml";
 
     public static final String UNIT_TEST_LOCALIZATION_BEANS_XML = "/unit-test-localization-beans.xml";
 
