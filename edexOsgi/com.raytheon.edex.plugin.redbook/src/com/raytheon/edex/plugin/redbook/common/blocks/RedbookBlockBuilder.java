a1a4e8d1e9a1bf76e56b59307dcebcfc1e1ab46e
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2013-04-29 15:25:14 -0500

    Issue #1958 Refactoring to improve performance of RedbookDecoder.
    Amend: Updates from peer comments.
    
    Change-Id: I78231666266335cb01251197b7cc9b47641e53fb
    
    Former-commit-id: 88dee905a21cfe5b1b94b82e7cf5b1c49a5c273a [formerly ead702c7a4ccf46c0e238080a964a6e12e8fbde6 [formerly 19c2a4d0b0e51274d635a0cb01ceafeb34fcf036] [formerly 88dee905a21cfe5b1b94b82e7cf5b1c49a5c273a [formerly 49f674d14cd9e7a42c295cc1db2fe36c645366f5]]]
    Former-commit-id: ead702c7a4ccf46c0e238080a964a6e12e8fbde6 [formerly 19c2a4d0b0e51274d635a0cb01ceafeb34fcf036]
    Former-commit-id: ead702c7a4ccf46c0e238080a964a6e12e8fbde6
    Former-commit-id: e2b6e449a104746de78ea4dae3c1dfc6e0fc0d68

---
+++ edexOsgi/com.raytheon.edex.plugin.redbook/src/com/raytheon/edex/plugin/redbook/common/blocks/RedbookBlockBuilder.java
@@ -0,0 +1,143 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.edex.plugin.redbook.common.blocks;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import com.raytheon.edex.plugin.redbook.common.blocks.RedbookBlock.RedbookBlockFactory;
+import com.raytheon.edex.plugin.redbook.decoder.RedbookFcstMap;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.util.PropertiesUtil;
+import com.raytheon.uf.common.util.ReflectionException;
+import com.raytheon.uf.common.util.ReflectionUtil;
+
+/**
+ * 
+ * Build RedbookBlocks from a buffer.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 20080516           1131 jkorman     Initial Coding.
+ * Apr 29, 2013 1958       bgonzale    Added class RedbookBlockHeader,
+ *                                     and moved reflective calls to the
+ *                                     mapping population method.  Map now
+ *                                     contains factory objects.
+ * 
+ * </pre>
+ * 
+ * @author jkorman
+ * @version 1.0
+ */
+public class RedbookBlockBuilder {
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(RedbookFcstMap.class);
+
+    private static final int MIN_REMAINING = 4;
+
+    private static final String FACTORY_NAME = "$Factory";
+
+    private final Map<String, RedbookBlockFactory> blockFactoryMap = new HashMap<String, RedbookBlockFactory>();
+
+    public RedbookBlockBuilder() {
+        populateMappings();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public RedbookBlock getBlock(ByteBuffer dataBuffer) {
+        RedbookBlock blockInstance = null;
+
+        if (dataBuffer != null) {
+            RedbookBlockHeader header = getHeader(dataBuffer);
+            RedbookBlockFactory factory = blockFactoryMap
+                    .get(header.blockFactoryKey);
+
+            if (factory == null) {
+                blockInstance = new DefaultBlock(header, dataBuffer);
+            } else {
+                blockInstance = factory.createBlock(header, dataBuffer);
+            }
+        }
+        return blockInstance;
+    }
+
+    /**
+     * 
+     * @param dataBuffer
+     * @return
+     */
+    public static RedbookBlockHeader getHeader(ByteBuffer dataBuffer) {
+        RedbookBlockHeader header = null;
+        short rawHdr = dataBuffer.getShort();
+        byte rawMode = dataBuffer.get();
+        byte rawSubMode = dataBuffer.get();
+
+        // Must have at least MIN_REMAINING
+        if (dataBuffer.remaining() >= MIN_REMAINING) {
+            header = new RedbookBlockHeader(rawHdr, rawMode, rawSubMode);
+        } else {
+            header = RedbookBlockHeader.DEFAULT;
+        }
+        return header;
+    }
+
+    /**
+     * 
+     */
+    private void populateMappings() {
+        final String redbookBlockProperties = "/res/conf/RedbookBlocks.properties";
+
+        try {
+            Properties redbookClassProps = PropertiesUtil.read(this.getClass()
+                    .getResourceAsStream(redbookBlockProperties));
+
+            for (String key : redbookClassProps.stringPropertyNames()) {
+                String factoryClassName = redbookClassProps.get(key)
+                        + FACTORY_NAME;
+
+                try {
+                    RedbookBlockFactory factory = ReflectionUtil
+                            .newInstanceOfAssignableType(
+                                    RedbookBlockFactory.class, factoryClassName);
+
+                    blockFactoryMap.put((String) key, factory);
+                } catch (ReflectionException e) {
+                    statusHandler.error("Could not instantiate "
+                            + factoryClassName, e);
+                }
+            }
+        } catch (IOException e) {
+            statusHandler.error(
+                    "Could not load properties from the property file "
+                            + redbookBlockProperties, e);
+        }
+    }
+}
