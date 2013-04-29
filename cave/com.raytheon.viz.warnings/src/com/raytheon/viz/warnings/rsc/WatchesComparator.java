7e923539d5359d57a404085d2a89855525d0e301
Steve Harris <Steven_L_Harris@raytheon.com>
2013-04-29 12:25:07 -0500

    13.4.1-3 baseline
    Former-commit-id: 54afedf6176ed15f53513b3150f5fd3f4ad4574a [formerly 54afedf6176ed15f53513b3150f5fd3f4ad4574a [formerly 02412063da81fbfd3f1bbdd48700e1b1ccdeb4fd]]
    Former-commit-id: 86e96f378c5980c8c8aa141b43dd1b83e2ed8c67
    Former-commit-id: 8a41dd204882b5efcc01dd92aa14443252ef2360

--- cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WatchesComparator.java
+++ cave/com.raytheon.viz.warnings/src/com/raytheon/viz/warnings/rsc/WatchesComparator.java
@@ -1,3 +1,22 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.viz.warnings.rsc;
 
 import java.util.Comparator;
@@ -6,9 +25,8 @@ import com.raytheon.uf.common.dataplugin.warning.AbstractWarningRecord;
 import com.raytheon.uf.common.dataplugin.warning.WarningRecord.WarningAction;
 
 /**
- * 
- * Compares the WarningRecords by phenSig, ETN, action, then starttime
- * (descending order)
+ * Comparator class for SPC watches. The logic for the compare method differs
+ * from the WarningRecordComparator.
  * 
  * <pre>
  * 
@@ -16,20 +34,18 @@ import com.raytheon.uf.common.dataplugin.warning.WarningRecord.WarningAction;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * May 12, 2011            jsanchez     Initial creation
- * Sep 27, 2012     1149   jsanchez     Updated order of actions. Put CON before CAN,EXP
- * Apr 18, 2013     1877   jsanchez     Ordered by starttime before action to handle updates and initial load of the resource.
+ * Apr 18, 2013            jsanchez     Initial creation
+ * 
  * </pre>
  * 
  * @author jsanchez
  * @version 1.0
  */
-public class WarningRecordComparator implements
-        Comparator<AbstractWarningRecord> {
 
+public class WatchesComparator implements Comparator<AbstractWarningRecord> {
     /**
-     * Compares the WarningRecords by phenSig, ETN, starttime (ascending order),
-     * then action
+     * Compares the WarningRecords by phenSig, ETN, action, then starttime
+     * (ascending order)
      */
     @Override
     public int compare(AbstractWarningRecord wr1, AbstractWarningRecord wr2) {
@@ -46,10 +62,6 @@ public class WarningRecordComparator implements
             }
 
             if (rval == 0) {
-                rval = wr1.getStartTime().compareTo(wr2.getStartTime());
-            }
-
-            if (rval == 0) {
                 if (wr1.getAct() != null && wr2.getAct() != null) {
                     WarningAction act1 = WarningAction.valueOf(wr1.getAct());
                     WarningAction act2 = WarningAction.valueOf(wr2.getAct());
@@ -61,15 +73,19 @@ public class WarningRecordComparator implements
                         rval = 1;
                     } else if (act1 == WarningAction.CON
                             && (act2 == WarningAction.CAN || act2 == WarningAction.EXP)) {
-                        return 1;
+                        return -1;
                     } else if (act2 == WarningAction.CON
                             && (act1 == WarningAction.CAN || act1 == WarningAction.EXP)) {
-                        return -1;
+                        return 1;
                     } else {
                         rval = wr1.getAct().compareTo(wr2.getAct());
                     }
                 }
 
+            }
+
+            if (rval == 0) {
+                rval = wr1.getStartTime().compareTo(wr2.getStartTime());
             }
         }
         return rval;
