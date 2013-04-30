873257c09b4dffd2338407446dce30888980418e
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-04-30 14:19:52 -0500

    Issue #1961 Add ability to disable grib tables.
    Former-commit-id: ca9a6ecf990fe3d72b4e1facad59faa32ddd717c [formerly ca9a6ecf990fe3d72b4e1facad59faa32ddd717c [formerly c46c523ba053ccf7d87ecb0debf5c1681dd7090c]] [formerly 1b15b0d916bbdfca63229a3db7e3227bef1dee38] [formerly ca9a6ecf990fe3d72b4e1facad59faa32ddd717c [formerly ca9a6ecf990fe3d72b4e1facad59faa32ddd717c [formerly c46c523ba053ccf7d87ecb0debf5c1681dd7090c]] [formerly 1b15b0d916bbdfca63229a3db7e3227bef1dee38] [formerly c6490ac3c3e0945dd853d69ceebc2c8d66e95369 [formerly c6490ac3c3e0945dd853d69ceebc2c8d66e95369 [formerly 63d20bc2e14bbf0cc07ea12598f6c5d2794f576a]] [formerly 1b15b0d916bbdfca63229a3db7e3227bef1dee38 [formerly 1b15b0d916bbdfca63229a3db7e3227bef1dee38 [formerly 1370f32e8a2e2efd4b238b0a9b0e3621ad13043d]]]]]
    Former-commit-id: c6490ac3c3e0945dd853d69ceebc2c8d66e95369 [formerly c6490ac3c3e0945dd853d69ceebc2c8d66e95369 [formerly 63d20bc2e14bbf0cc07ea12598f6c5d2794f576a]]
    Former-commit-id: e7eb3f8e3a26f390737541a8a6f71a4e6dde8650 [formerly f89502e2cab0fbedc8038935a47db2b14a6f9541] [formerly c67dd59c7319ca8e3c4fc49212d6fdd7861c4e73 [formerly b88b19568bf7af83d30dc5474c6811574e2f534f]] [formerly 93e5ebd7f828125521fb279969644b299d7465cc [formerly 9aa069341442ad627465dcdc33d53439e4ebc33b] [formerly a8a931ad5b4104b349c6065b5a48ec7e8ce44630 [formerly 159533b98668fc9717e7047f7ff1842e2bc985ad]]]
    Former-commit-id: 84eaef00eb46c14756188da0fa20c2bd58236937 [formerly 9306a4ac00a35adabc9b22247a87c13568f4c529] [formerly 99490fbb52f3585c2772d8f392cab98a345fe32e [formerly 031e8f6c57f4f0f4672873feea755a7fe4ab07b3]]
    Former-commit-id: 94d6a1d76927b8e6b4030b1026ef3ecbdafa21e6 [formerly 9f5eaa52f74c300e48e2b1c009c7a8d85e75adb9]
    Former-commit-id: a189d7f14a7d4ce60540e5f90887d9c85d0c847e

--- edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/util/GribModelLookup.java
+++ edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/util/GribModelLookup.java
@@ -48,6 +48,24 @@ import com.raytheon.uf.common.util.mapping.Alias;
 import com.raytheon.uf.common.util.mapping.AliasList;
 import com.raytheon.uf.common.util.mapping.MultipleMappingException;
 
+/**
+ * 
+ * Lookup a GridModel based off the information in the grib file.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 30, 2013            bsteffen    Initial javadoc
+ * Apr 30, 2013 1961       bsteffen    Add ability to disable grib tables.
+ * 
+ * </pre>
+ * 
+ * @author unknown
+ * @version 1.0
+ */
 public class GribModelLookup {
     private static final transient IUFStatusHandler statusHandler = UFStatus
             .getHandler(GribModelLookup.class);
@@ -199,8 +217,12 @@ public class GribModelLookup {
             }
         }
 
-        modelSet.addModels(initCommonStaticModels());
+        addModels(modelSet);
+        long endTime = System.currentTimeMillis();
+        logger.info("Grib models initialized: " + (endTime - startTime) + "ms");
+    }
 
+    private void addModels(GridModelSet modelSet) {
         for (GridModel model : modelSet.getModels()) {
             modelsByName.put(model.getName(), model);
             for (int process : model.getProcess()) {
@@ -218,8 +240,6 @@ public class GribModelLookup {
                 }
             }
         }
-        long endTime = System.currentTimeMillis();
-        logger.info("Grib models initialized: " + (endTime - startTime) + "ms");
     }
 
     /**
@@ -234,8 +254,8 @@ public class GribModelLookup {
      * @return
      * @throws GribException
      */
-    private List<GridModel> initCommonStaticModels() {
-        List<GridModel> modelSet = new ArrayList<GridModel>();
+    public Boolean initCommonStaticModels() {
+        GridModelSet modelSet = new GridModelSet();
         LocalizationContext commonStaticSite = PathManagerFactory
                 .getPathManager().getContext(
                         LocalizationContext.LocalizationType.COMMON_STATIC,
@@ -250,9 +270,11 @@ public class GribModelLookup {
 
         for (LocalizationFile modelFile : legacyFiles) {
             try {
+                logger.info("Loading deprecated gribModel file: "
+                        + modelFile.getFile());
                 GridModelSet fileSet = JAXB.unmarshal(modelFile.getFile(),
                         GridModelSet.class);
-                modelSet.addAll(fileSet.getModels());
+                modelSet.addModels(fileSet.getModels());
                 ArrayList<DatasetInfo> infoList = new ArrayList<DatasetInfo>(
                         fileSet.getModels().size());
                 for (GridModel model : fileSet.getModels()) {
@@ -315,7 +337,10 @@ public class GribModelLookup {
                 }
             }
         }
-        return modelSet;
+        if (modelSet.getModels() != null) {
+            addModels(modelSet);
+        }
+        return modelSet.getModels() != null && !modelSet.getModels().isEmpty();
     }
 
     private String toKey(Integer center, Integer subcenter, String grid,
