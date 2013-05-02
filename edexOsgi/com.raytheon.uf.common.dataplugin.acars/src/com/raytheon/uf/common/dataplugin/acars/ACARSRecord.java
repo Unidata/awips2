7c91a8352193f6feb7558a2b1ae35e43517f4ffe
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-05-02 16:58:15 -0500

    Issue #1869 Remove dataURI column from PluginDataObject. Change-Id: I3450130d8e68c065683794ba941a83897fe36745
    Former-commit-id: 79015595ee4b6e25a2bdc92bcd364637e9d236ae [formerly 4209e014d8858e966d427601d06b852cb4306a52 [formerly 7fd2829cd730562a549a2f3c423ac5ead15872e7]]
    Former-commit-id: 4209e014d8858e966d427601d06b852cb4306a52
    Former-commit-id: dab2af2df560c1285bfd6721399ae1777b48da34

--- edexOsgi/com.raytheon.uf.common.dataplugin.acars/src/com/raytheon/uf/common/dataplugin/acars/ACARSRecord.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.acars/src/com/raytheon/uf/common/dataplugin/acars/ACARSRecord.java
@@ -31,6 +31,8 @@ import javax.measure.quantity.Velocity;
 import javax.measure.unit.NonSI;
 import javax.measure.unit.SI;
 import javax.measure.unit.Unit;
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -70,6 +72,8 @@ import com.vividsolutions.jts.geom.Geometry;
  * May 21, 2009       2338 jsanchez    Updated the getMessageData.
  * Apr 4, 2013        1846 bkowal      Added an index on refTime and forecastTime
  * Apr 12, 2013       1857 bgonzale    Added SequenceGenerator annotation.
+ * May 07, 2013 1869       bsteffen    Remove dataURI column from
+ *                                     PluginDataObject.
  * 
  * </pre>
  * 
@@ -787,4 +791,10 @@ public class ACARSRecord extends PluginDataObject implements ISpatialEnabled,
         return result;
     }
     
+    @Override
+    @Column
+    @Access(AccessType.PROPERTY)
+    public String getDataURI() {
+        return super.getDataURI();
+    }
 }
