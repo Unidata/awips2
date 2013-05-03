fd46b826250f975f4829308ada5e12da62a736b5
Steve Harris <Steven_L_Harris@raytheon.com>
2013-05-03 09:20:30 -0500

    13.4.1-4.1 baseline (recovered lost bin directories for 13.4.1-4)
    Former-commit-id: 95ccb973f95ce15794e4a05b1130bf16d28fa942 [formerly 29777b406e56e922a2ad0854106eb7cfc0b77826] [formerly 344054d3e90a903927b0f6520a6edfbef4dab45f] [formerly 344054d3e90a903927b0f6520a6edfbef4dab45f [formerly 1a6fd7edfdaddb9520f8eac7e0937967720efd7a]] [formerly 95ccb973f95ce15794e4a05b1130bf16d28fa942 [formerly 29777b406e56e922a2ad0854106eb7cfc0b77826] [formerly 344054d3e90a903927b0f6520a6edfbef4dab45f] [formerly 344054d3e90a903927b0f6520a6edfbef4dab45f [formerly 1a6fd7edfdaddb9520f8eac7e0937967720efd7a]] [formerly 0f76c3cbee09fd94f8fa31febf40e6d4b1ab68f8 [formerly 344054d3e90a903927b0f6520a6edfbef4dab45f [formerly 1a6fd7edfdaddb9520f8eac7e0937967720efd7a] [formerly 0f76c3cbee09fd94f8fa31febf40e6d4b1ab68f8 [formerly dca18e1b350f5424ec129b6cd0d602ceb86e119b]]]]]
    Former-commit-id: 0f76c3cbee09fd94f8fa31febf40e6d4b1ab68f8
    Former-commit-id: dfd1fbc9f4526de1f26e04920ae6f46bd506f41b [formerly f83b6daf5667cc026304cff34be33951f61772c6] [formerly c1349f4a6d25f171f26fb2e430cbb9a1c10bb0c6] [formerly bd58348d91e443ccca0622bd1d2e89f4dc3fca89 [formerly 40a0e843311ddd470a12258a3bc95ce73d614c01] [formerly c1349f4a6d25f171f26fb2e430cbb9a1c10bb0c6 [formerly 99a7521fd67c20e3e04a23b102968a4868fce9a5]]]
    Former-commit-id: 3c45d3063b5d39a7e17fa46789bc9cabb3a0b2f9 [formerly aa7844c83d5bb0a0e9bf3d45f714ce4f67810c47] [formerly e1a369a499b1e83f2b4a57157ad280cb79f678b3 [formerly 764924cd1e9611893446dbd6bc5080265f52be14]]
    Former-commit-id: e1a369a499b1e83f2b4a57157ad280cb79f678b3
    Former-commit-id: 4647d70fd8fe3ea639cc7c50b5272b4ea3413324

---
+++ edexOsgi/build.edex/esb/bin/start.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+# edex startup script
+
+# Verify that awips2-python and awips2-java are installed.
+rpm -q awips2-python > /dev/null 2>&1
+RC=$?
+if [ ${RC} -ne 0 ]; then
+   echo "ERROR: awips2-python Must Be Installed."
+   echo "Unable To Continue ... Terminating."
+   exit 1
+fi
+
+rpm -q awips2-java > /dev/null 2>&1
+RC=$?
+if [ ${RC} -ne 0 ]; then
+   echo "ERROR: awips2-java Must Be Installed."
+   echo "Unable To Continue ... Terminating."
+   exit 1
+fi
+
+rpm -q awips2-psql > /dev/null 2>&1
+RC=$?
+if [ ${RC} -ne 0 ]; then
+   echo "ERROR: awips2-psql Must Be Installed."
+   echo "Unable To Continue ... Terminating."
+   exit 1
+fi
+
+# Find the locations of awips2-python and awips2-java.
+PYTHON_INSTALL="/awips2/python"
+JAVA_INSTALL="/awips2/java"
+PSQL_INSTALL="/awips2/psql"
+
+path_to_script=`readlink -f $0`
+dir=$(dirname $path_to_script)
+
+export EDEX_HOME=$(dirname $dir)
+awips_home=$(dirname $EDEX_HOME)
+
+# Source The File With The Localization Information
+source ${dir}/setup.env
+
+#source /awips2/edex/bin/yajsw/bin/setenv.sh
+
+### AWIPS 1 support ###
+if [ -f /etc/rc.config.d/AWIPS ]; then
+   . /etc/rc.config.d/AWIPS
+fi
+export SHLIB_PATH=$PROJECT/sharedlib
+### End AWIPS 1 support ###
+
+export HOSTNAME=`hostname`
+
+# set Python & Java into the path
+export PATH=$awips_home/bin:${JAVA_INSTALL}/bin:${PYTHON_INSTALL}/bin:$PATH
+
+# set Service Backup scripts into the path
+export PATH=$PATH:$awips_home/GFESuite/bin:$awips_home/GFESuite/ServiceBackup/scripts
+
+# set AWIPS 1 stuff into path
+export PATH=$PATH:$PROJECT/bin
+
+export JAVA_HOME="${JAVA_INSTALL}"
+export LD_LIBRARY_PATH=$EDEX_HOME/lib/native/linux32/awips1:${JAVA_INSTALL}/lib:${PYTHON_INSTALL}/lib:${PSQL_INSTALL}/lib:$PROJECT/sharedLib
+export LD_LIBRARY_PATH=/awips2/edex/lib/lib_illusion:$LD_LIBRARY_PATH
+
+export LD_PRELOAD="libpython.so"
+export FXA_DATA=$EDEX_HOME/data/fxa
+export ALLOW_ARCHIVE_DATA="false"
+
+# setup environment for HPE
+export AMQP_SPEC=$awips_home/python/share/amqp/amqp.0-10.xml
+
+#-------------------------------------------------------------------------
+#read and interpret the command line arguments
+#-------------------------------------------------------------------------
+CONSOLE_FLAG=on
+CONSOLE_LOGLEVEL=DEBUG
+DEBUG_FLAG=off
+PROFILE_FLAG=off
+HIGH_MEM_FLAG=off
+CONF_FILE="wrapper.conf"
+RUN_MODE=
+for arg in $@
+do
+  case $arg in
+    -b|-d|--debug|-db|-bd) DEBUG_FLAG=on;;
+    -p|--profiler) PROFILE_FLAG=on;;
+    -h|--highmem) HIGH_MEM_FLAG=on;;
+    -noConsole) CONSOLE_FLAG=off;;
+    *) RUN_MODE=$arg;;
+  esac
+done
+
+export HIGH_MEM_FLAG
+export EDEX_RUN_MODE=$RUN_MODE
+
+if [ $CONSOLE_FLAG == "off" ]; then
+	CONSOLE_LOGLEVEL=NONE
+fi
+
+export CONSOLE_LOGLEVEL
+
+# source environment files
+. $EDEX_HOME/etc/default.sh
+
+if [ -e $EDEX_HOME/etc/${RUN_MODE}.sh ]; then
+    . $EDEX_HOME/etc/${RUN_MODE}.sh
+fi
+
+if [ $PROFILE_FLAG == "on" ]; then
+    . $EDEX_HOME/etc/profiler.sh
+fi
+
+# enable core dumps
+#ulimit -c unlimited
+
+# determine whether we are running a 32-bit or 64-bit version of EDEX -
+# based on the AWIPS II Java that has been installed
+EDEX_BITS="32"
+EXPECTED_ARCH=`file ${JAVA_INSTALL}/bin/java | awk '{ print $3; }'`
+if [ "${EXPECTED_ARCH}" = "64-bit" ]; then
+   EDEX_BITS="64"
+fi
+export EDEX_BITS=${EDEX_BITS}
+
+WRAPPER_ARGS=""
+if [ $DEBUG_FLAG == "on" ]; then
+   WRAPPER_ARGS="wrapper.java.debug.port=${EDEX_DEBUG_PORT}"
+   echo "To Debug ... Connect to Port: ${EDEX_DEBUG_PORT}."
+fi
+
+java -jar ${EDEX_HOME}/bin/yajsw/wrapper.jar -c ${EDEX_HOME}/conf/${CONF_FILE} ${WRAPPER_ARGS}
