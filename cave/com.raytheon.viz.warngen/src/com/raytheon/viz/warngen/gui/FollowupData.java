7636a7fcadd903229fa6fbd5022e880c86c75fad
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2013-05-08 09:57:13 -0500

    Issue #1973 Changes to properly determine Issue and Purge time when ingesting a warngen.
    Change-Id: If8045d03084b9085b573cc11c42b6a2f93ffacc8
    
    Former-commit-id: 938cd275cc6fe13e7656e71615ad5c09cf481a4e [formerly d1a3e3dc2ffff2563852cfb5d279e25533f91fbc [formerly b794f0b882d88cec6f552bf10f7f58096dfd5a69]]
    Former-commit-id: d1a3e3dc2ffff2563852cfb5d279e25533f91fbc
    Former-commit-id: 3b27e54b31d598957c0100dfdba99c6fece05477

--- cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/gui/FollowupData.java
+++ cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/gui/FollowupData.java
@@ -1,3 +1,22 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.viz.warngen.gui;
 
 import java.util.Calendar;
@@ -5,17 +24,37 @@ import java.util.Calendar;
 import com.raytheon.uf.common.dataplugin.warning.AbstractWarningRecord;
 import com.raytheon.uf.common.dataplugin.warning.WarningRecord;
 import com.raytheon.uf.common.time.SimulatedTime;
+import com.raytheon.uf.common.time.util.TimeUtil;
 
+/**
+ * A subclass of Warning Record to create the display strings needed by the GUI.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ *                                     Initial creation
+ * May 7, 2013  1973       rferrel     Changes to properly display Issue Time.
+ * 
+ * </pre>
+ * 
+ * @author rferrel
+ * @version 1.0
+ */
 public class FollowupData extends WarningRecord {
 
     private static final long serialVersionUID = 1L;
 
-    /** String displayed in the drop down update list */
+    /**
+     * String displayed in the drop down update list.
+     */
     public String displayString;
 
     /**
      * String used to test if this object is equivalent to one of the updated
-     * items in the drop down
+     * items in the drop down.
      */
     public String equvialentString;
 
@@ -39,21 +78,21 @@ public class FollowupData extends WarningRecord {
      */
     private String getDisplayString(WarningAction status,
             AbstractWarningRecord record) {
-        StringBuffer rval = new StringBuffer();
+        StringBuilder rval = new StringBuilder();
         if (record.getProductClass().equals("T")) {
             rval.append("T.");
         }
-        rval.append(status.toString() + "-");
-        rval.append(record.getOfficeid() + ".");
-        rval.append(record.getPhen() + ".");
-        rval.append(record.getSig() + ".");
+        rval.append(status.toString()).append("-");
+        rval.append(record.getOfficeid()).append(".");
+        rval.append(record.getPhen()).append(".");
+        rval.append(record.getSig()).append(".");
         rval.append(record.getEtn());
 
         if (status != WarningAction.CAN) {
             rval.append(buildExpStr(status, record));
         }
 
-        equvialentString = rval.toString().substring(0,
+        equvialentString = rval.substring(0,
                 record.getProductClass().equals("T") ? 20 : 18);
 
         return rval.toString();
@@ -70,29 +109,27 @@ public class FollowupData extends WarningRecord {
      */
     private String buildExpStr(WarningAction status,
             AbstractWarningRecord record) {
-        StringBuffer rval = new StringBuffer();
+        StringBuilder rval = new StringBuilder();
         Calendar cal = Calendar.getInstance();
         cal.setTime(SimulatedTime.getSystemTime().getTime());
-        // Positive means not yet expired
-        long diffMins = (record.getEndTime().getTimeInMillis() - cal
-                .getTimeInMillis()) / (60 * 1000);
-        if (diffMins == 0) {
-            rval.append(" Expired");
-        } else if (diffMins > 0) {
-            rval.append(" Exp in " + diffMins + " min");
+        if (status != WarningAction.COR) {
+            // Positive means not yet expired
+            long diffMins = (record.getEndTime().getTimeInMillis() - cal
+                    .getTimeInMillis()) / TimeUtil.MILLIS_PER_MINUTE;
+            if (diffMins == 0) {
+                rval.append(" Expired");
+            } else if (diffMins > 0) {
+                rval.append(" Exp in ").append(diffMins).append(" min");
+            } else {
+                rval.append(" Exp ").append(-diffMins).append(" min ago");
+            }
         } else {
-            rval.append(" Exp " + -diffMins + " min ago");
-        }
-        if (status == WarningAction.COR) {
-            rval.delete(0, rval.length());
-            diffMins = (cal.getTimeInMillis() - record.getIssueTime()
-                    .getTimeInMillis()) / (60 * 1000);
+            long diffMins = (cal.getTimeInMillis() - record.getIssueTime()
+                    .getTimeInMillis()) / TimeUtil.MILLIS_PER_MINUTE;
             if (diffMins == 0) {
                 rval.append(" Just Issued");
-            } else if (diffMins > 0) {
-                rval.append(" Issued " + diffMins + " min ago");
-            } else if (diffMins < 0) {
-                rval.append(" Issued " + diffMins + " min ago");
+            } else {
+                rval.append(" Issued ").append(diffMins).append(" min ago");
             }
         }
         return rval.toString();
