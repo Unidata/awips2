5dc7de691b21ec13dba3a4920465438f48b67fec
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-05-14 18:13:44 -0500

    Issue #1869 Remove DataURI column from bufrmos. Change-Id: Ic1a25588a45c2dc8a1a089332a4d6a33c41e205e
    Former-commit-id: 026a824e607e057a40ab8e550f663badf4f618a2 [formerly 026a824e607e057a40ab8e550f663badf4f618a2 [formerly 5b740e5d614d9cac2476335a0754ccd0e1185c84]]
    Former-commit-id: fabbc29fe8d05b43712ec3b64f02bc499806470c
    Former-commit-id: 3f540d16f085edf45a838f35afc8caaa290a7404

--- edexOsgi/com.raytheon.edex.plugin.bufrmos/src/com/raytheon/edex/plugin/bufrmos/common/BufrMosNgmData.java
+++ edexOsgi/com.raytheon.edex.plugin.bufrmos/src/com/raytheon/edex/plugin/bufrmos/common/BufrMosNgmData.java
@@ -19,9 +19,6 @@
  **/
 package com.raytheon.edex.plugin.bufrmos.common;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -45,10 +42,12 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * May 25, 2011            rjpeter     Initial creation
- * Apr 4, 2013        1846 bkowal      Added an index on refTime and forecastTime
- * Apr 12, 2013       1857 bgonzale    Added SequenceGenerator annotation.
+ * Apr 04, 2013 1846       bkowal      Added an index on refTime and
+ *                                     forecastTime
+ * Apr 12, 2013 1857       bgonzale    Added SequenceGenerator annotation.
  * May 07, 2013 1869       bsteffen    Remove dataURI column from
  *                                     PluginDataObject.
+ * May 14, 2013 1869       bsteffen    Remove DataURI column from bufrmos.
  * 
  * </pre>
  * 
@@ -57,7 +56,8 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
  */
 @Entity
 @SequenceGenerator(initialValue = 1, name = PluginDataObject.ID_GEN, sequenceName = "bufrmosNgmseq")
-@Table(name = "bufrmosNgm", uniqueConstraints = { @UniqueConstraint(columnNames = { "dataURI" }) })
+@Table(name = "bufrmosNgm", uniqueConstraints = { @UniqueConstraint(columnNames = {
+        "location_id", "refTime", "forecastTime" }) })
 /*
  * Both refTime and forecastTime are included in the refTimeIndex since
  * forecastTime is unlikely to be used.
@@ -76,10 +76,5 @@ public class BufrMosNgmData extends BufrMosData {
     public MOSType getType() {
         return MOSType.NGM;
     }
-    @Override
-    @Column
-    @Access(AccessType.PROPERTY)
-    public String getDataURI() {
-        return super.getDataURI();
-    }
+
 }
