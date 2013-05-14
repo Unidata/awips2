3f540d16f085edf45a838f35afc8caaa290a7404
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-05-14 18:13:44 -0500

    Issue #1869 Remove DataURI column from bufrmos. Change-Id: Ic1a25588a45c2dc8a1a089332a4d6a33c41e205e
    Former-commit-id: 026a824e607e057a40ab8e550f663badf4f618a2 [formerly 5b740e5d614d9cac2476335a0754ccd0e1185c84]
    Former-commit-id: fabbc29fe8d05b43712ec3b64f02bc499806470c

--- deltaScripts/13.5.1/dropDataURI.sh
+++ deltaScripts/13.5.1/dropDataURI.sh
@@ -4,16 +4,43 @@
 
 PSQL="/awips2/psql/bin/psql"
 
-echo "INFO: Dropping dataURI columns."
-
-# TODO this script will need to be extended for unique constraint.
-for table in gfe;
-do
-   echo "INFO: Dropping DataURI column from $table"
-   ${PSQL} -U awips -d metadata -c "ALTER TABLE $table DROP COLUMN IF EXISTS datauri;"
+# takes one arg: a table name
+# drops the datauri constraint and column if they exist
+function dropDatauri {
+   echo "INFO: Dropping DataURI column from $1"
+   ${PSQL} -U awips -d metadata -c "ALTER TABLE $1 CONSTRAINT IF EXISTS ${1}_datauri_key;"
+   ${PSQL} -U awips -d metadata -c "ALTER TABLE $1 DROP COLUMN IF EXISTS datauri;"
    if [ $? -ne 0 ]; then
       echo "ERROR: Failed to drop dataURI column for $table"
       echo "FATAL: The update has failed."
       exit 1
    fi
-done
+}
+
+# takes three args: table, constraint name, unique columns
+# will first drop the constraint if it exists and then adds it back, this is
+# fairly inefficient if it does exist but operationally it won't exist and for
+# testing this allows the script to be run easily as a noop.
+function dropDatauriAndAddConstraint {
+   dropDatauri $1
+   ${PSQL} -U awips -d metadata -c "ALTER TABLE $1 DROP CONSTRAINT IF EXISTS $2;"
+   ${PSQL} -U awips -d metadata -c "ALTER TABLE $1 ADD CONSTRAINT $2 UNIQUE $3;"
+   if [ $? -ne 0 ]; then
+      echo "ERROR: Failed to add new unique constraint for $table"
+      echo "FATAL: The update has failed."
+      exit 1
+   fi
+}
+
+echo "INFO: Dropping dataURI columns."
+
+dropDatauri gfe
+dropDatauriAndAddConstraint bufrmosavn bufrmosavn_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmoseta bufrmoseta_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmosgfs bufrmosgfs_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmoshpc bufrmoshpc_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmoslamp bufrmoslamp_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmosmrf bufrmosmrf_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+dropDatauriAndAddConstraint bufrmosngm bufrmosngm_location_id_reftime_forecasttime_key "(location_id, reftime, forecasttime)"
+
+echo "INFO: dataURI columns dropped successfully"
