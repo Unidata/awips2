a314bd8f4a2817939e694d7b91310c823ebaf745
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-05-16 13:04:36 -0500

    Issue #2000 Subscription overlap configuration GUI
    Amend:
      Peer review comments.
    
    Change-Id: Ie951ca4ce61099d0e25ed4938fc71981f34833d6
    
    Former-commit-id: 554347f17c1224e55ef311d754c1ee0709896ad4 [formerly 5b62669f8eb495617feca562af96c6b1a4ee173c] [formerly 554347f17c1224e55ef311d754c1ee0709896ad4 [formerly 5b62669f8eb495617feca562af96c6b1a4ee173c] [formerly 38249cd7b9d328fa020e897b4ec38150d8e8a4e5 [formerly c7ad25ab90220a2aa25e704b10c9a7a96af2a8a8]]]
    Former-commit-id: 38249cd7b9d328fa020e897b4ec38150d8e8a4e5
    Former-commit-id: 3a504d1e7b8804abf4f17543a3568573d8bc1e55 [formerly 7858a3b57f087d46a998d2471f9252ff7e8a534c]
    Former-commit-id: d12b26ac1de921cedba32ddf86ea6a6a0cc46377

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/system/SystemLatencyTab.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/system/SystemLatencyTab.java
@@ -19,26 +19,11 @@
  **/
 package com.raytheon.uf.viz.datadelivery.system;
 
-import java.util.Collections;
+import java.util.List;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.List;
 
-import com.raytheon.uf.common.auth.user.IUser;
 import com.raytheon.uf.common.datadelivery.request.DataDeliveryPermission;
-import com.raytheon.uf.common.status.IUFStatusHandler;
-import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.common.status.UFStatus.Priority;
-import com.raytheon.uf.viz.core.auth.UserController;
-import com.raytheon.uf.viz.core.exception.VizException;
-import com.raytheon.uf.viz.datadelivery.services.DataDeliveryServices;
-import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
 
 /**
  * System Management Main Dialog.
@@ -53,279 +38,51 @@ import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
  * Oct 03, 2012  1241       djohnson   Use {@link DataDeliveryPermission} and registry handlers.
  * Jan 04, 2012  1420       mpduff     Add delete rule function.
  * Jan 14, 2013  1286       djohnson   Rule list is single item selectable.
+ * May 17, 2013  2000       djohnson   Consolidate duplicate code into {@link SystemRulesTab}.
  * 
  * </pre>
  * 
  * @author jpiatt
  * @version 1.0
  */
-public class SystemLatencyTab {
-
-    /** Status Handler */
-    private final IUFStatusHandler statusHandler = UFStatus
-            .getHandler(SystemLatencyTab.class);
-
-    private final String notAuthorizedMsg = " is not authorized to create, edit, or delete rules using the Data Delivery System Management\nPermission: ";
-
-    /** Parent Composite */
-    private final Composite parentComp;
-
-    /** Priority Composite */
-    private Composite latencyComp;
-
-    /** Create and edit rule dialog */
-    private CreateEditRuleDlg ruleDlg;
+public class SystemLatencyTab extends SystemRulesTab {
 
     /** Rule type constant */
-    private final String LATENCY_TYPE = "latency";
-
-    /** Available List widget */
-    private List latencyList;
-
-    /** Flag for create and edit. */
-    private boolean create;
-
-    /** Edit rule button. */
-    private Button editBtn;
-
-    /** New rule button. */
-    private Button newBtn;
-
-    /** Delete rule button. */
-    private Button deleteBtn;
-
-    /** Button Height. */
-    private final int buttonHeight = SWT.DEFAULT;
-
-    /** Button Width. */
-    private final int buttonWidth = 70;
+    private static final String LATENCY_TYPE = "latency";
 
     /**
      * Constructor.
      * 
      * @param parentComp
-     *            The Composite holding these controls
+     *            the parent comp
      */
     public SystemLatencyTab(Composite parentComp) {
-        this.parentComp = parentComp;
-        init();
+        super(parentComp, LATENCY_TYPE);
     }
 
     /**
-     * Initialize the tab.
-     */
-    private void init() {
-
-        createLatencyRulesTab();
-        createSideButtons();
-
-    }
-
-    /**
-     * Create the bar that may be expanded depending on item count.
-     */
-    private void createLatencyRulesTab() {
-
-        GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
-        GridLayout gl = new GridLayout(2, false);
-
-        latencyComp = new Composite(parentComp, SWT.NONE);
-        latencyComp.setLayout(gl);
-        gd.widthHint = 375;
-        gd.heightHint = 200;
-
-        latencyList = new List(latencyComp, SWT.BORDER | SWT.MULTI
-                | SWT.V_SCROLL | SWT.H_SCROLL | SWT.SINGLE);
-        latencyList.setLayoutData(gd);
-        latencyList.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent e) {
-                enableButtons(latencyList.getSelectionCount() > 0);
-            }
-        });
-
-        loadList();
-    }
-
-    /**
-     * Create the move up/down controls
-     */
-    private void createSideButtons() {
-        GridData actionData = new GridData(SWT.DEFAULT, SWT.CENTER, false, true);
-        GridLayout actionLayout = new GridLayout(1, false);
-        Composite actionComp = new Composite(latencyComp, SWT.NONE);
-        actionComp.setLayout(actionLayout);
-        actionComp.setLayoutData(actionData);
-
-        GridData btnData = new GridData(buttonWidth, buttonHeight);
-
-        newBtn = new Button(actionComp, SWT.PUSH);
-        newBtn.setText("New...");
-        newBtn.setLayoutData(btnData);
-        newBtn.setEnabled(true);
-        newBtn.setToolTipText("Create a new rule");
-        newBtn.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent event) {
-                create = true;
-                handleLatencyRule();
-                if (latencyList.getItemCount() > 0) {
-                    latencyList.select(0);
-                    enableButtons(true);
-                } else {
-                    enableButtons(false);
-                }
-            }
-        });
-
-        btnData = new GridData(buttonWidth, buttonHeight);
-        editBtn = new Button(actionComp, SWT.PUSH);
-        editBtn.setText("Edit...");
-        editBtn.setLayoutData(btnData);
-        editBtn.setEnabled(false);
-        editBtn.setToolTipText("Edit item selected in the list");
-        editBtn.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent event) {
-                create = false;
-                int idx = latencyList.getSelectionIndex();
-                handleLatencyRule();
-                if (!latencyList.isDisposed()) {
-                    latencyList.select(idx);
-                }
-            }
-        });
-
-        btnData = new GridData(buttonWidth, buttonHeight);
-        deleteBtn = new Button(actionComp, SWT.PUSH);
-        deleteBtn.setText("Delete...");
-        deleteBtn.setLayoutData(btnData);
-        deleteBtn.setEnabled(false);
-        deleteBtn.setToolTipText("Delete item selected in the list");
-        deleteBtn.addSelectionListener(new SelectionAdapter() {
-            @Override
-            public void widgetSelected(SelectionEvent event) {
-                handleDeleteRule();
-                if (latencyList.getItemCount() > 0) {
-                    latencyList.select(0);
-                }
-            }
-        });
-    }
-
-    /**
-     * Enable the buttons?
+     * Get the tab text.
      * 
-     * @param enable
-     *            setting for the buttons
+     * @return the tab text
      */
-    private void enableButtons(boolean enable) {
-        deleteBtn.setEnabled(enable);
-        editBtn.setEnabled(enable);
+    @Override
+    public String getTabText() {
+        return "Latency Rules";
     }
 
     /**
-     * New/Edit latency rule action handler.
+     * {@inheritDoc}
      */
-    private void handleLatencyRule() {
-        final DataDeliveryPermission permission = DataDeliveryPermission.SYSTEM_MANAGEMENT_CREATE;
-        IUser user = UserController.getUserObject();
-        String msg = user.uniqueId() + notAuthorizedMsg + permission;
-
-        try {
-            if (DataDeliveryServices.getPermissionsService()
-                    .checkPermission(user, msg, permission).isAuthorized()) {
-
-                if (ruleDlg == null || ruleDlg.isDisposed()) {
-
-                    // New
-                    if (create) {
-                        ruleDlg = new CreateEditRuleDlg(parentComp.getShell(),
-                                create, LATENCY_TYPE);
-                    } else {
-                        // Edit
-                        String ruleName = null;
-                        int selectedIdx = latencyList.getSelectionIndex();
-                        if (selectedIdx > -1) {
-                            ruleName = latencyList.getItem(selectedIdx);
-                        } else if (latencyList.getItemCount() <= 0) {
-                            DataDeliveryUtils.showMessage(
-                                    parentComp.getShell(), SWT.ERROR,
-                                    "Create Rule", "Please create a rule.");
-                            return;
-                        } else {
-                            DataDeliveryUtils.showMessage(
-                                    parentComp.getShell(), SWT.ERROR,
-                                    "Select Rule",
-                                    "Please select a rule for edit.");
-                            return;
-                        }
-                        ruleDlg = new CreateEditRuleDlg(parentComp.getShell(),
-                                create, ruleName, LATENCY_TYPE);
-                    }
-                    ruleDlg.open();
-                } else {
-                    ruleDlg.bringToTop();
-                }
-
-            }
-        } catch (VizException e) {
-            statusHandler.handle(Priority.PROBLEM,
-                    "Error occurred in authorization request", e);
-        }
+    @Override
+    protected List<String> getRuleNames() {
+        return SystemRuleManager.getInstance().getLatencyRuleNames();
     }
 
     /**
-     * Delete rule action handler.
+     * {@inheritDoc}
      */
-    private void handleDeleteRule() {
-        final DataDeliveryPermission permission = DataDeliveryPermission.SYSTEM_MANAGEMENT_CREATE;
-        IUser user = UserController.getUserObject();
-        String msg = user.uniqueId() + notAuthorizedMsg + permission;
-
-        try {
-            if (DataDeliveryServices.getPermissionsService()
-                    .checkPermission(user, msg, permission).isAuthorized()) {
-
-                String ruleName = null;
-                int selectedIdx = latencyList.getSelectionIndex();
-                if (selectedIdx > -1) {
-                    int answer = DataDeliveryUtils.showYesNoMessage(
-                            parentComp.getShell(), "Delete?",
-                            "Are you sure you want to delete this rule?");
-                    if (answer == SWT.YES) {
-                        ruleName = latencyList.getItem(selectedIdx);
-                        SystemRuleManager.getInstance().deleteLatencyRule(
-                                ruleName);
-                        loadList();
-                        if (latencyList.getItemCount() == 0) {
-                            enableButtons(false);
-                        }
-                    }
-                } else {
-                    DataDeliveryUtils.showMessage(parentComp.getShell(),
-                            SWT.ERROR, "Select Rule",
-                            "Please select a rule for delete.");
-                    return;
-                }
-            }
-        } catch (VizException e) {
-            statusHandler.handle(Priority.PROBLEM,
-                    "Error occurred in authorization request", e);
-        }
-    }
-
-    /**
-     * Load the rule list.
-     */
-    public void loadList() {
-        latencyList.removeAll();
-
-        // Get the list of latency rule names
-        java.util.List<String> rules = null;
-        rules = SystemRuleManager.getInstance().getLatencyRuleNames();
-
-        Collections.sort(rules, String.CASE_INSENSITIVE_ORDER);
-        latencyList.setItems(rules.toArray(new String[rules.size()]));
+    @Override
+    protected void deleteRule(String ruleName) {
+        SystemRuleManager.getInstance().deleteLatencyRule(ruleName);
     }
 }
