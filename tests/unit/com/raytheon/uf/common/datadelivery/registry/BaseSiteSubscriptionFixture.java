b6b01c3e811e6aa0a73357076ce4c823d4e3e578
Mike Duff <michael.duff@raytheon.com>
2013-05-20 13:15:58 -0500

    Issue #2020 - Implement Shared Subscriptions Cave-side.
    Change-Id: I96e156043ec34e7ec98c2d8c3aafb6916c31e7cf
    
    Former-commit-id: 1e00c7f935cf22ac5064060069164af6044f2150

--- tests/unit/com/raytheon/uf/common/datadelivery/registry/BaseSiteSubscriptionFixture.java
+++ tests/unit/com/raytheon/uf/common/datadelivery/registry/BaseSiteSubscriptionFixture.java
@@ -1,30 +1,30 @@
 /**
  * This software was developed and / or modified by Raytheon Company,
  * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
- *
+ * 
  * U.S. EXPORT CONTROLLED TECHNICAL DATA
  * This software product contains export-restricted data whose
  * export/transfer/disclosure is restricted by U.S. law. Dissemination
  * to non-U.S. persons whether in the United States or abroad requires
  * an export license or other authorization.
- *
+ * 
  * Contractor Name:        Raytheon Company
  * Contractor Address:     6825 Pine Street, Suite 340
  *                         Mail Stop B8
  *                         Omaha, NE 68106
  *                         402.291.0100
- *
+ * 
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.common.datadelivery.registry.handlers;
+package com.raytheon.uf.common.datadelivery.registry;
 
-import com.raytheon.uf.common.datadelivery.registry.InitialPendingUserSubscription;
-import com.raytheon.uf.common.registry.handler.IRegistryObjectHandler;
+import java.util.Random;
+
+import com.raytheon.uf.common.registry.ebxml.RegistryUtil;
 
 /**
- * The {@link IRegistryObjectHandler} interface for
- * {@link InitialPendingUserSubscription}.
+ * Adds attributes specific to {@link SiteSubscription} types.
  * 
  * <pre>
  * 
@@ -32,15 +32,33 @@ import com.raytheon.uf.common.registry.handler.IRegistryObjectHandler;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Sep 18, 2012 1169       djohnson     Initial creation
- * Sep 24, 2012 1157       mpduff       Change to use InitialPendingUserSubscription.
- * Sep 28, 2012 1187       djohnson     Extend {@link IBaseSubscriptionHandler}.
+ * Mar 29, 2013 1841       djohnson     Initial creation
  * 
  * </pre>
  * 
  * @author djohnson
  * @version 1.0
  */
-public interface IPendingUserSubscriptionHandler extends
-        IBasePendingSubscriptionHandler<InitialPendingUserSubscription> {
+
+public abstract class BaseSiteSubscriptionFixture<T extends SiteSubscription>
+        extends BaseSubscriptionFixture<T> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public T getInstance(long seedValue, Random random) {
+        T subscription = super.getInstance(seedValue, random);
+
+        subscription.setOwner("owner" + random.nextInt());
+        subscription.setId(RegistryUtil.getRegistryObjectKey(subscription));
+
+        return subscription;
+    }
+
+    /**
+     * @return
+     */
+    @Override
+    protected abstract T getSubscription();
 }
