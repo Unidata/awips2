5b21a6cfa4e60e87a7ac13b08695f1b3077b4000
Mike Duff <michael.duff@raytheon.com>
2013-05-20 13:15:58 -0500

    Issue #2020 - Implement Shared Subscriptions Cave-side.
    Change-Id: I96e156043ec34e7ec98c2d8c3aafb6916c31e7cf
    
    Former-commit-id: 80b1f95f0c0b6d8ff052012a6fed5d4fc95ac29d [formerly 192ce891d00bf62cb8791636ff4b1cc0bbc980fd] [formerly 80b1f95f0c0b6d8ff052012a6fed5d4fc95ac29d [formerly 192ce891d00bf62cb8791636ff4b1cc0bbc980fd] [formerly b6b01c3e811e6aa0a73357076ce4c823d4e3e578 [formerly 1e00c7f935cf22ac5064060069164af6044f2150]]]
    Former-commit-id: b6b01c3e811e6aa0a73357076ce4c823d4e3e578
    Former-commit-id: 6ee0ef55ff9d6fb60aa99d7b599af21f3b5430ca [formerly a9004667b75e2ebcd6ae974e598c12d0ab37b1e4]
    Former-commit-id: 53ca0058369c5b99d046d19d397ca40585c15cff

--- edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/ebxml/slots/SetSlotConverter.java
+++ edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/ebxml/slots/SetSlotConverter.java
@@ -10,26 +10,26 @@ import oasis.names.tc.ebxml.regrep.xsd.rim.v4.StringValueType;
 import oasis.names.tc.ebxml.regrep.xsd.rim.v4.ValueType;
 
 /**
- * A <code>SlotConverter</code> implementation for converting the
- * members of a Set into a RegistryObject slot type. A collection
- * slot type will be created and the Set members converted to elements
- * with slot values of a appropriate type for the member. The collection
- * type for the slot will be set to Set.
+ * A <code>SlotConverter</code> implementation for converting the members of a
+ * Set into a RegistryObject slot type. A collection slot type will be created
+ * and the Set members converted to elements with slot values of a appropriate
+ * type for the member. The collection type for the slot will be set to Set.
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * May 15, 2012 455        jspinks     Initial creation
- *
+ * May 15, 2013 1040       mpduff      Made non-abstract.
+ * 
  * </pre>
- *
+ * 
  * @author jspinks
  * @version 1.0
  */
-public abstract class SetSlotConverter implements SlotConverter {
+public class SetSlotConverter implements SlotConverter {
 
     /**
      * Extract a List of SlotType Objects from the given slotValue Object.
@@ -44,65 +44,67 @@ public abstract class SetSlotConverter implements SlotConverter {
      *         provided.
      * 
      * @throws IllegalArgumentException
-     *             If the slotValue Object provided is not an instance of Set, or
-     *             the Set member cannot be converted to a value.
+     *             If the slotValue Object provided is not an instance of Set,
+     *             or the Set member cannot be converted to a value.
      * 
      * @see java.util.Map
      */
     @Override
-    public List<SlotType> getSlots(String slotName, Object slotValue) throws IllegalArgumentException {
+    public List<SlotType> getSlots(String slotName, Object slotValue)
+            throws IllegalArgumentException {
         List<SlotType> slots = new ArrayList<SlotType>();
-        
-        
+
         SlotType slot = new SlotType();
         slot.setName(slotName);
         List<ValueType> collectionValues = new ArrayList<ValueType>();
         CollectionValueType cvt = new CollectionValueType();
-        
-        
+
         if (slotValue instanceof Set) {
-            
-            for (Object o : (Set<?>)slotValue) {
+
+            for (Object o : (Set<?>) slotValue) {
                 ValueType vt = newEntrySlot(o);
                 if (vt != null) {
-                   collectionValues.add(newEntrySlot(o));
+                    collectionValues.add(newEntrySlot(o));
                 } else {
-                    throw new IllegalArgumentException("Set with parameterized type " + o.getClass().getName() +
-                            " cannot be converted by " + SetSlotConverter.class.getName());
+                    throw new IllegalArgumentException(
+                            "Set with parameterized type "
+                                    + o.getClass().getName()
+                                    + " cannot be converted by "
+                                    + SetSlotConverter.class.getName());
                 }
             }
 
-                
+        } else {
+            throw new IllegalArgumentException("Object of type "
+                    + slotValue.getClass().getName()
+                    + " cannot be converted by "
+                    + SetSlotConverter.class.getName());
         }
-        else {
-           throw new IllegalArgumentException("Object of type " + slotValue.getClass().getName() +
-                   " cannot be converted by " + SetSlotConverter.class.getName());
-        }
-        
+
         cvt.setCollectionValue(collectionValues);
         cvt.setCollectionType("urn:oasis:names:tc:ebxml-regrep:CollectionType:Set");
         slot.setSlotValue(cvt);
         slots.add(slot);
         return slots;
     }
-    
+
     /**
      * Create a new value for inclusion in the collection slot values.
      * 
      * @param value
-     *        The Object to extract the slot value from. 
-     *        
+     *            The Object to extract the slot value from.
+     * 
      * @return A ValueType to add to the collection slot.
      */
     private ValueType newEntrySlot(Object value) {
-        
+
         // TODO: Add more types as necessary.
         if (value instanceof String) {
             StringValueType entry = new StringValueType();
-            entry.setStringValue((String)value);
+            entry.setStringValue((String) value);
             return entry;
         }
-        
+
         return null;
     }
 }
