cc2e4f8295a59a9a9911ece848d69fdb70e0c200
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-05-23 18:42:11 -0500

    Issue #2005 stop cave from starting if spring fails, prompt user if they want to restart
    Change-Id: I6b77e06c5737dab04c6c1a89cc2f5242a784bc53
    
    Former-commit-id: 29b3e508e29cddc591c754f772a4e3866455d31f [formerly 631567ced9029df30f0c316c04a51fda5dfc60b1] [formerly 7d1fc954f6f73637dc53c4b492638ef6e43298ff] [formerly 29b3e508e29cddc591c754f772a4e3866455d31f [formerly 631567ced9029df30f0c316c04a51fda5dfc60b1] [formerly 7d1fc954f6f73637dc53c4b492638ef6e43298ff] [formerly 7d233ff5906abe59c6d9495f57d9ea6a9ffd6816 [formerly 7d1fc954f6f73637dc53c4b492638ef6e43298ff [formerly ef313253ec669167b9e1deb2562170c117123937]]]]
    Former-commit-id: 7d233ff5906abe59c6d9495f57d9ea6a9ffd6816
    Former-commit-id: 7fa0ba30692b7a91b6813394001b4cd0a79a405f [formerly e976a4c94e9bfbcdc5a6cbe737d674e316bf9652] [formerly 6b94d7fe2342db507b4bed04b7cd4e98b8e66316 [formerly 9e37d21dc619e9af10e04afb27ab4f7973b71461]]
    Former-commit-id: f01b8647daca1eedd6ca3dbbfa015814e459bd93 [formerly 543e0c77461973c7b8d0efc508784d23bf18af1b]
    Former-commit-id: afcf650681ab16ee498dab16b877e3fffcd54690

--- cave/com.raytheon.uf.viz.spring.dm/src/com/raytheon/uf/viz/spring/dm/Activator.java
+++ cave/com.raytheon.uf.viz.spring.dm/src/com/raytheon/uf/viz/spring/dm/Activator.java
@@ -9,6 +9,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
@@ -34,6 +35,7 @@ import org.osgi.framework.Constants;
  * Jan 24, 2013 1522       bkowal       Halt initialization if a p2 installation 
  *                                      has been started
  * Mar 05, 2013 1754       djohnson     Catch exceptions and allow as much of the Spring container to boot as possible.
+ * May 23, 2013 2005       njensen      Added springSuccess flag
  * 
  * </pre>
  * 
@@ -42,102 +44,110 @@ import org.osgi.framework.Constants;
  */
 public class Activator implements BundleActivator {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "com.raytheon.uf.viz.spring.dm";
+    // The plug-in ID
+    public static final String PLUGIN_ID = "com.raytheon.uf.viz.spring.dm";
 
-	private static final String SPRING_PATH = "res" + IPath.SEPARATOR
-			+ "spring";
+    private static final String SPRING_PATH = "res" + IPath.SEPARATOR
+            + "spring";
 
-	private static final String SPRING_FILE_EXT = "*.xml";
+    private static final String SPRING_FILE_EXT = "*.xml";
 
-	// The shared instance
-	private static Activator plugin;
+    private static final Pattern COMMA_SPLIT = Pattern.compile("[,]");
 
-	/**
-	 * The constructor
-	 */
-	public Activator() {
-	}
+    private static final Pattern SEMICOLON_SPLIT = Pattern.compile("[;]");
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
-	 */
-	@Override
+    // The shared instance
+    private static Activator plugin;
+
+    private boolean springSuccess = true;
+
+    /**
+     * The constructor
+     */
+    public Activator() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+     */
+    @Override
     public void start(BundleContext context) throws Exception {
-		if (this.isInstallOperation()) {
-			return;
-		}
-		plugin = this;
+        if (this.isInstallOperation()) {
+            return;
+        }
+        plugin = this;
 
-		Map<String, OSGIXmlApplicationContext> contextMap = new HashMap<String, OSGIXmlApplicationContext>();
-		Set<String> processing = new HashSet<String>();
-		Bundle[] bundles = context.getBundles();
-		Map<String, Bundle> bundleMap = new HashMap<String, Bundle>();
-		for (Bundle b : bundles) {
-			bundleMap.put(b.getSymbolicName(), b);
-		}
-		for (Bundle b : bundles) {
-			createContext(bundleMap, contextMap, b, processing);
-		}
-	}
+        Map<String, OSGIXmlApplicationContext> contextMap = new HashMap<String, OSGIXmlApplicationContext>();
+        Set<String> processing = new HashSet<String>();
+        Bundle[] bundles = context.getBundles();
+        Map<String, Bundle> bundleMap = new HashMap<String, Bundle>();
+        for (Bundle b : bundles) {
+            bundleMap.put(b.getSymbolicName(), b);
+        }
+        for (Bundle b : bundles) {
+            createContext(bundleMap, contextMap, b, processing);
+        }
+    }
 
-	private OSGIXmlApplicationContext createContext(
-			Map<String, Bundle> bundles,
-			Map<String, OSGIXmlApplicationContext> contextMap, Bundle bundle,
-			Set<String> processing) {
-		String bundleName = bundle.getSymbolicName();
-		OSGIXmlApplicationContext appCtx = contextMap.get(bundleName);
-		if (contextMap.containsKey(bundleName) == false
-				&& bundleName.contains(".edex.") == false) {
-			if (processing.contains(bundleName)) {
-				throw new RuntimeException(
-						"Found recursive spring dependency while processing plugins: "
-								+ bundleName);
-			}
-			processing.add(bundleName);
+    private OSGIXmlApplicationContext createContext(
+            Map<String, Bundle> bundles,
+            Map<String, OSGIXmlApplicationContext> contextMap, Bundle bundle,
+            Set<String> processing) {
+        String bundleName = bundle.getSymbolicName();
+        OSGIXmlApplicationContext appCtx = contextMap.get(bundleName);
+        if (contextMap.containsKey(bundleName) == false
+                && bundleName.contains(".edex.") == false) {
+            if (processing.contains(bundleName)) {
+                springSuccess = false;
+                throw new RuntimeException(
+                        "Found recursive spring dependency while processing plugins: "
+                                + bundleName);
+            }
+            processing.add(bundleName);
 
-			// No context created yet and not edex project, check for files
-			Enumeration<?> entries = bundle.findEntries(SPRING_PATH,
-					SPRING_FILE_EXT, true);
-			if (entries != null) {
-				List<String> files = new ArrayList<String>();
-				while (entries.hasMoreElements()) {
-					URL url = (URL) entries.nextElement();
-					try {
-						url = FileLocator.toFileURL(url);
-						files.add(url.toString());
-					} catch (IOException e) {
-						throw new RuntimeException(
-								"Error resolving spring file: " + url, e);
-					}
-				}
-				if (files.size() > 0) {
-					// Files found, check for dependencies
-					String requiredBundlesHeader = (String) bundle.getHeaders()
-							.get(Constants.REQUIRE_BUNDLE);
-					// Split comma separated string from MANIFEST
-					String[] requiredBundles = requiredBundlesHeader
-							.split("[,]");
-					List<OSGIXmlApplicationContext> parentContexts = new ArrayList<OSGIXmlApplicationContext>();
-					for (String requiredBndl : requiredBundles) {
-						// Extract bundle name which is first item in
-						// semicolon
-						// split list
-						String[] bndlParts = requiredBndl.split("[;]");
-						Bundle reqBndl = bundles.get(bndlParts[0]);
-						if (reqBndl != null) {
-							// Found bundle, process context for bundle
-							OSGIXmlApplicationContext parent = createContext(
-									bundles, contextMap, reqBndl, processing);
-							if (parent != null) {
-								// Context found, add to list
-								parentContexts.add(parent);
-							}
-						}
-					}
+            // No context created yet and not edex project, check for files
+            Enumeration<?> entries = bundle.findEntries(SPRING_PATH,
+                    SPRING_FILE_EXT, true);
+            if (entries != null) {
+                List<String> files = new ArrayList<String>();
+                while (entries.hasMoreElements()) {
+                    URL url = (URL) entries.nextElement();
+                    try {
+                        url = FileLocator.toFileURL(url);
+                        files.add(url.toString());
+                    } catch (IOException e) {
+                        throw new RuntimeException(
+                                "Error resolving spring file: " + url, e);
+                    }
+                }
+                if (files.size() > 0) {
+                    // Files found, check for dependencies
+                    String requiredBundlesHeader = (String) bundle.getHeaders()
+                            .get(Constants.REQUIRE_BUNDLE);
+                    // Split comma separated string from MANIFEST
+                    String[] requiredBundles = COMMA_SPLIT
+                            .split(requiredBundlesHeader);
+                    List<OSGIXmlApplicationContext> parentContexts = new ArrayList<OSGIXmlApplicationContext>();
+                    for (String requiredBndl : requiredBundles) {
+                        // Extract bundle name which is first item in
+                        // semicolon
+                        // split list
+                        String[] bndlParts = SEMICOLON_SPLIT
+                                .split(requiredBndl);
+                        Bundle reqBndl = bundles.get(bndlParts[0]);
+                        if (reqBndl != null) {
+                            // Found bundle, process context for bundle
+                            OSGIXmlApplicationContext parent = createContext(
+                                    bundles, contextMap, reqBndl, processing);
+                            if (parent != null) {
+                                // Context found, add to list
+                                parentContexts.add(parent);
+                            }
+                        }
+                    }
 
                     try {
                         if (parentContexts.size() > 0) {
@@ -159,58 +169,63 @@ public class Activator implements BundleActivator {
                         System.err
                                 .println("Errors booting the Spring container.  CAVE will not be fully functional.");
                         t.printStackTrace();
+                        springSuccess = false;
                     }
-				}
-			}
-			contextMap.put(bundleName, appCtx);
-		}
-		processing.remove(bundleName);
-		return appCtx;
-	}
+                }
+            }
+            contextMap.put(bundleName, appCtx);
+        }
+        processing.remove(bundleName);
+        return appCtx;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
-	 */
-	@Override
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    @Override
     public void stop(BundleContext context) throws Exception {
-		plugin = null;
-	}
+        plugin = null;
+    }
 
-	/**
-	 * Returns the shared instance
-	 * 
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
-	}
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
 
-	/**
-	 * Based on the command line arguments, determine whether or not an Eclipse
-	 * p2 repository will be installed
-	 * 
-	 * @return true if an Eclipse p2 repository is going to be installed, false
-	 *         otherwise
-	 */
-	private boolean isInstallOperation() {
-		final String P2_DIRECTOR = "org.eclipse.equinox.p2.director";
+    /**
+     * Based on the command line arguments, determine whether or not an Eclipse
+     * p2 repository will be installed
+     * 
+     * @return true if an Eclipse p2 repository is going to be installed, false
+     *         otherwise
+     */
+    private boolean isInstallOperation() {
+        final String P2_DIRECTOR = "org.eclipse.equinox.p2.director";
 
-		/**
-		 * We look at the command line arguments instead of the program
-		 * arguments (com.raytheon.uf.viz.application.ProgramArguments) because
-		 * the command line arguments include almost everything that was passed
-		 * as an argument to the Eclipse executable instead of just what CAVE is
-		 * interested in.
-		 */
-		for (String argument : Platform.getCommandLineArgs()) {
-			if (P2_DIRECTOR.equals(argument)) {
-				return Boolean.TRUE;
-			}
-		}
+        /**
+         * We look at the command line arguments instead of the program
+         * arguments (com.raytheon.uf.viz.application.ProgramArguments) because
+         * the command line arguments include almost everything that was passed
+         * as an argument to the Eclipse executable instead of just what CAVE is
+         * interested in.
+         */
+        for (String argument : Platform.getCommandLineArgs()) {
+            if (P2_DIRECTOR.equals(argument)) {
+                return Boolean.TRUE;
+            }
+        }
 
-		return Boolean.FALSE;
-	}
+        return Boolean.FALSE;
+    }
+
+    public boolean isSpringInitSuccessful() {
+        return springSuccess;
+    }
 }
