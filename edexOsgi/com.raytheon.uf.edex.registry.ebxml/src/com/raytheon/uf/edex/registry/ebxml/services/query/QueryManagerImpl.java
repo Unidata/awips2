e16dc02a548ff053466d9b3871e7d9bebfea449a
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-05-24 11:34:08 -0500

    Issue #2036 Implemented federated query capabilities
    Change-Id: I0a729281a1b08267486a9d5fdddd876ededf6e69
    
    Former-commit-id: d5263b2b758383d718509bd6675c7d6335a1e61b [formerly eac19de99604769ad8d9bb4f53c6ffce95903d9a]
    Former-commit-id: 4747f7d31973261fda05336d63b9d0f8fb4f4a38

--- edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/QueryManagerImpl.java
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/QueryManagerImpl.java
@@ -20,6 +20,16 @@
 package com.raytheon.uf.edex.registry.ebxml.services.query;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.annotation.Resource;
 import javax.xml.bind.JAXBException;
@@ -31,18 +41,31 @@ import oasis.names.tc.ebxml.regrep.xsd.query.v4.QueryExceptionType;
 import oasis.names.tc.ebxml.regrep.xsd.query.v4.QueryRequest;
 import oasis.names.tc.ebxml.regrep.xsd.query.v4.QueryResponse;
 import oasis.names.tc.ebxml.regrep.xsd.query.v4.ResponseOptionType;
+import oasis.names.tc.ebxml.regrep.xsd.rim.v4.FederationType;
 import oasis.names.tc.ebxml.regrep.xsd.rim.v4.QueryType;
+import oasis.names.tc.ebxml.regrep.xsd.rim.v4.RegistryObjectType;
+import oasis.names.tc.ebxml.regrep.xsd.rim.v4.RegistryType;
+import oasis.names.tc.ebxml.regrep.xsd.rim.v4.SlotType;
+import oasis.names.tc.ebxml.regrep.xsd.rim.v4.StringValueType;
+import oasis.names.tc.ebxml.regrep.xsd.rs.v4.RegistryExceptionType;
 import oasis.names.tc.ebxml.regrep.xsd.rs.v4.RegistryResponseStatus;
 import oasis.names.tc.ebxml.regrep.xsd.rs.v4.UnsupportedCapabilityExceptionType;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import com.raytheon.uf.common.registry.constants.CanonicalQueryTypes;
 import com.raytheon.uf.common.registry.constants.ErrorSeverity;
+import com.raytheon.uf.common.registry.constants.Format;
+import com.raytheon.uf.common.registry.constants.Languages;
+import com.raytheon.uf.common.registry.constants.QueryReturnTypes;
+import com.raytheon.uf.common.registry.constants.RegistryObjectTypes;
+import com.raytheon.uf.common.registry.services.RegistrySOAPServices;
 import com.raytheon.uf.common.serialization.SerializationUtil;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.time.util.ITimer;
 import com.raytheon.uf.common.time.util.TimeUtil;
+import com.raytheon.uf.edex.registry.ebxml.dao.FederationDao;
 import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
 import com.raytheon.uf.edex.registry.ebxml.services.query.types.IRegistryQuery;
 import com.raytheon.uf.edex.registry.ebxml.util.EbxmlExceptionUtil;
@@ -86,15 +109,36 @@ import com.raytheon.uf.edex.registry.ebxml.util.EbxmlObjectUtil;
 @Transactional
 public class QueryManagerImpl implements QueryManager {
 
-    @Resource
-    private WebServiceContext wsContext;
-
-    private boolean eagerFetch = false;
-
+    /** The logger */
     protected static final IUFStatusHandler statusHandler = UFStatus
             .getHandler(QueryManagerImpl.class);
 
+    @Resource
+    private WebServiceContext wsContext;
+
+    /**
+     * Boolean denoting that the results should be eagerly fetched from the
+     * database
+     */
+    private boolean eagerFetch = false;
+
+    /**
+     * Query type manager for getting query objects based on values provided in
+     * a submitted query
+     */
     private QueryTypeManager queryTypeMgr;
+
+    /**
+     * Data access object for interacting with FederationType objects. Used
+     * during processing of federated queries.
+     */
+    private FederationDao federationDao;
+
+    /**
+     * Executor service used to submit queries to federation members in parallel
+     * during the processing of a federated query
+     */
+    private ExecutorService queryExecutor;
 
     /**
      * ObjectRef - This option specifies that the QueryResponse MUST contain a
@@ -141,7 +185,7 @@ public class QueryManagerImpl implements QueryManager {
      * Creates a new QueryManagerImpl
      */
     public QueryManagerImpl() {
-
+        queryExecutor = Executors.newCachedThreadPool();
     }
 
     /**
@@ -165,14 +209,8 @@ public class QueryManagerImpl implements QueryManager {
         QueryResponse response = EbxmlObjectUtil.queryObjectFactory
                 .createQueryResponse();
         response.setStatus(RegistryResponseStatus.SUCCESS);
-        QueryType queryType = queryRequest.getQuery();
-        boolean federated = queryRequest.isFederated();
-        // TODO: Add support for federated queries
-        @SuppressWarnings("unused")
-        String federation = queryRequest.getFederation();
         String format = queryRequest.getFormat();
         String lang = queryRequest.getLang();
-        queryRequest.isMatchOlderVersions();
 
         if (!format.equals(DEFAULT_RESPONSE_FORMAT)) {
             // TODO: Implement support for different response formats
@@ -185,20 +223,6 @@ public class QueryManagerImpl implements QueryManager {
                                     "This EBXML registry currently does not currently support queries specifying response types other than "
                                             + DEFAULT_RESPONSE_FORMAT,
                                     ErrorSeverity.WARNING, statusHandler));
-            response.setStatus(RegistryResponseStatus.PARTIAL_SUCCESS);
-        }
-
-        if (federated) {
-            // TODO: Add support for federated queries
-            response.getException()
-                    .add(EbxmlExceptionUtil
-                            .createRegistryException(
-                                    UnsupportedCapabilityExceptionType.class,
-                                    "",
-                                    "Federated queries not currently supported",
-                                    "This EBXML registry currently does not currently support federated queries",
-                                    ErrorSeverity.WARNING, statusHandler));
-            response.setStatus(RegistryResponseStatus.PARTIAL_SUCCESS);
         }
 
         if (lang != null) {
@@ -206,17 +230,17 @@ public class QueryManagerImpl implements QueryManager {
             statusHandler.warn("Lang attribute currently not supported.");
         }
 
-        IRegistryQuery query = getQuery(queryType);
-
         try {
-            query.executeQuery(queryRequest, response, client);
-            if (eagerFetch) {
-                try {
-                    SerializationUtil.getJaxbManager().marshalToXml(response);
-                } catch (JAXBException e) {
-                    throw new EbxmlRegistryException(
-                            "Error eagerly fetching items", e);
+            if (queryRequest.isFederated()) {
+                if (federationDao.federationsExist()) {
+                    executeFederatedQuery(queryRequest, response, client);
+                } else {
+                    statusHandler
+                            .warn("Federated query request received but no federations are known to this registry. Executing query locally.");
+                    executeLocalQuery(queryRequest, response, client);
                 }
+            } else {
+                executeLocalQuery(queryRequest, response, client);
             }
         } catch (EbxmlRegistryException e) {
             throw EbxmlExceptionUtil.createMsgRegistryException(
@@ -232,6 +256,283 @@ public class QueryManagerImpl implements QueryManager {
         response.setRequestId(queryRequestId);
         return response;
 
+    }
+
+    /**
+     * Executes a query on the local registry
+     * 
+     * @param queryRequest
+     *            The query request object
+     * @param queryResponse
+     *            The query response object
+     * @param client
+     *            The address of the client that made the request
+     * @throws MsgRegistryException
+     *             If errors occur when resolving the query type
+     * @throws EbxmlRegistryException
+     *             If errors occur while executing the actual query
+     */
+    private void executeLocalQuery(QueryRequest queryRequest,
+            QueryResponse queryResponse, String client)
+            throws MsgRegistryException, EbxmlRegistryException {
+        IRegistryQuery query = getQuery(queryRequest.getQuery());
+        query.executeQuery(queryRequest, queryResponse, client);
+        if (eagerFetch) {
+            try {
+                SerializationUtil.getJaxbManager().marshalToXml(queryResponse);
+            } catch (JAXBException e) {
+                throw new EbxmlRegistryException(
+                        "Error eagerly fetching items", e);
+            }
+        }
+    }
+
+    /**
+     * Executes a federated query
+     * 
+     * @param queryRequest
+     *            The query request object
+     * @param queryResponse
+     *            The query response object
+     * @param client
+     *            The address of the client that made the request
+     * @throws MsgRegistryException
+     *             If errors occur when resolving the query type
+     * @throws EbxmlRegistryException
+     *             If errors occur while executing the actual query
+     */
+    private void executeFederatedQuery(QueryRequest queryRequest,
+            QueryResponse queryResponse, String client)
+            throws EbxmlRegistryException, MsgRegistryException {
+
+        /*
+         * If somehow, we get into this method with a non-federated query,
+         * execute locally
+         */
+        if (!queryRequest.isFederated()) {
+            statusHandler
+                    .warn("Non-federated query passed to federated query executor. Executing as a local query");
+            executeLocalQuery(queryRequest, queryResponse, client);
+            return;
+        }
+
+        /*
+         * Get the set of registries to query
+         */
+        statusHandler.info("Getting list of registries for federated query...");
+        Set<RegistryType> registriesToQuery = new HashSet<RegistryType>();
+        String federationToQuery = queryRequest.getFederation();
+        if (federationToQuery == null || federationToQuery.isEmpty()) {
+            List<FederationType> federations = federationDao.getAll();
+            for (FederationType federation : federations) {
+                registriesToQuery.addAll(getMembersOfFederation(federation
+                        .getId()));
+            }
+        } else {
+            registriesToQuery.addAll(getMembersOfFederation(federationToQuery));
+        }
+
+        /*
+         * If no registries found to query, execute as local query
+         */
+        if (registriesToQuery.isEmpty()) {
+            statusHandler
+                    .warn("Federated query found no registries to query! Executing as local query.");
+            executeLocalQuery(queryRequest, queryResponse, client);
+        } else {
+            statusHandler.info("Submitting federated query to "
+                    + registriesToQuery.size() + " remote registries.");
+
+            /*
+             * Create a list of queries to execute concurrently
+             */
+            List<RemoteRegistryQuery> queryRunners = new ArrayList<RemoteRegistryQuery>();
+            for (RegistryType registryToQuery : registriesToQuery) {
+                queryRunners.add(new RemoteRegistryQuery(registryToQuery,
+                        queryRequest));
+            }
+
+            try {
+                List<Future<QueryResponse>> responses = queryExecutor
+                        .invokeAll(queryRunners);
+
+                for (Future<QueryResponse> response : responses) {
+                    if (response.isCancelled()) {
+                        statusHandler
+                                .error("Federated query invocation was cancelled!");
+                    } else {
+                        try {
+                            consolidateQueryResponse(queryResponse,
+                                    response.get());
+                        } catch (CancellationException e) {
+                            statusHandler.error(
+                                    "Federated query invocation was cancelled",
+                                    e);
+                            queryResponse.getException().add(getException(e));
+                        } catch (ExecutionException e) {
+                            statusHandler
+                                    .error("Federated query invocation encountered an erroring execution!",
+                                            e);
+                            queryResponse.getException().add(getException(e));
+                        } catch (InterruptedException e) {
+                            statusHandler
+                                    .error("Federated query invocation was interrupted",
+                                            e);
+                            queryResponse.getException().add(getException(e));
+                        }
+                    }
+                }
+                if (queryResponse.getException().isEmpty()) {
+                    queryResponse.setStatus(RegistryResponseStatus.SUCCESS);
+                } else if (queryResponse.getException().size() == responses
+                        .size()) {
+                    throw new EbxmlRegistryException(
+                            "All "
+                                    + responses.size()
+                                    + " query requests generated by the federated query failed!");
+                } else {
+                    statusHandler
+                            .warn("Federated query was partial successful. "
+                                    + queryResponse.getException().size()
+                                    + " of " + responses.size() + " failed.");
+                    queryResponse
+                            .setStatus(RegistryResponseStatus.PARTIAL_SUCCESS);
+                }
+            } catch (InterruptedException e) {
+                throw new EbxmlRegistryException(
+                        "Error executing federated query!", e);
+            }
+        }
+    }
+
+    /**
+     * Convenience method to wrap an exception in a RegistryExceptionType
+     * 
+     * @param e
+     *            The exception to wrap
+     * @return The RegistryExceptionType wrapper
+     */
+    private RegistryExceptionType getException(Exception e) {
+        return EbxmlExceptionUtil.createRegistryException(
+                QueryExceptionType.class, "", e.getLocalizedMessage(),
+                e.getLocalizedMessage(), ErrorSeverity.ERROR, e, statusHandler);
+    }
+
+    /**
+     * Convenience method for consolidating two query responses together. Used
+     * for generating a single query response from a federated query that
+     * queries many registries
+     * 
+     * @param response1
+     *            The response to append to
+     * @param response2
+     *            The response that will be appended to response1
+     */
+    private void consolidateQueryResponse(QueryResponse response1,
+            QueryResponse response2) {
+        response1.addRegistryObjects(response2.getRegistryObjects());
+        response1.addObjectRefs(response2.getObjectRefs());
+        response1.getException().addAll(response2.getException());
+        response1.incrementResultCount(response2.getTotalResultCount());
+
+    }
+
+    /**
+     * Gets the RegistryType members of the given federation. If a federation is
+     * found to be a member of the given federation, this method is called
+     * recursively to get the set of actual registries.
+     * 
+     * @param federationId
+     *            The id of the federation to get the members for
+     * @return The RegistryType members of the federation
+     * @throws MsgRegistryException
+     *             If errors occur while querying for federation members.
+     */
+    private Set<RegistryType> getMembersOfFederation(String federationId)
+            throws MsgRegistryException {
+        Set<RegistryType> retVal = new HashSet<RegistryType>();
+        statusHandler.info("Getting federation members for " + federationId
+                + "...");
+        SlotType associationTypeSlot = new SlotType(
+                QueryConstants.ASSOCIATION_TYPE, new StringValueType(
+                        RegistryObjectTypes.ASSOCIATION_PATH));
+        SlotType sourceObjectIdSlot = new SlotType(
+                QueryConstants.SOURCE_OBJECT_ID, new StringValueType(
+                        federationId));
+        QueryType query = new QueryType(
+                CanonicalQueryTypes.FIND_ASSOCIATED_OBJECTS,
+                associationTypeSlot, sourceObjectIdSlot);
+        QueryRequest queryRequest = new QueryRequest(
+                "Query for federation members",
+                "Query to get the members of the federation",
+                new ResponseOptionType(QueryReturnTypes.REGISTRY_OBJECT, true),
+                query, false, null, Format.EBRIM, Languages.EN_US, 0, 0, 0,
+                false);
+        QueryResponse response = executeQuery(queryRequest);
+        if (response.getRegistryObjectList() != null) {
+            List<RegistryObjectType> responseObjects = response
+                    .getRegistryObjectList().getRegistryObject();
+            for (RegistryObjectType regObj : responseObjects) {
+                if (regObj instanceof RegistryType) {
+                    retVal.add((RegistryType) regObj);
+                } else if (regObj instanceof FederationType) {
+                    retVal.addAll(getMembersOfFederation(regObj.getId()));
+                }
+            }
+        }
+        return retVal;
+    }
+
+    /**
+     * 
+     * Callable class to run a query on a remote registry
+     * 
+     * <pre>
+     * 
+     * SOFTWARE HISTORY
+     * 
+     * Date         Ticket#     Engineer    Description
+     * ------------ ----------  ----------- --------------------------
+     * 5/24/2013    2036        bphillip    Initial implementation
+     * </pre>
+     * 
+     * @author bphillip
+     * @version 1
+     */
+    private class RemoteRegistryQuery implements Callable<QueryResponse> {
+
+        /** The registry to query */
+        private RegistryType registryToQuery;
+
+        /** The request to submit to the remote registry */
+        private QueryRequest queryRequest;
+
+        /**
+         * Creates a new RemoteRegistryQuery.
+         * 
+         * @param registryToQuery
+         *            The registry to query
+         * @param queryRequest
+         *            The request to submit to the remote registry
+         */
+        public RemoteRegistryQuery(RegistryType registryToQuery,
+                QueryRequest queryRequest) {
+            this.registryToQuery = registryToQuery;
+            this.queryRequest = queryRequest;
+            this.queryRequest.setFederated(false);
+            this.queryRequest.setFederation(null);
+
+        }
+
+        @Override
+        public QueryResponse call() throws Exception {
+            statusHandler.info("Submitting federated query to ["
+                    + registryToQuery.getId() + "] at ["
+                    + registryToQuery.getBaseURL() + "]...");
+            return RegistrySOAPServices.getQueryServiceForHost(
+                    registryToQuery.getBaseURL()).executeQuery(queryRequest);
+
+        }
     }
 
     /**
@@ -316,6 +617,10 @@ public class QueryManagerImpl implements QueryManager {
 
     public void setEagerFetch(boolean eagerFetch) {
         this.eagerFetch = eagerFetch;
+    }
+
+    public void setFederationDao(FederationDao federationDao) {
+        this.federationDao = federationDao;
     }
 
 }
