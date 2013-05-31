6234417095af592e2c2815a8d85b174416d7f5d3
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-05-31 11:25:44 -0500

    Issue #2038 Change datadelivery retrieval to be plugin contributable
    Amend: Fix class name resolution, no arguments to metadata adapter constructors
    
    Change-Id: I2c08563499a34996502d22f0f9685602b8897139
    
    Former-commit-id: 55aeff34220e7fdb32aab3514cef205571c3403f [formerly fc6effe4993694944d5331038d24310dec44a5d8]
    Former-commit-id: 4a8adeb80ac3bc013f3338c44d8592eebec0deb3

--- edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/metadata/adapters/AbstractMetadataAdapter.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/metadata/adapters/AbstractMetadataAdapter.java
@@ -22,11 +22,15 @@ package com.raytheon.uf.edex.datadelivery.retrieval.metadata.adapters;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.Validate;
+
 import com.raytheon.uf.common.datadelivery.retrieval.xml.RetrievalAttribute;
 import com.raytheon.uf.common.dataplugin.PluginDataObject;
-import com.raytheon.uf.common.dataplugin.grid.GridRecord;
-import com.raytheon.uf.common.dataplugin.madis.MadisRecord;
 import com.raytheon.uf.common.serialization.ISerializableObject;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.util.registry.GenericRegistry;
+import com.raytheon.uf.common.util.registry.RegistryException;
 
 /**
  * 
@@ -40,48 +44,99 @@ import com.raytheon.uf.common.serialization.ISerializableObject;
  * ------------ ---------- ----------- --------------------------
  * Nov 19, 2012            bsteffen     Initial javadoc
  * May 12, 2013  753       dhladky      Added support for Madis
+ * May 31, 2013 2038       djohnson     Plugin contributable registry.
  * 
  * </pre>
  * 
  * @author unknown
  * @version 1.0
  */
-public abstract class AbstractMetadataAdapter<RecordKey> implements ISerializableObject {
+public abstract class AbstractMetadataAdapter<RecordKey> implements
+        ISerializableObject {
+
+    private static final IUFStatusHandler statusHandler = UFStatus
+            .getHandler(AbstractMetadataAdapter.class);
 
     protected PluginDataObject[] pdos;
-    
+
     protected RetrievalAttribute attXML;
 
     protected static Map<String, String[]> parameterMap = new HashMap<String, String[]>();
 
+    protected static GenericRegistry<String, Class<AbstractMetadataAdapter<?>>> metadataAdapterRegistry = new GenericRegistry<String, Class<AbstractMetadataAdapter<?>>>() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public Object register(String t, Class<AbstractMetadataAdapter<?>> s)
+                throws RegistryException {
+
+            Validate.notNull(t);
+            Validate.notNull(s);
+
+            statusHandler.info("Registered metadata adapter [" + s.getName()
+                    + "] for class [" + t + "]");
+
+            return super.register(t, s);
+        }
+
+    };
+
     public static AbstractMetadataAdapter<?> getMetadataAdapter(Class<?> clazz,
             RetrievalAttribute attXML) throws InstantiationException {
 
-        AbstractMetadataAdapter<?> adapter = null;
-        
-        if (clazz == GridRecord.class) {
-            adapter = new GridMetadataAdapter(attXML);
-        } else if (clazz == MadisRecord.class) {
-            adapter = new MadisMetadataAdapter(attXML);
-        } else {
-            throw new IllegalArgumentException("Did not receive a class recognzed for retreival.");
+        final String className = clazz.getName();
+        Class<AbstractMetadataAdapter<?>> adapterClass = metadataAdapterRegistry
+                .getRegisteredObject(className);
+
+        if (adapterClass == null) {
+            throw new IllegalArgumentException(
+                    "There is no metadata adapter registered for class ["
+                            + className + "]");
         }
 
-        return adapter;
+        try {
+            // Must return a new instance every time, because none of the
+            // metadata adapters are thread safe
+            final AbstractMetadataAdapter<?> adapter = adapterClass
+                    .newInstance();
+            adapter.processAttributeXml(attXML);
+            return adapter;
+        } catch (IllegalAccessException e) {
+            final InstantiationException instantiationException = new InstantiationException();
+            instantiationException.initCause(e);
+            throw instantiationException;
+        }
     }
+
+    /**
+     * @param attXML
+     * 
+     * @throws InstantiationException
+     */
+    public abstract void processAttributeXml(RetrievalAttribute attXML)
+            throws InstantiationException;
 
     // setup an individual record from the direct plugin translation
     public abstract PluginDataObject getRecord(RecordKey o);
-    
+
     // set the size of the PDO array to return
-    public abstract void setPdos(int size);
-    
+    public abstract void allocatePdoArray(int size);
+
     /**
      * get the PDO list
+     * 
      * @return
      */
     public PluginDataObject[] getPdos() {
         return pdos;
     }
-  
+
+    /**
+     * @return the metadataAdapterRegistry
+     */
+    public static GenericRegistry<String, Class<AbstractMetadataAdapter<?>>> getMetadataAdapterRegistry() {
+        return metadataAdapterRegistry;
+    }
 }
