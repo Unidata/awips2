c2fedb3d23c691f6ecc364973a0aacb7f2c5ef93
Dave Hladky <david_j_hladky@raytheon.com>
2013-06-03 16:09:24 -0500

    Issue #2101 Subscription retrieval for WFS (MADIS) follow on of #1763, #2062 for 13.6.1
    Change-Id: I5942d35c77eaa0232147581381e1ff96c690cc03
    
    Former-commit-id: 5512104d861c09c5d8d2729e21bcc1e33add25cc [formerly 292a7a49dc70712565708250daebfa9735f4ccdd [formerly 776bbbba21084fc356e096c6a4418bdac319a81b] [formerly 5512104d861c09c5d8d2729e21bcc1e33add25cc [formerly 00fcd53e94c1ff71d65c42b5f7644f653a1fd179]]]
    Former-commit-id: 292a7a49dc70712565708250daebfa9735f4ccdd [formerly 776bbbba21084fc356e096c6a4418bdac319a81b]
    Former-commit-id: 292a7a49dc70712565708250daebfa9735f4ccdd
    Former-commit-id: 83c17aa05ea9ebca56a523333f291345347a475b

--- edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/util/WfsConnectionUtil.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/util/WfsConnectionUtil.java
@@ -1,6 +1,13 @@
 package com.raytheon.uf.edex.datadelivery.retrieval.util;
 
+import java.net.URI;
+
+import org.apache.http.client.methods.HttpGet;
+
 import com.raytheon.uf.common.comm.HttpClient;
+import com.raytheon.uf.common.comm.HttpClient.HttpClientResponse;
+import com.raytheon.uf.common.datadelivery.registry.Connection;
+import com.raytheon.uf.common.datadelivery.registry.Provider;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
@@ -16,6 +23,7 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * May 12, 2013 753        dhladky     created.
+ * May 31, 2013 1763       dhladky     refined.
  * 
  * </pre>
  * 
@@ -23,24 +31,41 @@ import com.raytheon.uf.common.status.UFStatus.Priority;
  * @version 1.0
  */
 
-
 public class WfsConnectionUtil {
-    
+
     private static final IUFStatusHandler statusHandler = UFStatus
-    .getHandler(WfsConnectionUtil.class);
-    
-    public static String wfsConnect(String address, String message) {
+            .getHandler(WfsConnectionUtil.class);
+
+    public static String wfsConnect(String url, Provider provider) {
         String xmlResponse = null;
-        // sets up any proxy info that might be necessary
-        ConnectionUtil.getProxyParameters();
-        HttpClient http = HttpClient.getInstance();
-        
+        HttpClient http = null;
+
         try {
-            xmlResponse = http.post(address, message);
+            // Sets up any proxy info that might be necessary
+            // TODO: consider using HTTP POST instead of GET
+            ConnectionUtil.getProxyParameters();
+            http = HttpClient.getInstance();
+            HttpGet get = new HttpGet();
+            URI uri = new URI(url);
+            Connection conn = provider.getConnection();
+            // check for the need to do a username password auth check
+            if (conn != null && conn.getUserName() != null
+                    && conn.getPassword() != null) {
+
+                http.setCredentials(uri.getHost(), uri.getPort(),
+                        provider.getName(), conn.getUserName(),
+                        conn.getPassword());
+            }
+
+            get.setURI(uri);
+            HttpClientResponse response = http.executeRequest(get);
+            xmlResponse = new String(response.data);
+
         } catch (Exception e) {
-            statusHandler.handle(Priority.PROBLEM, "Couldn't connect to WFS server: "+address+" with posted message of: "+message, e);
+            statusHandler.handle(Priority.PROBLEM,
+                    "Couldn't connect to WFS server: " + url, e);
         }
-        
+
         return xmlResponse;
     }
 
