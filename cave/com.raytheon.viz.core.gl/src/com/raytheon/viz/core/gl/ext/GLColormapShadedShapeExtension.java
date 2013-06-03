b28f451d17918a1c0b3678cb71d6e1cd32c45e33
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-06-03 17:19:18 -0500

    Issue #1638 Made gl extensions more usable by other gl projects and topo resource more extendable.
    Change-Id: Ia0fe27113f7d3e1ce86bcce231812b2e0bbe0f75
    
    Former-commit-id: 7f76bd3d984d9206980e50c1f51e0bbfbaf07bf5 [formerly 483247738be16ae76bc2f9891f557960e7ade60e]
    Former-commit-id: 3379b100cb5bdeeb97a82d296d37674799f7ff2a

--- cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/ext/GLColormapShadedShapeExtension.java
+++ cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/ext/GLColormapShadedShapeExtension.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.viz.core.gl.internal.ext;
+package com.raytheon.viz.core.gl.ext;
 
 import java.util.ArrayList;
 import java.util.List;
