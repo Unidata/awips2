98b288f275e9bb57f0b9e3c3a362fbcc3a10ce38
David Gillingham <David.Gillingham@raytheon.com>
2013-06-03 16:17:04 -0500

    Issue #2023: Pure python client implementation of Data Access Framework.
    Change-Id: I801b88192aafb4b139b5a1b397b33620c0b9f427
    
    Former-commit-id: 4bdf7178bdec3d15667fe9296e5c78c2dd9ceb16 [formerly ae3e7a64b01957a7c8d9c533004f8c39d32d1e83 [formerly edc77397fb74f07c66dd9f7b9ede5e8af81b4b18]]
    Former-commit-id: ae3e7a64b01957a7c8d9c533004f8c39d32d1e83
    Former-commit-id: 2c0af328b61bd6c54f11e0ff70991494ab3249de

--- pythonPackages/dynamicserialize/dstypes/com/vividsolutions/jts/geom/__init__.py
+++ pythonPackages/dynamicserialize/dstypes/com/vividsolutions/jts/geom/__init__.py
@@ -22,9 +22,11 @@
 
 __all__ = [
             'Coordinate',
+            'Envelope',
             'Geometry'
           ]
 
 from Coordinate import Coordinate
+from Envelope import Envelope
 from Geometry import Geometry
 
