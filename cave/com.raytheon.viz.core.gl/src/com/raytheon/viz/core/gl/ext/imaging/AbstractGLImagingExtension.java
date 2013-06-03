91de37404022d1cd8af59a149b270c15e68bc528
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-06-03 17:19:18 -0500

    Issue #1638 Made gl extensions more usable by other gl projects and topo resource more extendable.
    Change-Id: Ia0fe27113f7d3e1ce86bcce231812b2e0bbe0f75
    
    Former-commit-id: 7f76bd3d984d9206980e50c1f51e0bbfbaf07bf5 [formerly b28f451d17918a1c0b3678cb71d6e1cd32c45e33 [formerly 3379b100cb5bdeeb97a82d296d37674799f7ff2a] [formerly 7f76bd3d984d9206980e50c1f51e0bbfbaf07bf5 [formerly 483247738be16ae76bc2f9891f557960e7ade60e]]]
    Former-commit-id: b28f451d17918a1c0b3678cb71d6e1cd32c45e33 [formerly 3379b100cb5bdeeb97a82d296d37674799f7ff2a]
    Former-commit-id: b28f451d17918a1c0b3678cb71d6e1cd32c45e33
    Former-commit-id: 0df84a1b9a217ce51d1456fca7500788bc0873bb

--- cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/ext/imaging/AbstractGLImagingExtension.java
+++ cave/com.raytheon.viz.core.gl/src/com/raytheon/viz/core/gl/ext/imaging/AbstractGLImagingExtension.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.viz.core.gl.ext;
+package com.raytheon.viz.core.gl.ext.imaging;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
