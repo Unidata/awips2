d20e515b16bc70aeea22b303ad1d17f65cef017b
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2013-06-04 13:08:05 -0500

    Issue #1965 Initial archive retention dialog work. updated archive creation, purge, and test cases after Roger's last checkin. added edex plugin for feature. added an abstract base archive dialog.
    Change-Id: I7c9ca3df40a7591815a1aae4fabf660217d44574
    
    Former-commit-id: 13906f869ce509b731a8a8d44914758fd10bbfaa [formerly 13906f869ce509b731a8a8d44914758fd10bbfaa [formerly dcc56ad5d3e0d12ed4587ef95d479e95123e3a56]] [formerly b952eee03033a916590725a026228f7245534532] [formerly 55d19975bbbd4023f16490aebd4b6d1f0e5c1aeb [formerly 55d19975bbbd4023f16490aebd4b6d1f0e5c1aeb [formerly f53d6bf4fffff88f52a274a50340507ca45274f0]] [formerly b952eee03033a916590725a026228f7245534532 [formerly b952eee03033a916590725a026228f7245534532 [formerly 64b75031afaecd74649392a19cc0b10dcdb79a2b]]]]
    Former-commit-id: 55d19975bbbd4023f16490aebd4b6d1f0e5c1aeb [formerly 55d19975bbbd4023f16490aebd4b6d1f0e5c1aeb [formerly f53d6bf4fffff88f52a274a50340507ca45274f0]]
    Former-commit-id: c3f1f6d1f2d6838718c6cf492ea249cff3fe3e4d [formerly 85ec31e3d3655dfa8cb9773c51bd98ce7a85fd7e] [formerly 5ca1cdf0bffd0ffbac6853cb52813ea4f414f63f [formerly 9aff5f2321e51089f438efb664811d7fe0a0151b]]
    Former-commit-id: 9e0b1eb80705ae48009b03fadcbb968375171af6 [formerly 65d17722a9d9f89744ee2fd5ea388c354434f059]
    Former-commit-id: a334f31bf98b611d88a780b9bd453a9a161f4f44

---
+++ cave/com.raytheon.uf.viz.archive/src/com/raytheon/uf/viz/archive/ui/AbstractArchiveDlg.java
@@ -0,0 +1,430 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.archive.ui;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+
+import com.raytheon.uf.common.archive.config.ArchiveConfig;
+import com.raytheon.uf.common.archive.config.ArchiveConfigManager;
+import com.raytheon.uf.common.archive.config.ArchiveConfigManager.DisplayData;
+import com.raytheon.uf.common.archive.config.CategoryConfig;
+import com.raytheon.uf.common.util.SizeUtil;
+import com.raytheon.uf.viz.archive.data.ArchiveInfo;
+import com.raytheon.uf.viz.archive.data.CategoryInfo;
+import com.raytheon.uf.viz.archive.data.DirInfo;
+import com.raytheon.uf.viz.archive.data.IArchiveTotals;
+import com.raytheon.uf.viz.archive.ui.ArchiveTableComp.TableType;
+import com.raytheon.viz.ui.dialogs.CaveSWTDialog;
+import com.raytheon.viz.ui.dialogs.ICloseCallback;
+
+/**
+ * Abstract base class for Archive dialogs. Contains and manages information
+ * needed for the archive and category selectors and populates the selection
+ * table of display labels elements.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * May 30, 2013 1965       bgonzale     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bgonzale
+ * @version 1.0
+ */
+
+public abstract class AbstractArchiveDlg extends CaveSWTDialog implements
+        IArchiveTotals {
+
+    private static final String UNKNOWN_SIZE_TEXT = "????MB";
+
+    /** Table composite that holds the table controls. */
+    private ArchiveTableComp tableComp;
+
+    /** Archive config combo box. */
+    private Combo archCfgCbo;
+
+    /** Category combo box. */
+    private Combo categoryCbo;
+
+    /** Information for populating the various table displays. */
+    private final Map<String, ArchiveInfo> archiveInfoMap = new HashMap<String, ArchiveInfo>();
+
+    private Cursor busyCursor;
+
+    /**
+     * @param parentShell
+     */
+    public AbstractArchiveDlg(Shell parentShell) {
+        super(parentShell);
+        setupCursor();
+    }
+
+    /**
+     * @param parentShell
+     * @param swtStyle
+     */
+    public AbstractArchiveDlg(Shell parentShell, int swtStyle) {
+        super(parentShell, swtStyle);
+        setupCursor();
+    }
+
+    /**
+     * @param parentShell
+     * @param style
+     * @param caveStyle
+     */
+    public AbstractArchiveDlg(Shell parentShell, int style, int caveStyle) {
+        super(parentShell, style, caveStyle);
+        setupCursor();
+    }
+
+    /* (non-Javadoc)
+     * @see com.raytheon.uf.viz.archive.data.IArchiveTotals#getTotalSelectedItems()
+     */
+    @Override
+    public int getTotalSelectedItems() {
+        int totalSelected = 0;
+        for (ArchiveInfo archiveInfo : archiveInfoMap.values()) {
+            for (String categoryName : archiveInfo.getCategoryNames()) {
+                CategoryInfo categoryInfo = archiveInfo.get(categoryName);
+                for (DisplayData displayInfo : categoryInfo
+                        .getDisplayInfoList()) {
+                    if (displayInfo.isSelected()) {
+                        ++totalSelected;
+                    }
+                }
+            }
+        }
+        updateTotalSizeLabel();
+        return totalSelected;
+    }
+
+    public List<DisplayData> getAllSelected() {
+        List<DisplayData> allSelected = new ArrayList<ArchiveConfigManager.DisplayData>();
+        for (String archiveName : archiveInfoMap.keySet()) {
+            ArchiveInfo archiveInfo = archiveInfoMap.get(archiveName);
+            for (String categoryName : archiveInfo.getCategoryNames()) {
+                CategoryInfo categoryInfo = archiveInfo.get(categoryName);
+                for (DisplayData displayData : categoryInfo
+                        .getDisplayInfoList()) {
+                    if (displayData.isSelected()) {
+                        allSelected.add(displayData);
+                    }
+                }
+            }
+        }
+        return allSelected;
+    }
+
+    /**
+     * @return the name of the currently selected archive in the dialog; null if
+     *         none found
+     */
+    public String getSelectedArchiveName() {
+        int archiveSelection = archCfgCbo.getSelectionIndex();
+        String archiveName = archiveSelection == -1 ? null : archCfgCbo
+                .getItem(archiveSelection);
+        return archiveName;
+    }
+
+    /**
+     * @return the name of the currently selected category in the dialog; null
+     *         if none found
+     */
+    public String getSelectedCategoryName() {
+        int categorySelection = categoryCbo.getSelectionIndex();
+        String categoryName = categorySelection == -1 ? null : categoryCbo
+                .getItem(categorySelection);
+        return categoryName;
+    }
+
+    /**
+     * @return the currently selected archive in the dialog; null if none found
+     */
+    public ArchiveConfig getSelectedArchive() {
+        int archiveSelection = archCfgCbo.getSelectionIndex();
+        String archiveName = archiveSelection == -1 ? null : archCfgCbo
+                .getItem(archiveSelection);
+        return ArchiveConfigManager.getInstance().getArchive(archiveName);
+    }
+
+    /**
+     * @return the currently selected category in the dialog; null if none found
+     */
+    public CategoryConfig getSelectedCategory() {
+        String archiveName = getSelectedArchiveName();
+        ArchiveConfig archive = ArchiveConfigManager.getInstance().getArchive(
+                archiveName);
+        if (archive != null) {
+            String categoryName = getSelectedCategoryName();
+            return archive.getCategory(categoryName);
+        }
+        return null;
+    }
+
+    protected void updateTotalSizeLabel() {
+        long totalSize = 0;
+        for (String archiveName : archiveInfoMap.keySet()) {
+            ArchiveInfo archiveInfo = archiveInfoMap.get(archiveName);
+            for (String categoryName : archiveInfo.getCategoryNames()) {
+                CategoryInfo categoryInfo = archiveInfo.get(categoryName);
+                for (DisplayData displayData : categoryInfo
+                        .getDisplayInfoList()) {
+                    if (displayData.isSelected()) {
+                        long size = displayData.getSize();
+                        if (size < 0) {
+                            // Size still being computed.
+                            setTotalSizeText(UNKNOWN_SIZE_TEXT);
+                            return;
+                        } else {
+                            totalSize += size;
+                        }
+                    }
+                }
+            }
+        }
+        setTotalSizeText(SizeUtil.prettyByteSize(totalSize));
+    }
+
+    /**
+     * This method is called by the AbstractArchiveDlg to set the size text.
+     * 
+     * @param prettyByteSize
+     */
+    protected abstract void setTotalSizeText(String sizeStringText);
+
+    /**
+     * This method is called by the AbstractArchiveDlg to get the start of the
+     * time frame that bounds the data for the dialog.
+     * 
+     * @return GMT Calendar
+     */
+    protected abstract Calendar getStart();
+
+    /**
+     * This method is called by the AbstractArchiveDlg to get the end of the
+     * time frame that bounds the data for the dialog.
+     * 
+     * @return GMT Calendar
+     */
+    protected abstract Calendar getEnd();
+
+    /**
+     * This method is called by the AbstractArchiveDlg when the combo boxes are
+     * updated.
+     */
+    protected abstract void selectionsUpdated();
+
+    /**
+     * Create the Archive and Category combo controls.
+     * 
+     * @param comp
+     *            Composite to put the controls in.
+     */
+    protected void createComboControls(Composite comp) {
+        Composite comboComp = new Composite(comp, SWT.NONE);
+        GridLayout gl = new GridLayout(2, false);
+        GridData gd = new GridData(SWT.FILL, SWT.DEFAULT, true, false);
+        comboComp.setLayout(gl);
+        comboComp.setLayoutData(gd);
+
+        Label archCfgLbl = new Label(comboComp, SWT.NONE);
+        archCfgLbl.setText("Archive Config: ");
+
+        gd = new GridData(200, SWT.DEFAULT);
+        archCfgCbo = new Combo(comboComp, SWT.VERTICAL | SWT.DROP_DOWN
+                | SWT.BORDER | SWT.READ_ONLY);
+        archCfgCbo.setLayoutData(gd);
+        archCfgCbo.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                String archiveName = archCfgCbo.getItem(archCfgCbo
+                        .getSelectionIndex());
+                populateCategoryCbo(archiveName);
+            }
+        });
+
+        Label catLbl = new Label(comboComp, SWT.NONE);
+        catLbl.setText("Category: ");
+
+        gd = new GridData(200, SWT.DEFAULT);
+        categoryCbo = new Combo(comboComp, SWT.VERTICAL | SWT.DROP_DOWN
+                | SWT.BORDER | SWT.READ_ONLY);
+        categoryCbo.setLayoutData(gd);
+        categoryCbo.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                String archiveName = archCfgCbo.getItem(archCfgCbo
+                        .getSelectionIndex());
+                String categoryName = categoryCbo.getItem(categoryCbo
+                        .getSelectionIndex());
+                populateTableComp(archiveName, categoryName);
+            }
+        });
+
+        ArchiveConfigManager.getInstance().reset();
+
+        createTable();
+        populateComboBoxes();
+        updateTableComp();
+    }
+
+    /**
+     * Create the table control.
+     */
+    protected void createTable() {
+        tableComp = new ArchiveTableComp(shell, TableType.Case, this);
+    }
+
+    /**
+     * Update table based on current item selections in archive and category.
+     */
+    protected void updateTableComp() {
+        populateTableComp(getSelectedArchiveName(), getSelectedCategoryName());
+    }
+
+    /**
+     * Add a separator line to the provided container.
+     * 
+     * @param container
+     *            Composite.
+     * @param orientation
+     *            Vertical or horizontal orientation.
+     */
+    protected void addSeparator(Composite container, int orientation) {
+        // Separator label
+        GridData gd;
+
+        if (orientation == SWT.HORIZONTAL) {
+            gd = new GridData(SWT.FILL, SWT.DEFAULT, true, false);
+        } else {
+            gd = new GridData(SWT.DEFAULT, SWT.FILL, false, true);
+        }
+
+        Label sepLbl = new Label(container, SWT.SEPARATOR | orientation);
+        sepLbl.setLayoutData(gd);
+    }
+
+    /**
+     * Init busyCursor. On close, make sure that cursor reverts back to unbusy
+     * state if it isn't already.
+     */
+    private void setupCursor() {
+        busyCursor = getParent().getDisplay().getSystemCursor(SWT.CURSOR_WAIT);
+        this.setCloseCallback(new ICloseCallback() {
+            @Override
+            public void dialogClosed(Object returnValue) {
+                setCursorBusy(false);
+            }
+        });
+    }
+
+    /**
+     * Initial population up of the combo boxes.
+     */
+    private void populateComboBoxes() {
+        ArchiveConfigManager manager = ArchiveConfigManager.getInstance();
+        boolean doSelect = false;
+        for (String archiveName : manager.getArchiveDataNamesList()) {
+            archCfgCbo.add(archiveName);
+            doSelect = true;
+        }
+
+        if (doSelect) {
+            archCfgCbo.select(0);
+            String archiveName = archCfgCbo.getItem(0);
+            populateCategoryCbo(archiveName);
+        }
+    }
+
+    /**
+     * Populate the category combo based on the archive name and populate the
+     * table.
+     * 
+     * @param archiveName
+     */
+    private void populateCategoryCbo(String archiveName) {
+        ArchiveConfigManager manager = ArchiveConfigManager.getInstance();
+        categoryCbo.removeAll();
+        for (String categoryName : manager.getCategoryNames(archiveName)) {
+            categoryCbo.add(categoryName);
+        }
+        categoryCbo.select(0);
+        String categoryName = categoryCbo.getItem(0);
+        populateTableComp(archiveName, categoryName);
+    }
+
+    private void populateTableComp(String archiveName, String categoryName) {
+
+        setCursorBusy(true);
+        DirInfo.clearQueue();
+        ArchiveInfo archiveInfo = archiveInfoMap.get(archiveName);
+        if (archiveInfo == null) {
+            archiveInfo = new ArchiveInfo();
+            archiveInfoMap.put(archiveName, archiveInfo);
+        }
+
+        CategoryInfo categoryInfo = archiveInfo.get(categoryName);
+        if (categoryInfo == null) {
+            ArchiveConfigManager manager = ArchiveConfigManager.getInstance();
+            List<DisplayData> displayInfos = manager.getDisplayInfo(
+                    archiveName, categoryName);
+            categoryInfo = new CategoryInfo(archiveName, categoryName,
+                    displayInfos);
+            archiveInfo.add(categoryInfo);
+        }
+
+        for (DisplayData displayInfo : categoryInfo.getDisplayInfoList()) {
+            new DirInfo(displayInfo, getStart(), getEnd());
+        }
+
+        tableComp.populateTable(categoryInfo.getDisplayInfoList());
+        selectionsUpdated();
+        setCursorBusy(false);
+    }
+
+    private void setCursorBusy(boolean state) {
+        Cursor cursor = null;
+        if (state) {
+            cursor = busyCursor;
+        }
+        shell.setCursor(cursor);
+    }
+
+}
