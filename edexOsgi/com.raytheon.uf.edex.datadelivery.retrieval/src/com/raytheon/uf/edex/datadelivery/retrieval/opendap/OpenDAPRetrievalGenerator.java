63f8ff76c3ff7448d83f68b3c8d5f71b61b0b53c
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-06-05 11:15:48 -0500

    Issue #2038 Add support for point data type subscriptions to BandwidthManager
    Amend: Peer review comments, improve time considerations for Point subscriptions
    
    Change-Id: I9dde7f7da9dab9c8f3163a87b1ebe64303fc7dc2
    
    Former-commit-id: c677ae32d75079493ce84428c03632c63968d7f0 [formerly f7ed3ec86188635eb069878ac490af06ea98e4a2]
    Former-commit-id: 7c4a6261fcb168774d4215d44a20207dc9abd053

--- edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/opendap/OpenDAPRetrievalGenerator.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.retrieval/src/com/raytheon/uf/edex/datadelivery/retrieval/opendap/OpenDAPRetrievalGenerator.java
@@ -36,8 +36,9 @@ import com.raytheon.uf.common.datadelivery.registry.GriddedDataSetMetaData;
 import com.raytheon.uf.common.datadelivery.registry.Levels;
 import com.raytheon.uf.common.datadelivery.registry.OpenDapGriddedDataSet;
 import com.raytheon.uf.common.datadelivery.registry.Parameter;
-import com.raytheon.uf.common.datadelivery.registry.Provider.ProviderType;
+import com.raytheon.uf.common.datadelivery.registry.Provider;
 import com.raytheon.uf.common.datadelivery.registry.Provider.ServiceType;
+import com.raytheon.uf.common.datadelivery.registry.ProviderType;
 import com.raytheon.uf.common.datadelivery.registry.Subscription;
 import com.raytheon.uf.common.datadelivery.registry.SubscriptionBundle;
 import com.raytheon.uf.common.datadelivery.registry.Time;
@@ -402,11 +403,7 @@ class OpenDAPRetrievalGenerator extends RetrievalGenerator {
 
         // Coverage and type processing
         Coverage cov = sub.getCoverage();
-        ProviderType pt = null;
-        if (cov instanceof GriddedCoverage){
-            pt = ProviderType.GRID;
-            retrieval.setProviderType(pt);
-        } else {
+        if (!(cov instanceof GriddedCoverage)) {
             throw new UnsupportedOperationException(
                     "OPENDAP retrieval does not yet support coverages other than Gridded. ");
         }
@@ -420,7 +417,18 @@ class OpenDAPRetrievalGenerator extends RetrievalGenerator {
         att.setParameter(lparam);
         att.setEnsemble(ensemble);
         att.setSubName(retrieval.getSubscriptionName());
-        att.setPlugin(pt.getPlugin());
+        Provider provider;
+        try {
+            provider = DataDeliveryHandlers.getProviderHandler().getByName(
+                    sub.getProvider());
+        } catch (RegistryHandlerException e) {
+            throw new IllegalArgumentException(
+                    "Error looking up the provider!", e);
+        }
+        // Look up the provider's configured plugin for this data type
+        ProviderType providerType = provider.getProviderType(sub
+                .getDataSetType());
+        att.setPlugin(providerType.getPlugin());
         att.setProvider(sub.getProvider());
         retrieval.addAttribute(att);
 
