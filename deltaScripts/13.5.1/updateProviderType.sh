1425620cce38b18ab4d07758194f00970ec518a7
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-06-10 14:25:02 -0500

    Issue #2038 Delta script should also update providers in the database
    Change-Id: I07f058af1702d43f3aa60f0a8646b016f6866b3b
    
    Former-commit-id: 2b5daedc75398f6b8e27b4948c79f30f3d168c6d [formerly f361c49484c669213ba98bc2950c7d5a66450339] [formerly 2b5daedc75398f6b8e27b4948c79f30f3d168c6d [formerly f361c49484c669213ba98bc2950c7d5a66450339] [formerly 14c7d2d81e8ba04c85a4be7a135b218844d005a6 [formerly de9efbe2f2a74f5c2b2d730153f4831f0c17336e]]]
    Former-commit-id: 14c7d2d81e8ba04c85a4be7a135b218844d005a6
    Former-commit-id: b0658fda214e88f6bbb8855e2a306eb3fb56f4cd [formerly 582c618ace7dc120fefa0f12a02932ae63c6df86]
    Former-commit-id: 27120c17f07a4719854be6c0a4a5d8224724e60c

--- deltaScripts/13.5.1/updateProviderType.sh
+++ deltaScripts/13.5.1/updateProviderType.sh
@@ -26,6 +26,38 @@ do
    rm $FILE.md5
 done
 
+# Dump the provider rows from the database for modification
+PROVIDER_ROWS=/tmp/provider_rows.tmp
+psql -U awips -d metadata -c "\copy (select key, stringvalue from ebxml.value where stringvalue like '%<provider %') To '${PROVIDER_ROWS}'";
+
+# Get old separator
+OIFS=$IFS
+
+IFS=$'\n'
+for f in `cat ${PROVIDER_ROWS}`
+do
+   IFS=$'\t'
+   arr2=( $f )
+
+   KEY=${arr2[0]}
+   XML_FILE=/tmp/${KEY}.xml
+
+   # Write out database contents
+   echo "${arr2[1]}" > ${XML_FILE}
+   # Remove carriage returns
+   sed -i 's/\\n//g' ${XML_FILE}
+
+   # Run the xslt transform on the tmp file
+   xsltproc ${XSLT_SCRIPT} ${XML_FILE} > ${XML_FILE}.new
+
+   # Insert the new xml into the database
+   NEW_XML=`cat ${XML_FILE}.new`
+   psql -U awips -d metadata -c "UPDATE ebxml.value SET stringvalue = '${NEW_XML}' WHERE key = '${KEY}'"
+done
+
+# Restore old separator
+IFS=$OIFS
+
 echo "INFO: the update has completed successfully!"
 
 exit 0
