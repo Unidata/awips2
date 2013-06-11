7c11dea797435fc41d3d119d92b5378f08bd58ff
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-06-11 13:07:14 -0500

    Issue #2038 Fixes for 13.5.1 delta scripts
    Former-commit-id: 0ac8d01147b707630534778f3b53e246bb0b6cfa [formerly 660ca8fcec2b349c4894fa3f052b049ad5e5be5a] [formerly bac07c65a17fe40b23e3052171c72834eefccf80 [formerly 41ead90acf0d06e959e513e5a25869eb6e18150a]]
    Former-commit-id: bac07c65a17fe40b23e3052171c72834eefccf80
    Former-commit-id: ec3e8b641191d04c1e5642ce832fdf75586dbd37

--- deltaScripts/13.5.1/updateProviderTypeDb.sh
+++ deltaScripts/13.5.1/updateProviderTypeDb.sh
@@ -8,23 +8,7 @@ if [ ! -f ${XSLT_SCRIPT} ]; then
    exit 1
 fi
 
-echo "INFO: update started - updating ProviderType to be a class proper"
-
-# Update subscription manager configuration files
-for FILE in `find /awips2/edex/data/utility/common_static -iname \*-harvester.xml`
-do
-    cp $FILE $FILE.bak
-    xsltproc ${XSLT_SCRIPT} ${FILE}.bak > ${FILE}
-      
-   # Make sure each command succeeds
-   if [ $? -ne 0 ]; then
-     echo "FATAL: the update has failed!"
-     exit 1
-   fi
-      
-   # Delete the md5 file
-   rm $FILE.md5
-done
+echo "INFO: update started - updating ProviderType to be a class proper in the database"
 
 # Dump the provider rows from the database for modification
 PROVIDER_ROWS=/tmp/provider_rows.tmp
