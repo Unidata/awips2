28fb05fcae4b07242378555cbe62d56e91a22100
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-06-11 14:14:09 -0500

    Issue #2090 Improve the performance of Madis purging
    Amend: Add note to madisPurgeRules.xml about purge keys
    
    Change-Id: I5c0d86d63e7b725cf2b1e6419c8dfcf6ed8396ef
    
    Former-commit-id: 2aa51f1e5f2fcbadaf5988e66278a160b29ae4d6 [formerly 2aa51f1e5f2fcbadaf5988e66278a160b29ae4d6 [formerly 3261439e5230bf9d2f2d7e5e81547ae00d924d1e]]
    Former-commit-id: 6b228eca8668b89043e8df6701efe14dcfd11f83
    Former-commit-id: c3c89ca1638b93914170962f11f0ffab5a404b2a

--- edexOsgi/com.raytheon.uf.edex.plugin.madis/src/com/raytheon/uf/edex/plugin/madis/MadisDao.java
+++ edexOsgi/com.raytheon.uf.edex.plugin.madis/src/com/raytheon/uf/edex/plugin/madis/MadisDao.java
@@ -23,11 +23,13 @@ package com.raytheon.uf.edex.plugin.madis;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
 import com.raytheon.uf.common.dataplugin.PluginException;
 import com.raytheon.uf.common.dataplugin.madis.MadisRecord;
 import com.raytheon.uf.common.dataquery.db.QueryParam;
@@ -36,14 +38,16 @@ import com.raytheon.uf.common.pointdata.spatial.ObStation;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.common.util.CollectionUtil;
 import com.raytheon.uf.edex.database.DataAccessLayerException;
+import com.raytheon.uf.edex.database.plugin.PluginDao;
 import com.raytheon.uf.edex.database.query.DatabaseQuery;
 import com.raytheon.uf.edex.pointdata.PointDataDbDescription;
 import com.raytheon.uf.edex.pointdata.PointDataPluginDao;
 import com.raytheon.uf.edex.pointdata.spatial.ObStationDao;
 
 /**
- * MadisDao  MADIS data DAO
+ * MadisDao MADIS data DAO
  * 
  * <pre>
  * 
@@ -51,6 +55,7 @@ import com.raytheon.uf.edex.pointdata.spatial.ObStationDao;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * MAR 27, 2013 1746       dhladky     MADIS data record creation
+ * Jun 11, 2013 2090       djohnson    Override purgeDataByRefTime to improve purge performance.
  * 
  * </pre>
  * 
@@ -59,22 +64,22 @@ import com.raytheon.uf.edex.pointdata.spatial.ObStationDao;
  */
 
 public class MadisDao extends PointDataPluginDao<MadisRecord> {
-    
+
     /** The station dao */
     private ObStationDao obDao = new ObStationDao();
-    
+
     private static final IUFStatusHandler statusHandler = UFStatus
-    .getHandler(MadisDao.class);
-   
+            .getHandler(MadisDao.class);
+
     public List<?> queryBySpatialBox(double upperLeftLat, double upperLeftLon,
             double lowerRightLat, double lowerRightLon)
             throws DataAccessLayerException {
 
         List<ObStation> stationList = obDao.queryBySpatialBox(upperLeftLat,
                 upperLeftLon, lowerRightLat, lowerRightLon);
-       
+
         List<String> stationNames = new ArrayList<String>();
-        for (ObStation ob: stationList) {
+        for (ObStation ob : stationList) {
             stationNames.add(ob.getIcao());
         }
 
@@ -104,8 +109,8 @@ public class MadisDao extends PointDataPluginDao<MadisRecord> {
      */
     public Object[] queryDataUriColumn(final String dataUri) {
 
-        String sql = "select datauri from awips.madis where datauri='" + dataUri
-                + "';";
+        String sql = "select datauri from awips.madis where datauri='"
+                + dataUri + "';";
 
         Object[] results = executeSQLQuery(sql);
 
@@ -128,7 +133,8 @@ public class MadisDao extends PointDataPluginDao<MadisRecord> {
         } catch (JAXBException e) {
             statusHandler.error("Unable to load madis Point Data Description",
                     e);
-            throw new PluginException("Unable to load madis Point Data Description!", e);
+            throw new PluginException(
+                    "Unable to load madis Point Data Description!", e);
         }
 
     }
@@ -137,7 +143,6 @@ public class MadisDao extends PointDataPluginDao<MadisRecord> {
     public PointDataDescription getPointDataDescription(Map<String, Object> obj) {
         return hdf5DataDescription;
     }
-
 
     public ObStationDao getObDao() {
         return obDao;
@@ -161,7 +166,7 @@ public class MadisDao extends PointDataPluginDao<MadisRecord> {
     public MadisRecord newObject() {
         return new MadisRecord();
     }
-    
+
     /*
      * (non-Javadoc)
      * 
@@ -196,5 +201,51 @@ public class MadisDao extends PointDataPluginDao<MadisRecord> {
         return dbDataDescription;
     }
 
+    /**
+     * Overridden because {@link PluginDao} retrieves all PDO instances prior to
+     * purging them, in order to calculute HDF5 paths for each one. In the case
+     * of {@link Madis} objects, the granularity is only down to the hour level
+     * therefore we can just pull one, calculate the HDF5 path, and purge all
+     * entities without retrieving them.
+     */
+    @Override
+    public int purgeDataByRefTime(Date refTime,
+            Map<String, String> productKeys, boolean trackHdf5,
+            boolean trackToUri, Map<String, List<String>> hdf5FileToUriPurged)
+            throws DataAccessLayerException {
+
+        DatabaseQuery dataQuery = new DatabaseQuery(this.daoClass);
+
+        if (refTime != null) {
+            dataQuery.addQueryParam(PURGE_VERSION_FIELD, refTime);
+        }
+
+        if ((productKeys != null) && (productKeys.size() > 0)) {
+            for (Map.Entry<String, String> pair : productKeys.entrySet()) {
+                dataQuery.addQueryParam(pair.getKey(), pair.getValue());
+            }
+        }
+
+        // Keep track of the old max results so we can use the same query to
+        // find an example pdo to get the HDF5 path
+        final Integer oldMaxResults = dataQuery.getMaxResults();
+        dataQuery.setMaxResults(1);
+        @SuppressWarnings("unchecked")
+        final List<PluginDataObject> pdos = (List<PluginDataObject>) this
+                .queryByCriteria(dataQuery);
+        if (CollectionUtil.isNullOrEmpty(pdos)) {
+            return 0;
+        }
+
+        // Restore the old max results so it targets all entities
+        dataQuery.setMaxResults(oldMaxResults);
+        int numberDeleted = this.deleteByCriteria(dataQuery);
+
+        if (trackHdf5 && (hdf5FileToUriPurged != null)) {
+            purgeHdf5ForPdos(trackToUri, hdf5FileToUriPurged, pdos);
+        }
+
+        return numberDeleted;
+    }
 
 }
