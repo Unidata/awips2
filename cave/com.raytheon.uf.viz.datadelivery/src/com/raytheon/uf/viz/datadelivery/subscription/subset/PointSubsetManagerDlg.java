4735212532b417265cb0740f2d630396856acd67
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-06-13 10:54:08 -0500

    Issue #2106 Speed up Bandwidth Manager subscription scheduling
    Change-Id: Iafceeb64a2b56c562ca737ba083ec3225524f7ba
    
    Former-commit-id: 2574f5a25d057aa0bd3813c22552e34533883552 [formerly 2574f5a25d057aa0bd3813c22552e34533883552 [formerly 693bfd77ddc216e658f6e1e4a38f48a2df1fe3bf]]
    Former-commit-id: 8500856bc18dd63623c4822878ece14413d81c82
    Former-commit-id: b3cad4e40bf1f1308ce5b66625e1513e079af233

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/PointSubsetManagerDlg.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/PointSubsetManagerDlg.java
@@ -174,6 +174,9 @@ public class PointSubsetManagerDlg extends
         newTime.setStartDate(new Date());
         newTime.setEndDate(new Date());
         sub.setTime(newTime);
+        // Setting arbitrary size until mpduff and dhladky figure out their data
+        // size estimates
+        sub.setDataSetSize(50);
 
         Coverage cov = new Coverage();
 
