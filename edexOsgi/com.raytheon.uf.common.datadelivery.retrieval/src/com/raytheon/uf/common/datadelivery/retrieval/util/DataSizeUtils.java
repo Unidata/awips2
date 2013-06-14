7023522ba64dd6a6a4c532ea325d0489a2eb002a
Mike Duff <michael.duff@raytheon.com>
2013-06-14 12:30:11 -0500

    Issue #2108 - Refactor DataSizeUtils
    Change-Id: I0e633fcd69edbbb25b930ac4797ae0934655bd0f
    
    Former-commit-id: 9d66579fb9486aac023c07fd97b0cd806681d2b9 [formerly 0411daa2b5c9a67e8cacdbf253097d194a5cfae1 [formerly 532af54aaee3367b2a860d9ffd808912aac73992] [formerly 9d66579fb9486aac023c07fd97b0cd806681d2b9 [formerly b6fffb21340d210a5a63d5f060f1dfe97b4868fb]]]
    Former-commit-id: 0411daa2b5c9a67e8cacdbf253097d194a5cfae1 [formerly 532af54aaee3367b2a860d9ffd808912aac73992]
    Former-commit-id: 0411daa2b5c9a67e8cacdbf253097d194a5cfae1
    Former-commit-id: a90c9e25b02c394e77a181b908aef5619b7711a5

--- edexOsgi/com.raytheon.uf.common.datadelivery.retrieval/src/com/raytheon/uf/common/datadelivery/retrieval/util/DataSizeUtils.java
+++ edexOsgi/com.raytheon.uf.common.datadelivery.retrieval/src/com/raytheon/uf/common/datadelivery/retrieval/util/DataSizeUtils.java
@@ -19,24 +19,18 @@
  **/
 package com.raytheon.uf.common.datadelivery.retrieval.util;
 
-import java.util.List;
-import java.util.Map;
-
 import org.geotools.geometry.jts.ReferencedEnvelope;
 
-import com.raytheon.uf.common.datadelivery.registry.Coverage;
 import com.raytheon.uf.common.datadelivery.registry.DataSet;
-import com.raytheon.uf.common.datadelivery.registry.Ensemble;
+import com.raytheon.uf.common.datadelivery.registry.DataType;
 import com.raytheon.uf.common.datadelivery.registry.EnvelopeUtils;
 import com.raytheon.uf.common.datadelivery.registry.GriddedCoverage;
 import com.raytheon.uf.common.datadelivery.registry.GriddedDataSet;
-import com.raytheon.uf.common.datadelivery.registry.Levels;
-import com.raytheon.uf.common.datadelivery.registry.Parameter;
+import com.raytheon.uf.common.datadelivery.registry.PointDataSet;
 import com.raytheon.uf.common.datadelivery.registry.PointTime;
 import com.raytheon.uf.common.datadelivery.registry.Provider.ServiceType;
+import com.raytheon.uf.common.datadelivery.registry.Subscription;
 import com.raytheon.uf.common.datadelivery.retrieval.xml.RetrievalAttribute;
-import com.raytheon.uf.common.gridcoverage.GridCoverage;
-import com.raytheon.uf.common.util.CollectionUtil;
 import com.vividsolutions.jts.geom.Coordinate;
 
 /**
@@ -53,6 +47,7 @@ import com.vividsolutions.jts.geom.Coordinate;
  * Oct 31, 2012  1278      mpduff     Clarified a Javadoc comment.
  * Dec 10, 2012  1259      bsteffen   Switch Data Delivery from LatLon to referenced envelopes.
  * Jun 11, 2013  2021      dhladky    WFS semi-scientific sizing.
+ * Jun 14, 2013  2108      mpduff     Abstracted the class.
  * 
  * </pre>
  * 
@@ -60,16 +55,38 @@ import com.vividsolutions.jts.geom.Coordinate;
  * @version 1.0
  */
 
-public class DataSizeUtils {
+public abstract class DataSizeUtils<DS extends DataSet> {
 
     /**
-     * Get data size
+     * Factory method to get a DataSizeUtils.
+     * 
+     * @param dataSet
+     *            The data set
+     * @param subscription
+     *            The subscription
+     * @return The correct implementation of DataSizeUtils
+     */
+    public static DataSizeUtils<?> getInstance(DataSet dataSet,
+            Subscription subscription) {
+        DataSizeUtils<?> dsu = null;
+        if (subscription.getDataSetType() == DataType.GRID) {
+            dsu = new GriddedDataSizeUtils((GriddedDataSet) dataSet);
+        } else if (subscription.getDataSetType() == DataType.POINT) {
+            dsu = new PointDataSizeUtils((PointDataSet) dataSet);
+        }
+
+        return dsu;
+    }
+
+    /**
+     * Get data size.
      * 
      * @param ra
+     *            RetrievalAttribute
      * @param st
-     * @return
+     *            ServiceType
+     * @return dataset size
      */
-
     public static long calculateSize(RetrievalAttribute ra, ServiceType st) {
 
         if (st == ServiceType.OPENDAP) {
@@ -89,15 +106,16 @@ public class DataSizeUtils {
             }
 
         } else if (st == ServiceType.WFS) {
-  
+
             ReferencedEnvelope re = ra.getCoverage().getRequestEnvelope();
             Coordinate ur = EnvelopeUtils.getUpperRightLatLon(re);
             Coordinate ll = EnvelopeUtils.getLowerLeftLatLon(re);
             double lonSpan = Math.abs(ll.x - ur.x);
             double latSpan = Math.abs(ll.y - ur.y);
-            PointTime time = (PointTime)ra.getTime();
-            long l = st.getRequestBytesPerLatLonBoxAndTime(latSpan, lonSpan, time.getInterval());
-            
+            PointTime time = (PointTime) ra.getTime();
+            long l = st.getRequestBytesPerLatLonBoxAndTime(latSpan, lonSpan,
+                    time.getInterval());
+
             return l;
         } else {
             throw new IllegalStateException(
@@ -107,158 +125,59 @@ public class DataSizeUtils {
     }
 
     /** Bytes per Kilobyte */
-    private static final int bytesPerKilobyte = 1024;
+    protected static final int bytesPerKilobyte = 1024;
 
     /** Data Set Object */
-    private DataSet dataSet = null;
+    protected DS dataSet;
 
     /** Data Set Size */
-    private long size = 0;
+    protected final long size = 0;
 
     /** Full Data Set Size in bytes */
-    private long fullSize = -999;
-
-    /** Number of requested grids */
-    private int numRequestedGrids = 0;
-
-    /** Number of forecast hours */
-    private int numFcstHours = 0;
-
-    private int numEnsembleMembers = 0;
-
-    /** Envelope */
-    private ReferencedEnvelope envelope = null;
-
-    /** Number of grid cells */
-    private int numberOfGridCells = 0;
-
-    /** Default Constructor */
-    public DataSizeUtils() {
-
-    }
+    protected long fullSize = -999;
 
     /**
-     * Constructor.
+     * Returns the estimated full data set size in bytes.
      * 
-     * @param dataSetName
-     *            The data set name
+     * @return full data set size in bytes
      */
-    public DataSizeUtils(DataSet dataSet) {
-        this.dataSet = dataSet;
-    }
+    public abstract long getFullSizeInBytes();
 
-    private void calculateGridCells() {
-        if (dataSet != null) {
-            Coverage cov = dataSet.getCoverage();
-            if (cov instanceof GriddedCoverage) {
-                GriddedCoverage griddedCov = (GriddedCoverage) cov;
-
-                GridCoverage subgridCov = griddedCov
-                        .getRequestGridCoverage(envelope);
-                if (subgridCov == null) {
-                    subgridCov = griddedCov.getGridCoverage();
-                }
-                int nx = subgridCov.getNx();
-                int ny = subgridCov.getNy();
-
-                numberOfGridCells = nx * ny;
-
-            }
-        }
-    }
+    /**
+     * Get the data set size for the provided subscription.
+     * 
+     * @param subscription
+     *            Subscription for calculating the size
+     * @return Data size in bytes
+     */
+    protected abstract long getDataSetSizeInBytes(Subscription subscription);
 
     /**
      * @return the dataSet
      */
-    public DataSet getDataSet() {
+    public DS getDataSet() {
         return dataSet;
     }
 
-    public long getDataSetSizeInBytes() {
-        long l = numRequestedGrids
-                * numFcstHours
-                * numEnsembleMembers
-                * dataSet.getServiceType().getRequestBytesPerParameterPerLevel(
-                        numberOfGridCells);
-        return l;
+    /**
+     * Returns the estimated data set size in kB.
+     * 
+     * @param subscription
+     *            The subscription to size
+     * 
+     * @return the size in kB
+     */
+    public long getDataSetSizeInKb(Subscription subscription) {
+        return getDataSetSizeInBytes(subscription) / bytesPerKilobyte;
     }
 
     /**
-     * Returns the estimated data set size in KB.
+     * Returns the estimated full dataset size in kB.
      * 
-     * @return
+     * @return full data set size in kB
      */
-    public long getDataSetSize() {
-        return getDataSetSizeInBytes() / bytesPerKilobyte;
-    }
-
-    /**
-     * Returns the estimated full dataset size in bytes.
-     * 
-     * @return
-     */
-    public long getFullSizeInBytes() {
-        if (dataSet != null) {
-            if (fullSize == -999) {
-                Coverage cov = dataSet.getCoverage();
-                if (cov instanceof GriddedCoverage) {
-                    GriddedCoverage griddedCov = (GriddedCoverage) cov;
-                    long numCells = griddedCov.getGridCoverage().getNx()
-                            * griddedCov.getGridCoverage().getNy();
-                    // Default to 1 forecast hour if not a gridded data set
-                    long numEns = 1;
-                    long fcstHrs = 1;
-                    if (dataSet instanceof GriddedDataSet) {
-                        GriddedDataSet gDataSet = (GriddedDataSet) dataSet;
-                        fcstHrs = gDataSet.getForecastHours().size();
-                        if (gDataSet.getEnsemble() != null) {
-                            numEns = gDataSet.getEnsemble().getMemberCount();
-                        }
-                    }
-                    Map<String, Parameter> paramMap = dataSet.getParameters();
-
-                    // get the number of grids available
-                    long numGridsAvailable = 0;
-
-                    for (Parameter p : paramMap.values()) {
-                        int numLevels = p.getLevels().getLevel().size();
-
-                        // parameter is always at least on one level, level just
-                        // may not be named/enumerated
-                        numGridsAvailable += (numLevels > 0 ? numLevels : 1);
-                    }
-
-                    fullSize = numEns
-                            * fcstHrs
-                            * numGridsAvailable
-                            * dataSet.getServiceType()
-                                    .getRequestBytesPerParameterPerLevel(
-                                            numCells);
-                }
-            }
-        }
-
-        return fullSize;
-    }
-
-    /**
-     * Returns the estimated full dataset size in KB.
-     * 
-     * @return
-     */
-    public long getFullSize() {
+    public long getFullSizeInKb() {
         return getFullSizeInBytes() / bytesPerKilobyte;
-    }
-
-    public int getNumberOfGridCells() {
-        return numberOfGridCells;
-    }
-
-    /**
-     * @return the numFcstHours
-     */
-    public int getNumFcstHours() {
-        return numFcstHours;
     }
 
     /**
@@ -266,75 +185,5 @@ public class DataSizeUtils {
      */
     public long getSize() {
         return size;
-    }
-
-    /**
-     * Reset the state of this object
-     */
-    public void reset() {
-        numRequestedGrids = 0;
-        size = 0;
-        dataSet = null;
-    }
-
-    public void setEnvelope(ReferencedEnvelope envelope) {
-        this.envelope = envelope;
-        if (envelope != null) {
-            calculateGridCells();
-        }
-    }
-
-    /**
-     * @param dataSet
-     *            the dataSet to set
-     */
-    public void setDataSet(DataSet dataSet) {
-        this.dataSet = dataSet;
-    }
-
-    /**
-     * @param numFcstHours
-     *            the numFcstHours to set
-     */
-    public void setNumFcstHours(int numFcstHours) {
-        this.numFcstHours = numFcstHours;
-    }
-
-    /**
-     * @param numRequestedGrids
-     *            the numRequestedGrids to set
-     */
-    public void determineNumberRequestedGrids(List<Parameter> parameterList) {
-        int numGrids = 0;
-
-        // Get the number of requested grids
-        if (!CollectionUtil.isNullOrEmpty(parameterList)) {
-            for (Parameter par : parameterList) {
-                Levels parLevels = par.getLevels();
-                int numSelectedLevels = parLevels.getSelectedLevelIndices()
-                        .size();
-                if (numSelectedLevels < 1) {
-                    // if parameter is not available on more than level, then by
-                    // default the single level is selected
-                    if (parLevels.size() <= 1) {
-                        numSelectedLevels = 1;
-                    }
-                    // else user did not select any levels for this parameter
-                }
-
-                numGrids += numSelectedLevels;
-            }
-        }
-
-        this.numRequestedGrids = numGrids;
-    }
-
-    public void setNumEnsembleMembers(Ensemble ensemble) {
-        if (ensemble == null) {
-            this.numEnsembleMembers = 1;
-        } else {
-            this.numEnsembleMembers = ensemble.getSelectedMemberCount();
-        }
-
     }
 }
