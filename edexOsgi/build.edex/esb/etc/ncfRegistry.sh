86b096bc9b3ad16896c856fa85a9083183aeca40
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-06-27 12:33:34 -0500

    Issue #2142 initial cutover from log4j to SLF4J and logback
    Change-Id: I6953739821992fadb2b92da0dc659512b90a5895
    
    Former-commit-id: a4de603bcf59ee297cff102ea99d78e946a6c3d6 [formerly 316d6c09c64dc1ca1e4b798179bf48005ac14e40] [formerly a4de603bcf59ee297cff102ea99d78e946a6c3d6 [formerly 316d6c09c64dc1ca1e4b798179bf48005ac14e40] [formerly db6152e524ec4a8a610a4d7d80ba3e867eefe3a3 [formerly 7725d8a6cd00b3c30856b48ef86ab081b67d400b]]]
    Former-commit-id: db6152e524ec4a8a610a4d7d80ba3e867eefe3a3
    Former-commit-id: 5a6b0f99cc313c68cf0d9207a4695bcef3aeb94d [formerly 6da64f329cb89352dad5121f90c8d1d80d18f39a]
    Former-commit-id: efcf0c8519388de1ef8a0e3072d67ff81792f03f

--- edexOsgi/build.edex/esb/etc/ncfRegistry.sh
+++ edexOsgi/build.edex/esb/etc/ncfRegistry.sh
@@ -24,7 +24,7 @@ export MAX_PERM_SIZE=192m
 export METADATA_POOL_MIN=10
 export EDEX_DEBUG_PORT=5011
 export EDEX_JMX_PORT=1622
-export LOG4J_CONF=log4j-registry.xml
+export LOG_CONF=logback-registry.xml
 export MGMT_PORT=9607
 
 # Registry specific ports
