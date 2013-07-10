45636a133223fb564dccaa88a0a37944e8e0b762
Dustin Johnson <Dustin.P.Johnson@raytheon.com>
2013-07-10 15:36:00 -0500

    Issue #2106 Improve BandwidthManager performance, proposed changes are checked much quicker
    Change-Id: I3a61de1904d7256de04a97f40f38bf18d78b1438
    
    Former-commit-id: a7ef4084a0d2a3a16b9c1e15d099d5796557fad0 [formerly a7ef4084a0d2a3a16b9c1e15d099d5796557fad0 [formerly 0c6d48d985e553e3a9fee95302a543fc2b73836e]] [formerly b36200d583e6ea74182386e3ba5038e7645b218c] [formerly a7ef4084a0d2a3a16b9c1e15d099d5796557fad0 [formerly a7ef4084a0d2a3a16b9c1e15d099d5796557fad0 [formerly 0c6d48d985e553e3a9fee95302a543fc2b73836e]] [formerly b36200d583e6ea74182386e3ba5038e7645b218c] [formerly 73828b8d00181d8a8163c95a7f9aabb83535bbda [formerly 73828b8d00181d8a8163c95a7f9aabb83535bbda [formerly ec2ddd13b1cb3a99bf99176b1bc7f7311272735b]] [formerly b36200d583e6ea74182386e3ba5038e7645b218c [formerly b36200d583e6ea74182386e3ba5038e7645b218c [formerly 59eafdd07754e3711e9c3babd946dfd18c161404]]]]]
    Former-commit-id: 73828b8d00181d8a8163c95a7f9aabb83535bbda [formerly 73828b8d00181d8a8163c95a7f9aabb83535bbda [formerly ec2ddd13b1cb3a99bf99176b1bc7f7311272735b]]
    Former-commit-id: b043945f38269ed9179964a3cef1fdffec8fbbcd [formerly 312f65cd5c455e482e3945a6202d48dcda78da56] [formerly 7b0c21e24a37ed2c758aeeff83d63638cf101697 [formerly 10884ca1371d580e319811565f47fa1c9e047eb6]] [formerly a782efed2f50fdba1f3d282075a9c7cd5bec0c6a [formerly 49d94fc413e34495ff1453c438ae43e9ba8233d7] [formerly a411d6a57c9a966975a68b6e27682531264c23b8 [formerly 580bc2cadf44a91e877ce4fbfc36dc838c1f5c8b]]]
    Former-commit-id: 10859afe7814600d519cfd187f1c7c3e62640102 [formerly 6ca163c955b97eadbc11a250d2c8b4ebdcaa472a] [formerly 26163120b347bee6c1e2e8c9089c9ee32f014e41 [formerly f790277454cec440213ca6cdf802dfb51d0612a5]]
    Former-commit-id: 0869755609a2187fbd3525b055aed10159b62759 [formerly e556493c77ef27b7db4dbd9a2cc30833a868cb09]
    Former-commit-id: 79fc9c5784e914184a1de5f51359ba42d5828cf9

--- edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/notification/BandwidthAsyncEventBusFactory.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/notification/BandwidthAsyncEventBusFactory.java
@@ -25,9 +25,7 @@ import java.util.concurrent.Executors;
 
 import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
-import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.edex.core.EDEXUtil;
 
 /**
  * Creates asynchronous Google event buses.
@@ -41,6 +39,7 @@ import com.raytheon.uf.edex.core.EDEXUtil;
  * Dec 11, 2012 1286       djohnson     Initial creation
  * Feb 06, 2013 1543       djohnson     Changes to correspond with EventBus changes.
  * May 28, 2013 1650       djohnson     Changes to match functionality in general event bus handling.
+ * Jul 09, 2013 2106       djohnson     No Spring required to get thread pool sizes, remove subscriptionBus.
  * 
  * </pre>
  * 
@@ -48,31 +47,25 @@ import com.raytheon.uf.edex.core.EDEXUtil;
  * @version 1.0
  */
 public class BandwidthAsyncEventBusFactory implements BandwidthEventBusFactory {
-    private static final IUFStatusHandler statusHandler = UFStatus
-            .getHandler(BandwidthAsyncEventBusFactory.class);
-
     private final AsyncEventBus dataSetBus;
-
-    private final AsyncEventBus subscriptionBus;
 
     private final AsyncEventBus retrievalBus;
 
     public BandwidthAsyncEventBusFactory() {
-        BandwidthEventBusConfig config = (BandwidthEventBusConfig) EDEXUtil
-                .getESBComponent("BandwidthEventBusConfig");
-        // If no bean was defined, use the defaults defined in the
-        // class.
-        if (config == null) {
-            statusHandler
-                    .info("No BandwidthEventBusConfig defined.  Using defaults.");
-            config = new BandwidthEventBusConfig();
-        }
-        dataSetBus = new AsyncEventBus(Executors.newFixedThreadPool(config
-                .getDataSetMetaDataPoolSize()));
-        subscriptionBus = new AsyncEventBus(Executors.newFixedThreadPool(config
-                .getSubscriptionPoolSize()));
-        retrievalBus = new AsyncEventBus(Executors.newFixedThreadPool(config
-                .getRetrievalPoolSize()));
+        BandwidthEventBusConfig config = new BandwidthEventBusConfig();
+
+        final int dataSetMetaDataPoolSize = config.getDataSetMetaDataPoolSize();
+        final int retrievalPoolSize = config.getRetrievalPoolSize();
+
+        UFStatus.getHandler(BandwidthAsyncEventBusFactory.class)
+                .info(String
+                        .format("Creating event bus with dataSetMetaDataPoolSize [%s] retrievalPoolSize [%s].",
+                                dataSetMetaDataPoolSize, retrievalPoolSize));
+
+        dataSetBus = new AsyncEventBus(
+                Executors.newFixedThreadPool(dataSetMetaDataPoolSize));
+        retrievalBus = new AsyncEventBus(
+                Executors.newFixedThreadPool(retrievalPoolSize));
     }
 
     /**
@@ -81,14 +74,6 @@ public class BandwidthAsyncEventBusFactory implements BandwidthEventBusFactory {
     @Override
     public EventBus getDataSetBus() {
         return dataSetBus;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public EventBus getSubscriptionBus() {
-        return subscriptionBus;
     }
 
     /**
@@ -104,8 +89,7 @@ public class BandwidthAsyncEventBusFactory implements BandwidthEventBusFactory {
      */
     @Override
     public List<EventBus> getEventBuses() {
-        return Arrays.<EventBus> asList(dataSetBus, retrievalBus,
-                subscriptionBus);
+        return Arrays.<EventBus> asList(dataSetBus, retrievalBus);
     }
 
 }
