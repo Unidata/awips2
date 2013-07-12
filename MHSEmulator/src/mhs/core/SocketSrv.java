3af47effc5ca7b2a000dba11b59d251ed4569d33
David Gillingham <David.Gillingham@raytheon.com>
2013-07-12 13:49:42 -0500

    Issue #2009: Implement dual-domain GFE export_grids solution. Update MHS emulator program to support new directory structure and some code cleanup and refactoring.
    Change-Id: Ib3cf5c0123cd87497517d4221edea5883db9157d
    
    Former-commit-id: 1d3f3da9cc4b8979c20800d91a357400a0a6e6d4 [formerly 0f4f24beb8172e72f3dcf23acd483ec6a4be6c69] [formerly f8b6ec20c178cf5e8f7473dbe3464931cf633756 [formerly ad7acb2d563c79d92d84ec62405b492907191af3]]
    Former-commit-id: f8b6ec20c178cf5e8f7473dbe3464931cf633756
    Former-commit-id: 751188ab5ad15c419a548b9e851982edbe3e0e20

--- MHSEmulator/src/mhs/core/SocketSrv.java
+++ MHSEmulator/src/mhs/core/SocketSrv.java
@@ -1,5 +1,6 @@
 package mhs.core;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -7,26 +8,62 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * ??? ??  ????            bphillip     Initial creation
+ * Jul 15, 2013  #2099     dgilling     Code cleanup.
+ * 
+ * </pre>
+ * 
+ * @author bphillip
+ * @version 1.0
+ */
 public class SocketSrv {
+
+    private File propertiesFile;
+
+    private boolean runRsync;
+
+    private String myMHS;
+
+    private Properties serverProps;
+
+    private ExecutorService mhsRequestHandler;
+
+    private ScheduledExecutorService rsyncThread;
 
     private int fileIndex = 0;
 
     private String fileBase;
-
-    private Properties serverProps;
 
     private String configDir;
 
@@ -38,131 +75,188 @@ public class SocketSrv {
 
     private Map<Integer, String> commandMap;
 
-    private RsyncThread rsync;
-
-    private String propertiesFile;
-
-    private String myMHS;
+    private int serverPort;
 
     public static void main(String[] args) {
-
         if (!System.getProperty("user.name").equals("root")) {
             System.out
                     .println("Socket Server must be run as root user!  Current user: "
                             + System.getProperty("user.name"));
             System.exit(1);
         }
+
+        File propertiesFile = new File(args[0]);
+        String mhsId = args[1];
+        boolean startRsync = Boolean.parseBoolean(args[2]);
+
+        if (!propertiesFile.isFile()) {
+            System.out.println("Specified properties file ["
+                    + propertiesFile.toString() + "] does not exist. Exiting.");
+            System.exit(1);
+        }
+
         try {
-            SocketSrv srv = null;
-            srv = new SocketSrv(args[0], args[1], args[2]);
-            srv.run();
+            final SocketSrv server = new SocketSrv(propertiesFile, mhsId,
+                    startRsync);
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+
+                @Override
+                public void run() {
+                    server.shutdown();
+                }
+            });
+            server.run();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private SocketSrv(String propertiesFile, String myMHS, String startRsync)
-            throws Exception {
-        this.propertiesFile = propertiesFile;
-        this.myMHS = myMHS;
-
-        writeMyMHS(myMHS);
+    private SocketSrv(File propertiesFile, String mhsId, boolean startRsync)
+            throws UnknownHostException, IOException {
         System.out.println("Setting up server ("
                 + InetAddress.getLocalHost().getCanonicalHostName() + ")");
-        commandMap = new HashMap<Integer, String>();
-        configDir = propertiesFile.substring(0,
-                propertiesFile.lastIndexOf(File.separator) + 1);
-        loadProperties(true);
-        if (startRsync.equals("true")) {
-            System.out.println("Starting Rsync Thread...");
-            rsync = new RsyncThread(serverProps);
-            rsync.start();
-            System.out.println("Rsync Thread started!");
-        }
 
-    }
+        this.propertiesFile = propertiesFile;
+        this.myMHS = mhsId;
+        this.runRsync = startRsync;
 
-    private void writeMyMHS(String myMHS) throws Exception {
-        BufferedWriter out = new BufferedWriter(new FileWriter(
-                MhsUtil.MY_MHS_FILE));
-        out.write(myMHS + "\n");
-        out.write(this.propertiesFile);
-        out.close();
-    }
+        writeMyMHS(myMHS);
 
-    private void loadProperties(boolean print) throws Exception {
-        serverProps = new Properties();
-        FileInputStream fis = new FileInputStream(propertiesFile);
-        serverProps.load(fis);
-        fis.close();
-        fileBase = serverProps.getProperty("DATA_FOLDER");
-        centralServerDir = serverProps.getProperty("CENTRAL_SERVER");
-        binDir = serverProps.getProperty("UTIL_DIR");
+        this.commandMap = new HashMap<Integer, String>();
+        this.configDir = this.propertiesFile.getParent();
+        loadProperties();
         loadRcvHandlerTable();
-        if (print) {
-            System.out.println("      Received Data directory: " + fileBase);
-            System.out.println("Central Server Data Directory: "
-                    + centralServerDir);
-            System.out.println("             Config directory: " + configDir);
+
+        this.fileBase = serverProps.getProperty("DATA_FOLDER");
+        this.centralServerDir = serverProps.getProperty("CENTRAL_SERVER");
+        this.binDir = serverProps.getProperty("UTIL_DIR");
+        this.serverPort = Integer.parseInt(serverProps
+                .getProperty("SERVER_PORT"));
+
+        System.out.println("\tReceived Data directory: " + fileBase);
+        System.out.println("\tCentral Server Data Directory: "
+                + centralServerDir);
+        System.out.println("\tConfig directory: " + configDir);
+
+        this.mhsRequestHandler = Executors.newSingleThreadExecutor();
+        if (this.runRsync) {
+            System.out.println("Starting Rsync Thread...");
+            this.rsyncThread = Executors.newSingleThreadScheduledExecutor();
         }
     }
 
-    public void run() throws Exception {
+    public void run() throws IOException {
+        Runnable rsyncJob = new RsyncThread(serverProps);
+        rsyncThread.scheduleWithFixedDelay(rsyncJob, 1, 1, TimeUnit.SECONDS);
 
-        int port = Integer.parseInt((String) serverProps.get("SERVER_PORT"));
-        ServerSocket srv = new ServerSocket(port);
+        ServerSocket socket = new ServerSocket(serverPort);
+        while (!mhsRequestHandler.isShutdown()) {
+            try {
+                log("Waiting for connections...");
+                final Socket conn = socket.accept();
+                Runnable processTask = new Runnable() {
 
-        while (true) {
-            log("Waiting for connections...");
-            Socket socket = srv.accept();
-            InetSocketAddress client = (InetSocketAddress) socket
-                    .getRemoteSocketAddress();
-            log("Connected to client: " + client.getHostName() + " at "
-                    + client);
-            loadProperties(false);
-            String sender = getMhsOfSender(client);
-            log("Message is from: " + sender);
-
-            InputStream in = socket.getInputStream();
-            byte[] message = null;
-            Map<String, String> params = new HashMap<String, String>();
-            String flag = "";
-            while (true) {
-                if (in.available() == 0) {
-                    Thread.sleep(100);
-                    continue;
-                }
-
-                message = getMessage(in);
-
-                if (message.length < 50) {
-                    String strMessage = new String(message);
-                    if (strMessage.equals(MhsUtil.END_TOKEN)) {
-                        log("Disconnected from  client: " + client);
-                        if (params.containsKey("-c")) {
-                            executeAction(sender, params);
-                            files.clear();
-                            params.clear();
-                            flag = "";
+                    @Override
+                    public void run() {
+                        try {
+                            handleRequest(conn);
+                        } catch (Exception e) {
+                            e.printStackTrace();
                         }
-                        break;
                     }
-                    if (strMessage.startsWith("-")) {
-                        flag = strMessage;
-                    } else {
-                        params.put(flag, strMessage);
-                    }
-                } else {
-                    log("File Received of size: " + message.length);
-                    files.add(writeToFile(myMHS + "-" + params.get("-MSGID"),
-                            message));
+                };
+                mhsRequestHandler.execute(processTask);
+            } catch (RejectedExecutionException e) {
+                if (!mhsRequestHandler.isShutdown()) {
+                    e.printStackTrace();
                 }
             }
         }
     }
 
+    public void shutdown() {
+        mhsRequestHandler.shutdown();
+        if (rsyncThread != null) {
+            rsyncThread.shutdown();
+        }
+    }
+
+    private void handleRequest(Socket connection) throws IOException,
+            InterruptedException {
+        InetSocketAddress client = (InetSocketAddress) connection
+                .getRemoteSocketAddress();
+        log("Connected to client: " + client.getHostName() + " at " + client);
+
+        loadProperties();
+        String sender = getMhsOfSender(client);
+        log("Message is from: " + sender);
+
+        InputStream in = connection.getInputStream();
+        byte[] message = null;
+        Map<String, String> params = new HashMap<String, String>();
+        String flag = "";
+        while (true) {
+            if (in.available() == 0) {
+                Thread.sleep(100);
+                continue;
+            }
+
+            message = getMessage(in);
+
+            if (message.length < 50) {
+                String strMessage = new String(message);
+                if (strMessage.equals(MhsUtil.END_TOKEN)) {
+                    log("Disconnected from  client: " + client);
+                    if (params.containsKey("-c")) {
+                        executeAction(sender, params);
+                        files.clear();
+                        params.clear();
+                        flag = "";
+                    }
+                    break;
+                }
+                if (strMessage.startsWith("-")) {
+                    flag = strMessage;
+                } else {
+                    params.put(flag, strMessage);
+                }
+            } else {
+                log("File Received of size: " + message.length);
+                files.add(writeToFile(myMHS + "-" + params.get("-MSGID"),
+                        message));
+            }
+        }
+    }
+
+    private void writeMyMHS(String myMHS) throws IOException {
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new FileWriter(MhsUtil.MY_MHS_FILE));
+            out.write(myMHS + "\n");
+            out.write(propertiesFile.getPath());
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+
+    private void loadProperties() throws IOException {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(propertiesFile);
+            Properties newProps = new Properties();
+            newProps.load(fis);
+            serverProps = newProps;
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+    }
+
     private void executeAction(String sender, Map<String, String> params)
-            throws Exception {
+            throws IOException, InterruptedException {
         int action = Integer.parseInt(params.get("-c"));
 
         if (!commandMap.containsKey(action)) {
@@ -200,31 +294,19 @@ public class SocketSrv {
         command = command.replace("%SENDER", sender.toLowerCase());
         String[] cmdArray = command.split(" ");
         log("Executing: " + command);
-        //
-        // Map<String, String> sysEnv = System.getenv();
-        // Map<String, String> newEnv = new HashMap<String, String>();
-        // for (String key : sysEnv.keySet()) {
-        // newEnv.put(key, sysEnv.get(key));
-        // }
-        // newEnv.put("PATH", "/awips2/python/bin/:"+sysEnv.get("PATH"));
-        // newEnv.put("LD_PRELOAD", "/awips2/python/lib/libpython2.7.so");
-        // newEnv.put("LD_LIBRARY_PATH", "/awips2/python/lib");
-        // String[] envp = new String[newEnv.keySet().size()];
-        // int i = 0;
-        // for (String key : newEnv.keySet()) {
-        // envp[i] = key.trim() + "=" + newEnv.get(key).trim();
-        // i++;
-        // }
+
         Process p = null;
         try {
             p = Runtime.getRuntime().exec(cmdArray);
             p.waitFor();
         } finally {
-            p.destroy();
+            if (p != null) {
+                p.destroy();
+            }
         }
     }
 
-    private byte[] getMessage(InputStream in) throws Exception {
+    private byte[] getMessage(InputStream in) throws IOException {
         byte[] sizeBytes = new byte[4];
         readBytes(in, sizeBytes);
         int expectedSize = MhsUtil.byteArrayToInt(sizeBytes, 0);
@@ -233,7 +315,7 @@ public class SocketSrv {
         return message;
     }
 
-    private void readBytes(InputStream in, byte[] bytes) throws Exception {
+    private void readBytes(InputStream in, byte[] bytes) throws IOException {
         int expectedSize = bytes.length;
         int bytesRead = 0;
         int totalBytesRead = 0;
@@ -245,13 +327,20 @@ public class SocketSrv {
     }
 
     private String writeToFile(String fileName, byte[] contents)
-            throws Exception {
+            throws IOException, InterruptedException {
         String fileFQN = fileBase + fileName + "." + getFileIndex();
         log("Writing file: " + fileFQN);
-        FileOutputStream fos = new FileOutputStream(new File(fileFQN));
-        fos.write(contents);
-        fos.flush();
-        fos.close();
+
+        BufferedOutputStream out = null;
+        try {
+            out = new BufferedOutputStream(new FileOutputStream(new File(
+                    fileFQN)));
+            out.write(contents);
+        } finally {
+            if (out != null) {
+                out.close();
+            }
+        }
 
         Process p = null;
         try {
@@ -259,7 +348,9 @@ public class SocketSrv {
                     new String[] { "/bin/chmod", "777", fileFQN });
             p.waitFor();
         } finally {
-            p.destroy();
+            if (p != null) {
+                p.destroy();
+            }
         }
         return fileFQN;
     }
@@ -269,40 +360,34 @@ public class SocketSrv {
         if (fileIndex == 1000) {
             fileIndex = 0;
         }
-        String fileNumber = String.valueOf(fileIndex);
-        if (fileNumber.length() == 1) {
-            fileNumber = "00" + fileNumber;
-        } else if (fileNumber.length() == 2) {
-            fileNumber = "0" + fileNumber;
-        }
-        return fileNumber;
+
+        NumberFormat formatter = new DecimalFormat("000");
+        return formatter.format(fileIndex);
     }
 
-    private void loadRcvHandlerTable() throws Exception {
+    private void loadRcvHandlerTable() throws IOException {
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(configDir
+            in = new BufferedReader(new FileReader(configDir + File.separator
                     + "rcv_handler.tbl"));
-            String str = null;
-            String[] tokens = null;
 
+            String str = null;
             while ((str = in.readLine()) != null) {
-                String command = "";
-                tokens = str.split(" ");
+                StringBuilder commandBuilder = new StringBuilder();
+                String[] tokens = str.split(" ");
                 for (int i = 1; i < tokens.length; i++) {
                     String cmd = tokens[i].trim();
                     if (!cmd.isEmpty()) {
                         if (i != 1) {
-                            command += " ";
+                            commandBuilder.append(' ');
                         }
-                        command += cmd;
+                        commandBuilder.append(cmd);
                     }
                 }
-                command = command.trim();
 
-                commandMap.put(Integer.parseInt(tokens[0]), command);
+                String commandString = commandBuilder.toString().trim();
+                commandMap.put(Integer.parseInt(tokens[0]), commandString);
             }
-            in.close();
         } finally {
             if (in != null) {
                 in.close();
@@ -311,20 +396,19 @@ public class SocketSrv {
     }
 
     private String getMhsOfSender(InetSocketAddress address) {
-
         String hostAddress = address.getAddress().getHostAddress();
-        for (Object key : serverProps.keySet()) {
-            String value = serverProps.getProperty((String) key);
+        for (String mhsId : serverProps.stringPropertyNames()) {
+            String value = serverProps.getProperty(mhsId);
             if (value.contains(",")) {
                 String[] addrs = value.split(",");
                 for (String addr : addrs) {
                     if (addr.contains(hostAddress)) {
-                        return (String) key;
+                        return mhsId;
                     }
                 }
             } else {
                 if (value.contains(hostAddress)) {
-                    return (String) key;
+                    return mhsId;
                 }
             }
         }
