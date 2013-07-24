2af2b119aeeffb4748460113a2f9ac945776e8c4
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-07-24 12:45:29 -0500

    Issue #2164 - disable build of awips2-notification temporarily.
    Former-commit-id: 5b49d9304377cc049eb180ab3e915071baa12b95 [formerly 5b49d9304377cc049eb180ab3e915071baa12b95 [formerly bedb6e5b555cbc646a0a8e1b5a1f09e1ddade0a0]] [formerly 9ae7f63e2622d0c404cdc6687365ea54ef59f8eb] [formerly dd51352ed680e51076797ea2db2732707dc397f3 [formerly dd51352ed680e51076797ea2db2732707dc397f3 [formerly cccb37ebc0a100cb15e48eca485d169098da3ef5]] [formerly 9ae7f63e2622d0c404cdc6687365ea54ef59f8eb [formerly 9ae7f63e2622d0c404cdc6687365ea54ef59f8eb [formerly 69f7d9afd3d61fe1a68b16b98d7d3a41d4fcc2c4]]]]
    Former-commit-id: dd51352ed680e51076797ea2db2732707dc397f3 [formerly dd51352ed680e51076797ea2db2732707dc397f3 [formerly cccb37ebc0a100cb15e48eca485d169098da3ef5]]
    Former-commit-id: fec5f8297e471f91ef28480dc009dd5ce754c0f7 [formerly d3aaa1ef903b3025311ea8221676867229f825ef] [formerly 59225e68d1f1a981aaeef21af1d7f8779ed23eff [formerly 779c2ea7a204bfad8663c36088ec67948cde4bd1]]
    Former-commit-id: 98f2ab236e134742357c1c66fbbd2d20e5597f70 [formerly 693a72ad6a2d61f2a4bd29bcd30b3b81909d4b58]
    Former-commit-id: c3f08b351655b9ac324f7fe6ff6a9869a885f7a1

--- rpms/build/i386/build.sh
+++ rpms/build/i386/build.sh
@@ -176,7 +176,7 @@ if [ "${1}" = "-delta" ]; then
    buildRPM "awips2-hydroapps-shared"
    buildRPM "awips2-localapps-environment"
    buildRPM "awips2-maps-database"
-   buildRPM "awips2-notification"
+   #buildRPM "awips2-notification"
    buildRPM "awips2-pypies"
    buildRPM "awips2-data.hdf5-topo"
    buildRPM "awips2-data.gfe"
