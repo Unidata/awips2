5bd0d92363277774da3d6649e793eec6e25d358b
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2013-08-05 10:51:39 -0500

    Issue #1693 Modified Submit objects to conform to 4.0 spec.  Added remote object references and some cleanup
    Change-Id: I0f4eafb3fa68f2d1eeade4f047c95d714502cf56
    
    Former-commit-id: 2aa7bdc1a8a942eaa0589527e8987b6fc518479f

--- edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ServiceBindingType.java
+++ edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ServiceBindingType.java
@@ -31,6 +31,7 @@ import javax.xml.bind.annotation.XmlType;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import com.raytheon.uf.common.registry.schemas.ebxml.util.annotations.RegistryObjectReference;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
@@ -63,12 +64,13 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 @XmlType(name = "ServiceBindingType")
 @DynamicSerialize
 @Entity
-@Cache(region="registryObjects",usage = CacheConcurrencyStrategy.TRANSACTIONAL)
+@Cache(region = "registryObjects", usage = CacheConcurrencyStrategy.TRANSACTIONAL)
 @Table(schema = "ebxml", name = "ServiceBinding")
 public class ServiceBindingType extends RegistryObjectType {
 
     @XmlAttribute
     @DynamicSerializeElement
+    @RegistryObjectReference
     protected String serviceInterface;
 
     /**
--- edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ServiceEndpointType.java
+++ edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ServiceEndpointType.java
@@ -32,6 +32,7 @@ import javax.xml.bind.annotation.XmlType;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
+import com.raytheon.uf.common.registry.schemas.ebxml.util.annotations.RegistryObjectReference;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
@@ -66,7 +67,7 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 @XmlType(name = "ServiceEndpointType")
 @DynamicSerialize
 @Entity
-@Cache(region="registryObjects",usage = CacheConcurrencyStrategy.TRANSACTIONAL)
+@Cache(region = "registryObjects", usage = CacheConcurrencyStrategy.TRANSACTIONAL)
 @Table(schema = "ebxml", name = "ServiceEndpoint")
 public class ServiceEndpointType extends RegistryObjectType {
 
@@ -77,6 +78,7 @@ public class ServiceEndpointType extends RegistryObjectType {
 
     @XmlAttribute
     @DynamicSerializeElement
+    @RegistryObjectReference
     protected String serviceBinding;
 
     /**
