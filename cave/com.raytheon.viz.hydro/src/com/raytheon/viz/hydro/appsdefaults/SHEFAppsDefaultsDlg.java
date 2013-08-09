b95a96f4c960c683e5f0f906081d8f20699c8ad3
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-08-09 14:24:49 -0500

    Issue #2033 Moved some localization files from build.cave to plugins they are used in.  Fixed CAVELocalizationAdapter to search all plugins when file not found in base etc folder for cave static base
    Change-Id: I2a26ce21ca5127ddb0b32e1f645a544e83f5503f
    
    Former-commit-id: d79cfe8ad16860755adbde521dda52a36d87f514 [formerly 5c707a35dcaedf5ae37119e56503aa8af59631fc]
    Former-commit-id: 0f3c7911205fbbe62c6f4ce6546f501cd5d03e18

--- cave/com.raytheon.viz.hydro/src/com/raytheon/viz/hydro/appsdefaults/SHEFAppsDefaultsDlg.java
+++ cave/com.raytheon.viz.hydro/src/com/raytheon/viz/hydro/appsdefaults/SHEFAppsDefaultsDlg.java
@@ -55,8 +55,9 @@ import com.raytheon.viz.ui.dialogs.CaveSWTDialog;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Jan 26, 2011            mpduff     Initial creation
+ * Jan 26, 2011            mpduff      Initial creation
  * Dec 07, 2012 1353       rferrel     Make non-blocking dialog.
+ * Aug 09, 2013  2033      mschenke    Switched File.separator to IPathManager.SEPARATOR
  * 
  * </pre>
  * 
@@ -68,7 +69,7 @@ public class SHEFAppsDefaultsDlg extends CaveSWTDialog {
     private final IUFStatusHandler statusHandler = UFStatus
             .getHandler(SHEFAppsDefaultsDlg.class);
 
-    private final String CONFIG_FILE_NAME = "hydro" + File.separatorChar
+    private final String CONFIG_FILE_NAME = "hydro" + IPathManager.SEPARATOR
             + "shefGadTokens.xml";
 
     /**
