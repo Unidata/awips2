2bd2c2c30888d9ed069395a64e35b399fc6cac64
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-08-12 15:41:39 -0500

    Issue #2142 more removing of dependencies from common plugins to edex.common
    Change-Id: I3ccb07227ef6adef233898e7fae6678ea4d18c7f
    
    Former-commit-id: 5b76502b482e132b0520653a4352146b68cec097 [formerly f5ed25574e08479e33cea70aa5fd8ea646b5c967] [formerly f5ed25574e08479e33cea70aa5fd8ea646b5c967 [formerly 23802cfe0282eaf00647ca191f3694be69aadd32]] [formerly 0dbce743f6d53e8d541db0c0a2afee1bfadebe5e [formerly 0dbce743f6d53e8d541db0c0a2afee1bfadebe5e [formerly 28c79d0a56097f9c4c11e682bf88f5c5160a026e]]] [formerly 5b76502b482e132b0520653a4352146b68cec097 [formerly f5ed25574e08479e33cea70aa5fd8ea646b5c967] [formerly f5ed25574e08479e33cea70aa5fd8ea646b5c967 [formerly 23802cfe0282eaf00647ca191f3694be69aadd32]] [formerly 0dbce743f6d53e8d541db0c0a2afee1bfadebe5e [formerly 0dbce743f6d53e8d541db0c0a2afee1bfadebe5e [formerly 28c79d0a56097f9c4c11e682bf88f5c5160a026e]]] [formerly 4b2ed4345047c4cea5440502803f28fb2efe0e44 [formerly 4b2ed4345047c4cea5440502803f28fb2efe0e44 [formerly c2bf2163d93c7008eb5a5fb4a80c368b392dd27b]] [formerly 0dbce743f6d53e8d541db0c0a2afee1bfadebe5e [formerly 28c79d0a56097f9c4c11e682bf88f5c5160a026e] [formerly 28c79d0a56097f9c4c11e682bf88f5c5160a026e [formerly 2342170ce4cf51f12759afa26fc5af46f35c6188]]]]]
    Former-commit-id: 4b2ed4345047c4cea5440502803f28fb2efe0e44 [formerly 4b2ed4345047c4cea5440502803f28fb2efe0e44 [formerly c2bf2163d93c7008eb5a5fb4a80c368b392dd27b]]
    Former-commit-id: d4b24833941831d52a249e3b57a4b9b39229bce1 [formerly 2915301ab1145da7d8a7ecc14e2b672ab11be125] [formerly 932cd9a006ea1a86443b8aa3c712c07e0a708fd9 [formerly 2117ec71c668edc0c08b538fa59c3d3527a71581]] [formerly 60fcccc4f36aab6f81a5e65a62ea815889a8ba8f [formerly 4f20e4f94221a0177c6a483d1e4f98263bd9723c] [formerly 7e5fd2a94e70c93704d2a357da3311a16f36847c [formerly 018845db8f37e9c287f79d9c6f2d08f55a823b1a]]]
    Former-commit-id: 4bea47cc10b97134c6a35b456244859b0a8d457a [formerly 7f79cb561b5e56066159387411134bb0ad6cef8a] [formerly 8ffa0f194f931e35a2126a1aee27038400e1f260 [formerly f0a8895a98c79a03b03fd1e8ac39e4248e5acb06]]
    Former-commit-id: b683c7ef4eec1429407f444b0fedd2b206a109f7 [formerly 3801d84a2f758026c0919edc5a97a63b54928e70]
    Former-commit-id: 6411fa61141c3b8f7bc00e35b18bed1ef398158b

--- edexOsgi/com.raytheon.uf.edex.plugin.bufrncwf/src/com/raytheon/uf/edex/plugin/bufrncwf/BUFRncwfDao.java
+++ edexOsgi/com.raytheon.uf.edex.plugin.bufrncwf/src/com/raytheon/uf/edex/plugin/bufrncwf/BUFRncwfDao.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.common.dataplugin.ncwf.dao;
+package com.raytheon.uf.edex.plugin.bufrncwf;
 
 import java.util.List;
 
