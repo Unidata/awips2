4b1f65c7e83d5660a6150548408978c44d433774
Steve Harris <Steven_L_Harris@raytheon.com>
2013-08-22 09:12:43 -0400

    13.5.1-18 baseline
    Former-commit-id: 2d939d61142d709b7b9dbb77f442b17d0b5a95ed [formerly 2d939d61142d709b7b9dbb77f442b17d0b5a95ed [formerly d931b80e70a0327f4eb616e2fc31401e3bba149c]] [formerly afb34814ddeaee0f04a73ae9908458d3bb479403] [formerly 2f9aa87a9ffde7f03866a6228e51e118c14034e7 [formerly 2f9aa87a9ffde7f03866a6228e51e118c14034e7 [formerly 0a7acf96c7a891f83cd998888bb9b2f41292e293]] [formerly afb34814ddeaee0f04a73ae9908458d3bb479403 [formerly afb34814ddeaee0f04a73ae9908458d3bb479403 [formerly f407ad6db2f621a5be95939aea88f49d874d1ab6]]]]
    Former-commit-id: 2f9aa87a9ffde7f03866a6228e51e118c14034e7 [formerly 2f9aa87a9ffde7f03866a6228e51e118c14034e7 [formerly 0a7acf96c7a891f83cd998888bb9b2f41292e293]]
    Former-commit-id: 2f9aa87a9ffde7f03866a6228e51e118c14034e7
    Former-commit-id: fc0aa2f3b2539969cc27c03bc61d627a81a30449 [formerly 261b1da9d63de8664cbc9f4078a532554f362abb]
    Former-commit-id: c064014b50689e2f75984510137adcbb520ddbb4

--- rpms/awips2.core/Installer.notification/scripts/profile.d/awips2Notification.sh
+++ rpms/awips2.core/Installer.notification/scripts/profile.d/awips2Notification.sh
@@ -2,23 +2,30 @@
 
 # Is awips2-notification Installed?
 rpm -q awips2-notification > /dev/null 2>&1
-RC=$?
-if [ ${RC} -ne 0 ]; then
+if [ $? -ne 0 ]; then
    return
 fi
 
 # Determine Where awips2-notification Has Been Installed.
 NOTIFICATION_INSTALL="/awips2/notification"
-if [ "${NOTIFICATION_INSTALL}" = "" ]; then
-   return
-fi
+QPID_LIB_DIR="/awips2/qpid/lib"
 
 # Update The Environment.
 # Determine if awips2-notification is Already On LD_LIBRARY_PATH
 CHECK_PATH=`echo ${LD_LIBRARY_PATH} | grep ${NOTIFICATION_INSTALL}`
 if [ "${CHECK_PATH}" = "" ]; then
    # awips2-notification Is Not On LD_LIBRARY_PATH; Add It.
-   export LD_LIBRARY_PATH=${NOTIFICATION_INSTALL}/lib:${LD_LIBRARY_PATH}
+   _lib_dir=${NOTIFICATION_INSTALL}/lib
+   if [ -d ${NOTIFICATION_INSTALL}/lib64 ]; then
+      _lib_dir=${NOTIFICATION_INSTALL}/lib64
+   fi
+   export LD_LIBRARY_PATH=${_lib_dir}:${LD_LIBRARY_PATH}
+fi
+
+# Determine if the qpid lib directory is already on LD_LIBRARY_PATH
+CHECK_PATH=`echo ${LD_LIBRARY_PATH} | grep ${QPID_LIB_DIR}`
+if [ "${CHECK_PATH}" = "" ]; then
+   export LD_LIBRARY_PATH=${QPID_LIB_DIR}:$LD_LIBRARY_PATH
 fi
 
 # Determine if awips2-notification Is Already Part Of The Path.
