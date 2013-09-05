617bb96846e79e057545080d9e0316dd781fbffc
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2013-09-05 12:56:07 -0500

    Issue #2316 Better upgrade script and feature files. Change-Id: Ia86dce4fb73893a63ab35003eef8341cbfb0cd4b
    Former-commit-id: 39ceb2eeb58510f5d77a39b13ceef44c2bf438ee [formerly af8fbacf72edbb7affb3207a2951041feb60c508] [formerly 72728121e42a7a05f5b2ab8921b845d4156db7d7 [formerly 7855257369af383399d6cc2ea94d743e139263d2]]
    Former-commit-id: 72728121e42a7a05f5b2ab8921b845d4156db7d7
    Former-commit-id: c09b38e5b56066a44931708ac84f68bfb56f8402

--- deltaScripts/14.2.1/dropPirepAirep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# DR #2316,2317 drop pirep,ncpirep,airep,ncairep
-
-PSQL="/awips2/psql/bin/psql"
-
-${PSQL} -U awips -d metadata -c "delete from plugin_info where name in ('pirep','ncpirep','airep','ncairep');"
-${PSQL} -U awips -d metadata -c "drop table pirep, pirep_anc_data, ncpirep, ncpirep_anc_data, airep, ncairep;"
