3d0b711ccf1b0873e0d1217179956dc6fbbd6cdb
Steve Harris <Steven_L_Harris@raytheon.com>
2013-09-12 11:13:59 -0400

    13.5.2-4 baseline
    Former-commit-id: 58c682142755d86feefc08200ab4f7be2bc6b0aa [formerly e3c935d48bb3918e3c1c1b35dae84257260a5565 [formerly da631849bc9eaaa5c83c2393520b6099413107a5]]
    Former-commit-id: e3c935d48bb3918e3c1c1b35dae84257260a5565
    Former-commit-id: 595e7bc0d9bf23535efae812bedab78d3a4e2563

---
+++ pythonPackages/dynamicserialize/dstypes/gov/noaa/nws/ncep/common/dataplugin/gempak/request/GetTimesResponse.py
@@ -0,0 +1,14 @@
+
+# File auto-generated against equivalent DynamicSerialize Java class
+
+class GetTimesResponse(object):
+
+    def __init__(self):
+        self.times = None
+
+    def getTimes(self):
+        return self.times
+
+    def setTimes(self, times):
+        self.times = times
+
