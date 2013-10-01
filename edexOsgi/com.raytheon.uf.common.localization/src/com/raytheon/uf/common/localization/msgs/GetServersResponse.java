006a97cb22792eaef529e20262173b759e3045e3
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-10-01 17:05:24 -0500

    Issue #2361 remove XML annotations from localization requests/responses
    Change-Id: Ia2aa791f7e68524d19f24522c7824a977d25d58f
    
    Former-commit-id: 1810a8cf6d37aea48666f7bb133008617ca16c62 [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df] [formerly dd75847b7b2b2a4a674fc049fc5a81dd28a2c29a] [formerly 396e69180da9cc58926fd7027845b8790468bb7a [formerly dd75847b7b2b2a4a674fc049fc5a81dd28a2c29a [formerly ef569f59ca91c73cfbfad4b062fb0110d33533d3]]]
    Former-commit-id: 396e69180da9cc58926fd7027845b8790468bb7a
    Former-commit-id: 65d1242b3cdd63e3ebb6a298f4c1b96d6ee987a6 [formerly 3b92de5abc2d6e712a403058d21f4c7d8c59ac95]
    Former-commit-id: 28ce7782fe115e7d5c8add93ade7e92aa42abe5e

--- edexOsgi/com.raytheon.uf.common.localization/src/com/raytheon/uf/common/localization/msgs/GetServersResponse.java
+++ edexOsgi/com.raytheon.uf.common.localization/src/com/raytheon/uf/common/localization/msgs/GetServersResponse.java
@@ -21,7 +21,6 @@ package com.raytheon.uf.common.localization.msgs;
 
 import java.util.Map;
 
-import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
@@ -44,7 +43,7 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
  * @version 1.0
  */
 @DynamicSerialize
-public class GetServersResponse implements ISerializableObject {
+public class GetServersResponse {
 
     @DynamicSerializeElement
     private String httpServer;
