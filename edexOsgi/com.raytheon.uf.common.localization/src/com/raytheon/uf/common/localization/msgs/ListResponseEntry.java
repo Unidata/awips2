82651e0b23fee95585a9950f402311fc7e5dcfa8
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-10-01 17:05:24 -0500

    Issue #2361 remove XML annotations from localization requests/responses
    Change-Id: Ia2aa791f7e68524d19f24522c7824a977d25d58f
    
    Former-commit-id: 2e19bdae5bae8f624044732e802a96a8e6f5df55 [formerly 1810a8cf6d37aea48666f7bb133008617ca16c62] [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df] [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df [formerly dd75847b7b2b2a4a674fc049fc5a81dd28a2c29a]] [formerly 2e19bdae5bae8f624044732e802a96a8e6f5df55 [formerly 1810a8cf6d37aea48666f7bb133008617ca16c62] [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df] [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df [formerly dd75847b7b2b2a4a674fc049fc5a81dd28a2c29a]] [formerly 396e69180da9cc58926fd7027845b8790468bb7a [formerly 8e17babd91c5e884aa07dad6f74b672f46eeb1df [formerly dd75847b7b2b2a4a674fc049fc5a81dd28a2c29a] [formerly 396e69180da9cc58926fd7027845b8790468bb7a [formerly ef569f59ca91c73cfbfad4b062fb0110d33533d3]]]]]
    Former-commit-id: 396e69180da9cc58926fd7027845b8790468bb7a
    Former-commit-id: 7b45029a6ae2baa56589d810979f0e49d8f1255d [formerly 006a97cb22792eaef529e20262173b759e3045e3] [formerly 28ce7782fe115e7d5c8add93ade7e92aa42abe5e] [formerly e576a7f11bd504baecc469a300d555bfb9d79780 [formerly 65d1242b3cdd63e3ebb6a298f4c1b96d6ee987a6] [formerly 28ce7782fe115e7d5c8add93ade7e92aa42abe5e [formerly 3b92de5abc2d6e712a403058d21f4c7d8c59ac95]]]
    Former-commit-id: 5015bda0778ea19931583e03214b957d2698a594 [formerly ecd0f27815ae60d6978e1f0ba0fff1be6371fcde] [formerly c18e9ee060ba19669f5f6412bfbf19e9882306cd [formerly 38edfa0d07fd708b7e3f814bd84b48e0431a103f]]
    Former-commit-id: c18e9ee060ba19669f5f6412bfbf19e9882306cd
    Former-commit-id: bd680298a8e54738af12a2940e9a1bddb7f27516

--- edexOsgi/com.raytheon.uf.common.localization/src/com/raytheon/uf/common/localization/msgs/ListResponseEntry.java
+++ edexOsgi/com.raytheon.uf.common.localization/src/com/raytheon/uf/common/localization/msgs/ListResponseEntry.java
@@ -22,15 +22,8 @@ package com.raytheon.uf.common.localization.msgs;
 
 import java.util.Date;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import com.raytheon.uf.common.localization.LocalizationContext;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationLevel;
-import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
@@ -44,42 +37,35 @@ import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
  * ------------ ---------- ----------- --------------------------
  * Apr 19, 2007            chammack    Initial Creation.
  * Aug 22, 2008  #1502     bclement    Added JAXB/Serializable annotations
+ * Oct 01, 2013   2361     njensen     Removed XML annotations
  * 
  * </pre>
  * 
  * @author chammack
  * @version 1.0
  */
-@XmlRootElement
-@XmlAccessorType(XmlAccessType.NONE)
-@DynamicSerialize
-public class ListResponseEntry implements ISerializableObject {
 
-    @XmlAttribute
+@DynamicSerialize
+public class ListResponseEntry {
+
     @DynamicSerializeElement
     protected String fileName;
 
-    @XmlElement
     @DynamicSerializeElement
     protected LocalizationContext context;
 
-    @XmlAttribute
     @DynamicSerializeElement
     protected Date date;
 
-    @XmlAttribute
     @DynamicSerializeElement
     protected String checksum;
 
-    @XmlAttribute
     @DynamicSerializeElement
     protected boolean directory;
 
-    @XmlAttribute
     @DynamicSerializeElement
     protected LocalizationLevel protectedLevel;
 
-    @XmlAttribute
     @DynamicSerializeElement
     protected boolean existsOnServer;
 
