c2471fd381b0f79afec1dcda1203c6e9953ba17d
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-10-10 13:50:45 -0500

    Issue #2104 Fixed EnvelopeIntersection issue with inverted enveloped.  Fixed various bugs that prevented proper testing of XML file attached to 2104.
    Amend: Accepted ticket, cleanup, comments
    
    Change-Id: I78649dd0268b030624888544740a8d7af3e83e97
    
    Former-commit-id: 01c0e022d5ea3fe58c75387cacd047d400175d0f [formerly 7a66a3c9952c12c9ec20e00fdf64953b7e395270 [formerly fb196f5b8659e568ea1406cc5c5e01aa82525a76]]
    Former-commit-id: 7a66a3c9952c12c9ec20e00fdf64953b7e395270
    Former-commit-id: b64050ff1e6eab51f3dbca080209f0373adeb72c

--- cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/map/actions/ScaleHandler.java
+++ cave/com.raytheon.uf.viz.d2d.ui/src/com/raytheon/uf/viz/d2d/ui/map/actions/ScaleHandler.java
@@ -20,31 +20,19 @@
 
 package com.raytheon.uf.viz.d2d.ui.map.actions;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-import com.raytheon.uf.common.serialization.SerializationException;
-import com.raytheon.uf.common.serialization.SerializationUtil;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
 import com.raytheon.uf.viz.core.IDisplayPane;
 import com.raytheon.uf.viz.core.IDisplayPaneContainer;
-import com.raytheon.uf.viz.core.drawables.AbstractRenderableDisplay;
-import com.raytheon.uf.viz.core.drawables.ResourcePair;
-import com.raytheon.uf.viz.core.exception.VizException;
+import com.raytheon.uf.viz.core.drawables.IRenderableDisplay;
 import com.raytheon.uf.viz.core.globals.VizGlobalsManager;
-import com.raytheon.uf.viz.core.map.IMapDescriptor;
 import com.raytheon.uf.viz.core.maps.actions.NewMapEditor;
-import com.raytheon.uf.viz.core.maps.scales.MapScales;
-import com.raytheon.uf.viz.core.maps.scales.MapScales.MapScale;
-import com.raytheon.uf.viz.core.procedures.Bundle;
-import com.raytheon.uf.viz.d2d.core.map.D2DMapRenderableDisplay;
+import com.raytheon.uf.viz.core.maps.scales.IMapScaleDisplay;
 import com.raytheon.viz.ui.EditorUtil;
 
 /**
@@ -57,6 +45,7 @@ import com.raytheon.viz.ui.EditorUtil;
  * Jul 24, 2007             randerso    Initial Creation.
  * Oct 21, 2008   #1450     randerso    Fixed to support multipane editors
  * Mar 21, 2013       1638  mschenke    Changed map scales not tied to d2d
+ * Oct  9, 2013       2104  mschenke    Switched to use logic in the IMapScaleDisplay
  * 
  * </pre>
  * 
@@ -107,92 +96,14 @@ public class ScaleHandler extends AbstractHandler {
             statusHandler.handle(Priority.PROBLEM,
                     "Could not set scale on null editor");
         }
-        MapScale mapScale = MapScales.getInstance().getScaleByName(scale);
-        if (mapScale == null) {
-            statusHandler.handle(Priority.PROBLEM,
-                    "Could not find scale with name: " + scale);
-        } else {
-            File scaleBundle = mapScale.getFile();
-            if (scaleBundle == null || scaleBundle.exists() == false
-                    || scaleBundle.isFile() == false) {
-                statusHandler.handle(Priority.PROBLEM,
-                        "Could not find scale file: " + mapScale.getFileName());
-            } else {
-                try {
-                    Object obj = SerializationUtil
-                            .jaxbUnmarshalFromXmlFile(scaleBundle);
-                    if (obj instanceof Bundle == false) {
-                        throw new SerializationException(
-                                "Deserialized object is not of type "
-                                        + Bundle.class);
-                    }
 
-                    Bundle b = (Bundle) obj;
-                    AbstractRenderableDisplay[] displays = b.getDisplays();
-                    if (displays.length != 1) {
-                        throw new VizException(
-                                "Scale bundle must have exactly 1 renderable display, "
-                                        + scaleBundle.getAbsolutePath()
-                                        + " had " + displays.length);
-                    }
-                    if (displays[0] instanceof D2DMapRenderableDisplay == false) {
-                        throw new VizException("Bundle, "
-                                + scaleBundle.getAbsolutePath()
-                                + ", is not a valid D2D map scale bundle");
-                    }
-
-                    D2DMapRenderableDisplay rd = (D2DMapRenderableDisplay) displays[0];
-
-                    for (IDisplayPane pane : editor.getDisplayPanes()) {
-                        D2DMapRenderableDisplay existingDisplay = (D2DMapRenderableDisplay) pane
-                                .getRenderableDisplay();
-                        IMapDescriptor existingMD = existingDisplay
-                                .getDescriptor();
-
-                        // set the projection
-                        existingMD.setGridGeometry(rd.getDescriptor()
-                                .getGridGeometry());
-                        pane.setZoomLevel(1.0f);
-                        pane.scaleToClientArea();
-
-                        // set the scale name
-                        existingDisplay.setScale(mapScale.getDisplayName());
-
-                        // remove the existing map resources
-                        List<ResourcePair> remove = new ArrayList<ResourcePair>();
-                        for (ResourcePair rp : existingMD.getResourceList()) {
-                            // if resource is not a map layer add it to the new
-                            // md
-                            if (rp.getProperties().isMapLayer()) {
-                                remove.add(rp);
-                            }
-                        }
-                        existingMD.getResourceList().removeAll(remove);
-
-                        // add the new map resources
-                        for (ResourcePair rp : rd.getDescriptor()
-                                .getResourceList()) {
-                            if (rp.getProperties().isMapLayer()) {
-                                if (rp.getResource() == null) {
-                                    rp.instantiateResource(existingMD);
-                                }
-                                existingMD.getResourceList().add(rp);
-                            }
-                        }
-                    }
-
-                    VizGlobalsManager.getCurrentInstance().updateUI(editor);
-                } catch (SerializationException e) {
-                    statusHandler.handle(
-                            Priority.PROBLEM,
-                            "Error deserializing file: "
-                                    + scaleBundle.getAbsolutePath(), e);
-                } catch (VizException e) {
-                    statusHandler.handle(Priority.PROBLEM,
-                            "Error loading scale bundle", e);
-                }
+        for (IDisplayPane pane : editor.getDisplayPanes()) {
+            IRenderableDisplay display = pane.getRenderableDisplay();
+            if (display instanceof IMapScaleDisplay) {
+                ((IMapScaleDisplay) display).changeScale(scale);
             }
         }
-    }
 
+        VizGlobalsManager.getCurrentInstance().updateUI(editor);
+    }
 }
