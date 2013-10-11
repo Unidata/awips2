e7ed45f3c7b981d4bd356dd5550ab0d2813fc322
Max Schenkelberg <max.schenkelberg@raytheon.com>
2013-10-11 16:17:52 -0500

    Issue #2333 Removed more unused fields from satellite record
    Change-Id: I02ec3ff328606673d717c31e583c2679389e87ec
    
    Former-commit-id: ffec5061a028f916cee186128044e28efb3c8044 [formerly ffec5061a028f916cee186128044e28efb3c8044 [formerly 3c2492cdbe798a8446af4181c90a64674bf3db27]] [formerly b68b8de21b0fc8785da8147a5d2a6cbf5889d7da] [formerly ffec5061a028f916cee186128044e28efb3c8044 [formerly ffec5061a028f916cee186128044e28efb3c8044 [formerly 3c2492cdbe798a8446af4181c90a64674bf3db27]] [formerly b68b8de21b0fc8785da8147a5d2a6cbf5889d7da] [formerly 1a2747d4df9a94034f15249b0f9159970a35d7fb [formerly 1a2747d4df9a94034f15249b0f9159970a35d7fb [formerly 8fddd5a57bd4669f7004aa5b968ee1c2d12cde26]] [formerly b68b8de21b0fc8785da8147a5d2a6cbf5889d7da [formerly b68b8de21b0fc8785da8147a5d2a6cbf5889d7da [formerly 91c5da670e6b23814532b473c93237744398369d]]]]]
    Former-commit-id: 1a2747d4df9a94034f15249b0f9159970a35d7fb [formerly 1a2747d4df9a94034f15249b0f9159970a35d7fb [formerly 8fddd5a57bd4669f7004aa5b968ee1c2d12cde26]]
    Former-commit-id: 5c841a9d635606b6109817054f92264497b86663 [formerly 3e9ecc49835e952df1518b869cbf51bcc85f39fd] [formerly 971d4b4af05d4e803df2c50258aadf5933199f51 [formerly 9bd27e3c4394d4a783741dc597030078e642f4a5]] [formerly 088d751bfd5783fdf5636e4eb0b7c30fade77b9d [formerly b3c344270a64c972da99cca1b7b167eaf3bcec73] [formerly d97d26194aee2397849bdea48ab279622c871dc7 [formerly 3b53d18be440118a5e0b52602827090b99a3a337]]]
    Former-commit-id: 1c4b10a180108e7ca57c365ca1b610532b098c01 [formerly ec0c3a61f902ea932af94ba574696fd24bd7dfbd] [formerly 7eb2ebd2334ba066c083e5f083ab9e9f2b7f2e73 [formerly 67491d2af062906cbf123037202a7fea4d1b3a2c]]
    Former-commit-id: af75d5fdbd46c0e64d8e3b48a4a33fab55d20a91 [formerly 2bc44908ff9e702438f4291eff86beed741e24ef]
    Former-commit-id: e9828edbaa8ad3bb6d164a32692e06f9e344e136

--- deltaScripts/14.2.1/satellite_spatial_update/update_satellite_spatial.sh
+++ deltaScripts/14.2.1/satellite_spatial_update/update_satellite_spatial.sh
@@ -8,7 +8,7 @@ PSQL="/awips2/psql/bin/psql"
 JAVA="/awips2/java/bin/java"
 
 # Update columns on tables for satellite
-${PSQL} -U awips -d metadata -c "ALTER TABLE satellite DROP COLUMN upperrightlat, DROP COLUMN upperrightlon;"
+${PSQL} -U awips -d metadata -c "ALTER TABLE satellite DROP COLUMN upperrightlat, DROP COLUMN upperrightlon, DROP COLUMN sizerecords, DROP COLUMN numrecords;"
 ${PSQL} -U awips -d metadata -c "ALTER TABLE satellite_spatial DROP COLUMN la1, DROP COLUMN la2, DROP COLUMN latin, DROP COLUMN lo1, DROP COLUMN lo2, DROP COLUMN lov;"
 ${PSQL} -U awips -d metadata -c "ALTER TABLE satellite_spatial ADD COLUMN minx double precision, ADD COLUMN miny double precision;"
 
