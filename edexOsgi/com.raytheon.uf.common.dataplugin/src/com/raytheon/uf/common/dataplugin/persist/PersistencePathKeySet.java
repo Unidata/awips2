02d26d0f84c3350d9901eb21fef55e64e288dc3b
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-11-08 12:30:39 -0600

    Issue #2361 remove ISerializableObject from persistence path keys
    Change-Id: I7ff7e5234c7c88ed475014c755f3df7d1ee23b16
    
    Former-commit-id: 044f4fd87a83d6b83eef7f46854f1b484536c825

--- edexOsgi/com.raytheon.uf.common.dataplugin/src/com/raytheon/uf/common/dataplugin/persist/PersistencePathKeySet.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin/src/com/raytheon/uf/common/dataplugin/persist/PersistencePathKeySet.java
@@ -1,3 +1,22 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.uf.common.dataplugin.persist;
 
 import java.util.List;
@@ -8,11 +27,23 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import com.raytheon.uf.common.serialization.ISerializableObject;
-
+/**
+ * Set of PersistencePathKeys
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * --/--/----                          Initial creation
+ * Nov 08, 2013 2361       njensen     Remove ISerializableObject
+ * 
+ * </pre>
+ * 
+ */
 @XmlRootElement(name = "pathKeySet")
 @XmlAccessorType(XmlAccessType.NONE)
-public class PersistencePathKeySet implements ISerializableObject{
+public class PersistencePathKeySet {
 
     @XmlElements({ @XmlElement(name = "pathKey", type = PersistencePathKey.class) })
     private List<PersistencePathKey> pathKeys;
