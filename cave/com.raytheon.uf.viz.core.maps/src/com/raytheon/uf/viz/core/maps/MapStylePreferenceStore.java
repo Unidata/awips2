49ba1585d6590d14fa6b57e7ec6d059ce943e783
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-11-08 10:38:11 -0600

    Issue #2361 fix map style preferences
    Change-Id: I34f4dd16a1c10196e4d8b0e32aab9fd3cefe7049
    
    Former-commit-id: 6bf408ef0ceae8134d1a7c3be6f6da9861a8de43 [formerly 6bf408ef0ceae8134d1a7c3be6f6da9861a8de43 [formerly 058c963aa201bbafc7ca115ee82de3e075663f42]] [formerly ec319b7dd081efd48459af8d1a37e6fbbd532e8a] [formerly 6bf408ef0ceae8134d1a7c3be6f6da9861a8de43 [formerly 6bf408ef0ceae8134d1a7c3be6f6da9861a8de43 [formerly 058c963aa201bbafc7ca115ee82de3e075663f42]] [formerly ec319b7dd081efd48459af8d1a37e6fbbd532e8a] [formerly 959ba04607aa082807518876c4dffee1d28ea5a7 [formerly 959ba04607aa082807518876c4dffee1d28ea5a7 [formerly 43bbf3134763e8e0182b11a7dcd149e3d0932c0d]] [formerly ec319b7dd081efd48459af8d1a37e6fbbd532e8a [formerly ec319b7dd081efd48459af8d1a37e6fbbd532e8a [formerly 4a3ae6311f17e56d9ba095f80f5c6b6c2529c5ae]]]]]
    Former-commit-id: 959ba04607aa082807518876c4dffee1d28ea5a7 [formerly 959ba04607aa082807518876c4dffee1d28ea5a7 [formerly 43bbf3134763e8e0182b11a7dcd149e3d0932c0d]]
    Former-commit-id: a82a5bec1ea36b8960ae1e82110f388c080dec23 [formerly dd76997ee48eb7ca81f4c5192b817d1db404d199] [formerly 219b8c33e29c3b29a17c04e46bb0c0e3fdcb99d3 [formerly 8a27263b0c9a20838d02d6f1df74f89d734f2204]] [formerly 1c556d12bb911c07ba42a542c159b35348e921ba [formerly 7869e95c9484a4de48a90c424c55b23293c961ba] [formerly 43804dadeb27c30a7249d1ad699909102fc84bea [formerly 5ecde78b028ffb833c5a09162c1021020bbf5041]]]
    Former-commit-id: 300b380b04446ad5efd442fb2e212aa777648157 [formerly 3a203180ea715949e077a54ddef3c3e219c20c8d] [formerly dde648330f3a0086e8233ce06ecd244cf9522c53 [formerly f836fe065e04802e8b47f3510796dcd346cac25b]]
    Former-commit-id: 0f3203a9b8240d3fe8dd2280d7a156d8c596dd12 [formerly 7ce7a168eca0ace73ca4705528f05e36f9404cb8]
    Former-commit-id: 780c717ddbedf9e5f91bb0aa41a995fc25ea614f

--- cave/com.raytheon.uf.viz.core.maps/src/com/raytheon/uf/viz/core/maps/MapStylePreferenceStore.java
+++ cave/com.raytheon.uf.viz.core.maps/src/com/raytheon/uf/viz/core/maps/MapStylePreferenceStore.java
@@ -20,10 +20,12 @@
 package com.raytheon.uf.viz.core.maps;
 
 import java.io.File;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -37,12 +39,13 @@ import com.raytheon.uf.common.localization.LocalizationContext.LocalizationType;
 import com.raytheon.uf.common.localization.LocalizationFile;
 import com.raytheon.uf.common.localization.PathManagerFactory;
 import com.raytheon.uf.common.localization.exception.LocalizationException;
-import com.raytheon.uf.common.serialization.ISerializableObject;
-import com.raytheon.uf.common.serialization.SerializationException;
-import com.raytheon.uf.common.serialization.SerializationUtil;
+import com.raytheon.uf.common.serialization.JAXBManager;
+import com.raytheon.uf.common.serialization.jaxb.JAXBClassLocator;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.viz.core.reflect.SubClassLocator;
+import com.raytheon.uf.viz.core.rsc.capabilities.AbstractCapability;
 import com.raytheon.uf.viz.core.rsc.capabilities.Capabilities;
 
 /**
@@ -53,10 +56,11 @@ import com.raytheon.uf.viz.core.rsc.capabilities.Capabilities;
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * May 7, 2010            randerso     Initial creation
+ * May 7, 2010             randerso    Initial creation
  * Jan 25, 2013 DR 15649   D. Friedman Clone capabilities in get/put.
  *                                     Stored preferences in a sub-directory
  *                                     and observe changes.
+ * Nov 08, 2013 2361       njensen     Use JAXBManager for XML
  * 
  * </pre>
  * 
@@ -66,13 +70,15 @@ import com.raytheon.uf.viz.core.rsc.capabilities.Capabilities;
 
 @XmlRootElement()
 @XmlAccessorType(XmlAccessType.FIELD)
-public class MapStylePreferenceStore implements ISerializableObject {
+public class MapStylePreferenceStore {
     private static final transient IUFStatusHandler statusHandler = UFStatus
             .getHandler(MapStylePreferenceStore.class);
 
     private static final String MAPSTYLE_FILENAME = "mapStyles/mapstylepreferences.xml";
 
     private static final String OLD_MAPSTYLE_FILENAME = "mapstylepreferences.xml";
+
+    private static JAXBManager jaxb;
 
     private static class MapStylePreferenceKey {
         private String perspective;
@@ -195,13 +201,13 @@ public class MapStylePreferenceStore implements ISerializableObject {
         IPathManager pathMgr = PathManagerFactory.getPathManager();
 
         if (siteLf == null) {
-            siteLf = pathMgr.getLocalizationFile(pathMgr
-                    .getContext(LocalizationType.CAVE_STATIC,
-                            LocalizationLevel.SITE), MAPSTYLE_FILENAME);
+            siteLf = pathMgr.getLocalizationFile(pathMgr.getContext(
+                    LocalizationType.CAVE_STATIC, LocalizationLevel.SITE),
+                    MAPSTYLE_FILENAME);
 
-            userLf = pathMgr.getLocalizationFile(pathMgr
-                    .getContext(LocalizationType.CAVE_STATIC,
-                            LocalizationLevel.USER), MAPSTYLE_FILENAME);
+            userLf = pathMgr.getLocalizationFile(pathMgr.getContext(
+                    LocalizationType.CAVE_STATIC, LocalizationLevel.USER),
+                    MAPSTYLE_FILENAME);
 
             ILocalizationFileObserver obs = new ILocalizationFileObserver() {
                 @Override
@@ -215,22 +221,26 @@ public class MapStylePreferenceStore implements ISerializableObject {
             siteLf.addFileUpdatedObserver(obs);
             userLf.addFileUpdatedObserver(obs);
 
-            /* DR 15649 for OB 13.3.1: If the map style preferences are in the
-             * old location, move it to the correct place.  This code can be
+            /*
+             * DR 15649 for OB 13.3.1: If the map style preferences are in the
+             * old location, move it to the correct place. This code can be
              * removed in the future.
              */
-            if (! userLf.exists()) {
-                LocalizationFile oldUserLf = pathMgr.getLocalizationFile(pathMgr
-                        .getContext(LocalizationType.CAVE_STATIC,
+            if (!userLf.exists()) {
+                LocalizationFile oldUserLf = pathMgr.getLocalizationFile(
+                        pathMgr.getContext(LocalizationType.CAVE_STATIC,
                                 LocalizationLevel.USER), OLD_MAPSTYLE_FILENAME);
 
                 if (oldUserLf.exists()) {
                     try {
                         userLf.write(oldUserLf.read());
                         oldUserLf.delete();
-                        statusHandler.handle(Priority.INFO, "Moved user map style preferences to new location");
+                        statusHandler
+                                .handle(Priority.INFO,
+                                        "Moved user map style preferences to new location");
                     } catch (LocalizationException e) {
-                        statusHandler.handle(Priority.PROBLEM, "Unable to move map style preferences", e);
+                        statusHandler.handle(Priority.PROBLEM,
+                                "Unable to move map style preferences", e);
                         e.printStackTrace();
                     }
                 }
@@ -239,9 +249,9 @@ public class MapStylePreferenceStore implements ISerializableObject {
 
         if (siteLf.exists()) {
             try {
-                combinedPreferences = ((MapStylePreferenceStore) SerializationUtil
-                        .jaxbUnmarshalFromXmlFile(siteLf.getFile())).preferences;
-            } catch (SerializationException e) {
+                combinedPreferences = getJaxbManager().unmarshalFromXmlFile(
+                        MapStylePreferenceStore.class, siteLf.getFile()).preferences;
+            } catch (Exception e) {
                 statusHandler
                         .handle(Priority.PROBLEM,
                                 "Exception while loading site map style preferences",
@@ -253,17 +263,16 @@ public class MapStylePreferenceStore implements ISerializableObject {
 
         if (userLf.exists()) {
             try {
-                preferences = ((MapStylePreferenceStore) SerializationUtil
-                        .jaxbUnmarshalFromXmlFile(userLf.getFile())).preferences;
+                preferences = getJaxbManager().unmarshalFromXmlFile(
+                        MapStylePreferenceStore.class, userLf.getFile()).preferences;
 
                 // merge user into site
                 for (Entry<MapStylePreferenceKey, Capabilities> entry : preferences
                         .entrySet()) {
-                    combinedPreferences.put(entry.getKey(),
-                            entry.getValue());
+                    combinedPreferences.put(entry.getKey(), entry.getValue());
                 }
 
-            } catch (SerializationException e) {
+            } catch (Exception e) {
                 statusHandler
                         .handle(Priority.PROBLEM,
                                 "Exception while loading user map style preferences",
@@ -272,6 +281,18 @@ public class MapStylePreferenceStore implements ISerializableObject {
         } else {
             preferences = new HashMap<MapStylePreferenceKey, Capabilities>();
         }
+    }
+
+    private static synchronized JAXBManager getJaxbManager()
+            throws JAXBException {
+        if (jaxb == null) {
+            SubClassLocator locator = new SubClassLocator();
+            Collection<Class<?>> classes = JAXBClassLocator.getJAXBClasses(
+                    locator, AbstractCapability.class,
+                    MapStylePreferenceStore.class);
+            jaxb = new JAXBManager(classes.toArray(new Class<?>[0]));
+        }
+        return jaxb;
     }
 
     private MapStylePreferenceStore() {
@@ -308,8 +329,7 @@ public class MapStylePreferenceStore implements ISerializableObject {
 
         File file = lf.getFile();
         try {
-            SerializationUtil
-                    .jaxbMarshalToXmlFile(this, file.getAbsolutePath());
+            getJaxbManager().marshalToXmlFile(this, file.getAbsolutePath());
             lf.save();
         } catch (Exception e) {
             statusHandler.handle(Priority.PROBLEM,
