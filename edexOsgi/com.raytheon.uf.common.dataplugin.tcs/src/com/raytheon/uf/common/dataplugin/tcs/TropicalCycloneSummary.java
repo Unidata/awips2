f986662369857e0886d5192b86794b337cb666d4
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-11-11 13:04:21 -0600

    Issue #2297 - fix 14.2.1 merge
    Former-commit-id: fee6a5dc9cfc07b4fa19e0442715f9c579c45074 [formerly e2a02ca2470a5f362f83382dee46031bc55c5209] [formerly fee6a5dc9cfc07b4fa19e0442715f9c579c45074 [formerly e2a02ca2470a5f362f83382dee46031bc55c5209] [formerly 411e0fa7c4b87100f61d1e8038cecd6bc0f632d7 [formerly e5b298f51871ab2ed8be9e70cd1de5a4f8ce7f2b]]]
    Former-commit-id: 411e0fa7c4b87100f61d1e8038cecd6bc0f632d7
    Former-commit-id: b1a6d2d828859cd78d57814c4f73aba7c42e833f [formerly e937ba954b8ce43f474e36e394b101afa379c3e0]
    Former-commit-id: 18bb3ef947b78865ac3968071e19a8c5c14803c0

--- edexOsgi/com.raytheon.uf.common.dataplugin.tcs/src/com/raytheon/uf/common/dataplugin/tcs/TropicalCycloneSummary.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.tcs/src/com/raytheon/uf/common/dataplugin/tcs/TropicalCycloneSummary.java
@@ -185,7 +185,6 @@ public class TropicalCycloneSummary extends PersistablePluginDataObject
     }
 
     @Override
-    @Override
     public SurfaceObsLocation getSpatialObject() {
         return location;
     }
