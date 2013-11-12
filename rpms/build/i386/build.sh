33ce83428419fdbb8c3c676c5d0818906b3001f1
Bryan Kowal <Bryan.Kowal@raytheon.com>
2013-11-12 17:43:10 -0600

    Issue #2297 - update build scripts to complete the full delta build
    Former-commit-id: bc25e93a7d76e6746b6031083028e0ce88aaa406 [formerly 46264b5fcc96acbffd095513b5997b1d2dcaf605] [formerly 631c03601cacb7ad6b97b0524e0151db5f28246b] [formerly 631c03601cacb7ad6b97b0524e0151db5f28246b [formerly 7cac007ef1a7b06bea40e4efb5f13bb77cceb779]] [formerly bc25e93a7d76e6746b6031083028e0ce88aaa406 [formerly 46264b5fcc96acbffd095513b5997b1d2dcaf605] [formerly 631c03601cacb7ad6b97b0524e0151db5f28246b] [formerly 631c03601cacb7ad6b97b0524e0151db5f28246b [formerly 7cac007ef1a7b06bea40e4efb5f13bb77cceb779]] [formerly d76033ff3d6ccba9a04623242eec1cc079f39afb [formerly 631c03601cacb7ad6b97b0524e0151db5f28246b [formerly 7cac007ef1a7b06bea40e4efb5f13bb77cceb779] [formerly d76033ff3d6ccba9a04623242eec1cc079f39afb [formerly b605c29ae839ca7950bc04426f09eb310587a18f]]]]]
    Former-commit-id: d76033ff3d6ccba9a04623242eec1cc079f39afb
    Former-commit-id: 9b8fa911c37675439a098a1e9dec7b56b92bf116 [formerly d8a1e159512adc1db80260f45f4bff3c8c2b5eed] [formerly e9ff5e018b78647abfe3baab36d00a5b061d2560] [formerly feb2aa0b0dda7ccd1063558cea4d558d5bad9640 [formerly 08a3d0dc0c028b514f152d7d2df94af1610acc50] [formerly e9ff5e018b78647abfe3baab36d00a5b061d2560 [formerly 084d772588f60ca28757a759a6313fbc0d4b91ff]]]
    Former-commit-id: 597b2e152fd29eb82d76be1e75ec26565ac4609d [formerly 5a7b66c6f75c115cad84c4d0aaf076e8a21ad3a3] [formerly d83504922b1c5f999a1cb85ac06f9d4a982771ed [formerly 8867114ff80ca94a1fdf74fa7df130c99a50f2d3]]
    Former-commit-id: d83504922b1c5f999a1cb85ac06f9d4a982771ed
    Former-commit-id: 6b9cae71e21f60b6b782cc0a3b87bedd107cd10a

--- rpms/build/i386/build.sh
+++ rpms/build/i386/build.sh
@@ -143,6 +143,8 @@ if [ "${1}" = "-postgres" ]; then
 fi
 
 if [ "${1}" = "-delta" ]; then
+   buildRPM "awips2"
+   buildRPM "awips2-common-base"
    buildCAVE
    if [ $? -ne 0 ]; then
       exit 1
@@ -156,10 +158,8 @@ if [ "${1}" = "-delta" ]; then
    buildRPM "awips2-ncep-database"
    buildRPM "awips2-gfesuite-client"
    buildRPM "awips2-gfesuite-server"
-   buildRPM "awips2-python"
    buildRPM "awips2-python-dynamicserialize"
    buildRPM "awips2-python-ufpy"
-   buildRPM "awips2-python-qpid"
 
    buildRPM "awips2-aviation-shared"
    buildRPM "awips2-cli"
