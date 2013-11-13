6c28068d212f24a40e2ed09df4468bbdb4b5a208
Brian Clements <brian.d.clements@raytheon.com>
2013-11-13 13:33:49 -0600

    Issue #2393 removed pypies interpolation
    updated datastore interface
    updated pypies python code
    updated python generated from java classes
    moved in-java interpolation code from sat dao to utility classes
    changed ncep mcidas dao to use in-java interpolation
    changed VIIRS dao to use common interpolation code
    
    
    Former-commit-id: 42f72149677daef5df405fc741e202c0a7d64c2b [formerly d35fc591d26ba69e9afdb3c46ffc4bfdd7c1602b] [formerly fe58826a1e1cdefd992deb6f969a0c1e97a6a84a] [formerly 0f40ae2b99c248548bf280427ade470b4337f60f [formerly fe58826a1e1cdefd992deb6f969a0c1e97a6a84a [formerly 6f9ad823de5d0b30ae7c8206b4a4fdf1e3a2b22b]]]
    Former-commit-id: 0f40ae2b99c248548bf280427ade470b4337f60f
    Former-commit-id: 2a623a799e9a08a389ada1365b70ef555ee0c5d1 [formerly b762864f88c711c306b1c569a4e1891f336f6763]
    Former-commit-id: 10e63616ff6acfe09e0ce4cfe83ae3346f2e0f1a

---
+++ edexOsgi/com.raytheon.uf.edex.database/src/com/raytheon/uf/edex/database/plugin/DownscaleStoreUtil.java
@@ -0,0 +1,157 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.database.plugin;
+
+import java.awt.Rectangle;
+
+import org.opengis.referencing.operation.TransformException;
+
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.datastorage.DataStoreFactory;
+import com.raytheon.uf.common.datastorage.IDataStore;
+import com.raytheon.uf.common.datastorage.StorageException;
+import com.raytheon.uf.common.datastorage.records.IDataRecord;
+import com.raytheon.uf.common.geospatial.interpolation.GridDownscaler;
+import com.raytheon.uf.common.geospatial.interpolation.data.AbstractDataWrapper;
+import com.raytheon.uf.common.geospatial.interpolation.data.DataWrapper1D;
+
+/**
+ * Utility for storing downscaled data to datastore
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Nov 19, 2013  2393      bclement     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bclement
+ * @version 1.0
+ */
+
+public class DownscaleStoreUtil {
+
+    /**
+     * Interface for creating IDataRecords for interpolation levels
+     */
+    public static interface IDataRecordCreator {
+
+        /**
+         * Create new data record for level
+         * 
+         * @param data
+         * @param downScaleLevel
+         * @param size
+         * @return
+         * @throws StorageException
+         */
+        public IDataRecord create(Object data, int downScaleLevel,
+                Rectangle size) throws StorageException;
+
+        /**
+         * @return fill value
+         */
+        public double getFillValue();
+    }
+
+    /**
+     * Create and add interpolated levels from dataSource.
+     * 
+     * @param dataStore
+     * @param downScaler
+     * @param dataSource
+     * @param creator
+     * @return number of levels not including the base level
+     * @throws StorageException
+     */
+    public static <T extends PluginDataObject> int storeInterpolated(
+            IDataStore dataStore, GridDownscaler downScaler,
+            DataWrapper1D dataSource, IDataRecordCreator creator)
+            throws StorageException {
+        // default to batch storage
+        return storeInterpolated(dataStore, downScaler, dataSource, creator,
+                false);
+    }
+
+    /**
+     * Create and add interpolated levels from dataSource.
+     * 
+     * @param dataStore
+     * @param downScaler
+     * @param dataSource
+     * @param creator
+     * @param storeAfterEach
+     *            if true, call store method on dataStore after each level is
+     *            created
+     * @return number of levels not including the base level
+     * @throws StorageException
+     */
+    public static <T extends PluginDataObject> int storeInterpolated(
+            IDataStore dataStore, GridDownscaler downScaler,
+            DataWrapper1D dataSource, IDataRecordCreator creator,
+            boolean storeAfterEach) throws StorageException {
+
+        dataSource.setFillValue(creator.getFillValue());
+
+        // How many interpolation levels do we need for this data?
+        int levels = downScaler.getNumberOfDownscaleLevels();
+
+        // How many interpolation levels do we need for this data? Includes
+        // the base level!
+        // Subtract one for the base level data.
+        int downScaleLevels = levels - 1;
+        if (DataStoreFactory.isInterpolated(levels)) {
+            for (int level = 0; level < downScaleLevels; level++) {
+                int downScaleLevel = level + 1;
+                Rectangle size = downScaler.getDownscaleSize(downScaleLevel);
+
+                DataWrapper1D dest = AbstractDataWrapper.createNew(
+                        dataSource.getClass(), size);
+                dest.setFillValue(creator.getFillValue());
+                try {
+                    // Downscale from previous level
+                    downScaler.downscale(downScaleLevel - 1, downScaleLevel,
+                            dataSource, dest);
+                    Object data = dest.getArray();
+                    if (data == null) {
+                        throw new StorageException(
+                                "Unable to get downscaled data from destination type: "
+                                        + dest.getClass(), null);
+                    }
+                    IDataRecord dr = creator.create(data, downScaleLevel, size);
+                    dataStore.addDataRecord(dr);
+                    if (storeAfterEach) {
+                        dataStore.store();
+                    }
+                    // Set source to current level
+                    dataSource = dest;
+                } catch (TransformException e) {
+                    throw new StorageException(
+                            "Error creating downscaled data", null, e);
+                }
+            }
+        }
+        return downScaleLevels;
+    }
+
+}
