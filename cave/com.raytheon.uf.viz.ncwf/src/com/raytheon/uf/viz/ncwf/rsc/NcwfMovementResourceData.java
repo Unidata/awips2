55682480e13d2fb1e77705b46a4115edfd80fd1c
Brian Clements <brian.d.clements@raytheon.com>
2013-11-14 14:33:41 -0600

    Issue #2361 fixed ncwf hibernate pickup
    the new way of finding hibernate classes requires project names to match package names
    fixed ncwf to have matching package and project names
    
    
    Former-commit-id: 00490c2db27a6ab658aa532df46119c9619438cf [formerly 4924917e28275d9bb8bb21feceb6016ce2c91c40]
    Former-commit-id: cacdf313c59170b00ec6f71accf3d33c3ab8468b

--- cave/com.raytheon.uf.viz.ncwf/src/com/raytheon/uf/viz/ncwf/rsc/NcwfMovementResourceData.java
+++ cave/com.raytheon.uf.viz.ncwf/src/com/raytheon/uf/viz/ncwf/rsc/NcwfMovementResourceData.java
@@ -23,7 +23,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 
 import com.raytheon.uf.common.dataplugin.PluginDataObject;
-import com.raytheon.uf.common.dataplugin.ncwf.BUFRncwf;
+import com.raytheon.uf.common.dataplugin.bufrncwf.BUFRncwf;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
