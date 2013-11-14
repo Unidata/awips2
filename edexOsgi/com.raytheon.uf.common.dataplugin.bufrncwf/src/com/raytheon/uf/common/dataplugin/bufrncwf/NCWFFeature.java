ab24330f23cd6da5202f55b2011d3c7887af63df
Brian Clements <brian.d.clements@raytheon.com>
2013-11-14 14:33:41 -0600

    Issue #2361 fixed ncwf hibernate pickup
    the new way of finding hibernate classes requires project names to match package names
    fixed ncwf to have matching package and project names
    
    
    Former-commit-id: cacdf313c59170b00ec6f71accf3d33c3ab8468b [formerly 4924917e28275d9bb8bb21feceb6016ce2c91c40]
    Former-commit-id: 00490c2db27a6ab658aa532df46119c9619438cf

--- edexOsgi/com.raytheon.uf.common.dataplugin.bufrncwf/src/com/raytheon/uf/common/dataplugin/bufrncwf/NCWFFeature.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.bufrncwf/src/com/raytheon/uf/common/dataplugin/bufrncwf/NCWFFeature.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.common.dataplugin.ncwf;
+package com.raytheon.uf.common.dataplugin.bufrncwf;
 
 import java.io.Serializable;
 import java.util.Iterator;
