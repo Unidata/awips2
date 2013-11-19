052119fffef42ca5b06d7e8bd5f1d8209ddf5f66
Richard Peter <richard.peter@raytheon.com>
2013-11-19 16:58:45 -0600

    Issue #2170: Update Plugin Notification Framework
    Change-Id: I2ec99fadbe2f383bb0892c59601350395f1e5277
    
    Former-commit-id: f2d99af20e660e239791e08ff588edc45dfef274 [formerly 5905c5d08d8d96cb1e6231726782cd0107efa7cf] [formerly 2085a9cd33d7e29deecd402ac7d4cf8ed656273b] [formerly f2d99af20e660e239791e08ff588edc45dfef274 [formerly 5905c5d08d8d96cb1e6231726782cd0107efa7cf] [formerly 2085a9cd33d7e29deecd402ac7d4cf8ed656273b] [formerly fbda83aeb9ca7c1db77627fffa79832591281827 [formerly 2085a9cd33d7e29deecd402ac7d4cf8ed656273b [formerly 081a19d5cfca3d717a332357dbaead199767e0b8]]]]
    Former-commit-id: fbda83aeb9ca7c1db77627fffa79832591281827
    Former-commit-id: 14ed9331d30d3f9e2102aca4e4b408158b0f1e35 [formerly 670e63d0dd61b2c20ec606c4f6975ebac93f5af4] [formerly 92cc0eed5d5ca22d3b43329858c1619c9363e66a [formerly f7f9a4e57c4848eee1090372fc1f9440071331f4]]
    Former-commit-id: 5edc348b297d22aeb20b7b1cbe81e90329464d25 [formerly ff5fbcf012b95b978c932122178f44011d572109]
    Former-commit-id: 90e23f5c6b3b6d22a5a3e664a220ccbb877d4691

--- edexOsgi/com.raytheon.uf.edex.ingest/src/com/raytheon/uf/edex/ingest/notification/PluginNotifier.java
+++ edexOsgi/com.raytheon.uf.edex.ingest/src/com/raytheon/uf/edex/ingest/notification/PluginNotifier.java
@@ -19,24 +19,47 @@
  **/
 package com.raytheon.uf.edex.ingest.notification;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.RecipientList;
+import javax.xml.bind.JAXBException;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
+import com.raytheon.uf.common.dataplugin.PluginDataObject;
+import com.raytheon.uf.common.dataplugin.PluginException;
+import com.raytheon.uf.common.dataplugin.annotations.DataURIUtil;
+import com.raytheon.uf.common.dataquery.DecisionTree;
+import com.raytheon.uf.common.dataquery.requests.RequestConstraint;
+import com.raytheon.uf.common.localization.IPathManager;
+import com.raytheon.uf.common.localization.LocalizationFile;
+import com.raytheon.uf.common.localization.PathManagerFactory;
+import com.raytheon.uf.common.localization.exception.LocalizationException;
+import com.raytheon.uf.common.serialization.JAXBManager;
+import com.raytheon.uf.common.serialization.SerializationException;
+import com.raytheon.uf.common.status.IPerformanceStatusHandler;
 import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.PerformanceStatus;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
-import com.raytheon.uf.edex.core.EDEXUtil;
+import com.raytheon.uf.common.time.util.ITimer;
+import com.raytheon.uf.common.time.util.TimeUtil;
 import com.raytheon.uf.edex.core.EdexException;
+import com.raytheon.uf.edex.ingest.notification.PluginNotifierConfig.EndpointType;
+import com.raytheon.uf.edex.ingest.notification.PluginNotifierConfig.NotifyFormat;
+import com.raytheon.uf.edex.ingest.notification.router.DataUriRouter;
+import com.raytheon.uf.edex.ingest.notification.router.INotificationRouter;
+import com.raytheon.uf.edex.ingest.notification.router.PdoRouter;
 
 /**
  * Plugins can register routes with this and then be generically fired to. Helps
- * to reduce dependencies as we no longer need to call a route directly.
+ * to reduce dependencies as we no longer need to call a route directly. All
+ * registration must occur before messages are being picked up. Otherwise
+ * concurrency problems may occur.
  * 
  * <pre>
  * 
@@ -44,8 +67,9 @@ import com.raytheon.uf.edex.core.EdexException;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Jun 13, 2013            mnash     Initial creation
- * 
+ * Jun 13, 2013            mnash       Initial creation.
+ * Nov 19, 2013 2170       rjpeter     Add plugin contributed config files, filtering,
+ *                                     and support for pdo vs datauri.
  * </pre>
  * 
  * @author mnash
@@ -53,55 +77,311 @@ import com.raytheon.uf.edex.core.EdexException;
  */
 
 public class PluginNotifier {
-
     private static final IUFStatusHandler theHandler = UFStatus
             .getHandler(PluginNotifier.class);
 
-    private static final Multimap<String, String> routes = ArrayListMultimap
-            .create();
+    private final IPerformanceStatusHandler perfLog = PerformanceStatus
+            .getHandler("Notification:");
 
-    private static final PluginNotifier distributor = new PluginNotifier();
-
-    private PluginNotifier() {
-    }
-
-    public PluginNotifier getInstance() {
-        return distributor;
-    }
+    private static final int DEFAULT_TIME_TO_LIVE = 300000;
 
     /**
-     * Normally called from Spring, this allows a set of commands to be
-     * registered for a specific plugin.
-     * 
-     * @param name
-     * @param command
-     * @return
+     * Decision tree for plugin notification.
      */
-    public PluginNotifier register(String name, String command) {
-        routes.put(name, command);
-        return this;
-    }
+    private final DecisionTree<INotificationRouter> tree = new DecisionTree<INotificationRouter>();
+
+    private final List<INotificationRouter> receiveAllRoutes = new LinkedList<INotificationRouter>();
+
+    private final List<INotificationRouter> filteredRoutes = new LinkedList<INotificationRouter>();
 
     /**
-     * Takes the pluginName, which is what was registered for, and sends to all
-     * the routes that are registered as that.
-     * 
-     * @param exchange
-     * @return
+     * Set of loaded names. Used for duplicate detection.
      */
-    @RecipientList
-    public List<String> send(Exchange exchange) {
-        Message in = exchange.getIn();
-        String name = (String) in.getHeader("pluginName");
-        List<String> list = new ArrayList<String>();
-        for (String route : routes.get(name)) {
+    private final Set<String> loadedNames = new HashSet<String>();
+
+    public PluginNotifier() throws JAXBException {
+        loadConfigurations();
+    }
+
+    private synchronized void loadConfigurations() throws JAXBException {
+        JAXBManager mgr = new JAXBManager(PluginNotifierConfigList.class,
+                PluginNotifierConfig.class);
+
+        IPathManager pathMgr = PathManagerFactory.getPathManager();
+        LocalizationFile[] files = pathMgr.listStaticFiles("notification",
+                new String[] { ".xml" }, false, true);
+        for (LocalizationFile lf : files) {
             try {
-                EDEXUtil.getMessageProducer().sendAsyncUri(route, in.getBody());
-            } catch (EdexException e) {
+                File f = lf.getFile(true);
+                if (f.length() > 0) {
+                    // empty files may be used to override base files to remove
+                    // functionality
+                    InputStream is = null;
+
+                    try {
+                        is = lf.openInputStream();
+
+                        PluginNotifierConfigList confList = (PluginNotifierConfigList) mgr
+                                .unmarshalFromInputStream(is);
+                        List<PluginNotifierConfig> configs = confList
+                                .getNotificationConfigs();
+                        if ((configs != null) && !configs.isEmpty()) {
+                            for (PluginNotifierConfig conf : configs) {
+                                register(conf, false);
+                            }
+                        }
+                    } catch (SerializationException e) {
+                        theHandler.handle(Priority.PROBLEM,
+                                "Unable to deserialize " + f.getPath(), e);
+                    } catch (InvalidNotificationConfigException e) {
+                        theHandler.handle(
+                                Priority.PROBLEM,
+                                "Unable to load plugin configuration "
+                                        + f.getPath(), e);
+                    } finally {
+                        if (is != null) {
+                            try {
+                                is.close();
+                            } catch (IOException e) {
+                                // ignore
+                            }
+                        }
+                    }
+                }
+            } catch (LocalizationException e) {
                 theHandler.handle(Priority.PROBLEM,
-                        "Unable to send message to " + route, e);
+                        "Error occurred accessing file: " + lf, e);
             }
         }
-        return list;
+
+        rebuildTree();
+
+    }
+
+    /**
+     * Register the given PluginNotifierConfig.
+     * 
+     * @param config
+     * @return
+     */
+    public synchronized void register(PluginNotifierConfig config)
+            throws InvalidNotificationConfigException {
+        register(config, true);
+    }
+
+    /**
+     * Register the given PluginNotifierConfig.
+     * 
+     * @param config
+     * @param rebuildTree
+     *            Whether or not to rebuild the internal tree. If many things
+     *            are being registered can improve performance to only build
+     *            tree once at the end.
+     * @return
+     */
+    public synchronized void register(PluginNotifierConfig config,
+            boolean rebuildTree) throws InvalidNotificationConfigException {
+        validate(config);
+
+        INotificationRouter router = null;
+        switch (config.getFormat()) {
+        case DATAURI:
+            router = new DataUriRouter(config);
+            break;
+        case PDO:
+            router = new PdoRouter(config);
+            break;
+        default:
+            throw new InvalidNotificationConfigException(
+                    "No INotificationRouter registered for format: "
+                            + config.getFormat());
+        }
+
+        Map<String, RequestConstraint>[] metadataMaps = config.getMetadataMap();
+        boolean receiveAll = (metadataMaps == null)
+                || (metadataMaps.length == 0)
+                || ((metadataMaps.length == 1) && ((metadataMaps[0] == null) || metadataMaps[0]
+                        .isEmpty()));
+
+        if (receiveAll) {
+            // null or empty constraint map implies receive all data
+            receiveAllRoutes.add(router);
+        } else {
+            filteredRoutes.add(router);
+            for (Map<String, RequestConstraint> metadataMap : metadataMaps) {
+                tree.insertCriteria(metadataMap, router);
+            }
+
+            if (rebuildTree) {
+                tree.rebuildTree();
+            }
+        }
+
+        loadedNames.add(config.getEndpointName());
+    }
+
+    /**
+     * Validate the passed config
+     * 
+     * @param config
+     * @return
+     */
+    private void validate(PluginNotifierConfig config)
+            throws InvalidNotificationConfigException {
+        String endpoint = config.getEndpointName();
+        if ((endpoint == null) || (endpoint.trim().length() == 0)) {
+            throw new InvalidNotificationConfigException(
+                    "endpointName is required");
+        }
+
+        if (loadedNames.contains(endpoint)) {
+            throw new InvalidNotificationConfigException("PluginConfiguration "
+                    + endpoint + ": endpointName is already in use");
+        }
+
+        EndpointType type = config.getEndpointType();
+        if (type == null) {
+            StringBuilder msg = new StringBuilder(180);
+            msg.append("PluginConfiguration ")
+                    .append(endpoint)
+                    .append(": missing required field endpointType.  Valid values for ")
+                    .append(NotifyFormat.PDO).append(" format are ")
+                    .append(EndpointType.DIRECTVM).append(" and ")
+                    .append(EndpointType.VM).append(".  Valid values for ")
+                    .append(NotifyFormat.DATAURI).append(" format are ")
+                    .append(EndpointType.DIRECTVM).append(", ")
+                    .append(EndpointType.VM).append(", ")
+                    .append(EndpointType.QUEUE).append(", and ")
+                    .append(EndpointType.TOPIC);
+            throw new InvalidNotificationConfigException(msg.toString());
+        }
+
+        NotifyFormat format = config.getFormat();
+        if (NotifyFormat.PDO.equals(format)
+                && (EndpointType.QUEUE.equals(type) || EndpointType.TOPIC
+                        .equals(type))) {
+            StringBuilder msg = new StringBuilder(120);
+            msg.append("PluginConfiguration ").append(endpoint)
+                    .append(": endpointType ").append(type)
+                    .append(" is invalid for format ").append(format)
+                    .append(".  Valid values for ").append(NotifyFormat.PDO)
+                    .append(" format are ").append(EndpointType.DIRECTVM)
+                    .append(" and ").append(EndpointType.VM);
+            throw new InvalidNotificationConfigException(msg.toString());
+        }
+
+        if (!EndpointType.QUEUE.equals(type) && config.isDurable()) {
+            theHandler.warn("PluginConfiguration: " + endpoint
+                    + " durable setting only valid on QUEUE type endpoints");
+        }
+
+        if ((EndpointType.QUEUE.equals(type) || EndpointType.TOPIC.equals(type))
+                && (config.getTimeToLive() < 0)) {
+            theHandler
+                    .warn("PluginConfiguration: "
+                            + endpoint
+                            + " has invalid time to live.  Time to live for JMS endpoints must be 0 or greater.  Setting to default of: "
+                            + DEFAULT_TIME_TO_LIVE + " ms");
+            config.setTimeToLive(DEFAULT_TIME_TO_LIVE);
+        }
+    }
+
+    /**
+     * Rebuild the tree based on all register'd configurations.
+     */
+    public void rebuildTree() {
+        tree.rebuildTree();
+    }
+
+    /**
+     * Checks the pdo's against the registered routes. Data will then be
+     * transformed and queued or sent immediately depending on configuration.
+     * 
+     * @param pdos
+     * @return
+     */
+    public void notify(PluginDataObject... pdos) {
+        if ((pdos != null) && (pdos.length > 0)) {
+            ITimer timer = TimeUtil.getTimer();
+            timer.start();
+            if (!receiveAllRoutes.isEmpty()) {
+                for (PluginDataObject pdo : pdos) {
+                    for (INotificationRouter router : receiveAllRoutes) {
+                        router.process(pdo);
+                    }
+                }
+
+                for (INotificationRouter router : receiveAllRoutes) {
+                    try {
+                        router.sendImmediateData();
+                    } catch (EdexException e) {
+                        theHandler.handle(
+                                Priority.PROBLEM,
+                                "Unable to send notification data to "
+                                        + router.getRoute(), e);
+                    }
+                }
+            }
+
+            if (!filteredRoutes.isEmpty()) {
+                Set<INotificationRouter> routesWithData = new HashSet<INotificationRouter>();
+                for (PluginDataObject pdo : pdos) {
+                    try {
+                        List<INotificationRouter> routers = tree
+                                .searchTree(DataURIUtil.createDataURIMap(pdo));
+                        for (INotificationRouter router : routers) {
+                            router.process(pdo);
+                            routesWithData.add(router);
+                        }
+                    } catch (PluginException e) {
+                        theHandler.handle(Priority.PROBLEM,
+                                e.getLocalizedMessage(), e);
+                    }
+                }
+
+                for (INotificationRouter router : routesWithData) {
+                    try {
+                        router.sendImmediateData();
+                    } catch (EdexException e) {
+                        theHandler.handle(
+                                Priority.PROBLEM,
+                                "Unable to send notification data to "
+                                        + router.getRoute(), e);
+                    }
+                }
+            }
+            timer.stop();
+            perfLog.logDuration("Processed " + pdos.length + " pdos",
+                    timer.getElapsedTime());
+        }
+    }
+
+    /**
+     * Send the queued notifications.
+     * 
+     * @return
+     */
+    public void sendQueuedNotifications() {
+        for (INotificationRouter router : receiveAllRoutes) {
+            try {
+                router.sendQueuedData();
+            } catch (EdexException e) {
+                theHandler.handle(
+                        Priority.PROBLEM,
+                        "Unable to send notification data to "
+                                + router.getRoute(), e);
+            }
+        }
+
+        for (INotificationRouter router : filteredRoutes) {
+            try {
+                router.sendQueuedData();
+            } catch (EdexException e) {
+                theHandler.handle(
+                        Priority.PROBLEM,
+                        "Unable to send notification data to "
+                                + router.getRoute(), e);
+            }
+        }
     }
 }
