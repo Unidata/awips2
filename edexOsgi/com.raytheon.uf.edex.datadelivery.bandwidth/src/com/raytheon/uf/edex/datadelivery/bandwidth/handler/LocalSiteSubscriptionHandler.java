6e19c4dab10d75cbee6363e5b51b7463b88bbca8
Brad Gonzales <bradley_l_gonzales@raytheon.com>
2013-11-19 19:16:22 -0600

    Issue #2545 Renamed datadelivery edex impl files to facilitate modes exclusion.  Added monilithic impl config file for a registry that runs as an ncf and wfo for devRegistry mode.  Added empty site and shared and local subscription handlers to enable the FindSubscriptionHandler bean to be configured through spring. Fix for re-routing Insert and Update registry events as data set metadata events which was lost when the registry was configured to distinguish between insert and update events.  Moved subscriptionRoutesToSchedule bean (and other beans specific to an implementation) to the ncf, wfo, and monolithic edex impl spring files.  Removed the wfo and ncf specific code from HibernateBandwidthInitializer.executeAfterRegistryInit() since the customized subscription handlers will be injected through spring.  Removed the SBN route from wfo mode.
    Amend: Comment updates from peer review.
    
    Change-Id: Ic4c0dca8ebf1e86537e46a6231af58bb65c83cf8
    
    Former-commit-id: 053f20c70be96a0e6c05bb840e76dd47ef1feee3 [formerly 053f20c70be96a0e6c05bb840e76dd47ef1feee3 [formerly c245f741b9e78d8e4503698336d9929536e72e0d]]
    Former-commit-id: 95a1914c71fcf58339136eae732fd75aefba7f31
    Former-commit-id: 04829d37e2f876ff17d290bebc6dc73f68731583

---
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/handler/LocalSiteSubscriptionHandler.java
@@ -0,0 +1,78 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ *
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ *
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ *
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.datadelivery.bandwidth.handler;
+
+import com.raytheon.edex.site.SiteUtil;
+import com.raytheon.uf.common.datadelivery.registry.SiteSubscription;
+import com.raytheon.uf.common.datadelivery.registry.ebxml.SiteSubscriptionQuery;
+import com.raytheon.uf.common.datadelivery.registry.handlers.SiteSubscriptionHandler;
+import com.raytheon.uf.common.registry.handler.IRegistryObjectHandler;
+
+/**
+ * {@link IRegistryObjectHandler} implementation for {@link SiteSubscription}.
+ * Retrieves only subscritions for the given site id. If the site id is not set
+ * then it retrieves all.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Nov 14, 2013 2545       bgonzale    Initial creation.
+ * 
+ * </pre>
+ * 
+ * @author bgonzale
+ * @version 1.0
+ */
+public class LocalSiteSubscriptionHandler extends SiteSubscriptionHandler {
+
+    private String officeId;
+
+    /**
+     * Default Constructor.
+     */
+    public LocalSiteSubscriptionHandler() {
+        this(SiteUtil.getSite());
+    }
+
+    /**
+     * Initialization Constructor.
+     * 
+     * @param officeId
+     */
+    public LocalSiteSubscriptionHandler(String officeId) {
+        this.officeId = officeId;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected SiteSubscriptionQuery getQuery() {
+        SiteSubscriptionQuery query = new SiteSubscriptionQuery();
+        if (officeId != null) {
+            query.setOfficeId(officeId);
+        }
+        return query;
+    }
+
+}
