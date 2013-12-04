fb3b7b0a9fd160fe40dd091f4f5f2df0c337e0fa
Ron Anderson <ron.anderson@raytheon.com>
2013-12-04 17:09:16 -0600

    Issue #2331 Fixed Topo retrieval for smartTools/procedures
    Change-Id: Iab9d5216f278836f3b8b2245330fdb2b254a5ae3
    
    Former-commit-id: 218a052bbf92a1d616e34f5b99ae4e6c726bf83f [formerly 497236ca74a462ea800adb1123f2914735fe26d8] [formerly b5906249ccb7a9199337d9bfef50d6dfee46a55b] [formerly 20eeb2796e02ef0b812083cfa8bd0dacd95b945f [formerly b5906249ccb7a9199337d9bfef50d6dfee46a55b [formerly a7a201ec657a333f6e6f5c3c1d12a3d1f9b50a4c]]]
    Former-commit-id: 20eeb2796e02ef0b812083cfa8bd0dacd95b945f
    Former-commit-id: 473bec12d3fe432d141080f1a78bde66bd1ac17b [formerly 103fed386f9aa6734f453a32861503dfb69638b9]
    Former-commit-id: e30d6041ee7dd54e494dd4c4c0902c9699592fd1

--- cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/internal/GFETopoManager.java
+++ cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/internal/GFETopoManager.java
@@ -19,24 +19,15 @@
  **/
 package com.raytheon.viz.gfe.core.internal;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.raytheon.uf.common.dataplugin.gfe.GridDataHistory;
-import com.raytheon.uf.common.dataplugin.gfe.GridDataHistory.OriginType;
 import com.raytheon.uf.common.dataplugin.gfe.db.objects.DatabaseID;
-import com.raytheon.uf.common.dataplugin.gfe.db.objects.GridParmInfo;
-import com.raytheon.uf.common.dataplugin.gfe.db.objects.ParmID;
 import com.raytheon.uf.common.dataplugin.gfe.db.objects.DatabaseID.DataType;
-import com.raytheon.uf.common.dataplugin.gfe.slice.IGridSlice;
+import com.raytheon.uf.common.dataplugin.gfe.db.objects.GridLocation;
+import com.raytheon.uf.common.dataplugin.gfe.db.objects.ParmID;
+import com.raytheon.uf.common.dataplugin.gfe.server.message.ServerResponse;
 import com.raytheon.uf.common.dataplugin.gfe.slice.ScalarGridSlice;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.common.status.UFStatus.Priority;
-import com.raytheon.uf.common.time.TimeRange;
-import com.raytheon.viz.gfe.Activator;
 import com.raytheon.viz.gfe.GFEServerException;
-import com.raytheon.viz.gfe.constants.StatusConstants;
 import com.raytheon.viz.gfe.core.DataManager;
 import com.raytheon.viz.gfe.core.ITopoManager;
 
@@ -51,7 +42,8 @@ import com.raytheon.viz.gfe.core.ITopoManager;
  * SOFTWARE HISTORY
  * Date			Ticket#		Engineer	Description
  * ------------	----------	-----------	--------------------------
- * Jul 2, 2008		#1160	randerso	Initial creation
+ * Jul  2, 2008		#1160	randerso	Initial creation
+ * Nov 20, 2013     #2331   randerso    Re-implemented to better match A1 design
  * 
  * </pre>
  * 
@@ -60,23 +52,14 @@ import com.raytheon.viz.gfe.core.ITopoManager;
  */
 
 public class GFETopoManager implements ITopoManager {
-    private static final transient IUFStatusHandler statusHandler = UFStatus.getHandler(GFETopoManager.class);
+    private static final transient IUFStatusHandler statusHandler = UFStatus
+            .getHandler(GFETopoManager.class);
 
     private DataManager dataManager;
 
     private ScalarGridSlice compositeTopo;
 
-    private ParmID compositeTopoId;
-
-    private ParmID editTopoId;
-
-    // private GridLocation gloc;
-    //
-    // private GridParmInfo gpi;
-
-    private GridDataHistory[] gdh;
-
-    private TimeRange validTime;
+    private ParmID compositeParmID;
 
     /**
      * Constructor taking a pointer to the DataManager
@@ -86,24 +69,10 @@ public class GFETopoManager implements ITopoManager {
     public GFETopoManager(DataManager dataManager) {
         this.dataManager = dataManager;
 
-        // this.gloc =
-        // this.dataManager.getParmManager().compositeGridLocation();
-
-        this.compositeTopoId = new ParmID("Topography", new DatabaseID(
-                this.dataManager.getSiteID(), DataType.GRID, "Topo", "Topo"),
-                "SFC");
-
-        this.editTopoId = new ParmID("Topo", new DatabaseID(this.dataManager
-                .getSiteID(), DataType.GRID, "EditTopo", "Topo"), "SFC");
-
-        // this.gpi = new GridParmInfo(this.compositeTopoId, this.gloc,
-        // GridType.SCALAR, "Feet MSL", "Topography", -16404.0f, 16404.0f,
-        // 0, true, new TimeConstraints(0, 0, 0), false);
-
-        this.validTime = TimeRange.allTimes();
-
-        this.gdh = new GridDataHistory[] { new GridDataHistory(
-                OriginType.OTHER, this.compositeTopoId, this.validTime) };
+        // hard coding this here to match what is done in TopoDatabaseManager
+        // this avoids retrieving the topo data just to get the parm ID.
+        DatabaseID did = new DatabaseID("Topo", DataType.GRID, "Topo", "Topo");
+        this.compositeParmID = new ParmID("Topo", did);
     }
 
     /*
@@ -114,23 +83,20 @@ public class GFETopoManager implements ITopoManager {
     @Override
     public synchronized ScalarGridSlice getCompositeTopo() {
         if (compositeTopo == null) {
-            List<TimeRange> trs = new ArrayList<TimeRange>();
-            trs.add(TimeRange.allTimes());
+            GridLocation gloc = dataManager.getParmManager()
+                    .compositeGridLocation();
             try {
-                List<IGridSlice> slice = dataManager.getClient().getGridData(
-                        editTopoId, trs);
-                GridParmInfo gpi = dataManager.getClient().getGridParmInfo(
-                        editTopoId);
-                gpi.setParmID(compositeTopoId);
-
-                if (slice != null) {
-                    compositeTopo = new ScalarGridSlice(this.validTime, gpi,
-                            this.gdh, ((ScalarGridSlice) slice.get(0))
-                                    .getScalarGrid());
+                ServerResponse<ScalarGridSlice> sr = this.dataManager
+                        .getClient().getTopoData(gloc);
+                if (!sr.isOkay()) {
+                    statusHandler
+                            .error("Error getting topography data from IFPServer: "
+                                    + sr.message());
                 }
+                compositeTopo = sr.getPayload();
             } catch (GFEServerException e) {
-                statusHandler.handle(Priority.PROBLEM,
-                        "Unable to load topography", e);
+                statusHandler.error(
+                        "Error getting topography data from IFPServer: ", e);
             }
         }
         return compositeTopo;
@@ -143,6 +109,6 @@ public class GFETopoManager implements ITopoManager {
      */
     @Override
     public ParmID getCompositeParmID() {
-        return this.compositeTopoId;
+        return this.compositeParmID;
     }
 }
