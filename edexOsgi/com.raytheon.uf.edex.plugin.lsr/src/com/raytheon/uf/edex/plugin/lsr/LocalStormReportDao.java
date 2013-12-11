ebf52bf2836f9c2b7312da5aac363a4bd0c5cad2
Nate Jensen <Nathan.Jensen@raytheon.com>
2013-12-10 18:27:33 -0600

    Issue #2581 remove LSR dataURI column
    Change-Id: I9d5ddc8346e6b9ac0ec56d09d50b6b19a5bc682f
    
    Former-commit-id: 65365cf9eb34a0a7b06f7db2fbc86f3827cce908 [formerly 65365cf9eb34a0a7b06f7db2fbc86f3827cce908 [formerly 6188356e72ff4d4416a53fb42e7f4a6eb279cb7f]] [formerly fa674baf8915613b8470c8068173423a449c6a52] [formerly 65365cf9eb34a0a7b06f7db2fbc86f3827cce908 [formerly 65365cf9eb34a0a7b06f7db2fbc86f3827cce908 [formerly 6188356e72ff4d4416a53fb42e7f4a6eb279cb7f]] [formerly fa674baf8915613b8470c8068173423a449c6a52] [formerly d0074ba3ad508b0c421009da7fdc5aabb0d1a2c6 [formerly d0074ba3ad508b0c421009da7fdc5aabb0d1a2c6 [formerly e037c179fee8118a492cdb5b9ac70c3c678a6033]] [formerly fa674baf8915613b8470c8068173423a449c6a52 [formerly fa674baf8915613b8470c8068173423a449c6a52 [formerly 88124207249673c3bf29915b6f6318bd2397ecd5]]]]]
    Former-commit-id: d0074ba3ad508b0c421009da7fdc5aabb0d1a2c6 [formerly d0074ba3ad508b0c421009da7fdc5aabb0d1a2c6 [formerly e037c179fee8118a492cdb5b9ac70c3c678a6033]]
    Former-commit-id: 4c276a85fe01a993fd4c399b2b258d5243201ae1 [formerly ff09bbf5a104c5298491bc503576486afb79dea2] [formerly 576412b1a938bc6d4cb7fadafd6a7efb6481cdc1 [formerly 44789838959edd1529d69c7f00ab02f902ee7bcf]] [formerly 81a0a198471fc36d1d3066b970389270199d2939 [formerly 996ea09723af98bfa08724939590c1dcab149d44] [formerly db6a9629204d888f63cfa13684d165fece63cbb0 [formerly 34c1af18a7c9d628f2f86f48e45fe23226e1c995]]]
    Former-commit-id: eb48aa59c142cafa27c295db78dd54ce7454657a [formerly 96a4a1b2413cac5a58194c941f5bce3182e64301] [formerly e76ff139550f03e1630c6689ae0227a89d1a13f0 [formerly b5450572f21838116082ea9e7dacfbf0a5fc53d9]]
    Former-commit-id: a096b9be15640e6ff513991506603570e1d37b00 [formerly e548e4723910eee9554a46529f9aba1674788533]
    Former-commit-id: 53ec89baa6ee8e6f2a0438ec7a0866806b5f94ba

--- edexOsgi/com.raytheon.uf.edex.plugin.lsr/src/com/raytheon/uf/edex/plugin/lsr/LocalStormReportDao.java
+++ edexOsgi/com.raytheon.uf.edex.plugin.lsr/src/com/raytheon/uf/edex/plugin/lsr/LocalStormReportDao.java
@@ -19,76 +19,36 @@
  **/
 package com.raytheon.uf.edex.plugin.lsr;
 
-import java.util.List;
-
 import com.raytheon.uf.common.dataplugin.PluginException;
 import com.raytheon.uf.common.dataplugin.lsr.LocalStormReport;
-import com.raytheon.uf.edex.database.DataAccessLayerException;
 import com.raytheon.uf.edex.pointdata.PointDataPluginDao;
 
 /**
- * 
+ * DAO for Local Storm Reports
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Oct 1, 2009            jkorman     Initial creation
- *
+ * Oct 01, 2009            jkorman     Initial creation
+ * Dec 10, 2013 2581       njensen     Removed unused methods
+ * 
  * </pre>
- *
+ * 
  * @author jkorman
- * @version 1.0 
+ * @version 1.0
  */
 public class LocalStormReportDao extends PointDataPluginDao<LocalStormReport> {
 
     /**
      * Creates a new ObsStationDao
-     * @throws PluginException 
+     * 
+     * @throws PluginException
      */
     public LocalStormReportDao(String pluginName) throws PluginException {
         super(pluginName);
-    }
-
-    /**
-     * Retrieves an sfcobs report using the datauri .
-     * 
-     * @param dataURI
-     *            The dataURI to match against.
-     * @return The report record if it exists.
-     */
-    public LocalStormReport queryByDataURI(String dataURI) {
-        LocalStormReport report = null;
-        List<?> obs = null;
-        try {
-            obs = queryBySingleCriteria("dataURI", dataURI);
-        } catch (DataAccessLayerException e) {
-            e.printStackTrace();
-        }
-        if ((obs != null) && (obs.size() > 0)) {
-            report = (LocalStormReport) obs.get(0);
-        }
-        return report;
-    }
-
-    /**
-     * Queries for to determine if a given data uri exists on the sfcobs table.
-     * 
-     * @param dataUri
-     *            The DataURI to find.
-     * @return An array of objects. If not null, there should only be a single
-     *         element.
-     */
-    public Object[] queryDataUriColumn(final String dataUri) {
-
-        String sql = "select datauri from awips.lsr where datauri='"
-                + dataUri + "';";
-
-        Object[] results = executeSQLQuery(sql);
-
-        return results;
     }
 
     @Override
