ac5540213e70a09e9155c270ec0eae6924b90f4c
Slav Korolev <vyacheslav.s.korolev@raytheon.com>
2014-01-06 15:59:59 -0500

    Issue #2653. Corrected decoding of SNOW monitor data.
    Former-commit-id: 696728bafd05a10af344f151b6c1442668c0f18b [formerly 5bbb7740731a19cb6699fd312ae3e2b8d98bae2e] [formerly 714fd018ef275cf91c0713f137d58fc5f854b198] [formerly 696728bafd05a10af344f151b6c1442668c0f18b [formerly 5bbb7740731a19cb6699fd312ae3e2b8d98bae2e] [formerly 714fd018ef275cf91c0713f137d58fc5f854b198] [formerly ab7fc816b4bbdda2d4ae18164b6493beb8f2ab39 [formerly 714fd018ef275cf91c0713f137d58fc5f854b198 [formerly 050f002ff7af3e68c31f5c1949ff5f82fa2e7a9b]]]]
    Former-commit-id: ab7fc816b4bbdda2d4ae18164b6493beb8f2ab39
    Former-commit-id: 679b61deea51598cea8bc421eb8ee47f54b0e73a [formerly eb5ec3527587135beec684e3b2bb08df826d60a1] [formerly 5aa04e4abf5d9d29b644d2a212f6393c065cce96 [formerly 0fb9810e91c19f28e584dc1aabb34fa9c9d62472]]
    Former-commit-id: 9c64be7095aadcd4adef7f3b53b64c6684b3b86d [formerly 4fd070dd4676a58897082a7d9799484e39a9dd43]
    Former-commit-id: 4c3ce254b680cf044b16234662e1ba90fc433610

--- cave/com.raytheon.uf.viz.monitor/src/com/raytheon/uf/viz/monitor/GenerateFSSObReport.java
+++ cave/com.raytheon.uf.viz.monitor/src/com/raytheon/uf/viz/monitor/GenerateFSSObReport.java
@@ -7,103 +7,116 @@ import com.raytheon.uf.common.dataplugin.PluginDataObject;
 import com.raytheon.uf.common.dataplugin.fssobs.FSSObsRecord;
 import com.raytheon.uf.common.monitor.data.ObConst;
 import com.raytheon.uf.common.monitor.data.ObConst.ReportType;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.viz.monitor.data.ObReport;
 
 /**
- * @author skorolev (initial creation)
+ * Generates FSSObs Report for tables.
  * 
- * Change history:
- * Date           Ticket #   Engineer     Description 
- * ---------------------------------------------------
- * May 15, 2012   14510      zhao         Modified generateObReport()
- *
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ *                         skorolev     Initial creation
+ * May 15, 2012 14510      zhao         Modified generateObReport()
+ * Jan 06, 2014  2653      skorolev     Included SNOW data into ObReport.
+ * 
+ * 
+ * </pre>
+ * 
+ * @author skorolev
+ * @version 1.0
  */
 public class GenerateFSSObReport {
+    private static IUFStatusHandler statusHandler = UFStatus
+            .getHandler(GenerateFSSObReport.class);
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.raytheon.uf.viz.monitor.IObReportable#generateObReport(com.raytheon
-	 * .uf.common.dataplugin.PluginDataObject,
-	 * com.raytheon.uf.viz.monitor.data.ObReport,
-	 * com.raytheon.uf.common.monitor.data.ObConst.ChosenAppKey)
-	 */
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * com.raytheon.uf.viz.monitor.IObReportable#generateObReport(com.raytheon
+     * .uf.common.dataplugin.PluginDataObject,
+     * com.raytheon.uf.viz.monitor.data.ObReport,
+     * com.raytheon.uf.common.monitor.data.ObConst.ChosenAppKey)
+     */
     public static ObReport generateObReport(PluginDataObject report) {
-		// Generate the observation report.
-            ObReport obReport = new ObReport();
-			FSSObsRecord metar = (FSSObsRecord) report;
-			try {
-				obReport.setObservationTime(metar.getTimeObs().getTime());
-	            obReport.setRefHour(metar.getRefHour().getTime());
-			} catch (Exception e) {
-				System.out.println("Warning: missing obsTime or refHour at getTimeObs() when processing obs data; " + e + "; trying to set obsTime and refHour from dataURI");
-				obReport.setTimesFromFssobDataURI(report.getDataURI());
-			}
-			obReport.setPlatformId(metar.getPlatformId());
-			obReport.setStationary(true);
-			obReport.setLatitude((float) metar.getLatitude());
-			obReport.setLongitude((float) metar.getLongitude());
-            // Table data:
-            obReport.setCeiling(metar.getCeiling());
-            obReport.setWindDir(metar.getWindDir());
-			obReport.setWindSpeed(metar.getWindSpeed());
-            obReport.setMaxWindSpeed(metar.getMaxWindSpeed());
-            obReport.setWindGust(metar.getWindGust());
-            obReport.setRelativeHumidity(metar.getRelativeHumidity());
-            try {
-                obReport.setVisibility(metar.getVisibility());
-            } catch (Exception e) {
-                obReport.setVisibility(0);
-            }
-            obReport.setDewpoint(metar.getDewpoint());
-            obReport.setTemperature(metar.getTemperature());
-            obReport.setDewpointDepr(metar.getDewpointDepr());
+        // Generate the observation report.
+        ObReport obReport = new ObReport();
+        FSSObsRecord metar = (FSSObsRecord) report;
+        try {
+            obReport.setObservationTime(metar.getTimeObs().getTime());
+            obReport.setRefHour(metar.getRefHour().getTime());
+        } catch (Exception e) {
+            statusHandler
+                    .error("Warning: missing obsTime or refHour at getTimeObs() when processing obs data; "
+                            + e
+                            + "; trying to set obsTime and refHour from dataURI");
+            obReport.setTimesFromFssobDataURI(report.getDataURI());
+        }
+        obReport.setPlatformId(metar.getPlatformId());
+        obReport.setStationary(true);
+        obReport.setLatitude((float) metar.getLatitude());
+        obReport.setLongitude((float) metar.getLongitude());
+        // Table data:
+        obReport.setCeiling(metar.getCeiling());
+        obReport.setWindDir(metar.getWindDir());
+        obReport.setWindSpeed(metar.getWindSpeed());
+        obReport.setMaxWindSpeed(metar.getMaxWindSpeed());
+        obReport.setWindGust(metar.getWindGust());
+        obReport.setRelativeHumidity(metar.getRelativeHumidity());
+        try {
+            obReport.setVisibility(metar.getVisibility());
+        } catch (Exception e) {
+            obReport.setVisibility(0);
+        }
+        obReport.setDewpoint(metar.getDewpoint());
+        obReport.setTemperature(metar.getTemperature());
+        obReport.setDewpointDepr(metar.getDewpointDepr());
 
-            obReport.setPresentWx(getPrWX(metar.getPresWeather()));
+        obReport.setPresentWx(getPrWX(metar.getPresWeather()));
 
-            obReport.setHighResWaveHeight(ObConst.MISSING);
-            obReport.setWaveSteepness(metar.getWaveSteepness());
+        obReport.setHighResWaveHeight(ObConst.MISSING);
+        obReport.setWaveSteepness(metar.getWaveSteepness());
 
-			obReport.setSeaLevelPress(metar.getSeaLevelPress());
-			obReport.setWavePeriod(metar.getWavePeriod());
-			obReport.setWindGust(metar.getWindGust());
-			obReport.setPSwellHeight(metar.getPrimarySwellWaveHeight()
-					.floatValue());
-			obReport.setPSwellPeriod(metar.getPrimarySwellWavePeriod());
-			obReport.setPSwellDir(metar.getPrimarySwellWaveDir().floatValue());
-			obReport.setSSwellHeight(metar.getSecondarySwellWaveHeight()
-					.floatValue());
-			obReport.setSSwellPeriod(metar.getSecondarySwellWavePeriod());
-			obReport.setSSwellDir(metar.getSecondarySwellWaveDir().floatValue());
-			obReport.setPressure(metar.getPressureAltimeter());
-			obReport.setPressureChange(metar.getPressChange3Hour());
-			try {
-				obReport.setPressChangeChar(Short.parseShort(metar
-						.getPressChangeChar()));
-			} catch (NumberFormatException e) {
-				obReport.setPressChangeChar((short) 0);
-			}
+        obReport.setSeaLevelPress(metar.getSeaLevelPress());
+        obReport.setWavePeriod(metar.getWavePeriod());
+        obReport.setWindGust(metar.getWindGust());
+        obReport.setPSwellHeight(metar.getPrimarySwellWaveHeight().floatValue());
+        obReport.setPSwellPeriod(metar.getPrimarySwellWavePeriod());
+        obReport.setPSwellDir(metar.getPrimarySwellWaveDir().floatValue());
+        obReport.setSSwellHeight(metar.getSecondarySwellWaveHeight()
+                .floatValue());
+        obReport.setSSwellPeriod(metar.getSecondarySwellWavePeriod());
+        obReport.setSSwellDir(metar.getSecondarySwellWaveDir().floatValue());
+        obReport.setPressure(metar.getPressureAltimeter());
+        obReport.setPressureChange(metar.getPressChange3Hour());
+        try {
+            obReport.setPressChangeChar(Short.parseShort(metar
+                    .getPressChangeChar()));
+        } catch (NumberFormatException e) {
+            obReport.setPressChangeChar((short) 0);
+        }
 
-			obReport.setHourlyPrecip(metar.getHourlyPrecip());
-			// obReport.setPresentWx(WeatherCondition.toCanonicalForm(metar.getWeatherCondition()));
-			obReport.setRawMessage(metar.getRawMessage());
-        // TODO: report type ???
-			obReport.setReportType(ReportType.METAR);
+        obReport.setHourlyPrecip(metar.getHourlyPrecip());
 
-			// if (chosenAppKey == ChosenAppKey.SNOW) {
-			// obReport = ObUtil.generateObReportSnow(obReport,
-			// metar.getMessageData());
-			// } else if (chosenAppKey == ChosenAppKey.FOG) {
-			// obReport = ObUtil.generateObReportFog(obReport,
-			// metar.getSkyCoverage(),
-			// metar.getVertVisibility());
-			// }
-            obReport.setSeaSurfaceTemp(metar.getSeaSurfaceTemp());
-            return obReport;
-	}
+        obReport.setRawMessage(metar.getRawMessage());
+        obReport.setReportType(ReportType.METAR);
+        obReport.setSnincrHourly(metar.getSnincrHourly());
+        obReport.setSnincrTotal(metar.getSnincrTotal());
+        obReport.setFrostbiteTime(metar.getFrostbiteTime());
+        obReport.setWindChill(metar.getWindChill());
+        obReport.setSnowDepth(metar.getSnowDepth());
+        obReport.setSeaSurfaceTemp(metar.getSeaSurfaceTemp());
+        return obReport;
+    }
 
     /**
+     * Construct Present Weather string.
+     * 
      * @param presWeather
      * @return
      */
