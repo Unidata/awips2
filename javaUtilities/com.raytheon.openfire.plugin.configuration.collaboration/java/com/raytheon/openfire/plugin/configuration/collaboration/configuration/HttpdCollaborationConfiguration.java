b235e068b340278ba9e913aa47b8b4ef7f533020
Brian Clements <brian.d.clements@raytheon.com>
2014-01-07 09:49:11 -0600

    Issue #2563 added packet extension to openfire plugin
    openfire plugin now sends configuration in packet extension
    legacy method of putting config in chat message still supported by default
    added configuration to web gui to toggle legacy message method
    
    
    Former-commit-id: 7c83614039c6cdece7a3361af5a514a29a7bedbe [formerly a2c709fb34b90f194083b152c1d9b87a512b57ec] [formerly 2e53fcbccbfc2beda22844fd7489ea16ad98a58c [formerly a26eeaae28de6e41fdc49a2b75ad6814ece478cd]]
    Former-commit-id: 2e53fcbccbfc2beda22844fd7489ea16ad98a58c
    Former-commit-id: 04e7c2f749802199c8f28e91c26d64f5a7b6c794

--- javaUtilities/com.raytheon.openfire.plugin.configuration.collaboration/java/com/raytheon/openfire/plugin/configuration/collaboration/configuration/HttpdCollaborationConfiguration.java
+++ javaUtilities/com.raytheon.openfire.plugin.configuration.collaboration/java/com/raytheon/openfire/plugin/configuration/collaboration/configuration/HttpdCollaborationConfiguration.java
@@ -1,9 +1,24 @@
 /**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
  * 
- */
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.openfire.plugin.configuration.collaboration.configuration;
 
-import com.raytheon.openfire.plugin.configuration.collaboration.util.HttpdCollaborationUtil;
 
 /**
  * Used to store and format the information retrieved from the
@@ -15,7 +30,8 @@ import com.raytheon.openfire.plugin.configuration.collaboration.util.HttpdCollab
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Aug 7, 2012            bkowal     Initial creation
+ * Aug 07, 2012            bkowal      Initial creation
+ * Jan 06, 2013  2563      bclement    removed config preamble
  * 
  * </pre>
  * 
@@ -41,9 +57,7 @@ public class HttpdCollaborationConfiguration {
 
 	@Override
 	public String toString() {
-		return HttpdCollaborationUtil.CONFIG_PREAMBLE + URL_PARAMETER_NAME
-				+ " : " + this.getHttpdCollaborationURL()
-				+ HttpdCollaborationUtil.DIRECTIVE_SUFFIX;
+        return URL_PARAMETER_NAME + " : " + this.getHttpdCollaborationURL();
 	}
 
 	public String getHttpdCollaborationURL() {
