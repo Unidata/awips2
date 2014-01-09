ddea8e6941a0b2917659bd30643a7ab35bfd760b
Max Schenkelberg <max.schenkelberg@raytheon.com>
2014-01-09 10:36:11 -0600

    Issue #2680 Fixed various OpenGL errors
    Change-Id: I7ed70adcb1591a9feb66a4fe80c3249f2e6b339b
    (cherry picked from commit 431ffa97ae4b1c9159c4607e51dbed23c77b76d2 [formerly f4765d157724f4cb4a58a84c773cab5ec12c8c5e] [formerly da402e6c81cf55e9c811cb3237b3f2db47c29567 [formerly 037cd48c627c363350fae940b1969138e9e91c01] [formerly 5d488d27b9d7b7035f7d4173ee816717378eef02] [formerly 5d488d27b9d7b7035f7d4173ee816717378eef02 [formerly 83c600ada9a9b8a70757bccd1b6af023d1637b51]] [formerly da402e6c81cf55e9c811cb3237b3f2db47c29567 [formerly 037cd48c627c363350fae940b1969138e9e91c01] [formerly 5d488d27b9d7b7035f7d4173ee816717378eef02] [formerly 5d488d27b9d7b7035f7d4173ee816717378eef02 [formerly 83c600ada9a9b8a70757bccd1b6af023d1637b51]] [formerly f4765d157724f4cb4a58a84c773cab5ec12c8c5e [formerly 5d488d27b9d7b7035f7d4173ee816717378eef02 [formerly 83c600ada9a9b8a70757bccd1b6af023d1637b51] [formerly f4765d157724f4cb4a58a84c773cab5ec12c8c5e [formerly 59d4c2b9965a7c208863af8f5a3ba4145ea2bbf4]]]]]])
    
    Former-commit-id: 255e27a1f987b0942884af662e3097f5534abea6 [formerly b63edcc8a72b2d330cbb1cdccc7917b5a6ace41e] [formerly 122c3442f80fe851ea4ddfbd121cbbf68dc5276a] [formerly 122c3442f80fe851ea4ddfbd121cbbf68dc5276a [formerly 0c840cf724f11c5f6e83124ccb070cdfd442aa2e]] [formerly 255e27a1f987b0942884af662e3097f5534abea6 [formerly b63edcc8a72b2d330cbb1cdccc7917b5a6ace41e] [formerly 122c3442f80fe851ea4ddfbd121cbbf68dc5276a] [formerly 122c3442f80fe851ea4ddfbd121cbbf68dc5276a [formerly 0c840cf724f11c5f6e83124ccb070cdfd442aa2e]] [formerly d566ed99276e94286271206981916244a6201255 [formerly 122c3442f80fe851ea4ddfbd121cbbf68dc5276a [formerly 0c840cf724f11c5f6e83124ccb070cdfd442aa2e] [formerly d566ed99276e94286271206981916244a6201255 [formerly 6c3817256b4cf6b5deca8d73d0df1d1f89b4ea82]]]]]
    Former-commit-id: d566ed99276e94286271206981916244a6201255
    Former-commit-id: a90e24ddbb74902ea26594231098a76d163e0d0c [formerly b495451e3f1e1861b42540f52a63f58065de46fa] [formerly e1717b272917589b417978c810eb6a33b883655c] [formerly ef2345594a57c6316fe1110309a1334e7f48facf [formerly 5c9afda4f5c9656fe346333485798a21a785033b] [formerly e1717b272917589b417978c810eb6a33b883655c [formerly f5ff746126f812c2866f21e00fd318777db76507]]]
    Former-commit-id: c01d10d401f1821141b2e4b65965ab8156351baf [formerly 59db9c4c77b351f78f275bd48b818837b3a50530] [formerly 6953e818879b6c7f9b65c1315890e56fdcba6b46 [formerly 1cdbdfcebafa973c59041d313b10244c9bdd4d8f]]
    Former-commit-id: 6953e818879b6c7f9b65c1315890e56fdcba6b46
    Former-commit-id: de4a452a0757cdac272ea7fe881f5bf214ec2361

--- cave/com.raytheon.viz.core.gl/localization/glsl/colormap.glsl
+++ cave/com.raytheon.viz.core.gl/localization/glsl/colormap.glsl
@@ -11,22 +11,20 @@ uniform float bkgrndGreen;
 uniform float bkgrndBlue;
 
 void main(void){
-	sampler1D colorMap = colorMapping.colorMap;
 	float logFactor = colorMapping.logFactor;
 	int applyMask = colorMapping.applyMask;
-	sampler1D alphaMask = colorMapping.alphaMask;
 	
 	// Lookup color in colorMap for index
 	float index = gl_TexCoord[0].s;
 	if ( logFactor > 0.0 ) {
 		index = getLogFactorIndex(index, logFactor);
 	}
-	vec4 color = texture1D(colorMap, index).rgba;
+	vec4 color = texture1D(colorMapping.colorMap, index).rgba;
 	
 	// Apply alpha mask if set 
     float alpha = color.a;
     if ( applyMask == 1 ) {
-        if ( texture1D(alphaMask , index ).r != 0.0 ) {
+        if ( texture1D(colorMapping.alphaMask , index ).r != 0.0 ) {
             color = vec4(bkgrndRed, bkgrndGreen, bkgrndBlue, alpha);
         }
     }
