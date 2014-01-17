115c04ac155e4438c21ccab11df49301ebc84ff8
Richard Peter <richard.peter@raytheon.com>
2014-01-17 14:12:34 -0600

    Issue #2125: Clean up hibernate warnings
    Change-Id: Ieb78e7e518a90f09c8db6d72213d5bffa357414e
    
    Former-commit-id: 0c058f461c5d96bed2914453d46726befc1183b6 [formerly f30181d73030e2d28dfa00da5ad1a733d4cff46a [formerly e2d20753e158ca59163f238ad23790a5d20b3e42] [formerly 0c058f461c5d96bed2914453d46726befc1183b6 [formerly 4a99614b887fe02e1ad9831fc9414ceb6808ba7b]]]
    Former-commit-id: f30181d73030e2d28dfa00da5ad1a733d4cff46a [formerly e2d20753e158ca59163f238ad23790a5d20b3e42]
    Former-commit-id: f30181d73030e2d28dfa00da5ad1a733d4cff46a
    Former-commit-id: d7e7ca2e26a29d5083345f6fd1d7765a6a21c193

--- edexOsgi/com.raytheon.uf.edex.database/src/com/raytheon/uf/edex/database/cluster/ClusterTaskPK.java
+++ edexOsgi/com.raytheon.uf.edex.database/src/com/raytheon/uf/edex/database/cluster/ClusterTaskPK.java
@@ -27,7 +27,7 @@ import javax.persistence.Embeddable;
 import com.raytheon.uf.common.serialization.ISerializableObject;
 
 /**
- * TODO Add Description
+ * Primary key for a ClusterTask.
  * 
  * <pre>
  * 
@@ -35,7 +35,7 @@ import com.raytheon.uf.common.serialization.ISerializableObject;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Feb 22, 2010            chammack     Initial creation
- * 
+ * Jan 17, 2014 2125       rjpeter     EmbeddedId should implement hashCode/equals.
  * </pre>
  * 
  * @author chammack
@@ -80,6 +80,45 @@ public class ClusterTaskPK implements ISerializableObject, Serializable {
      */
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result)
+                + ((details == null) ? 0 : details.hashCode());
+        result = (prime * result) + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ClusterTaskPK other = (ClusterTaskPK) obj;
+        if (details == null) {
+            if (other.details != null) {
+                return false;
+            }
+        } else if (!details.equals(other.details)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
     }
 
 }
