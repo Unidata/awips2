f0f1a832c7a835fefd3b3a7730aae868a18a74df
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-01-22 08:21:17 -0500

    VLab Issue #2841 - SWPC delivery for 14.2.1
    Contains functionality for image calculations such
    as differencing and ratios.
    Also includes config files for displaying SWPC gridded terrestrial data.
    Amending commit to address code-review concerns with some java classes.
    
    Change-Id: I3e6e8dbd5015ef825386c1203c333091c6a24f25
    
    Former-commit-id: 5823fba56ae3036682814548004afd076174bb06

--- ncep/gov.noaa.nws.ncep.viz.rsc.solarimage/src/gov/noaa/nws/ncep/viz/rsc/solarimage/rsc/SolarImageResourceData.java
+++ ncep/gov.noaa.nws.ncep.viz.rsc.solarimage/src/gov/noaa/nws/ncep/viz/rsc/solarimage/rsc/SolarImageResourceData.java
@@ -1,11 +1,11 @@
 package gov.noaa.nws.ncep.viz.rsc.solarimage.rsc;
 
-import java.text.ParseException;
-import java.text.ParsePosition;
-
 import gov.noaa.nws.ncep.viz.common.display.NcDisplayType;
 import gov.noaa.nws.ncep.viz.resources.AbstractNatlCntrsRequestableResourceData;
 import gov.noaa.nws.ncep.viz.ui.display.ColorBarFromColormap;
+
+import java.text.ParseException;
+import java.text.ParsePosition;
 
 import javax.measure.unit.Unit;
 import javax.measure.unit.UnitFormat;
@@ -20,151 +20,190 @@ import com.raytheon.uf.viz.core.rsc.AbstractNameGenerator;
 import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
 import com.raytheon.uf.viz.core.rsc.LoadProperties;
 
+/**
+ * The resource data class for Solar Image.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer         Description
+ * ------------ ---------- -----------      --------------------------
+ * 02/21/2013    958       qzhou            Initial creation
+ * 
+ * </pre>
+ * 
+ * @author qzhou
+ * @version 1.0
+ */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlType(name = "SolarImageResourceData")
-public class SolarImageResourceData extends AbstractNatlCntrsRequestableResourceData {
+public class SolarImageResourceData extends
+        AbstractNatlCntrsRequestableResourceData {
 
-	private static final String instrumentParam = "instrument";
-	private static final String wavelengthParam = "wavelength";
-	private static final String intTimeParam    = "intTime";
+    private static final String instrumentParam = "instrument";
 
-	@XmlElement
-	private Float alpha;
+    private static final String wavelengthParam = "wavelength";
 
-	@XmlElement
-	private Float brightness;
-	
-	@XmlElement
-	private Float contrast;
-	
-	@XmlElement
+    private static final String intTimeParam = "intTime";
+
+    @XmlElement
+    private Float alpha;
+
+    @XmlElement
+    private Float brightness;
+
+    @XmlElement
+    private Float contrast;
+
+    @XmlElement
+    private Integer cylindrical;
+
+    @XmlElement
     private String colorMapName;
-	
-	@XmlElement
+
+    @XmlElement
     private ColorBarFromColormap colorBar;
-	
-	@XmlElement
-	private String displayUnitStr;
-	
-	private Unit<?> displayUnit;
-    
+
+    @XmlElement
+    private String displayUnitStr;
+
+    @XmlElement
+    private String imageFunction;
+
+    private Unit<?> displayUnit;
+
     public SolarImageResourceData() {
 
         super();
-        
+
         // called by AbstractVizResource.getName()
         // and we delegate back to the resource
         this.nameGenerator = new AbstractNameGenerator() {
 
             @Override
             public String getName(AbstractVizResource<?, ?> resource) {
-            	return ((SolarImageResource)resource).getLegendStr();
+                return ((SolarImageResource) resource).getLegendStr();
             }
         };
     }
-    
-	@Override
-	public NcDisplayType[] getSupportedDisplayTypes() {
-		return new NcDisplayType[] { NcDisplayType.SOLAR_DISPLAY };
-	}
 
-	// The following methods assume that the constraints in the metadata map
-	// are are for 'exact' matches. ie. that the one constraint value is the value
-	// that will be queried.
-    public String getInstrument() {
-    	return ( metadataMap.containsKey(instrumentParam) ?
-			     metadataMap.get(instrumentParam).getConstraintValue() : "" );
+    @Override
+    public NcDisplayType[] getSupportedDisplayTypes() {
+        return new NcDisplayType[] { NcDisplayType.SOLAR_DISPLAY };
     }
-    
+
+    // The following methods assume that the constraints in the metadata map
+    // are are for 'exact' matches. ie. that the one constraint value is the
+    // value
+    // that will be queried.
+    public String getInstrument() {
+        return (metadataMap.containsKey(instrumentParam) ? metadataMap.get(
+                instrumentParam).getConstraintValue() : "");
+    }
+
     public String getWavelength() {
-    	return ( metadataMap.containsKey(wavelengthParam) ?
-			     metadataMap.get(wavelengthParam).getConstraintValue() : "" );
+        return (metadataMap.containsKey(wavelengthParam) ? metadataMap.get(
+                wavelengthParam).getConstraintValue() : "");
     }
 
     public String getIntTime() {
-    	return ( metadataMap.containsKey(intTimeParam) ?
-			     metadataMap.get(intTimeParam).getConstraintValue() : "" );
+        return (metadataMap.containsKey(intTimeParam) ? metadataMap.get(
+                intTimeParam).getConstraintValue() : "");
     }
 
-	@Override
-	protected AbstractVizResource<?, ?> constructResource(
-			LoadProperties loadProperties, PluginDataObject[] objects)
-			throws VizException {
-		return new SolarImageResource(this, loadProperties);
-	}
-	
-	public Unit<?> getDisplayUnit() {
-		if( displayUnit == null ) {
-			setDisplayUnitStr( displayUnitStr );
-		}
-		return displayUnit;
-	}
-	
-	public String getDisplayUnitStr() {
-		return displayUnitStr;
-	}
+    public String getImageFunction() {
 
-	public void setDisplayUnitStr(String dispUnitStr) {
-		this.displayUnitStr = dispUnitStr;
+        return imageFunction;
+    }
 
-		if( displayUnit == null ) {
-			if( displayUnitStr != null ) {
-	            try {
-	            	displayUnit = UnitFormat.getUCUMInstance().parseSingleUnit(
-	            			 displayUnitStr, new ParsePosition(0));
-	            } catch (ParseException e) {
-	                System.out.println("Unable parse display units : " + displayUnitStr );
-	            }
-			}
-		}
-	}
-	
-	public String getColorMapName() {
-		return colorMapName;
-	}
+    public void setImageFunction(String imageFunction) {
+        this.imageFunction = imageFunction;
+    }
 
-	public void setColorMapName(String cmapName) {
-		colorMapName = cmapName;
-	}
-	
+    @Override
+    protected AbstractVizResource<?, ?> constructResource(
+            LoadProperties loadProperties, PluginDataObject[] objects)
+            throws VizException {
+        return new SolarImageResource(this, loadProperties);
+    }
+
+    public Unit<?> getDisplayUnit() {
+        if (displayUnit == null) {
+            setDisplayUnitStr(displayUnitStr);
+        }
+        return displayUnit;
+    }
+
+    public String getDisplayUnitStr() {
+        return displayUnitStr;
+    }
+
+    public void setDisplayUnitStr(String dispUnitStr) {
+        this.displayUnitStr = dispUnitStr;
+
+        if (displayUnit == null) {
+            if (displayUnitStr != null) {
+                try {
+                    displayUnit = UnitFormat.getUCUMInstance().parseSingleUnit(
+                            displayUnitStr, new ParsePosition(0));
+                } catch (ParseException e) {
+                    System.out.println("Unable parse display units : "
+                            + displayUnitStr);
+                }
+            }
+        }
+    }
+
+    public String getColorMapName() {
+        return colorMapName;
+    }
+
+    public void setColorMapName(String cmapName) {
+        colorMapName = cmapName;
+    }
+
     public ColorBarFromColormap getColorBar() {
-		return colorBar;
-	}
+        return colorBar;
+    }
 
-	public void setColorBar(ColorBarFromColormap cBar) {
-		this.colorBar = cBar;
-	}
-	
+    public void setColorBar(ColorBarFromColormap cBar) {
+        this.colorBar = cBar;
+    }
+
     public Float getAlpha() {
-		return alpha;
-	}
+        return alpha;
+    }
 
+    public void setAlpha(Float alpha) {
+        this.alpha = alpha;
+    }
 
-	public void setAlpha(Float alpha) {
-		this.alpha = alpha;
-	}
+    public Float getBrightness() {
+        return brightness;
+    }
 
+    public void setBrightness(Float brightness) {
+        this.brightness = brightness;
+    }
 
-	public Float getBrightness() {
-		return brightness;
-	}
+    public Float getContrast() {
+        return contrast;
+    }
 
+    public void setContrast(Float contrast) {
+        this.contrast = contrast;
+    }
 
-	public void setBrightness(Float brightness) {
-		this.brightness = brightness;
-	}
+    public Integer getCylindrical() {
+        return cylindrical;
+    }
 
+    public void setCylindrical(Integer cylindrical) {
+        this.cylindrical = cylindrical;
+    }
 
-	public Float getContrast() {
-		return contrast;
-	}
-
-	public void setContrast(Float contrast) {
-		this.contrast = contrast;
-	}
-
-
-	@Override
+    @Override
     public boolean equals(Object obj) {
         if (!super.equals(obj)) {
             return false;
@@ -183,7 +222,7 @@ public class SolarImageResourceData extends AbstractNatlCntrsRequestableResource
                 && this.colorMapName.equals(other.colorMapName) == false) {
             return false;
         }
-        
+
         if (this.displayUnitStr != null && other.displayUnitStr == null) {
             return false;
         } else if (this.displayUnitStr == null && other.displayUnitStr != null) {
@@ -192,29 +231,30 @@ public class SolarImageResourceData extends AbstractNatlCntrsRequestableResource
                 && this.displayUnitStr.equals(other.displayUnitStr) == false) {
             return false;
         }
-       
-        
-        if ( (this.alpha != null && other.alpha == null)
-        		|| (this.alpha == null && other.alpha != null)
-        		|| (this.alpha != null && this.alpha.equals(other.alpha) == false)){
-        	return false;
-        	
-	    }
-        	
-        if ( (this.brightness != null && other.brightness == null)
-        		|| (this.brightness == null && other.brightness != null)
-        		|| (this.brightness != null && this.brightness.equals(other.brightness) == false)){
-        	return false;
-        	
-	    }
-        
-        if ( (this.contrast != null && other.contrast == null)
-        		|| (this.contrast == null && other.contrast != null)
-        		|| (this.contrast != null && this.contrast.equals(other.contrast) == false)){
-        	return false;
-        	
-	    }
-        
+
+        if ((this.alpha != null && other.alpha == null)
+                || (this.alpha == null && other.alpha != null)
+                || (this.alpha != null && this.alpha.equals(other.alpha) == false)) {
+            return false;
+
+        }
+
+        if ((this.brightness != null && other.brightness == null)
+                || (this.brightness == null && other.brightness != null)
+                || (this.brightness != null && this.brightness
+                        .equals(other.brightness) == false)) {
+            return false;
+
+        }
+
+        if ((this.contrast != null && other.contrast == null)
+                || (this.contrast == null && other.contrast != null)
+                || (this.contrast != null && this.contrast
+                        .equals(other.contrast) == false)) {
+            return false;
+
+        }
+
         return true;
     }
 }
