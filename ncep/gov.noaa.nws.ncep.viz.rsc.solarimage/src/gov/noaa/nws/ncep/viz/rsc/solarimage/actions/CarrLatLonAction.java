6d9136b9fa24d6695ec2d99d652b2fe1bfc57971
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-01-22 08:21:17 -0500

    VLab Issue #2841 - SWPC delivery for 14.2.1
    Contains functionality for image calculations such
    as differencing and ratios.
    Also includes config files for displaying SWPC gridded terrestrial data.
    Amending commit to address code-review concerns with some java classes.
    
    Change-Id: I3e6e8dbd5015ef825386c1203c333091c6a24f25
    
    Former-commit-id: bcccb662ede903006195a97cc12b9fd645e23fd4 [formerly f841786213ce5bee3b4eb2d9fbc2093ee3a43718] [formerly 90a5cf53d3ce326999ac1bfde08bfaaf62f7ffd7] [formerly 90a5cf53d3ce326999ac1bfde08bfaaf62f7ffd7 [formerly e02136a026a635e1356527ce5ec94930bf41dac8]] [formerly bcccb662ede903006195a97cc12b9fd645e23fd4 [formerly f841786213ce5bee3b4eb2d9fbc2093ee3a43718] [formerly 90a5cf53d3ce326999ac1bfde08bfaaf62f7ffd7] [formerly 90a5cf53d3ce326999ac1bfde08bfaaf62f7ffd7 [formerly e02136a026a635e1356527ce5ec94930bf41dac8]] [formerly f0f1a832c7a835fefd3b3a7730aae868a18a74df [formerly 90a5cf53d3ce326999ac1bfde08bfaaf62f7ffd7 [formerly e02136a026a635e1356527ce5ec94930bf41dac8] [formerly f0f1a832c7a835fefd3b3a7730aae868a18a74df [formerly 5823fba56ae3036682814548004afd076174bb06]]]]]
    Former-commit-id: f0f1a832c7a835fefd3b3a7730aae868a18a74df
    Former-commit-id: 536dbbe36d5449f2bcca7e4e17c71955777e7563 [formerly 3d70db144fda7d9a905780eb3aaeee4b9305e326] [formerly 0c053a145b1f0e31e1087596f4dbd410b1241038] [formerly b084c86a7b93985367e046baf1b3239542482172 [formerly 94129f483f09e5269c4bcaed7df0cc11fee8fd8f] [formerly 0c053a145b1f0e31e1087596f4dbd410b1241038 [formerly 25ad64175cced81a1c9aebe402b27a652607b8c8]]]
    Former-commit-id: 894188b09f87b1ef308378c3eb8a17271b6d4459 [formerly 51d88b7968dbe16f577ea62d0699b46d048806b3] [formerly 14b5ceaa177a38e060bab22349a042c0660e17e2 [formerly a8426c759bec70ef12f739a7498011cd0898b6b3]]
    Former-commit-id: 14b5ceaa177a38e060bab22349a042c0660e17e2
    Former-commit-id: 80ca139a7b6b0a569ad3b5dd86f0afaff338df05

--- ncep/gov.noaa.nws.ncep.viz.rsc.solarimage/src/gov/noaa/nws/ncep/viz/rsc/solarimage/actions/CarrLatLonAction.java
+++ ncep/gov.noaa.nws.ncep.viz.rsc.solarimage/src/gov/noaa/nws/ncep/viz/rsc/solarimage/actions/CarrLatLonAction.java
@@ -1,13 +1,13 @@
 package gov.noaa.nws.ncep.viz.rsc.solarimage.actions;
 
+import gov.noaa.nws.ncep.viz.rsc.solarimage.rsc.SolarImageResource;
+
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
-
-import gov.noaa.nws.ncep.viz.rsc.solarimage.rsc.SolarImageResource;
 
 import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
 import com.raytheon.viz.ui.cmenu.AbstractRightClickAction;
@@ -23,19 +23,18 @@ import com.raytheon.viz.ui.cmenu.AbstractRightClickAction;
  *  Date         Ticket#     Engineer    Description
  *  ------------ ----------  ----------- --------------------------
  * Feb 21, 2013  958         qzhou     Initial Creation.
- * 
+ * 12/17/2013    958         qzhou     Fixed cylind and latlon context menu leftover problem
+ * 01/22/2014    1046        qzhou     Move enum to MenuConstants
  * </pre>
  * 
  * @author qzhou
  * @version 1
  */
 
-public class CarrLatLonAction extends AbstractRightClickAction 
-	implements  IMenuCreator {
-	
-	private Menu menu;	
-	 
-	private static String[] latLonIntervals = {"No Overlay", "10", "15",  "30", "45", "60"};
+public class CarrLatLonAction extends AbstractRightClickAction implements
+        IMenuCreator {
+
+    private Menu menu;
 
     /**
      * Default constructor.
@@ -48,11 +47,10 @@ public class CarrLatLonAction extends AbstractRightClickAction
 
     @Override
     public IMenuCreator getMenuCreator() {
-    	this.setChecked(false);
+        this.setChecked(false);
         return this;
     }
 
-    
     @Override
     public void dispose() {
         if (menu != null) {
@@ -68,7 +66,7 @@ public class CarrLatLonAction extends AbstractRightClickAction
         }
         menu = new Menu(parent);
         fillMenu(menu, parent.getDisplay());
-        
+
         return menu;
     }
 
@@ -79,50 +77,55 @@ public class CarrLatLonAction extends AbstractRightClickAction
         }
         menu = new Menu(parent);
         fillMenu(menu, parent.getDisplay());
-       
+
         return menu;
     }
 
     public void fillMenu(Menu menu, Display d) {
-    	
-    	AbstractVizResource<?, ?> rsc = getSelectedRsc();
-    	if (rsc instanceof SolarImageResource) {
-	    	
-	        boolean found = false;
-	        String currentInterval = getTopMostSelectedResource().getCapability(
-	        		CarrLatLonCapability.class).getInterval();
-	        
-	        for (String intvl : latLonIntervals) {
-	            boolean selected = intvl.equals(currentInterval);
-	            found |= selected;
-	            ActionContributionItem actionItem = new ActionContributionItem(
-	                    new ChangeLatLonIntervalInternalAction(rsc, intvl, d, selected));
-	           actionItem.fill(menu, -1);
-	           
-	        }
-	
-	        if (!found) {
-	            ActionContributionItem actionItem = new ActionContributionItem(
-	                    new ChangeLatLonIntervalInternalAction(rsc, currentInterval, d, true));
-	            actionItem.fill(menu, -1);	            
-	        }
-    	}
+
+        AbstractVizResource<?, ?> rsc = getSelectedRsc();
+        if (rsc instanceof SolarImageResource) {
+
+            boolean found = false;
+            String currentInterval = getTopMostSelectedResource()
+                    .getCapability(CarrLatLonCapability.class).getInterval();
+
+            for (String intvl : MenuConstants.latLonIntervals) {
+                boolean selected = intvl.equals(currentInterval);
+                found |= selected;
+                ActionContributionItem actionItem = new ActionContributionItem(
+                        new ChangeLatLonIntervalInternalAction(rsc, intvl, d,
+                                selected));
+                actionItem.fill(menu, -1);
+
+            }
+
+            if (!found) {
+                ActionContributionItem actionItem = new ActionContributionItem(
+                        new ChangeLatLonIntervalInternalAction(rsc,
+                                currentInterval, d, true));
+                actionItem.fill(menu, -1);
+            }
+        }
     }
 
     private class ChangeLatLonIntervalInternalAction extends Action {
         private String interval;
+
         AbstractVizResource<?, ?> resource = null;
 
-        public ChangeLatLonIntervalInternalAction(AbstractVizResource<?, ?> resource, String intvl, Display d, boolean selected) {
+        public ChangeLatLonIntervalInternalAction(
+                AbstractVizResource<?, ?> resource, String intvl, Display d,
+                boolean selected) {
             super(intvl);
             this.resource = resource;
-            this.interval = intvl;           
-        	this.setChecked(false);
-                  
+            this.interval = intvl;
+            this.setChecked(false);
+
             if (selected) {
-               this.setChecked(true);
-            } 
-                       
+                this.setChecked(true);
+            }
+
         }
 
         /*
@@ -142,24 +145,40 @@ public class CarrLatLonAction extends AbstractRightClickAction
          */
         @Override
         public void run() {
-        	
-        	if (interval != null) {
-            		
-        		if (this.getText().equals("No Overlay")) {
-            		((SolarImageResource) resource).setLatLonOverlay(false);
-            		((SolarImageResource) resource).isCarrington = false;
-        		}
-        		else {
-        			((SolarImageResource) resource).setLatLonOverlay(true);
-        			((SolarImageResource) resource).isCarrington = true;
-        		}
 
-        		getTopMostSelectedResource().getCapability(CarrLatLonCapability.class).setInterval(interval);
-        		getTopMostSelectedResource().getCapability(StonyLatLonCapability.class).setInterval("No Overlay");	
-        	}  
+            if (interval != null) {
+
+                if (this.getText().equals(MenuConstants.latLonIntervals[0])) {
+                    if (getTopMostSelectedResource()
+                            .getCapability(StonyLatLonCapability.class)
+                            .getInterval()
+                            .equals(MenuConstants.latLonIntervals[0])) {
+                        ((SolarImageResource) resource).setLatLonOverlay(false);
+                        ((SolarImageResource) resource).isCarrington = false;
+                        getTopMostSelectedResource().getCapability(
+                                CarrLatLonCapability.class).setInterval(
+                                interval);
+                    }
+                } else {
+                    if (!getTopMostSelectedResource()
+                            .getCapability(CylindricalCedCapability.class)
+                            .getCylind().equals(MenuConstants.projections[1])) {
+                        ((SolarImageResource) resource).setLatLonOverlay(true);
+                        ((SolarImageResource) resource).isCarrington = true;
+                        getTopMostSelectedResource().getCapability(
+                                CarrLatLonCapability.class).setInterval(
+                                interval);
+
+                        getTopMostSelectedResource().getCapability(
+                                StonyLatLonCapability.class).setInterval(
+                                MenuConstants.latLonIntervals[0]);
+                    }
+                }
+
+            }
             getContainer().refresh();
         }
-    } 
+    }
 
     /*
      * (non-Javadoc)
@@ -172,4 +191,3 @@ public class CarrLatLonAction extends AbstractRightClickAction
     }
 
 }
-
