f78b216bcd13c28b1957f20b9adf154afa696f0a
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-01-24 10:46:37 -0500

    VLab Issue #2867 - NCEP CAVE plugin delivery for 14.2.1
    Many changes in NCEP Viz plugins for 14.2.1 enhancements and big fixes
    for the National Centers Perspective and PGEN
    
    Change-Id: I4fa707cf0844800a4c77961e110338daafbb0354
    
    Former-commit-id: 14d218ddc51a93d66ea2f6e51f13d9d65d8c501d [formerly 14d218ddc51a93d66ea2f6e51f13d9d65d8c501d [formerly e3a35c9c3eb2184818b21ba1b23aac988a0148ee]]
    Former-commit-id: 369d2592fef50574dc0b40de74026bec6bd31261
    Former-commit-id: 648069308933b43d192b3a31c014e3deec49d938

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/attrdialog/AttrSettings.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/attrdialog/AttrSettings.java
@@ -10,6 +10,12 @@ package gov.noaa.nws.ncep.ui.pgen.attrdialog;
 
 import gov.noaa.nws.ncep.ui.pgen.PgenStaticDataProvider;
 import gov.noaa.nws.ncep.ui.pgen.elements.AbstractDrawableComponent;
+import gov.noaa.nws.ncep.ui.pgen.elements.DECollection;
+import gov.noaa.nws.ncep.ui.pgen.elements.DrawableElement;
+import gov.noaa.nws.ncep.ui.pgen.elements.Layer;
+import gov.noaa.nws.ncep.ui.pgen.elements.Line;
+import gov.noaa.nws.ncep.ui.pgen.elements.Outlook;
+import gov.noaa.nws.ncep.ui.pgen.elements.Product;
 import gov.noaa.nws.ncep.ui.pgen.elements.Text;
 import gov.noaa.nws.ncep.ui.pgen.elements.Track;
 import gov.noaa.nws.ncep.ui.pgen.file.FileTools;
@@ -20,6 +26,7 @@ import gov.noaa.nws.ncep.ui.pgen.productmanage.ProductConfigureDialog;
 import java.io.File;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -41,7 +48,7 @@ import com.raytheon.uf.viz.core.exception.VizException;
  * 08/11		?			B. Yin		Put current time for Storm Track
  * 11/11		?  			B. Yin		Load settings for different Activity Type.
  * 09/12					B. Hebbard	Merge changes by RTS OB12.9.1 (format only, this file)
- * 
+ * 10/13		TTR768		J. Wu		Load/set default attributes for outlook labels (Text).
  * </pre>
  * 
  * @author	J. Wu
@@ -51,13 +58,20 @@ public class AttrSettings {
 	
 	private static AttrSettings  INSTANCE = null;
 	
+    /**
+     * General Settings.
+     */
 	private static HashMap<String, AbstractDrawableComponent> settings = null;
-
-    private static String settingsTblLocal = "." + File.separator;   
-
     public static String settingsFileName = "settings_tbl.xml";
-    
-	/**
+
+    /**
+     * Settings for outlook.
+     */
+    public static String outlookSettingsFileName = "outlooksettings.xml";    
+	private static HashMap<String, AbstractDrawableComponent> outlookLineSettings = null;
+	private static HashMap<String, AbstractDrawableComponent> outlookLabelSettings = null;
+
+    /**
 	 * Private constructor
      * 
 	 * @throws VizException
@@ -66,6 +80,8 @@ public class AttrSettings {
         super();
         settings = new HashMap<String, AbstractDrawableComponent>();
         loadSettingsTable();
+        
+        loadOutlookSettings();
 	}
 	
 	/**
@@ -98,6 +114,20 @@ public class AttrSettings {
 	}
 
 	/**
+	 * Gets the default line settings for outlook loaded from the outlooksettings.xml.
+	 */
+	public HashMap<String, AbstractDrawableComponent> getOutlookLineSettings() {				
+		return outlookLineSettings;
+	}
+
+	/**
+	 * Gets the default label (text) settings for outlook loaded from the outlooksettings.xml.
+	 */
+	public HashMap<String, AbstractDrawableComponent> getOutlookLabelSettings() {				
+		return outlookLabelSettings;
+	}
+	
+	/**
      * @param value
      *            the DrawableElement to set gilbert: noticed this wasn't being
      *            used AND I wasn't sure how to modify it for the new pgenType
@@ -125,15 +155,14 @@ public class AttrSettings {
 	}
 	
 	/**
-     * Load default settings from settings_tbl.xml First try to load from user's
-     * local directory; if not found, load the base directory.
+     * Load default settings from settings_tbl.xml from localization.
 	 */
 	private void loadSettingsTable() {		
 		
 		/*
 		 *  Get the settings table file from localization
 		 */		
-		File settingsFile =		PgenStaticDataProvider.getProvider().getFile(
+		File settingsFile =	PgenStaticDataProvider.getProvider().getFile(
                 PgenStaticDataProvider.getProvider().getPgenLocalizationRoot()
                         + settingsFileName);
 		
@@ -144,6 +173,9 @@ public class AttrSettings {
 		loadPgenSettings( settingsFile.getAbsolutePath() );
 	}
 	
+	/**
+     * Load default settings defined for an PGEN activity.
+	 */
 	public void loadProdSettings( String prodName ){
 		
 		if ( prodName == null || prodName.isEmpty() ){
@@ -171,6 +203,9 @@ public class AttrSettings {
 		}
 	}
 	
+	/**
+     * Read in a settings file from the input file.
+	 */
 	public boolean loadPgenSettings( String fileName ){
 		
 		boolean ret = false;
@@ -180,57 +215,122 @@ public class AttrSettings {
 		File sFile = new File( fileName );
 		
 		try {
-		if ( sFile.canRead() ){
-			Products products = FileTools.read( fileName );
-	        
-	        List<gov.noaa.nws.ncep.ui.pgen.elements.Product> prds;
-	        
-	        prds = ProductConverter.convert( products );
-	               
-	        for ( gov.noaa.nws.ncep.ui.pgen.elements.Product p:prds ) {
-	        	
-                    for (gov.noaa.nws.ncep.ui.pgen.elements.Layer layer : p
-                            .getLayers()) {
-	        		
-                        for (gov.noaa.nws.ncep.ui.pgen.elements.AbstractDrawableComponent de : layer
-                                .getDrawables()) {
-	        		    
-	                    String pgenID = null;
-	                    pgenID = de.getPgenType();
+			if ( sFile.canRead() ){
+				Products products = FileTools.read( fileName );
 
-	        			if ( pgenID != null ) {
-	       				    newSettings.put( pgenID, de );        			
-	        			}
-	        			
-	        			if ( pgenID.equalsIgnoreCase("General Text")){
-	        				((Text)de).setText(new String[]{""});
-                            } else if (pgenID.equalsIgnoreCase("STORM_TRACK")) {
-	        				//set Track time to current time
-                                Calendar cal1 = Calendar.getInstance(TimeZone
-                                        .getTimeZone("GMT"));
-	        				Calendar cal2 = (Calendar)cal1.clone();
-	        				cal2.add(Calendar.HOUR_OF_DAY, 1);
-	        				
-	        				((Track)de).setFirstTimeCalendar( cal1);
-	        				((Track)de).setSecondTimeCalendar( cal2);
+				List<gov.noaa.nws.ncep.ui.pgen.elements.Product> prds;
 
-	        			}
-	        		}      	          		
-	        	}
-	        }
-	        
-	        if ( newSettings.size() > 0 ) {
-	        	settings.clear();
-	        	settings.putAll(newSettings);
-	        	ret = true;
-	        }
-	        
-		}
+				prds = ProductConverter.convert( products );
+
+				for ( gov.noaa.nws.ncep.ui.pgen.elements.Product p:prds ) {
+
+					for (gov.noaa.nws.ncep.ui.pgen.elements.Layer layer : p
+							.getLayers()) {
+
+						for (gov.noaa.nws.ncep.ui.pgen.elements.AbstractDrawableComponent de : layer
+								.getDrawables()) {
+
+							String pgenID = null;
+							pgenID = de.getPgenType();
+
+							if ( pgenID != null ) {
+								newSettings.put( pgenID, de );        			
+							}
+
+							if ( pgenID.equalsIgnoreCase("General Text")){
+								((Text)de).setText(new String[]{""});
+							} else if (pgenID.equalsIgnoreCase("STORM_TRACK")) {
+								//set Track time to current time
+								Calendar cal1 = Calendar.getInstance(TimeZone
+										.getTimeZone("GMT"));
+								Calendar cal2 = (Calendar)cal1.clone();
+								cal2.add(Calendar.HOUR_OF_DAY, 1);
+
+								((Track)de).setFirstTimeCalendar( cal1);
+								((Track)de).setSecondTimeCalendar( cal2);
+
+							}
+						}      	          		
+					}
+				}
+
+				if ( newSettings.size() > 0 ) {
+					settings.clear();
+					settings.putAll(newSettings);
+					ret = true;
+				}
+
+			}
         } catch (Exception e) {
 			ret = false;
 		}
 		
 		return ret;
+	}
+	
+	/**
+	 *  Load default settings from outlooksettings.xml
+	 *  
+	 *  Setting is a hash map with outlook type and label string as keys.
+	 */
+	private static void loadOutlookSettings() {		
+		
+		if ( outlookLineSettings == null ) {
+		    outlookLineSettings = new HashMap<String, AbstractDrawableComponent>();
+		}
+		
+		if ( outlookLabelSettings == null ) {
+		    outlookLabelSettings = new HashMap<String, AbstractDrawableComponent>();
+		}
+		
+		String settingFile = PgenStaticDataProvider.getProvider().getFileAbsolutePath(
+				   PgenStaticDataProvider.getProvider().getPgenLocalizationRoot() + outlookSettingsFileName );
+	
+		gov.noaa.nws.ncep.ui.pgen.file.Products products = FileTools.read( settingFile );
+		
+        if ( products != null ){
+        	List<Product> prds;
+
+        	prds = ProductConverter.convert( products );
+
+        	for ( Product p:prds ) {
+
+        		for ( Layer layer:p.getLayers() ) {
+
+        			Iterator<AbstractDrawableComponent> it = layer.getComponentIterator();
+        			while( it.hasNext()){
+        				AbstractDrawableComponent adc = it.next();
+        				if ( adc.getName().equalsIgnoreCase("OUTLOOK")) {
+        					Iterator<AbstractDrawableComponent> itLn = ((Outlook)adc).getComponentIterator();
+        					while ( itLn.hasNext() ){
+        						AbstractDrawableComponent lnGrp = itLn.next();
+        						if ( lnGrp.getName().equalsIgnoreCase(Outlook.OUTLOOK_LABELED_LINE) ) {
+        							String key = null;
+        							Line ln = null;
+        							Text txt = null;
+        							Iterator<DrawableElement> itDe = ((DECollection)lnGrp).createDEIterator();
+        							while( itDe.hasNext() ){
+        								DrawableElement de = itDe.next();
+        								if( de instanceof Text ){
+        									txt = (Text)de;
+        									key = ((Outlook)adc).getOutlookType() + txt.getText()[0];
+        								}
+        								else if ( de instanceof Line ){
+        									ln = (Line) de;
+        								}
+        							}
+        							
+        							if ( key != null && ln != null ){
+        								outlookLineSettings.put( key, ln );
+        								outlookLabelSettings.put( key, txt );
+        							}       						
+        						}        						
+        					}       					
+        				}
+        			}      	          		
+        		}
+        	}        
+        }
 	}
 	   
 }
--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/elements/MultiPointElement.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/elements/MultiPointElement.java
@@ -7,13 +7,13 @@
  */
 package gov.noaa.nws.ncep.ui.pgen.elements;
 
+import gov.noaa.nws.ncep.ui.pgen.display.FillPatternList.FillPattern;
+import gov.noaa.nws.ncep.ui.pgen.display.IAttribute;
+import gov.noaa.nws.ncep.ui.pgen.display.IMultiPoint;
+
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
-
-import gov.noaa.nws.ncep.ui.pgen.display.IAttribute;
-import gov.noaa.nws.ncep.ui.pgen.display.IMultiPoint;
-import gov.noaa.nws.ncep.ui.pgen.display.FillPatternList.FillPattern;
 
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
@@ -34,307 +34,316 @@ import com.vividsolutions.jts.geom.impl.CoordinateArraySequence;
  * 07/09		#131		J. Wu   	Avoided null FillPattern.
  * 10/09		#160		G. Zhang	Added Sigmet: attr,getter/setter,update, getPgenCategory
  * 04/11		#?			B. Yin		Re-factor IAttribute
- *
+ * 
  * </pre>
  * 
- * @author	J. Wu
- * @version	0.0.1
+ * @author J. Wu
+ * @version 0.0.1
  */
-public abstract class MultiPointElement extends DrawableElement implements IMultiPoint {
-	
-	protected Color[]			colors;
-	protected float 			lineWidth;
-	protected double			sizeScale;
-	protected boolean			closed;
-	protected boolean			filled;
-	protected int				smoothFactor;
-	protected FillPattern		fillPattern;
-	protected ArrayList<Coordinate>	linePoints;
-	IAttribute 		attr;// for SIGMET
-	
-	/**
-	 *  Default constructor
-	 */
-	protected MultiPointElement() {
-		colors = new Color[]{Color.red};
-		lineWidth = (float)1.0;
-		sizeScale = 1.0;
-		closed = false;
-		filled = false;
-		linePoints = null;
-		smoothFactor = 2;
-		fillPattern = FillPattern.SOLID;
-	}
+public abstract class MultiPointElement extends DrawableElement implements
+        IMultiPoint {
 
-	/**
-	 * @param range
-	 * @param colors
-	 * @param lineWidth
-	 * @param sizeScale
-	 * @param closed
-	 * @param filled
-	 * @param linePoints
-	 * @param smoothFactor
-	 * @param fillPattern
-	 * @param linePattern
-	 */
-	protected MultiPointElement(Coordinate[] range,
-			Color[] colors, float lineWidth, double sizeScale, boolean closed,
-			boolean filled, List<Coordinate> linePoints, int smoothFactor,
-			FillPattern fillPattern, String pgenCategory, String pgenType) {
-		super(range, pgenCategory, pgenType);
-		this.colors = colors;
-		this.lineWidth = lineWidth;
-		this.sizeScale = sizeScale;
-		this.closed = closed;
-		this.filled = filled;
-		this.smoothFactor = smoothFactor;
-		
-		this.linePoints = new ArrayList<Coordinate>();
-		
-		if ( linePoints != null ) {
-			this.linePoints.addAll(linePoints);
-		}
-			
-		FillPattern fp = FillPattern.SOLID;
-		if ( fillPattern != null ) {
-		    this.fillPattern = fillPattern;
-		}
-		else {
-			this.fillPattern = fp;
-		}
+    protected Color[] colors;
 
-	}
+    protected float lineWidth;
 
-	/**
-	 * Gets the Lat/lon location of the object
-	 * @return Lat/lon coordinate
-	 */
-	@Override
+    protected double sizeScale;
+
+    protected boolean closed;
+
+    protected boolean filled;
+
+    protected int smoothFactor;
+
+    protected FillPattern fillPattern;
+
+    protected ArrayList<Coordinate> linePoints;
+
+    IAttribute attr;// for SIGMET
+
+    /**
+     * Default constructor
+     */
+    protected MultiPointElement() {
+        colors = new Color[] { Color.red };
+        lineWidth = (float) 1.0;
+        sizeScale = 1.0;
+        closed = false;
+        filled = false;
+        linePoints = null;
+        smoothFactor = 2;
+        fillPattern = FillPattern.SOLID;
+    }
+
+    /**
+     * @param range
+     * @param colors
+     * @param lineWidth
+     * @param sizeScale
+     * @param closed
+     * @param filled
+     * @param linePoints
+     * @param smoothFactor
+     * @param fillPattern
+     * @param linePattern
+     */
+    protected MultiPointElement(Coordinate[] range, Color[] colors,
+            float lineWidth, double sizeScale, boolean closed, boolean filled,
+            List<Coordinate> linePoints, int smoothFactor,
+            FillPattern fillPattern, String pgenCategory, String pgenType) {
+        super(range, pgenCategory, pgenType);
+        this.colors = colors;
+        this.lineWidth = lineWidth;
+        this.sizeScale = sizeScale;
+        this.closed = closed;
+        this.filled = filled;
+        this.smoothFactor = smoothFactor;
+
+        this.linePoints = new ArrayList<Coordinate>();
+
+        if (linePoints != null) {
+            this.linePoints.addAll(linePoints);
+        }
+
+        FillPattern fp = FillPattern.SOLID;
+        if (fillPattern != null) {
+            this.fillPattern = fillPattern;
+        } else {
+            this.fillPattern = fp;
+        }
+
+    }
+
+    /**
+     * Gets the Lat/lon location of the object
+     * 
+     * @return Lat/lon coordinate
+     */
+    @Override
     public Coordinate[] getLinePoints() {
-		
-		Coordinate a[] = new Coordinate[ linePoints.size() ];
-		linePoints.toArray(a);
-		return a;
-			
-	}
-	
-	/**
-	 * Gets array of colors associated with the object
-	 * @return Color array
-	 */
-	@Override
-	public Color[] getColors() {
-		return colors;
-	}
-	
-	/**
-	 * Gets the width of the line pattern
-	 * @return line width
-	 */
-	@Override
-	public float getLineWidth() {
-		return lineWidth;
-	}
 
-	/**
-	 * Gets the size scale factor for the object
-	 * @return size scale factor
-	 */
-	@Override
-	public double getSizeScale() {
-		return sizeScale;
-	}
-	
-	/**
-	 * Sets the Lat/lon location of the object
-	 */
-	public void setLinePoints(List<Coordinate> linePoints) {
-		    this.linePoints = new ArrayList<Coordinate>(linePoints);	
-	}
-	
-	/**
-	 * Sets the color list associated with the object
-	 */
-	public void setColors(Color[] colors) {
-		if ( colors != null ){
-			if ( getPgenType() != null && getPgenType().contains("FRONT") ) {
-				
-				if ( getPgenType().contains("STATIONARY_FRONT") ){
-					//for stationary front
-					if ( this.colors.length > colors.length){
-						// stationary fronts need two color, if there is only one input, keep the original 2nd color
-						for ( int ii = 0; ii < colors.length; ii++ ){
-							this.colors[ii] = colors[ii];
-						}
-					}
-					else {
-						this.colors = colors;
-					}
-				}
-				else {
-					//for other fronts
-					if ( this.colors.length < colors.length ){
-						//for other fronts, keep only the major color
-						for ( int ii = 0; ii < this.colors.length; ii++ ){
-							this.colors[ii] = colors[ii];
-						}
-					}
-					else {
-						this.colors = colors;
+        Coordinate a[] = new Coordinate[linePoints.size()];
+        linePoints.toArray(a);
+        return a;
 
-					}
-				}
-			}
-			else {
-				this.colors = colors;
-			}
-		}
-	}
-	
-	/**
-	 * Sets the width of the line pattern
-	 */
-	public void setLineWidth(float lineWidth) {
-		if ( !(new Float(lineWidth).isNaN())){
-			this.lineWidth = lineWidth;
-		}
-	}
-	
-	/**
-	 * Sets the size scale factor for the object
-	 */
-	public void setSmoothFactor(int smoothFactor) {
-		if ( smoothFactor >= 0 ){
-			this.smoothFactor = smoothFactor;
-		}
-	}
-	
-	/**
-	 * Sets whether the object should be filled.
-	 */
-	public void setFilled(Boolean filled) {
-		if ( filled != null ){
-			this.filled = filled;
-		}
-	}
-	
-	/**
-	 * Sets whether the object should be closed.
-	 */
-	public void setClosed(Boolean closed) {
-		if ( closed != null ){
-			this.closed = closed;
-		}
-	}
-	
-	/**
-	 * Sets the size scale factor for the object
-	 */
-	public void setSizeScale(double sizeScale) {
-		if ( !(new Double(sizeScale).isNaN()))
-				this.sizeScale = sizeScale;
-	}
-	
-	/**
-	 * Sets the fill pattern for the objects
-	 */
-	public void setFillPattern(FillPattern fillPattern) {
-		this.fillPattern = fillPattern;
-	}
-	
-	/**
-	 * Update the attributes
-	 */	
-	public void update(IAttribute iattr) {
-		if ( iattr instanceof IMultiPoint ){
-			IMultiPoint attr = (IMultiPoint) iattr;
-	
-			this.setColors(attr.getColors() );
-			this.setLineWidth(attr.getLineWidth());	    
-			this.setSizeScale(attr.getSizeScale());
+    }
 
-			this.setAttr(attr);
-		}
-	}
-	
-	/**
-	 * IAttribute getter/setter for SIGMET
-	 * gzhang
-	 */
-	public IAttribute getAttr(){
-		return this.attr;
-	}
-	
-	public void setAttr(IAttribute attr){
-		this.attr = attr;
-	}
-	
-	/**
-	 * pgenCategory getter for SIGMET
-	 * gzhang
-	 */
-	
-	public String getPgenCategory(){
-		return this.pgenCategory;
-	}
-		
-	public ArrayList<Coordinate> getPoints(){
-		
-		return linePoints;
-		
-	}
-	
-	public void setPointsOnly( ArrayList<Coordinate> pts ){
-		
-		this.linePoints = pts;
-		
-	}
-	
-	public void addPoint(int index, Coordinate point) {
-		
-		linePoints.add(index, point);
-		
-	}
-	
-	public void removePoint(int index ) {
-		
-		linePoints.remove( index );
-		
-	}
-	
-	/**
+    /**
+     * Gets array of colors associated with the object
+     * 
+     * @return Color array
+     */
+    @Override
+    public Color[] getColors() {
+        return colors;
+    }
+
+    /**
+     * Gets the width of the line pattern
+     * 
+     * @return line width
+     */
+    @Override
+    public float getLineWidth() {
+        return lineWidth;
+    }
+
+    /**
+     * Gets the size scale factor for the object
+     * 
+     * @return size scale factor
+     */
+    @Override
+    public double getSizeScale() {
+        return sizeScale;
+    }
+
+    /**
+     * Sets the Lat/lon location of the object
+     */
+    public void setLinePoints(List<Coordinate> linePoints) {
+        this.linePoints = new ArrayList<Coordinate>(linePoints);
+    }
+
+    /**
+     * Sets the color list associated with the object
+     */
+    public void setColors(Color[] colors) {
+        if (colors != null) {
+            if (getPgenType() != null && getPgenType().contains("FRONT")) {
+
+                if (getPgenType().contains("STATIONARY_FRONT")) {
+                    // for stationary front
+                    if (this.colors.length > colors.length) {
+                        // stationary fronts need two color, if there is only
+                        // one input, keep the original 2nd color
+                        for (int ii = 0; ii < colors.length; ii++) {
+                            this.colors[ii] = colors[ii];
+                        }
+                    } else {
+                        this.colors = colors;
+                    }
+                } else {
+                    // for other fronts
+                    if (this.colors.length < colors.length) {
+                        // for other fronts, keep only the major color
+                        for (int ii = 0; ii < this.colors.length; ii++) {
+                            this.colors[ii] = colors[ii];
+                        }
+                    } else {
+                        this.colors = colors;
+
+                    }
+                }
+            } else {
+                this.colors = colors;
+            }
+        }
+    }
+
+    /**
+     * Sets the width of the line pattern
+     */
+    public void setLineWidth(float lineWidth) {
+        if (!(new Float(lineWidth).isNaN())) {
+            this.lineWidth = lineWidth;
+        }
+    }
+
+    /**
+     * Sets the size scale factor for the object
+     */
+    public void setSmoothFactor(int smoothFactor) {
+        if (smoothFactor >= 0) {
+            this.smoothFactor = smoothFactor;
+        }
+    }
+
+    /**
+     * Sets whether the object should be filled.
+     */
+    public void setFilled(Boolean filled) {
+        if (filled != null) {
+            this.filled = filled;
+        }
+    }
+
+    /**
+     * Sets whether the object should be closed.
+     */
+    public void setClosed(Boolean closed) {
+        if (closed != null) {
+            this.closed = closed;
+        }
+    }
+
+    /**
+     * Sets the size scale factor for the object
+     */
+    public void setSizeScale(double sizeScale) {
+        if (!(new Double(sizeScale).isNaN()))
+            this.sizeScale = sizeScale;
+    }
+
+    /**
+     * Sets the fill pattern for the objects
+     */
+    public void setFillPattern(FillPattern fillPattern) {
+        this.fillPattern = fillPattern;
+    }
+
+    /**
+     * Update the attributes
+     */
+    public void update(IAttribute iattr) {
+        if (iattr instanceof IMultiPoint) {
+
+            IMultiPoint attr = (IMultiPoint) iattr;
+
+            this.setColors(attr.getColors());
+            this.setLineWidth(attr.getLineWidth());
+            this.setSizeScale(attr.getSizeScale());
+
+            this.setAttr(attr);
+        }
+    }
+
+    /**
+     * IAttribute getter/setter for SIGMET gzhang
+     */
+    public IAttribute getAttr() {
+        return this.attr;
+    }
+
+    public void setAttr(IAttribute attr) {
+        this.attr = attr;
+    }
+
+    /**
+     * pgenCategory getter for SIGMET gzhang
+     */
+
+    public String getPgenCategory() {
+        return this.pgenCategory;
+    }
+
+    public ArrayList<Coordinate> getPoints() {
+
+        return linePoints;
+
+    }
+
+    public void setPointsOnly(ArrayList<Coordinate> pts) {
+
+        this.linePoints = pts;
+
+    }
+
+    public void addPoint(int index, Coordinate point) {
+
+        linePoints.add(index, point);
+
+    }
+
+    public void removePoint(int index) {
+
+        linePoints.remove(index);
+
+    }
+
+    /**
      * Generate a JTS polygon from a set of points.
      * 
-     * It is assumed that the first point is not repeated at the end
-     * in the input array of point.
+     * It is assumed that the first point is not repeated at the end in the
+     * input array of point.
      * 
-     * @param points	array of points
+     * @param points
+     *            array of points
      * @return
      */
-	public Polygon toJTSPolygon() {
-		GeometryFactory geometryFactory = new GeometryFactory();
+    public Polygon toJTSPolygon() {
+        GeometryFactory geometryFactory = new GeometryFactory();
 
-		Coordinate[] coords = new Coordinate[linePoints.size()+1]; 
-			
-		for (int ii = 0; ii<linePoints.size(); ii++ ){
-			coords[ii] = linePoints.get(ii);
-		}
-		coords[coords.length - 1] = coords[0];
+        Coordinate[] coords = new Coordinate[linePoints.size() + 1];
 
-		CoordinateArraySequence cas = new CoordinateArraySequence( coords );
-		LinearRing ring = new LinearRing( cas, geometryFactory );	
+        for (int ii = 0; ii < linePoints.size(); ii++) {
+            coords[ii] = linePoints.get(ii);
+        }
+        coords[coords.length - 1] = coords[0];
 
-		Polygon polygon = new Polygon( ring, null, geometryFactory );
-		
-		return polygon;
-	}
-	
-	/**
-	 * Gets "filled".
-	 */
-	public boolean getFilled() {		
-		return filled;
-	}
+        CoordinateArraySequence cas = new CoordinateArraySequence(coords);
+        LinearRing ring = new LinearRing(cas, geometryFactory);
+
+        Polygon polygon = new Polygon(ring, null, geometryFactory);
+
+        return polygon;
+    }
+
+    /**
+     * Gets "filled".
+     */
+    public boolean getFilled() {
+        return filled;
+    }
 
 }
