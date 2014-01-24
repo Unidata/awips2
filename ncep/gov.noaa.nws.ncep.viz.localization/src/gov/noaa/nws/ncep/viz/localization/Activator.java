077dbe83f703f9dcea87633cda89a3dc5c1a1778
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-01-24 10:33:14 -0500

    VLab Issue #2867 - NCEP CAVE plugin delivery for 14.2.1
    Updates to configuration files used by the National Centers Perspective
    
    Change-Id: Ie2c8c4f8d0eb57031fb25acbe450b6e3ac56c109
    
    Former-commit-id: 10a718853dcdd60008362eda24e4f3f558476d86 [formerly 4b28ca1bafe4792e7854f495c8164d49fec96ea8 [formerly 95bbc2f2b25f7f8a8bc18a5c0f4f7272812ba210] [formerly 10a718853dcdd60008362eda24e4f3f558476d86 [formerly 12d9868db2939c53ed49b1cf3092f7d638189696]]]
    Former-commit-id: 4b28ca1bafe4792e7854f495c8164d49fec96ea8 [formerly 95bbc2f2b25f7f8a8bc18a5c0f4f7272812ba210]
    Former-commit-id: 4b28ca1bafe4792e7854f495c8164d49fec96ea8
    Former-commit-id: 688690ea97c7671416d6fcb69553026cb6f31ec7

--- ncep/gov.noaa.nws.ncep.viz.localization/src/gov/noaa/nws/ncep/viz/localization/Activator.java
+++ ncep/gov.noaa.nws.ncep.viz.localization/src/gov/noaa/nws/ncep/viz/localization/Activator.java
@@ -16,52 +16,25 @@ import com.raytheon.uf.viz.core.localization.LocalizationManager;
 
 /**
  * The activator class controls the plug-in life cycle
+ * 
+ *  #1051 Greg Hull - moved desk code to NmapCommon and trigger via spring
  */
 public class Activator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "gov.noaa.nws.ncep.viz.localization";
 
-	public static final String DESK_ARG = "-desk"; 
-//	public static final String DESK_ENV = "DESK"; 
-
 	// The shared instance
 	private static Activator plugin;
-
-	private static String currentDesk="None";
 	
 	public Activator() {		
-		/*
-		 * Now the priority is: 1. command line argument, 2. from UI preference setting
-		 * 3. system variable 
-		 */
-		String desk = ProgramArguments.getInstance().getString( DESK_ARG ); 
-		
-		if( desk != null && !desk.trim().isEmpty() ) {
-			desk = desk.trim().toUpperCase();
-			System.out.println("Setting Desk to "+ desk+" from Program Arguement.");
-			currentDesk = desk;
-		}
-//		else {
-//			desk = System.getenv( DESK_ENV );
-//			
-//			if( desk != null && !desk.trim().isEmpty() ) {
-//				desk = desk.trim();
-//				System.out.println("Setting Desk to "+ desk+" from Environment variable.");
-//				currentDesk = desk;
-//			}						
-//		}
-	
-		
-		// THE SITE is set by the LocalizationManager from either the  
-		// preferences or the program args.
 
 	}
 
-	public static String getCurrentDesk() {
-		return currentDesk;
-	}
-				
+//	public static String getCurrentDesk() {
+//		return currentDesk;
+//	}
+//				
 	private void locationTest() {
 		String configurationLocation = Platform.getConfigurationLocation().getURL().getPath(); 
 		System.out.println("Platform.getConfigurationLocation().getURL().getPath()="+configurationLocation); 
