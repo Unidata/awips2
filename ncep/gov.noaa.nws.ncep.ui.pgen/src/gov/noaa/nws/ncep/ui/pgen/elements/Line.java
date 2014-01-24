b52d0bc459f41ea99d07b9054332df5de25bf575
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-01-24 10:46:37 -0500

    VLab Issue #2867 - NCEP CAVE plugin delivery for 14.2.1
    Many changes in NCEP Viz plugins for 14.2.1 enhancements and big fixes
    for the National Centers Perspective and PGEN
    
    Change-Id: I4fa707cf0844800a4c77961e110338daafbb0354
    
    Former-commit-id: 01f013e2d07ce756092bf90f0f7a95f8f95eb52c [formerly e1d16e1aeb039a9205da17a77078b9753551b213] [formerly 14d218ddc51a93d66ea2f6e51f13d9d65d8c501d] [formerly 01f013e2d07ce756092bf90f0f7a95f8f95eb52c [formerly e1d16e1aeb039a9205da17a77078b9753551b213] [formerly 14d218ddc51a93d66ea2f6e51f13d9d65d8c501d] [formerly 369d2592fef50574dc0b40de74026bec6bd31261 [formerly 14d218ddc51a93d66ea2f6e51f13d9d65d8c501d [formerly e3a35c9c3eb2184818b21ba1b23aac988a0148ee]]]]
    Former-commit-id: 369d2592fef50574dc0b40de74026bec6bd31261
    Former-commit-id: 1bb9e4c3e4c6c021482f51740fba2874d9827408 [formerly 405cdee1caa72fa18376df0a1ee4ed2fc8410b98] [formerly cbeb097387af8cc564bfd909a4840579c3c07c47 [formerly 648069308933b43d192b3a31c014e3deec49d938]]
    Former-commit-id: d7a18e7961e4f977446c1916d8722f1551e40297 [formerly f78b216bcd13c28b1957f20b9adf154afa696f0a]
    Former-commit-id: 7a3a3de0480be5dd41cd2c74ae0f0f87e3fce2d0

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/elements/Line.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/elements/Line.java
@@ -7,6 +7,12 @@
  */
 package gov.noaa.nws.ncep.ui.pgen.elements;
 
+import gov.noaa.nws.ncep.ui.pgen.annotation.ElementOperations;
+import gov.noaa.nws.ncep.ui.pgen.annotation.Operation;
+import gov.noaa.nws.ncep.ui.pgen.display.FillPatternList.FillPattern;
+import gov.noaa.nws.ncep.ui.pgen.display.IAttribute;
+import gov.noaa.nws.ncep.ui.pgen.display.ILine;
+
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,12 +21,6 @@ import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
 import com.vividsolutions.jts.geom.Point;
-
-import gov.noaa.nws.ncep.ui.pgen.annotation.Operation;
-import gov.noaa.nws.ncep.ui.pgen.annotation.ElementOperations;
-import gov.noaa.nws.ncep.ui.pgen.display.IAttribute;
-import gov.noaa.nws.ncep.ui.pgen.display.ILine;
-import gov.noaa.nws.ncep.ui.pgen.display.FillPatternList.FillPattern;
 
 /**
  * Class to represent a line element.
@@ -33,55 +33,56 @@ import gov.noaa.nws.ncep.ui.pgen.display.FillPatternList.FillPattern;
  * 05/09        #42         S. Gilbert  Added pgenType and pgenCategory to constructors and copy()
  * 03/10		#223		M.Laryukhin	getCentroid() method is added
  * 04/11		#?			B. Yin		Re-factor IAttribute
- *
+ * 
  * </pre>
  * 
- * @author	J. Wu
- * @version	0.0.1
+ * @author J. Wu
+ * @version 0.0.1
  */
-@ElementOperations ( {Operation.CONNECT, Operation.COPY_MOVE, Operation.EXTRAPOLATE, Operation.FLIP,
-	                  Operation.DELETE_PART, Operation.DELETE_POINT, Operation.MODIFY, 
-	                  Operation.INTERPOLATE, Operation.ADD_POINT} )
-public class Line extends MultiPointElement  implements ILine{
-	
-	private boolean flipSide = false;
-	
-	/**
-	 *  Default constructor
-	 */
-	public Line() {
-	}
+@ElementOperations({ Operation.CONNECT, Operation.COPY_MOVE,
+        Operation.EXTRAPOLATE, Operation.FLIP, Operation.DELETE_PART,
+        Operation.DELETE_POINT, Operation.MODIFY, Operation.INTERPOLATE,
+        Operation.ADD_POINT })
+public class Line extends MultiPointElement implements ILine {
 
-	/**
-	 * @param deleted
-	 * @param range
-	 * @param colors
-	 * @param lineWidth
-	 * @param sizeScale
-	 * @param closed
-	 * @param filled
-	 * @param linePoints
-	 * @param smoothFactor
-	 * @param fillPattern
-	 * @param pgenCategory
-	 * @param pgenType
-	 */
-	public Line(Coordinate[] range, Color[] colors,
-			float lineWidth, double sizeScale, boolean closed, boolean filled,
-			List<Coordinate> linePoints, int smoothFactor, FillPattern fillPattern,
-			String pgenCategory, String pgenType) {
-		super(range, colors, lineWidth, sizeScale, closed, filled,
-				linePoints, smoothFactor, fillPattern, pgenCategory, pgenType);
-	}
+    private boolean flipSide = false;
 
-	/**
-	 * @return the string
-	 */
-	public String toString() {
-		StringBuilder	result = new StringBuilder( getClass().getSimpleName());
+    /**
+     * Default constructor
+     */
+    public Line() {
+    }
 
-        result.append("Category:\t" + pgenCategory + "\n");        
-        result.append("Type:\t" + pgenType + "\n");        
+    /**
+     * @param deleted
+     * @param range
+     * @param colors
+     * @param lineWidth
+     * @param sizeScale
+     * @param closed
+     * @param filled
+     * @param linePoints
+     * @param smoothFactor
+     * @param fillPattern
+     * @param pgenCategory
+     * @param pgenType
+     */
+    public Line(Coordinate[] range, Color[] colors, float lineWidth,
+            double sizeScale, boolean closed, boolean filled,
+            List<Coordinate> linePoints, int smoothFactor,
+            FillPattern fillPattern, String pgenCategory, String pgenType) {
+        super(range, colors, lineWidth, sizeScale, closed, filled, linePoints,
+                smoothFactor, fillPattern, pgenCategory, pgenType);
+    }
+
+    /**
+     * @return the string
+     */
+    public String toString() {
+        StringBuilder result = new StringBuilder(getClass().getSimpleName());
+
+        result.append("Category:\t" + pgenCategory + "\n");
+        result.append("Type:\t" + pgenType + "\n");
         result.append("Color:\t" + colors[0] + "\n");
         result.append("LineWidth:\t" + lineWidth + "\n");
         result.append("SizeScale:\t" + sizeScale + "\n");
@@ -89,175 +90,182 @@ public class Line extends MultiPointElement  implements ILine{
         result.append("Filled:\t" + filled + "\n");
         result.append("SmoothFactor:\t" + smoothFactor + "\n");
         result.append("FillPattern:\t" + fillPattern + "\n");
-        result.append("Location:\t\n"); 
-        for ( Coordinate point : linePoints )	{
-        	result.append("\t" + point.x + "\t" + point.y + "\n");
+        result.append("Location:\t\n");
+        for (Coordinate point : linePoints) {
+            result.append("\t" + point.x + "\t" + point.y + "\n");
         }
-     	 	       		
-		return result.toString();
-	}	
-	
-	/**
-	 *  Update the attributes for the object
-	 */	
-	@Override
-	public void update(IAttribute iattr) {
-		if ( iattr instanceof ILine ){
-			ILine attr = (ILine)iattr;
-			super.update(attr);
-			this.setClosed(attr.isClosedLine() );
-			this.setFilled(attr.isFilled() );
-			this.setSmoothFactor(attr.getSmoothFactor());       
-			if ( attr.isFilled()!= null &&  attr.isFilled()) this.setFillPattern(attr.getFillPattern());
-		}
-	}
 
-	/**
-	 * Creates a copy of this object.  This is a deep copy and new objects are
-	 * created so that we are not just copying references of objects
-	 */
-	@Override
-	public DrawableElement copy() {
+        return result.toString();
+    }
 
-		/*
-		 * create a new Line object and initially set its attributes to this one's
-		 */
-		Line newLine = new Line();
-		
-		/*
-		 * new Strings are created for Type and LinePattern
-		 */
-		newLine.setPgenCategory(new String(this.getPgenCategory()));
-		newLine.setPgenType(new String(this.getPgenType()));
-		newLine.setParent(this.getParent());
-		
-		newLine.update(this);
-		
-		/*
-		 * new Coordinates points are created and set, so we don't just set 
-		 * references
-		 */
-		ArrayList<Coordinate> ptsCopy = new ArrayList<Coordinate>();
-		for (int i=0; i < this.getPoints().size(); i++) {
-			ptsCopy.add(new Coordinate(this.getPoints().get(i)));
-		}
-		newLine.setPoints(ptsCopy);
-		
-		/*
-		 * new colors are created and set, so we don't just set 
-		 * references
-		 */
-		Color[] colorCopy = new Color[this.getColors().length];
-		for (int i=0; i<this.getColors().length; i++) {
-			colorCopy[i] = new Color(this.getColors()[i].getRed(),
-					                 this.getColors()[i].getGreen(),
-					                 this.getColors()[i].getBlue() );
-		}
-		newLine.setColors(colorCopy);
-		newLine.setFlipSide(this.flipSide);
-		
-		return newLine;
+    /**
+     * Update the attributes for the object
+     */
+    @Override
+    public void update(IAttribute iattr) {
+        super.update(iattr);
+        if (iattr instanceof ILine) {
+            ILine attr = (ILine) iattr;
+            this.setClosed(attr.isClosedLine());
+            this.setFilled(attr.isFilled());
+            this.setSmoothFactor(attr.getSmoothFactor());
+            if (attr.isFilled() != null && attr.isFilled())
+                this.setFillPattern(attr.getFillPattern());
+        }
+    }
 
-	}
+    /**
+     * Creates a copy of this object. This is a deep copy and new objects are
+     * created so that we are not just copying references of objects
+     */
+    @Override
+    public DrawableElement copy() {
 
-	/**
-	 * @return the flipSide
-	 */
-	public boolean isFlipSide() {
-		return flipSide;
-	}
+        /*
+         * create a new Line object and initially set its attributes to this
+         * one's
+         */
+        Line newLine = new Line();
 
-	/**
-	 * @param flipSide the flipSide to set
-	 */
-	public void setFlipSide(boolean flipSide) {
-		this.flipSide = flipSide;
-	}
+        /*
+         * new Strings are created for Type and LinePattern
+         */
+        newLine.setPgenCategory(new String(this.getPgenCategory()));
+        newLine.setPgenType(new String(this.getPgenType()));
+        newLine.setParent(this.getParent());
 
-	/**
-	 * Gets the name of the line pattern associated with this object
-	 */
-	@Override
-	public String getPatternName() {
-		return getPgenType();
-	}	
+        newLine.update(this);
 
-	
-	/**
-	 * Gets the smooth factor used to create line path
-	 * @return Line smoothing factor
-	 */
-	@Override
-	public int getSmoothFactor() {
-		return smoothFactor;
-	}
-	
-	/**
-	 * Checks whether the line path is closed.
-	 * @return true, if line path is closed.
-	 */
-	@Override
-	public Boolean isClosedLine() {
-		return closed;
-	}
-	
-	/**
-	 * Checks whether the object should be filled
-	 * @return true, if a fill pattern applies
-	 */
-	@Override
-	public Boolean isFilled() {
-		return filled;
-	}
-	
-	/**
-	 * Specifies the Fill Pattern to use, if isFilled returns true.
-	 * @return The Fill Pattern associated with the object
-	 */
-	@Override
-	public FillPattern getFillPattern() {
-		FillPattern fp = FillPattern.SOLID;
-		
-		if ( fillPattern != null ) {
-			fp = fillPattern;
-		}
+        /*
+         * new Coordinates points are created and set, so we don't just set
+         * references
+         */
+        ArrayList<Coordinate> ptsCopy = new ArrayList<Coordinate>();
+        for (int i = 0; i < this.getPoints().size(); i++) {
+            ptsCopy.add(new Coordinate(this.getPoints().get(i)));
+        }
+        newLine.setPoints(ptsCopy);
 
-		return fp;
+        /*
+         * new colors are created and set, so we don't just set references
+         */
+        Color[] colorCopy = new Color[this.getColors().length];
+        for (int i = 0; i < this.getColors().length; i++) {
+            colorCopy[i] = new Color(this.getColors()[i].getRed(),
+                    this.getColors()[i].getGreen(),
+                    this.getColors()[i].getBlue());
+        }
+        newLine.setColors(colorCopy);
+        newLine.setFlipSide(this.flipSide);
 
-	}
-		
-	/**
-	 * Get the east most point
-	 * @return
-	 */
-	public Coordinate getEastMostPoint(){
-		Coordinate eastMostPt = getPoints().get(0);
-		
-		for ( Coordinate pt : getPoints()){
-			if ( pt.x > eastMostPt.x ){
-				eastMostPt = pt;
-			}
-		}
-		
-		return eastMostPt;
-	}
-		
-	/**
-	 * Returns the coordinate of the centroid constructed from the points of this line. 
-	 * 
-	 * @return
-	 */
-	public Coordinate getCentroid() {
-		 if (getPoints().size() < 2){
-		 		return null;
-		 }
-		 GeometryFactory factory = new GeometryFactory();
-		 Coordinate[] a = new Coordinate[getPoints().size() +1 ];
-		 getPoints().toArray(a);
-		 a[a.length -1] = a[0]; // add the first point to the end
-		 LineString g = factory.createLineString(a);
-		 Point p = g.getCentroid();
-		 return p.getCoordinate();
-	}
+        return newLine;
+
+    }
+
+    /**
+     * @return the flipSide
+     */
+    public boolean isFlipSide() {
+        return flipSide;
+    }
+
+    /**
+     * @param flipSide
+     *            the flipSide to set
+     */
+    public void setFlipSide(boolean flipSide) {
+        this.flipSide = flipSide;
+    }
+
+    /**
+     * Gets the name of the line pattern associated with this object
+     */
+    @Override
+    public String getPatternName() {
+        return getPgenType();
+    }
+
+    /**
+     * Gets the smooth factor used to create line path
+     * 
+     * @return Line smoothing factor
+     */
+    @Override
+    public int getSmoothFactor() {
+        return smoothFactor;
+    }
+
+    /**
+     * Checks whether the line path is closed.
+     * 
+     * @return true, if line path is closed.
+     */
+    @Override
+    public Boolean isClosedLine() {
+        return closed;
+    }
+
+    /**
+     * Checks whether the object should be filled
+     * 
+     * @return true, if a fill pattern applies
+     */
+    @Override
+    public Boolean isFilled() {
+        return filled;
+    }
+
+    /**
+     * Specifies the Fill Pattern to use, if isFilled returns true.
+     * 
+     * @return The Fill Pattern associated with the object
+     */
+    @Override
+    public FillPattern getFillPattern() {
+        FillPattern fp = FillPattern.SOLID;
+
+        if (fillPattern != null) {
+            fp = fillPattern;
+        }
+
+        return fp;
+
+    }
+
+    /**
+     * Get the east most point
+     * 
+     * @return
+     */
+    public Coordinate getEastMostPoint() {
+        Coordinate eastMostPt = getPoints().get(0);
+
+        for (Coordinate pt : getPoints()) {
+            if (pt.x > eastMostPt.x) {
+                eastMostPt = pt;
+            }
+        }
+
+        return eastMostPt;
+    }
+
+    /**
+     * Returns the coordinate of the centroid constructed from the points of
+     * this line.
+     * 
+     * @return
+     */
+    public Coordinate getCentroid() {
+        if (getPoints().size() < 2) {
+            return null;
+        }
+        GeometryFactory factory = new GeometryFactory();
+        Coordinate[] a = new Coordinate[getPoints().size() + 1];
+        getPoints().toArray(a);
+        a[a.length - 1] = a[0]; // add the first point to the end
+        LineString g = factory.createLineString(a);
+        Point p = g.getCentroid();
+        return p.getCoordinate();
+    }
 
 }
