ea9f3e9f1b3385f0ee8680661864a9d30fa7938d
Slav Korolev <vyacheslav.s.korolev@raytheon.com>
2014-01-30 11:43:16 -0500

    Issue #2757. Fixed issues in Area Configuration Dialogs.
    Change-Id: Ic3d3d5c41df6c8d80aac8e434e4c42a5ef9d1ecf
    
    Former-commit-id: 07db3429f9a05823db82d02c5d5d31bad2a807b3 [formerly 07db3429f9a05823db82d02c5d5d31bad2a807b3 [formerly 6013121a9915e9d4aef2e37e439d92974cd61124]] [formerly ea573d2c30306247ae7c83f025c9676aa11a320d] [formerly 07db3429f9a05823db82d02c5d5d31bad2a807b3 [formerly 07db3429f9a05823db82d02c5d5d31bad2a807b3 [formerly 6013121a9915e9d4aef2e37e439d92974cd61124]] [formerly ea573d2c30306247ae7c83f025c9676aa11a320d] [formerly ea19ee0722e4ca9137aa141a752fcdc98ff5e6e4 [formerly ea19ee0722e4ca9137aa141a752fcdc98ff5e6e4 [formerly a768f540f01a58d0c8d85685a6c0460ce009fd1e]] [formerly ea573d2c30306247ae7c83f025c9676aa11a320d [formerly ea573d2c30306247ae7c83f025c9676aa11a320d [formerly 81826338bda946aff4ac8a7b9ea83c2eb72397d5]]]]]
    Former-commit-id: ea19ee0722e4ca9137aa141a752fcdc98ff5e6e4 [formerly ea19ee0722e4ca9137aa141a752fcdc98ff5e6e4 [formerly a768f540f01a58d0c8d85685a6c0460ce009fd1e]]
    Former-commit-id: dc4c7ea6172d963ed5504c56e644f1645f561664 [formerly 243e2bff54888cee60fd66b44d0a90e1e39c46a0] [formerly 89c3de4c705f818498c43574e914fa8a85cd0fb5 [formerly d2d4672a46805b88f657d8b0804d8f77a5fb2a2f]] [formerly 8d090a107dc39cefb02eb1cd852a49ec948b6f0f [formerly e9bd96aa2e36cab0d20f69000c6a5798fa0b03bc] [formerly 582fe3763121ff239c8a47e39fff1d758e7f70c3 [formerly 6d9a066c36c9d8cc7049b26d00b1425a1b3029b3]]]
    Former-commit-id: 0235b3e54069b3a679c98d60a9c17b92f3556698 [formerly 88aebf6cf3ff090f90a82073846710b28f6c8768] [formerly bf6e90f0a28742e6f2a0ec26cd5bf8fa7a370086 [formerly baf598a6ffe8b196f76b85e240e8aaca8d9714f5]]
    Former-commit-id: 2f6405127383ea2bf86f2308e0155fee7a086833 [formerly 559d0f94997f4c04f68dcac6b814d4e99d6e43f2]
    Former-commit-id: d57b48b2417defb754aa4cbc2aa0fbdae1274037

--- cave/com.raytheon.uf.viz.monitor.snow/src/com/raytheon/uf/viz/monitor/snow/threshold/SnowThresholdMgr.java
+++ cave/com.raytheon.uf.viz.monitor.snow/src/com/raytheon/uf/viz/monitor/snow/threshold/SnowThresholdMgr.java
@@ -29,145 +29,135 @@ import com.raytheon.uf.viz.monitor.thresholds.AbstractThresholdMgr;
 import com.raytheon.uf.viz.monitor.util.MonitorConfigConstants.SnowDisplay;
 import com.raytheon.uf.viz.monitor.util.MonitorConfigConstants.SnowMonitor;
 
-
 /**
  * This class manages the Snow thresholds for display and monitor.
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Dec 27, 2009 #3963      lvenable     Initial creation
- *
+ * Feb 03, 2014 #2757      skorolev     Fixed reInitialize()
+ * 
  * </pre>
- *
+ * 
  * @author lvenable
  * @version 1.0
  */
-public class SnowThresholdMgr extends AbstractThresholdMgr
-{
-private static SnowThresholdMgr classInstance;
-    
-    private SnowThresholdMgr()
-    {
+public class SnowThresholdMgr extends AbstractThresholdMgr {
+    private static SnowThresholdMgr classInstance;
+
+    private SnowThresholdMgr() {
         super("DefaultSnowDisplayThresholds.xml",
-        "DefaultSnowMonitorThresholds.xml",
-        "snow");
+                "DefaultSnowMonitorThresholds.xml", "snow");
 
         areaConfigMgr = getAreaConfigMgr();
         init(); // call init() after areaConfigMgr is set
     }
-    
-    public static SnowThresholdMgr getInstance()
-    {
-        if (classInstance == null)
-        {
+
+    public static SnowThresholdMgr getInstance() {
+        if (classInstance == null) {
             classInstance = new SnowThresholdMgr();
         }
-        
+
         return classInstance;
     }
 
     /**
-     * DR#11279:
-     * When monitor area configuration is changed, 
-     * threshold manager needs to be re-initialized 
-     * using the new monitor area configuration
+     * DR#11279: When monitor area configuration is changed, threshold manager
+     * needs to be re-initialized using the new monitor area configuration
      */
     public static void reInitialize() {
-    	if ( classInstance != null ) {
-    		classInstance = null;
-    		classInstance = new SnowThresholdMgr();
-    	}
+        if (classInstance != null) {
+            classInstance = null;
+        }
+        // Update threshold file.
+        classInstance = new SnowThresholdMgr();
+        classInstance.loadDefaultMonitorThreshold();
+        classInstance.saveMonitorThresholds();
     }
 
     @Override
-    protected ArrayList<String> getThresholdKeys(DataUsageKey dataUsage)
-    {
+    protected ArrayList<String> getThresholdKeys(DataUsageKey dataUsage) {
         ArrayList<String> threshKeys = new ArrayList<String>();
-        
-        if (dataUsage == DataUsageKey.DISPLAY)
-        {
-            for (SnowDisplay snowDisp : SnowDisplay.values())
-            {
+
+        if (dataUsage == DataUsageKey.DISPLAY) {
+            for (SnowDisplay snowDisp : SnowDisplay.values()) {
                 threshKeys.add(snowDisp.getXmlKey());
             }
-        }
-        else if (dataUsage == DataUsageKey.MONITOR)
-        {
-            for (SnowMonitor snowMon : SnowMonitor.values())
-            {
+        } else if (dataUsage == DataUsageKey.MONITOR) {
+            for (SnowMonitor snowMon : SnowMonitor.values()) {
                 threshKeys.add(snowMon.getXmlKey());
             }
         }
-        
+
         return threshKeys;
     }
 
-	@Override
-	public MonitorConfigurationManager getAreaConfigMgr() {
+    @Override
+    public MonitorConfigurationManager getAreaConfigMgr() {
         if (areaConfigMgr == null) {
             LocalizationManager mgr = LocalizationManager.getInstance();
             String siteScope = mgr.getCurrentSite();
-                  
+
             areaConfigMgr = SnowMonitorConfigurationManager.getInstance();
             areaConfigMgr.readConfigXml(siteScope);
         }
         return areaConfigMgr;
-	}
+    }
 
-//    /**
-//     * Get the path where the display thresholds XML files are contained.
-//     * @return File path.
-//     */
-//    @Override
-//    public String getDisplayThresholdPath()
-//    {
-//        String fs = String.valueOf(File.separatorChar);
-//        StringBuilder sb = new StringBuilder();
-//        
-//        sb.append("snow").append(fs);
-//        sb.append("threshold").append(fs);
-//        sb.append("display").append(fs);        
-//        
-//        return sb.toString();
-//    }
-//    
-//    /**
-//     * Get the path where the monitor thresholds XML files are contained.
-//     * @return File path.
-//     */
-//    @Override
-//    public String getMonitorThresholdPath()
-//    {
-//        String fs = String.valueOf(File.separatorChar);
-//        StringBuilder sb = new StringBuilder();
-//        
-//        sb.append("snow").append(fs);
-//        sb.append("threshold").append(fs);
-//        sb.append("monitor").append(fs);        
-//        
-//        return sb.toString();
-//    }
-//    
-//    /**
-//     * Get the path where the XML file containing the user selected
-//     * default file is located.
-//     * @return The path of the user selected default file XML.
-//     */
-//    @Override
-//    public String getDefaultThresholdFilePath()
-//    {
-//        String fs = String.valueOf(File.separatorChar);
-//        StringBuilder sb = new StringBuilder();
-//        
-//        sb.append("snow").append(fs);
-//        sb.append("threshold").append(fs);
-//        sb.append("display").append(fs);
-//        sb.append("defaultThresh").append(fs);
-//        
-//        return sb.toString();
-//    }
+    // /**
+    // * Get the path where the display thresholds XML files are contained.
+    // * @return File path.
+    // */
+    // @Override
+    // public String getDisplayThresholdPath()
+    // {
+    // String fs = String.valueOf(File.separatorChar);
+    // StringBuilder sb = new StringBuilder();
+    //
+    // sb.append("snow").append(fs);
+    // sb.append("threshold").append(fs);
+    // sb.append("display").append(fs);
+    //
+    // return sb.toString();
+    // }
+    //
+    // /**
+    // * Get the path where the monitor thresholds XML files are contained.
+    // * @return File path.
+    // */
+    // @Override
+    // public String getMonitorThresholdPath()
+    // {
+    // String fs = String.valueOf(File.separatorChar);
+    // StringBuilder sb = new StringBuilder();
+    //
+    // sb.append("snow").append(fs);
+    // sb.append("threshold").append(fs);
+    // sb.append("monitor").append(fs);
+    //
+    // return sb.toString();
+    // }
+    //
+    // /**
+    // * Get the path where the XML file containing the user selected
+    // * default file is located.
+    // * @return The path of the user selected default file XML.
+    // */
+    // @Override
+    // public String getDefaultThresholdFilePath()
+    // {
+    // String fs = String.valueOf(File.separatorChar);
+    // StringBuilder sb = new StringBuilder();
+    //
+    // sb.append("snow").append(fs);
+    // sb.append("threshold").append(fs);
+    // sb.append("display").append(fs);
+    // sb.append("defaultThresh").append(fs);
+    //
+    // return sb.toString();
+    // }
 }
