434d5c4c6cf679315b7937a26b7bec08aeaa210d
Ron Anderson <ron.anderson@raytheon.com>
2014-02-11 17:38:22 -0600

    Issue #2788 Fix topo for large areas.
    Change-Id: Idb4a19ffea527ad40efb5e586913aab9d37602df
    
    Former-commit-id: 8ef94ea21681f429fb90965d82f99c64f4efb64c [formerly 69ee4792ad3e6f6ec124366116ad24c0d68c5eb5] [formerly 3e78908bb5eba71292adecfc1eb6bc4759be7e39] [formerly 3e78908bb5eba71292adecfc1eb6bc4759be7e39 [formerly fcf45916f47aacfec054d7a3f16046eb861e6484]] [formerly 2f067699f2a94765bef43e97f8ec2cd8630b9226 [formerly 3e78908bb5eba71292adecfc1eb6bc4759be7e39 [formerly fcf45916f47aacfec054d7a3f16046eb861e6484] [formerly 2f067699f2a94765bef43e97f8ec2cd8630b9226 [formerly 882576fcf6f41b9a41ea8f4a31a40c8c31d51aad]]]]
    Former-commit-id: 2f067699f2a94765bef43e97f8ec2cd8630b9226
    Former-commit-id: d520f8d08747355126bced1207a4b6b823b314ac [formerly 3ebfa79176cc7f3e52da6c61f6bee73013042f0c] [formerly d9b82b4ebe39a963775fe7e4d17299c4e5425316 [formerly 2d7c87ac849ff8a8cc0d59b59ea2f3702c785a90]]
    Former-commit-id: d9b82b4ebe39a963775fe7e4d17299c4e5425316
    Former-commit-id: 4d8ebf6712b91fd29bf9fbc1ba453a5b4da67bda

--- javaUtilities/com.raytheon.uf.topo.utilities/src/com/raytheon/uf/topo/utilities/TopoImporter.java
+++ javaUtilities/com.raytheon.uf.topo.utilities/src/com/raytheon/uf/topo/utilities/TopoImporter.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.common.topo.util;
+package com.raytheon.uf.topo.utilities;
 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
@@ -34,7 +34,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.raytheon.uf.common.datastorage.DataStoreFactory;
+import org.opengis.referencing.crs.ProjectedCRS;
+
 import com.raytheon.uf.common.datastorage.IDataStore;
 import com.raytheon.uf.common.datastorage.StorageProperties;
 import com.raytheon.uf.common.datastorage.StorageProperties.Compression;
@@ -44,6 +45,8 @@ import com.raytheon.uf.common.datastorage.records.IntegerDataRecord;
 import com.raytheon.uf.common.datastorage.records.LongDataRecord;
 import com.raytheon.uf.common.datastorage.records.ShortDataRecord;
 import com.raytheon.uf.common.geospatial.MapUtil;
+import com.raytheon.uf.common.pypies.PyPiesDataStore;
+import com.raytheon.uf.common.pypies.PypiesProperties;
 
 /**
  * Import topo data into HDF5
@@ -53,7 +56,8 @@ import com.raytheon.uf.common.geospatial.MapUtil;
  * SOFTWARE HISTORY
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Oct 22, 2009      #3280 randerso     Initial creation
+ * Oct 22, 2009     #3280  randerso    Initial creation
+ * Feb 11, 2014     #2788  randerso    Changed to use PyPiesDataStore
  * 
  * </pre>
  * 
@@ -68,21 +72,13 @@ public class TopoImporter {
 
         String byteOrder;
 
-        String layout;
-
         int nRows;
 
         int nCols;
 
-        int nBands;
-
         int nBits;
 
-        int bandRowBytes;
-
         int totalRowBytes;
-
-        int bandGapBytes;
 
         long noData;
 
@@ -95,6 +91,18 @@ public class TopoImporter {
         double yDim;
 
         public TopoHdr(File file) {
+            if (file.getName().endsWith(".HDR")) {
+                readHdrFile(file);
+            } else if (file.getName().endsWith(".ers")) {
+                readErsFile(file);
+            } else {
+                throw new IllegalArgumentException(
+                        "Unrecognized header file format: "
+                                + file.getAbsolutePath());
+            }
+        }
+
+        private void readHdrFile(File file) {
             BufferedReader in = null;
             try {
                 this.file = file;
@@ -105,22 +113,14 @@ public class TopoImporter {
 
                     if ("BYTEORDER".equals(s[0])) {
                         byteOrder = s[1];
-                    } else if ("LAYOUT".equals(s[0])) {
-                        layout = s[1];
                     } else if ("NROWS".equals(s[0])) {
                         nRows = Integer.parseInt(s[1]);
                     } else if ("NCOLS".equals(s[0])) {
                         nCols = Integer.parseInt(s[1]);
-                    } else if ("NBANDS".equals(s[0])) {
-                        nBands = Integer.parseInt(s[1]);
                     } else if ("NBITS".equals(s[0])) {
                         nBits = Integer.parseInt(s[1]);
-                    } else if ("BANDROWBYTES".equals(s[0])) {
-                        bandRowBytes = Integer.parseInt(s[1]);
                     } else if ("TOTALROWBYTES".equals(s[0])) {
                         totalRowBytes = Integer.parseInt(s[1]);
-                    } else if ("BANDGAPBYTES".equals(s[0])) {
-                        bandGapBytes = Integer.parseInt(s[1]);
                     } else if ("NODATA".equals(s[0])) {
                         noData = Long.parseLong(s[1]);
                     } else if ("ULXMAP".equals(s[0])) {
@@ -147,6 +147,91 @@ public class TopoImporter {
                     }
                 }
             }
+        }
+
+        private void readErsFile(File file) {
+            BufferedReader in = null;
+            try {
+                this.file = file;
+                in = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    line = line.replace(" = ", "   ");
+                    String[] s = line.split("\\s+");
+
+                    if ("ByteOrder".equals(s[0])) {
+                        byteOrder = s[1];
+                    } else if ("NrOfLines".equals(s[0])) {
+                        nRows = Integer.parseInt(s[1]);
+                    } else if ("NrOfCellsPerLine".equals(s[0])) {
+                        nCols = Integer.parseInt(s[1]);
+                    } else if ("CellType".equals(s[0])) {
+                        if (s[1].equals("Signed16BitInteger")) {
+                            nBits = 16;
+                        } else if (s[1].equals("Signed32BitInteger")) {
+                            nBits = 32;
+                        } else if (s[1].equals("Signed64BitInteger")) {
+                            nBits = 64;
+                        } else {
+                            throw new IllegalArgumentException(
+                                    "Unrecognized data type: " + s[1]);
+                        }
+                    } else if ("NullCellValue".equals(s[0])) {
+                        noData = Long.parseLong(s[1]);
+                    } else if ("Longitude".equals(s[0])) {
+                        ulXmap = parseDMS(s[1]);
+                    } else if ("Latitude".equals(s[0])) {
+                        ulYmap = parseDMS(s[1]);
+                    } else if ("Xdimension".equals(s[0])) {
+                        xDim = Double.parseDouble(s[1]);
+                    } else if ("Ydimension".equals(s[0])) {
+                        yDim = Double.parseDouble(s[1]);
+                    } else {
+                        // TODO: fully recognize the ers header file
+                        System.out.println("Unrecognized line in file "
+                                + file.getAbsolutePath() + "\n" + line);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    // convert upper left coordinate from corner to
+                    // center of cell
+                    ulXmap += xDim / 2;
+                    ulYmap -= yDim / 2;
+
+                    totalRowBytes = (nCols * nBits) / 8;
+                }
+            }
+        }
+
+        private double parseDMS(String dms) {
+            // parse a string in degrees:minutes:seconds format into decimal
+            // degrees
+            double degrees = Double.NaN;
+            String[] s = dms.split(":");
+            if (s.length == 3) {
+                try {
+                    int deg = Integer.parseInt(s[0]);
+                    int min = Integer.parseInt(s[1]);
+                    double sec = Double.parseDouble(s[2]);
+                    degrees = deg + (min - ((sec / 60.0) / 60.0));
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("Unable to parse DMS: "
+                            + dms, e);
+                }
+            } else {
+                throw new IllegalArgumentException("Unable to parse DMS: "
+                        + dms);
+            }
+            return degrees;
         }
 
         /*
@@ -176,7 +261,7 @@ public class TopoImporter {
 
             @Override
             public boolean accept(File dir, String name) {
-                return name.endsWith(".HDR");
+                return name.endsWith(".HDR") || name.endsWith(".ers");
             }
 
         };
@@ -195,6 +280,11 @@ public class TopoImporter {
             hdrList.add(new TopoHdr(file));
         }
 
+        if (hdrList.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "No recognized header files found");
+        }
+
         // sort the hdr files by descending lat/ascending lon
         Collections.sort(hdrList);
 
@@ -202,12 +292,14 @@ public class TopoImporter {
         // determine the total dataset dimensions
         double startLat = hdrList.get(0).ulYmap;
         double startLon = hdrList.get(0).ulXmap;
+        double xDim = hdrList.get(0).xDim;
+        double yDim = hdrList.get(0).yDim;
 
         int last = hdrList.size() - 1;
         double endLat = hdrList.get(last).ulYmap
-                - (hdrList.get(last).nRows - 1) * hdrList.get(last).yDim;
+                - ((hdrList.get(last).nRows - 1) * hdrList.get(last).yDim);
         double endLon = hdrList.get(last).ulXmap
-                + (hdrList.get(last).nCols - 1) * hdrList.get(last).xDim;
+                + ((hdrList.get(last).nCols - 1) * hdrList.get(last).xDim);
         int bits = hdrList.get(0).nBits;
 
         double expectedLat = startLat;
@@ -233,7 +325,7 @@ public class TopoImporter {
             maxCols = Math.max(cols, maxCols);
 
             expectedLat = hdr.ulYmap;
-            expectedLon = hdr.ulXmap + hdr.nCols * hdr.xDim;
+            expectedLon = hdr.ulXmap + (hdr.nCols * hdr.xDim);
             if ((expectedLon - startLon) > (360.0 - TOL)) {
                 expectedLon = startLon;
                 expectedLat -= hdr.nRows * hdr.yDim;
@@ -244,8 +336,8 @@ public class TopoImporter {
         }
 
         // create the hdf5 file
-        File hdf = new File(dir.getParent() + File.separatorChar
-                + dir.getName().toLowerCase() + "_unpacked.hdf");
+        File hdf = new File("topo" + File.separatorChar
+                + dir.getName().toLowerCase() + "_unpacked.h5");
         if (hdf.exists()) {
             System.out
                     .println(hdf.getAbsolutePath()
@@ -253,7 +345,9 @@ public class TopoImporter {
             System.exit(-1);
         }
 
-        IDataStore store = DataStoreFactory.getDataStore(hdf);
+        PypiesProperties pypiesProps = new PypiesProperties();
+        pypiesProps.setAddress("http://localhost:9582");
+        IDataStore store = new PyPiesDataStore(hdf, true, pypiesProps);
 
         String dataset = "full";
         long[] sizes = new long[] { maxCols, maxRows };
@@ -263,31 +357,47 @@ public class TopoImporter {
         properties.setChunked(true);
 
         IDataRecord record = null;
-        if (bits <= 8) {
-            record = new ByteDataRecord(dataset, "/", null, 2, sizes);
+        if (bits <= Byte.SIZE) {
+            record = new ByteDataRecord(dataset, null, null, 2, sizes);
             record.setFillValue(Byte.MIN_VALUE);
-        } else if (bits <= 16) {
-            record = new ShortDataRecord(dataset, "/", null, 2, sizes);
+        } else if (bits <= Short.SIZE) {
+            record = new ShortDataRecord(dataset, null, null, 2, sizes);
             record.setFillValue(Short.MIN_VALUE);
-        } else if (bits <= 32) {
-            record = new IntegerDataRecord(dataset, "/", null, 2, sizes);
+        } else if (bits <= Integer.SIZE) {
+            record = new IntegerDataRecord(dataset, null, null, 2, sizes);
             record.setFillValue(Integer.MIN_VALUE);
-        } else if (bits <= 64) {
-            record = new LongDataRecord(dataset, "/", null, 2, sizes);
+        } else if (bits <= Long.SIZE) {
+            record = new LongDataRecord(dataset, null, null, 2, sizes);
             record.setFillValue(Long.MIN_VALUE);
         } else {
             System.out.println("NBITS > 64");
             System.exit(-1);
         }
 
+        double centralMeridian = 0.0;
+        double latOfOrigin = 0.0;
+
+        double cm = (startLon + endLon) / 2;
+        centralMeridian = Math.round(cm);
+
+        ProjectedCRS crs = MapUtil.constructEquidistantCylindrical(
+                MapUtil.AWIPS_EARTH_RADIUS, MapUtil.AWIPS_EARTH_RADIUS,
+                centralMeridian, latOfOrigin);
+
         Map<String, Object> attributes = new LinkedHashMap<String, Object>();
         attributes.put("Width", maxCols);
         attributes.put("Height", maxRows);
-        attributes.put("ulLat", startLat);
-        attributes.put("ulLon", startLon);
-        attributes.put("lrLat", endLat);
-        attributes.put("lrLon", endLon);
-        attributes.put("CRS", MapUtil.LATLON_PROJECTION.toWKT());
+
+        // Using arrays to work around serialization "feature" that returns
+        // floats when doubles are stored
+        // H5DataStore created these arrays automatically, Pypies does not
+        attributes.put("xDim", new double[] { xDim });
+        attributes.put("yDim", new double[] { yDim });
+        attributes.put("ulLat", new double[] { startLat });
+        attributes.put("ulLon", new double[] { startLon });
+        attributes.put("lrLat", new double[] { endLat });
+        attributes.put("lrLon", new double[] { endLon });
+        attributes.put("CRS", crs.toWKT());
 
         record.setProperties(properties);
         record.setDataAttributes(attributes);
@@ -301,8 +411,14 @@ public class TopoImporter {
         rows = 0;
         cols = 0;
         for (TopoHdr hdr : hdrList) {
-            File dem = new File(hdr.file.getAbsolutePath().replace(".HDR",
-                    ".DEM"));
+            String demPath = hdr.file.getAbsolutePath();
+            if (demPath.endsWith(".HDR")) {
+                demPath = demPath.replace(".HDR", ".DEM");
+            } else if (demPath.endsWith(".ers")) {
+                demPath = demPath.replace(".ers", "");
+            }
+
+            File dem = new File(demPath);
 
             System.out.println(cols + ", " + rows + "  " + dem.getName());
 
@@ -311,76 +427,75 @@ public class TopoImporter {
                 in = new DataInputStream(new FileInputStream(dem));
                 ByteBuffer buffer = ByteBuffer.allocate(hdr.totalRowBytes);
 
-                if ("M".equals(hdr.byteOrder)) {
+                if (hdr.byteOrder.startsWith("M")) {
                     buffer.order(ByteOrder.BIG_ENDIAN);
                 } else {
                     buffer.order(ByteOrder.LITTLE_ENDIAN);
-                    System.out.println("WARNING: unrecognize BYTEORDER \""
-                            + hdr.byteOrder + "\" assuming LITTLE_ENDIAN");
                 }
 
                 sizes[0] = hdr.nCols;
                 sizes[1] = 1;
                 record = null;
+                long writeSize = 0;
                 for (int i = 0; i < hdr.nRows; i++) {
                     in.read(buffer.array());
                     buffer.rewind();
-                    if (bits <= 8) {
+                    if (bits <= Byte.SIZE) {
                         byte[] byteData = new byte[hdr.nCols];
                         for (int j = 0; j < byteData.length; j++) {
                             byteData[j] = buffer.get();
 
                             // replace no data flag with 0
                             if (byteData[j] == (byte) hdr.noData) {
-                                byteData[j] = 0;
+                                byteData[j] = Byte.MIN_VALUE;
                             }
                         }
-                        record = new ByteDataRecord(dataset, "/", null, 2,
+                        record = new ByteDataRecord(dataset, null, null, 2,
                                 sizes);
                         record.setMinIndex(new long[] { cols, rows + i });
                         record.setFillValue(Byte.MIN_VALUE);
                         ((ByteDataRecord) record).setByteData(buffer.array());
-                    } else if (bits <= 16) {
+                    } else if (bits <= Short.SIZE) {
                         short[] shortData = new short[hdr.nCols];
                         for (int j = 0; j < shortData.length; j++) {
                             shortData[j] = buffer.getShort();
 
                             // replace no data flag with 0
                             if (shortData[j] == (short) hdr.noData) {
-                                shortData[j] = 0;
+                                shortData[j] = Short.MIN_VALUE;
                             }
                         }
-                        record = new ShortDataRecord(dataset, "/", null, 2,
+                        record = new ShortDataRecord(dataset, null, null, 2,
                                 sizes);
                         record.setMinIndex(new long[] { cols, rows + i });
                         record.setFillValue(Short.MIN_VALUE);
                         ((ShortDataRecord) record).setShortData(shortData);
-                    } else if (bits <= 32) {
+                    } else if (bits <= Integer.SIZE) {
                         int[] intData = new int[hdr.nCols];
                         for (int j = 0; j < intData.length; j++) {
                             intData[j] = buffer.getInt();
 
                             // replace no data flag with 0
                             if (intData[j] == (int) hdr.noData) {
-                                intData[j] = 0;
+                                intData[j] = Integer.MIN_VALUE;
                             }
                         }
-                        record = new IntegerDataRecord(dataset, "/", null, 2,
+                        record = new IntegerDataRecord(dataset, null, null, 2,
                                 sizes);
                         record.setMinIndex(new long[] { cols, rows + i });
                         record.setFillValue(Integer.MIN_VALUE);
                         ((IntegerDataRecord) record).setIntData(intData);
-                    } else if (bits <= 64) {
+                    } else if (bits <= Long.SIZE) {
                         long[] longData = new long[hdr.nCols];
                         for (int j = 0; j < longData.length; j++) {
                             longData[j] = buffer.getLong();
 
                             // replace no data flag with 0
                             if (longData[j] == hdr.noData) {
-                                longData[j] = 0;
+                                longData[j] = Long.MIN_VALUE;
                             }
                         }
-                        record = new LongDataRecord(dataset, "/", null, 2,
+                        record = new LongDataRecord(dataset, null, null, 2,
                                 sizes);
                         record.setMinIndex(new long[] { cols, rows + i });
                         record.setFillValue(Long.MIN_VALUE);
@@ -388,6 +503,11 @@ public class TopoImporter {
                     }
 
                     store.addDataRecord(record, properties);
+                    writeSize += hdr.totalRowBytes;
+                    if (writeSize > (64 * 1024 * 1024)) {
+                        store.store();
+                        writeSize = 0;
+                    }
                 }
                 store.store();
             } catch (Throwable e) {
@@ -410,7 +530,7 @@ public class TopoImporter {
                 cols = 0;
             }
         }
-        System.out.println("took " + (System.currentTimeMillis() - t0) / 1000
+        System.out.println("took " + ((System.currentTimeMillis() - t0) / 1000)
                 + " seconds");
     }
 }
