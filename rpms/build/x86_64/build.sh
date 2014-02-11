dce599ed47a8103986ac5b550dd65f69e933ffdb
Brian.Dyke <Brian.Dyke@noaa.gov>
2014-02-11 15:52:50 -0500

    14.1.1-18 baseline
    Former-commit-id: 1029a34bfb9eac4c40ed56efd5308125a1389033 [formerly 1f27652aa3abf592cabf4468015d07a21913e83d] [formerly f05a2220cdd2c0d54d956313393a7706c6122632] [formerly f05a2220cdd2c0d54d956313393a7706c6122632 [formerly 575e328123bfbc199e55435a6555ba49512b161a]] [formerly 1029a34bfb9eac4c40ed56efd5308125a1389033 [formerly 1f27652aa3abf592cabf4468015d07a21913e83d] [formerly f05a2220cdd2c0d54d956313393a7706c6122632] [formerly f05a2220cdd2c0d54d956313393a7706c6122632 [formerly 575e328123bfbc199e55435a6555ba49512b161a]] [formerly 1d6a9456b7d6847327f1eb8cef049d7860dd8cdb [formerly f05a2220cdd2c0d54d956313393a7706c6122632 [formerly 575e328123bfbc199e55435a6555ba49512b161a] [formerly 1d6a9456b7d6847327f1eb8cef049d7860dd8cdb [formerly a8009c4ee3effc8586b830244db8db716ec0f37c]]]]]
    Former-commit-id: 1d6a9456b7d6847327f1eb8cef049d7860dd8cdb
    Former-commit-id: c465982c297a80377c375c9a07d241b5863bd5c9 [formerly 1c7bf39f73f9c6291c9be689d932f521a4d62704] [formerly d61ae6c5f4f0261e649bfafac00e6fee37c145f2] [formerly 673beee0d0b01bc69a4ad14a3a68f869a345e932 [formerly 57fbd21c8831f46d66fe301b77916657b0ab9f05] [formerly d61ae6c5f4f0261e649bfafac00e6fee37c145f2 [formerly c5857c30f2483dab037b2385ce0cca10fc1ca801]]]
    Former-commit-id: ec150c0b90835cc423916ac75ab1c42ebedc91c4 [formerly 48b7eeaa6cb834b10db17643ac629aaa112f6878] [formerly f47dd3ef595359597eb40acdee18bf3a4eb8be47 [formerly d1db845406a5f47f49d19440a8602b13269c7277]]
    Former-commit-id: f47dd3ef595359597eb40acdee18bf3a4eb8be47
    Former-commit-id: fc21d2dee4b7b4972457589601db07b934b0f612

--- rpms/build/x86_64/build.sh
+++ rpms/build/x86_64/build.sh
@@ -410,7 +410,8 @@ fi
 if [ "${1}" = "-viz" ]; then
    buildRPM "awips2"
    buildRPM "awips2-common-base"
-   buildRPM "awips2-python-numpy"
+   #buildRPM "awips2-python-numpy"
+   buildRPM "awips2-ant"
    #buildRPM "awips2-python-dynamicserialize"
    #buildRPM "awips2-python"
    #buildRPM "awips2-adapt-native"
@@ -421,9 +422,9 @@ if [ "${1}" = "-viz" ]; then
    #buildRPM "awips2-httpd-pypies"
    #buildRPM "awips2-hydroapps-shared"
    #buildRPM "awips2-rcm"
-   buildRPM "awips2-gfesuite-client"
-   buildRPM "awips2-gfesuite-server"
-   buildRPM "awips2-tools"
+   #buildRPM "awips2-gfesuite-client"
+   #buildRPM "awips2-gfesuite-server"
+   #buildRPM "awips2-tools"
    #buildRPM "awips2-cli"
    buildCAVE
    if [ $? -ne 0 ]; then
