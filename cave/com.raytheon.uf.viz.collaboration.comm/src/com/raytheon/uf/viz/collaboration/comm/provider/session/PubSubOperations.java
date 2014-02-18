4f7cdd21935d82920142ba52253e59dadaf1840c
Brian Clements <brian.d.clements@raytheon.com>
2014-02-18 09:49:40 -0600

    Issue #2751 implemented room/topic ownership transfer
    Former-commit-id: ab72938246e3408c89ad9708136232df9a2fd97d [formerly a8ff1effc126959d13da9099d8e11bfe8eb45f36 [formerly c3cadff7055655ec9d497d6eea5424dd6022ced5] [formerly ab72938246e3408c89ad9708136232df9a2fd97d [formerly cf5041f2d052c4900e2871e0fc8dc1a2b71794de]]]
    Former-commit-id: a8ff1effc126959d13da9099d8e11bfe8eb45f36 [formerly c3cadff7055655ec9d497d6eea5424dd6022ced5]
    Former-commit-id: a8ff1effc126959d13da9099d8e11bfe8eb45f36
    Former-commit-id: 4e165aba58fb8da3beb90ca53672166e0d60d9ae

---
+++ cave/com.raytheon.uf.viz.collaboration.comm/src/com/raytheon/uf/viz/collaboration/comm/provider/session/PubSubOperations.java
@@ -0,0 +1,68 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.comm.provider.session;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.IQ;
+import org.jivesoftware.smackx.pubsub.packet.PubSub;
+import org.jivesoftware.smackx.pubsub.packet.PubSubNamespace;
+import org.jivesoftware.smackx.pubsub.packet.SyncPacketSend;
+
+/**
+ * PubSub operations not implemented by the Smack PubSub manager. This includes
+ * operations to modify ownership of a topic node.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Feb 18, 2014 2751      bclement     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bclement
+ * @version 1.0
+ */
+public class PubSubOperations {
+
+    private PubSubOperations() {
+    }
+
+    /**
+     * Send packet to change affiliation of user on a pubsub topic node
+     * 
+     * @param conn
+     * @param affiliation
+     * @throws XMPPException
+     */
+    public static void sendAffiliationPacket(XMPPConnection conn,
+            ChangeAffiliationExtension affiliation) throws XMPPException {
+        PubSub packet = new PubSub();
+        packet.setType(IQ.Type.SET);
+        packet.setTo("pubsub." + conn.getServiceName());
+        packet.setPubSubNamespace(PubSubNamespace.OWNER);
+        packet.addExtension(affiliation);
+        SyncPacketSend.getReply(conn, packet);
+    }
+
+}
