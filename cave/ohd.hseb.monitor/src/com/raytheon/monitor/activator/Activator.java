a01a6d68d07c2a7a834127da518f16bf56fe8326
Max Schenkelberg <max.schenkelberg@raytheon.com>
2014-02-27 16:12:21 -0600

    Issue #2861 Rewrote HierarchicalPreferenceStore to be more efficient and thread safe when reading values.
    Amend: Added comments and cleaned up preference store
    
    Change-Id: Iac708c0988fc1b8f552d3bccf76b1706d6324b8d
    
    Former-commit-id: cc34e3dbebe13ec4f63b7529f5053391135837ef

--- cave/ohd.hseb.monitor/src/com/raytheon/monitor/activator/Activator.java
+++ cave/ohd.hseb.monitor/src/com/raytheon/monitor/activator/Activator.java
@@ -4,13 +4,24 @@ import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-import com.raytheon.uf.common.localization.exception.LocalizationException;
-import com.raytheon.uf.common.status.UFStatus;
-import com.raytheon.uf.common.status.UFStatus.Priority;
 import com.raytheon.uf.viz.core.localization.HierarchicalPreferenceStore;
 
 /**
+ * 
  * The activator class controls the plug-in life cycle
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 3, 2014  2861       mschenke    Create preference store immediately
+ * 
+ * </pre>
+ * 
+ * @author unknown
+ * @version 1.0
  */
 public class Activator extends AbstractUIPlugin {
 
@@ -20,7 +31,8 @@ public class Activator extends AbstractUIPlugin {
     // The shared instance
     private static Activator plugin;
 
-    private IPersistentPreferenceStore prefs;
+    private IPersistentPreferenceStore prefs = new HierarchicalPreferenceStore(
+            this);
 
     /**
      * The constructor
@@ -70,17 +82,6 @@ public class Activator extends AbstractUIPlugin {
      */
     @Override
     public IPersistentPreferenceStore getPreferenceStore() {
-        try {
-            if (prefs == null) {
-                prefs = new HierarchicalPreferenceStore(this);
-            }
-        } catch (LocalizationException e) {
-            UFStatus.getHandler().handle(
-                    Priority.PROBLEM,
-                    "Error reading preference store: "
-                            + e.getLocalizedMessage(), e);
-        }
-
         return prefs;
     }
 }
