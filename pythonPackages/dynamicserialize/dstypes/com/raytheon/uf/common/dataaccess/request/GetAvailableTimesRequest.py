9d757d79e2563816e6725a8609adea1a217eb687
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-03-03 17:43:26 -0600

    Issue #2673 Add ability to query only ref times to Data Access Layer.
    Former-commit-id: 2230164e1fabbef4059ec498b5120bed6da81c1d [formerly 641d4acd159c789f9644765a8bfcaf3f67f150a0] [formerly 2230164e1fabbef4059ec498b5120bed6da81c1d [formerly 641d4acd159c789f9644765a8bfcaf3f67f150a0] [formerly 9fcb7308580edc6ff8715d2213814a8a26d7a10b [formerly 37c406a9945bfb6ccb99d2472b44d649f2ce18d0]]]
    Former-commit-id: 9fcb7308580edc6ff8715d2213814a8a26d7a10b
    Former-commit-id: b9b5068329947491c5dc76ac2c497d8ac404f055 [formerly 1d2d1e20f5cf371fece19df32e9e4d7e0b2a2582]
    Former-commit-id: 4f01f96cf4a314282af7d0c8b1209396a6b26d6a

--- pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataaccess/request/GetAvailableTimesRequest.py
+++ pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataaccess/request/GetAvailableTimesRequest.py
@@ -27,6 +27,7 @@
 #    Date            Ticket#       Engineer       Description
 #    ------------    ----------    -----------    --------------------------
 #    05/28/13         #2023        dgilling       Initial Creation.    
+#    03/03/14         #2673        bsteffen       Add ability to query only ref times.
 #
 #
 
@@ -38,4 +39,10 @@ class GetAvailableTimesRequest(AbstractDataAccessRequest):
 
     def __init__(self):
         super(GetAvailableTimesRequest, self).__init__()
+        self.refTimeOnly = False
+        
+    def getRefTimeOnly(self):
+        return self.refTimeOnly
 
+    def setRefTimeOnly(self, refTimeOnly):
+        self.refTimeOnly = refTimeOnly
