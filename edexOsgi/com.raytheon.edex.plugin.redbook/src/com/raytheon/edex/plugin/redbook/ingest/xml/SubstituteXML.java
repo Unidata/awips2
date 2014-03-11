693793eb92ec4634cf48d5490cc01f42fc8460ec
Mike Duff <michael.duff@raytheon.com>
2014-03-11 15:54:35 -0500

    Issue #2858 - Implement NDM menus for ncep/hydro SPC Peer review comments
    Change-Id: Icbafa1e08ea9bc564c3f302715f527b92a5a55e5
    
    Former-commit-id: 9aa0b40ed576c0100d36d5c1bac0f251e3f2f3f9 [formerly a74573fef8cc09ed15341d9873ae1dd2ba561478]
    Former-commit-id: b434ab5bb3a0b093efed6e24033c2309356ee99f

---
+++ edexOsgi/com.raytheon.edex.plugin.redbook/src/com/raytheon/edex/plugin/redbook/ingest/xml/SubstituteXML.java
@@ -0,0 +1,97 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.edex.plugin.redbook.ingest.xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ * Redbook hazard substitute object.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 07, 2014    2858    mpduff      Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mpduff
+ * @version 1.0
+ */
+@XmlAccessorType(XmlAccessType.NONE)
+public class SubstituteXML {
+
+    /** Substitute key */
+    @XmlAttribute(name = "key")
+    protected String key;
+
+    /** Substitute value */
+    @XmlAttribute(name = "value")
+    protected String value;
+
+    /**
+     * @return the key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * @param key
+     *            the key to set
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value
+     *            the value to set
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        final String nl = System.getProperty("line.separator");
+        StringBuilder sb = new StringBuilder();
+        sb.append("    Key:   ").append(this.key).append(nl);
+        sb.append("    Value: ").append(this.value).append(nl);
+
+        return sb.toString();
+    }
+}
