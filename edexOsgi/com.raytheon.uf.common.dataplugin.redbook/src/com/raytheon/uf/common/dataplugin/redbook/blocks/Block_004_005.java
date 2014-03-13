c33b97aefec3cb16b5b9e867ebcbc7f5c271d318
Nate Jensen <Nathan.Jensen@raytheon.com>
2014-03-13 11:45:49 -0500

    Issue #2907 split edex.redbook plugin into common and edex redbook plugins
    Change-Id: If9f56b7891276410d54880387701cf6ff6291ffe
    
    Former-commit-id: ff79380bdf5417665a817bdcc1f5588bb193fb04 [formerly ff79380bdf5417665a817bdcc1f5588bb193fb04 [formerly 692ec623b22eeac06bd3d4de3e6942072bf416cc]] [formerly ee2929af54b8db827fde1221bf5a3c1dc665e839 [formerly 087307a781d78cc65a7e248c63e5182385aed10b]] [formerly ff79380bdf5417665a817bdcc1f5588bb193fb04 [formerly ff79380bdf5417665a817bdcc1f5588bb193fb04 [formerly 692ec623b22eeac06bd3d4de3e6942072bf416cc]] [formerly ee2929af54b8db827fde1221bf5a3c1dc665e839 [formerly 087307a781d78cc65a7e248c63e5182385aed10b]] [formerly cebdec53e99636403e74f16aedf2e77925c4a24c [formerly cebdec53e99636403e74f16aedf2e77925c4a24c [formerly cc1cb28eedde5089f6eb3fcbfd630c63f27a230e]] [formerly 087307a781d78cc65a7e248c63e5182385aed10b [formerly 087307a781d78cc65a7e248c63e5182385aed10b [formerly 62923fbae80131fab6256923bc32bc46e46940ba]]]]]
    Former-commit-id: cebdec53e99636403e74f16aedf2e77925c4a24c [formerly cebdec53e99636403e74f16aedf2e77925c4a24c [formerly cc1cb28eedde5089f6eb3fcbfd630c63f27a230e]]
    Former-commit-id: 12ef9286e8a9961da45643c90f1bca223add7abf [formerly 82e456027fedc05bb3c59074e34019a860dd8766] [formerly 47d6e3843ae19af8f1d6257c09230f81da58a7f5 [formerly 027a141c94aec3e3a16aa6c4caa0871371178d75]] [formerly c95c4ffb01694b0003e103049acfe535c9c80e9a [formerly 091c2206c4862eb7088e669ebb7561c349109875] [formerly 70bd6766205d2ea38df1885605967f39429fe740 [formerly daf6997a59f0d0898e28d646a2934e6f60127b0a]]]
    Former-commit-id: 420b6f47d3d443c99137028dc72dcfe3031e4019 [formerly ad3151ba9358995f8ef2a27798262001dcf8955f] [formerly 6960f7c41d931d72ee35afdddb43fda93ef58849 [formerly 1485f7ff48be1d4e6bafab39612f4b38b9ef2d5a]]
    Former-commit-id: 43e7e74f28f25025443e231c80c70f0688df1ad8 [formerly b1b0f40cdfb562cb9639d0df9eb1760ec8105531]
    Former-commit-id: 8a9485e5314b1534870ed81c5d94141b226749cc

--- edexOsgi/com.raytheon.uf.common.dataplugin.redbook/src/com/raytheon/uf/common/dataplugin/redbook/blocks/Block_004_005.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.redbook/src/com/raytheon/uf/common/dataplugin/redbook/blocks/Block_004_005.java
@@ -17,7 +17,7 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.edex.plugin.redbook.common.blocks;
+package com.raytheon.uf.common.dataplugin.redbook.blocks;
 
 import java.nio.ByteBuffer;
 
@@ -30,9 +30,11 @@ import java.nio.ByteBuffer;
  * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * 20080512           1131 jkorman     Initial implementation.
- * Apr 29, 2013 1958       bgonzale    Added class RedbookBlockHeader,
- *                                     and nested Factory class.
+ * May 12, 2008 1131       jkorman     Initial implementation.
+ * Apr 29, 2013 1958       bgonzale    Added class RedbookBlockHeader, and
+ *                                     nested Factory class.
+ * Mar 13, 2014 2907       njensen     split edex.redbook plugin into common and
+ *                                     edex redbook plugins
  * 
  * </pre>
  * 
@@ -58,7 +60,7 @@ public class Block_004_005 extends RedbookBlock {
     public Block_004_005(RedbookBlockHeader header, ByteBuffer data) {
         super(header, data);
         populate(data);
-        if(hasChkSum()) {
+        if (hasChkSum()) {
             data.getShort();
         }
     }
@@ -68,13 +70,14 @@ public class Block_004_005 extends RedbookBlock {
             dropShortsFromTheBuffer(data);
         }
     }
-    
+
     /**
      * 
      */
+    @Override
     public StringBuilder toString(StringBuilder sb) {
         sb = super.toString(sb);
-        
+
         return sb;
     }
 }
