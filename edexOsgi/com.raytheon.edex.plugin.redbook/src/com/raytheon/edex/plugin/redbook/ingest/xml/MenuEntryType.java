d757b01eddb3145dc027d256c45ab076848158de
Mike Duff <michael.duff@raytheon.com>
2014-03-18 09:02:15 -0500

    Issue #2855 - Implement HPC menus for NDM, refactor NDM code so more code is common. Peer review comments
    Change-Id: Ie27e8f93d976dcc0573631472986898b3ad8c9ef
    
    Former-commit-id: cb6a3e2cf5a5640a6b91d1fdb7a79ca6ed2120b9 [formerly 105a11d8f926dfeeb0278eb61f8bb0ed453a7e2c [formerly 29a88687a788305be537ef97cce13e3f45338205]]
    Former-commit-id: 105a11d8f926dfeeb0278eb61f8bb0ed453a7e2c
    Former-commit-id: 26866f4d73a79656d4c71d08dee9af13a37a2a76

---
+++ edexOsgi/com.raytheon.edex.plugin.redbook/src/com/raytheon/edex/plugin/redbook/ingest/xml/MenuEntryType.java
@@ -0,0 +1,85 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.edex.plugin.redbook.ingest.xml;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * NDM Menu Entry Types.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Mar 12, 2014   2855     mpduff      Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mpduff
+ * @version 1.0
+ */
+@XmlType(name = "type")
+@XmlEnum
+public enum MenuEntryType {
+    /** Menu Item type */
+    @XmlEnumValue("productButton")
+    ProductButton("productButton"),
+
+    /** Menu Separator type */
+    @XmlEnumValue("separator")
+    Separator("separator"),
+
+    /** Submenu type */
+    @XmlEnumValue("submenu")
+    Submenu("submenu"),
+
+    /** Substitute xml element type */
+    @XmlEnumValue("substitute")
+    Substitute("substitute"),
+
+    /** Menu Title type */
+    @XmlEnumValue("title")
+    Title("title");
+
+    /** Menu entry type */
+    private String entryType;
+
+    /**
+     * Private constructor.
+     * 
+     * @param entryType
+     */
+    private MenuEntryType(String entryType) {
+        this.entryType = entryType;
+    }
+
+    /**
+     * Get the type.
+     * 
+     * @return The type
+     */
+    public String getType() {
+        return entryType;
+    }
+}
