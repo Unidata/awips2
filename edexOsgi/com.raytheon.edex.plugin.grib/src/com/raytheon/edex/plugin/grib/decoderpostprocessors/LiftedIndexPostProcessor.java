7337fcf30c9691b58bb323597c5b2f85e1113921
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-03-28 17:18:49 -0500

    Issue #2874 RTOFS temperature unit correction and style enhancements
    Former-commit-id: dae20b905afadba2ca6a4bc6ec9589cc27a1d618 [formerly f2c605f6d3bcb82aa92740f8d62ef3fd64384037] [formerly a846430a84c628071e231e8f062ca990efc56356] [formerly dae20b905afadba2ca6a4bc6ec9589cc27a1d618 [formerly f2c605f6d3bcb82aa92740f8d62ef3fd64384037] [formerly a846430a84c628071e231e8f062ca990efc56356] [formerly 3baaf6543d68cd117f5791b543fcfd0427857940 [formerly a846430a84c628071e231e8f062ca990efc56356 [formerly 129dff2511c50abe9367e233fb64b5ca923db258]]]]
    Former-commit-id: 3baaf6543d68cd117f5791b543fcfd0427857940
    Former-commit-id: 12d697fc1c51850c4685788e83774c765123b8a6 [formerly 92beb626b40dbff4875a485716e37f908e28b56d] [formerly 34465f247844dc3904337e65921ee04c89a51fef [formerly 6125fee4e81edec229e74a088da57b272bfb9ab3]]
    Former-commit-id: e02fd4e9f63f34408435342cf436796d5f2fe02c [formerly 5b21c365f2525e16593ddc2991319ff15219b700]
    Former-commit-id: 4e84bd90eefca9a6be55f6f7e318f2d85ac9e6d8

--- edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/LiftedIndexPostProcessor.java
+++ edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/LiftedIndexPostProcessor.java
@@ -1,61 +1,52 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
 package com.raytheon.edex.plugin.grib.decoderpostprocessors;
 
-import java.util.Arrays;
-import java.util.List;
-
-import javax.measure.converter.UnitConverter;
-import javax.measure.unit.SI;
-
 import com.raytheon.edex.plugin.grib.exception.GribException;
-import com.raytheon.uf.common.dataplugin.grid.GridRecord;
+
 
 /**
  * 
- * Adjusts data values on Lifted Index to match the units. Lifted index should
- * always be centered around 0 however some sources go through a Celsius to
- * Kelvin conversion which results in data centered around 273.15, this attempts
- * to detect and revert this conversion.
+ * @deprecated Only exists for backwards compatibility in localization files,
+ *             use TemperatureCorrectionPostProcessor directly in the future.
  * 
- * <pre>
+ *             <pre>
  * 
  * SOFTWARE HISTORY
  * 
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Nov 18, 2010            bsteffen     Initial creation
+ * Date          Ticket#  Engineer    Description
+ * ------------- -------- ----------- --------------------------
+ * Nov 18, 2010           bsteffen    Initial creation
+ * Mar 28, 2010  2874     bsteffen    Deprecated
  * 
  * </pre>
  * 
  * @author bsteffen
  * @version 1.0
  */
-public class LiftedIndexPostProcessor implements IDecoderPostProcessor {
+@Deprecated
+public class LiftedIndexPostProcessor extends
+        TemperatureCorrectionPostProcessor {
 
-    private static final List<String> LIFTED_INDEX_PARAMETERS = Arrays.asList(
-            "SLI", "BLI", "PLI");
-
-    private static final String KELVIN_UNIT_STRING = "K";
-
-    private static final float MAX_VALID = 150;
-
-    private static final UnitConverter k2c = SI.KELVIN
-            .getConverterTo(SI.CELSIUS);
-
-    @Override
-    public GridRecord[] process(GridRecord record) throws GribException {
-        // Change units of Lifted Index to match the data
-        if (LIFTED_INDEX_PARAMETERS.contains(record.getParameter()
-                .getAbbreviation())
-                && KELVIN_UNIT_STRING.equals(record.getParameter()
-                        .getUnitString())) {
-            float[] data = (float[]) record.getMessageData();
-            for (int i = 0; i < data.length; i++) {
-                if (data[i] > MAX_VALID) {
-                    data[i] = (float) k2c.convert(data[i]);
-                }
-            }
-        }
-        return new GridRecord[] { record };
+    public LiftedIndexPostProcessor() throws GribException {
+        super();
     }
 
 }
