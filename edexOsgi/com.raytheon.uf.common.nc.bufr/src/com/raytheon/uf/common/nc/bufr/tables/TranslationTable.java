88c338e999266f73bab6796f48696b3044e4ff24
Brian Clements <brian.d.clements@raytheon.com>
2014-04-03 16:23:55 -0500

    Issue #2905 added BUFR parsing utilities
    added table units parser for BUFR table values
    added time units parser for BUFR time values
    added lookup methods to translation table
    added mapping class for BUFR fields to point data container parameters
    
    
    Former-commit-id: dacfc4082dec1c4889bca759626bf34422a1466a [formerly ed7a808b8771e3b1e44f152d9cc2ad2fb2a89b82 [formerly eaeab6dc6dfc9e0f3544ccb1ebea53669d2222c7]]
    Former-commit-id: ed7a808b8771e3b1e44f152d9cc2ad2fb2a89b82
    Former-commit-id: 43512fd90a20176a4c466c4e59b838d097207518

--- edexOsgi/com.raytheon.uf.common.nc.bufr/src/com/raytheon/uf/common/nc/bufr/tables/TranslationTable.java
+++ edexOsgi/com.raytheon.uf.common.nc.bufr/src/com/raytheon/uf/common/nc/bufr/tables/TranslationTable.java
@@ -19,8 +19,15 @@
  **/
 package com.raytheon.uf.common.nc.bufr.tables;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -38,6 +45,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Mar 24, 2014 2905       bclement     Initial creation
+ * Apr 03, 2014 2905       bclement     added lookup methods
  * 
  * </pre>
  * 
@@ -48,6 +56,10 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class TranslationTable {
 
+    public static enum TableType {
+        CODE, FLAG;
+    }
+
     @XmlAttribute(required = true)
     private String bufrTable;
 
@@ -56,6 +68,16 @@ public class TranslationTable {
 
     @XmlElement(name = "entry")
     private List<TableEntry> entries;
+
+    public static final Pattern ALL_PATTERN = Pattern
+            .compile("^[Aa]ll\\s+(\\d+)$");
+
+    public static final Pattern RANGE_PATTERN = Pattern
+            .compile("^(\\d+)-(\\d+)$");
+
+    public static final Pattern SINGLE_PATTERN = Pattern.compile("^\\d+$");
+
+    private transient volatile ArrayList<TableEntry> indexedEntries;
 
     /**
      * 
@@ -89,6 +111,161 @@ public class TranslationTable {
     }
 
     /**
+     * Lookup table entries for given key. Table will be interpreted differently
+     * depending on the table type. Code tables are a one-to-one mapping of
+     * index to value. Flag tables are bitmaps where values are returned for
+     * every set bit in the key.
+     * 
+     * @param key
+     * @param type
+     * @return
+     */
+    public List<TableEntry> lookupEntries(int key, TableType type) {
+        if (TableType.CODE.equals(type)) {
+            return lookupCodeEntries(key);
+        } else if (TableType.FLAG.equals(type)) {
+            return lookupFlagEntries(key);
+        } else {
+            throw new InvalidParameterException("Invalid table type: " + type);
+        }
+    }
+
+    /**
+     * Get table entry at key index
+     * 
+     * @param key
+     * @return
+     */
+    protected List<TableEntry> lookupCodeEntries(int key) {
+        ArrayList<TableEntry> indexed = getIndexedEntries();
+        if (key < 0 || key >= indexed.size()) {
+            return Collections.emptyList();
+        }
+        return Arrays.asList(indexed.get(key));
+    }
+
+    /**
+     * Flag table keys are bit maps stored in an integer. The bits are aligned
+     * so that the first bit in the bitmap is at the most significant bit of the
+     * integer. This method walks the bitmap in the key and returns every table
+     * entry that has a set bit in the bitmap. However, if the last bit in the
+     * bitmap is set, only a special table entry for Missing Value is returned.
+     * 
+     * @param key
+     *            unsigned bitmap
+     * @return
+     */
+    protected List<TableEntry> lookupFlagEntries(int key) {
+        ArrayList<TableEntry> indexed = getIndexedEntries();
+        final int lastIndex = indexed.size() - 1;
+        /* the set bit starts at 1, we want to move size - 1 bits over */
+        int mask = (0x80000000 >>> lastIndex);
+        List<TableEntry> rval;
+        if ((key & mask) != 0) {
+            /* last entry in table is Missing Value, return entry at that index */
+            rval = Arrays.asList(indexed.get(lastIndex));
+        } else {
+            /* not size of total table because missing value won't be included */
+            rval = new ArrayList<TableEntry>(lastIndex);
+            /* move back one from the missing value bit */
+            mask <<= 1;
+            /* walk table backwards adding included values */
+            for (int i = lastIndex - 1; i > -1; --i, mask <<= 1) {
+                if ((key & mask) != 0) {
+                    rval.add(indexed.get(i));
+                }
+            }
+        }
+        return rval;
+    }
+
+    /**
+     * Get processed entries ordered by index. Results are cached.
+     * 
+     * @return
+     */
+    protected ArrayList<TableEntry> getIndexedEntries() {
+        if (entries == null) {
+            return new ArrayList<TableEntry>(0);
+        }
+        if (indexedEntries == null) {
+            synchronized (this) {
+                if (indexedEntries == null) {
+                    indexedEntries = createCodeList(entries);
+                }
+            }
+        }
+        return indexedEntries;
+    }
+
+    /*
+     * priority queue node for ordered table entries
+     */
+    private static class QueueNode {
+        public final int index;
+
+        public final TableEntry entry;
+
+        public static final Comparator<QueueNode> comp = new Comparator<QueueNode>() {
+            @Override
+            public int compare(QueueNode o1, QueueNode o2) {
+                return Integer.compare(o1.index, o2.index);
+            }
+        };
+
+        public QueueNode(int index, TableEntry entry) {
+            this.index = index;
+            this.entry = entry;
+        }
+    }
+
+    /**
+     * Process table entries by ordering entries by index and placing them in an
+     * array-backed list for easy indexing
+     * 
+     * @param entries
+     * @return
+     */
+    private ArrayList<TableEntry> createCodeList(List<TableEntry> entries) {
+        /* put in heap first to fix any order issues in the XML */
+        PriorityQueue<QueueNode> queue = new PriorityQueue<QueueNode>(
+                entries.size(), QueueNode.comp);
+        for (TableEntry entry : entries) {
+            String indexRange = entry.getIndexRange().trim();
+            Matcher m = SINGLE_PATTERN.matcher(indexRange);
+            if (m.matches()) {
+                int index = Integer.valueOf(indexRange);
+                queue.add(new QueueNode(index, entry));
+            } else if ((m = RANGE_PATTERN.matcher(indexRange)).matches()) {
+                int start = Integer.parseInt(m.group(1));
+                int end = Integer.parseInt(m.group(2));
+                for (int i = start; i <= end; ++i) {
+                    queue.add(new QueueNode(i, entry));
+                }
+            } else if (((m = ALL_PATTERN.matcher(indexRange)).matches())) {
+                int index = Integer.valueOf(m.group(1));
+                queue.add(new QueueNode(index, entry));
+            } else {
+                throw new IllegalStateException("Invalid index range value: "
+                        + indexRange);
+            }
+        }
+        /* populate lookup table and verify table integrity */
+        ArrayList<TableEntry> rval = new ArrayList<TableEntry>(queue.size());
+        int prev = -1;
+        for (QueueNode node : queue) {
+            if (prev > -1 && prev != (node.index - 1)) {
+                throw new IllegalStateException("Invalid lookup table "
+                        + bufrTable
+                        + ", table must have contiguous index ranges");
+            }
+            rval.add(node.entry);
+            prev = node.index;
+        }
+        return rval;
+    }
+
+    /**
      * Append new entry to entries
      * 
      * @param entry
@@ -113,6 +290,21 @@ public class TranslationTable {
      */
     public void setBufrTable(String bufrTable) {
         this.bufrTable = bufrTable;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description
+     *            the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     /**
