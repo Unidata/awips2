a7123fd58ec545f782810869ffe7ec185cf79cde
Ron Anderson <ron.anderson@raytheon.com>
2014-04-04 12:07:32 -0500

    Issue #2694 Converted GetBrokerConnections to Java.
    Change-Id: Ic9445906ed88036d6598ffec0319de9c89f8c163
    
    Former-commit-id: 6222cb6eb9594a27798d6182f2097d0559860905 [formerly 7c9f4091c2098d456410c9aeaa3ec47835b7f977] [formerly d16380e50e44ed1fb01ee80f637bd47c050edcb0] [formerly d16380e50e44ed1fb01ee80f637bd47c050edcb0 [formerly a6794beaa5d920b8846d79161d09350a596d4a72]] [formerly df9c5c81d67967aad1996140e3d35a0393d663fd [formerly d16380e50e44ed1fb01ee80f637bd47c050edcb0 [formerly a6794beaa5d920b8846d79161d09350a596d4a72] [formerly df9c5c81d67967aad1996140e3d35a0393d663fd [formerly 102c81e1aafdcfa171440df1cba97d27b2ba3c2a]]]]
    Former-commit-id: df9c5c81d67967aad1996140e3d35a0393d663fd
    Former-commit-id: b4c4e64b16df7d08436aef4a610aea258919fda9 [formerly 7524b26cc572e199e0f694889a08af77618fbbde] [formerly d1cb49422247d365455a659097ab998da5f7fd0c [formerly d778e9dac6d50fe4485fa17fd9e9932f0a94294e]]
    Former-commit-id: d1cb49422247d365455a659097ab998da5f7fd0c
    Former-commit-id: 454323fd743d927b1cbddd05a64bcee5343a6f31

---
+++ edexOsgi/com.raytheon.uf.edex.esb.camel/src/com/raytheon/uf/edex/esb/camel/jms/IBrokerConnectionsProvider.java
@@ -0,0 +1,49 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.esb.camel.jms;
+
+import java.util.List;
+
+/**
+ * Interface for retrieving list of client IDs of active broker connections
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Apr 4, 2014            randerso     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author randerso
+ * @version 1.0
+ */
+public interface IBrokerConnectionsProvider {
+
+    /**
+     * @return list of Client IDs for active broker connections
+     * 
+     * @throws Exception
+     */
+    public List<String> getConnections() throws Exception;
+
+}
