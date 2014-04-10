890faf2d73d8466bce2c43e294c7dca1584d450d
Mike Duff <michael.duff@raytheon.com>
2014-04-10 10:43:49 -0500

    Issue #2864 - Change how saved subset files are stored.
    Change-Id: I57c68196a633a55a6f9c1548eff7ad449fadb88c
    
    Former-commit-id: 3052e5f92f4150fa82bfd93f512bb1670f926ec1

--- cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/SubsetFileManager.java
+++ cave/com.raytheon.uf.viz.datadelivery/src/com/raytheon/uf/viz/datadelivery/subscription/subset/SubsetFileManager.java
@@ -20,6 +20,12 @@
 package com.raytheon.uf.viz.datadelivery.subscription.subset;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -29,6 +35,7 @@ import javax.xml.bind.Unmarshaller;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 
+import com.raytheon.uf.common.datadelivery.registry.DataType;
 import com.raytheon.uf.common.localization.IPathManager;
 import com.raytheon.uf.common.localization.LocalizationContext;
 import com.raytheon.uf.common.localization.LocalizationContext.LocalizationLevel;
@@ -65,6 +72,7 @@ import com.raytheon.uf.viz.datadelivery.utils.DataDeliveryUtils;
  * Nov 19, 2012  1289      bgonzale   Added deleteArea(String) method.
  * Jun 04, 2013   223      mpduff     Added PointTimeXML to JaxB context.
  * Oct 11, 2013   2386     mpduff     Refactor DD Front end.
+ * Apr 10, 2014   2864     mpduff     Changed how saved subset files are stored.
  * 
  * </pre>
  * 
@@ -97,14 +105,15 @@ public class SubsetFileManager {
     /** Unmarshaller object */
     private Unmarshaller unmarshaller;
 
-    /** Array of subset files */
-    private LocalizationFile[] subsetFiles;
+    private final Map<DataType, Set<LocalizationFile>> subsetFiles = new HashMap<DataType, Set<LocalizationFile>>();
 
     /**
-     * Private constructor.
+     * Constructor.
      */
     private SubsetFileManager() {
         createContext();
+
+        populate();
     }
 
     /**
@@ -172,7 +181,6 @@ public class SubsetFileManager {
             return true;
         } catch (JAXBException e) {
             statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
-            e.printStackTrace();
         } catch (LocalizationOpFailedException e) {
             statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
         }
@@ -241,18 +249,40 @@ public class SubsetFileManager {
     /**
      * Get all the saved subset files
      * 
-     * @return LocalizationFile[]
+     * @param type
+     *            Data type of files to retrieve
+     * 
+     * @return LocalizationFile[] Array of files of DataType type
      */
-    public LocalizationFile[] getSubsets() {
+    private Set<LocalizationFile> getSubsetFiles(DataType type) {
         IPathManager pm = PathManagerFactory.getPathManager();
 
         LocalizationContext context = pm.getContext(
                 LocalizationType.CAVE_STATIC, LocalizationLevel.USER);
         String[] extensions = new String[] { "xml" };
-        this.subsetFiles = pm.listFiles(context, SUBSET_PATH, extensions,
-                false, true);
+        String path = null;
 
-        return subsetFiles;
+        path = SUBSET_PATH + type.toString() + File.separator;
+
+        LocalizationFile[] fileArr = pm.listFiles(context, path, extensions,
+                false, true);
+        Set<LocalizationFile> files = new TreeSet<LocalizationFile>();
+        for (LocalizationFile file : fileArr) {
+            files.add(file);
+        }
+
+        return files;
+    }
+
+    private void populate() {
+        subsetFiles.clear();
+        for (DataType type : DataType.values()) {
+            subsetFiles.put(type, getSubsetFiles(type));
+        }
+    }
+
+    public Set<LocalizationFile> getLocalizationFiles(DataType type) {
+        return subsetFiles.get(type);
     }
 
     /**
@@ -260,17 +290,20 @@ public class SubsetFileManager {
      * 
      * @param subset
      *            the object to save
+     * @param type
+     *            DataType
      * @param shell
      *            The calling dialog's shell
      * @return true if successfully saved
      */
-    public boolean saveSubset(SubsetXML subset, Shell shell) {
+    public boolean saveSubset(SubsetXML subset, DataType type, Shell shell) {
         IPathManager pm = PathManagerFactory.getPathManager();
 
         LocalizationContext context = pm.getContext(
                 LocalizationType.CAVE_STATIC, LocalizationLevel.USER);
         LocalizationFile subsetLocFile = pm.getLocalizationFile(context,
-                SUBSET_PATH + subset.getSubsetName());
+                SUBSET_PATH + File.separator + type.toString() + File.separator
+                        + subset.getSubsetName());
 
         if (subsetLocFile.getFile().exists()) {
             String msg = "The file "
@@ -286,7 +319,7 @@ public class SubsetFileManager {
         try {
             marshaller.marshal(subset, subsetLocFile.getFile());
             subsetLocFile.save();
-
+            subsetFiles.get(type).add(subsetLocFile);
             return true;
         } catch (JAXBException e) {
             statusHandler.handle(Priority.ERROR, e.getLocalizedMessage(), e);
@@ -302,16 +335,13 @@ public class SubsetFileManager {
      * 
      * @param subsetName
      *            The subset name
+     * @param type
+     *            The data type
      * 
-     * @return The SubsetXML object
+     * @return The SubsetXML object or null if none exist
      */
-    public SubsetXML loadSubset(String subsetName) {
-        // Load the subset files if not already loaded
-        if (subsetFiles == null || subsetFiles.length == 0) {
-            getSubsets();
-        }
-
-        for (LocalizationFile lf : subsetFiles) {
+    public SubsetXML loadSubset(String subsetName, DataType type) {
+        for (LocalizationFile lf : subsetFiles.get(type)) {
             if (lf.getFile().getName().equals(subsetName)) {
                 try {
                     return (SubsetXML) unmarshaller.unmarshal(lf.getFile());
@@ -326,26 +356,76 @@ public class SubsetFileManager {
     }
 
     /**
+     * Load a saved subset into memory
+     * 
+     * @param file
+     *            The subset name
+     * 
+     * @return The SubsetXML object or null if none exist
+     */
+    public SubsetXML loadSubset(LocalizationFile file) {
+
+        if (file.exists()) {
+            try {
+                return (SubsetXML) unmarshaller.unmarshal(file.getFile());
+            } catch (JAXBException e) {
+                statusHandler.handle(Priority.PROBLEM, e.getLocalizedMessage(),
+                        e);
+            }
+        }
+
+        return null;
+    }
+
+    /**
      * Delete the subset
      * 
      * @param subsetName
      *            The name of the subset to delete
+     * @param type
+     *            The data type
      */
-    public void deleteSubset(String subsetName) {
-        // Load the subset files if not already loaded
-        if (subsetFiles == null || subsetFiles.length == 0) {
-            getSubsets();
-        }
-
-        for (LocalizationFile lf : subsetFiles) {
+    public void deleteSubset(String subsetName, DataType type) {
+        LocalizationFile file = null;
+        for (LocalizationFile lf : subsetFiles.get(type)) {
             if (lf.getFile().getName().equals(subsetName)) {
                 try {
                     lf.delete();
+                    file = lf;
+                    break;
                 } catch (LocalizationOpFailedException e) {
                     statusHandler.handle(Priority.PROBLEM,
                             e.getLocalizedMessage(), e);
                 }
             }
         }
+
+        if (file != null) {
+            subsetFiles.get(type).remove(file);
+        }
+    }
+
+    public String[] getAllLocalizationFileNames() {
+        List<String> fileList = new ArrayList<String>();
+
+        for (DataType type : DataType.values()) {
+            Set<LocalizationFile> files = getLocalizationFiles(type);
+            for (LocalizationFile file : files) {
+                fileList.add(type.toString() + ":" + file.getFile().getName());
+            }
+        }
+
+        return fileList.toArray(new String[fileList.size()]);
+    }
+
+    public LocalizationFile getFile(String fileName, DataType dataType) {
+        Set<LocalizationFile> files = subsetFiles.get(dataType);
+        for (LocalizationFile file : files) {
+            if (file.getFile().getName().equals(fileName)) {
+                return file;
+            }
+        }
+
+        return null;
     }
 }
