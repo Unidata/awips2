4afb673ae9916deab751ec172fdae65fd3bfdcf3
Steve Harris <Steven_L_Harris@raytheon.com>
2014-04-28 09:54:24 -0500

    Omaha #3085 - Removed ufcore and ufcore-foss plugins
    Former-commit-id: 491b7682045e4cce5446d05ffb3da98a3003ae34 [formerly 03e7d5d06fa736c6e8ab65ab9a024bb4a9a2bf0c [formerly a89621797d52cb28c9804e72f6ee540730d1762e] [formerly 491b7682045e4cce5446d05ffb3da98a3003ae34 [formerly 5dacfcc17cf99c604c0d4d2669c0aab962eca3ca]]]
    Former-commit-id: 03e7d5d06fa736c6e8ab65ab9a024bb4a9a2bf0c [formerly a89621797d52cb28c9804e72f6ee540730d1762e]
    Former-commit-id: 03e7d5d06fa736c6e8ab65ab9a024bb4a9a2bf0c
    Former-commit-id: 8fae55b404e1295b429273fe9a1f015d97ea008e

--- cave/com.raytheon.viz.core.gl/localization/glsl/raster.glsl
+++
@@ -1,9 +0,0 @@
-#include <coloring>
-
-uniform sampler2D rawTex;
-uniform ColorModifiers modifiers;
-
-void main(void) {
-    vec4 textureColor = texture2D(rawTex, gl_TexCoord[0].st);
-    gl_FragColor = applyColorModifiers(textureColor, modifiers);
-}
