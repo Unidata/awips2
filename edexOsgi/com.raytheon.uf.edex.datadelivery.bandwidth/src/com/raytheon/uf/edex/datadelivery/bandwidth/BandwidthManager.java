bf42c8d740a066d38b654cae1b478de2ef4373c6
Dave Hladky <david_j_hladky@raytheon.com>
2014-04-30 13:33:03 -0500

    Issue #2992 Updated DD list of active registries Change-Id: Iaef7dc76442d1e101cf9865a542a777afe1722ca
    Former-commit-id: c352643c794865ebe9ab008e012fdc3c681f764e [formerly a34955102d1316135c4151210daba0e5807ff6c9]
    Former-commit-id: 3b00ae03ccc26af2e9ed4bee1478d330bb35bb26

--- edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/BandwidthManager.java
+++ edexOsgi/com.raytheon.uf.edex.datadelivery.bandwidth/src/com/raytheon/uf/edex/datadelivery/bandwidth/BandwidthManager.java
@@ -22,8 +22,8 @@ import com.google.common.collect.Sets;
 import com.raytheon.edex.util.Util;
 import com.raytheon.uf.common.auth.exception.AuthorizationException;
 import com.raytheon.uf.common.auth.user.IUser;
-import com.raytheon.uf.common.datadelivery.bandwidth.IBandwidthRequest;
-import com.raytheon.uf.common.datadelivery.bandwidth.IBandwidthRequest.RequestType;
+import com.raytheon.uf.common.datadelivery.bandwidth.BandwidthRequest;
+import com.raytheon.uf.common.datadelivery.bandwidth.BandwidthRequest.RequestType;
 import com.raytheon.uf.common.datadelivery.bandwidth.IProposeScheduleResponse;
 import com.raytheon.uf.common.datadelivery.bandwidth.ProposeScheduleResponse;
 import com.raytheon.uf.common.datadelivery.bandwidth.data.BandwidthGraphData;
@@ -80,8 +80,8 @@ import com.raytheon.uf.edex.datadelivery.bandwidth.retrieval.RetrievalPlan;
 import com.raytheon.uf.edex.datadelivery.bandwidth.retrieval.RetrievalStatus;
 import com.raytheon.uf.edex.datadelivery.bandwidth.util.BandwidthDaoUtil;
 import com.raytheon.uf.edex.datadelivery.bandwidth.util.BandwidthUtil;
-import com.raytheon.uf.edex.datadelivery.util.DataDeliveryIdUtil;
 import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
+import com.raytheon.uf.edex.registry.ebxml.util.RegistryIdUtil;
 
 /**
  * Abstract {@link IBandwidthManager} implementation which provides core
@@ -148,6 +148,7 @@ import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
  * Feb 11, 2014 2771       bgonzale     Added handler for GET_DATADELIVERY_ID request.
  * Feb 10, 2014 2636       mpduff       Changed how retrieval plan is updated over time.
  * Apr 02, 2014  2810      dhladky      Priority sorting of subscriptions.
+ * Apr 22, 2014 2992       dhladky      Ability to get list of all registry nodes containing data.
  * 
  * </pre>
  * 
@@ -155,13 +156,16 @@ import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
  * @version 1.0
  */
 public abstract class BandwidthManager<T extends Time, C extends Coverage>
-        extends AbstractPrivilegedRequestHandler<IBandwidthRequest<T, C>>
+        extends AbstractPrivilegedRequestHandler<BandwidthRequest<T, C>>
         implements IBandwidthManager<T, C> {
 
     protected static final IUFStatusHandler statusHandler = UFStatus
             .getHandler(BandwidthManager.class);
 
     private static final Pattern RES_PATTERN = Pattern.compile("^res");
+    
+    /** used to query for registry subscription object owners **/
+    private static final String objectType = "'urn:oasis:names:tc:ebxml-regrep:ObjectType:RegistryObject:com.raytheon.uf.common.datadelivery.registry.%Subscription'";
 
     // Requires package access so it can be accessed from the maintenance task
     final IBandwidthDao<T, C> bandwidthDao;
@@ -173,6 +177,8 @@ public abstract class BandwidthManager<T extends Time, C extends Coverage>
     protected final BandwidthDaoUtil<T, C> bandwidthDaoUtil;
 
     private final IBandwidthDbInit dbInit;
+    
+    private final RegistryIdUtil idUtil;
 
     // Instance variable and not static, because there are multiple child
     // implementation classes which should each have a unique prefix
@@ -185,11 +191,13 @@ public abstract class BandwidthManager<T extends Time, C extends Coverage>
     public BandwidthManager(IBandwidthDbInit dbInit,
             IBandwidthDao<T, C> bandwidthDao,
             RetrievalManager retrievalManager,
-            BandwidthDaoUtil<T, C> bandwidthDaoUtil) {
+            BandwidthDaoUtil<T, C> bandwidthDaoUtil,
+            RegistryIdUtil idUtil) {
         this.dbInit = dbInit;
         this.bandwidthDao = bandwidthDao;
         this.retrievalManager = retrievalManager;
         this.bandwidthDaoUtil = bandwidthDaoUtil;
+        this.idUtil = idUtil;
     }
 
     /**
@@ -878,7 +886,7 @@ public abstract class BandwidthManager<T extends Time, C extends Coverage>
      * {@inheritDoc}
      */
     @Override
-    public Object handleRequest(IBandwidthRequest<T, C> request)
+    public Object handleRequest(BandwidthRequest<T, C> request)
             throws Exception {
 
         ITimer timer = TimeUtil.getTimer();
@@ -982,7 +990,10 @@ public abstract class BandwidthManager<T extends Time, C extends Coverage>
             response = getBandwidthGraphData();
             break;
         case GET_DATADELIVERY_ID:
-            response = DataDeliveryIdUtil.getId();
+            response = RegistryIdUtil.getId();
+            break;
+        case GET_DATADELIVERY_REGISTRIES:
+            response = idUtil.getUniqueRegistries(objectType);
             break;
         case GET_SUBSCRIPTION_STATUS:
             Subscription<T, C> sub = null;
@@ -1440,7 +1451,7 @@ public abstract class BandwidthManager<T extends Time, C extends Coverage>
      */
     @Override
     public AuthorizationResponse authorized(IUser user,
-            IBandwidthRequest request) throws AuthorizationException {
+            BandwidthRequest request) throws AuthorizationException {
         return new AuthorizationResponse(true);
     }
 
