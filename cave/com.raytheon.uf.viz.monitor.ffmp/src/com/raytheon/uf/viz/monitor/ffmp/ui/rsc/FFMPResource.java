7b7da767363abd94e26de0642648ebc2421944e6
Gang.Zhang <Gang.Zhang@noaa.gov>
2014-05-01 15:44:44 +0100

    ASM #507 - Fix incorrect FFMP  gap calculation
    Change-Id: Iecfdcaa07aca79b0e17396a794851cec0e6cba25
    
    Former-commit-id: 12c35396a0569fd5902132de662a39c3566dd948 [formerly 7d2b256ba9956243fc95d86b0e82e4ed495f0350] [formerly 74dc7a5d7c2dfa500f233187464fec74eca15e4a] [formerly 12c35396a0569fd5902132de662a39c3566dd948 [formerly 7d2b256ba9956243fc95d86b0e82e4ed495f0350] [formerly 74dc7a5d7c2dfa500f233187464fec74eca15e4a] [formerly 090ed2938687c6a9e92f9ff824ea6945718640bd [formerly 74dc7a5d7c2dfa500f233187464fec74eca15e4a [formerly 9b33d5187b4535d5500762e15e35e89ccbf5f5da]]]]
    Former-commit-id: 090ed2938687c6a9e92f9ff824ea6945718640bd
    Former-commit-id: 33a90ae4c77fc4cf9ac6214ac1b77178e1b7c1a7 [formerly 3131e25e697392a58cc5b413900f5c948440e62b] [formerly d4a69ce734b225aa7656a40a997cd81b9f326650 [formerly 7c7adbd77d3942713f9b86c1751fbe017f506ad5]]
    Former-commit-id: 899c9d1d8f87cbdd6308b497bb254f9b31aba862 [formerly 2b25e8c401aff8041e89ce06611505b2f6c27812]
    Former-commit-id: d4b87f3bad10b1c34e58334fab11692566bf758e

--- cave/com.raytheon.uf.viz.monitor.ffmp/src/com/raytheon/uf/viz/monitor/ffmp/ui/rsc/FFMPResource.java
+++ cave/com.raytheon.uf.viz.monitor.ffmp/src/com/raytheon/uf/viz/monitor/ffmp/ui/rsc/FFMPResource.java
@@ -176,6 +176,7 @@ import com.vividsolutions.jts.geom.Point;
  * Jan 21, 2014  DR 15874   gzhang		Use getValue() for QPFSCAN independent. 
  * Feb 19, 2014 2819        randerso    Removed unnecessary .clone() call
  * Mar  3, 2014 2804        mschenke    Set back up clipping pane
+ * Apr 30, 2014  DR 16148   gzhang      Filter Basin Dates for Trend and Table Gap. 
  * </pre>
  * 
  * @author dhladky
@@ -3102,6 +3103,9 @@ public class FFMPResource extends
         boolean guid = false;
         Date oldestRefTime = getOldestTime();
         Date mostRecentRefTime = getPaintTime().getRefTime();
+        
+        Date barrierTime = getTableTime();// DR 16148
+        Date minUriTime = getTimeOrderedKeys().get(0);// DR 16148
 
         // grabs the basins we need
         try {
@@ -3114,7 +3118,10 @@ public class FFMPResource extends
 
             if (rateBasin != null) {
                 for (Date date : rateBasin.getValues().keySet()) {
-
+                    
+                    if (date.before(minUriTime) || date.before(barrierTime) || date.after(mostRecentRefTime))   
+                        continue;// DR 16148
+                    
                     double dtime = FFMPGuiUtils.getTimeDiff(mostRecentRefTime,
                             date);
                     fgd.setRate(dtime, (double) rateBasin.getValue(date));
@@ -3139,6 +3146,9 @@ public class FFMPResource extends
             if (qpeBasin != null) {
 
                 for (Date date : qpeBasin.getValues().keySet()) {
+                    
+                    if (date.before(minUriTime) || date.before(barrierTime) || date.after(mostRecentRefTime))   
+                        continue;// DR 16148
 
                     double dtime = FFMPGuiUtils.getTimeDiff(mostRecentRefTime,
                             date);
