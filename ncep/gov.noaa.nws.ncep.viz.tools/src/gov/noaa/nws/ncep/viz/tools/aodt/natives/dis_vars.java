79244f50323762b776f4c3b3ff00be7b3423528e
Bryan Kowal <Bryan.Kowal@raytheon.com>
2014-05-08 16:20:35 -0500

    Issue #3108 - update bignsharp for Windows 64-bit compatibility. Upgrade JNA.
    Amend: updated and/or added change revision history comments
    Amend: fixed Java requirement in JNA classpath
    Amend: fixed Java requirement in JNA MANIFEST
    
    Change-Id: I5033f05c48a271fe08d3ec2ac01fe79481564371
    
    Former-commit-id: 9c29e6739cdc23a6f1e388970e91666d9f8095f5 [formerly fb12234836a3562156a30531e6961c0a064588bd]
    Former-commit-id: 1e1f46e68baf4d9af2970f723f76bf0be1332ad9

--- ncep/gov.noaa.nws.ncep.viz.tools/src/gov/noaa/nws/ncep/viz/tools/aodt/natives/dis_vars.java
+++ ncep/gov.noaa.nws.ncep.viz.tools/src/gov/noaa/nws/ncep/viz/tools/aodt/natives/dis_vars.java
@@ -1,27 +1,75 @@
+/**
+ * 
+ * 
+ * This java class performs the NSHARP NsharpNative functions.
+ * This code has been developed by the NCEP-SIB for use in the AWIPS2 system.
+ * 
+ * <pre>
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#     Engineer    Description
+ * -------      -------     --------    -----------
+ * ??/??/???    ?                       Initial coding
+ * 05/08/2014   3108        bkowal      Updated structure classes for the JNA upgrade.
+ * 
+ *
+ * </pre>
+ * 
+ * @author ?
+ * @version 1.0
+ */
 package gov.noaa.nws.ncep.viz.tools.aodt.natives;
+
+import java.util.Arrays;
+import java.util.List;
 
 import com.sun.jna.Structure;
 
-public class dis_vars extends Structure{
-	/// Output number of lines
+public class dis_vars extends Structure {
+    // / Output number of lines
     public double xrectl;
-    /// Output number of elems
+
+    // / Output number of elems
     public double xrecte;
+
     public dis_vars() {
-            super();
+        super();
     }
+
     /**
-     * @param xrectl Output number of lines<br>
-     * @param xrecte Output number of elems
+     * @param xrectl
+     *            Output number of lines<br>
+     * @param xrecte
+     *            Output number of elems
      */
     public dis_vars(double xrectl, double xrecte) {
-            super();
-            this.xrectl = xrectl;
-            this.xrecte = xrecte;
+        super();
+        this.xrectl = xrectl;
+        this.xrecte = xrecte;
     }
-    protected ByReference newByReference() { return new ByReference(); }
-    protected ByValue newByValue() { return new ByValue(); }
-    protected dis_vars newInstance() { return new dis_vars(); }
-    public static class ByReference extends dis_vars implements com.sun.jna.Structure.ByReference {}
-    public static class ByValue extends dis_vars implements com.sun.jna.Structure.ByValue {}
+
+    protected ByReference newByReference() {
+        return new ByReference();
+    }
+
+    protected ByValue newByValue() {
+        return new ByValue();
+    }
+
+    protected dis_vars newInstance() {
+        return new dis_vars();
+    }
+
+    public static class ByReference extends dis_vars implements
+            com.sun.jna.Structure.ByReference {
+    }
+
+    public static class ByValue extends dis_vars implements
+            com.sun.jna.Structure.ByValue {
+    }
+
+    @Override
+    protected List getFieldOrder() {
+        return Arrays.asList(new String[] { "xrectl", "xrecte" });
+    }
 }
