a07ad6724ee7a86b73eff60e7d355319b35ea55b
Bryan Kowal <Bryan.Kowal@raytheon.com>
2014-05-08 16:20:35 -0500

    Issue #3108 - update bignsharp for Windows 64-bit compatibility. Upgrade JNA.
    Amend: updated and/or added change revision history comments
    Amend: fixed Java requirement in JNA classpath
    Amend: fixed Java requirement in JNA MANIFEST
    
    Change-Id: I5033f05c48a271fe08d3ec2ac01fe79481564371
    
    Former-commit-id: 79244f50323762b776f4c3b3ff00be7b3423528e [formerly 1e1f46e68baf4d9af2970f723f76bf0be1332ad9] [formerly 79244f50323762b776f4c3b3ff00be7b3423528e [formerly 1e1f46e68baf4d9af2970f723f76bf0be1332ad9] [formerly 9c29e6739cdc23a6f1e388970e91666d9f8095f5 [formerly fb12234836a3562156a30531e6961c0a064588bd]]]
    Former-commit-id: 9c29e6739cdc23a6f1e388970e91666d9f8095f5
    Former-commit-id: 2098b015f1b6e23dcedd21a7e0b249461fd7c704 [formerly c1e9154e89f469cd93ac8fa79aa2fff187b02a91]
    Former-commit-id: ec41dab6d06b2f951edc16b3ffe8f2709a589813

--- ncep/gov.noaa.nws.ncep.viz.tools/src/gov/noaa/nws/ncep/viz/tools/aodt/natives/tiff_header.java
+++ ncep/gov.noaa.nws.ncep.viz.tools/src/gov/noaa/nws/ncep/viz/tools/aodt/natives/tiff_header.java
@@ -1,32 +1,82 @@
+/**
+ * 
+ * 
+ * This java class performs the NSHARP NsharpNative functions.
+ * This code has been developed by the NCEP-SIB for use in the AWIPS2 system.
+ * 
+ * <pre>
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#     Engineer    Description
+ * -------      -------     --------    -----------
+ * ??/??/???    ?                       Initial coding
+ * 05/08/2014   3108        bkowal      Updated structure classes for the JNA upgrade.
+ * 
+ *
+ * </pre>
+ * 
+ * @author ?
+ * @version 1.0
+ */
 package gov.noaa.nws.ncep.viz.tools.aodt.natives;
+
+import java.util.Arrays;
+import java.util.List;
 
 import com.sun.jna.Structure;
 
-public class tiff_header extends Structure{
-	 /// Byte order
+public class tiff_header extends Structure {
+    // / Byte order
     public int order;
-    /// Version
+
+    // / Version
     public int version;
-    /// Pointer
+
+    // / Pointer
     public int point;
+
     public tiff_header() {
-            super();
+        super();
     }
+
     /**
-     * @param order Byte order<br>
-     * @param version Version<br>
-     * @param point Pointer
+     * @param order
+     *            Byte order<br>
+     * @param version
+     *            Version<br>
+     * @param point
+     *            Pointer
      */
     public tiff_header(int order, int version, int point) {
-            super();
-            this.order = order;
-            this.version = version;
-            this.point = point;
+        super();
+        this.order = order;
+        this.version = version;
+        this.point = point;
     }
-    protected ByReference newByReference() { return new ByReference(); }
-    protected ByValue newByValue() { return new ByValue(); }
-    protected tiff_header newInstance() { return new tiff_header(); }
-    public static class ByReference extends tiff_header implements com.sun.jna.Structure.ByReference {}
-    public static class ByValue extends tiff_header implements com.sun.jna.Structure.ByValue {}
+
+    protected ByReference newByReference() {
+        return new ByReference();
+    }
+
+    protected ByValue newByValue() {
+        return new ByValue();
+    }
+
+    protected tiff_header newInstance() {
+        return new tiff_header();
+    }
+
+    public static class ByReference extends tiff_header implements
+            com.sun.jna.Structure.ByReference {
+    }
+
+    public static class ByValue extends tiff_header implements
+            com.sun.jna.Structure.ByValue {
+    }
+
+    @Override
+    protected List getFieldOrder() {
+        return Arrays.asList(new String[] { "order", "version", "point" });
+    }
 
 }
