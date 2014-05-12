994f7d820d9f741e375501fc2353d0f34069db26
Brian Clements <brian.d.clements@raytheon.com>
2014-05-12 17:32:54 -0500

    Omaha #2536 moved com.raytheon.edex.plugin.text to com.raytheon.uf.edex.plugin.text
    minor clode cleanup
    
    
    Former-commit-id: f68b6921c08b8bccf355fa9c991c232f2ffa2076

---
+++ edexOsgi/com.raytheon.uf.edex.plugin.text/src/com/raytheon/uf/edex/plugin/text/handler/TextProductInfoCreateHandler.java
@@ -0,0 +1,46 @@
+package com.raytheon.uf.edex.plugin.text.handler;
+
+import com.raytheon.edex.textdb.dao.TextProductInfoDao;
+import com.raytheon.uf.common.dataplugin.text.db.TextProductInfo;
+import com.raytheon.uf.common.dataplugin.text.request.TextProductInfoCreateRequest;
+import com.raytheon.uf.common.serialization.comm.IRequestHandler;
+
+/**
+ * TODO Add Description Register request and handler beans in text-request.xml.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 19May2010               cjeanbap    Initial creation
+ * 02Aug2010    2187       cjeanbap    Move AlarmAlertUtil.sendProductAlarmAlert() 
+ *                                     outside of if-statement.
+ * 
+ * </pre>
+ * 
+ * @author cjeanbap
+ * @version 1.0
+ */
+public class TextProductInfoCreateHandler implements
+        IRequestHandler<TextProductInfoCreateRequest> {
+
+    private TextProductInfoDao textDao = new TextProductInfoDao();
+
+    @Override
+    public Object handleRequest(TextProductInfoCreateRequest request)
+            throws Exception {
+        Boolean rval = Boolean.TRUE;
+
+        String cccid = request.getCccid();
+        String nnnid = request.getNnnid();
+        String xxxid = request.getXxxid();
+        TextProductInfo info = textDao.find(cccid, nnnid, xxxid);
+        if (info == null) {
+            info = new TextProductInfo(cccid, nnnid, xxxid);
+            rval = textDao.write(info);
+        }
+
+        return rval;
+    }
+}
