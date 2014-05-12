4ed91c892a3dca8e35ed8946c538a2492fa5752b
Brian Clements <brian.d.clements@raytheon.com>
2014-05-12 17:32:54 -0500

    Omaha #2536 moved com.raytheon.edex.plugin.text to com.raytheon.uf.edex.plugin.text
    minor clode cleanup
    
    
    Former-commit-id: e36d47ed6f3faec377f6e347cdfa09fb38b6ad82 [formerly e36d47ed6f3faec377f6e347cdfa09fb38b6ad82 [formerly 648bed10a3f52f03205b6fa04c2ab1a0d9ea51af]] [formerly b9d9beeb4682efb9837667527bcdc81be751fe3b] [formerly e36d47ed6f3faec377f6e347cdfa09fb38b6ad82 [formerly e36d47ed6f3faec377f6e347cdfa09fb38b6ad82 [formerly 648bed10a3f52f03205b6fa04c2ab1a0d9ea51af]] [formerly b9d9beeb4682efb9837667527bcdc81be751fe3b] [formerly a1c81a34cc5353a6426013a281ee53994be1d08e [formerly a1c81a34cc5353a6426013a281ee53994be1d08e [formerly 994f7d820d9f741e375501fc2353d0f34069db26]] [formerly b9d9beeb4682efb9837667527bcdc81be751fe3b [formerly b9d9beeb4682efb9837667527bcdc81be751fe3b [formerly f68b6921c08b8bccf355fa9c991c232f2ffa2076]]]]]
    Former-commit-id: a1c81a34cc5353a6426013a281ee53994be1d08e [formerly a1c81a34cc5353a6426013a281ee53994be1d08e [formerly 994f7d820d9f741e375501fc2353d0f34069db26]]
    Former-commit-id: d8001bc90f385064dec0ac2c733e64268c94992c [formerly 576d058d2371cb9862131f5967785650d49dcad7] [formerly 6f3fbbeb37886fc0869386eca1e83fe5d9e71ec5 [formerly a7b1987c9711372973b46939e69ce28056771480]] [formerly 13843336584a6f89e54cdf5777c2783d339ae7f6 [formerly 0db2127de1ec5e460e36bb326fef487253174948] [formerly 99ae550291c37c42cabbdf9cf883e79b36532d92 [formerly 558aefad6c60ac2198b287d3fbce7825f3136750]]]
    Former-commit-id: 7c07f3b2ca40ca3059587cfa1c985881b739eecc [formerly 26fa7db7e13868d680e32d00ea16ee33526b2f86] [formerly c458b9083848cc4d1157c084aed4e67e19280b0a [formerly c68dabe4109e6e9c553acf32431de6ba4e05ae9c]]
    Former-commit-id: f1e1ede424eafcc52c181992093b1fe11f5a3900 [formerly 7fb1888b5fe5e6cf8ae2402ec24bb8f890610c1d]
    Former-commit-id: 6530c7d8c7dad5ea1cfa49ae3aea56527572ec36

---
+++ edexOsgi/com.raytheon.uf.edex.plugin.text/src/com/raytheon/uf/edex/plugin/text/handler/StdTextProductRequestHandler.java
@@ -0,0 +1,133 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+
+package com.raytheon.uf.edex.plugin.text.handler;
+
+import java.util.Date;
+
+import com.raytheon.edex.textdb.alarms.AlarmAlertUtil;
+import com.raytheon.edex.textdb.dbapi.impl.TextDB;
+import com.raytheon.uf.common.dataplugin.text.db.OperationalStdTextProduct;
+import com.raytheon.uf.common.dataplugin.text.db.PracticeStdTextProduct;
+import com.raytheon.uf.common.dataplugin.text.db.StdTextProduct;
+import com.raytheon.uf.common.dataplugin.text.request.StdTextProductServerRequest;
+import com.raytheon.uf.common.serialization.comm.IRequestHandler;
+import com.raytheon.uf.common.status.IUFStatusHandler;
+import com.raytheon.uf.common.status.UFStatus;
+import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.edex.core.EDEXUtil;
+import com.raytheon.uf.edex.core.EdexException;
+
+/**
+ * Handles StdTextProductServerRequest by placing them onto the LDAD queue and
+ * sending product alarm alerts as specified by the request
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * 19May2010               cjeanbap    Initial creation
+ * 07Jul2010    2187       cjeanbap    Submit correct Afos Id.
+ * 07Jul2010    2187       cjeanbap    Submit AfosId to AlarmAlertUtil.
+ * 02Aug2010    2187       cjeanbap    Move AlarmAlertUtil.sendProductAlarmAlert() 
+ *                                     outside of if-statement.
+ * May 12, 2014 2536       bclement    removed unused import
+ * 
+ * </pre>
+ * 
+ * @author cjeanbap
+ * @version 1.0
+ */
+
+public class StdTextProductRequestHandler implements
+        IRequestHandler<StdTextProductServerRequest> {
+    private static final transient IUFStatusHandler statusHandler = UFStatus.getHandler(StdTextProductRequestHandler.class);
+
+    private static final String WATCH_WARN_QUEUE = "ldadWatchWarnDirect";
+
+    private TextDB dao;
+
+    public StdTextProductRequestHandler() {
+        dao = new TextDB();
+    }
+
+    @Override
+    public Object handleRequest(StdTextProductServerRequest request)
+            throws Exception {
+        Date d = new Date();
+        String wmoid = request.getWmoid();
+        String site = request.getSite();
+        String cccid = request.getCccid();
+        String nnnid = request.getNnnid();
+        String xxxid = request.getXxxid();
+        String hdrtime = request.getHdrtime();
+        String bbbid = request.getBbbid();
+        Long createtime = request.getCreatetime();
+        String product = request.getProduct();
+        boolean operationalFlag = request.isOpertionalFlag();
+
+        StdTextProduct text = (operationalFlag ? new OperationalStdTextProduct()
+                : new PracticeStdTextProduct());
+        text.setWmoid(wmoid);
+        text.setSite(site);
+        text.setCccid(cccid);
+        text.setNnnid(nnnid);
+        text.setXxxid(xxxid);
+        text.setHdrtime(hdrtime);
+        text.setBbbid(bbbid);
+        if (createtime == null) {
+            createtime = new Date().getTime();
+        }
+        text.setRefTime(createtime);
+        text.setProduct(product);
+        boolean success = dao.writeProduct(text);
+
+        if (success) {
+            String afosId = text.getCccid() + text.getNnnid() + text.getXxxid();
+            if (operationalFlag) {
+                sendTextToQueue(afosId, WATCH_WARN_QUEUE);
+            }
+
+            AlarmAlertUtil.sendProductAlarmAlert(afosId, new Date(createtime),
+                    operationalFlag);
+        }
+        return d;
+    }
+
+    /**
+     * 
+     * Sends an asynchronous message to the specified queue. This is basically a
+     * wrapper of the utility method that handles/logs any errors.
+     * 
+     * @param message
+     *            the message to send
+     * @param queue
+     *            the queue to receive the message
+     */
+    private void sendTextToQueue(String message, String queue) {
+        try {
+            EDEXUtil.getMessageProducer().sendAsync(queue, message);
+        } catch (EdexException e) {
+            statusHandler.handle(Priority.PROBLEM, "Unable to send product '"
+                            + message + "' to queue '" + queue + "'", e);
+        }
+    }
+}
