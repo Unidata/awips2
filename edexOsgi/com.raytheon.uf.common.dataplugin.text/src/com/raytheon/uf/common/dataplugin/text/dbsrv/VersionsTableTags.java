de232ae55cbdbe065adad957c27172704ab70fcb
Brian Clements <brian.d.clements@raytheon.com>
2014-05-13 09:16:59 -0500

    Omaha #2536 merged uf.edex.textdbsrv into text plugins
    split into uf.common.dataplugin.text and uf.edex.plugin.text
    
    
    Former-commit-id: 4d40a98ad7ce44e2230b993d53ef0eea9599f795 [formerly a06d332ff49e3f809f48891a8f45dae48f575f09] [formerly 4d40a98ad7ce44e2230b993d53ef0eea9599f795 [formerly a06d332ff49e3f809f48891a8f45dae48f575f09] [formerly 2f5cffbe158b5095f404832abf08d4f55e851274 [formerly 5f238c51d856984808dee0d2ba62b6a8fbdbd6a1]]]
    Former-commit-id: 2f5cffbe158b5095f404832abf08d4f55e851274
    Former-commit-id: 7761b741128dc8cbfb3b34bfa38544b4fb54834a [formerly 8ddae2fc54ad3dc011a5a8d15d32d79d3653ca68]
    Former-commit-id: f61d1ac9761fd451d252ee44f33c2d64465e7370

--- edexOsgi/com.raytheon.uf.common.dataplugin.text/src/com/raytheon/uf/common/dataplugin/text/dbsrv/VersionsTableTags.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.text/src/com/raytheon/uf/common/dataplugin/text/dbsrv/VersionsTableTags.java
@@ -17,40 +17,27 @@
  * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
  * further licensing information.
  **/
-package com.raytheon.uf.edex.services.textdbsrv;
+package com.raytheon.uf.common.dataplugin.text.dbsrv;
 
-import com.raytheon.uf.common.message.Message;
 
 /**
- * TODO Add Description
+ * Enumeration for textdbsrv version header fields
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Oct 7, 2008        1538 jkorman     Initial creation
- *
+ * Oct 8, 2008        1538 jkorman     Initial creation
+ * May 15, 2014 2536       bclement    moved from uf.edex.textdbsrv
+ * 
  * </pre>
- *
+ * 
  * @author jkorman
- * @version 1.0	
+ * @version 1.0
  */
 
-public interface ICommandExecutor {
-
-    
-    /**
-     * 
-     * @param cmdMessage
-     * @return
-     */
-    public Message execute(Message cmdMessage);
-    
-    /**
-     * 
-     */
-    public void dispose();
-
+public enum VersionsTableTags {
+    OP, PRODID, VERSION;
 }
