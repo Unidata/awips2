e325f8681ec747b8c6d2ab9646beb741c71391de
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-05-15 14:50:35 -0400

    VLab Issue #3720 - NCEP EDEX and EDEX Common delivery for 14.3.1
    Table updates for GRIB decoding; Updates to gpd, mcidas, pafm, and pgen
    plugins
    
    Change-Id: Ic6874210998d7f5a6b164ef45c6918246388ee68
    
    Former-commit-id: 80c8dae07cb252d5fdd8882d75361aec4fb8587d [formerly 4602f86f47fdc5725bc8e6050ea9916ae3337d2a] [formerly bd2dad11b5eee5ab72c6baf15d20c03a69a1819c] [formerly ca9b7c8dfcf3750245ee7b39a6a25951d9febf1b [formerly bd2dad11b5eee5ab72c6baf15d20c03a69a1819c [formerly 3c904361e7176592cc873a3414c18ad5107aea54]]]
    Former-commit-id: ca9b7c8dfcf3750245ee7b39a6a25951d9febf1b
    Former-commit-id: 14374ef5541f62dc41754f884fecd9cb2dab8bd0 [formerly 4e5968d7b65299d36a28b279125efcf8c84b045b]
    Former-commit-id: da02372ba6b85592847ea73ec26db2980106901a

--- ncep/gov.noaa.nws.ncep.common.dataplugin.gpd/src/gov/noaa/nws/ncep/common/dataplugin/gpd/product/GenericPointDataProductContainer.java
+++ ncep/gov.noaa.nws.ncep.common.dataplugin.gpd/src/gov/noaa/nws/ncep/common/dataplugin/gpd/product/GenericPointDataProductContainer.java
@@ -1,5 +1,9 @@
 /**
- * 
+ * This code has unlimited rights, and is provided "as is" by the National Centers 
+ * for Environmental Prediction, without warranty of any kind, either expressed or implied, 
+ * including but not limited to the implied warranties of merchantability and/or fitness 
+ * for a particular purpose.
+* 
  * 
  * This code has been developed by the NCEP-SIB for use in the AWIPS2 system.
  * 
