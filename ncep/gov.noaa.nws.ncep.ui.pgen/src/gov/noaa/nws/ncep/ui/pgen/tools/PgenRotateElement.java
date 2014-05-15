8e9bcd9ad76eeaf9e2f605184416c9e4837cd689
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-05-15 16:08:55 -0400

    VLab Issue #3721 - NCEP CAVE plugin delivery for 14.3.1
    PGEN Bug Fixes
    
    Change-Id: Ie50d8f829e0dfb00fab254945a46592553cdb59c
    
    Former-commit-id: 192bc476fbbef23de2081a65aafa34d85b0ca037 [formerly d3a1e1a58f32e5cc4e0c865ad987e92a9fe185c1]
    Former-commit-id: 282aeff63d3a884f59483ca17c5c70d0975ef736

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/tools/PgenRotateElement.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/tools/PgenRotateElement.java
@@ -8,8 +8,9 @@
 
 package gov.noaa.nws.ncep.ui.pgen.tools;
 
-import static java.lang.Math.*;
-
+import static java.lang.Math.atan2;
+import static java.lang.Math.toDegrees;
+import gov.noaa.nws.ncep.ui.pgen.PgenUtil;
 import gov.noaa.nws.ncep.ui.pgen.annotation.Operation;
 import gov.noaa.nws.ncep.ui.pgen.attrdialog.TrackExtrapPointInfoDlg;
 import gov.noaa.nws.ncep.ui.pgen.display.ISinglePoint;
@@ -34,6 +35,7 @@ import com.vividsolutions.jts.geom.Coordinate;
  * ------------	----------	-----------	--------------------------
  * 01/2010                  Mikhail L.  Initial Creation.
  * 06/2010		#280		Moved two methods to PgenToolUtils.
+ * 04/2014      TTR900      pswamy      R-click cannot return to SELECT from Rotate and DEL_OBJ
  * 
  * </pre>
  * 
@@ -42,260 +44,283 @@ import com.vividsolutions.jts.geom.Coordinate;
 
 public class PgenRotateElement extends AbstractPgenDrawingTool {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.raytheon.viz.ui.tools.AbstractTool#runTool()
-	 */
-	@Override
-	protected void activateTool() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.viz.ui.tools.AbstractTool#runTool()
+     */
+    @Override
+    protected void activateTool() {
 
-		attrDlg = null;
-		if (buttonName == null){
-			buttonName = new String("Select");
-		}
+        attrDlg = null;
+        if (buttonName == null) {
+            buttonName = new String("Select");
+        }
 
-		super.activateTool();
+        super.activateTool();
 
-	}
+    }
 
-	/**
-	 * Returns the current mouse handler.
-	 * 
-	 * @return
-	 */
-	public IInputHandler getMouseHandler() {
-		if (mouseHandler == null) {
-			mouseHandler = new PgenSelectRotateHandler();
-		}
-		return mouseHandler;
-	}
+    /**
+     * Returns the current mouse handler.
+     * 
+     * @return
+     */
+    public IInputHandler getMouseHandler() {
+        if (mouseHandler == null) {
+            mouseHandler = new PgenSelectRotateHandler();
+        }
+        return mouseHandler;
+    }
 
-	/**
-	 * Implements input handler for mouse events.
-	 * 
-	 * @author bingfan, Mikhail L.
-	 * 
-	 */
-	public class PgenSelectRotateHandler extends InputHandlerDefaultImpl {
+    /**
+     * Implements input handler for mouse events.
+     * 
+     * @author bingfan, Mikhail L.
+     * 
+     */
+    public class PgenSelectRotateHandler extends InputHandlerDefaultImpl {
 
-		OperationFilter rotateFilter = new OperationFilter( Operation.ROTATE );
-		
-		/** Attribute dialog for displaying track points info */
-		TrackExtrapPointInfoDlg trackExtrapPointInfoDlg = null;
+        OperationFilter rotateFilter = new OperationFilter(Operation.ROTATE);
 
-		/** Flag if any point of the element is selected. */
-		protected boolean ptSelected = false;
+        /** Attribute dialog for displaying track points info */
+        TrackExtrapPointInfoDlg trackExtrapPointInfoDlg = null;
 
-		/** The original direction is needed for undo. */
-		private Double oldDir = null; // using Double instead of double because we need to use null value
+        /** Flag if any point of the element is selected. */
+        protected boolean ptSelected = false;
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseDown(int,
-		 * int, int)
-		 */
-		@Override
-		public boolean handleMouseDown(int x, int y, int button) {
-        	if ( !isResourceEditable() ) return false;
+        /** The original direction is needed for undo. */
+        private Double oldDir = null; // using Double instead of double because
+                                      // we need to use null value
 
-			// Check if mouse is in geographic extent
-			Coordinate loc = mapEditor.translateClick(x, y);
-			if (loc == null || shiftDown ) {
-				return false;
-			}
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseDown(int,
+         * int, int)
+         */
+        @Override
+        public boolean handleMouseDown(int x, int y, int button) {
+            if (!isResourceEditable())
+                return false;
 
-			if (button == 1) {
+            // Check if mouse is in geographic extent
+            Coordinate loc = mapEditor.translateClick(x, y);
+            if (loc == null || shiftDown) {
+                return false;
+            }
 
-				// Return if an element or a point has been selected
-				if (ptSelected || drawingLayer.getSelectedDE() != null) {
-					return true;
-				}
+            if (button == 1) {
 
-				// Get the nearest element and set it as the selected element.
-				DrawableElement elSelected = drawingLayer.getNearestElement(loc, rotateFilter);
+                // Return if an element or a point has been selected
+                if (ptSelected || drawingLayer.getSelectedDE() != null) {
+                    return true;
+                }
 
-				/*
-				if (elSelected instanceof TCAElement) {
-					PgenUtil.loadTCATool(elSelected);
-				} else if (elSelected instanceof WatchBox) {
-					PgenUtil.loadWatchBoxModifyTool(elSelected);
-				}
-				*/
+                // Get the nearest element and set it as the selected element.
+                DrawableElement elSelected = drawingLayer.getNearestElement(
+                        loc, rotateFilter);
 
-				if (elSelected != null) drawingLayer.setSelected(elSelected);
+                /*
+                 * if (elSelected instanceof TCAElement) {
+                 * PgenUtil.loadTCATool(elSelected); } else if (elSelected
+                 * instanceof WatchBox) {
+                 * PgenUtil.loadWatchBoxModifyTool(elSelected); }
+                 */
 
-				mapEditor.refresh();
-				return false;
+                if (elSelected != null)
+                    drawingLayer.setSelected(elSelected);
 
-			} else if (button == 3) {
+                mapEditor.refresh();
+                return false;
 
-				if (trackExtrapPointInfoDlg != null){
-					trackExtrapPointInfoDlg.close();
-					trackExtrapPointInfoDlg = null;
-				}
+            } else if (button == 3) {
 
-				drawingLayer.removeGhostLine();
-				ptSelected = false;
-				drawingLayer.removeSelected();
-				mapEditor.refresh();
+                if (trackExtrapPointInfoDlg != null) {
+                    trackExtrapPointInfoDlg.close();
+                    trackExtrapPointInfoDlg = null;
+                }
 
-				return false;
-			} 
+                if (drawingLayer.getSelectedDE() != null) {
+                    drawingLayer.removeGhostLine();
+                    ptSelected = false;
+                    drawingLayer.removeSelected();
+                    mapEditor.refresh();
 
-			return false;
-		}
+                } else {
+                    // set selecting mode
+                    PgenUtil.setSelectingMode();
+                }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseDownMove(int,
-		 * int, int)
-		 */
-		@Override
-		public boolean handleMouseDownMove(int x, int y, int button) {
-        	if ( !isResourceEditable() ) return false;
+                return true;
+            }
 
-			// Check if mouse is in geographic extent
-			Coordinate loc = mapEditor.translateClick(x, y);
-			if (loc == null || shiftDown  ) return false;
+            return false;
+        }
 
-			DrawableElement el = drawingLayer.getSelectedDE();
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseDownMove(int,
+         * int, int)
+         */
+        @Override
+        public boolean handleMouseDownMove(int x, int y, int button) {
+            if (!isResourceEditable())
+                return false;
 
-			// The requirements are to rotate Vector and Text elements only
-			// Do not modify other elements
-			if (el != null) {
-				Coordinate origin = ((ISinglePoint)el).getLocation();
+            // Check if mouse is in geographic extent
+            Coordinate loc = mapEditor.translateClick(x, y);
+            if (loc == null || shiftDown)
+                return false;
 
-				if (el instanceof Vector) {
-					if (oldDir == null) {
-						oldDir = ((Vector) el).getDirection(); // autoboxing to Double
-					}
-					double[] swtCoordinates = mapEditor.translateInverseClick(origin);
-					Double newDir = PgenToolUtils.calculateAngle(oldDir, swtCoordinates[0], swtCoordinates[1], x, y);
-					if("Hash".equals(((Vector) el).getPgenType())){
-						newDir = PgenToolUtils.transformToRange0To360(180 - newDir);
-						newDir -= southOffsetAngle(origin);
-					} else {
-						// offset for the location point
-						newDir += southOffsetAngle(origin);
-					}
-					((Vector) el).setDirection(newDir);
+            DrawableElement el = drawingLayer.getSelectedDE();
 
-				} else if (el instanceof Text) {
-					if (oldDir == null) {
-						oldDir = ((Text) el).getRotation(); // autoboxing to Double
-					}
-					double[] swtCoordinates = mapEditor.translateInverseClick(origin);
-					Double newRotation = 180 - PgenToolUtils.calculateAngle(oldDir, swtCoordinates[0], swtCoordinates[1], x, y);
-					newRotation = PgenToolUtils.transformToRange0To360(newRotation);
-					if(((Text) el).getRotationRelativity() == TextRotation.NORTH_RELATIVE){
-						// offset for the location point
-						newRotation -= southOffsetAngle(origin);
-					}
-					((Text) el).setRotation(newRotation);
-				}
-				
-				drawingLayer.resetElement(el); // reset display of this element
-				mapEditor.refresh();
-			}
-			
-			return true;
-		}
+            // The requirements are to rotate Vector and Text elements only
+            // Do not modify other elements
+            if (el != null) {
+                Coordinate origin = ((ISinglePoint) el).getLocation();
 
-		/**
-		 * Calculates the angle difference of "south" relative to the screen's y-axis 
-		 * at a given lat/lon location.
-		 * @param loc - The point location in Lat/Lon coordinates
-		 * @return The angle difference of "north" versus pixel coordinate's y-axis
-		 */
-		private double southOffsetAngle( Coordinate loc ) {
-			
-			double delta = 0.05;
-			/*
-			 * copy/paste from DisplayElementFactory
-			 * 
-			 * Calculate points in pixel coordinates just south and north of
-			 * original location.  
-			 */
-	        double[] south = { loc.x, loc.y - delta, 0.0 };
-	        double[] pt1 = drawingLayer.getDescriptor().worldToPixel(south);
+                if (el instanceof Vector) {
+                    if (oldDir == null) {
+                        oldDir = ((Vector) el).getDirection(); // autoboxing to
+                                                               // Double
+                    }
+                    double[] swtCoordinates = mapEditor
+                            .translateInverseClick(origin);
+                    Double newDir = PgenToolUtils.calculateAngle(oldDir,
+                            swtCoordinates[0], swtCoordinates[1], x, y);
+                    if ("Hash".equals(((Vector) el).getPgenType())) {
+                        newDir = PgenToolUtils
+                                .transformToRange0To360(180 - newDir);
+                        newDir -= southOffsetAngle(origin);
+                    } else {
+                        // offset for the location point
+                        newDir += southOffsetAngle(origin);
+                    }
+                    ((Vector) el).setDirection(newDir);
 
-	        double[] north = { loc.x, loc.y + delta, 0.0 };
-	        double[] pt2 = drawingLayer.getDescriptor().worldToPixel(north);
-	        
-			return -90 - toDegrees(atan2( (pt2[1]-pt1[1]), (pt2[0]-pt1[0]) ));
-		}
-		
+                } else if (el instanceof Text) {
+                    if (oldDir == null) {
+                        oldDir = ((Text) el).getRotation(); // autoboxing to
+                                                            // Double
+                    }
+                    double[] swtCoordinates = mapEditor
+                            .translateInverseClick(origin);
+                    Double newRotation = 180 - PgenToolUtils.calculateAngle(
+                            oldDir, swtCoordinates[0], swtCoordinates[1], x, y);
+                    newRotation = PgenToolUtils
+                            .transformToRange0To360(newRotation);
+                    if (((Text) el).getRotationRelativity() == TextRotation.NORTH_RELATIVE) {
+                        // offset for the location point
+                        newRotation -= southOffsetAngle(origin);
+                    }
+                    ((Text) el).setRotation(newRotation);
+                }
 
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseUp(int, int,
-		 * int)
-		 */
-		@Override
-		public boolean handleMouseUp(int x, int y, int button) {
-        	if ( !isResourceEditable() ) return false;
+                drawingLayer.resetElement(el); // reset display of this element
+                mapEditor.refresh();
+            }
 
-			// Finish the editing
-			if (button == 1 && drawingLayer != null) {
+            return true;
+        }
 
-				// Create a copy of the currently selected element
-				DrawableElement el = drawingLayer.getSelectedDE();
+        /**
+         * Calculates the angle difference of "south" relative to the screen's
+         * y-axis at a given lat/lon location.
+         * 
+         * @param loc
+         *            - The point location in Lat/Lon coordinates
+         * @return The angle difference of "north" versus pixel coordinate's
+         *         y-axis
+         */
+        private double southOffsetAngle(Coordinate loc) {
 
-				if (el != null && oldDir != null) {
+            double delta = 0.05;
+            /*
+             * copy/paste from DisplayElementFactory
+             * 
+             * Calculate points in pixel coordinates just south and north of
+             * original location.
+             */
+            double[] south = { loc.x, loc.y - delta, 0.0 };
+            double[] pt1 = drawingLayer.getDescriptor().worldToPixel(south);
 
-					DrawableElement newEl = (DrawableElement) el.copy();
+            double[] north = { loc.x, loc.y + delta, 0.0 };
+            double[] pt2 = drawingLayer.getDescriptor().worldToPixel(north);
 
-					drawingLayer.resetElement(el);
-					
-					if (el instanceof Vector) {
-						((Vector) el).setDirection(oldDir);
-						oldDir = null;
-					} else if (el instanceof Text) {
-						((Text) el).setRotation(oldDir);
-						oldDir = null;
-					}
+            return -90 - toDegrees(atan2((pt2[1] - pt1[1]), (pt2[0] - pt1[0])));
+        }
 
-					drawingLayer.replaceElement(el, newEl);
-					drawingLayer.setSelected(newEl);
+        /*
+         * (non-Javadoc)
+         * 
+         * @see com.raytheon.viz.ui.input.IInputHandler#handleMouseUp(int, int,
+         * int)
+         */
+        @Override
+        public boolean handleMouseUp(int x, int y, int button) {
+            if (!isResourceEditable())
+                return false;
 
-					mapEditor.refresh();
-				}
-			}
-			
-			return false;
-		}
+            // Finish the editing
+            if (button == 1 && drawingLayer != null) {
 
-		/**
-		 * Gets the nearest point of an selected element to the input point
-		 * 
-		 * @param el
-		 *            element
-		 * @param pt
-		 *            input point
-		 * @return
-		 */
-		protected int getNearestPtIndex(MultiPointElement el, Coordinate pt) {
+                // Create a copy of the currently selected element
+                DrawableElement el = drawingLayer.getSelectedDE();
 
-			int ptId = 0;
-			double minDistance = -1;
-			GeodeticCalculator gc;
-			gc = new GeodeticCalculator(drawingLayer.getCoordinateReferenceSystem());
-			gc.setStartingGeographicPoint(pt.x, pt.y);
-			int index = 0;
-			for (Coordinate elPoint : el.getPoints()) {
-				gc.setDestinationGeographicPoint(elPoint.x, elPoint.y);
-				double dist = gc.getOrthodromicDistance();
-				if (minDistance < 0 || dist < minDistance) {
-					minDistance = dist;
-					ptId = index;
-				}
-				index++;
-			}
-			return ptId;
-		}
-	}
+                if (el != null && oldDir != null) {
+
+                    DrawableElement newEl = (DrawableElement) el.copy();
+
+                    drawingLayer.resetElement(el);
+
+                    if (el instanceof Vector) {
+                        ((Vector) el).setDirection(oldDir);
+                        oldDir = null;
+                    } else if (el instanceof Text) {
+                        ((Text) el).setRotation(oldDir);
+                        oldDir = null;
+                    }
+
+                    drawingLayer.replaceElement(el, newEl);
+                    drawingLayer.setSelected(newEl);
+
+                    mapEditor.refresh();
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * Gets the nearest point of an selected element to the input point
+         * 
+         * @param el
+         *            element
+         * @param pt
+         *            input point
+         * @return
+         */
+        protected int getNearestPtIndex(MultiPointElement el, Coordinate pt) {
+
+            int ptId = 0;
+            double minDistance = -1;
+            GeodeticCalculator gc;
+            gc = new GeodeticCalculator(
+                    drawingLayer.getCoordinateReferenceSystem());
+            gc.setStartingGeographicPoint(pt.x, pt.y);
+            int index = 0;
+            for (Coordinate elPoint : el.getPoints()) {
+                gc.setDestinationGeographicPoint(elPoint.x, elPoint.y);
+                double dist = gc.getOrthodromicDistance();
+                if (minDistance < 0 || dist < minDistance) {
+                    minDistance = dist;
+                    ptId = index;
+                }
+                index++;
+            }
+            return ptId;
+        }
+    }
 }
