1836055746923ddf6a5b03da582fb92375bcbc73
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-05-20 12:29:30 -0500

    Omaha #2913 Remove dead code using JAI
    Former-commit-id: 696134a1bc6924fcbea85aed566a8e6d8f1238bf [formerly 696134a1bc6924fcbea85aed566a8e6d8f1238bf [formerly 59a108822e5e9a220ce724709e36272ef9f2380e]] [formerly d5d3f5605a0e227f630e2bd155a021e643156034] [formerly 696134a1bc6924fcbea85aed566a8e6d8f1238bf [formerly 696134a1bc6924fcbea85aed566a8e6d8f1238bf [formerly 59a108822e5e9a220ce724709e36272ef9f2380e]] [formerly d5d3f5605a0e227f630e2bd155a021e643156034] [formerly 6613c9fc422706a409b19f9304fe70dcc2f86791 [formerly 6613c9fc422706a409b19f9304fe70dcc2f86791 [formerly d48333444cf46dea3e899164576ace4ad4d115e1]] [formerly d5d3f5605a0e227f630e2bd155a021e643156034 [formerly d5d3f5605a0e227f630e2bd155a021e643156034 [formerly 767fc083618827ad06beff9f6728ea9dd42528f6]]]]]
    Former-commit-id: 6613c9fc422706a409b19f9304fe70dcc2f86791 [formerly 6613c9fc422706a409b19f9304fe70dcc2f86791 [formerly d48333444cf46dea3e899164576ace4ad4d115e1]]
    Former-commit-id: 60b2b5574a7159c48b1a7eaede00a037aa665a38 [formerly e0a628fd2b36a41a6b8dca8b119e47b708161733] [formerly ddd3f8eca1cb55c2f3fe3a58654d5a0ad77fe415 [formerly a56f4750761936c8af4b71c423a6fd4854b13b8e]] [formerly 4e0ff9a3d5765e25ccff4095da1954a5a5dd3625 [formerly 7072109a0818ecd841545b0d3b0d39f4d5b18b53] [formerly 532e51130733cd6beaeb97d7b1566fcde7fd5fdb [formerly 98ad1b079ebc8c189eddb1339f07f580c94fda9c]]]
    Former-commit-id: d6c1bee7f861bb43a1fa8f4c5ff9028eafed80c7 [formerly 06554396666a51b4f1df4175c9d431e53dad636e] [formerly 013acd2e8734f74bd34e1fbee4c8c81bb93cd79c [formerly 4b940cb5993c3ceae70a51532b635039981edbdc]]
    Former-commit-id: a48c4fc231bd27c99a5d811b085aea2dc099193f [formerly 58e57046510c8c1fe04492d6b4cfcd67e5e071d9]
    Former-commit-id: fe5a780d5a59734abaf65455804cb36378f3c5b6

--- edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/RemapGrid.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/RemapGrid.java
@@ -20,24 +20,12 @@
 
 package com.raytheon.uf.common.dataplugin.gfe;
 
-import java.awt.RenderingHints;
-import java.awt.image.Raster;
-import java.awt.image.RenderedImage;
 import java.lang.ref.SoftReference;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 
-import javax.media.jai.BorderExtender;
-import javax.media.jai.Interpolation;
-import javax.media.jai.JAI;
-import javax.media.jai.ParameterBlockJAI;
-import javax.media.jai.PlanarImage;
-
-import org.geotools.coverage.grid.GridCoverage2D;
-import org.geotools.coverage.grid.GridCoverageFactory;
 import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.coverage.grid.GridGeometry2D;
-import org.opengis.geometry.Envelope;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
 
@@ -78,6 +66,8 @@ import com.vividsolutions.jts.geom.Coordinate;
  * 07/17/13     #2185      bsteffen    Cache computed grid reprojections.
  * 08/13/13     #1571      randerso    Passed fill values into interpolator.
  * 03/07/14     #2791      bsteffen    Move Data Source/Destination to numeric plugin.
+ * 05/19/14     #2913      bsteffen    Remove rescale option.
+ * 
  * 
  * </pre>
  * 
@@ -91,8 +81,6 @@ public class RemapGrid {
     /** The output grid location describing the destination data */
     private GridLocation destinationGloc;
 
-    private boolean rescale = false;
-
     private SoftReference<Grid2DFloat> rotationRef;
 
     /**
@@ -104,24 +92,8 @@ public class RemapGrid {
      *            The destination grid location describing the destination data
      */
     public RemapGrid(GridLocation sourceGloc, GridLocation destinationGloc) {
-        this(sourceGloc, destinationGloc, false);
-    }
-
-    /**
-     * Constructs a new RemapGrid with the given input and output grid locations
-     * 
-     * @param sourceGloc
-     *            The source grid location describing the source data
-     * @param destinationGloc
-     *            The destination grid location describing the destination data
-     * @param rescale
-     *            true if data is to be rescaled
-     */
-    public RemapGrid(GridLocation sourceGloc, GridLocation destinationGloc,
-            boolean rescale) {
         this.sourceGloc = sourceGloc;
         this.destinationGloc = destinationGloc;
-        this.rescale = rescale;
         this.rotationRef = new SoftReference<Grid2DFloat>(null);
     }
 
@@ -549,67 +521,25 @@ public class RemapGrid {
         GridGeometry2D sourceGeometry = MapUtil.getGridGeometry(sourceGloc);
 
         float[] data = input.getFloats();
-        float[] f1 = null;
 
-        /*
-         * Checks the CRSs to see if they are the same. If they are, then
-         * reprojection is not necessary, only resampling
-         */
-        if (rescale
-                && sourceGeometry.getCoordinateReferenceSystem().toWKT()
-                        .equals(destinationGloc.getCrsWKT())) {
+        GridGeometry2D destGeometry = MapUtil.getGridGeometry(destinationGloc);
 
-            GridCoverageFactory factory = new GridCoverageFactory();
+        GridReprojection interp = PrecomputedGridReprojection.getReprojection(
+                sourceGeometry, destGeometry);
 
-            Envelope inputEnvelope = sourceGeometry.getEnvelope();
+        DataSource source = new GeographicDataSource(FloatBuffer.wrap(data),
+                sourceGeometry);
+        source = FillValueFilter.apply(source, inputFill);
+        ;
 
-            // Map the data into an array
-            float[][] dataPoints = new float[input.getYdim()][input.getXdim()];
+        FloatBufferWrapper rawDest = new FloatBufferWrapper(
+                destGeometry.getGridRange2D());
+        DataDestination dest = InverseFillValueFilter.apply(
+                (DataDestination) rawDest, outputFill);
 
-            int i = 0;
-            for (int y = 0; y < input.getYdim(); y++) {
-                for (int x = 0; x < input.getXdim(); x++) {
-                    dataPoints[y][x] = data[i++];
-                }
-            }
+        interp.reprojectedGrid(new BilinearInterpolation(), source, dest);
 
-            // Construct the source grid coverage object
-            GridCoverage2D inputGC = factory.create("in", dataPoints,
-                    inputEnvelope);
-
-            float scaleX = ((float) destinationGloc.getNx() / (float) sourceGloc
-                    .getNx());
-            float scaleY = ((float) destinationGloc.getNy() / (float) sourceGloc
-                    .getNy());
-            PlanarImage image = scaleGrid(inputGC.getRenderedImage(), scaleX,
-                    scaleY);
-            Raster rasterData = image.getData();
-            f1 = rasterData.getPixels(rasterData.getMinX(),
-                    rasterData.getMinY(), rasterData.getWidth(),
-                    rasterData.getHeight(), f1);
-
-            JAI.getDefaultInstance().getTileCache().flush();
-        } else {
-            GridGeometry2D destGeometry = MapUtil
-                    .getGridGeometry(destinationGloc);
-
-            GridReprojection interp = PrecomputedGridReprojection
-                    .getReprojection(sourceGeometry, destGeometry);
-
-            DataSource source = new GeographicDataSource(
-                    FloatBuffer.wrap(data), sourceGeometry);
-            source = FillValueFilter.apply(source, inputFill);
-            ;
-
-            FloatBufferWrapper rawDest = new FloatBufferWrapper(
-                    destGeometry.getGridRange2D());
-            DataDestination dest = InverseFillValueFilter.apply(
-                    (DataDestination) rawDest, outputFill);
-
-            interp.reprojectedGrid(new BilinearInterpolation(), source, dest);
-
-            f1 = rawDest.getArray();
-        }
+        float[] f1 = rawDest.getArray();
 
         // Remap the the output data into a Grid2DFloat object
 
@@ -619,37 +549,6 @@ public class RemapGrid {
         return retVal;
     }
 
-    /**
-     * Resamples the grid to a new resolution using scale values
-     * 
-     * @param img
-     *            The image to scale
-     * @param xScale
-     *            The new horizontal resolution
-     * @param yScale
-     *            The new vertical resolution
-     * @return The resampled image
-     */
-    private PlanarImage scaleGrid(RenderedImage img, float xScale, float yScale) {
-
-        PlanarImage scaledImg;
-
-        ParameterBlockJAI param = new ParameterBlockJAI("Scale");
-        param.addSource(img);
-        param.setParameter("xScale", xScale);
-        param.setParameter("yScale", yScale);
-        Interpolation interpol = Interpolation
-                .getInstance(Interpolation.INTERP_BICUBIC);
-        RenderingHints hint = new RenderingHints(JAI.KEY_BORDER_EXTENDER,
-                BorderExtender.createInstance(BorderExtender.BORDER_COPY));
-
-        param.setParameter("interpolation", interpol);
-
-        scaledImg = JAI.create("Scale", param, hint);
-
-        return scaledImg;
-
-    }
 
     private float getRot(int x, int y) {
         Grid2DFloat rotation;
