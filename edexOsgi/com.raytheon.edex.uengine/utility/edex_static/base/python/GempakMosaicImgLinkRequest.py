c134e4ebaaa3d4e256ab7a37762444c23aa95494
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-05-21 14:38:28 -0500

    Omaha #2913 Remove ability to create images from uengine.
    Former-commit-id: b7d15aac2252cd2f1752097599c23922bb19c5e9 [formerly f843fb4cdd78f4f847f0a2d45fe78106af88b3b4]
    Former-commit-id: ab739befec0f13a9966d1face2b69f3f838e1926

--- edexOsgi/com.raytheon.edex.uengine/utility/edex_static/base/python/GempakMosaicImgLinkRequest.py
+++ edexOsgi/com.raytheon.edex.uengine/utility/edex_static/base/python/GempakMosaicImgLinkRequest.py
@@ -18,6 +18,7 @@
 #    Date            Ticket#       Engineer             Description
 #    ------------    ----------    -----------          --------------------------
 #    12/22/09        173_partB     mgamazaychikov       Initial Creation
+#    05/20/14        2913          bsteffen             Remove unused imports
 #
 
 import BaseRequest
@@ -76,7 +77,6 @@ class GempakMosaicImgLinkRequest(BaseRequest.BaseRequest):
         from com.raytheon.edex.uengine.tasks.decode import FileIn
         from com.raytheon.edex.uengine.tasks.output import FileOut
         from gov.noaa.nws.ncep.edex.plugin.mosaic.uengine import DecodeMosaicImage
-        from com.raytheon.edex.uengine.tasks.process import ColorMapImage, ReprojectImage, ImageOut       
         response = ArrayList()
         size = self.queryResults.size() 
         for i in range(size):
