fc7a5c0294efc64a5a627a03b31b7a62aad709ba
Ron Anderson <ron.anderson@raytheon.com>
2014-06-02 17:14:18 -0500

    Omaha #3052 Improved TopoQuery error handling to throw exceptions with useful error messages.
    Change-Id: Ic181682611801ea244b986e8a2ce1400c4dc15d1
    
    Former-commit-id: f9a3515b6751376173632f24635314f3a194003a [formerly 49d4eba710b00545d404b1f4b3f84d8289984282] [formerly bd50ae9854aef5f603dd7e81b5423d1cbc6228be] [formerly bd50ae9854aef5f603dd7e81b5423d1cbc6228be [formerly 3f169399b8aeb168130ad50e80bb3cb1bc9c9b29]] [formerly 304364e3569f9030d04abd3489ca07707d5bd8ed [formerly bd50ae9854aef5f603dd7e81b5423d1cbc6228be [formerly 3f169399b8aeb168130ad50e80bb3cb1bc9c9b29] [formerly 304364e3569f9030d04abd3489ca07707d5bd8ed [formerly 0d42d4fdc56122c3df912c93e5e09b0ab28c1b37]]]]
    Former-commit-id: 304364e3569f9030d04abd3489ca07707d5bd8ed
    Former-commit-id: 51b9db2f7f089a6085ac7f98e0cac9736b5b10a8 [formerly bba329f4d5727f79bd95b57a11fd41ca3994a509] [formerly 060b0221fa3ab57b460953bafb800764ce8dbfc1 [formerly 3ff441f9da64d4fcb430070d6791e220bf2cfc32]]
    Former-commit-id: 060b0221fa3ab57b460953bafb800764ce8dbfc1
    Former-commit-id: 9b0c3f23552c5dfbd2a5dff2791b013ceafce649

--- cave/com.raytheon.viz.grid/src/com/raytheon/viz/grid/data/TopoRequestableData.java
+++ cave/com.raytheon.viz.grid/src/com/raytheon/viz/grid/data/TopoRequestableData.java
@@ -27,15 +27,16 @@ import javax.measure.unit.SI;
 import org.geotools.coverage.grid.GridGeometry2D;
 
 import com.raytheon.uf.common.comm.CommunicationException;
-import com.raytheon.uf.common.inventory.data.AbstractRequestableData;
-import com.raytheon.uf.common.inventory.exception.DataCubeException;
 import com.raytheon.uf.common.dataplugin.level.LevelFactory;
 import com.raytheon.uf.common.datastorage.Request;
 import com.raytheon.uf.common.datastorage.records.FloatDataRecord;
 import com.raytheon.uf.common.gridcoverage.GridCoverage;
+import com.raytheon.uf.common.inventory.data.AbstractRequestableData;
+import com.raytheon.uf.common.inventory.exception.DataCubeException;
 import com.raytheon.uf.common.status.IUFStatusHandler;
 import com.raytheon.uf.common.status.UFStatus;
 import com.raytheon.uf.common.status.UFStatus.Priority;
+import com.raytheon.uf.common.topo.TopoException;
 import com.raytheon.uf.common.topo.TopoQuery;
 import com.raytheon.viz.grid.util.SliceUtil;
 
@@ -90,12 +91,13 @@ public class TopoRequestableData extends AbstractRequestableData {
         if (rval == null) {
             // retrieve topo data and assign record to FloatDataRecord
             GridGeometry2D gridGeom = coverage.getGridGeometry();
-            float[] heights = TopoQuery.getInstance().getHeight(gridGeom);
-
-            if (heights != null) {
+            try {
+                float[] heights = TopoQuery.getInstance().getHeight(gridGeom);
                 rval = new FloatDataRecord(null, null, heights, 2, new long[] {
                         coverage.getNx(), coverage.getNy() });
                 topoCache.put(coverage, rval);
+            } catch (TopoException e) {
+                throw new DataCubeException("Unable to retrieve topo data", e);
             }
         }
         if (arg instanceof Request) {
