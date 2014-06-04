03395c2da7b3de4025431711871d2331180f9d15
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-06-04 16:34:27 -0500

    Omaha #3232 Radar code improvements.
    Former-commit-id: 562e2e764b599c6986572eaba64964937cfb1278 [formerly 26325b57bec4a76321b26b43f2eca135e715395d] [formerly 8587b9b43b5d734bbc6b9fdc4f027018db638a2f [formerly 696f45d67c5e5b02183f97cfcc139db017f01848]]
    Former-commit-id: 8587b9b43b5d734bbc6b9fdc4f027018db638a2f
    Former-commit-id: 3eb6b79c5678cc48212230ee8ba6a34176f06284

--- edexOsgi/com.raytheon.uf.common.dataplugin.radar/src/com/raytheon/uf/common/dataplugin/radar/level3/SpecialGraphicSymbolPacket.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.radar/src/com/raytheon/uf/common/dataplugin/radar/level3/SpecialGraphicSymbolPacket.java
@@ -24,27 +24,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.raytheon.uf.common.serialization.ISerializableObject;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
 /**
- * TODO Add Description
+ * A packet for holding special graphics symbols.
  * 
  * <pre>
  * SOFTWARE HISTORY
- * Date         Ticket#    Engineer    Description
- * ------------ ---------- ----------- --------------------------
- * Nov 5, 2008            mnash     Initial creation
- * 07/29/2013   2148       mnash       Refactor registering of packets to Spring
+ * Date          Ticket#  Engineer    Description
+ * ------------- -------- ----------- --------------------------
+ * Nov 05, 2008           mnash       Initial creation
+ * Jul 29, 2013  2148     mnash       Refactor registering of packets to Spring
+ * Jun 04, 2014  3232     bsteffen    Remove ISerializableObject
+ * 
  * </pre>
  * 
  * @author mnash
  * @version 1.0
  */
 @DynamicSerialize
-public class SpecialGraphicSymbolPacket extends SymbologyPacket implements
-        ISerializableObject {
+public class SpecialGraphicSymbolPacket extends SymbologyPacket {
 
     /**
      * @param packetId
@@ -61,8 +61,7 @@ public class SpecialGraphicSymbolPacket extends SymbologyPacket implements
     }
 
     @DynamicSerialize
-    public static class SpecialGraphicPoint implements SymbologyPoint,
-            ISerializableObject {
+    public static class SpecialGraphicPoint implements SymbologyPoint {
         @DynamicSerializeElement
         public int i;
 
