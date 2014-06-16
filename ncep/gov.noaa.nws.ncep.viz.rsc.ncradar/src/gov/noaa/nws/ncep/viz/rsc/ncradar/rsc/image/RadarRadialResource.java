a0c7a7477787fea9fe509e7120fe8e403269b6b2
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-06-16 10:55:48 -0500

    Omaha #2061 Update ncep copies of radar resources.
    Former-commit-id: 346a20d6c7f8cf2943d37ffdcd27373b4bb014ad [formerly 346a20d6c7f8cf2943d37ffdcd27373b4bb014ad [formerly 8d521a40d0cbd373abce58e082a05494515769d0]] [formerly 14b5c0d792514da8c2f1806d8f97f8c3f20845ab] [formerly cc07b687e3b679f0754811db30273230b6b6bdc5 [formerly cc07b687e3b679f0754811db30273230b6b6bdc5 [formerly 8064a74866df91f8f6e31ad74d051c0ba0a0969b]] [formerly 14b5c0d792514da8c2f1806d8f97f8c3f20845ab [formerly 14b5c0d792514da8c2f1806d8f97f8c3f20845ab [formerly 64b5e484c0dd8409ab0b791763c31681c94a38d3]]]]
    Former-commit-id: cc07b687e3b679f0754811db30273230b6b6bdc5 [formerly cc07b687e3b679f0754811db30273230b6b6bdc5 [formerly 8064a74866df91f8f6e31ad74d051c0ba0a0969b]]
    Former-commit-id: 71dbb85f29a92dfaeacdfcb97dd28c71cd2c10f3 [formerly 0db6a441719d3369d5bb238126febbbb5c026ed6] [formerly fff0d3f6f52de9719527cc2ef413634b056ae2a3 [formerly 50a1fd7cf97fcec39aa2a5ef826a031cb83bef87]]
    Former-commit-id: a49dc37dc8961c64c6feb71e8005c9c480ca924d [formerly 7fc201d3e8ca41a817cb83f2dfde006311aad479]
    Former-commit-id: 6a0f9789751b93a554b6ccd6645dfadbddeaf254

--- ncep/gov.noaa.nws.ncep.viz.rsc.ncradar/src/gov/noaa/nws/ncep/viz/rsc/ncradar/rsc/image/RadarRadialResource.java
+++ ncep/gov.noaa.nws.ncep.viz.rsc.ncradar/src/gov/noaa/nws/ncep/viz/rsc/ncradar/rsc/image/RadarRadialResource.java
@@ -7,42 +7,30 @@
  */
 package gov.noaa.nws.ncep.viz.rsc.ncradar.rsc.image;
 
-import java.awt.Rectangle;
-import java.util.HashMap;
-import java.util.Map;
+import gov.noaa.nws.ncep.viz.rsc.ncradar.rsc.RadarImageResource;
+import gov.noaa.nws.ncep.viz.rsc.ncradar.rsc.RadarResourceData;
 
+import java.awt.Rectangle;
+
+import javax.measure.Measure;
+import javax.measure.quantity.Length;
 import javax.measure.unit.NonSI;
 
 import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.raytheon.uf.common.colormap.prefs.ColorMapParameters;
-import com.raytheon.uf.common.dataplugin.IDecoderGettable.Amount;
 import com.raytheon.uf.common.dataplugin.radar.RadarRecord;
-import com.raytheon.uf.common.dataquery.requests.RequestConstraint;
-import com.raytheon.uf.common.time.DataTime;
 import com.raytheon.uf.viz.core.DrawableImage;
 import com.raytheon.uf.viz.core.IGraphicsTarget;
 import com.raytheon.uf.viz.core.IMesh;
-import com.raytheon.uf.viz.core.catalog.LayerProperty;
-import com.raytheon.uf.viz.core.catalog.ScriptCreator;
-import com.raytheon.uf.viz.core.comm.Connector;
 import com.raytheon.uf.viz.core.drawables.IImage;
 import com.raytheon.uf.viz.core.drawables.ext.colormap.IColormappedImageExtension;
 import com.raytheon.uf.viz.core.exception.VizException;
 import com.raytheon.uf.viz.core.map.IMapDescriptor;
 import com.raytheon.uf.viz.core.map.MapDescriptor;
 import com.raytheon.uf.viz.core.rsc.LoadProperties;
-import com.raytheon.uf.viz.core.rsc.ResourceType;
-import com.raytheon.viz.awipstools.capabilities.EAVCapability;
-import com.raytheon.viz.awipstools.common.EstimatedActualVelocity;
-
-import com.raytheon.viz.radar.DefaultVizRadarRecord;
 import com.raytheon.viz.radar.VizRadarRecord;
-import gov.noaa.nws.ncep.viz.rsc.ncradar.rsc.RadarImageResource;
-import gov.noaa.nws.ncep.viz.rsc.ncradar.rsc.RadarResourceData;
 import com.raytheon.viz.radar.rsc.image.IRadialMeshExtension;
-
-import com.vividsolutions.jts.geom.Coordinate;
 
 /**
  * TODO Add Description
@@ -59,6 +47,7 @@ import com.vividsolutions.jts.geom.Coordinate;
  * 03/30/2012   #651       S. Gurung   Removed method resourceChanged
  * 09-04-2012              B. Hebbard  Add getGridGeometry() to descriptor per OB12.9.1 RTS
  *                                     change IRadialMeshExtension.constructMesh 2nd param
+ * 06/16/2014   #2061      bsteffen    update IRangeableResource
  * 
  * </pre>
  * 
@@ -85,11 +74,11 @@ public class RadarRadialResource extends RadarImageResource<MapDescriptor> {
      * @see com.raytheon.viz.radar.rsc.AbstractRadarResource#getElevation()
      */
     @Override
-    public Amount getElevation() {
+    public Measure<?, Length> getElevation() {
         RadarRecord radarRecord = getCurrentRadarRecord();
 
         if (radarRecord != null) {
-            return new Amount(radarRecord.getElevation(), NonSI.FOOT);
+            return Measure.valueOf(radarRecord.getElevation(), NonSI.FOOT);
         }
         return super.getElevation();
     }
