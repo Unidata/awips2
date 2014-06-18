c220a11003987d34f7ef238b1ab4910ff71249ba
Brian.Dyke <Brian.Dyke@noaa.gov>
2014-06-18 08:55:49 -0400

    CM-MERGE:OB13.5.6-4 into 14.1.3
    Former-commit-id: 48fb810c83f0783f8bc553eca8397b83cc086a6b [formerly e3dc97e246ffff834fa0245f51253511f577a9a9] [formerly be589ceb9b5737a8ea38ed733f4c6c004c07c27b] [formerly be589ceb9b5737a8ea38ed733f4c6c004c07c27b [formerly 399c06820a4a544250dd35971b5d8e0c0dcc8a53]] [formerly 8ecdd9a192529b783e4aa81f2d171fd0156a0077 [formerly be589ceb9b5737a8ea38ed733f4c6c004c07c27b [formerly 399c06820a4a544250dd35971b5d8e0c0dcc8a53] [formerly 8ecdd9a192529b783e4aa81f2d171fd0156a0077 [formerly 86dc406cd227362abc5274f4cd64813ceb91d00d]]]]
    Former-commit-id: 8ecdd9a192529b783e4aa81f2d171fd0156a0077
    Former-commit-id: 00089684d6f405bf63b66585cb6f0cb2bd6fb18d [formerly 9723a25d830a41cf2e060dc218ad841643217c3e] [formerly e28febcf270ef014707b196a8a01d5bc3daed03f [formerly 9031f58d02f95674498128ecf209893a944e7f57]]
    Former-commit-id: e28febcf270ef014707b196a8a01d5bc3daed03f
    Former-commit-id: f09000ec64283002467c9067cf2d0dab36391266

--- cave/com.raytheon.viz.awipstools/src/com/raytheon/viz/awipstools/common/stormtrack/StormTrackDisplay.java
+++ cave/com.raytheon.viz.awipstools/src/com/raytheon/viz/awipstools/common/stormtrack/StormTrackDisplay.java
@@ -100,6 +100,8 @@ import com.vividsolutions.jts.geom.LineString;
  *  06-24-2013  DR 16317   D. Friedman Handle "motionless" track.
  *  04-07-2014  DR 17232   D. Friedman Make sure pivot indexes are valid.
  *  06-03-14    3191       njensen     Fix postData to not retrieve
+ *  06-17-2014  DR17409 mgamazaychikov Fix futurePoints calculation in generateNewTrackInfo()
+ *                                     and generateExistingTrackInfo()
  * 
  * </pre>
  * 
@@ -910,7 +912,7 @@ public class StormTrackDisplay implements IRenderable {
         // time, the arrow of the pathcast is drawn behind the last frame
         if (state.duration >= 0) {
             for (int i = 1; i < futurePoints.length - (remainder == 0 ? 0 : 1); ++i) {
-                timeInMillis += minIntervalInSeconds * 1000;
+                timeInMillis += interval * 60 * 1000;
                 DataTime time = new DataTime(new Date(timeInMillis));
 
                 double distance = speed
@@ -1062,7 +1064,7 @@ public class StormTrackDisplay implements IRenderable {
         // time, the arrow of the pathcast is drawn behind the last frame
         if (state.duration >= 0) {
             for (int i = 1; i < futurePoints.length - (remainder == 0 ? 0 : 1); ++i) {
-                timeInMillis += minIntervalInSeconds * 1000;
+                timeInMillis += interval * 60 * 1000;
                 DataTime time = new DataTime(new Date(timeInMillis));
 
                 double distance = speed
