ca853f7e93dc3ec79988e442fa227deaea0e9e50
Ben Steffensmeier <Benjamin.P.Steffensmeier@raytheon.com>
2014-06-19 13:03:29 -0500

    Omaha #3238 Implement Interrogatable in SatResource.
    Former-commit-id: 698a2b174606c8425778f5672b613f2d74fb8997

--- cave/com.raytheon.viz.satellite/src/com/raytheon/viz/satellite/tileset/SatTileSetRenderable.java
+++ cave/com.raytheon.viz.satellite/src/com/raytheon/viz/satellite/tileset/SatTileSetRenderable.java
@@ -19,12 +19,23 @@
  **/
 package com.raytheon.viz.satellite.tileset;
 
+import org.geotools.coverage.grid.GeneralGridEnvelope;
+import org.geotools.coverage.grid.GridGeometry2D;
+import org.opengis.coverage.grid.GridEnvelope;
+
 import com.raytheon.uf.common.colormap.image.ColorMapData;
 import com.raytheon.uf.common.dataplugin.satellite.SatelliteRecord;
+import com.raytheon.uf.common.geospatial.data.GeographicDataSource;
+import com.raytheon.uf.common.numeric.source.DataSource;
+import com.raytheon.uf.viz.core.DrawableImage;
 import com.raytheon.uf.viz.core.IGraphicsTarget;
+import com.raytheon.uf.viz.core.VizApp;
+import com.raytheon.uf.viz.core.drawables.IColormappedImage;
+import com.raytheon.uf.viz.core.drawables.IImage;
 import com.raytheon.uf.viz.core.rsc.AbstractVizResource;
 import com.raytheon.uf.viz.core.tile.RecordTileSetRenderable;
 import com.raytheon.uf.viz.core.tile.Tile;
+import com.raytheon.uf.viz.core.tile.TileLevel;
 
 /**
  * Satellite tile set renderable, uses {@link SatDataRetriever} for {@link Tile}
@@ -37,6 +48,8 @@ import com.raytheon.uf.viz.core.tile.Tile;
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Jun 19, 2013            mschenke     Initial creation
+ * Jun 19, 2014 3238       bsteffen     Add method to create a DataSource for
+ *                                      a tile level.
  * 
  * </pre>
  * 
@@ -77,5 +90,66 @@ public class SatTileSetRenderable extends RecordTileSetRenderable {
 
     public SatelliteRecord getSatelliteRecord() {
         return (SatelliteRecord) record;
+    }
+
+    public GeographicDataSource getCurrentLevelDataSource() {
+        TileLevel level = tileSet.getTileLevel(lastPaintedLevel);
+        DataSource tile = new TileLevelDataSource(level);
+        GridEnvelope range = tileSetGeometry.getGridRange();
+        int levelFactor = 2 * level.getLevel();
+        if (levelFactor == 0) {
+            levelFactor = 1;
+        }
+        int startX = range.getLow(0) / levelFactor;
+        int startY = range.getLow(1) / levelFactor;
+        int width = range.getSpan(0) / levelFactor;
+        int height = range.getSpan(1) / levelFactor;
+
+        GridGeometry2D levelGeometry = new GridGeometry2D(
+                new GeneralGridEnvelope(new int[] { startX, startY },
+                        new int[] { width, height }, false),
+                tileSetGeometry.getEnvelope());
+
+        return new GeographicDataSource(tile, levelGeometry);
+    }
+
+    private class TileLevelDataSource implements DataSource {
+
+        private final TileLevel level;
+
+        public TileLevelDataSource(TileLevel level) {
+            this.level = level;
+        }
+
+        @Override
+        public double getDataValue(final int x, final int y) {
+            Tile tile = level.getTile((double) x, (double) y);
+            IColormappedImage cmapImage = null;
+            if (tile != null) {
+                DrawableImage di = imageMap.get(tile);
+                if (di != null) {
+                    IImage image = di.getImage();
+                    if (image instanceof IColormappedImage) {
+                        cmapImage = (IColormappedImage) image;
+                    }
+                }
+            }
+            if (cmapImage != null) {
+                final IColormappedImage theImage = cmapImage;
+                final double[] result = new double[1];
+                VizApp.runSync(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        result[0] = theImage.getValue(x % tileSize, y
+                                % tileSize);
+                    }
+
+                });
+                return result[0];
+            }
+            return Double.NaN;
+        }
+
     }
 }
