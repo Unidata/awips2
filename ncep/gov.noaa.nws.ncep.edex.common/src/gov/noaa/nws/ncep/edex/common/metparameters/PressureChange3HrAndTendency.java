069194fa7d2ae1d8a65c96527cf62152e5de5118
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-06-23 15:20:37 -0400

    VLab Issue #4001 - NCEP EDEX and EDEX Common delivery for 14.2.1-n
    Met parameter changes, and removed most NcInventory definitions
    
    Change-Id: Id55a18abf7e4938a85aa7d5a0ce15105fe490afa
    
    Former-commit-id: b9bfb9bd769aaa72899afee839f15c3da274c95f [formerly 4c6ec612d8cfa9111fcf8cc388db16068428a935 [formerly 3a5a3f3a461c24388d9e1301bc43993963461f3d]]
    Former-commit-id: 4c6ec612d8cfa9111fcf8cc388db16068428a935
    Former-commit-id: 91528c474cae0d39dbe8b43e901684ed4ab684e7

---
+++ ncep/gov.noaa.nws.ncep.edex.common/src/gov/noaa/nws/ncep/edex/common/metparameters/PressureChange3HrAndTendency.java
@@ -0,0 +1,73 @@
+package gov.noaa.nws.ncep.edex.common.metparameters;
+
+import gov.noaa.nws.ncep.edex.common.metparameters.MetParameterFactory.DeriveMethod;
+import gov.noaa.nws.ncep.edex.common.metparameters.parameterconversion.PRLibrary;
+import gov.noaa.nws.ncep.edex.common.metparameters.parameterconversion.PRLibrary.InvalidValueException;
+
+import javax.measure.quantity.Dimensionless;
+import javax.measure.unit.Unit;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.raytheon.uf.common.serialization.ISerializableObject;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
+import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
+
+/*
+ * Used for the PTND button, which combines P03C + PTSY
+ * PressureChange3Hr + PressureTendencySymbol
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+@DynamicSerialize
+public class PressureChange3HrAndTendency extends AbstractMetParameter implements Dimensionless, ISerializableObject {
+
+    @DynamicSerializeElement
+    private static final long serialVersionUID = -6602297437762954327L;
+
+    public PressureChange3HrAndTendency() {
+        super(UNIT);
+
+    }
+
+    @DeriveMethod
+    public PressureChange3HrAndTendency derive(PressChange3HrAbsVal p, PressureTendencySymbol ptsy) throws InvalidValueException, NullPointerException {
+
+        if (p.hasValidValue() && ptsy.hasValidValue()) {
+            Number n = (Number) new Integer(ptsy.getStringValue());
+            Amount ptsyAmount = new Amount(n, Unit.ONE);
+            Amount theP03CAmount = PRLibrary.prP03CAbsVal(p, ptsyAmount);
+
+            this.setValue(theP03CAmount);
+            this.setAssociatedMetParam(copyDerivedPTSY(ptsy));
+
+            // Works, ptsy values can be seen form here
+            // System.out.print("==> derive called: ptsy: " + ptsy.toString());
+            if (this.getAssociatedMetParam() == null) {
+                // System.out.print(": BUT assoc metparm IS NULL");
+            } else {
+                // System.out.println(": ASSOC metparm to string: " + this.getAssociatedMetParam().toString());
+            }
+
+        } else {
+            this.setValueToMissing();
+        }
+
+        return this;
+    }
+
+    private PressureTendencySymbol copyDerivedPTSY(PressureTendencySymbol ptsy) {
+
+        PressureTendencySymbol cptsy = new PressureTendencySymbol();
+
+        cptsy.setDataTime(ptsy.getDataTime());
+        cptsy.setStringValue(ptsy.getStringValue());
+        cptsy.setUnit(ptsy.getUnit());
+        cptsy.setValidTime(ptsy.getValidTime());
+
+        return cptsy;
+
+    }
+
+}// end class PressureChange3HrAndTendency
