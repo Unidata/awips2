444d61f77628e2819f16e1d05a356298f7026bb7
Richard Peter <richard.peter@raytheon.com>
2014-06-23 17:20:46 -0500

    Omaha #3225 Reduce default hibernate connections
    Change-Id: Ia32e465b77d1e201ab4f3b1474af610753602b06
    
    Former-commit-id: 152930abde259231519be4d6d356028dd58cb278 [formerly b6092098481ba2d92f7ae0baf5b4de6c497cb2e7] [formerly 2bbb6fd90caf0e408dee379df96eb0068457c76c] [formerly 47081e7ae3b8f528c2d492d22acb6581e6be9541 [formerly 2bbb6fd90caf0e408dee379df96eb0068457c76c [formerly fa5b34de9240e02a1ae555d7ccaf65efa2039a9b]]]
    Former-commit-id: 47081e7ae3b8f528c2d492d22acb6581e6be9541
    Former-commit-id: 695463de89b665ec6f7ff4a32bfa1e84a7cb56cc [formerly 2502fc95434d7c83c47abf5e4c969e7e4f90f20e]
    Former-commit-id: 13c116bae56243ca4a159804fec5cccd204343f4

--- edexOsgi/build.edex/esb/etc/devRegistry.sh
+++ edexOsgi/build.edex/esb/etc/devRegistry.sh
@@ -21,7 +21,6 @@
 
 export MAX_MEM=1536 # in Meg
 export MAX_PERM_SIZE=192m
-export METADATA_POOL_MIN=10
 export EDEX_DEBUG_PORT=5012
 export EDEX_JMX_PORT=1620
 export LOG_CONF=logback-registry.xml
