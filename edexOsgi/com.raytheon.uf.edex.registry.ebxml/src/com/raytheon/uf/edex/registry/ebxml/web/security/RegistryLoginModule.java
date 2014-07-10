ec91eaed8fd5d48a95c4f86be23dfcc95105f997
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2014-07-10 13:58:05 -0500

    Omaha #3350 Added user/role authentication. Added XACML authorization
    Change-Id: Id307fdc04ba7a74c9e81650c7b4ba272405cf6df
    
    Former-commit-id: d4997f5a90112296f579833a7e82df11fc0d3072 [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b] [formerly d4997f5a90112296f579833a7e82df11fc0d3072 [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b] [formerly 6cb73e63a15b75e9922e015ccd92e880ba9bd241 [formerly 81d0249971d4004067b6aef672e00417ddb83b36]]]
    Former-commit-id: 6cb73e63a15b75e9922e015ccd92e880ba9bd241
    Former-commit-id: 6959bdcdb2e3602050baaab6cae562e3e5f97d6e [formerly 576e2eb0f8d8febccfbec3bf95b42fdf034a0080]
    Former-commit-id: b0ddc103a77e20460e3283e00138b6744736ef69

---
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/web/security/RegistryLoginModule.java
@@ -0,0 +1,80 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.edex.registry.ebxml.web.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.ws.WebServiceException;
+
+import org.eclipse.jetty.plus.jaas.spi.AbstractLoginModule;
+import org.eclipse.jetty.plus.jaas.spi.UserInfo;
+import org.eclipse.jetty.util.security.Credential;
+import org.eclipse.jetty.util.security.Password;
+
+import com.raytheon.uf.common.registry.services.RegistryServiceException;
+
+/**
+ * 
+ * The registry login module used by the Jetty server hosting the registry services
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#     Engineer    Description
+ * ------------ ----------  ----------- --------------------------
+ * 7/10/2014    1717        bphillip    Initial creation
+ * </pre>
+ * 
+ * @author bphillip
+ * @version 1
+ **/
+public class RegistryLoginModule extends AbstractLoginModule {
+
+    /**
+     * Creates a new RegistryLoginModule
+     */
+    public RegistryLoginModule() {
+        super();
+    }
+
+    @Override
+    public UserInfo getUserInfo(final String userName)  {
+        String[] user = null;
+        try {
+            user = CredentialCache.getInstance().getUser(userName);
+        } catch (RegistryServiceException e) {
+            throw new WebServiceException("User [" + userName
+                    + " Not authorized!",e);
+        }
+        for(String userField:user){
+            if(userField == null){
+                throw new WebServiceException("User [" + userName
+                        + " Not authorized!");
+            }
+        }
+        List<String> roleList = new ArrayList<String>(1);
+        roleList.add(user[2]);
+        Credential credential = new Password(user[1]);
+        UserInfo userInfo = new UserInfo(userName, credential, roleList);
+        return userInfo;
+    }
+}
