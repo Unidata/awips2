c0b0d0d9bc9f9c42c2f2b8b305368cc9dc525360
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2014-07-10 13:58:05 -0500

    Omaha #3350 Added user/role authentication. Added XACML authorization
    Change-Id: Id307fdc04ba7a74c9e81650c7b4ba272405cf6df
    
    Former-commit-id: a62e029c8e9b7a2b0fa865b929f76f6abc8bc75b [formerly a62e029c8e9b7a2b0fa865b929f76f6abc8bc75b [formerly d4997f5a90112296f579833a7e82df11fc0d3072]] [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b] [formerly 150bc4c30d1b01d99c60a570b3bb802ccce0903a [formerly 150bc4c30d1b01d99c60a570b3bb802ccce0903a [formerly 6cb73e63a15b75e9922e015ccd92e880ba9bd241]] [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b [formerly 81d0249971d4004067b6aef672e00417ddb83b36]]]]
    Former-commit-id: 150bc4c30d1b01d99c60a570b3bb802ccce0903a [formerly 150bc4c30d1b01d99c60a570b3bb802ccce0903a [formerly 6cb73e63a15b75e9922e015ccd92e880ba9bd241]]
    Former-commit-id: 19096aa2b84bc963f1ee89ff218b7bb4a6357de8 [formerly 00d8c1cddb442d8340ffdaa2b9b0a0edf256bfd6] [formerly 958293e7c94ef9fc04eb0d27324581abe5287e22 [formerly 576e2eb0f8d8febccfbec3bf95b42fdf034a0080]]
    Former-commit-id: 13efd896587e767b5cd5a748c64202a78da6437d [formerly 6959bdcdb2e3602050baaab6cae562e3e5f97d6e]
    Former-commit-id: 39a9c2d9f92060f1cd597fd16c049c457c6497f4

--- edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/ebxml/RegistryUtil.java
+++ edexOsgi/com.raytheon.uf.common.registry.ebxml/src/com/raytheon/uf/common/registry/ebxml/RegistryUtil.java
@@ -54,7 +54,6 @@ import com.raytheon.uf.common.registry.ebxml.slots.SlotConverter;
 import com.raytheon.uf.common.registry.ebxml.slots.StringSlotConverter;
 import com.raytheon.uf.common.serialization.SerializationException;
 import com.raytheon.uf.common.time.util.ImmutableDate;
-import com.raytheon.uf.common.util.ClusterIdUtil;
 import com.raytheon.uf.common.util.CollectionUtil;
 import com.raytheon.uf.common.util.ReflectionException;
 import com.raytheon.uf.common.util.ReflectionUtil;
@@ -84,6 +83,7 @@ import com.raytheon.uf.common.util.ReflectionUtil;
  * Apr 24, 2014 2992       dhladky     fixed all objects in ebxml owned by NCF, bad.
  * 6/5/2014     1712       bphillip    Registry now communicates over https
  * June 25, 2014 3273      dhladky     Remove all DD environment variables from setup.env
+ * 7/10/2014    1717       bphillip    Changed default user
  * 
  * </pre>
  * 
@@ -104,7 +104,7 @@ public final class RegistryUtil {
     
     public static final String registryUser = "Registry";
     
-    public static final String defaultUser = "NCF";
+    public static final String defaultUser = RegistryObjectType.SYSTEM_USER;
 
     static {
         if (System.getProperty("ebxml.registry.host") != null
@@ -121,7 +121,7 @@ public final class RegistryUtil {
     /**
      * The default internal owner is the local registry ID
      */
-    public static final String DEFAULT_OWNER = ClusterIdUtil.getId();
+    public static final String DEFAULT_OWNER = defaultUser;
 
     // A private mapping of attribute types to slot types, used when storing an
     // object to the registry to map QueryableAttributes to SlotConverters.
