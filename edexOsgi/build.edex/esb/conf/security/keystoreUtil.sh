c74f855d31096bbad3add3633ca24af482ad07bc
Benjamin Phillippe <Benjamin_D_Phillippe@raytheon.com>
2014-07-10 13:58:05 -0500

    Omaha #3350 Added user/role authentication. Added XACML authorization
    Change-Id: Id307fdc04ba7a74c9e81650c7b4ba272405cf6df
    
    Former-commit-id: 6cb73e63a15b75e9922e015ccd92e880ba9bd241 [formerly cebfbebea626bb4b7f5f058ac5244ef60e98b75b [formerly 81d0249971d4004067b6aef672e00417ddb83b36]]
    Former-commit-id: cebfbebea626bb4b7f5f058ac5244ef60e98b75b
    Former-commit-id: d4997f5a90112296f579833a7e82df11fc0d3072

--- edexOsgi/build.edex/esb/conf/security/keystoreUtil.sh
+++ edexOsgi/build.edex/esb/conf/security/keystoreUtil.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+SETUP_ENV=/awips2/edex/bin/setup.env
+source $SETUP_ENV
+
+JAVA_BIN=/awips2/java/jre/bin/java
+
 securityDir=/awips2/edex/conf/security
-securityProps=$securityDir/security.properties
+securityPropertiesDir=/awips2/edex/conf/resources/site/$AW_SITE_IDENTIFIER
+securityProps=$securityPropertiesDir/security.properties
 publicKeyFile=PublicKey.cer
 keystore=keystore.jks
 truststore=truststore.jks
 
 keystorePw=
-keyAlias=$(hostname -s)
 keyPw=
-
+encryptionKey=encrypt
 truststorePw=password
 
 function usage {
@@ -23,8 +28,32 @@ function generateKeystores() {
 
 echo "Generating keystores"
 
+if [ -z $CLUSTER_ID ] 
+then
+	echo "CLUSTER_ID undefined. Determining from hostname..."
+	HOST=$(hostname -s)
+	CLUSTER_ID=${HOST:$(expr index "$HOST" -)} | tr '[:lower:]' '[:upper:]'
+fi
+
+if [ -z $CLUSTER_ID ] 
+then	
+	echo "CLUSTER_ID could not be determined from hostname. Using site as CLUSTER_ID"
+	CLUSTER_ID=$AW_SITE_IDENTIFIER
+fi
+
+echo "CLUSTER_ID set to: $CLUSTER_ID"
+
+keyAlias=$CLUSTER_ID
+# Write the cluster ID to the setup.env file
+sed -i "s@^export CLUSTER_ID.*@export CLUSTER_ID=$CLUSTER_ID@g" $SETUP_ENV
+
+
 if [ ! -d "$securityDir" ]; then
    mkdir $securityDir
+fi
+
+if [ ! -d "$securityPropertiesDir" ]; then
+   mkdir -p $securityPropertiesDir
 fi
 
 while [ -z $keystorePw ];
@@ -74,20 +103,29 @@ keytool -genkeypair -alias $keyAlias -keypass $keyPw -keystore $keystore -storep
 echo -n "Exporting public key..."
 exportOutput=`keytool -exportcert -alias $keyAlias -keystore $keystore -file $keyAlias$publicKeyFile -storepass $keystorePw 2>&1`
 echo "Done!"
-obfuscatedKeystorePassword=`java -cp /awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-http-7.6.14.v20131031.jar:/awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-util-7.6.14.v20131031.jar org.eclipse.jetty.util.security.Password $keystorePw 2>&1 | grep OBF`
-
+obfuscatedKeystorePassword=`$JAVA_BIN -cp /awips2/edex/lib/dependencies/org.apache.commons.codec/commons-codec-1.4.jar:/awips2/edex/lib/plugins/com.raytheon.uf.common.security.jar com.raytheon.uf.common.security.encryption.AESEncryptor encrypt $encryptionKey $keystorePw 2>&1`
 echo "Generating trust store..."
 
 keytool -genkey -alias tmp -keypass tempPass -dname CN=foo -keystore $truststore -storepass $truststorePw
 keytool -delete -alias tmp -keystore $truststore -storepass $truststorePw
 keytool -import -trustcacerts -file $keyAlias$publicKeyFile -alias $keyAlias -keystore $truststore -storepass $truststorePw
 
-obfuscatedTruststorePassword=`java -cp /awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-http-7.6.14.v20131031.jar:/awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-util-7.6.14.v20131031.jar org.eclipse.jetty.util.security.Password $truststorePw 2>&1 | grep OBF`
+jettyObscuredPassword=`$JAVA_BIN -cp /awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-http-7.6.14.v20131031.jar:/awips2/edex/lib/dependencies/org.eclipse.jetty/jetty-util-7.6.14.v20131031.jar org.eclipse.jetty.util.security.Password $keystorePw 2>&1 | grep OBF`
+
+obfuscatedTruststorePassword=`$JAVA_BIN -cp /awips2/edex/lib/dependencies/org.apache.commons.codec/commons-codec-1.4.jar:/awips2/edex/lib/plugins/com.raytheon.uf.common.security.jar com.raytheon.uf.common.security.encryption.AESEncryptor encrypt $encryptionKey $truststorePw 2>&1`
 
 
 echo -n "Generating security properties file..."
 
-echo "edex.security.keystore.path=$securityDir/$keystore" > $securityProps
+echo "# This file was automatically generated using /awips2/edex/conf/security/keystoreUtil.sh" > $securityProps
+echo "java.security.auth.login.config=/awips2/edex/conf/security/realms.properties" >> $securityProps
+echo "edex.security.auth.user=$keyAlias" >> $securityProps
+echo "edex.security.auth.password=$obfuscatedKeystorePassword" >> $securityProps
+echo "edex.security.auth.authorizationType=Basic" >> $securityProps
+echo "edex.security.auth.loginService.name=RegistryRealm" >> $securityProps
+echo "edex.security.auth.loginService.realm=RegistryRealm" >> $securityProps
+echo "edex.security.encryption.key=$encryptionKey" >> $securityProps
+echo "edex.security.keystore.path=$securityDir/$keystore" >> $securityProps
 echo "edex.security.keystore.alias=$keyAlias" >> $securityProps
 echo "edex.security.keystore.password=$obfuscatedKeystorePassword" >> $securityProps
 echo "edex.security.keystore.type=JKS" >> $securityProps
@@ -100,7 +138,7 @@ echo "edex.security.disableCNCheck=false" >>$securityProps
 
 echo "#The following configuration items are used with the wss4j in/out interceptors" >> $securityProps
 echo "org.apache.ws.security.crypto.merlin.keystore.file=security/$keystore" >> $securityProps
-echo "org.apache.ws.security.crypto.merlin.keystore.password=$obfuscatedKeystorePassword" >> $securityProps
+echo "org.apache.ws.security.crypto.merlin.keystore.password=$jettyObscuredPassword" >> $securityProps
 echo "org.apache.ws.security.crypto.merlin.keystore.type=JKS" >> $securityProps
 echo "org.apache.ws.security.crypto.merlin.keystore.alias=$keyAlias" >> $securityProps
 
