b2826881d1d5a594577d5f8034088c49f23f1972
Nate Jensen <Nathan.Jensen@raytheon.com>
2014-07-31 09:45:01 -0500

    Omaha #3184 renamed valid identifiers to optional
    Change-Id: I9653f9e00b3c693b46b516bc52ff029cd4fe1a81
    
    Former-commit-id: 3a9c29fa88320f37746e304fea59bdad241778cc [formerly 77d0661861374121d2182c52b1c4288ccbb2d083] [formerly 857b849d378375aaeac661b0f8dcba96c7d0e2e6 [formerly 3c1ae47a760e694a73f9d796d13625891b3c7dde]] [formerly 857179e08dd9a3a15bdf2e9ae2b80e7a7c9b5fc5 [formerly 3c1ae47a760e694a73f9d796d13625891b3c7dde [formerly d0e68c2fa1cc531263622410e4364b87f222139f]]]
    Former-commit-id: 857179e08dd9a3a15bdf2e9ae2b80e7a7c9b5fc5
    Former-commit-id: 5f5ccd371d4bb68eebbd37f681f382e2a4178a4a [formerly f3ce06c00e13124ea1694eea1ae03dea35970fb8]
    Former-commit-id: 79ae12875826c17859655933b32ac6c812648e03

--- pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataaccess/request/GetOptionalIdentifiersRequest.py
+++ pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataaccess/request/GetOptionalIdentifiersRequest.py
@@ -27,13 +27,14 @@
 #    Date            Ticket#       Engineer       Description
 #    ------------    ----------    -----------    --------------------------
 #    07/23/14         #3185        njensen        Initial Creation.    
+#    07/30/14         #3185        njensen        Renamed valid to optional
 #
 #
 
 from dynamicserialize.dstypes.com.raytheon.uf.common.dataaccess.request import AbstractIdentifierRequest
 
-class GetValidIdentifiersRequest(AbstractIdentifierRequest):
+class GetOptionalIdentifiersRequest(AbstractIdentifierRequest):
 
     def __init__(self):
-        super(GetValidIdentifiersRequest, self).__init__()    
+        super(GetOptionalIdentifiersRequest, self).__init__()    
 
