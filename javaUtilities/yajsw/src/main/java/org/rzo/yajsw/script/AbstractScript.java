b3a4280c4bb69cd06e2b0c7a03c4baa8be5e5ed4
Bryan Kowal <Bryan.Kowal@raytheon.com>
2014-08-07 16:22:31 -0500

    Issue #3470 - the wrapperCapture script is now packaged in the awips2-edex and awips2-qpid-java-broker rpms. additional log4j jar is now included in the awips2-qpid-java-common rpm. Fixed AbstractScript import error.
    Change-Id: I7acfdff45944b8d9dd2f75e85cca3cc6d54b1903
    
    Former-commit-id: f1d38a608a57e468bc8c10bb0f2e0dc87b3683c3 [formerly f1d38a608a57e468bc8c10bb0f2e0dc87b3683c3 [formerly 09442217cd5b2a16860bf5344bdfab03313ceeb3]] [formerly a9b4b4124a172a20b4273f2ffa588d450820923c] [formerly cd3d4537118f4530517778c7c4078fde5b44ab62 [formerly cd3d4537118f4530517778c7c4078fde5b44ab62 [formerly b568c25e106b6fa6db2444cf933a9e5eb7152ba1]] [formerly a9b4b4124a172a20b4273f2ffa588d450820923c [formerly a9b4b4124a172a20b4273f2ffa588d450820923c [formerly d11649deb78aa356f27a22f33b38f7238513576c]]]]
    Former-commit-id: cd3d4537118f4530517778c7c4078fde5b44ab62 [formerly cd3d4537118f4530517778c7c4078fde5b44ab62 [formerly b568c25e106b6fa6db2444cf933a9e5eb7152ba1]]
    Former-commit-id: e53c64c9ab296f9aba2a3432af1a371eb345c9f6 [formerly 1e58f38a04e0e591aa682ca03cb8fa52aaf69fe5] [formerly 63a9cf3044fef283f0bc2d58049386f4aa24bb02 [formerly 1d6eb95f9f95fb48164c530ccc7a301ac9529313]]
    Former-commit-id: c7432d2817cf121a66104e794c95f224372ac463 [formerly b0c94a2d6174bdd75c8fac45025e0c9bcd046bff]
    Former-commit-id: 2c3d50f24c546d3fd106cfbc1f99f2e051b32f9d

--- javaUtilities/yajsw/src/main/java/org/rzo/yajsw/script/AbstractScript.java
+++ javaUtilities/yajsw/src/main/java/org/rzo/yajsw/script/AbstractScript.java
@@ -16,6 +16,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.jboss.netty.util.HashedWheelTimer;
 import org.jboss.netty.util.Timeout;
@@ -28,28 +29,26 @@ import org.rzo.yajsw.wrapper.WrappedProcess;
 /**
  * The Class AbstractScript.
  */
-public abstract class AbstractScript implements Script
-{
+public abstract class AbstractScript implements Script {
 
 	/** The _name. */
-	String					_name;
+	String _name;
 
 	/** The _timeout. */
-	int						_timeout	= 30000;
+	int _timeout = 30000;
 
-	WrappedProcess			_process;
+	WrappedProcess _process;
 
-	String					_id;
+	String _id;
 
-	String[]				_args;
-	
+	String[] _args;
+
 	final static Timer TIMER = new HashedWheelTimer();
-	static final ExecutorService	EXECUTOR		= (ThreadPoolExecutor) new ThreadPoolExecutor(0, 50, 120L, TimeUnit.SECONDS,
-			new SynchronousQueue<Runnable>(), new DaemonThreadFactory("scriptExecutorInternal"));
+	static final ExecutorService EXECUTOR = (ThreadPoolExecutor) new ThreadPoolExecutor(
+			0, 50, 120L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(),
+			new DaemonThreadFactory("scriptExecutorInternal"));
 	volatile Future _future;
 	volatile Timeout _timerTimeout;
-
-
 
 	/**
 	 * Instantiates a new abstract script.
@@ -58,8 +57,8 @@ public abstract class AbstractScript implements Script
 	 *            the script
 	 * @param timeout
 	 */
-	public AbstractScript(String script, String id, WrappedProcess process, String[] args, int timeout)
-	{
+	public AbstractScript(String script, String id, WrappedProcess process,
+			String[] args, int timeout) {
 		_name = script;
 		_process = process;
 		_id = id;
@@ -76,34 +75,35 @@ public abstract class AbstractScript implements Script
 	 * java.lang.String, java.lang.Object)
 	 */
 	public abstract Object execute(String line);
+
 	public abstract void interrupt();
+
 	abstract void log(String msg);
 
-	synchronized public void executeWithTimeout(final String line)
-	{
-        /**
-         * Changed by rjpeter  Aug 07, 2014.
-         */
-        _future = EXECUTOR.submit(new Callable<Object>() {
-            @Override
-            public Object call() {
-                return execute(line);
-            }
-        });
+	synchronized public void executeWithTimeout(final String line) {
+		/**
+		 * Changed by rjpeter Aug 07, 2014.
+		 */
+		_future = EXECUTOR.submit(new Callable<Object>() {
+			@Override
+			public Object call() {
+				return execute(line);
+			}
+		});
 
-        // wait for script to finish
-        try {
-            _future.get(_timeout, TimeUnit.MILLISECONDS);
-        } catch (TimeoutException e) {
-            log("script " + _name + " took too long -> interrupt");
-            try {
-                interrupt();
-            } catch (Throwable e1) {
+		// wait for script to finish
+		try {
+			_future.get(_timeout, TimeUnit.MILLISECONDS);
+		} catch (TimeoutException e) {
+			log("script " + _name + " took too long -> interrupt");
+			try {
+				interrupt();
+			} catch (Throwable e1) {
 
-            }
-        } catch (Exception e) {
+			}
+		} catch (Exception e) {
 
-        }
+		}
 	}
 
 	/*
@@ -111,8 +111,7 @@ public abstract class AbstractScript implements Script
 	 * 
 	 * @see org.rzo.yajsw.script.Script#getScript()
 	 */
-	public String getScript()
-	{
+	public String getScript() {
 		return _name;
 	}
 
@@ -121,8 +120,7 @@ public abstract class AbstractScript implements Script
 	 * 
 	 * @return the timeout
 	 */
-	public int getTimeout()
-	{
+	public int getTimeout() {
 		return _timeout;
 	}
 
@@ -132,8 +130,7 @@ public abstract class AbstractScript implements Script
 	 * @param timeout
 	 *            the new timeout
 	 */
-	public void setTimeout(int timeout)
-	{
+	public void setTimeout(int timeout) {
 		_timeout = timeout;
 	}
 
