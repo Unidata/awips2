5d0fbe9e7b2346d690aad1de38e02d4ea3896ae9
Nathan Bowler <Nathan.A.Bowler@raytheon.com>
2014-08-12 10:32:24 -0400

    Omaha #3393 Add request/response for RadarDataRecord. Remove broken RadarDao override. Convert a2_rad scripts.
    Change-Id: Ie3ea761920c1ecec233e1fc586f3cbcd5914f1c6
    
    Former-commit-id: 234c05ec0783b5ffa9b655b9594ce0dc26960f9e [formerly 10f163ec37468285431ae715ec1c1c235910bc21] [formerly f70776903c1fe0bacdf69e7843874cfa951d5f70] [formerly f70776903c1fe0bacdf69e7843874cfa951d5f70 [formerly 2fcc25bb9bd3945dd37d8fe672fe2e8ea47449b1]] [formerly 8fdc47855e8b4eecb7794d37f1eb1640ed4447c8 [formerly f70776903c1fe0bacdf69e7843874cfa951d5f70 [formerly 2fcc25bb9bd3945dd37d8fe672fe2e8ea47449b1] [formerly 8fdc47855e8b4eecb7794d37f1eb1640ed4447c8 [formerly 44ca6fb8a26cc9af7267ce635a200eb3c26ce9f8]]]]
    Former-commit-id: 8fdc47855e8b4eecb7794d37f1eb1640ed4447c8
    Former-commit-id: 5b7dddf8cf41948f7ae180b79732ca5500dd62a3 [formerly 774f44cab9d2b4fee8d681e9226c84c8f3a3bc47] [formerly b1fbb4e678039d6d4372942b6c1d302613d05011 [formerly f2be9c044e454d8445d4dd3c3bfa525f04da6713]]
    Former-commit-id: b1fbb4e678039d6d4372942b6c1d302613d05011
    Former-commit-id: 7a50028eff603799487b65e09a90029a26291df1

--- pythonPackages/msaslaps/radar/a2gtradStub.py
+++ pythonPackages/msaslaps/radar/a2gtradStub.py
@@ -1,208 +1,166 @@
-import BaseRequest
-from com.raytheon.uf.common.message.response import ResponseMessageGeneric
-from com.raytheon.edex.plugin.radar.dao import RadarDao
-
-# Perform a radar request for data of interest
-rr = BaseRequest.BaseRequest("radar")
-rr.addParameter("icao","KKKK","=")
-rr.addParameter("productCode","MMMM","=") 
-rr.addParameter("primaryElevationAngle","EEEE","=")
-rr.addParameter("dataTime","AAAAA",">=")
-rr.addParameter("dataTime","BBBBB","<=")
-hedfmt = "FFF"
-encoding = XXXXX
-
-result = rr.execute()
-size = result.size()
-if size == 0:
-   return ResponseMessageGeneric("Data not available")
-
-# ResponseMessageGeneric.  Payload is RadarRecord
-rmg = result.get(0)
-# return rmg
-
-# RadarRecord
-rrec = rmg.getContents()
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+#
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+#
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+#
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
 
 #
-#  From here to the end is the part we know how to do for radar but not
-#  for radar.
+# Gets data for a single radar product from the A-II database.  The result is
+# output to stdout as ASCII.  This uses a data-specific Request/Response instead
+# of the DataAccessLayer in order to preserve data-genericness of the interface.
 #
-mytime = rrec.getDataURI().split('/',4)[2]
+#  
+#    
+#     SOFTWARE HISTORY
+#    
+#    Date            Ticket#       Engineer       Description
+#    ------------    ----------    -----------    --------------------------
+#    08/11/2014      3393          nabowle        Initial modification. Replaces UEngine 
+#                                                 with a custom Request/Response.
+#
+#
 
-# RadarDao.  Inherits from PluginDao, which has a getHDF5Data method,
-# which takes a PluginDataObject as an arg.
-raddao = RadarDao("radar")
+import argparse
+import a2radcommon
 
-# returns IDataRecord[].  IDataRecord is implemented by only one class --
-# AbstractStorageRecord.  ASR is extended by a few *DataRecord classes; one
-# of them is ByteDataRecord
-idra = raddao.getHDF5Data(rrec,-1)
+def get_args():
+    parser = a2radcommon.get_args_parser()
+    parser.add_argument("--hex",  action='store_const', dest="encoding",
+                    const=1, help="Hex encoding.", metavar="encoding")
+    parser.add_argument("--int",  action='store_const', dest="encoding",
+                    const=0, help="Delimited integer encoding.",
+                    metavar="encoding")
+    parser.add_argument("--extended", action='store_true', default=False,
+                    dest="extended", help="Output the extended header.")
+    return parser.parse_args()
 
-msg = "No data."
-if len(idra) > 0:
-   # pick radar Data record
-   # record 0 contains Angles             getFloatData
-   # record 1 contains Data               getByteData
-   # record 2 contains DependentValues    getShortData
-   # record 3 contains ProductVals        getByteData
-   # record 4 contains RecordVals         getByteData
-   # record 5 contains StormIds           getByteData
-   # record 6 contains Symbology          getByteData
-   # record 7 contains SymbologyData      getByteData
-   # record 8 contains Thresholds         getShortData
-   dattyp = "raster"
-   for ii in range(len(idra)):
-      if idra[ii].getName() == "Data":
-         rdat = idra[ii]
-      elif idra[ii].getName() == "Angles":
-         azdat = idra[ii]
-         dattyp = "radial"
-      elif idra[ii].getName() == "DependentValues":
-         depVals = idra[ii].getShortData()
-#     elif idra[ii].getName() == "ProductVals":
-#        prodVals = idra[ii].getByteData()
-#     elif idra[ii].getName() == "RecordVals":
-#        recVals = idra[ii].getByteData()
-#     elif idra[ii].getName() == "StormIds":
-#        stormVals = idra[ii].getByteData()
-#     elif idra[ii].getName() == "Symbology":
-#        symVals = idra[ii].getByteData()
-#     elif idra[ii].getName() == "SymbologyData":
-#        symData = idra[ii].getByteData()
-      elif idra[ii].getName() == "Thresholds":
-         threshVals = idra[ii].getShortData()
 
-   # this hints at the IDR's concrete class:  ByteDataRecord
-   #print "true type of IDataRecord:", idr.getDataObject().toString()
+def main():
+    user_args = get_args()
 
-   dim = rdat.getDimension()
-   if dim != 2:
-       return ResponseMessageGeneric(msg)
+    records = a2radcommon.send_request(user_args)
 
-   yLen = rdat.getSizes()[0]
-   xLen = rdat.getSizes()[1]
+    if not records:
+        # print "Data not available"
+        return
 
-   # byte[] -- the raw data
-   array = rdat.getByteData()
-   arraySize = len(array)
-   if xLen * yLen != arraySize:
-      return ResponseMessageGeneric(msg)
+    description = user_args.description
+    if not description:
+        print >> sys.stderr, "Description not provided"
+        return
 
-   # get data for azimuth angles if we have them.
-   if dattyp == "radial" :
-      azVals = azdat.getFloatData()
-      azValsLen = len(azVals)
-      if yLen != azValsLen:
-         return ResponseMessageGeneric(msg)
-   description = "DDDDD"
+    format = user_args.extended
+    encoding = user_args.encoding
 
-   # Encode dimensions, time, mapping, description, tilt, and VCP
-   if hedfmt == "x" :
-      msg = "\n"+ str(xLen) + " " + str(yLen) + " " + mytime + " " + \
-             dattyp + " " + str(rrec.getLatitude()) + " " +  \
-             str(rrec.getLongitude()) + " " +  \
-             str(rrec.getElevation()) + " " +  \
-             str(rrec.getElevationNumber()) + " " +  \
-             description + " " + str(rrec.getTrueElevationAngle()) + " " + \
-             str(rrec.getVolumeCoveragePattern()) + "\n"
-   else :
-      msg = "\n"+ str(xLen) + " " + str(yLen) + " " + mytime + " " + \
-             dattyp + " " + description + " " + \
-             str(rrec.getTrueElevationAngle()) + " " + \
-             str(rrec.getVolumeCoveragePattern()) + "\n"
-   
-   # Encode level labels
-   spec = [".", "TH", "ND", "RF", "BI", "GC", "IC", "GR", "WS", "DS",
-           "RA", "HR", "BD", "HA", "UK"]
-   nnn = len(threshVals)
-   j = 0
-   while j<nnn :
-      lo = threshVals[j] % 256
-      hi = threshVals[j] / 256
-      msg += " "
-      j += 1
-      if hi < 0 :
-         if lo > 14 :
-            msg += "."
-         else :
-            msg += spec[lo]
-         continue
-      if hi % 16 >= 8 :
-         msg += ">"
-      elif hi % 8 >= 4 :
-         msg += "<"
-      if hi % 4 >= 2 :
-         msg += "+"
-      elif hi % 2 >= 1 :
-         msg += "-"
-      if hi >= 64 :
-         msg += "%.2f"%(lo*0.01)
-      elif hi % 64 >= 32 :
-         msg += "%.2f"%(lo*0.05)
-      elif hi % 32 >= 16 :
-         msg += "%.1f"%(lo*0.1)
-      else :
-         msg += str(lo)
-   msg += "\n"
+    print_record(records[0], format, description, encoding)
 
-   # Encode product dependent parameters 17 through 26
-   nnn = len(depVals)
-   j = 0
-   while j<nnn :
-      msg += " " + str(depVals[j])
-      j += 1
-   msg += "\n"
 
-   if dattyp == "radial" :
-      j = 0
-      while j<azValsLen :
-          msg += "%.1f"%azVals[j] + " "
-          j += 1
-      msg += "\n"
+def print_record(record, format, description, encoding):
 
-   plus  = " ghijklmnopqrstuvwxyz"
-   minus = " GHIJKLMNOPQRSTUVWXYZ"
-   nxy = yLen*xLen
-   j = 0
-   while j<nxy :
-       i = 0
-       kk = array[i+j]
-       if kk<0 : kk += 256
-       if encoding == 0 :
-          msg += str(kk)
-       elif encoding == 1 :
-          msg += "%2.2x"%kk
-       elif kk == 0 :
-          msg += "@"
-       elif kk == 255 :
-          msg += "#"
-       else :
-          msg += "%2.2x"%kk
-       i += 1
-       while i<xLen :
-          k = array[i+j]
-          if k<0 : kk += 256
-          if encoding == 0 :
-             msg += " "+str(k)
-          elif encoding == 1 :
-             msg += "%2.2x"%k
-          elif k==0 :
-             msg += "@"
-          elif k == 255 :
-             msg += "#"
-          elif k==kk :
-             msg += "."
-          elif k>kk+20 or k<kk-20 :
-             msg += "%2.2x"%k
-          elif k>kk :
-             msg += plus[k-kk]
-          else :
-             msg += minus[kk-k]
-          kk = k
-          i += 1
-       msg += "\n"
-       j += xLen
+    idra = record.getHdf5Data()
 
-return ResponseMessageGeneric(msg)
+    rdat,azdat,depVals,threshVals = a2radcommon.get_hdf5_data(idra)
+    
+    if not rdat:
+        # Graphic, XY
+        # print "Unsupported radar format"
+        return
+
+    dim = rdat.getDimension()
+    if dim != 2:
+        # print "Data not available"
+        return
+
+    yLen = rdat.getSizes()[0]
+    xLen = rdat.getSizes()[1]
+
+    # byte[] -- the raw data
+    array = rdat.getByteData()
+    arraySize = len(array)
+    if xLen * yLen != arraySize:
+       # print "Data not available"
+       return
+
+    # get data for azimuth angles if we have them.
+    if azdat :
+        azVals = azdat.getFloatData()
+        azValsLen = len(azVals)
+        if yLen != azValsLen:
+            # print "Data not available"
+            return
+
+    msg = a2radcommon.get_header(record, format, xLen, yLen, azdat, description)
+
+    msg += a2radcommon.encode_thresh_vals(threshVals)
+    msg += a2radcommon.encode_dep_vals(depVals)
+    if azdat :
+        msg += a2radcommon.encode_radial(azVals)
+    msg += encode_data(yLen, xLen, array, encoding)
+
+    print msg.strip()
+
+
+def encode_data(yLen, xLen, array, encoding):
+    plus  = " ghijklmnopqrstuvwxyz"
+    minus = " GHIJKLMNOPQRSTUVWXYZ"
+    nxy = yLen*xLen
+    j = 0
+    msg = ""
+    while j<nxy :
+        i = 0
+        kk = array[i+j]
+        if kk<0 : kk += 256
+        if encoding == 0 :
+            msg += str(kk)
+        elif encoding == 1 :
+            msg += "%2.2x"%kk
+        elif kk == 0 :
+            msg += "@"
+        elif kk == 255 :
+            msg += "#"
+        else :
+            msg += "%2.2x"%kk
+        i += 1
+        while i<xLen :
+            k = array[i+j]
+            if k<0 : kk += 256
+            if encoding == 0 :
+                msg += " "+str(k)
+            elif encoding == 1 :
+                msg += "%2.2x"%k
+            elif k==0 :
+                msg += "@"
+            elif k == 255 :
+                msg += "#"
+            elif k==kk :
+                msg += "."
+            elif k>kk+20 or k<kk-20 :
+                msg += "%2.2x"%k
+            elif k>kk :
+                msg += plus[k-kk]
+            else :
+                msg += minus[kk-k]
+            kk = k
+            i += 1
+        msg += "\n"
+        j += xLen  
+    return msg
+
+
+if __name__ == '__main__':
+    main()
 
