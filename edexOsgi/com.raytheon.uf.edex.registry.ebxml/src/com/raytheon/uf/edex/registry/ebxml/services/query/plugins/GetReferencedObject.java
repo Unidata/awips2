76e977eaa4b3ea3104566d0b7155b80a992c73f8
Nate Jensen <Nathan.Jensen@raytheon.com>
2014-08-15 15:54:08 -0500

    Omaha #3356 move EbxmlNamespaces and RegrepUtil to unique package name to allow removal of duplicate package name of com.raytheon.uf.common.registry
    Change-Id: I502e14e8c729d606f6122aa969eef7d8761181c4
    
    Former-commit-id: 6b51b46952d457d4b5cec4d6a0615b41324d4a06 [formerly c612ca234c8acbc78fb341a29a9b5d0602c5ffe1] [formerly 83b666c0df0247ff8529bd5033821fc53f6a5360] [formerly 1d3047f3d83708fbc1efa24a7887567f02f63232 [formerly 83b666c0df0247ff8529bd5033821fc53f6a5360 [formerly 9adcbd81d10aa3d464d6dcb0b79b7500f0796cbd]]]
    Former-commit-id: 1d3047f3d83708fbc1efa24a7887567f02f63232
    Former-commit-id: 4b2829fe53aef19ba82f912bd39e3a208ebc1e41 [formerly 84e126537072b9c11530cde2d8bc334d54d8c0c9]
    Former-commit-id: af101e7bb6a36828bced5f10aae54aab51304497

--- edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/plugins/GetReferencedObject.java
+++ edexOsgi/com.raytheon.uf.edex.registry.ebxml/src/com/raytheon/uf/edex/registry/ebxml/services/query/plugins/GetReferencedObject.java
@@ -39,9 +39,9 @@ import oasis.names.tc.ebxml.regrep.xsd.rim.v4.StringValueType;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.raytheon.uf.common.registry.EbxmlNamespaces;
 import com.raytheon.uf.common.registry.constants.CanonicalQueryTypes;
 import com.raytheon.uf.common.registry.constants.QueryReturnTypes;
+import com.raytheon.uf.common.registry.schemas.ebxml.util.EbxmlNamespaces;
 import com.raytheon.uf.edex.registry.ebxml.exception.EbxmlRegistryException;
 import com.raytheon.uf.edex.registry.ebxml.services.lifecycle.ObjectReferenceResolver;
 import com.raytheon.uf.edex.registry.ebxml.services.query.QueryConstants;
