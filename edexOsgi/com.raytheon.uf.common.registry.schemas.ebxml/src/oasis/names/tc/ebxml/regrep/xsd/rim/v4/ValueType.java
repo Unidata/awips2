c612ca234c8acbc78fb341a29a9b5d0602c5ffe1
Nate Jensen <Nathan.Jensen@raytheon.com>
2014-08-15 15:54:08 -0500

    Omaha #3356 move EbxmlNamespaces and RegrepUtil to unique package name to allow removal of duplicate package name of com.raytheon.uf.common.registry
    Change-Id: I502e14e8c729d606f6122aa969eef7d8761181c4
    
    Former-commit-id: 1d3047f3d83708fbc1efa24a7887567f02f63232 [formerly 9adcbd81d10aa3d464d6dcb0b79b7500f0796cbd]
    Former-commit-id: 83b666c0df0247ff8529bd5033821fc53f6a5360

--- edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ValueType.java
+++ edexOsgi/com.raytheon.uf.common.registry.schemas.ebxml/src/oasis/names/tc/ebxml/regrep/xsd/rim/v4/ValueType.java
@@ -38,7 +38,7 @@ import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import com.raytheon.uf.common.dataplugin.persist.IPersistableDataObject;
-import com.raytheon.uf.common.registry.RegrepUtil;
+import com.raytheon.uf.common.registry.schemas.ebxml.util.RegrepUtil;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 
 /**
