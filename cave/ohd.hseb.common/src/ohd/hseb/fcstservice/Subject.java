1d59bbc93faf4004c71a613dc1982c02270daff0
Steve Harris <Steven_L_Harris@raytheon.com>
2014-08-21 11:04:08 -0500

    Omaha #3368 - Move ohd plugin to new repository
    Former-commit-id: aec275dc31baaaa238c31517db7f0bcb5f26faea [formerly 8c467a9fa599edfaa20f74d3147d5dda8e021c5c] [formerly aec275dc31baaaa238c31517db7f0bcb5f26faea [formerly 8c467a9fa599edfaa20f74d3147d5dda8e021c5c] [formerly 28e88dfe7e14f64f4535d4a478cd000ba5a4b795 [formerly 56dfed379b0e41c435a3226c3f02b9bc5232b4fd]]]
    Former-commit-id: 28e88dfe7e14f64f4535d4a478cd000ba5a4b795
    Former-commit-id: 7542fda9a7c9fdd1aee9f25447ace06a76688ab4 [formerly 675dbe0c212a9a5e880dcc1e64fa7748c6d1adc4]
    Former-commit-id: b5f3fe44ed746232d2d6047fc50d1644a64883f3

--- cave/ohd.hseb.common/src/ohd/hseb/fcstservice/Subject.java
+++
@@ -1,8 +0,0 @@
-package ohd.hseb.fcstservice;   
-
-public interface Subject //Subject of the observer pattern
-{
-	public void addObserver(Observer observer);
-	public void removeObserver(Observer observer);
-	public void letKnowOfChanges();
-}
