fca0b35305a3464a6164d2a538ef1f3cb309d5cd
Chris Cody <Chris.Cody@raytheon.com>
2014-09-02 09:34:06 -0500

    Omaha #3365 Removing and realigning AWIPS2 Baseline tests Change-Id: Iba40d0b074df500c52b7302ab59a688680153f48
    Former-commit-id: b3954d113954f2434621eed61fd4786716ab6245 [formerly bdd4697486880c39b9283cdfd8e044db17b4e4fc] [formerly bdd4697486880c39b9283cdfd8e044db17b4e4fc [formerly 9e823a47d6122d5bbeb532d16537c67772766799]] [formerly f4445d24cd9db459b2eb8b578b97279d6d490651 [formerly f4445d24cd9db459b2eb8b578b97279d6d490651 [formerly 97000cc70e8d826b3e8828a1731278673e0ac635]]]
    Former-commit-id: f4445d24cd9db459b2eb8b578b97279d6d490651
    Former-commit-id: b26d786c7c2e99a1e3dc49c7e30eaae3f0790085 [formerly 39887a4b9c977182e345c3c396b8f4e81893ba96]
    Former-commit-id: d101ae7020a9e4e4aae0be38bdb52a3c127ee129

--- tests/integration/com/raytheon/uf/edex/registry/ebxml/dao/PluginSubscribedListener.java
+++ tests/integration/com/raytheon/uf/edex/registry/ebxml/dao/PluginSubscribedListener.java
@@ -29,7 +29,6 @@ import oasis.names.tc.ebxml.regrep.xsd.rim.v4.NotificationType;
 import oasis.names.tc.ebxml.regrep.xsd.rs.v4.RegistryResponseType;
 
 import org.junit.Ignore;
-
 import com.raytheon.uf.common.registry.schemas.ebxml.util.EbxmlNamespaces;
 
 /**
