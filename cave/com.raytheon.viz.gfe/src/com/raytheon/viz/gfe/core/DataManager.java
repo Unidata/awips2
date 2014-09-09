57a354203839e0ed34757afafdc9eceee5327dea
Ron Anderson <ron.anderson@raytheon.com>
2014-09-09 11:04:25 -0500

    Issue #3592 Fixed several localization issue in GFE
    Change-Id: I2e65649d490372b5c1738f07c79b5ed15885f8c7
    
    Former-commit-id: b91141ef7b66fc3cfd1756fa766ef3a5e5dc069b [formerly a5ef55df33009948fec139e7729dd53ae4251667]
    Former-commit-id: 52e5636261f96ece57357b5bb53f873e93823cc0

--- cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/DataManager.java
+++ cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/DataManager.java
@@ -100,6 +100,7 @@ import com.raytheon.viz.gfe.textformatter.TextProductManager;
  * 09/05/2013    2307      dgilling    Use better PythonScript constructor.
  * 09/16/2013    2033      dgilling    Remove unused IToolController.
  * 12/09/2013    2367      dgilling    Instantiate ProcedureJobPool here.
+ * 09/09/2014    3592      randerso    Added call to SampleSetManager.dispose()
  * 
  * </pre>
  * 
@@ -290,6 +291,7 @@ public class DataManager {
      * {@link DataManagerFactory#dispose(Object)}
      */
     void dispose() {
+        sampleSetManager.dispose();
         selectTimeRangeManager.dispose();
         refManager.dispose();
         parmManager.dispose();
