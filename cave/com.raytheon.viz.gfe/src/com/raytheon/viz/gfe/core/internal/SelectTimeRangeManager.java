be55fa498409ceeedf81943612620677a2c776f9
Ron Anderson <ron.anderson@raytheon.com>
2014-09-09 11:04:25 -0500

    Issue #3592 Fixed several localization issue in GFE
    Change-Id: I2e65649d490372b5c1738f07c79b5ed15885f8c7
    
    Former-commit-id: 52e5636261f96ece57357b5bb53f873e93823cc0 [formerly 52e5636261f96ece57357b5bb53f873e93823cc0 [formerly a5ef55df33009948fec139e7729dd53ae4251667]]
    Former-commit-id: b91141ef7b66fc3cfd1756fa766ef3a5e5dc069b
    Former-commit-id: c23122797d424faef7356eeeb6c8623b0c3b6083

--- cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/internal/SelectTimeRangeManager.java
+++ cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/core/internal/SelectTimeRangeManager.java
@@ -22,15 +22,15 @@ package com.raytheon.viz.gfe.core.internal;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.TreeMap;
 
 import com.raytheon.uf.common.dataplugin.gfe.time.SelectTimeRange;
 import com.raytheon.uf.common.dataplugin.gfe.time.SelectTimeRange.Mode;
@@ -62,6 +62,7 @@ import com.raytheon.viz.gfe.core.msgs.SelectTimeRangesChangedMsg;
  * Dec 3, 2009       #3135 randerso    Initial creation
  * Aug 1, 2012       #965  dgilling    Change location of SelectTimeRange.
  * Aug 6, 2013       #1561 njensen     Use pm.listFiles() instead of pm.listStaticFiles()
+ * Sep 8, 2104       #3592 randerso    Changed to use new pm listStaticFiles()
  * 
  * </pre>
  * 
@@ -78,23 +79,17 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
 
     private static final String FILE_EXT = ".SELECTTR";
 
-    private static final IPathManager pathManager = PathManagerFactory
-            .getPathManager();
-
     private static final TimeZone ZULU = TimeZone.getTimeZone("GMT");
 
-    protected DataManager dataManager;
+    private IPathManager pathManager;
 
     private TimeZone timeZone;
-
-    private String[] inventory;
 
     private Map<String, SelectTimeRange> rangeMap;
 
     private LocalizationFile selectTRDir;
 
     public SelectTimeRangeManager(DataManager dataManager) {
-        this.dataManager = dataManager;
         // Find the referenced time zone
         timeZone = TimeZone.getTimeZone("GMT");
         try {
@@ -105,13 +100,13 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
                     "Unable to retrieve GFE time zone, using GMT", e);
         }
 
-        IPathManager pathMgr = PathManagerFactory.getPathManager();
-        selectTRDir = pathMgr.getLocalizationFile(pathMgr.getContext(
+        this.pathManager = PathManagerFactory.getPathManager();
+        selectTRDir = pathManager.getLocalizationFile(pathManager.getContext(
                 LocalizationType.COMMON_STATIC, LocalizationLevel.BASE),
                 FILE_PATH);
         selectTRDir.addFileUpdatedObserver(this);
 
-        fileUpdated(null);
+        loadInventory();
     }
 
     @Override
@@ -123,15 +118,17 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
 
     @Override
     public String[] inventory() {
+        String[] inventory = this.rangeMap.keySet().toArray(
+                new String[this.rangeMap.size()]);
         return inventory;
     }
 
     private SelectTimeRange loadTimeRange(LocalizationFile lf) {
-        File file = lf.getFile();
+        String rangeName = rangeNameFromFileName(lf.getName());
 
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(file));
+            in = new BufferedReader(new InputStreamReader(lf.openInputStream()));
             String[] s = in.readLine().split("\\s+");
             int start = Integer.parseInt(s[0]);
             int end = Integer.parseInt(s[1]);
@@ -140,21 +137,22 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
                 mode = Mode.values()[Integer.parseInt(s[2])];
             }
 
-            SelectTimeRange range = new SelectTimeRange(FileUtil.unmangle(file
-                    .getName().replace(FILE_EXT, "")), start, end, mode, lf
-                    .getContext().getLocalizationLevel(),
+            SelectTimeRange range = new SelectTimeRange(rangeName, start, end,
+                    mode, lf.getContext().getLocalizationLevel(),
                     (mode.equals(Mode.ZULU) ? ZULU : timeZone));
             return range;
         } catch (Exception e) {
             statusHandler.handle(Priority.PROBLEM,
-                    "Error reading SELECTTR file " + file.getAbsolutePath(), e);
+                    "Error reading SELECTTR file "
+                            + lf.getFile().getAbsolutePath(), e);
         } finally {
             if (in != null) {
                 try {
                     in.close();
                 } catch (IOException e) {
                     statusHandler.handle(Priority.PROBLEM,
-                            "Error closing file " + file.getAbsolutePath(), e);
+                            "Error closing file "
+                                    + lf.getFile().getAbsolutePath(), e);
                 }
             }
         }
@@ -173,22 +171,26 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
                         LocalizationLevel.USER), FileUtil.join(FILE_PATH,
                 FileUtil.mangle(name) + FILE_EXT));
 
-        File file = lf.getFile();
         BufferedWriter out = null;
         try {
-            out = new BufferedWriter(new FileWriter(file));
+            out = new BufferedWriter(new OutputStreamWriter(
+                    lf.openOutputStream()));
             out.write(String.format("%d %d %d", start, end, mode.ordinal()));
             out.close();
+            out = null;
             lf.save();
         } catch (Exception e) {
             statusHandler.handle(Priority.PROBLEM,
-                    "Error writing SELECTTR file " + file.getAbsolutePath(), e);
+                    "Error writing SELECTTR file "
+                            + lf.getFile().getAbsolutePath(), e);
         } finally {
             try {
-                out.close();
+                if (out != null) {
+                    out.close();
+                }
             } catch (IOException e) {
                 statusHandler.handle(Priority.PROBLEM, "Error closing file "
-                        + file.getAbsolutePath(), e);
+                        + lf.getFile().getAbsolutePath(), e);
             }
         }
     }
@@ -210,9 +212,48 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
 
     @Override
     public void fileUpdated(FileUpdatedMessage message) {
-        LocalizationFile[] files = pathManager.listFiles(pathManager
-                .getLocalSearchHierarchy(LocalizationType.COMMON_STATIC),
-                FILE_PATH, new String[] { FILE_EXT }, false, true);
+        SelectTimeRange range;
+
+        switch (message.getChangeType()) {
+        case ADDED:
+        case UPDATED:
+            LocalizationFile lf = pathManager.getLocalizationFile(
+                    message.getContext(), message.getFileName());
+            range = loadTimeRange(lf);
+            if (range != null) {
+                this.rangeMap.put(range.getName(), range);
+            }
+            break;
+
+        case DELETED:
+            LocalizationFile[] files = pathManager.listStaticFiles(
+                    LocalizationType.COMMON_STATIC, message.getFileName(),
+                    new String[] { FILE_EXT }, false, true);
+
+            if (files.length > 0) {
+                range = loadTimeRange(files[0]); // should only be one
+                if (range != null) {
+                    this.rangeMap.put(range.getName(), range);
+                }
+            } else {
+                String rangeName = rangeNameFromFileName(message.getFileName());
+                this.rangeMap.remove(rangeName);
+            }
+            break;
+
+        default:
+            statusHandler.error("Unexpected FileChangeType received: "
+                    + message.getChangeType().name());
+            break;
+        }
+
+        new SelectTimeRangesChangedMsg().send();
+    }
+
+    private void loadInventory() {
+        LocalizationFile[] files = pathManager.listStaticFiles(
+                LocalizationType.COMMON_STATIC, FILE_PATH,
+                new String[] { FILE_EXT }, false, true);
 
         List<SelectTimeRange> ranges = new ArrayList<SelectTimeRange>(
                 files.length);
@@ -224,19 +265,22 @@ public class SelectTimeRangeManager implements ISelectTimeRangeManager,
         }
         Collections.sort(ranges);
 
-        String[] inv = new String[ranges.size()];
-        Map<String, SelectTimeRange> map = new HashMap<String, SelectTimeRange>();
-        int i = 0;
+        Map<String, SelectTimeRange> map = new TreeMap<String, SelectTimeRange>();
         for (SelectTimeRange range : ranges) {
-            inv[i++] = range.getName();
             map.put(range.getName(), range);
         }
 
         synchronized (this) {
-            this.inventory = inv;
             this.rangeMap = map;
         }
 
         new SelectTimeRangesChangedMsg().send();
+    }
+
+    private String rangeNameFromFileName(String fileName) {
+        File file = new File(fileName);
+        String rangeName = FileUtil.unmangle(file.getName().replace(FILE_EXT,
+                ""));
+        return rangeName;
     }
 }
