78bd28461dee400a63de65ead97074d7e2d45344
David Friedman <David.Friedman@noaa.gov>
2014-09-10 19:25:55 +0000

    ASM #16730 - WarnGen Watch wording error when issuing product only over Marine Locations
    Change-Id: I361a1635cf11f3c66801f33c74034028b3e0271d
    
    Former-commit-id: 4f98ee09de29502e3a1ef7e03556675632bf0f68 [formerly bed361fe355ad1b2a4f2ce1ad421808fc275f932] [formerly 8d7ba120a299b6849467ccfac99e260830ccc2ff [formerly 17d27fca04fb733d74e5bac99bfed3112bd81b6c]]
    Former-commit-id: 8d7ba120a299b6849467ccfac99e260830ccc2ff
    Former-commit-id: 94d78ed0107477beab29661d42b0346553b928a6

--- cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/gui/WarngenLayer.java
+++ cave/com.raytheon.viz.warngen/src/com/raytheon/viz/warngen/gui/WarngenLayer.java
@@ -213,6 +213,7 @@ import com.vividsolutions.jts.io.WKTReader;
  * 07/28/2014  DR 17475    Qinglu Lin  Updated populateStrings() and findLargestQuadrant(), removed findLargestGeometry(), 
  *                                     added createAreaAndCentroidMaps() and movePopulatePt(), updated paintText() to center W.
  * 08/20/2014  ASM #16703  D. Friedman Make geo feature types for watches explicit
+ * 09/10/2014  ASM #16730  D. Friedman Fix NPE.
  * </pre>
  * 
  * @author mschenke
@@ -340,9 +341,11 @@ public class WarngenLayer extends AbstractStormTrackResource {
 
         private Set<String> getAllFipsInArea(Geometry warningArea) {
             Set<String> fipsIds = new HashSet<String>();
-            for (int n = 0; n < warningArea.getNumGeometries(); ++n) {
-                Geometry area = warningArea.getGeometryN(n);
-                fipsIds.add(getFips(area));
+            if (warningArea != null) {
+                for (int n = 0; n < warningArea.getNumGeometries(); ++n) {
+                    Geometry area = warningArea.getGeometryN(n);
+                    fipsIds.add(getFips(area));
+                }
             }
             return fipsIds;
         }
