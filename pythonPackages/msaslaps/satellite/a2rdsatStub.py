8f18ed535e2ddb7cb79b9d54a66032b518cf6a24
Nathan Bowler <Nathan.A.Bowler@raytheon.com>
2014-10-01 14:54:50 -0400

    Omaha #3596 Update scripts to use DAF. Always put creatingEntity in returned attributes.
    Change-Id: I456ff499ccf9446622d1535323afc0d0d1f9b70d
    
    Former-commit-id: 2d5d90c055731943437b03f5a49103dbaefc5e22 [formerly 368b6341b0804bd72d8a64b1a0c2b72addafe77b] [formerly 2d5d90c055731943437b03f5a49103dbaefc5e22 [formerly 368b6341b0804bd72d8a64b1a0c2b72addafe77b] [formerly e4779f2e666efe8d7d733eaceb4bef83194ddf49 [formerly d67d1e941b89357e7d86fa0f810a82f9e01f4c27]]]
    Former-commit-id: e4779f2e666efe8d7d733eaceb4bef83194ddf49
    Former-commit-id: b52c1bcafd584ddb524f823bdd05c3cc03f5b236 [formerly a72fec1b3870f8a169337ba5de20a1fbb701a29b]
    Former-commit-id: ae5bde3c3d6fc05d4820c5ed493c16270489954b

--- pythonPackages/msaslaps/satellite/a2rdsatStub.py
+++ pythonPackages/msaslaps/satellite/a2rdsatStub.py
@@ -1,59 +1,138 @@
-import BaseRequest
-from com.raytheon.uf.common.message.response import ResponseMessageGeneric
-from com.raytheon.edex.plugin.satellite.dao import SatelliteDao
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+#
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+#
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+#
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
 
-# Perform a satellite request for data of interest
-sr = BaseRequest.BaseRequest("satellite")
-sr.addParameter("creatingEntity","EEEEE","=")
-sr.addParameter("sectorID","SSSSS","=")
-sr.addParameter("physicalElement","CCCCC","=")
-sr.addParameter("dataTime","AAAAA",">=")
-sr.addParameter("dataTime","BBBBB","<=")
+# Gets data for a single satellite sector from the A-II database. The result is
+# output to stdout as ASCII. The first line returned has the dimensions of the
+# image, the time, and the source satellite of the data set returned. The rest
+# is one line per row of satellite data.  The data for each row undergoes second
+# order compression Each pixel value of 0 or 255 is encoded as @ or #,
+# respectively. Otherwise the first pixel on the row and any pixel that is more
+# than 20 counts  different than the previous one is encoded as two hex digits.
+# Pixels the same as the previous are encoded as a period, pixels from 1 to 20
+# counts less than the previous are encoded as G through Z, and pixels from 1 to
+# 20 counts more than the previous are encoded as g through z. There are no
+# delimeters between the encoding for each pixel.
+#
+#     SOFTWARE HISTORY
+#
+#    Date            Ticket#       Engineer       Description
+#    ------------    ----------    -----------    --------------------------
+#    Sep 29, 2014    3596          nabowle        Initial modification. Replace UEngine with DAF.
+#
+#
 
-result = sr.execute()
-size = result.size()
-if size == 0:
-   return ResponseMessageGeneric("Data not available")
+import a2dafcommon
+import argparse
+import sys
 
-# ResponseMessageGeneric.  Payload is SatelliteRecord
-rmg = result.get(0)
-part = "PPPPP"
-encoding = XXXXX
+from datetime import datetime
+from datetime import timedelta
+from ufpy.dataaccess import DataAccessLayer
+from dynamicserialize.dstypes.com.raytheon.uf.common.time import TimeRange
 
-# SatelliteRecord
-srec = rmg.getContents()
-mytime = srec.getDataURI().split('/',4)[2]
-myent = srec.getCreatingEntity()
+def get_args():
+    parser = argparse.ArgumentParser(conflict_handler="resolve")
+    parser.add_argument("-h", action="store", dest="host",
+                    help="EDEX server hostname (optional)",
+                    metavar="hostname")
+    parser.add_argument("--datetime", action="store", dest="datetime", 
+                    help="The start of the time range in YYYY-MM-DD HH:MM",
+                    metavar="datetime")
+    parser.add_argument("--slop", action="store", dest="slop", default=180,
+                    help="The amount of slop, in seconds, to allow around the datetime.",
+                    metavar="slop",  type=int)
+    parser.add_argument("--sector", action="store", dest="sector", 
+                    help="The sector ID.", metavar="sectorID")
+    parser.add_argument("--physical", action="store", dest="physical", 
+                    help="The physical element.", metavar="physicalElement")
+    parser.add_argument("--entity", action="store", dest="entity", 
+                    help="The creating entity (optional)",
+                    metavar="creatingEntity")
+    parser.add_argument("--partition", action="store", dest="partition", 
+                    help="Upper case letter indicating partition to get.",
+                    metavar="partition", default="0")
+    parser.add_argument("--hex",  action='store_const', dest="encoding",
+                    const=1, help="Hex encoding.", metavar="encoding")
+    parser.add_argument("--int",  action='store_const', dest="encoding",
+                    const=0, help="Delimited integer encoding.",
+                    metavar="encoding")
+    return parser.parse_args()
 
-# SatelliteDao.  Inherits from PluginDao, which has a getHDF5Data method,
-# which takes a PluginDataObject as an arg.
-satdao = SatelliteDao("satellite")
 
-# returns IDataRecord[].  IDataRecord is implemented by only one class --
-# AbstractStorageRecord.  ASR is extended by a few *DataRecord classes; one
-# of them is ByteDataRecord
-idra = satdao.getHDF5Data(srec,-1)
+def main():
+    user_args = get_args()
 
-msg = "No data."
-if len(idra) > 0:
-    # pick an arbitrary datarecord
-    idr = idra[0]
+    if user_args.host:
+        DataAccessLayer.changeEDEXHost(user_args.host)
 
-    # this hints at the IDR's concrete class:  ByteDataRecord
-    #print "true type of IDataRecord:", idr.getDataObject().toString()
+    slop = user_args.slop
 
-    dim = idr.getDimension()
-    if dim != 2:
-        return ResponseMessageGeneric(msg)
+    dateTimeStr = user_args.datetime
+    if not dateTimeStr:
+        print >> sys.stderr, "DateTime not provided"
+        return
 
-    xLen = idr.getSizes()[0]
-    yLen = idr.getSizes()[1]
+    physicalElement = user_args.physical
+    if not physicalElement:
+        print >> sys.stderr, "PhysicalElement not provided"
+        return
 
-    # byte[] -- the raw data
-    barray = idr.getByteData()
-    barraySize = len(barray)
-    if xLen * yLen != barraySize:
-        return ResponseMessageGeneric(msg)
+    sectorID = user_args.sector
+    if not sectorID:
+        print >> sys.stderr, "SectorID not provided"
+        return
+
+    creatingEntity = user_args.entity
+    part = user_args.partition
+    encoding = user_args.encoding
+
+    dateTime = datetime.strptime(dateTimeStr, "%Y-%m-%d %H:%M")
+    beginRange = dateTime - timedelta(0, slop)
+    endRange = dateTime + timedelta(0, slop)
+
+    timerange = TimeRange(beginRange, endRange)
+
+    req = DataAccessLayer.newDataRequest("satellite")
+    req.setParameters(physicalElement)
+    req.setLocationNames(sectorID)
+
+    if creatingEntity:
+        req.addIdentifier("creatingEntity", creatingEntity)
+
+    grids = DataAccessLayer.getGridData(req, timerange)
+
+    if not grids:
+#        print "Data not available"
+        return
+
+    grid = grids[0]
+    data = grid.getRawData()
+    myent = grid.getAttribute("creatingEntity")
+    mytime = a2dafcommon.datatime_to_string(grid.getDataTime()) + ".0"
+
+    if data is None or len(data) == 0:
+#        print "No data."
+        return
+
+    yLen = len(data[0])
+    xLen = len(data)
 
     plus  = " ghijklmnopqrstuvwxyz"
     minus = " GHIJKLMNOPQRSTUVWXYZ"
@@ -77,48 +156,72 @@ if len(idra) > 0:
     elif part=="A" or nxy>limit :
        nxy = k
 
-    msg = "\n"
+    msg = ""
     if part<="A" :
        msg += str(xLen) + " " + str(yLen) + " "
        msg += mytime + " " + myent + "\n"
 
+    dataIdx = int(j / yLen)
+    arrIdx = j % yLen
+    arr = data[dataIdx]
+    i = 0
+    kk = None
     while j<nxy :
-        i = 0
-        kk = barray[i+j]
+        i=0
+        kk = int(arr[arrIdx])
         if kk<0 : kk += 256
         if encoding == 0 :
-           msg += str(kk)
+            msg += str(kk)
         elif encoding == 1 :
-           msg += "%2.2x"%kk
+            msg += "%2.2x"%kk
         elif kk == 0 :
-           msg += "@"
+            msg += "@"
         elif kk == 255 :
-           msg += "#"
+            msg += "#"
         else :
-           msg += "%2.2x"%kk
-        i += 1
+            msg += "%2.2x"%kk
+        i+=1
+        arrIdx+=1
+        if arrIdx >= yLen :
+            arrIdx = 0
+            dataIdx += 1
+            if  dataIdx >= xLen:
+                break
+            arr = data[dataIdx]
         while i<xLen :
-           k = barray[i+j]
-           if k<0 : k += 256
-           if encoding == 0 :
+            k = int(arr[arrIdx])
+            if k<0 : k += 256
+            if encoding == 0 :
               msg += " "+str(k)
-           elif encoding == 1 :
+            elif encoding == 1 :
               msg += "%2.2x"%k
-           elif k==0 :
+            elif k==0 :
               msg += "@"
-           elif k == 255 :
+            elif k == 255 :
               msg += "#"
-           elif k==kk :
+            elif k==kk :
               msg += "."
-           elif k>kk+20 or k<kk-20 :
+            elif k>kk+20 or k<kk-20 :
               msg += "%2.2x"%k
-           elif k>kk :
+            elif k>kk :
               msg += plus[k-kk]
-           else :
+            else :
               msg += minus[kk-k]
-           kk = k
-           i += 1
-        msg += "\n"
-        j += xLen
+            kk = k
 
-return ResponseMessageGeneric(msg)
+            arrIdx+=1
+            i+=1
+            if arrIdx >= yLen :
+                arrIdx = 0
+                dataIdx += 1
+                if  dataIdx >= xLen:
+                    break
+                arr = data[dataIdx]
+            
+        msg += "\n"
+        j+= xLen
+
+    print msg.strip()
+
+if __name__ == '__main__':
+    main()
