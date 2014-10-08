c8133ac38f01d9a48df820b5401b3b2f851d9a3a
Ron Anderson <ron.anderson@raytheon.com>
2014-10-08 18:05:27 -0500

    Issue #3684 Fix some issues found after #3684 with certain GFE configurations
    Change-Id: I92b45588d9d4fe9169708c07119767017426d468
    
    Former-commit-id: a64a36d18267aded2aa40d64290add354ec8dfc4 [formerly 8225700629914cd234ab758b746e61a77e7d0139 [formerly 4a1a3aef82e1ce6900b7e213ca1cc59e2b27a83e] [formerly a64a36d18267aded2aa40d64290add354ec8dfc4 [formerly 6dc55b51414414aa416c9ad0c688d1d34ba08c17]]]
    Former-commit-id: 8225700629914cd234ab758b746e61a77e7d0139 [formerly 4a1a3aef82e1ce6900b7e213ca1cc59e2b27a83e]
    Former-commit-id: 8225700629914cd234ab758b746e61a77e7d0139
    Former-commit-id: 0463c1c7a266cbd9f43502ca9f8d4143f8d8de2f

--- edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/util/GfeUtil.java
+++ edexOsgi/com.raytheon.uf.common.dataplugin.gfe/src/com/raytheon/uf/common/dataplugin/gfe/util/GfeUtil.java
@@ -67,6 +67,7 @@ import com.vividsolutions.jts.operation.polygonize.Polygonizer;
  *                                     favor of new GridLocation constructor
  * 06/24/13     #2044      randerso    Changed format of hdf5 group to include 
  *                                     minutes for satellite data
+ * 10/08/14     #3684      randerso    Removed NOTIFY
  * 
  * </pre>
  * 
@@ -104,8 +105,6 @@ public class GfeUtil {
     public static final String KEY_SEPARATOR = "|";
 
     public static final String HAZARDS_KEY = "Hazards";
-
-    public static final String NOTIFY = "gfeNotify";
 
     private static Pattern DISCRETE_PATTERN = Pattern
             .compile("'[\\w<>+/^\\[\\]\\.,:-]*'");
