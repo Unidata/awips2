e8d1ce9c415d4d3e4383e74c6497f9c2589387f8
Mark Peters <Mark.A.Peters@raytheon.com>
2014-10-16 10:28:34 -0500

    Omaha #3709 Collaboration chat windows need to support changing foreground and background color
    Change-Id: I1f208c3e12e0fe989ae395423ad28454be3589f7
    
    Former-commit-id: 5bbbe31916866892bdba3cabfe43f51816a1762d [formerly 5a6f3ff09c8217845e6e3dbc79c53f813ac6009d] [formerly ec62b9a6f13388cdf1ecfa5a093fda1d0a685f70] [formerly ec62b9a6f13388cdf1ecfa5a093fda1d0a685f70 [formerly 54a3e8d2eb4366fa992cbeaa0bf23bae8aa3da6f]] [formerly 5bbbe31916866892bdba3cabfe43f51816a1762d [formerly 5a6f3ff09c8217845e6e3dbc79c53f813ac6009d] [formerly ec62b9a6f13388cdf1ecfa5a093fda1d0a685f70] [formerly ec62b9a6f13388cdf1ecfa5a093fda1d0a685f70 [formerly 54a3e8d2eb4366fa992cbeaa0bf23bae8aa3da6f]] [formerly e002132009eb04e519f3ae25f529bd2a8e607289 [formerly ec62b9a6f13388cdf1ecfa5a093fda1d0a685f70 [formerly 54a3e8d2eb4366fa992cbeaa0bf23bae8aa3da6f] [formerly e002132009eb04e519f3ae25f529bd2a8e607289 [formerly 8e00cd23205c4ab3f83c5594373f3736afe7fe83]]]]]
    Former-commit-id: e002132009eb04e519f3ae25f529bd2a8e607289
    Former-commit-id: a7309c9d2b9e8adec58ae5f25c0e1125ebba1586 [formerly 7502a7d0952f61d89d96e1005ab233ac91973917] [formerly db58207c6db925a462bd000cbbfd4c08ba6684bf] [formerly ce57d54fed9ba3d0d2806e58c8e0f542b3591594 [formerly e3b623b44a7ae728bf706820577b5598655114ce] [formerly db58207c6db925a462bd000cbbfd4c08ba6684bf [formerly 5fc9bdec3b220279bfdd556c09284f8c794f9f72]]]
    Former-commit-id: 3b65b0f40d1cf4a4fcb7b3345e0e1a81115ba959 [formerly d3c168634bee505091e42265b3383c4c55f4428e] [formerly 91eedeee506f56a01633aa7552f066384d11d347 [formerly 4b8da0d85961ea97c80cdb85d151e5774160aa7e]]
    Former-commit-id: 91eedeee506f56a01633aa7552f066384d11d347
    Former-commit-id: a026cdaf387c61141e882bbb30fb9c0e58d8a475

---
+++ cave/com.raytheon.uf.viz.collaboration.ui/src/com/raytheon/uf/viz/collaboration/ui/actions/ChatDisplayChangeEvent.java
@@ -0,0 +1,84 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.collaboration.ui.actions;
+
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.graphics.RGB;
+
+/**
+ * Store font/color change information
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#     Engineer    Description
+ * ------------ ----------  ----------- --------------------------
+ * Oct 14, 2014 3709        mapeters    Initial creation.
+ * 
+ * </pre>
+ * 
+ * @author mapeters
+ * @version 1.0
+ */
+
+public class ChatDisplayChangeEvent {
+
+    private ChangeType type;
+
+    private RGB color;
+
+    private FontData font;
+
+    public enum ChangeType {
+        BACKGROUND, FOREGROUND, FONT;
+    }
+
+    private ChatDisplayChangeEvent(ChangeType type, RGB color) {
+        this.type = type;
+        this.color = color;
+    }
+
+    private ChatDisplayChangeEvent(ChangeType type, FontData font) {
+        this.type = type;
+        this.font = font;
+    }
+
+    public static ChatDisplayChangeEvent createColorEvent(ChangeType type,
+            RGB color) {
+        return new ChatDisplayChangeEvent(type, color);
+    }
+
+    public static ChatDisplayChangeEvent createFontEvent(FontData font) {
+        return new ChatDisplayChangeEvent(ChangeType.FONT, font);
+    }
+
+    public RGB getColor() {
+        return this.color;
+    }
+
+    public ChangeType getChangeType() {
+        return this.type;
+    }
+
+    public FontData getFont() {
+        return this.font;
+    }
+}
