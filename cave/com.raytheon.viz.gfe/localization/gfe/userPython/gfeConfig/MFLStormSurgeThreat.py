e49972bfcdeac87ec82af9d05ced7300143a4fb1
Ana Rivera <ana.rivera@noaa.gov>
2014-11-14 16:07:36 +0000

    VLab Issue #5318 - Baseline Hurricane Threats and Impacts (O&M RM16791); fixes #5318
    Change-Id: Ibbc9e39a294417524c57c86e68e92644433ba847
    
    Former-commit-id: 499c58a2534dad60efe0b8b38e77d997ea6f23cc [formerly 061fd0af289d49ed04615734cde6b4d48af0e60d] [formerly e54f3305d8b0d8af7199dd19f971b640126818a5 [formerly 8a4e53a23a222dc88e3db8cc053fc31c33d3ed42]]
    Former-commit-id: e54f3305d8b0d8af7199dd19f971b640126818a5
    Former-commit-id: 23b3f1bc77a99a153d5fcb0690acaf11e03737a4

---
+++ cave/com.raytheon.viz.gfe/localization/gfe/userPython/gfeConfig/MFLStormSurgeThreat.py
@@ -0,0 +1,93 @@
+# ----------------------------------------------------------------------------
+# This software is in the public domain, furnished "as is", without technical
+# support, and with no warranty, express or implied, as to its usefulness for
+# any purpose.
+# 
+# MFLStormSurgeThreat
+#
+# Author: M. Volkmer/P. Santos
+# ----------------------------------------------------------------------------
+
+from gfeConfig import *
+#HideConfigFile = 1
+#------------------------------------------------------------------------
+# GIF/PNG Graphic Product Generation (ifpIMAGE program)
+#------------------------------------------------------------------------
+
+# IMAGE SIZE
+# Both default to 400
+#Png_height    = 720
+Png_width     = 492
+
+#NAME OF ELEMENT DISPLAYED AS IMAGE
+Png_image = 'StormSurgeThreat'
+
+Png_parms = ['StormSurgeThreat_SFC:_Official -1']
+StormSurgeThreat_spatialImageType = ["Image"]
+
+# Smoothing...
+#Png_smoothImage = 1
+
+# NWS Logos
+Png_logo = 1
+Png_logoString = "Storm Surge Threat - NWS Miami FL"
+
+# Turn off Color Bar
+Png_omitColorBar = 0
+
+# COLOR TABLES
+StormSurgeThreat_defaultColorTable='GFE/gHLS_new'
+#LocalSurgeThreat_maxColorTableValue = 6.0 
+#LocalSurgeThreat_minColorTableValue = 0.0 
+ImageLegend_color = 'black'
+bgColor = 'white'
+# Counties_graphicColor = 'grey'
+Counties_MFL_graphicColor = 'black'
+Zones_MFL_graphicColor = 'black'
+Marine_Zones_MFL_graphicColor = 'black'
+Interstates_graphicColor = 'blue'
+States_graphicColor = 'gray'
+Lakes_graphicColor = 'blue'
+
+
+# MAP BACKGROUNDS
+# MapBackgrounds_default = ['Counties','Marine_Zones_MFL','Interstates']
+MapBackgrounds_default = ['Zones_MFL','Marine_Zones_MFL','Interstates','States','Lakes']
+MFL_mask = "StormSurgeWW_EditArea"
+
+DefaultSamples = ['MFLStormSurgeThreat']
+# Customize FONT SIZES here
+#TextFont0 =  "Bitstream Vera Sans Mono-bold-10"
+#TextFont1 =  "Bitstream Vera Sans Mono-bold-10"
+#TextFont2 =  "Bitstream Vera Sans Mono-bold-11"
+#TextFont3 =  "Bitstream Vera Sans Mono-bold-10"
+#TextFont4 =  "Bitstream Vera Sans Mono-bold-10"
+
+SESample_font = 1
+SELegend_font = 2
+SEColorBar_fgTextColor = "black"
+
+#CONFIGURATION FILE SETTINGS
+Png_filenamePrefix = 'MFLStormSurgeThreat' 
+Png_baseTimeFormat = ''
+
+#LEGENDS
+# Ability to turn on/off legends for the graphic generation.  Applies
+# only to graphic product generation and not GFE.  Defaults to on
+# if not specified.  Do not include a decimal point after the number.
+Png_legend = 1   #1 for visible, 0 for invisible
+
+# Legends display mode - 0 for UTC, 1 for local time
+# Do not include a decimal point after the number.
+Png_localTime = 1 # legend displays time in local or UTC (default to UTC)
+
+# Legend weather element name mode - 0 for weather element name,
+# 1 for weather element descriptive name
+#Png_descriptiveWeName = 1
+Png_descriptiveWeName = "ALT"
+Png_StormSurgeThreat_AltName="Updated"
+Png_legendFormat_LT_dur = ""
+#Png_legendFormat_LT_start = "%A %b %d"
+Png_legendFormat_LT_start = "%I %p %a %h %d"
+# Png_legendFormat_LT_start = ""
+Png_legendFormat_LT_end = ""
