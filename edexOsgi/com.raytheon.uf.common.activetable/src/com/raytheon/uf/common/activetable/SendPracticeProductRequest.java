f88fc4547055f2bfed5e299f87812985bf3ac023
Ron Anderson <ron.anderson@raytheon.com>
2014-11-14 14:03:00 -0600

    VLab Issue #4953 Added support for managing JSON files in practice mode.
    Change-Id: I081a11bd2ed9370c32354a0c5728400561f3d7f8
    
    Former-commit-id: 91040234087f23f81c132e4263bce99cdd2fc221 [formerly e8ac81c5850b8565ea1e375857cf9dfe531848a0]
    Former-commit-id: bb451e58c3dc21b452f37db607e62c1555f59b50

--- edexOsgi/com.raytheon.uf.common.activetable/src/com/raytheon/uf/common/activetable/SendPracticeProductRequest.java
+++ edexOsgi/com.raytheon.uf.common.activetable/src/com/raytheon/uf/common/activetable/SendPracticeProductRequest.java
@@ -19,38 +19,102 @@
  **/
 package com.raytheon.uf.common.activetable;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.raytheon.uf.common.message.IMessage;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 import com.raytheon.uf.common.serialization.comm.IServerRequest;
 
 /**
- * TODO Add Description
+ * A SendPracticeProductRequest with an optional offset time string.
  * 
  * <pre>
  * 
  * SOFTWARE HISTORY
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
- * Feb 10, 2010            njensen     Initial creation
+ * Jun 27, 2011            wldougher   Initial creation
+ * Nov 14, 2014  4953      randerso    Merged PracticeProductOfftimeRequest with this
  * 
  * </pre>
  * 
- * @author njensen
+ * @author wldougher
  * @version 1.0
  */
 
 @DynamicSerialize
-public class SendPracticeProductRequest implements IServerRequest {
+public class SendPracticeProductRequest implements IServerRequest, IMessage {
 
     @DynamicSerializeElement
     private String productText;
 
+    @DynamicSerializeElement
+    private String drtString;
+
+    @DynamicSerializeElement
+    private boolean notifyGFE;
+
+    /**
+     * @return the productText
+     */
     public String getProductText() {
         return productText;
     }
 
+    /**
+     * @param productText
+     *            the productText to set
+     */
     public void setProductText(String productText) {
         this.productText = productText;
+    }
+
+    /**
+     * @return the drtString
+     */
+    public String getDrtString() {
+        return drtString;
+    }
+
+    /**
+     * @param drtString
+     *            the drtString to set
+     */
+    public void setDrtString(String drtString) {
+        this.drtString = drtString;
+    }
+
+    /**
+     * Set the flag value that tells whether GFE notifications should be sent.
+     * 
+     * @param notifyGFE
+     *            the notifyGFE to set
+     */
+    public void setNotifyGFE(boolean notifyGFE) {
+        this.notifyGFE = notifyGFE;
+    }
+
+    /**
+     * @return the notifyGFE
+     */
+    public boolean isNotifyGFE() {
+        return notifyGFE;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.common.message.IMessage#getHeaders()
+     */
+    @Override
+    public Map<String, Object> getHeaders() {
+        Map<String, Object> headers = new HashMap<String, Object>(2, 1.0f);
+        headers.put("drtstring", getDrtString());
+        headers.put("notifygfe", isNotifyGFE());
+        return headers;
     }
 
 }
