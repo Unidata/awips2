b19736f5e7fbfef36102fd7363332424d6be8ab1
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-12-01 17:17:39 -0500

    VLab Issue #4003 - Mostly PGEN bug fixes
    minor updates to nsharp, lat/lon overlay, ntrans, and plotdata resource
    
    Change-Id: I2a1718ed7674edb786bceec13bcb20c29cc5d616
    
    Former-commit-id: 41e5dd000c0755e3b909b4a7ac5781a3b1a27bef [formerly 41e5dd000c0755e3b909b4a7ac5781a3b1a27bef [formerly 2d79944845ee0a168693cf538ea6d52538eb9b2d]] [formerly d9b57e6c7a5a4003602d6af1e625f686863a8f61] [formerly 02cfb919998af17c7b8c2850e09d6bbcb1a82f79 [formerly 02cfb919998af17c7b8c2850e09d6bbcb1a82f79 [formerly 57393984d300532ab07dff3238575e91d0fd1d60]] [formerly d9b57e6c7a5a4003602d6af1e625f686863a8f61 [formerly d9b57e6c7a5a4003602d6af1e625f686863a8f61 [formerly 8bdc663629050457b23d06c19bd7a925597c1288]]]]
    Former-commit-id: 02cfb919998af17c7b8c2850e09d6bbcb1a82f79 [formerly 02cfb919998af17c7b8c2850e09d6bbcb1a82f79 [formerly 57393984d300532ab07dff3238575e91d0fd1d60]]
    Former-commit-id: 1a97b3a099bf7e494b7c1991b8fb5d58b0584827 [formerly 77bac5ad06a433fe7841960a61c6d92467263aa5] [formerly a86cdba552e7d1f102d259fab41d2b682bf65148 [formerly dc922d5112b57f9a358a220b4914cf890e25289d]]
    Former-commit-id: 9898bb6f2f8307946fbaf4596ae874daf084f96a [formerly 6888956479b2d402c91fa96d7bf44cc911c1fcc1]
    Former-commit-id: a92a882e3184d7dbc9227ad21a1a5840d6bc37a2

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/display/RasterElementContainer.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/display/RasterElementContainer.java
@@ -15,59 +15,81 @@ import com.raytheon.uf.viz.core.map.IMapDescriptor;
 
 /**
  * An Element Container that can be used for most Symbol/Marker Elements.
- * Recreation of the IDisplayable objects is only done if the layer DisplayProperties change.
- * The raster images do not need to be recreated when panning or zooming.
+ * Recreation of the IDisplayable objects is only done if the layer
+ * DisplayProperties change. The raster images do not need to be recreated when
+ * panning or zooming.
+ * 
  * @author sgilbert
- *
+ * 
  */
 public class RasterElementContainer extends AbstractElementContainer {
 
-	private DisplayProperties saveProps = null;
-	
-	/**
-	 * @param element
-	 * @param mapDescriptor
-	 * @param target
-	 */
-	public RasterElementContainer(DrawableElement element,
-			IMapDescriptor mapDescriptor, IGraphicsTarget target) {
-		super(element, mapDescriptor, target);
-		// 
-	}
+    private DisplayProperties saveProps = null;
 
-	/* 	 
- 	 * Draws to the given graphics target.  Recreates the IDisplayable objects  
-	 * if the Layer properties change.
-	 * @see gov.noaa.nws.ncep.ui.pgen.display.AbstractTBNL#draw(com.raytheon.uf.viz.core.IGraphicsTarget, com.raytheon.uf.viz.core.drawables.PaintProperties)
-	 */
-	@Override
-	public void draw(IGraphicsTarget target, PaintProperties paintProps,
-				DisplayProperties dprops) {
-		draw(target, paintProps, dprops, false);
-	}
+    /**
+     * @param element
+     * @param mapDescriptor
+     * @param target
+     */
+    public RasterElementContainer(DrawableElement element,
+            IMapDescriptor mapDescriptor, IGraphicsTarget target) {
+        super(element, mapDescriptor, target);
+        //
+    }
 
-	/* 	 
- 	 * Draws to the given graphics target.  Recreates the IDisplayable objects  
-	 * if the Layer properties change.
-	 * @see gov.noaa.nws.ncep.ui.pgen.display.AbstractTBNL#draw(com.raytheon.uf.viz.core.IGraphicsTarget, com.raytheon.uf.viz.core.drawables.PaintProperties, boolean)
-	 */
-	@Override
-	public void draw(IGraphicsTarget target, PaintProperties paintProps,
-				DisplayProperties dprops, boolean needsCreate) {
-		
-		if ( displayEls == null ) needsCreate = true;
-		
-		if ( (dprops != null) &&  ! dprops.equals(saveProps) ) {
-			def.setLayerDisplayAttr(dprops.getLayerMonoColor(), dprops.getLayerColor(), dprops.getLayerFilled());
-			needsCreate = true;
-		}
-		
-		if ( needsCreate ) createDisplayables(paintProps);
-		saveProps = dprops;
-		
-		for ( IDisplayable each : displayEls ) {
-	         each.draw(target, paintProps);
-		}
-	}
+    /*
+     * Draws to the given graphics target. Recreates the IDisplayable objects if
+     * the Layer properties change.
+     * 
+     * @see
+     * gov.noaa.nws.ncep.ui.pgen.display.AbstractTBNL#draw(com.raytheon.uf.viz
+     * .core.IGraphicsTarget,
+     * com.raytheon.uf.viz.core.drawables.PaintProperties)
+     */
+    @Override
+    public void draw(IGraphicsTarget target, PaintProperties paintProps,
+            DisplayProperties dprops) {
+        draw(target, paintProps, dprops, false);
+    }
+
+    /*
+     * Draws to the given graphics target. Recreates the IDisplayable objects if
+     * the Layer properties change.
+     * 
+     * @see
+     * gov.noaa.nws.ncep.ui.pgen.display.AbstractTBNL#draw(com.raytheon.uf.viz
+     * .core.IGraphicsTarget,
+     * com.raytheon.uf.viz.core.drawables.PaintProperties, boolean)
+     */
+    @Override
+    public void draw(IGraphicsTarget target, PaintProperties paintProps,
+            DisplayProperties dprops, boolean needsCreate) {
+
+        if (displayEls == null) {
+            needsCreate = true;
+
+            /*
+             * TTR971 - needs to set display properties, otherwise the layer
+             * color may not take effect (e.g., after switching projection)
+             */
+            def.setLayerDisplayAttr(dprops.getLayerMonoColor(),
+                    dprops.getLayerColor(), dprops.getLayerFilled());
+        }
+
+        if ((dprops != null) && !dprops.equals(saveProps)) {
+            def.setLayerDisplayAttr(dprops.getLayerMonoColor(),
+                    dprops.getLayerColor(), dprops.getLayerFilled());
+            needsCreate = true;
+        }
+
+        if (needsCreate)
+            createDisplayables(paintProps);
+
+        saveProps = dprops;
+
+        for (IDisplayable each : displayEls) {
+            each.draw(target, paintProps);
+        }
+    }
 
 }
