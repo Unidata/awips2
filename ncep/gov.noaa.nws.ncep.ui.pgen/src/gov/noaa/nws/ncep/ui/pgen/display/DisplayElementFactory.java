edf345b90304c6cace4ca607a6dc5b2f53575e65
Stephen Gilbert <Stephen.Gilbert@noaa.gov>
2014-12-01 17:17:39 -0500

    VLab Issue #4003 - Mostly PGEN bug fixes
    minor updates to nsharp, lat/lon overlay, ntrans, and plotdata resource
    
    Change-Id: I2a1718ed7674edb786bceec13bcb20c29cc5d616
    
    Former-commit-id: 57393984d300532ab07dff3238575e91d0fd1d60 [formerly d9b57e6c7a5a4003602d6af1e625f686863a8f61 [formerly 8bdc663629050457b23d06c19bd7a925597c1288]]
    Former-commit-id: d9b57e6c7a5a4003602d6af1e625f686863a8f61
    Former-commit-id: 2d79944845ee0a168693cf538ea6d52538eb9b2d

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/display/DisplayElementFactory.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/display/DisplayElementFactory.java
@@ -143,6 +143,9 @@ import com.vividsolutions.jts.operation.distance.DistanceOp;
  * 05/14        TTR 995     J. Wu       Make contour label auto-placement an option.
  * 07/14        ?           B. Yin      Added support for dashed-line circle for TCM 12 feet sea.
  * 08/14		?			B. Yin		Fixed world wrap for TCM track and zero circle issues.
+ * 08/14        TTR972      J. Wu       Draw filled object as filled only if either its layer's "filled" flag
+ *                                      "true" or they are on the active layer,  .
+ * 09/14        TTR750      J. Wu       Draw track label with specified font styles.
  * </pre>
  * 
  * @author sgilbert
@@ -524,6 +527,7 @@ public class DisplayElementFactory {
         if (isFilled) {
             list.add(createFill(pts));
         }
+
         /*
          * Compile each IWireframeShape, create its LineDisplayElement, and add
          * to IDisplayable return list
@@ -1814,7 +1818,8 @@ public class DisplayElementFactory {
         double major = Math.sqrt((diff[0] * diff[0]) + (diff[1] * diff[1]));
         double minor = major * arc.getAxisRatio();
 
-        if (major / this.screenToExtent < 0.000001) { // ignore circles with major = 0
+        if (major / this.screenToExtent < 0.000001) { // ignore circles with
+                                                      // major = 0
             return slist;
         }
 
@@ -2035,8 +2040,9 @@ public class DisplayElementFactory {
                 Text txt = new Text(null, track.getFontName(),
                         track.getFontSize(), TextJustification.LEFT_JUSTIFY,
                         pt.getLocation(), 0.0, TextRotation.SCREEN_RELATIVE,
-                        new String[] { dtime }, FontStyle.BOLD, iniDspClr, 0,
-                        3, false, DisplayType.NORMAL, "Text", "General Text");
+                        new String[] { dtime }, track.getFontStyle(),
+                        iniDspClr, 0, 3, false, DisplayType.NORMAL, "Text",
+                        "General Text");
                 temps = createDisplayElements((IText) txt, paintProps);
                 slist.addAll(temps);
             }
@@ -2093,8 +2099,9 @@ public class DisplayElementFactory {
                 Text txt = new Text(null, track.getFontName(),
                         track.getFontSize(), TextJustification.LEFT_JUSTIFY,
                         pt.getLocation(), 0.0, TextRotation.SCREEN_RELATIVE,
-                        new String[] { dtime }, FontStyle.BOLD, expDspClr, 0,
-                        3, false, DisplayType.NORMAL, "Text", "General Text");
+                        new String[] { dtime }, track.getFontStyle(),
+                        expDspClr, 0, 3, false, DisplayType.NORMAL, "Text",
+                        "General Text");
                 temps = createDisplayElements((IText) txt, paintProps);
                 slist.addAll(temps);
             }
@@ -4814,10 +4821,22 @@ public class DisplayElementFactory {
      */
     private boolean getDisplayFillMode(Boolean filled) {
 
-        if (layerFilled) {
-            return layerFilled;
+        /*
+         * if (layerFilled) { return layerFilled; } else { return filled; }
+         */
+
+        /*
+         * TTR 972 - to match NMAP2 behavior, non-filled elements will always be
+         * drawn as non-filled. Filled objects should be drawn as filled only
+         * when the "filled" flag for its layer is set to "true" or they are on
+         * the active layer, so it is necessary to set the "layerFilled" flag to
+         * true before generating displayables for such objects (see
+         * PgenResource.drawFilledElement()).
+         */
+        if (filled && layerFilled) {
+            return true;
         } else {
-            return filled;
+            return false;
         }
 
     }
