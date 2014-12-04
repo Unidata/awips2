c32bc7a6a12b7e3cb8e80497a2fbe47752a064c7
Bingfan Yin <Bingfan.Yin@noaa.gov>
2014-12-04 13:38:50 -0500

    VLab Issue #5413 - Changes for PGEN in D2D fourth try
    Change-Id: Iec34f20209748a661ee07d2960bd79132233cf1a
    
    Former-commit-id: 5fc47fb52d489ca30289679e87108b1b838cc478 [formerly 5fc47fb52d489ca30289679e87108b1b838cc478 [formerly 4d15f42f935cd71c1962292ecfacabfe49127027]]
    Former-commit-id: fe2010b4ec1dcbc442514a6900dc24b4efb126f2
    Former-commit-id: 5e5ddae35795267380ff4f4ac8d60f0a3a4b0231

--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/PgenSession.java
+++ ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/PgenSession.java
@@ -7,18 +7,32 @@
  */
 package gov.noaa.nws.ncep.ui.pgen;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.raytheon.uf.viz.core.maps.display.VizMapEditor;
-import com.raytheon.viz.ui.editor.AbstractEditor;
-
+import gov.noaa.nws.ncep.ui.pgen.PgenUtil.PgenMode;
 import gov.noaa.nws.ncep.ui.pgen.controls.PgenCommandManager;
+import gov.noaa.nws.ncep.ui.pgen.filter.CategoryFilter;
 import gov.noaa.nws.ncep.ui.pgen.palette.PgenPaletteWindow;
 import gov.noaa.nws.ncep.ui.pgen.rsc.PgenResource;
 import gov.noaa.nws.ncep.ui.pgen.rsc.PgenResourceData;
+import gov.noaa.nws.ncep.ui.pgen.tools.AbstractPgenTool;
 
-//import gov.noaa.nws.ncep.viz.ui.display.NmapUiUtils;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.ui.IPartListener2;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartReference;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.WorkbenchPage;
+
+import com.raytheon.uf.viz.core.IDisplayPane;
+import com.raytheon.uf.viz.core.IRenderableDisplayChangedListener;
+import com.raytheon.uf.viz.core.drawables.IRenderableDisplay;
+import com.raytheon.uf.viz.core.maps.display.VizMapEditor;
+import com.raytheon.viz.ui.editor.AbstractEditor;
+import com.raytheon.viz.ui.editor.IMultiPaneEditor;
 
 /**
  * This singleton is intended to couple a PGEN Palette with a PGgenResource, so
@@ -27,7 +41,21 @@ import gov.noaa.nws.ncep.ui.pgen.rsc.PgenResourceData;
  * @author sgilbert
  * 
  */
-public class PgenSession {
+
+/**
+ * Implements a drawing layer for PGEN products.
+ * 
+ * <pre>
+ * SOFTWARE HISTORY
+ * Date       	Ticket#		Engineer	Description
+ * ------------	----------	-----------	--------------------------
+ * 12/14		R5413		B. Yin   	Added IPartListener2 and IRenderableDisplayChangedListener
+ * to make the swapping in D2D work
+ * 
+ */
+
+public class PgenSession implements IPartListener2,
+        IRenderableDisplayChangedListener {
 
     /*
      * The singleton instance
@@ -45,6 +73,12 @@ public class PgenSession {
     private PgenPaletteWindow palette = null;
 
     private List<AbstractEditor> editors = new ArrayList<AbstractEditor>();
+
+    /*
+     * Active PGEN tool
+     */
+    private AbstractPgenTool pgenTool = null;
+
     /*
      * Hide default constructor
      */
@@ -80,8 +114,9 @@ public class PgenSession {
         // set new PGEN resource
         pgenResource = rsc;
         // add the palette's stack listener to new resource's command Manager
-        if (pgenResource != null)
+        if (pgenResource != null && palette != null) {
             pgenResource.getCommandMgr().addStackListener(palette);
+        }
 
     }
 
@@ -155,8 +190,9 @@ public class PgenSession {
         palette = pal;
         // Register this palette's stack listener with the CommandManager, if
         // able
-        if (pgenResource != null)
+        if (pgenResource != null) {
             pgenResource.getCommandMgr().addStackListener(palette);
+        }
     }
 
     /**
@@ -197,13 +233,194 @@ public class PgenSession {
     public PgenPaletteWindow getPgenPalette() {
         return palette;
     }
-    
-    public void addEditor(AbstractEditor editor){
+
+    public void addEditor(AbstractEditor editor) {
         editors.add(editor);
     }
-    
-    public List<AbstractEditor> getEditors(){
+
+    public List<AbstractEditor> getEditors() {
         return editors;
+    }
+
+    /*
+     * Remove PGEN handler when swapping to side view. Also open PGEN palette if
+     * there is a PGEN resource when swapping to main editor.
+     * 
+     * (non-Javadoc)
+     * 
+     * @see com.raytheon.uf.viz.core.IRenderableDisplayChangedListener#
+     * renderableDisplayChanged(com.raytheon.uf.viz.core.IDisplayPane,
+     * com.raytheon.uf.viz.core.drawables.IRenderableDisplay,
+     * com.raytheon.uf.viz
+     * .core.IRenderableDisplayChangedListener.DisplayChangeType)
+     */
+    @SuppressWarnings("restriction")
+    @Override
+    public void renderableDisplayChanged(IDisplayPane pane,
+            IRenderableDisplay newRenderableDisplay, DisplayChangeType type) {
+
+        if (type == DisplayChangeType.ADD
+                && newRenderableDisplay.getContainer() instanceof VizMapEditor) {
+
+            VizMapEditor editorChanged = (VizMapEditor) newRenderableDisplay
+                    .getContainer();
+
+            if (PgenUtil.getPgenMode() == PgenMode.SINGLE) { // for D2d
+                                                             // swapping, single
+                                                             // pane mode
+                if (pgenResource != null) {
+                    pgenResource.removeGhostLine();
+                    pgenResource.removeSelected();
+                }
+
+                PgenUtil.setSelectingMode();
+            }
+
+            else { // for D2d swapping, multi-pane mode
+
+                // clean up current pgen resource
+                if (pgenResource != null) {
+                    pgenResource.closeDialogs();
+                    pgenResource.deactivatePgenTools();
+                    pgenResource.getCommandMgr().removeStackListener(palette);
+                }
+
+                if (palette != null) {
+                    if (PgenUtil.findPgenResource(editorChanged) == null) {
+                        // editor does not have PGEN, close the palette
+
+                        PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                                .getActivePage().hideView(palette);
+                        palette = null;
+                    } else {
+                        // editor has PGEN resource, reset to selecting mode
+                        pgenResource = PgenUtil.findPgenResource(editorChanged);
+                        pgenResource.setCatFilter(new CategoryFilter("Any"));
+                        palette.setCurrentCategory("Any");
+                        PgenUtil.setSelectingMode();
+                    }
+                } else {
+                    // palette is closed
+                    if (PgenUtil.findPgenResource(editorChanged) != null) {
+                        // editor has PGEN, open the palette
+                        IWorkbenchPage wpage = PlatformUI.getWorkbench()
+                                .getActiveWorkbenchWindow().getActivePage();
+
+                        IViewPart vpart = wpage.findView(PgenUtil.VIEW_ID);
+
+                        try {
+
+                            if (vpart == null) {
+
+                                vpart = wpage.showView(PgenUtil.VIEW_ID);
+                                IViewReference pgenViewRef = wpage
+                                        .findViewReference(PgenUtil.VIEW_ID);
+                                if (pgenViewRef != null
+                                        && wpage instanceof WorkbenchPage) {
+                                    ((WorkbenchPage) wpage)
+                                            .detachView(pgenViewRef);
+                                }
+                            } else {
+
+                                if (!wpage.isPartVisible(vpart)) {
+                                    vpart = wpage.showView(PgenUtil.VIEW_ID);
+                                    IViewReference pgenViewRef = wpage
+                                            .findViewReference(PgenUtil.VIEW_ID);
+                                    if (pgenViewRef != null
+                                            && wpage instanceof WorkbenchPage) {
+                                        ((WorkbenchPage) wpage)
+                                                .detachView(pgenViewRef);
+                                    }
+                                }
+                            }
+                            this.pgenResource = PgenUtil
+                                    .findPgenResource(editorChanged);
+                            this.pgenResource.setCatFilter(new CategoryFilter(
+                                    "Any"));
+                            this.palette.setCurrentCategory("Any");
+                            PgenUtil.setSelectingMode();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            }
+        } else if (type == DisplayChangeType.REMOVE
+                && !(newRenderableDisplay.getContainer() instanceof AbstractEditor)) {
+            // remove to side view
+            // unregister pgen handlers
+
+            if (newRenderableDisplay.getContainer() instanceof IMultiPaneEditor) {
+                IMultiPaneEditor sideView = (IMultiPaneEditor) newRenderableDisplay
+                        .getContainer();
+                if (this.getPgenTool() != null) {
+                    sideView.unregisterMouseHandler(this.getPgenTool()
+                            .getMouseHandler());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void partActivated(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partBroughtToTop(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partClosed(IWorkbenchPartReference partRef) {
+        IWorkbenchPart part = partRef.getPart(false);
+        if (part instanceof VizMapEditor) { // for D2D
+            if (PgenUtil.findPgenResource((VizMapEditor) part) != null) {
+                ((VizMapEditor) part)
+                        .removeRenderableDisplayChangedListener(this);
+            }
+        }
+    }
+
+    @Override
+    public void partDeactivated(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partOpened(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partHidden(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partVisible(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void partInputChanged(IWorkbenchPartReference partRef) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public AbstractPgenTool getPgenTool() {
+        return pgenTool;
+    }
+
+    public void setPgenTool(AbstractPgenTool pgenTool) {
+        this.pgenTool = pgenTool;
     }
 
 }
