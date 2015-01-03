af37bea025654c2146f7f8dddb309527e8850042
mjames-upc <mjames@unidata.ucar.edu>
2015-01-03 13:51:19 -0700

    LDM RPM build post install file edits.  build version and CAVE IDE max memory increase
    Former-commit-id: e260556e9bd941486e8894ee34dda3f772f94d5a

--- rpms/unidata/buildEnvironment.sh
+++ rpms/unidata/buildEnvironment.sh
@@ -13,8 +13,8 @@ export BUILD_WORKSPACE=${BUILD_DIR}/workspace
 # baseline is...
 export BASELINE="${JENKINS_WORKSPACE}"
 export AWIPSII_VERSION="14.2.1"
-export BUILD_NUMBER="6"
-export AWIPSII_RELEASE="17n${BUILD_NUMBER}"
+export BUILD_NUMBER="7"
+export AWIPSII_RELEASE="18n${BUILD_NUMBER}"
 
 export AWIPSII_TOP_DIR="${BUILD_WORKSPACE}/tmp/rpms_built_dir"
 
