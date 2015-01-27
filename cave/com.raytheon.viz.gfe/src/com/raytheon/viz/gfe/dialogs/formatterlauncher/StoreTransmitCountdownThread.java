d659eec7fde5d0b6223fe0477358ed65d5a832a3
Ron Anderson <ron.anderson@raytheon.com>
2015-01-27 13:40:56 -0600

    Omaha #4018 Formatted all formatter launcher code to make later code reviews easier
    Change-Id: Iaf415b8bdd3e8fdc46b8ea66e066b9c2ce3b6f7a
    
    Former-commit-id: ba693713ddc5916091e01c93de6d99a3b61f96c9 [formerly 06275c181216190bef5077afe9cb552322d39b7d] [formerly fe0d2389729f43b3fec1f5901ede9e0a0bb187b5 [formerly a7ab63820363726af5749db2f26ead6c63e607c2]] [formerly 5c08543da0da801df47e733ac66047a9f50ec635] [formerly ba693713ddc5916091e01c93de6d99a3b61f96c9 [formerly 06275c181216190bef5077afe9cb552322d39b7d] [formerly fe0d2389729f43b3fec1f5901ede9e0a0bb187b5 [formerly a7ab63820363726af5749db2f26ead6c63e607c2]] [formerly 5c08543da0da801df47e733ac66047a9f50ec635] [formerly 4bab4e49195245e5df1dca57dadec2b3dd91c398 [formerly 4bab4e49195245e5df1dca57dadec2b3dd91c398 [formerly 8a86832fb27a2cde6a6502896d028f655ca7e5d1]] [formerly 5c08543da0da801df47e733ac66047a9f50ec635 [formerly 5c08543da0da801df47e733ac66047a9f50ec635 [formerly 99ae51d268e784461c0686a9d86d512049a501ac]]]]]
    Former-commit-id: 4bab4e49195245e5df1dca57dadec2b3dd91c398 [formerly 4bab4e49195245e5df1dca57dadec2b3dd91c398 [formerly 8a86832fb27a2cde6a6502896d028f655ca7e5d1]]
    Former-commit-id: 54def4b0825cc04d3f8d4dbc723d9fd292b9eaf6 [formerly 623f90d3de485b35a59b7c41ab6c61a3daf9fee6] [formerly 02d70648215fb22fdc9c34667db094336e6906b9 [formerly a4c15c7beabe83ab7cf81ae63de1f3a27dd5bc80]] [formerly fbff390bd8eefe4b87c783852f3b968519f46c4f [formerly 04cebe08850022e8a07d9993581f5fac190d9b90] [formerly 1dfe469dd0753deb05fa2bc68f6d7563f0fca24a [formerly a00e04a8d723e15a91d2a889a39d02e3c00fc06b]]]
    Former-commit-id: 90a57f278eb6ea5c43b61cae43c0c49a60c4dd67 [formerly 09110c8d84692f552917a6b3cecb780e2fc3a122] [formerly dd95574412c88454fd92686db97aef3d72df7fc8 [formerly 6d25f74625498b2696dca07fcd93ee303567527b]]
    Former-commit-id: 826aaff5127b716e0d93ee00cd01f9f92738f25c [formerly 7ab27329ff1a18a712e845461b8be60358f5e459]
    Former-commit-id: 41f58d0fb6dabf46d86bf7d93aa928518f13a3ee

--- cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/dialogs/formatterlauncher/StoreTransmitCountdownThread.java
+++ cave/com.raytheon.viz.gfe/src/com/raytheon/viz/gfe/dialogs/formatterlauncher/StoreTransmitCountdownThread.java
@@ -36,7 +36,7 @@ import com.raytheon.viz.gfe.core.DataManager;
  * ------------ ---------- ----------- --------------------------
  * 18 APR 2008  ###        lvenable    Initial creation
  * 20 AUG 2010  4687	   cjeanbap	   &quot;null&quot; showed up in
- *									   countdown message.
+ * 								   countdown message.
  * 
  * </pre>
  * 
@@ -45,182 +45,181 @@ import com.raytheon.viz.gfe.core.DataManager;
  * 
  */
 public class StoreTransmitCountdownThread extends Thread {
-	/**
-	 * Parent display.
-	 */
-	private Display display;
+    /**
+     * Parent display.
+     */
+    private Display display;
 
-	/**
-	 * Progress bar to be updated.
-	 */
-	private ProgressBar progressBar;
+    /**
+     * Progress bar to be updated.
+     */
+    private ProgressBar progressBar;
 
-	/**
-	 * Flag indicating if the thread is done running.
-	 */
-	private boolean isDone = false;
+    /**
+     * Flag indicating if the thread is done running.
+     */
+    private boolean isDone = false;
 
-	/**
-	 * Flag indicating if the thread has been canceled.
-	 */
-	private boolean isCancelled = false;
+    /**
+     * Flag indicating if the thread has been canceled.
+     */
+    private boolean isCancelled = false;
 
-	/**
-	 * Count down label.
-	 */
-	private Label countdownLbl;
+    /**
+     * Count down label.
+     */
+    private Label countdownLbl;
 
-	/**
-	 * Count down string.
-	 */
-	private String countdownStr;
+    /**
+     * Count down string.
+     */
+    private String countdownStr;
 
-	/**
-	 * Counter.
-	 */
-	private int counter = 5;
+    /**
+     * Counter.
+     */
+    private int counter = 5;
 
-	/**
-	 * Store/Transmit callback.
-	 */
-	private IStoreTransmitProduct storeCB;
+    /**
+     * Store/Transmit callback.
+     */
+    private IStoreTransmitProduct storeCB;
 
-	/**
-	 * Count down prefix string.
-	 */
-	private String countdownPrefix;
+    /**
+     * Count down prefix string.
+     */
+    private String countdownPrefix;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param display
-	 *            Parent display.
-	 * @param progressBar
-	 *            Progress bar.
-	 * @param countdownLbl
-	 *            Count down label.
-	 * @param countdownStr
-	 *            Count down string.
-	 * @param cb
-	 *            Callback interface.
-	 * @param isStore
-	 *            True to display store, false to display transmit.
-	 */
-	public StoreTransmitCountdownThread(Display display,
-			ProgressBar progressBar, Label countdownLbl, String countdownStr,
-			IStoreTransmitProduct cb, boolean isStore) {
-		this.display = display;
-		this.progressBar = progressBar;
-		this.countdownLbl = countdownLbl;
-		this.countdownStr = countdownStr;
-		this.storeCB = cb;
-		countdownPrefix = new String();
-		CAVEMode opMode = DataManager.getCurrentInstance().getOpMode();
-		if (!opMode.equals(CAVEMode.OPERATIONAL)) {
-			countdownPrefix = "Simulated ";
-		}
-		if (isStore == true) {
-			countdownPrefix += "Store in ";
-		} else {
-			countdownPrefix += "Transmit in ";
-		}
-	}
+    /**
+     * Constructor.
+     * 
+     * @param display
+     *            Parent display.
+     * @param progressBar
+     *            Progress bar.
+     * @param countdownLbl
+     *            Count down label.
+     * @param countdownStr
+     *            Count down string.
+     * @param cb
+     *            Callback interface.
+     * @param isStore
+     *            True to display store, false to display transmit.
+     */
+    public StoreTransmitCountdownThread(Display display,
+            ProgressBar progressBar, Label countdownLbl, String countdownStr,
+            IStoreTransmitProduct cb, boolean isStore) {
+        this.display = display;
+        this.progressBar = progressBar;
+        this.countdownLbl = countdownLbl;
+        this.countdownStr = countdownStr;
+        this.storeCB = cb;
+        countdownPrefix = new String();
+        CAVEMode opMode = DataManager.getCurrentInstance().getOpMode();
+        if (!opMode.equals(CAVEMode.OPERATIONAL)) {
+            countdownPrefix = "Simulated ";
+        }
+        if (isStore == true) {
+            countdownPrefix += "Store in ";
+        } else {
+            countdownPrefix += "Transmit in ";
+        }
+    }
 
-	/**
-	 * Thread's run method.
-	 */
-	@Override
-	public void run() {
-		isDone = false;
-		countdownLabelStart();
+    /**
+     * Thread's run method.
+     */
+    @Override
+    public void run() {
+        isDone = false;
+        countdownLabelStart();
 
-		for (int i = 0; i < 6; i++) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+        for (int i = 0; i < 6; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-			if (isCancelled == false) {
-				display.asyncExec(new Runnable() {
-					public void run() {
-						if (progressBar.isDisposed()) {
-							return;
-						}
-						// Increment the progress bar
-						progressBar
-								.setSelection(progressBar.getSelection() + 1);
-						countdownLbl.setText(countdownPrefix + counter
-								+ " seconds...");
-						--counter;
-					}
-				});
-			} else {
-				break;
-			}
-		}
+            if (isCancelled == false) {
+                display.asyncExec(new Runnable() {
+                    public void run() {
+                        if (progressBar.isDisposed()) {
+                            return;
+                        }
+                        // Increment the progress bar
+                        progressBar.setSelection(progressBar.getSelection() + 1);
+                        countdownLbl.setText(countdownPrefix + counter
+                                + " seconds...");
+                        --counter;
+                    }
+                });
+            } else {
+                break;
+            }
+        }
 
-		if (isCancelled == false) {
-			countdownLabelFinished();
-		}
+        if (isCancelled == false) {
+            countdownLabelFinished();
+        }
 
-		isDone = true;
+        isDone = true;
 
-		storeCB.storeTransmitProduct();
-	}
+        storeCB.storeTransmitProduct();
+    }
 
-	/**
-	 * Check if the thread is done running.
-	 * 
-	 * @return True if the thread is done running, false if it is still running.
-	 */
-	public boolean isDone() {
-		return isDone;
-	}
+    /**
+     * Check if the thread is done running.
+     * 
+     * @return True if the thread is done running, false if it is still running.
+     */
+    public boolean isDone() {
+        return isDone;
+    }
 
-	/**
-	 * Cancel the running thread.
-	 */
-	public void cancelThread() {
-		isCancelled = true;
-	}
+    /**
+     * Cancel the running thread.
+     */
+    public void cancelThread() {
+        isCancelled = true;
+    }
 
-	/**
-	 * Check if the thread has been canceled.
-	 * 
-	 * @return True if the thread was canceled, false otherwise.
-	 */
-	public boolean threadCancelled() {
-		return isCancelled;
-	}
+    /**
+     * Check if the thread has been canceled.
+     * 
+     * @return True if the thread was canceled, false otherwise.
+     */
+    public boolean threadCancelled() {
+        return isCancelled;
+    }
 
-	/**
-	 * Set the count down label to have a red background and white text while
-	 * the Store/Transmit is in count down mode.
-	 */
-	private void countdownLabelStart() {
-		display.asyncExec(new Runnable() {
-			public void run() {
-				countdownLbl.setBackground(display
-						.getSystemColor(SWT.COLOR_RED));
-				countdownLbl.setForeground(display
-						.getSystemColor(SWT.COLOR_WHITE));
-			}
-		});
-	}
+    /**
+     * Set the count down label to have a red background and white text while
+     * the Store/Transmit is in count down mode.
+     */
+    private void countdownLabelStart() {
+        display.asyncExec(new Runnable() {
+            public void run() {
+                countdownLbl.setBackground(display
+                        .getSystemColor(SWT.COLOR_RED));
+                countdownLbl.setForeground(display
+                        .getSystemColor(SWT.COLOR_WHITE));
+            }
+        });
+    }
 
-	/**
-	 * Set the count down label back to its original state.
-	 */
-	private void countdownLabelFinished() {
-		display.asyncExec(new Runnable() {
-			public void run() {
-				countdownLbl.setText(countdownStr);
-				countdownLbl.setBackground(display
-						.getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
-				countdownLbl.setForeground(display
-						.getSystemColor(SWT.COLOR_BLACK));
-			}
-		});
-	}
+    /**
+     * Set the count down label back to its original state.
+     */
+    private void countdownLabelFinished() {
+        display.asyncExec(new Runnable() {
+            public void run() {
+                countdownLbl.setText(countdownStr);
+                countdownLbl.setBackground(display
+                        .getSystemColor(SWT.COLOR_WIDGET_BACKGROUND));
+                countdownLbl.setForeground(display
+                        .getSystemColor(SWT.COLOR_BLACK));
+            }
+        });
+    }
 }
