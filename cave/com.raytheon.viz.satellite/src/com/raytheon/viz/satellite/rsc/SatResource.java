79367259723ae9ce18f7fb32e93e8bdb9a551c78
Ben Steffensmeier <Ben.Steffensmeier@raytheon.com>
2015-02-17 11:35:59 -0600

    Omaha #4135 Fix speckles, set no data value for derived satellite data.
    Former-commit-id: 22822c1a65cef7a7c91fd5af48b26be2726c7a43 [formerly 19f1d29e52cbfbed429d8e08e49cf8a6e590da9a [formerly 65a50fe832ef6260dfd0e02c9fad9cd2eed6f6f2]]
    Former-commit-id: 19f1d29e52cbfbed429d8e08e49cf8a6e590da9a
    Former-commit-id: 6ec30c1372928aa7f34676ecd806e47d3c969ac8

--- cave/com.raytheon.viz.satellite/src/com/raytheon/viz/satellite/rsc/SatResource.java
+++ cave/com.raytheon.viz.satellite/src/com/raytheon/viz/satellite/rsc/SatResource.java
@@ -45,6 +45,7 @@ import com.raytheon.uf.common.colormap.prefs.DataMappingPreferences;
 import com.raytheon.uf.common.dataplugin.PluginDataObject;
 import com.raytheon.uf.common.dataplugin.satellite.SatMapCoverage;
 import com.raytheon.uf.common.dataplugin.satellite.SatelliteRecord;
+import com.raytheon.uf.common.dataplugin.satellite.units.generic.GenericPixel;
 import com.raytheon.uf.common.geospatial.IGridGeometryProvider;
 import com.raytheon.uf.common.geospatial.ReferencedCoordinate;
 import com.raytheon.uf.common.geospatial.data.GeographicDataSource;
@@ -123,6 +124,8 @@ import com.vividsolutions.jts.geom.Coordinate;
  *  Jun 12, 2014  3238      bsteffen    Implement Interrogatable
  *  Aug 21, 2014  DR 17313  jgerth      Set no data value if no data mapping
  *  Oct 15, 2014  3681      bsteffen    create renderable in interrogate if necessary.
+ *  Feb 17, 2015  4135      bsteffen    Set no data value for derived products.
+ * 
  * 
  * </pre>
  * 
@@ -396,8 +399,19 @@ public class SatResource extends
         if (persisted != null) {
             colorMapParameters.applyPersistedParameters(persisted);
         }
-        if (colorMapParameters.getDataMapping() == null)
-        	colorMapParameters.setNoDataValue(0);
+        if (colorMapParameters.getDataMapping() == null) {
+            if (unit instanceof GenericPixel) {
+                /**
+                 * Generic Pixel only comes from derived parameter which used
+                 * signed data so 0 is valid but -128 is used as a no data
+                 * value.
+                 */
+                colorMapParameters.setNoDataValue(Byte.MIN_VALUE);
+            } else {
+                colorMapParameters.setNoDataValue(0);
+            }
+
+        }
 
         getCapability(ColorMapCapability.class).setColorMapParameters(
                 colorMapParameters);
