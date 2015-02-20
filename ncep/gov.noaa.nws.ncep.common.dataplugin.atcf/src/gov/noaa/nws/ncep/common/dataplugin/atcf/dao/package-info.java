60d97fb1f392ddaba525d98d90b9d62883e71d41
Shawn.Hooper <Shawn.Hooper@noaa.gov>
2015-02-20 08:03:30 -0500

    Moved the CIMSS, gsd, and ncep directories to separate repos
    Change-Id: I4caa5d7425129d8d5df6c721b23226558ab4e2b3
    
    Former-commit-id: aead7df6572486047a9b98dc47741cf703997242 [formerly 55b9fd232088e9313cd12b4218c92be79662f124] [formerly 82d7ec26426ee205730f9a1c906c9281e71cf66a [formerly 3bb5371f916297eb6fd67d44246520edbc9ea254]] [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082] [formerly 0fd432a27399980ec1bf1b1eeb5cbcbbf9e16513 [formerly 0fd432a27399980ec1bf1b1eeb5cbcbbf9e16513 [formerly 22df67d70c68991d8f048da734d1e7e50311726c]] [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082 [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082 [formerly 5ed3823b82d5da57a3c1f1a7e4331911670acc28]]]]
    Former-commit-id: 0fd432a27399980ec1bf1b1eeb5cbcbbf9e16513 [formerly 0fd432a27399980ec1bf1b1eeb5cbcbbf9e16513 [formerly 22df67d70c68991d8f048da734d1e7e50311726c]]
    Former-commit-id: 45c53419764ae255fb07d7503c027f1cfbaa68b1 [formerly de41ff5ec99b5f43f7fe7beee3bc2d0239e893f4] [formerly 5fe0b175556de8a9aa00d693a31dd53694a65d0d [formerly 94af9fb28561a9cc5eb90396398509d0a96c9ce2]]
    Former-commit-id: 2e218254f8b35fd6976d16d2789796338d650588 [formerly eb87cbe1651edd1aa626485fed8e630c0052b4a6]
    Former-commit-id: b39196ea5dd86dc507afbaab0da5c517810ac8b5

--- ncep/gov.noaa.nws.ncep.common.dataplugin.atcf/src/gov/noaa/nws/ncep/common/dataplugin/atcf/dao/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * ATCF implementation of the plug-in pattern.  
- * 
- * Package includes classes to decode the ATCF message and persist the data.
- * 
- */
-package gov.noaa.nws.ncep.common.dataplugin.atcf.dao;
