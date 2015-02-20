dd01b5ab6ff59f0363890c42e969a07aa9d8e8c8
Shawn.Hooper <Shawn.Hooper@noaa.gov>
2015-02-20 08:03:30 -0500

    Moved the CIMSS, gsd, and ncep directories to separate repos
    Change-Id: I4caa5d7425129d8d5df6c721b23226558ab4e2b3
    
    Former-commit-id: beaa6406571f07c03ae22e5a78fd86a81ba271db [formerly fdbe5ce2f80941df56b2ce06ca32cb6f63ae1098] [formerly 3bb5371f916297eb6fd67d44246520edbc9ea254] [formerly 3bb5371f916297eb6fd67d44246520edbc9ea254 [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082]] [formerly 22df67d70c68991d8f048da734d1e7e50311726c [formerly 3bb5371f916297eb6fd67d44246520edbc9ea254 [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082] [formerly 22df67d70c68991d8f048da734d1e7e50311726c [formerly 5ed3823b82d5da57a3c1f1a7e4331911670acc28]]]]
    Former-commit-id: 22df67d70c68991d8f048da734d1e7e50311726c
    Former-commit-id: 6f822da4bcec598b0645ebd659ef173aad5f1707 [formerly 9a6b880d1827be7fba44fd3e086cd1f6284334de] [formerly eb87cbe1651edd1aa626485fed8e630c0052b4a6 [formerly 94af9fb28561a9cc5eb90396398509d0a96c9ce2]]
    Former-commit-id: eb87cbe1651edd1aa626485fed8e630c0052b4a6
    Former-commit-id: 642aa2d4981756b8dfce0bd52054a3df38deb02c

--- ncep/gov.noaa.nws.ncep.common.dataplugin.stormtrack/src/gov/noaa/nws/ncep/common/dataplugin/stormtrack/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
-* Contains utility classes for StormTrack plugin
-*/
-package gov.noaa.nws.ncep.common.dataplugin.stormtrack;
--- ncep/gov.noaa.nws.ncep.edex.plugin.stormtrack/src/gov/noaa/nws/ncep/edex/plugin/stormtrack/decoder/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * SGWH implementation of the plugin pattern.  
- * 
- * Package includes classes to decode the StormTrack message and persist the data.
- * 
- */
-package gov.noaa.nws.ncep.edex.plugin.stormtrack.decoder;
