eb87cbe1651edd1aa626485fed8e630c0052b4a6
Shawn.Hooper <Shawn.Hooper@noaa.gov>
2015-02-20 08:03:30 -0500

    Moved the CIMSS, gsd, and ncep directories to separate repos
    Change-Id: I4caa5d7425129d8d5df6c721b23226558ab4e2b3
    
    Former-commit-id: 3bb5371f916297eb6fd67d44246520edbc9ea254 [formerly 3d169da19540db6b4407d6a7f3da1bd1ab4c1082] [formerly 22df67d70c68991d8f048da734d1e7e50311726c [formerly 5ed3823b82d5da57a3c1f1a7e4331911670acc28]]
    Former-commit-id: 22df67d70c68991d8f048da734d1e7e50311726c
    Former-commit-id: 94af9fb28561a9cc5eb90396398509d0a96c9ce2

--- ncep/gov.noaa.nws.ncep.ui.nsharp/AwcNsharp/epson.h
+++
@@ -1,6 +0,0 @@
-/* EPSON Printer Routine Definitions */
-void print_sounding_epson( void );
-void epprinter_setup( FILE *fp );
-void epclose_printer( FILE *fp );
-void to_raster( short row, short col, FILE *fp );
-
--- ncep/gov.noaa.nws.ncep.ui.pgen/src/gov/noaa/nws/ncep/ui/pgen/display/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Contains classes used to convert PGEN Drawable Elements into   
- * objects that can be readily displayed.
- *   
- */
-package gov.noaa.nws.ncep.ui.pgen.display;
