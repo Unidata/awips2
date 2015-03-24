6fa0ce555676177408f512d2d21d9fcf9f13cb83
Sean.Webb <Sean.Webb@noaa.gov>
2015-03-24 14:31:17 -0400

    ASM #17189 - Removed rpm commands from profile scripts
    Change-Id: Id6abd356376c29a179278a127828e8616cc66fbd
    
    Former-commit-id: cd838863a7fafb8101f5816926e23d47997e6ea3 [formerly 0b51d70c7f71fdecdc2f4ab0988d1d1161019030]
    Former-commit-id: b6153f405821380ee5c09997f06739abd63228fa

--- rpms/awips2.core/Installer.cli/scripts/profile.d/awips2CLI.csh
+++ rpms/awips2.core/Installer.cli/scripts/profile.d/awips2CLI.csh
@@ -1,12 +1,8 @@
 #!/bin/csh
 
-# Ensure that awips2-cli is installed.
-rpm -q awips2-cli >& /dev/null
-set RC="$?"
-
 #if installed, set the variable
-if ( "${RC}" == "0" ) then
-	set CLI_INSTALL=`rpm -q --queryformat '%{INSTPREFIXES}' awips2-cli`
+if ( -d /awips2/fxa ) then
+	set CLI_INSTALL=/awips2/fxa
 	#if check CLI_INSTALL is set, set it in the path
 	if ( "${CLI_INSTALL}" != "" ) then
 		setenv PATH ${CLI_INSTALL}/bin:${PATH}
--- rpms/awips2.core/Installer.cli/scripts/profile.d/awips2CLI.sh
+++ rpms/awips2.core/Installer.cli/scripts/profile.d/awips2CLI.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-# Ensure that awips2-cli is installed.
-rpm -q awips2-cli > /dev/null 2>&1
-RC="$?"
-if [ ! "${RC}" = "0" ]; then
-   # awips2-cli is not installed.
-   return
-fi
-
 # Determine where awips2-cli has been installed.
-CLI_INSTALL=`rpm -q --queryformat '%{INSTPREFIXES}\n' awips2-cli`
-if [ "${CLI_INSTALL}" = "" ]; then
-   return
+if [ -d /awips2/fxa ]; then
+   CLI_INSTALL=/awips2/fxa
+   export PATH=${CLI_INSTALL}/bin:${PATH}
 fi
 
-export PATH=${CLI_INSTALL}/bin:${PATH}
