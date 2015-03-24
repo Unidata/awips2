b6153f405821380ee5c09997f06739abd63228fa
Sean.Webb <Sean.Webb@noaa.gov>
2015-03-24 14:31:17 -0400

    ASM #17189 - Removed rpm commands from profile scripts
    Change-Id: Id6abd356376c29a179278a127828e8616cc66fbd
    
    Former-commit-id: 0b51d70c7f71fdecdc2f4ab0988d1d1161019030

--- rpms/awips2.core/Installer.yajsw/scripts/profile.d/awips2Yajsw.sh
+++ rpms/awips2.core/Installer.yajsw/scripts/profile.d/awips2Yajsw.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Is Yajsw installed?
-rpm -q awips2-yajsw > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-	return
-fi
-
-YAJSW_INSTALL="/awips2/yajsw"
+if [ -d /awips2/yajsw ]; then
+   YAJSW_INSTALL="/awips2/yajsw"
 # Update The Environment
-export YAJSW_HOME=${YAJSW_INSTALL}
+   export YAJSW_HOME=${YAJSW_INSTALL}
+fi
