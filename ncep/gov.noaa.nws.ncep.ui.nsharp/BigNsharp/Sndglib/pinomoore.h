fdf4d18c851516c0ac6583fcd98f48b3817be4b1
AWIPS User <mjames@unidata.ucar.edu>
2015-04-23 03:59:57 -0600

    remove ncep from awips2-builds
    Former-commit-id: 4333fc928df4efeb343d01aef3f8220b1b127e0d [formerly d9437337a6bda5e8e4e3c2d5d516c3771d687add]
    Former-commit-id: 9b99b3a881b3d65b9cb45631fb3a309838868e09

--- ncep/gov.noaa.nws.ncep.ui.nsharp/BigNsharp/Sndglib/pinomoore.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PINOMOORE_H
-#define _PINOMOORE_H
-
-float pinomoore(void);
-float hailgrowth(float *max_diameter);
-float hailreduction(float max_diameter, float *sfc_diameter);
-float hailspeed(float diam, float pres, float dd_thw);
-
-#endif  /* _PINOMOORE_H */
--- ncep/gov.noaa.nws.ncep.viz.tools/inc/odtextern.h
+++
@@ -1,3 +0,0 @@
-/* external application functions defined in odtext directory */
-extern int extern_readsstvalue(char *,float,float,int *);
-extern void extern_qmessage(int,int,char *,char *);  /* XXX */
