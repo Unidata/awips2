0f4f73a2e204e04bee0781214515dadb6e08033a
AWIPS User <mjames@unidata.ucar.edu>
2015-04-23 03:59:57 -0600

    remove ncep from awips2-builds
    Former-commit-id: d760d63993af84cc2832699fc195874f8951f682 [formerly 4333fc928df4efeb343d01aef3f8220b1b127e0d] [formerly 9b99b3a881b3d65b9cb45631fb3a309838868e09 [formerly d9437337a6bda5e8e4e3c2d5d516c3771d687add]]
    Former-commit-id: 9b99b3a881b3d65b9cb45631fb3a309838868e09
    Former-commit-id: fdf4d18c851516c0ac6583fcd98f48b3817be4b1

--- ncep/gov.noaa.nws.ncep.edex.plugin.ssha/src/gov/noaa/nws/ncep/edex/plugin/ssha/decoder/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * SSHA implementation of the plugin pattern.  
- * 
- * Package includes classes to decode the  SSHA message and persist the data.
- * 
- */
-package gov.noaa.nws.ncep.edex.plugin.ssha.decoder;
--- ncep/gov.noaa.nws.ncep.ui.nsharp/BigNsharp/textsave.h
+++
@@ -1,4 +0,0 @@
-
-char *createtextsounding(void);
-int save_text(char *filename);
-
