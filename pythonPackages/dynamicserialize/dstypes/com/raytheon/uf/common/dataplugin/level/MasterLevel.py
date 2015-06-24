b59b5d3cd78161e5e4d99d9bb2617ae2d9373f85
David Gillingham <David.Gillingham@raytheon.com>
2015-06-24 15:57:21 -0500

    Omaha #4480: Implement API for retrieving pure python soundings through DAF.
    Change-Id: I11801ce387b2822e3276c4bfa715bc475afbe506
    
    Former-commit-id: f222bee8ed317033e97fcea8d0611d666653402a

--- pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataplugin/level/MasterLevel.py
+++ pythonPackages/dynamicserialize/dstypes/com/raytheon/uf/common/dataplugin/level/MasterLevel.py
@@ -27,6 +27,8 @@
 #    Date            Ticket#       Engineer       Description
 #    ------------    ----------    -----------    --------------------------
 #    05/29/13         2023         dgilling       Initial Creation.
+#    06/29/15         4480         dgilling       Implement __hash__, __eq__
+#                                                 and __str__.
 #
 #
 
@@ -38,6 +40,27 @@ class MasterLevel(object):
         self.unitString = None
         self.type = None
         self.identifier = None
+
+    def __hash__(self):
+        return hash(self.name)
+    
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        else:
+            return self.name == other.name
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __str__(self):
+        retVal = "MasterLevel["
+        retVal += "name=" + str(self.name) + ","
+        retVal += "type=" + str(self.type) + ","
+        retVal += "unit=" + str(self.unitString) + ","
+        retVal += "description=" + str(self.description)
+        retVal += "]"
+        return retVal
 
     def getName(self):
         return self.name
