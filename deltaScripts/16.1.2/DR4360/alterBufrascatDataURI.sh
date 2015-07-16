59995efc8e0576e7dbbeafc0ed35401dad5421b9
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2015-07-16 13:56:33 -0500

    Omaha #4360 DataURI colum changes for bufrascat
    Change-Id: Iab4dc4b429f19637570b0a7e0037b685dc169414
    
    Former-commit-id: 8d6032d4ddc5f34ffd72ecdf8a52e75bbcf33b78

---
+++ deltaScripts/16.1.2/DR4360/alterBufrascatDataURI.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+# DR #4360 - this update script will alter the dataURI columns from bufrascat
+
+PSQL="/awips2/psql/bin/psql"
+
+cmdDir=`dirname $0`
+
+source ${cmdDir}/commonFunctions.sh
+table=bufrascat
+
+# table and constraint names form AScatObs.
+echo "INFO: Start update of ${table} dataURI columns."
+renameConstraint ${table} uk_${table}_datauri_fields
+
+col=windSpd
+echo "INFO: Update ${table}'s ${col}"
+${PSQL} -U awips -d metadata -c "UPDATE ${table} SET ${col}=-9999 where ${col} is NULL ; "
+updateNotNullCol ${table} ${col}
+
+col=satId
+echo "Info Update ${table}'s ${col}"
+${PSQL} -U awips -d metadata -c "UPDATE ${table} SET ${col}=0 where ${col} is NULL ; "
+updateNotNullCol ${table} ${col}
+
+echo "INFO: ${table} dataURI columns updated successfully"
