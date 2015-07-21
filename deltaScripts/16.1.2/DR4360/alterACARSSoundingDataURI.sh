a23c66eaf07351fad1a5f44d27b38f8eccd924f1
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2015-07-21 15:38:52 -0500

    Omaha #4360 DataURI changes for table acarssounding.
    Former-commit-id: c626de1d9fed397fd1c2d9bf186be8d2d2b61b07

---
+++ deltaScripts/16.1.2/DR4360/alterACARSSoundingDataURI.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# DR #4360 - this update script will alter the dataURI columns from acarssounding
+
+PSQL="/awips2/psql/bin/psql"
+
+cmdDir=`dirname $0`
+
+source ${cmdDir}/commonFunctions.sh
+table=acarssounding
+
+# table and constraint names from ACARSSoundingRecord.
+echo "INFO: Start update of ${table} dataURI columns."
+renameConstraint ${table} uk_${table}_datauri_fields
+
+echo "INFO: ${table} dataURI columns updated successfully"
