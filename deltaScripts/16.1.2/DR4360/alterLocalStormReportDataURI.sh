be677f56be77b8c6d1b56490c06a5b74a4a5cb65
Roger Ferrel <Roger.L.Ferrel@raytheon.com>
2015-07-21 07:07:27 -0500

    Omaha #4360 DataURI changes for table lsr.
    Former-commit-id: 4c57fd6dbe81b61c8a3dbedd637a97c69908c41f

---
+++ deltaScripts/16.1.2/DR4360/alterLocalStormReportDataURI.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+# DR #4360 - this update script will alter the dataURI columns from lsr
+
+PSQL="/awips2/psql/bin/psql"
+
+cmdDir=`dirname $0`
+
+source ${cmdDir}/commonFunctions.sh
+table=lsr
+
+# table and constraint names form LocalStormReport.
+echo "INFO: Start update of ${table} dataURI columns."
+renameConstraint ${table} uk_${table}_datauri_fields
+col=eventtype
+echo "Info Update ${table}'s ${col}"
+${PSQL} -U awips -d metadata -c "DELETE from  ${table} where ${col} is NULL ; "
+updateNotNullCol ${table} ${col}
+echo "INFO: ${table} dataURI columns updated successfully"
