91f06183d3d3a6b541e57f4d689b4bb76df3b9b8
Nate Jensen <Nathan.Jensen@raytheon.com>
2015-08-05 17:01:50 -0500

    Omaha #4703 optimized some deriv params for numpy
        removed some unused imports
        cast NaN to float32 for numpy 1.9 safety
        more usage of in place arrays for memory efficiency
    
    Change-Id: I5dc4b365086c740164f3e5c92866f91f8f73ef14
    
    Former-commit-id: b40344398b832d9da80b94327de34ba5efab5740

--- edexOsgi/com.raytheon.uf.common.dataplugin.satellite/utility/common_static/base/derivedParameters/functions/satDivWVIR.py
+++ edexOsgi/com.raytheon.uf.common.dataplugin.satellite/utility/common_static/base/derivedParameters/functions/satDivWVIR.py
@@ -1,27 +1,36 @@
 ##
 # This software was developed and / or modified by Raytheon Company,
 # pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
+#
 # U.S. EXPORT CONTROLLED TECHNICAL DATA
 # This software product contains export-restricted data whose
 # export/transfer/disclosure is restricted by U.S. law. Dissemination
 # to non-U.S. persons whether in the United States or abroad requires
 # an export license or other authorization.
-# 
+#
 # Contractor Name:        Raytheon Company
 # Contractor Address:     6825 Pine Street, Suite 340
 #                         Mail Stop B8
 #                         Omaha, NE 68106
 #                         402.291.0100
-# 
+#
 # See the AWIPS II Master Rights File ("Master Rights File.pdf") for
 # further licensing information.
 ##
 
 # ----------------------------------------------------------------
 # Calculate the derived parameter WV/IR
-# 
+#
 # ----------------------------------------------------------------
+#
+# SOFTWARE HISTORY
+#
+# Date           Ticket#      Engineer      Description
+# ------------   ----------   -----------   -----------
+#                             ???           Initial creation
+# Aug 05, 2015   4703         njensen       cast 0 to uint8
+#
+
 import numpy
 
 ##
@@ -32,14 +41,13 @@ import numpy
 # @param Imager_11_micron_IR : Imager 11 micron IR
 # @return:
 # @rtype: numpy array or scalar
-# 
+#
 def execute(Imager_6pp7hh6pp5_micron_IR_ooWVcc, Imager_11_micron_IR):
- 
+
     uWV = Imager_6pp7hh6pp5_micron_IR_ooWVcc.astype(numpy.uint8)
     WV = (uWV * 0.708).astype(numpy.uint8)
     uIR = Imager_11_micron_IR.astype(numpy.uint8)
-    IR = numpy.where(uIR < 180, 0, uIR)
-    result = numpy.where(IR == 0, WV, IR)
+    IR = numpy.where(uIR < 180, numpy.uint8(0), uIR)
+    result = IR
+    result[IR == 0] = WV
     return result.astype(numpy.int8)
-
-       
