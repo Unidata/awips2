40b43954658ac5fec0b66718807c162c650ee11b
mjames-upc <mjames@unidata.ucar.edu>
2015-08-24 11:39:59 -0600

    scipy directory renaming

---
+++ pythonPackages/scipy/scipy/linalg/tests/test_atlas_version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#
+# Created by: Pearu Peterson, October 2003
+#
+
+from numpy.testing import *
+
+import scipy.linalg.atlas_version
+
+
+# No futher tests possible.
+# Importing atlas_version will print out atlas version.
