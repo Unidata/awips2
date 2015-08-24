984ff08665f65274335bcc4108483431476c26d9
mjames-upc <mjames@unidata.ucar.edu>
2015-08-24 13:42:10 -0600

    updates after moving to qpid 0.32
    Former-commit-id: 8d700c9efd2325f8cb4edbdc4e63044a6da6d996 [formerly c0ce0def37c19a9f49a2acedf3427abc923b597a] [formerly a4767873683fb067551c7c5d064e11b940010d8e [formerly 5de1a2aa7f6f9c509f9a9c9561a430c4f8689ff7]]
    Former-commit-id: 75c50d0a7fde635505c4fe796bb5365d44eb4cca [formerly 70fa8f8fae8bd819ad42054b52fc68819642e7dc]
    Former-commit-id: 463a7a8bacacb18c8b7c3d96bf7659d6a5446955

--- rpms/build/common/rpms.sh
+++ rpms/build/common/rpms.sh
@@ -65,7 +65,7 @@ function buildQPID()
       fi
    fi
    
-   cd ${WORKSPACE}/installers/RPMs/qpid-lib-0.30
+   cd ${WORKSPACE}/installers/RPMs/qpid-lib
    if [ $? -ne 0 ]; then
       echo "ERROR: Failed to build the qpid rpms."
       return 1
@@ -77,16 +77,16 @@ function buildQPID()
       return 1
    fi
 
-   #build 0.30
+   #build
    export AWIPS_II_TOP_DIR
-   cd ${WORKSPACE}/installers/RPMs/qpid-java-broker-0.30
+   cd ${WORKSPACE}/installers/RPMs/qpid-java-broker
    if [ $? -ne 0 ]; then
-      echo "ERROR: Failed to build Qpid Broker v0.30."
+      echo "ERROR: Failed to build Qpid Broker"
       return 1
    fi
    /bin/bash build.sh
    if [ $? -ne 0 ]; then
-      echo "ERROR: Failed to build Qpid Broker v0.30."
+      echo "ERROR: Failed to build Qpid Broker"
       return 1
    fi
 
