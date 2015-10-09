b8d2c1cbd9e564a6b0d892ad72bcdab483a21cb5
mjames-upc <mjames@unidata.ucar.edu>
2015-10-09 13:38:42 -0600

    LDM installs to /awips2/ldm as of build 14.4.1-7n8
    Former-commit-id: 6108653e33e92f07cab4f6a3d45f797c0d399080

--- rpms/awips2.upc/Installer.ldm/patch/profile.d/awipsLDM.sh
+++ rpms/awips2.upc/Installer.ldm/patch/profile.d/awipsLDM.sh
@@ -1,13 +1,13 @@
 CHECK_PATH=`echo ${LD_LIBRARY_PATH}`
 if [ "${CHECK_PATH}" = "" ]; then
-   export LD_LIBRARY_PATH=/usr/local/ldm/lib
+   export LD_LIBRARY_PATH=/awips2/ldm/lib
 else
-   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/ldm/lib
+   export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/awips2/ldm/lib
 fi
 
 CHECK_PATH=`echo ${PATH}`
 if [ "${CHECK_PATH}" = "" ]; then
-   export PATH=/usr/local/ldm/bin:/usr/local/ldm/decoders:/usr/local/ldm/util
+   export PATH=/awips2/ldm/bin:/awips2/ldm/decoders:/awips2/ldm/util
 else
-   export PATH=${PATH}:/usr/local/ldm/bin:/usr/local/ldm/decoders:/usr/local/ldm/util
+   export PATH=${PATH}:/awips2/ldm/bin:/awips2/ldm/decoders:/awips2/ldm/util
 fi
