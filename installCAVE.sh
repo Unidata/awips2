248e078f488a87c0cbbe448e35dad6a10694f245
mjames-upc <mjames@unidata.ucar.edu>
2015-10-14 15:04:47 -0600

    install scripts for CAVE and EDEX
    Former-commit-id: fcde3ecd40fb8d4364dcaf76d25ae2987eab94ff

---
+++ installCAVE.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -f
+#
+# installCAVE.sh - a short script to manage the yum repo setup and install
+#                  of AWIPS II CAVE
+#
+# 10/15         mjames@ucar.edu         Creation
+#
+if [ ! -f /etc/yum.repos.d/awips2.repo ]; then
+  echo ''
+  echo 'Downloading awips2repo yum file to /etc/yum.repos.d/awips2.repo'
+  echo ''
+  wget -O /etc/yum.repos.d/awips2.repo http://www.unidata.ucar.edu/software/awips2/doc/awips2.repo
+fi
+echo ''
+echo "Running 'yum clean all'"
+echo ''
+yum clean all
+echo ''
+echo "Running 'yum groupinstall awips2-cave'"
+echo ''
+yum groupinstall awips2-cave -y 2>&1 | tee -a /tmp/cave-install.log
+
+if getent passwd awips &>/dev/null; then
+  echo ''
+  echo "Setting permissions to user awips:fxalpha"
+  /bin/chown -R awips:fxalpha /awips2/cave /awips2/alertviz
+else
+  echo ''
+  echo "--- user awips does not exist"
+  echo "--- you should set owner/group permissions for /awips2/cave and /awips2/alertviz:"
+  echo "tried to run 'chown -R awips:fxalpha /awips2/cave /awips2/alertviz'"
+fi
+echo ""
+echo "Done..."
+echo ""
+exit
---
+++ installEDEX.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -f
+#
+# installEDEX.sh - a short script to manage the yum repo setup and install
+#                  of AWIPS II EDEX
+#
+# 10/15         mjames@ucar.edu         Creation
+#
+if [ ! -f /etc/yum.repos.d/awips2.repo ]; then
+  echo ''
+  echo 'Downloading awips2repo yum file to /etc/yum.repos.d/awips2.repo'
+  echo ''
+  wget -O /etc/yum.repos.d/awips2.repo http://www.unidata.ucar.edu/software/awips2/doc/awips2.repo
+fi
+echo ''
+echo "Running 'yum clean all'"
+echo ''
+yum clean all
+echo ''
+echo "Running 'yum groupinstall awips2-server'"
+echo ''
+yum groupinstall awips2-server -y 2>&1 | tee -a /tmp/edex-install.log
+
+if getent passwd awips &>/dev/null; then
+  echo -n ''
+else
+  echo ''
+  echo "--- user awips does not exist"
+  echo "--- you should set owner/group permissions for directories in /awips2/"
+fi
+echo ""
+echo "Done..."
+echo ""
+exit
