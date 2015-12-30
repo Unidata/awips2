ca996a2a8680090e29b8bf77bc08110ccf6f41a9
mjames-upc <mjames@ucar.edu>
2015-12-30 14:02:46 -0700

    cleanup unused and redundant directories on working branch

--- deltaScripts/14.3.1/DR2667/renameLightningSourceInBundles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script will rename the lightning source column in any D2D bundle files
-# This update is only for edex servers which host the cave localization files
-
-MY_DIR=`dirname $0`
-bash $MY_DIR/utility/updateLightningNameInXML.sh -b
-exit 0
-
