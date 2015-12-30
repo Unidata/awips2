87d626a78baf71bb27e36d2666585a7145db90f4
mjames-upc <mjames@ucar.edu>
2015-12-30 14:02:46 -0700

    cleanup unused and redundant directories on working branch
    Former-commit-id: ca996a2a8680090e29b8bf77bc08110ccf6f41a9

--- deltaScripts/13.6.1/createDDSystemStatusTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS ddsystemstatus
-(
-  name character varying(255) NOT NULL,
-  systemtype character varying(255) NOT NULL,
-  status character varying(255),
-  CONSTRAINT ddsystemstatus_pkey PRIMARY KEY (name, systemtype)
-)
--- deltaScripts/15.1.1/DR4522/updateActiveTable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-psql -h dx1 -U awips -d metadata -f updateActiveTable.sql
