8e41b02d0a458fba8f10af04db80139400cd991c
mjames-upc <mjames@ucar.edu>
2015-12-31 10:12:03 -0700

    path updates, some inconsequential
    Former-commit-id: 67e12bdb0510cc90073e83d674909840829eee84

--- rpms/awips2.core/Installer.edex-environment/wes2bridge.files/build/build.sh
+++ rpms/awips2.core/Installer.edex-environment/wes2bridge.files/build/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # User variables for local builds:
-export U_RPM_DESTINATION="/home/bkowal/rpmbuild"
+export U_RPM_DESTINATION="/home/mjames/rpmbuild"
 export U_COMPONENT_VERSION="11.9.0"
 export U_COMPONENT_RELEASE="1"
 export U_BUILD_ROOT="/tmp/${USER}/awips2-build"
 export U_UFRAME_ECLIPSE="/awips2/eclipse"
-export U_BASELINE_WORKSPACE="/common/bkowal/workspace"
+export U_BASELINE_WORKSPACE="/awips2/jenkins/buildspace/workspace"
 
 # build variables
 if [ "${RPM_DESTINATION}" = "" ]; then
