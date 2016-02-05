6f585b897f3071ac6860a969ddcb44ce2cd2ec60
Tom Gurney <thomas.t.gurney@raytheon.com>
2016-02-05 11:40:49 -0600

    Omaha #5310 Drop dataURI column from bufrncwf
    Change-Id: Ic6290db7b3f615fe188b2399d7466d145967ba63
    
    Former-commit-id: ec59ebd66b91d39117b405f01cccf7bfc006dac1

--- edexOsgi/com.raytheon.uf.edex.plugin.bufrncwf/src/com/raytheon/uf/edex/plugin/bufrncwf/BUFRncwfDao.java
+++ edexOsgi/com.raytheon.uf.edex.plugin.bufrncwf/src/com/raytheon/uf/edex/plugin/bufrncwf/BUFRncwfDao.java
@@ -19,78 +19,37 @@
  **/
 package com.raytheon.uf.edex.plugin.bufrncwf;
 
-import java.util.List;
-
 import com.raytheon.uf.common.dataplugin.PluginException;
 import com.raytheon.uf.common.dataplugin.bufrncwf.BUFRncwf;
-import com.raytheon.uf.edex.database.DataAccessLayerException;
 import com.raytheon.uf.edex.pointdata.PointDataPluginDao;
 
 /**
- * TODO Add Description
+ * DAO for BUFRncwf
  * 
  * <pre>
- *
+ * 
  * SOFTWARE HISTORY
- *
+ * 
  * Date         Ticket#    Engineer    Description
  * ------------ ---------- ----------- --------------------------
  * Aug 17, 2009            jkorman     Initial creation
- *
+ * Feb 04, 2016 5310       tgurney     Remove dead code
+ * 
  * </pre>
- *
+ * 
  * @author jkorman
- * @version 1.0	
+ * @version 1.0
  */
 
 public class BUFRncwfDao extends PointDataPluginDao<BUFRncwf> {
 
-    
     /**
-     * Creates a new ReccoDao
-     * @throws PluginException 
+     * Creates a new BUFRncwfDao
+     * 
+     * @throws PluginException
      */
     public BUFRncwfDao(String pluginName) throws PluginException {
         super(pluginName);
-    }
-
-    /**
-     * Retrieves an bufrua report using the datauri .
-     * 
-     * @param dataURI
-     *            The dataURI to match against.
-     * @return The report record if it exists.
-     */
-    public BUFRncwf queryByDataURI(String dataURI) {
-        BUFRncwf report = null;
-        List<?> obs = null;
-        try {
-            obs = queryBySingleCriteria("dataURI", dataURI);
-        } catch (DataAccessLayerException e) {
-            e.printStackTrace();
-        }
-        if((obs != null)&&(obs.size() > 0)) {
-            report = (BUFRncwf) obs.get(0);
-        }
-        return report;
-    }
-    
-    /**
-     * Queries for to determine if a given data uri exists on the bufrua table.
-     * 
-     * @param dataUri
-     *            The DataURI to find.
-     * @return An array of objects. If not null, there should only be a single
-     * element.
-     */
-    public Object[] queryDataUriColumn(final String dataUri) {
-
-        String sql = "select datauri from awips.bufrncwfua where datauri='"
-                + dataUri + "';";
-
-        Object[] results = executeSQLQuery(sql);
-
-        return results;
     }
 
     @Override
