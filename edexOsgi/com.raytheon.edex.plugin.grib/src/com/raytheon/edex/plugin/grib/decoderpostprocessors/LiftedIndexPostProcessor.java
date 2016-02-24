3bcda660747c0aa75a3d62ee4e41fdc4a50fd053
Melissa Porricel <Melissa.Porricelli@noaa.gov>
2016-02-24 13:42:59 -0500

    ASM #17308 - Correct lifted index units
    Change-Id: I872a48255c137d7221b6cd2be7f70e9d2171d617
    
    Former-commit-id: 3b5ddbed8f115eba688e0b48893a910e1f036972

--- edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/LiftedIndexPostProcessor.java
+++ edexOsgi/com.raytheon.edex.plugin.grib/src/com/raytheon/edex/plugin/grib/decoderpostprocessors/LiftedIndexPostProcessor.java
@@ -19,34 +19,61 @@
  **/
 package com.raytheon.edex.plugin.grib.decoderpostprocessors;
 
-import com.raytheon.edex.plugin.grib.exception.GribException;
+import java.util.Arrays;
+import java.util.List;
 
+import javax.measure.converter.UnitConverter;
+import javax.measure.unit.SI;
+
+import com.raytheon.edex.plugin.grib.exception.GribException;
+import com.raytheon.uf.common.dataplugin.grid.GridRecord;
 
 /**
  * 
- * @deprecated Only exists for backwards compatibility in localization files,
- *             use TemperatureCorrectionPostProcessor directly in the future.
+ * Adjusts data values on Lifted Index to store as Celcius values.
  * 
- *             <pre>
+ * <pre>
  * 
  * SOFTWARE HISTORY
  * 
- * Date          Ticket#  Engineer    Description
- * ------------- -------- ----------- --------------------------
- * Nov 18, 2010           bsteffen    Initial creation
- * Mar 28, 2010  2874     bsteffen    Deprecated
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Nov 18, 2010            bsteffen     Initial creation
+ * Mar 28, 2010  2874      bsteffen     Deprecated
+ * Jan 07, 2016  DR17308   MPorricelli  Reinstated 
  * 
  * </pre>
  * 
  * @author bsteffen
  * @version 1.0
  */
-@Deprecated
-public class LiftedIndexPostProcessor extends
-        TemperatureCorrectionPostProcessor {
+public class LiftedIndexPostProcessor extends DecoderPostProcessor {
 
-    public LiftedIndexPostProcessor() throws GribException {
-        super();
+    private static final List<String> LIFTED_INDEX_PARAMETERS = Arrays.asList(
+            "SLI", "BLI", "PLI");
+
+    private static final String KELVIN_UNIT_STRING = "K";
+
+    private static final float MAX_VALID = 150;
+
+    private static final UnitConverter k2c = SI.KELVIN
+            .getConverterTo(SI.CELSIUS);
+
+    @Override
+    public GridRecord[] process(GridRecord record) throws GribException {
+        // Change units of Lifted Index to match the data
+        if (LIFTED_INDEX_PARAMETERS.contains(record.getParameter()
+                .getAbbreviation())
+                && KELVIN_UNIT_STRING.equals(record.getParameter()
+                        .getUnitString())) {
+            float[] data = (float[]) record.getMessageData();
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] > MAX_VALID) {
+                    data[i] = (float) k2c.convert(data[i]);
+                }
+            }
+        }
+        return new GridRecord[] { record };
     }
 
 }
