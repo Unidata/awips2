08cd6a8fbd15b6e0ba154cb93a9bd87da102afd1
Kiran.Shrestha <Kiran.Shrestha@noaa.gov>
2016-03-11 19:53:49 +0000

    ASM #13302 - Green Times for upper air soundings do not show non-standard times
    Change-Id: Ifebf583404a712c904fdfdd416fcce2d5f54d2ac
    
    Former-commit-id: 58fe55e47aad7683a05cf6198aef145ff88f8947

--- edexOsgi/com.raytheon.edex.plugin.bufrua/src/com/raytheon/edex/plugin/bufrua/decoder/AbstractBUFRUAAdapter.java
+++ edexOsgi/com.raytheon.edex.plugin.bufrua/src/com/raytheon/edex/plugin/bufrua/decoder/AbstractBUFRUAAdapter.java
@@ -55,6 +55,7 @@ import com.raytheon.uf.edex.pointdata.PointDataPluginDao;
  *                                     bufrua.
  * Aug 30, 2013 2298       rjpeter     Make getPluginName abstract
  * May 14, 2014 2536       bclement    moved WMO Header to common, removed TimeTools
+ * Mar 11, 2016 13302      kshrestha   Removed adjustment of valid time hour
  * 
  * </pre>
  * 
@@ -62,11 +63,6 @@ import com.raytheon.uf.edex.pointdata.PointDataPluginDao;
  * @version 1.0
  */
 public abstract class AbstractBUFRUAAdapter extends BUFRPointDataAdapter<UAObs> {
-
-    private static final int[] HOUR_MAP = {
-            // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
-            0, -1, -2, -3, 2, 1, 0, -1, -2, -3, 2, 1, 0, -1, -2, -3, 2, 1, 0,
-            -1, -2, -3, 2, 1 };
 
     private static final int YEAR_POS = 4;
 
@@ -116,9 +112,6 @@ public abstract class AbstractBUFRUAAdapter extends BUFRPointDataAdapter<UAObs> 
                 Calendar validTime = obsData.getDataTime()
                         .getRefTimeAsCalendar();
 
-                // Now offset the "record" validTime using the hour mapping.
-                int hour = validTime.get(Calendar.HOUR_OF_DAY);
-                validTime.add(Calendar.HOUR_OF_DAY, HOUR_MAP[hour]);
                 // Set the new validTime back into the UAObs record.
 
                 Calendar maxFutureTime = Calendar.getInstance();
