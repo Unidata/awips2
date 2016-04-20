b47a9f89f5239674c873e350e729a4ba11e61f23
mjames-upc <mjames@unidata.ucar.edu>
2016-04-20 15:34:32 -0600

    generated WFO localization scales

--- localization/localization/utility/cave_static/site/createSiteMenus.sh
+++ localization/localization/utility/cave_static/site/createSiteMenus.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-#cd /home/awips/awips2-builds/localization/localization/utility/edex_static/site
-#cd /home/awips/awips2-builds/localization/localization/utility/common_static/site
-cd /home/awips/awips2-builds/localization/localization/utility/cave_static/site
+#cd /home/mjames/awips2-builds/localization/localization/utility/edex_static/site
+#cd /home/mjames/awips2-builds/localization/localization/utility/common_static/site
+cd /home/mjames/awips2-builds/localization/localization/utility/cave_static/site
 
 
 # need
 #site = OAX, OKC, etc.
 file=wfo.dat
-for site in $(cat /home/awips/awips2-builds/localization/localization/utility/cave_static/site/$file |cut -c -3)
+for site in $(cat /home/mjames/awips2-builds/localization/localization/utility/cave_static/site/$file |cut -c -3)
 do
    #cat $file |grep $site
    lat=$(cat $file |grep $site | cut -f9)
@@ -23,3 +23,6 @@ do
    grep -rl 'LONGITUDE' $site | xargs sed -i 's/LONGITUDE/'$lon'/g'
 
 done
+
+tar -cf site.tar --exclude='XXX' --exclude='createSiteMenus.sh' --exclude='wfo.dat' .
+scp site.tar awips@edex:
