89359bc2a8c5ddad32365b7c8837e0500a715a4e
mjames-upc <mjames@ucar.edu>
2016-04-20 15:29:32 -0500

    localization scale templates and generation script

---
+++ localization/localization/utility/cave_static/site/createSiteMenus.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+#cd /home/awips/awips2-builds/localization/localization/utility/edex_static/site
+#cd /home/awips/awips2-builds/localization/localization/utility/common_static/site
+cd /home/awips/awips2-builds/localization/localization/utility/cave_static/site
+
+
+# need
+#site = OAX, OKC, etc.
+file=wfo.dat
+for site in $(cat /home/awips/awips2-builds/localization/localization/utility/cave_static/site/$file |cut -c -3)
+do
+   #cat $file |grep $site
+   lat=$(cat $file |grep $site | cut -f9)
+   lon=$(cat $file |grep $site | cut -f10)
+   echo $site, $lat, $lon
+
+   rm -rf $site
+   cp -R XXX $site
+   grep -rl 'XXX' $site | xargs sed -i 's/XXX/'$site'/g'
+   grep -rl 'LATITUDE' $site | xargs sed -i 's/LATITUDE/'$lat'/g'
+   grep -rl 'LONGITUDE' $site | xargs sed -i 's/LONGITUDE/'$lon'/g'
+
+done
