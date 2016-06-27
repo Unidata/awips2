7d9227f0eacbe1198d0b0024972a5838a3be147e
mjames-upc <mjames@ucar.edu>
2016-06-27 15:02:08 -0500

    obs utility additions: derived parms, colored plots, radar colormaps
    Former-commit-id: 641fddcfbc639e7a55b7995e12be54eccd9e771f

---
+++ edexOsgi/com.raytheon.uf.common.dataplugin.obs/utility/common_static/base/derivedParameters/functions/filterObsWind.py
@@ -0,0 +1,104 @@
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+
+import numpy as np
+from numpy import *
+np.set_printoptions(suppress=True)
+
+# Wind < 35 ---- FOR SPD
+def execute(spd):
+    for count, i in enumerate(spd):
+        if i >= 35:
+            spd[count]=-9999
+        if np.isnan(i):
+            spd[count]=-9999
+    return spd
+
+# 35 =< Wind < 50 ---- FOR SPD
+def execute2(spd):
+    for count, i in enumerate(spd):
+        if (i < 35) or (i >=50):
+            spd[count]=-9999
+        if np.isnan(i):
+            spd[count]=-9999
+    return spd
+
+# Wind >= 50 ---- FOR SPD
+def execute3(spd):
+    for count, i in enumerate(spd):
+        if i < 50:
+            spd[count]=-9999
+        if np.isnan(i):
+            spd[count]=-9999
+    return spd
+
+# Wind < 35 ---- FOR GST
+def execute4(gst):
+    for count, i in enumerate(gst):
+        if i >= 35:
+            gst[count]=-9999
+        if np.isnan(i):
+            gst[count]=-9999
+    return gst
+
+# 35 =< Wind < 50 ---- FOR GST
+def execute5(gst):
+    for count, i in enumerate(gst):
+        if (i < 35) or (i >=50):
+            gst[count]=-9999
+        if np.isnan(i):
+            gst[count]=-9999
+    return gst
+
+# Wind >= 50 ---- FOR GST
+def execute6(gst):
+    for count, i in enumerate(gst):
+        if i < 50:
+            gst[count]=-9999
+        if np.isnan(i):
+            gst[count]=-9999
+    return gst
+
+# Wind < 35 ---- FOR DIR
+def execute7(spd, gst, dir):
+    for count, i in enumerate(spd):
+        if (i >= 35) or (gst[count] >= 35):
+            dir[count]=-9999
+        if np.isnan(dir[count]):
+            dir[count]=-9999
+    return dir
+
+# 35 =< Wind < 50 ---- FOR DIR
+def execute8(spd, gst, dir):
+    for count, i in enumerate(spd):
+        if ((i < 35) or (i >=50)) or ((gst[count] < 35) or (gst[count] >=50)):
+            dir[count]=-9999
+        if np.isnan(dir[count]):
+            dir[count]=-9999
+    return dir
+
+# Wind >= 50 ---- FOR DIR
+def execute9(spd, gst, dir):
+    for count, i in enumerate(spd):
+        if (i < 50) or (gst[count] < 50):
+            dir[count]=-9999
+        if np.isnan(dir[count]):
+            dir[count]=-9999
+    return dir
---
+++ edexOsgi/com.raytheon.uf.common.dataplugin.obs/utility/common_static/base/derivedParameters/functions/filterObscCigsSym.py
@@ -0,0 +1,130 @@
+##
+# This software was developed and / or modified by Raytheon Company,
+# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+# 
+# U.S. EXPORT CONTROLLED TECHNICAL DATA
+# This software product contains export-restricted data whose
+# export/transfer/disclosure is restricted by U.S. law. Dissemination
+# to non-U.S. persons whether in the United States or abroad requires
+# an export license or other authorization.
+# 
+# Contractor Name:        Raytheon Company
+# Contractor Address:     6825 Pine Street, Suite 340
+#                         Mail Stop B8
+#                         Omaha, NE 68106
+#                         402.291.0100
+# 
+# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+# further licensing information.
+##
+
+import numpy as np
+from numpy import *
+
+#MVFR
+def execute(cig, index, cigBase):
+    outerLen=len(cig)
+    innerLen=len(cig[0])
+    cbOuterDim=len(cigBase)
+    cbInnerDim=len(cigBase[0])    
+    endElement=outerLen*innerLen
+    if cbOuterDim == innerLen:
+        outerCount = 0
+        innerCount = 0     
+        for counter1, i in enumerate(cig):
+            for counter2, j in enumerate(i):
+                if ("BKN" not in j) and ("OVC" not in j) and ("VV" not in j):
+                    cig[counter1][counter2]=''
+                if (cigBase[outerCount][innerCount] > 3000) or (cigBase[outerCount][innerCount] < 1000):
+                    cig[counter1][counter2]=''
+                innerCount = innerCount + 1
+                if innerCount > (cbInnerDim-1):
+                    innerCount = 0
+                    outerCount = outerCount + 1
+    else:
+        target = open('/tmp/targetFile.txt','w')
+        target.write('cig and cigBase dimensions dont match')
+        target.close()
+    cig = cig.flatten() 
+    return cig[index:endElement:6]
+
+#IFR
+def execute2(cig, index, cigBase):
+    outerLen=len(cig)
+    innerLen=len(cig[0])
+    cbOuterDim=len(cigBase)
+    cbInnerDim=len(cigBase[0])    
+    endElement=outerLen*innerLen
+    if cbOuterDim == innerLen:
+        outerCount = 0
+        innerCount = 0     
+        for counter1, i in enumerate(cig):
+            for counter2, j in enumerate(i):
+                if ("BKN" not in j) and ("OVC" not in j) and ("VV" not in j):
+                    cig[counter1][counter2]=''
+                if (cigBase[outerCount][innerCount] >= 1000) or (cigBase[outerCount][innerCount] < 500):
+                    cig[counter1][counter2]=''
+                innerCount = innerCount + 1
+                if innerCount > (cbInnerDim-1):
+                    innerCount = 0
+                    outerCount = outerCount + 1
+    else:
+        target = open('/tmp/targetFile.txt','w')
+        target.write('cig and cigBase dimensions dont match')
+        target.close()
+    cig = cig.flatten() 
+    return cig[index:endElement:6]
+
+#LIFR
+def execute3(cig, index, cigBase):
+    outerLen=len(cig)
+    innerLen=len(cig[0])
+    cbOuterDim=len(cigBase)
+    cbInnerDim=len(cigBase[0])    
+    endElement=outerLen*innerLen
+    if cbOuterDim == innerLen:
+        outerCount = 0
+        innerCount = 0     
+        for counter1, i in enumerate(cig):
+            for counter2, j in enumerate(i):
+                if ("BKN" not in j) and ("OVC" not in j) and ("VV" not in j):
+                    cig[counter1][counter2]=''
+                if cigBase[outerCount][innerCount] >= 500:
+                    cig[counter1][counter2]=''
+                innerCount = innerCount + 1
+                if innerCount > (cbInnerDim-1):
+                    innerCount = 0
+                    outerCount = outerCount + 1
+    else:
+        target = open('/tmp/targetFile.txt','w')
+        target.write('cig and cigBase dimensions dont match')
+        target.close()
+    cig = cig.flatten() 
+    return cig[index:endElement:6]
+
+#VFR
+def execute4(cig, index, cigBase):
+    outerLen=len(cig)
+    innerLen=len(cig[0])
+    cbOuterDim=len(cigBase)
+    cbInnerDim=len(cigBase[0])    
+    endElement=outerLen*innerLen
+    if cbOuterDim == innerLen:
+        outerCount = 0
+        innerCount = 0     
+        for counter1, i in enumerate(cig):
+            for counter2, j in enumerate(i):
+                if ("BKN" not in j) and ("OVC" not in j) and ("VV" not in j):
+                    cig[counter1][counter2]=''
+                if cigBase[outerCount][innerCount] <= 3000:
+                    cig[counter1][counter2]=''
+                innerCount = innerCount + 1
+                if innerCount > (cbInnerDim-1):
+                    innerCount = 0
+                    outerCount = outerCount + 1
+    else:
+        target = open('/tmp/targetFile.txt','w')
+        target.write('cig and cigBase dimensions dont match')
+        target.close()
+    cig = cig.flatten() 
+    return cig[index:endElement:6]
