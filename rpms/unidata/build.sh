2588519baa28aab5a7f1f404813b70543c22faaf
mjames-upc <mjames@unidata.ucar.edu>
2017-03-20 19:29:31 -0600

    build group python

--- rpms/unidata/build.sh
+++ rpms/unidata/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -v
 set -xe
-rm -rf /awips2/jenkins/buildspace/workspace/AWIPS2-UPC_build/baseline/
+if  [ ${2} = "buildCAVE"]; then
+  rm -rf /awips2/jenkins/buildspace/workspace/AWIPS2-UPC_build/baseline/
+fi
 export JENKINS_WORKSPACE=/awips2/repo/awips2-builds
 
 # determine where we are ...
