4ae04df544ce6edb29e20e45dd799f8a742100a3
Michael James <mjames@ucar.edu>
2017-06-16 09:46:38 -0600

    edex start script cleanup; bump for grib jvm

--- build/deploy.edex.awips2/esb/etc/ingestGrib.sh
+++ build/deploy.edex.awips2/esb/etc/ingestGrib.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
-##
 numCores=`grep -c ^processor /proc/cpuinfo`
 
+# 8 decoder threads for 16 cpus
 let "GRIB_DECODE_THREADS=numCores / 2"
 
 if [ $GRIB_DECODE_THREADS -gt 12 ]; then
@@ -29,12 +11,12 @@ elif [ $GRIB_DECODE_THREADS -lt 4 ]; then
 fi
 
 # sets bounds based on # of threads available
-let "MAX_MEM = GRIB_DECODE_THREADS * 196" # in Meg
-let "GRIB_MAX_GRID_POINTS = GRIB_DECODE_THREADS * 25000000"
-let "GRID_PERSIST_THREADS = GRIB_DECODE_THREADS / 2"
+let "MAX_MEM = GRIB_DECODE_THREADS * 576" # 4608 Meg (4.5 GB)
+let "GRIB_MAX_GRID_POINTS = GRIB_DECODE_THREADS * 25000000" # 200,000,000
+let "GRID_PERSIST_THREADS = GRIB_DECODE_THREADS / 2" # 4
 let "GRID_POSTPROCESS_THREADS = 1"
-let "GRID_MAX_PERSIST_MEMORY_IN_MB = GRID_PERSIST_THREADS * 50"
-let "METADATA_POOL_MAX = GRIB_DECODE_THREADS * 2"
+let "GRID_MAX_PERSIST_MEMORY_IN_MB = GRID_PERSIST_THREADS * 128" # 1 GB
+let "METADATA_POOL_MAX = GRIB_DECODE_THREADS * 4" # 16
 
 export INIT_MEM=128 # in Meg
 export MAX_MEM
@@ -44,7 +26,6 @@ export GRID_PERSIST_THREADS
 export GRID_POSTPROCESS_THREADS
 export GRID_MAX_PERSIST_MEMORY_IN_MB
 export METADATA_POOL_MAX
-
 export EDEX_DEBUG_PORT=5007
 export EDEX_JMX_PORT=1618
 export MGMT_PORT=9603
