7987c58d9039ddabcd196c357487ffd4a6409e17
mjames-upc <mjames@unidata.ucar.edu>
2017-07-07 11:00:41 -0600

    wipe buildspace/workspace/tmp/mjames for proper cave build

--- rpms/unidata/build.sh
+++ rpms/unidata/build.sh
@@ -2,6 +2,7 @@
 set -xe
 if  [ ${2} = "buildCAVE"]; then
   rm -rf /awips2/jenkins/buildspace/workspace/AWIPS2-UPC_build/baseline/
+  rm -rf /awips2/jenkins/buildspace/workspace/tmp/mjames/
 fi
 export JENKINS_WORKSPACE=/awips2/repo/awips2-builds
 
