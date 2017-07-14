207ffbeb518a25ae8ea66190323a30c469cfab8a
Michael James <mjames@ucar.edu>
2017-07-14 09:18:56 -0600

    macOS build gridslice library

--- nativeLib/gridslice/src/gridslice.c
+++ nativeLib/gridslice/src/gridslice.c
@@ -1,23 +1,3 @@
-/*****************************************************************************************
- * COPYRIGHT (c), 2009, RAYTHEON COMPANY
- * ALL RIGHTS RESERVED, An Unpublished Work
- *
- * RAYTHEON PROPRIETARY
- * If the end user is not the U.S. Government or any agency thereof, use
- * or disclosure of data contained in this source code file is subject to
- * the proprietary restrictions set forth in the Master Rights File.
- *
- * U.S. GOVERNMENT PURPOSE RIGHTS NOTICE
- * If the end user is the U.S. Government or any agency thereof, this source
- * code is provided to the U.S. Government with Government Purpose Rights.
- * Use or disclosure of data contained in this source code file is subject to
- * the "Government Purpose Rights" restriction in the Master Rights File.
- *
- * U.S. EXPORT CONTROLLED TECHNICAL DATA
- * Use or disclosure of data contained in this source code file is subject to
- * the export restrictions set forth in the Master Rights File.
- ******************************************************************************************/
-
 /*
  * Python module that utilizes the AWIPSI sliceConvert functions to offer
  * slicing capability to numpy arrays.
