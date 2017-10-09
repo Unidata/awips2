5ded1abca782bcadc3d99f834a49eefe791915a4
mjames-upc <mjames@unidata.ucar.edu>
2017-10-09 17:20:05 -0600

    cleanup envvars for edex esb start.sh and ingestGrib.sh

--- build/deploy.edex.awips2/esb/etc/ingestGrib.sh
+++ build/deploy.edex.awips2/esb/etc/ingestGrib.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
-numCores=`grep -c ^processor /proc/cpuinfo`
+# See definitions in /awips2/edex/conf/resources/com.raytheon.edex.plugin.grib.properties
 
-# 8 decoder threads for 16 cpus
-let "GRIB_DECODE_THREADS=numCores / 2"
-
-if [ $GRIB_DECODE_THREADS -gt 12 ]; then
-    GRIB_DECODE_THREADS=12
-elif [ $GRIB_DECODE_THREADS -lt 4 ]; then
-    GRIB_DECODE_THREADS=4
-fi
-
-# sets bounds based on # of threads available
-let "MAX_MEM = GRIB_DECODE_THREADS * 576" # 4608 Meg (4.5 GB)
-let "GRIB_MAX_GRID_POINTS = GRIB_DECODE_THREADS * 25000000" # 200,000,000
-let "GRID_PERSIST_THREADS = GRIB_DECODE_THREADS / 2" # 4
-let "GRID_POSTPROCESS_THREADS = 1"
-let "GRID_MAX_PERSIST_MEMORY_IN_MB = GRID_PERSIST_THREADS * 128" # 1 GB
-let "METADATA_POOL_MAX = GRIB_DECODE_THREADS * 4" # 16
+let "MAX_MEM = GRIB_DECODE_THREADS * 576"
+let "GRIB_MAX_GRID_POINTS = GRIB_DECODE_THREADS * 25000000"
+let "METADATA_POOL_MAX = GRIB_DECODE_THREADS * 4"
 
 export INIT_MEM=128 # in Meg
 export MAX_MEM
 export GRIB_DECODE_THREADS
 export GRIB_MAX_GRID_POINTS
 export GRID_PERSIST_THREADS
-export GRID_POSTPROCESS_THREADS
+export GRID_POSTPROCESS_THREADS=1
 export GRID_MAX_PERSIST_MEMORY_IN_MB
 export METADATA_POOL_MAX
 export EDEX_DEBUG_PORT=5007
 export EDEX_JMX_PORT=1618
 export MGMT_PORT=9603
-
