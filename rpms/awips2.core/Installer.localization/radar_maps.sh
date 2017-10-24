c5ade346163cdcdadca45d119657efaf26458078
Michael James <mjames@ucar.edu>
2017-10-24 10:51:30 -0600

    two-pane radar map update for macOS remove interstate overlay

--- rpms/awips2.core/Installer.localization/radar_maps.sh
+++ rpms/awips2.core/Installer.localization/radar_maps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-BUILD_DIR=/home/mjames/awips2-builds/rpms/awips2.core/Installer.localization/
+UHOME=/Users/mjames
+BUILD_DIR=$UHOME/awips2-builds/rpms/awips2.core/Installer.localization/
 CAVE_DIR=$BUILD_DIR/utility/cave_static/bundles/scales/radar/
 mkdir -p $CAVE_DIR
 rm -rf $CAVE_DIR/*
@@ -21,28 +21,28 @@ do
 
    sitell=$(echo $site | tr '[:upper:]' '[:lower:]')
    cp -R $BUILD_DIR/RadarTwoPanel.xml $CAVE_DIR/Radar_$sitell.xml
-   grep -rl 'LOWX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/LOWX/'$lowx'/g'
-   grep -rl 'HIGHX' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/HIGHX/'$highx'/g'
-   grep -rl 'LOWY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/LOWY/'$lowy'/g'
-   grep -rl 'HIGHY' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/HIGHY/'$highy'/g'
-   grep -rl 'MINX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/MINX/'$minx'/g'
-   grep -rl 'MAXX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/MAXX/'$maxx'/g'
-   grep -rl 'MINY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/MINY/'$miny'/g'
-   grep -rl 'MAXY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/MAXY/'$maxy'/g'
-   grep -rl 'XXX'   $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/XXX/'$site'/g'
-   grep -rl 'xxx'   $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/xxx/'$sitell'/g'
-   grep -rl 'LATITUDE'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/LATITUDE/'$lat'/g'
-   grep -rl 'LONGITUDE' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i 's/LONGITUDE/'$lon'/g'
+   grep -rl 'LOWX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/LOWX/'$lowx'/g'
+   grep -rl 'HIGHX' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/HIGHX/'$highx'/g'
+   grep -rl 'LOWY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/LOWY/'$lowy'/g'
+   grep -rl 'HIGHY' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/HIGHY/'$highy'/g'
+   grep -rl 'MINX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/MINX/'$minx'/g'
+   grep -rl 'MAXX'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/MAXX/'$maxx'/g'
+   grep -rl 'MINY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/MINY/'$miny'/g'
+   grep -rl 'MAXY'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/MAXY/'$maxy'/g'
+   grep -rl 'XXX'   $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/XXX/'$site'/g'
+   grep -rl 'xxx'   $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/xxx/'$sitell'/g'
+   grep -rl 'LATITUDE'  $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/LATITUDE/'$lat'/g'
+   grep -rl 'LONGITUDE' $CAVE_DIR/Radar_$sitell.xml | xargs sed -i.bak 's/LONGITUDE/'$lon'/g'
    
-   cp $CAVE_DIR/Radar_$sitell.xml /home/mjames/awips2-core/viz/com.raytheon.uf.viz.core.maps/localization/bundles/scales/WSR88D/
+   cp $CAVE_DIR/Radar_$sitell.xml $UHOME/awips2-core/viz/com.raytheon.uf.viz.core.maps/localization/bundles/scales/WSR88D/
 
 done
 
 
-cp $CAVE_DIR/* /home/mjames/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/
-rm -rf /home/mjames/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_comp.xml
-rm -rf /home/mjames/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_info.xml
-rm -rf /home/mjames/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_proc.xml
+cp $CAVE_DIR/* $UHOME/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/
+rm -rf $UHOME/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_comp.xml
+rm -rf $UHOME/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_info.xml
+rm -rf $UHOME/awips2-builds/cave/com.raytheon.viz.radar/localization/bundles/site/Radar_proc.xml
 
 #	com.raytheon.viz.radar/localization/bundles/site/Radar_comp.xml
 #	com.raytheon.viz.radar/localization/bundles/site/Radar_info.xml
