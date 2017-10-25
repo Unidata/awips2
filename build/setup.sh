fc6e59bb55d6524ab4b8a805683ad067081b9c07
mjames-upc <mjames@unidata.ucar.edu>
2017-10-24 19:53:31 -0600

    travis build on el7 cleanup

--- build/setup.sh
+++ build/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -xe
-os_type=$1
-os_version=$2
+os_type=centos
+os_version=centos7
 
 if [ "$os_version" = "centos6" ]; then
     sudo docker run --rm=true -v `pwd`:/awips2/repo/awips2-builds:rw ${os_type}:${os_version} /bin/bash -c "bash -xe /awips2/repo/awips2-builds/build/build_rhel.sh ${os_type} ${os_version}"
