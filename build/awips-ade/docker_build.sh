96878a63399c10fcccd0a54588a82aed8d5bb408
mjames-upc <mjames@unidata.ucar.edu>
2017-12-08 11:37:52 -0700

    cave p2 build, docker builds, repo comps update for 17.1.1-4

--- build/awips-ade/docker_build.sh
+++ build/awips-ade/docker_build.sh
@@ -5,12 +5,12 @@ if [ -z "$1" ]; then
 fi
 os_version=$1
 
-existing=$(sudo docker images |grep awips-ade | awk '{ print $3 }'|uniq)
+existing=$(sudo docker images |grep awips-ade | grep $1 | awk '{ print $3 }')
 if [ ! -z "$existing" ]; then
    sudo docker rmi $existing
 fi
 pushd /awips2/repo/awips2-builds/build/awips-ade
 sudo docker build -t unidata/awips-ade -f Dockerfile.awips-ade.${os_version} .
-dockerID=$(sudo docker ps | grep awips-ade | awk '{print $1}' | head -1 )
+dockerID=$(sudo docker images | grep awips-ade | awk '{print $3}' | head -1 )
 sudo docker tag $dockerID unidata/awips-ade:17.1.1-${os_version} 
 sudo docker push unidata/awips-ade
