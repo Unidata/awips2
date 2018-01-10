69b944806f960771fa7a34e241d1cfb3619f0873
mjames-upc <mjames@unidata.ucar.edu>
2018-01-10 13:10:40 -0700

    build updates for 17.1.1-4

--- build/awips-ade/docker_build.sh
+++ build/awips-ade/docker_build.sh
@@ -13,4 +13,5 @@ pushd /awips2/repo/awips2-builds/build/awips-ade
 sudo docker build -t unidata/awips-ade -f Dockerfile.awips-ade.${os_version} .
 dockerID=$(sudo docker images | grep awips-ade | grep latest | awk '{print $3}' | head -1 )
 sudo docker tag $dockerID unidata/awips-ade:17.1.1-${os_version} 
+sudo docker rmi unidata/awips-ade:latest
 sudo docker push unidata/awips-ade
