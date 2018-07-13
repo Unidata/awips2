bd3cb4e29e7c911e1556031911c5181f37f915b0
mjames-upc <mjames@unidata.ucar.edu>
2018-07-13 15:12:26 -0600

    explicit max mem 4GB for default edex start

--- edexOsgi/deploy.edex.awips2/esb/etc/default.sh
+++ edexOsgi/deploy.edex.awips2/esb/etc/default.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 ##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
-##
-
-##
 # This is the default environment settings file for all EDEX modes. All
 # non-operational and/or development EDEX instances will only use this file.
 # All operational EDEX instances will utilize an instance-specific file
@@ -29,12 +9,7 @@
 # be created for the new operational instance at a minimum. 
 ##
 export INIT_MEM=512 # in Meg
-export MAX_MEM=1300 # in Meg
-
-if [ $HIGH_MEM == "on" ]; then
-    export MAX_MEM=4096
-fi
-
+export MAX_MEM=4096
 export EDEX_DEBUG_PORT=5005
 export METADATA_POOL_MAX=50
 export METADATA_POOL_TIMEOUT=300
@@ -45,7 +20,6 @@ export DEBUG_PARAM_4=""
 export PROFILER_PARAM_1=""
 export PROFILER_PARAM_2=""
 export PYPIES_MAX_CONN=50
-
 
 export SERIALIZE_POOL_MAX_SIZE=16
 export SERIALIZE_STREAM_INIT_SIZE_MB=2
