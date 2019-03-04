39611151f6703b5a4bf335fdb9f2c796cca850f5
mjames-upc <mjames@unidata.ucar.edu>
2019-03-04 14:26:28 -0700

    rm header

--- cave/com.raytheon.viz.gfe/localization/gfe/userPython/smartTools/Serp.py
+++ cave/com.raytheon.viz.gfe/localization/gfe/userPython/smartTools/Serp.py
@@ -1,21 +1,4 @@
 ##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
 ##
 # ----------------------------------------------------------------------------
 # SVN: $Revision: 130 $  $Date: 2010-07-30 17:45:24 +0000 (Fri, 30 Jul 2010) $
--- edexOsgi/com.raytheon.edex.plugin.gfe/utility/common_static/base/gfe/config/Maps.py
+++ edexOsgi/com.raytheon.edex.plugin.gfe/utility/common_static/base/gfe/config/Maps.py
@@ -1,21 +1,4 @@
 ##
-# This software was developed and / or modified by Raytheon Company,
-# pursuant to Contract DG133W-05-CQ-1067 with the US Government.
-# 
-# U.S. EXPORT CONTROLLED TECHNICAL DATA
-# This software product contains export-restricted data whose
-# export/transfer/disclosure is restricted by U.S. law. Dissemination
-# to non-U.S. persons whether in the United States or abroad requires
-# an export license or other authorization.
-# 
-# Contractor Name:        Raytheon Company
-# Contractor Address:     6825 Pine Street, Suite 340
-#                         Mail Stop B8
-#                         Omaha, NE 68106
-#                         402.291.0100
-# 
-# See the AWIPS II Master Rights File ("Master Rights File.pdf") for
-# further licensing information.
 ##
 # NOTE: THIS FILE SHOULD NOT BE USER_MODIFIED.  INSTEAD, REFER TO THE
 # DOCUMENTATION ON HOW ENTRIES IN THIS FILE MAY BE OVERRIDDEN.  REFER TO
