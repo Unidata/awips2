03b9bfb02f10d022ee2b9048e52cf9a1c4d3ed89
Shay Carter <srcarter@ucar.edu>
2021-06-15 21:11:01 -0600

    Modify create.sh for notatization - added logic to sign necessary files, update the minimum SDK, and to make sure to sign using a hardened runtime

--- build/macOS/create.sh
+++ build/macOS/create.sh
@@ -3,7 +3,11 @@
 # Create a signed application bundle from the garbage that Eclipse exports
 #
 # Author: mjames@ucar.edu
-# Last updated: June 12, 2018
+# Author: srcarter@ucar.edu
+# 
+# Updates
+# June 12, 2018 - mjames last updates
+# June 15, 2021 - srcarter updates for notarization errors (hardened runtime when signing)
 #
 workspace="$( cd "$(dirname "$0")" ; pwd -P )"
 if [ -z "$1" ]; then
@@ -33,14 +37,58 @@ rm -rf Cave.app/Contents/MacOS/cave.ini
 cp $workspace/Info.plist Cave.app/Contents/
 cp $workspace/libjep.dylib Cave.app/Contents/Resources/jre/lib/
 
+. ${workspace}/cert.sh
+
 #
 # jspawnhelper must be executable for pydev config within CAVE
 #
-chmod 755 Cave.app/Contents/Resources/jre/lib/jspawnhelper
+jsh=$template/Cave.app/Contents/Resources/jre/lib/jspawnhelper
+chmod 755 $jsh
+# min sdk must be updated
+minSDKCmd=/Users/awips1/dev/minSDK/fix_LC_VERSION_MIN_MACOSX/fixMonoMinVersion
+$minSDKCmd $jsh
+# must also be signed
+codesign --force --options=runtime --sign "${cert}" $jsh
+
+#
+# Need to sign specific packages
+#
+resDir=$template/Cave.app/Contents/Resources
+jreDir=$resDir/jre
+
+# jre/bin
+for file in ${jreDir}/bin/*;
+do
+        $minSDKCmd $file
+	codesign --force --options=runtime --sign "${cert}" $file
+done
+
+# all .dylib files
+find $resDir -name "*.dylib" -exec $minSDKCmd {} \; -exec codesign --force --options=runtime --sign "${cert}" "{}" \;
+
+# eclipse .so
+eclipseSO=$resDir/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.1.401.v20161122-1740/eclipse_1615.so
+codesign --force --options=runtime --sign "${cert}" $eclipseSO
+
+# jython jar -- need to extract a file, sign it, and then update the jar
+jythonDir=$resDir/plugins/org.python.pydev.jython_5.8.0.201706061859/
+jythonLib=jni/Darwin/libjffi-1.2.jnilib
+# extract the file that needs signing
+cd ${jythonDir}
+jar xvf ${jythonDir}/jython.jar ${jythonLib}
+# sign the file
+codesign --force --options=runtime --sign "${cert}" $jythonLib
+# update the jar with this signed file
+jar uf $jythonDir/jython.jar $jythonDir/$jythonLib
+# remove the extracted file
+rm -rf $jythonDir/jni
+cd ${template}
+
+## ----- Done signing included packages -----
 
 #
 # codesign the app
 #
 #codesign --force --sign "Developer ID Application: University Corporation for Atmospheric Research (DQ4ZFL4KLF)" $template/Cave.app
-. ${workspace}/cert.sh
-codesign --force --sign "${cert}" $template/Cave.app
+codesign --force --options=runtime --sign "${cert}" $template/Cave.app
+
