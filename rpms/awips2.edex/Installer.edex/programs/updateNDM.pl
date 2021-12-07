94737af29ed5a70113a4d1065ea74807935a9c3a
Tiffany Meyer <tiffanym@ucar.edu>
2021-12-07 13:11:55 -0500

    Couple more NDM modifications: -Created a script (updateNDM.pl) that runs via cron daily to check the Unidata public repo for new NDM files and copies them into the NDM trigger directory to install -Added a few files to be built into the RPM via component.spec -Added an entry in the cron for awips to run the NDM script

---
+++ rpms/awips2.edex/Installer.edex/programs/updateNDM.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+#This script gets installed with EDEX and a cronjob is setup to run this script daily. This script initially sets up a local git repo and is configured to just checkout the National Dataset files from Unidata's public git repo and then copies the files to /awips2/edex/data/ndm to keep the databases updated with new metar/station information.
+
+$ndmGitPath = "rpms/awips2.edex/Installer.edex/ndm/";
+
+#get the current branch/version of awips2
+$version = `grep AWIPSII_VERSION /awips2/repo/awips2/build/buildEnvironment.sh`;
+chomp $version;
+@split = split(/"/,$version);
+
+$branch = "unidata_$split[1]";
+
+#Check if ndm location has been created
+$ndmDir="/awips2/dev/ndm";
+if(!-d $ndmDir)
+{
+  `mkdir $ndmDir`;
+}
+
+#Check if ndm git files have been checked out yet
+if(!-e "$ndmDir/.git")
+{
+  `cd $ndmDir ; 
+   git init ; 
+   git remote add -f origin https://github.com/Unidata/awips2.git ; 
+   git config core.sparseCheckout true ;
+   echo \"$ndmGitPath\" >> .git/info/sparse-checkout ;
+   git checkout -t origin/$branch
+  `;
+}
+
+print "\n\n\n";
+
+#Pull latest files
+`cd $ndmDir ; 
+ git pull origin $branch
+`;
+
+#copy files to AWIPS-II NDM endpoint
+`rsync -aP $ndmDir/$ndmGitPath /awips2/edex/data/ndm/`;
+
