5eb7dcdb0af75f708f241eed1abc8f76ac95462d
ucar-tmeyer <tiffanym@ucar.edu>
2024-01-12 20:13:07 +0000

    Update crontab and checkFileTime script to be in bash instead of perl so we don't run into python environment issues

---
+++ rpms/awips2.upc/Installer.ldm/patch/dev/checkFileTime.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Concatenate grib and bufr files before ingesting them into edex
+# to reduce the number of jobs edex processes
+#
+# Author: srcarter@ucar.edu
+# Author: tiffanym@ucar.edu
+#
+# Updates
+# Jan 12, 2024 - First stab at translating from our existing perl script
+#
+#
+
+# We want to concatenate grib and bufr files
+paths=("/awips2/data_store/grid" "/awips2/data_store/modelsounding")
+
+for path in "${paths[@]}";
+do
+	# Check for relevant files that haven't been touched in the last two minutes
+	IFS=$'\n'
+	files=($(find $path -name "*-concat-*" -mmin +2 | grep "staging"))
+	unset IFS
+
+#	echo ${#files[@]}
+	for file in "${files[@]}";
+	do
+		/awips2/python/bin/python /awips2/ldm/dev/notifyAWIPS2-unidata.py $file
+#		/awips2/python/bin/python /awips2/fxa/bin/src/qpidNotify.py $file
+		echo $file
+	done
+done
+
